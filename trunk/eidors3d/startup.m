2e1fba59bc765ab1064113a2d2953636a592f412
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2004-06-24 08:22:50 +0000

    subdirs

---
+++ trunk/eidors3d/startup.m
@@ -0,0 +1,10 @@
+% Script to start EIDORS3D
+% Set path and variables correctly
+% $Id: startup.m,v 1.1 2004-06-24 08:22:50 aadler Exp $
+
+addpath('sample_data/');
+addpath('examples/');
+addpath('graphics_matlab/');
+addpath('graphics_vtk/');
+
+stderr=2; % file number of stderr
