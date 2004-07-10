b97f3f76c66e3eb2e912fca405ad800002433063
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2004-07-10 02:40:22 +0000

    move to plugable framework

---
+++ trunk/eidors3d/fwd_solve.m
@@ -0,0 +1,22 @@
+function data = fwd_solve( fwd_model, image)
+% FWD_SOLVE: calculate data from a fwd_model object and an image
+% 
+% fwd_solve can be called as
+%    data= fwd_solve( fwd_model, image)
+% or
+%    data= fwd_solve( image)
+%
+% in each case it will call the fwd_model.solve
+%                      or image.fwd_model.solve method
+%
+% data      is a measurement data structure
+% fwd_model is a fwd_model structure
+% image     is an image structure
+%
+% $Id: fwd_solve.m,v 1.1 2004-07-10 02:40:22 aadler Exp $
+
+if nargin==1
+   image= fwd_model;
+   fwd_model= image.fwd_model;
+end
+data= feval( fwd_model.solve, fwd_model, image);
