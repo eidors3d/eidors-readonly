7fa3a4169d0e42ac670cd11b47cfddcc2b33459f
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2004-07-16 17:06:42 +0000

    graphics functions

--- trunk/eidors3d/graphics_matlab/repaint_inho.m
+++ trunk/eidors3d/graphics_matlab/repaint_inho.m
@@ -4,72 +4,54 @@ function repaint_inho(mat,mat_ref,vtx,simp);
 %Repaints the simulated inhomogeneity according to the reference
 %distribution. (Increase -> Red, Decrease -> Blue) 
 %
-%
-%
 %mat     = The simulated (targeted) distribution.
 %mat_ref = The known initial (homogeneous) distribution.
 %vtx     = The vertices matrix.
 %simp    = The simplices matrix.
+%
+% $Id: repaint_inho.m,v 1.2 2004-07-16 17:06:42 aadler Exp $
 
-for ii=1:length(mat)
-       
-   hold on;
+inhomg= mat - mat_ref;
+hold('on');
+
+for ii= find( inhomg(:)' )
    
    this_x = simp(ii,:);
    
-   if abs(mat(ii)) > abs(mat_ref(ii))
-      
-      
-      Xas = vtx(this_x(1:3),1);
-      Yas = vtx(this_x(1:3),2);
-      Zas = vtx(this_x(1:3),3);
-      
-      patch(Xas,Yas,Zas,'r','EdgeColor','none');
-      
-      Xbs = vtx(this_x([1,2,4]),1);
-      Ybs = vtx(this_x([1,2,4]),2);
-      Zbs = vtx(this_x([1,2,4]),3);
-      patch(Xbs,Ybs,Zbs,'r','EdgeColor','none');
-      
-      
-      Xcs = vtx(this_x([1,3,4]),1);
-      Ycs = vtx(this_x([1,3,4]),2);
-      Zcs = vtx(this_x([1,3,4]),3);
-      patch(Xcs,Ycs,Zcs,'r','EdgeColor','none');
-      
-      
-      Xds = vtx(this_x(2:4),1);
-      Yds = vtx(this_x(2:4),2);
-      Zds = vtx(this_x(2:4),3);
-      patch(Xds,Yds,Zds,'r','EdgeColor','none');
-   
+   if inhomg(ii) > 0
+      colour= 'r';
+   elseif inhomg(ii) < 0;
+      colour= 'b';
+   else
+      error('shouldn''t get here');
    end
-   
-      if abs(mat(ii)) < abs(mat_ref(ii))
-         
+      
+      
       Xas = vtx(this_x(1:3),1);
       Yas = vtx(this_x(1:3),2);
       Zas = vtx(this_x(1:3),3);
-      patch(Xas,Yas,Zas,'b','EdgeColor','none');
+      
+      patch(Xas,Yas,Zas,colour,'EdgeColor','none');
       
       Xbs = vtx(this_x([1,2,4]),1);
       Ybs = vtx(this_x([1,2,4]),2);
       Zbs = vtx(this_x([1,2,4]),3);
-      patch(Xbs,Ybs,Zbs,'b','EdgeColor','none');
+      patch(Xbs,Ybs,Zbs,colour,'EdgeColor','none');
+      
       
       Xcs = vtx(this_x([1,3,4]),1);
       Ycs = vtx(this_x([1,3,4]),2);
       Zcs = vtx(this_x([1,3,4]),3);
-      patch(Xcs,Ycs,Zcs,'b','EdgeColor','none');
+      patch(Xcs,Ycs,Zcs,colour,'EdgeColor','none');
+      
       
       Xds = vtx(this_x(2:4),1);
       Yds = vtx(this_x(2:4),2);
       Zds = vtx(this_x(2:4),3);
-      patch(Xds,Yds,Zds,'b','EdgeColor','none');
-
-     end
+      patch(Xds,Yds,Zds,colour,'EdgeColor','none');
       
 end
+hold('off')
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % This is part of the EIDORS suite.
