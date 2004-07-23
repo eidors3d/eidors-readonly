37942345d8ff72f6ff70caa7d8a4c45e5cf37934
billlion <billlion@82006732-fb7e-4a22-9f28-86a212cce563>
2004-07-23 11:32:35 +0000

    Added netgen examples

---
+++ trunk/eidors3d/meshing/netgen/tankmodel.m
@@ -0,0 +1,50 @@
+% Script to make model of tank in E&EE
+% dimensions in mm
+% height 180mm
+% diam 140
+%diam elec 9mm
+tank_radius = 90
+tank_height = 140
+elecs_per_plane= 16
+no_of_planes = 5
+first_plane_starts = 30
+height_between_centres = 30
+electrode_radius= 9
+%tank_radius = 20
+%tank_height = 35
+%no_of_planes = 2
+%elecs_per_plane= 8
+%first_plane_starts = 10
+%height_between_centres = 15
+%electrode_radius= 2
+eps = 1e-8;
+[fid,mess]=fopen('tank.geo','w');
+fprintf(fid,'#Automatically generated cylinder surface mesh with round electrodes\n');
+fprintf(fid,'algebraic3d\n');
+fprintf(fid,'solid cy=cylinder(0,0,0;0,0,%6.2f;%6.2f) \n',tank_height, ...
+	tank_radius);
+fprintf(fid,' and plane(0,0,0;0,0,-1)\n  and  plane(0,0,%6.2f;0,0,1);\n',tank_height);  
+kelec = 0;
+for l=1:no_of_planes
+ z = first_plane_starts + (l-1)*height_between_centres;
+ for th =0:2*pi/elecs_per_plane:2*pi*(elecs_per_plane-1)/ elecs_per_plane
+    kelec=kelec+1;
+    [x,y]=pol2cart(th,tank_radius);
+	 	 
+    fprintf(fid,'solid el%d = sphere(%6.2f,%6.2f,%6.2f;%6.2f);\n',kelec,x+eps,y+eps,z+eps,electrode_radius );
+ end;
+end;
+nelec = no_of_planes*elecs_per_plane;
+fprintf(fid,'solid el = ');
+for k=1:(nelec-1)
+  fprintf(fid,'el%d or ',k);
+end
+fprintf(fid,'el%d;\n',nelec);
+fprintf(fid,'#cylinder with spheres scooped out\n');
+fprintf(fid,'solid body = cy and not el;\n');
+fprintf(fid,'#the bits which were scooped out\n');
+fprintf(fid,'solid e1 = cy and el;\n');
+fprintf(fid,'#Making two top level objects means they both get meshed.\n');
+fprintf(fid,'tlo body -transparent;\n');
+fprintf(fid,'tlo e1;\n');     
+fclose(fid);
