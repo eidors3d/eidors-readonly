1225e052cbb707bf6041da473fe45147e77bed1a
billlion <billlion@82006732-fb7e-4a22-9f28-86a212cce563>
2004-08-20 20:15:59 +0000

    New use of intersecting cylinders without 'blisters'
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@71 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors3d/meshing/netgen/tankmodel.m
+++ eidors3d/meshing/netgen/tankmodel.m
@@ -1,15 +1,17 @@
 % Script to make model of tank in E&EE
+% 20/8/2004 new version uses intersecting cylinder method to make electrodes without blisters WRBL
 % dimensions in mm
 % height 180mm
 % diam 140
 %diam elec 9mm
-tank_radius = 90
-tank_height = 140
-elecs_per_plane= 16
-no_of_planes = 5
-first_plane_starts = 30
-height_between_centres = 30
-electrode_radius= 9
+tank_radius = 20
+tank_height = 35
+elec_per_plane_index=3
+elecs_per_plane= 2^elec_per_plane_index
+no_of_planes = 2
+first_plane_starts = 10
+height_between_centres = 15
+electrode_radius= 2
 %tank_radius = 20
 %tank_height = 35
 %no_of_planes = 2
@@ -17,38 +19,33 @@ electrode_radius= 9
 %first_plane_starts = 10
 %height_between_centres = 15
 %electrode_radius= 2
-eps = 1e-8;
+eps = tank_radius*0.1;
 geofn='tank.geo';
 meshfn='tank.msh';
 [fid,mess]=fopen(geofn,'w');
 fprintf(fid,'#Automatically generated cylinder surface mesh with round electrodes\n');
 fprintf(fid,'algebraic3d\n');
-fprintf(fid,'solid cy=cylinder(0,0,0;0,0,%6.2f;%6.2f) \n',tank_height, ...
+
+fprintf(fid,'solid cyl=cylinder (0,0,0;0,0,%6.2f;%6.2f); \n',tank_height, ...
 	tank_radius);
-fprintf(fid,' and plane(0,0,0;0,0,-1)\n  and  plane(0,0,%6.2f;0,0,1);\n',tank_height);  
-kelec = 0;
+fprintf(fid, 'solid bigcyl= plane(0,0,0;0,0,-1)\n  and  plane(0,0,%6.2f;0,0,1)\n   and  cyl;\n',tank_height);  
+kcyl = 0;
 for l=1:no_of_planes
  z = first_plane_starts + (l-1)*height_between_centres;
- for th =0:2*pi/elecs_per_plane:2*pi*(elecs_per_plane-1)/ elecs_per_plane
-    kelec=kelec+1;
-    [x,y]=pol2cart(th,tank_radius);
+ for th =0:2*pi/elecs_per_plane:pi*(elecs_per_plane-1)/elecs_per_plane
+    kcyl=kcyl+1;
+    [x,y]=pol2cart(th,tank_radius+eps);
 	 	 
-    fprintf(fid,'solid el%d = sphere(%6.2f,%6.2f,%6.2f;%6.2f);\n',kelec,x+eps,y+eps,z+eps,electrode_radius );
+    fprintf(fid,'solid cyl%d = bigcyl and  cylinder (%6.3f,%6.3f,%6.3f;%6.3f,%6.3f,%6.3f;%6.3f);\n',kcyl,-x,-y,z,x,y,z,electrode_radius );
  end;
 end;
+
 nelec = no_of_planes*elecs_per_plane;
-fprintf(fid,'solid el = ');
-for k=1:(nelec-1)
-  fprintf(fid,'el%d or ',k);
+fprintf(fid,'tlo bigcyl;\n');
+for k=1:kcyl
+%  fprintf(fid,'tlo cyl%d;\n ',k);
+  fprintf(fid,'tlo cyl%d cyl -col=[1,0,0];\n ',k);
 end
-fprintf(fid,'el%d;\n',nelec);
-fprintf(fid,'#cylinder with spheres scooped out\n');
-fprintf(fid,'solid body = cy and not el;\n');
-fprintf(fid,'#the bits which were scooped out\n');
-fprintf(fid,'solid e1 = cy and el;\n');
-fprintf(fid,'#Making two top level objects means they both get meshed.\n');
-fprintf(fid,'tlo body -transparent;\n');
-fprintf(fid,'tlo e1;\n');     
 fclose(fid);
 % Now call Netgen in batchmode to mesh this CSG file
 disp('Calling Netgen. Please wait.....');
