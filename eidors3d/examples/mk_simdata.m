845cce07b540a070d5e86fad0b5b700aae0ce1c2
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2005-06-03 14:34:33 +0000

    3d models
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@103 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors3d/examples/mk_simdata.m
+++ eidors3d/examples/mk_simdata.m
@@ -1,11 +1,23 @@
 % How to make simulation data using EIDORS3D
-% $Id: mk_simdata.m,v 1.2 2005-06-02 17:13:49 aadler Exp $
+% $Id: mk_simdata.m,v 1.3 2005-06-03 14:34:31 aadler Exp $
 
 % 
 % Example 1: Create simple 16 electrode 2D model
 % 
 % get parameters for model from mk_circ_tank
 % param= mk_circ_tank(rings, levels, n_elec, n_planes )
-params= mk_circ_tank(8, [], 16, 1); 
-sim_mdl = eidors_obj('fwd_model', params);
-show_fem( sim_mdl );
+n_elec= 16;
+n_rings= 1;
+params= mk_circ_tank(8, [], n_elec, n_rings); 
+mdl_2d = eidors_obj('fwd_model', params);
+mdl_2d.stimulation= mk_stim_patterns(n_elec, n_rings, '{ad}','{ad}');
+% show_fem( sim_mdl );
+
+
+% 
+% Example 1: Create simple 16 electrode 3D model
+% 
+% get parameters for model from mk_circ_tank
+% param= mk_circ_tank(rings, levels, n_elec, n_planes )
+params= mk_circ_tank(8, [-.5,0,.5], n_elec, n_rings); 
+mdl_3d = eidors_obj('fwd_model', params);
