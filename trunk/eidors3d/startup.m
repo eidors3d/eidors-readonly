bac60e967fed6542608fb9c21c2af04cb8c6b5a6
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2005-06-04 18:59:44 +0000

    add algorithms/aa_1996

--- trunk/eidors3d/startup.m
+++ trunk/eidors3d/startup.m
@@ -1,11 +1,12 @@
 % Script to start EIDORS3D
 % Set path and variables correctly
-% $Id: startup.m,v 1.6 2004-07-22 03:32:36 aadler Exp $
+% $Id: startup.m,v 1.7 2005-06-04 18:59:44 aadler Exp $
 
 HOMEDIR=pwd;
 
 addpath( HOMEDIR );
 addpath([HOMEDIR, '/algorithms/np_2003']);
+addpath([HOMEDIR, '/algorithms/aa_1996']);
 addpath([HOMEDIR, '/models/aa_1996']);
 addpath([HOMEDIR, '/sample_data']);
 addpath([HOMEDIR, '/examples']);
