eea85811f6d4a5f735d38e4d9f4efa5c235c0cb7
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2005-06-04 17:12:39 +0000

    fwd_model works

--- trunk/eidors3d/algorithms/aa_1996/aa_calc_system_mat.m
+++ trunk/eidors3d/algorithms/aa_1996/aa_calc_system_mat.m
@@ -5,9 +5,9 @@ function s_mat= aa_calc_system_mat( fwd_model, img)
 % img       = image background for system matrix calc
 % s_mat.SS  = Unconnected system Matrix
 % s_mat.CC  = Connectivity Matrix
-% $Id: aa_calc_system_mat.m,v 1.1 2005-06-04 16:39:02 aadler Exp $
+% $Id: aa_calc_system_mat.m,v 1.2 2005-06-04 17:12:39 aadler Exp $
 
-p= np_fwd_parameters( fwd_model );
+p= aa_fwd_parameters( fwd_model );
 
 if 0; % old code - less efficient
     SS= spalloc( d*e , d*e, d*d*e);
@@ -18,19 +18,21 @@ if 0; % old code - less efficient
     end %for j=1:ELEMs 
 end
 
-de= p.d*p.e;
-SSiidx= floor([0:de-1]'/p.d)*p.d*ones(1,p.d) + ones(de,1)*(1:p.d) ;
-SSjidx= [1:de]'*ones(1,p.d);
-SSdata= zeros(de,p.d);
-dfact= (p.d-1)*(p.d-2); % Valid for d<=3
-for j=1:p.e
-  a=  inv([ ones(p.d,1), p.NODE( :, p.ELEM(:,j) )' ]);
-  idx= p.d*(j-1)+1:p.d*j;
-  SSdata(idx,1:p.d)= 2*a(2:p.d,:)'*a(2:p.d,:)/dfact/abs(det(a));
+d= p.n_dims+1;
+e= p.n_elem;
+n= p.n_node;
+SSiidx= floor([0:d*e-1]'/d)*d*ones(1,d) + ones(d*e,1)*(1:d) ;
+SSjidx= [1:d*e]'*ones(1,d);
+SSdata= zeros(d*e,d);
+dfact= (d-1)*(d-2); % Valid for d<=3
+for j=1:e
+  a=  inv([ ones(d,1), p.NODE( :, p.ELEM(:,j) )' ]);
+  idx= d*(j-1)+1 : d*j;
+  SSdata(idx,1:d)= 2*a(2:d,:)'*a(2:d,:)/dfact/abs(det(a));
 end %for j=1:ELEMs 
 SS= sparse(SSiidx,SSjidx,SSdata);
 
-CC= sparse((1:d*e),ELEM(:),ones(d*e,1), d*e, n);
+CC= sparse((1:d*e),p.ELEM(:),ones(d*e,1), d*e, n);
 
 s_mat.SS = SS;
 s_mat.CC = CC;
--- trunk/eidors3d/algorithms/aa_1996/aa_fwd_parameters.m
+++ trunk/eidors3d/algorithms/aa_1996/aa_fwd_parameters.m
@@ -6,11 +6,12 @@ function param = aa_fwd_parameters( fwd_model )
 %   param.n_elec   => number of electrodes
 %   param.n_node   => number of nodes (vertices)
 %   param.n_stim   => number of current stimulation patterns
+%   param.n_dims   => dimentions (2= 2D, 3=3D)
 %   param.n_meas   => number of measurements (total)
 %   param.NODE     => vertex matrix
 %   param.ELEM     => connection matrix
 %   param.QQ       => Current into each NODE
-% $Id: aa_fwd_parameters.m,v 1.2 2005-06-04 16:39:02 aadler Exp $
+% $Id: aa_fwd_parameters.m,v 1.3 2005-06-04 17:12:39 aadler Exp $
 
 param = eidors_obj('get-cache', fwd_model, 'aa_1996_fwd_param');
 
@@ -30,31 +31,32 @@ function pp= calc_param( fwd_model );
 pp.NODE= fwd_model.nodes';
 pp.ELEM= fwd_model.elems';
 
+n= size(pp.NODE,2);        %NODEs
 d= size(pp.ELEM,1);        %dimentions+1
 e= size(pp.ELEM,2);        %ELEMents
 p = length(fwd_model.stimulation );
 
 
 pp.QQ= zeros(e,p);
-for i=1:n_stim
+for i=1:p
     stim= fwd_model.stimulation(i);
     % for each electrode in this stimulation pattern
-    elecs= find( stim(i).stim_pattern );
+    elecs= find( stim.stim_pattern );
     for elec= elecs(:)'
     %   fwd_model.electrode(i).z_contact - unused here
         elec_nodes = fwd_model.electrode(elec).nodes;
         l_elec_nodes = length(elec_nodes);
-        curr= stim(i).stim_pattern( elec );
+        curr= stim.stim_pattern( elec );
 
         pp.QQ( elec_nodes, i) = curr / l_elec_nodes;
     end
 end
-        -----------------------
 
 
 % pack into a parameter return list
-param.n_elem   = e;
-param.n_elec   = length( fwd_model.electrode );
-param.n_node   = n;
-param.n_stim   = p;
+pp.n_elem   = e;
+pp.n_elec   = length( fwd_model.electrode );
+pp.n_node   = n;
+pp.n_stim   = p;
+pp.n_dims   = d-1;
 %param.n_meas   = 
