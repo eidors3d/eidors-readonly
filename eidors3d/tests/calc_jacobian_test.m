95f5f90163b15021275baf2a16058ea6687dfa4d
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2005-06-06 22:37:22 +0000

    Calculate Jacobian Test
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@123 82006732-fb7e-4a22-9f28-86a212cce563

---
+++ eidors3d/tests/calc_jacobian_test.m
@@ -0,0 +1,6 @@
+function ok= calc_jacobian_test
+% Verify Jacobian Calculation by small derivative from forward problem
+% $Id: calc_jacobian_test.m,v 1.1 2005-06-06 22:37:22 aadler Exp $
+
+isOctave= exist('OCTAVE_VERSION');
+
