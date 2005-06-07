b09aa2e3502f41c9c692d9820ba14cd49b59882a
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2005-06-07 03:18:08 +0000

    inverse solution
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@137 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors3d/algorithms/aa_1996/aa_inv_solve.m
+++ eidors3d/algorithms/aa_1996/aa_inv_solve.m
@@ -6,15 +6,21 @@ function img= aa_inv_solve( inv_model, data1, data2)
 % data1      => differential data at earlier time
 % data2      => differential data at later time
 
-% $Id: aa_inv_solve.m,v 1.1 2005-06-07 01:26:59 aadler Exp $
+% $Id: aa_inv_solve.m,v 1.2 2005-06-07 03:18:08 aadler Exp $
 
 fwd_model= inv_model.fwd_model;
 pp= aa_fwd_parameters( fwd_model );
 
 % The one_step reconstruction matrix is cached
+one_step_inv = eidors_obj('get-cache', inv_model, 'one_step_inv');
 if ~isempty(one_step_inv)
     eidors_msg('aa_inv_solve: using cached value', 2);
 else
+    % calc jacobian with homogeneous background
+    homg_img= eidors_obj('image', 'homog image', ...
+                         'elem_data', ones( pp.n_elem ,1), ...
+                         'fwd_model', fwd_model );
+
     J = calc_jacobian( fwd_model, homg_img);
 
     R = calc_image_prior( inv_model );
@@ -22,9 +28,8 @@ else
 
     one_step_inv= (J'*J +  hp*R)\J';
 
-    one_step_inv = eidors_obj('get-cache', inv_model, 'one_step_inv');
     eidors_obj('set-cache', inv_model, 'one_step_inv', one_step_inv);
-    eidors_msg('np_inv_solve: setting cached value', 2);
+    eidors_msg('aa_inv_solve: setting cached value', 2);
 end
 
 if pp.normalize
@@ -33,7 +38,7 @@ else
    dva= data1.meas - data2.meas;
 end
 
-sol = one_step_inv * dva;
+sol = one_step_inv * dva(:);
 
 % create a data structure to return
 img.name= 'solved by aa_inv_solve';
