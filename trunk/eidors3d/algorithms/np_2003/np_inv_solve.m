553e01660ee7203ea695883c8c43d504c8c565d4
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2005-06-07 01:21:54 +0000

    re-insert one_step_inverse caching

--- trunk/eidors3d/algorithms/np_2003/np_inv_solve.m
+++ trunk/eidors3d/algorithms/np_2003/np_inv_solve.m
@@ -6,22 +6,33 @@ function img= np_inv_solve( inv_model, data1, data2)
 % data1      => differential data at earlier time
 % data2      => differential data at later time
 
-% $Id: np_inv_solve.m,v 1.11 2005-06-07 01:14:21 aadler Exp $
+% $Id: np_inv_solve.m,v 1.12 2005-06-07 01:21:54 aadler Exp $
 
-p= np_fwd_parameters( fwd_model );
+% The one_step reconstruction matrix is cached
+if ~isempty(one_step_inv)
+    eidors_msg('np_inv_solve: using cached value', 2);
+else
+    p= np_fwd_parameters( fwd_model );
 
-% calc jacobian with homogeneous background
-homg_img= eidors_obj('image', 'homog image from np_inv_solve', ...
-                     'elem_data', ones( p.n_elem ,1), ...
-                     'fwd_model', fwd_model );
+    % calc jacobian with homogeneous background
+    homg_img= eidors_obj('image', 'homog image from np_inv_solve', ...
+                         'elem_data', ones( p.n_elem ,1), ...
+                         'fwd_model', fwd_model );
 
-J = calc_jacobian( fwd_model, homg_img);
+    J = calc_jacobian( fwd_model, homg_img);
 
-Reg = calc_image_prior( inv_model );
-tfac= calc_hyperparameter( inv_model );
+    Reg = calc_image_prior( inv_model );
+    tfac= calc_hyperparameter( inv_model );
 
-% Calculating a linear inverse solution
-one_step_inv= (J'*J +  tfac*Reg'*Reg)\J';
+    % Calculating a linear inverse solution
+    one_step_inv= (J'*J +  tfac*Reg'*Reg)\J';
+    fwd_model= inv_model.fwd_model;
+    one_step_inv = eidors_obj('get-cache', inv_model, 'np_2003_one_step_inv');
+
+    eidors_obj('set-cache', inv_model, 'np_2003_one_step_inv', one_step_inv);
+    eidors_msg('np_inv_solve: setting cached value', 2);
+end
+
 
 dva= data1.meas - data2.meas;
 sol = one_step_inv * dva;
