9cc86e197cbb8ebcec61de130b21a5a62dfe7c7c
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2005-06-07 00:31:45 +0000

    normalized Jacobian calculation
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@126 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors3d/tests/calc_jacobian_test.m
+++ eidors3d/tests/calc_jacobian_test.m
@@ -1,7 +1,8 @@
 function ok= calc_jacobian_test
 % Verify Jacobian Calculation by small derivative from forward problem
-% $Id: calc_jacobian_test.m,v 1.2 2005-06-07 00:16:23 aadler Exp $
+% $Id: calc_jacobian_test.m,v 1.3 2005-06-07 00:31:45 aadler Exp $
 
+ok= 1;
 
 % Create simple 16 electrode 2D model
 % 
@@ -15,6 +16,7 @@ params.stimulation= mk_stim_patterns(n_elec, n_rings, '{ad}','{ad}', ...
 params.solve=      'aa_fwd_solve';
 params.system_mat= 'aa_calc_system_mat';
 params.jacobian=   'aa_calc_jacobian';
+params.normalize_measurements = 0;
 mdl_2d = eidors_obj('fwd_model', params);
 
 % create homogeneous image + simulate data
@@ -46,6 +48,36 @@ tol= 1e-4*std(J(:));
 if sumdiff/length(testvec) > tol
    error('Jacobian calculation error');
    ok=0;
-else
-   ok=1;
+end
+
+
+% create normalized model
+params.normalize_measurements = 1;
+mdl_2d_norm = eidors_obj('fwd_model', params);
+% create homogeneous image with normalize_measurements
+mat= ones( size(mdl_2d.elems,1) ,1);
+homg_img= eidors_obj('image', 'homg image', ...
+                     'elem_data', mat, 'fwd_model', mdl_2d_norm );
+
+J= calc_jacobian( homg_img );
+sumdiff= 0;
+delta = 2e-5;
+testvec= [5,20,40,130];
+for testelem = testvec
+    mat= ones( size(mdl_2d.elems,1) ,1);
+    mat(testelem)= 1+delta;
+    inh_img= eidors_obj('image', 'inh', ...
+                         'elem_data', mat, 'fwd_model', mdl_2d );
+    inh_data=fwd_solve( inh_img);
+
+    simJ= 1/delta* (1 - inh_data.meas ./ homg_data.meas)';
+    
+%   plot([J(:,testelem) simJ]);
+    sumdiff = sumdiff + std( J(:,testelem) - simJ );
+end
+
+tol= 1e-4*std(J(:));
+if sumdiff/length(testvec) > tol
+   error('normalize Jacobian calculation error');
+   ok=0;
 end
