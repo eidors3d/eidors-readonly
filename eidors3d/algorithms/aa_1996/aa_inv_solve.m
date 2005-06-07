727811f29536aa0b7f6433cf6e5319f62d492839
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2005-06-07 01:26:59 +0000

    inverse solver: allows normalized and difference measurements
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@132 82006732-fb7e-4a22-9f28-86a212cce563

---
+++ eidors3d/algorithms/aa_1996/aa_inv_solve.m
@@ -0,0 +1,42 @@
+function img= aa_inv_solve( inv_model, data1, data2)
+% NP_INV_SOLVE inverse solver for Nick Polydorides EIDORS3D code
+% img= aa_inv_solve( inv_model, data1, data2)
+% img        => output image
+% inv_model  => inverse model struct
+% data1      => differential data at earlier time
+% data2      => differential data at later time
+
+% $Id: aa_inv_solve.m,v 1.1 2005-06-07 01:26:59 aadler Exp $
+
+fwd_model= inv_model.fwd_model;
+pp= aa_fwd_parameters( fwd_model );
+
+% The one_step reconstruction matrix is cached
+if ~isempty(one_step_inv)
+    eidors_msg('aa_inv_solve: using cached value', 2);
+else
+    J = calc_jacobian( fwd_model, homg_img);
+
+    R = calc_image_prior( inv_model );
+    hp= calc_hyperparameter( inv_model );
+
+    one_step_inv= (J'*J +  hp*R)\J';
+
+    one_step_inv = eidors_obj('get-cache', inv_model, 'one_step_inv');
+    eidors_obj('set-cache', inv_model, 'one_step_inv', one_step_inv);
+    eidors_msg('np_inv_solve: setting cached value', 2);
+end
+
+if pp.normalize
+   dva= 1 - data2.meas ./ data1.meas;
+else   
+   dva= data1.meas - data2.meas;
+end
+
+sol = one_step_inv * dva;
+
+% create a data structure to return
+img.name= 'solved by aa_inv_solve';
+img.elem_data = sol;
+img.inv_model= inv_model;
+img.fwd_model= fwd_model;
