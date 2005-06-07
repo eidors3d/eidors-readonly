fa1894f6b04c427073f0ab9df496032059de5bf6
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2005-06-07 00:16:23 +0000

    jacobian tester

--- trunk/eidors3d/tests/calc_jacobian_test.m
+++ trunk/eidors3d/tests/calc_jacobian_test.m
@@ -1,6 +1,51 @@
 function ok= calc_jacobian_test
 % Verify Jacobian Calculation by small derivative from forward problem
-% $Id: calc_jacobian_test.m,v 1.1 2005-06-06 22:37:22 aadler Exp $
+% $Id: calc_jacobian_test.m,v 1.2 2005-06-07 00:16:23 aadler Exp $
 
-isOctave= exist('OCTAVE_VERSION');
 
+% Create simple 16 electrode 2D model
+% 
+n_elec= 16;
+n_rings= 1;
+ options = {'no_meas_current','no_rotate_meas'};
+params= mk_circ_tank(8, [], n_elec, n_rings); 
+
+params.stimulation= mk_stim_patterns(n_elec, n_rings, '{ad}','{ad}', ...
+                            options, 10);
+params.solve=      'aa_fwd_solve';
+params.system_mat= 'aa_calc_system_mat';
+params.jacobian=   'aa_calc_jacobian';
+mdl_2d = eidors_obj('fwd_model', params);
+
+% create homogeneous image + simulate data
+mat= ones( size(mdl_2d.elems,1) ,1);
+
+homg_img= eidors_obj('image', 'homg image', ...
+                     'elem_data', mat, 'fwd_model', mdl_2d );
+
+homg_data=fwd_solve( homg_img);
+J= calc_jacobian( homg_img );
+
+sumdiff= 0;
+delta = 2e-5;
+testvec= [5,20,40,130];
+for testelem = testvec
+    mat= ones( size(mdl_2d.elems,1) ,1);
+    mat(testelem)= 1+delta;
+    inh_img= eidors_obj('image', 'inh', ...
+                         'elem_data', mat, 'fwd_model', mdl_2d );
+    inh_data=fwd_solve( inh_img);
+
+    simJ= 1/delta* (homg_data.meas-inh_data.meas)';
+    
+%   plot([J(:,testelem) simJ]);
+    sumdiff = sumdiff + std( J(:,testelem) - simJ );
+end
+
+tol= 1e-4*std(J(:));
+if sumdiff/length(testvec) > tol
+   error('Jacobian calculation error');
+   ok=0;
+else
+   ok=1;
+end
