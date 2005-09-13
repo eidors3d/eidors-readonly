648c5399456135eb7638be65049f1568ec79941e
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2005-09-13 20:32:35 +0000

    3D meshing with 2D reconstructor

---
+++ trunk/eidors3d/examples/demo_3d_simdata.m
@@ -0,0 +1,68 @@
+% How to make simulation data using EIDORS3D
+% $Id: demo_3d_simdata.m,v 1.1 2005-09-13 20:32:35 aadler Exp $
+
+eidors_msg('log_level',1); % 2 for most messages
+
+% 
+% Example 1: Create simple 16 electrode 2D model
+% 
+% get parameters for model from mk_circ_tank
+% param= mk_circ_tank(rings, levels, n_elec, n_planes )
+n_elec= 16;
+n_rings= 1;
+levels= [-.5:.1:.5];
+e_planes = ceil( length(levels)/2 );
+params= mk_circ_tank(8, levels, n_elec, e_planes); 
+
+%options = {'no_meas_current','rotate_meas'};
+ options = {'no_meas_current','no_rotate_meas'};
+params.stimulation= mk_stim_patterns(n_elec, n_rings, '{ad}','{ad}', ...
+                            options, 10);
+params.solve=      'aa_fwd_solve';
+params.system_mat= 'aa_calc_system_mat';
+params.jacobian=   'aa_calc_jacobian';
+mdl_3d = eidors_obj('fwd_model', params);
+show_fem( mdl_3d );
+
+
+% create homogeneous image + simulate data
+mat= ones( size(mdl_3d.elems,1) ,1);
+homg_img= eidors_obj('image', 'homogeneous image', ...
+                     'elem_data', mat, ...
+                     'fwd_model', mdl_3d );
+homg_data=fwd_solve( homg_img);
+
+% create inhomogeneous image + simulate data
+mat(5)= 2;
+inh_img= eidors_obj('image', 'homogeneous image', ...
+                     'elem_data', mat, ...
+                     'fwd_model', mdl_3d );
+inh_data=fwd_solve( inh_img);
+
+% 
+% Step 2: Create different model for reconstruction
+% 
+params= mk_circ_tank(8, [], n_elec, n_rings); 
+
+params.stimulation= mk_stim_patterns(n_elec, n_rings, '{ad}','{ad}', ...
+                            options, 10);
+params.solve=      'aa_fwd_solve';
+params.system_mat= 'aa_calc_system_mat';
+params.jacobian=   'aa_calc_jacobian';
+mdl_2d_2 = eidors_obj('fwd_model', params);
+
+inv2d.name= 'EIT inverse';
+inv2d.solve=       'aa_inv_solve';
+%inv2d.hyperparameter.func = 'aa_calc_noise_figure';
+%inv2d.hyperparameter.noise_figure= 2;
+%inv2d.hyperparameter.tgt_elems= 1:4;
+ inv2d.hyperparameter.value = 1e-8;
+ inv2d.image_prior.func= 'tikhonov_image_prior';
+%inv2d.image_prior.func= 'aa_calc_image_prior';
+inv2d.reconst_type= 'difference';
+inv2d.fwd_model= mdl_2d_2;
+inv2d= eidors_obj('inv_model', inv2d);
+
+img= inv_solve( inv2d, inh_data, homg_data);
+show_fem(img);
+
