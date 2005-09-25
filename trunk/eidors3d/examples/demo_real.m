c13154ce704cedbe4656f1651fc4c59f233bfe40
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2005-09-25 23:04:36 +0000

    move to subfunctions

--- trunk/eidors3d/examples/demo_real.m
+++ trunk/eidors3d/examples/demo_real.m
@@ -1,104 +1,39 @@
+function demo_real;
 % DEMO to show usage of EIDORS3D
-% $Id: demo_real.m,v 1.27 2005-07-01 10:17:02 aadler Exp $
-
-clear; 
+% $Id: demo_real.m,v 1.28 2005-09-25 23:04:36 aadler Exp $
 clc;
-warning('off');
 
 isOctave= exist('OCTAVE_VERSION');
-
-datareal= 'datareal.mat';
-datacom=  'datacom.mat';
-if isOctave
-    datareal= file_in_loadpath(datareal);
-    datacom=  file_in_loadpath(datacom);
-    page_screen_output= 0;
-end
-
 eidors_msg('log_level',2); % most messages
 
 disp('step 1: create FEM model structure');
-
-load(datareal,'vtx','simp');
-%srf : the boundary surfaces (triangles)
-%vtx : the vertices of the model (coordinates of the nodes)
-%simp: the simplices of the model (connectivity in tetrahedral)
-
 %
 % create a 'fwd_model' object with name demo_mdl
 %
 
+[vtx,simp, bdy] = get_model_elems;
+
 demo_mdl.name = 'demo real model';
 demo_mdl.nodes= vtx;
 demo_mdl.elems= simp;
-demo_mdl.boundary= dubs3( simp );
+demo_mdl.boundary= bdy;
 demo_mdl.solve=      'np_fwd_solve';
 demo_mdl.jacobian=   'np_calc_jacobian';
 demo_mdl.system_mat= 'np_calc_system_mat';
 
-% The model could also be created this way
-%demo_mdl= eidors_obj('model', 'Demo real model', ...
-%                     'nodes', vtx, ...
-%                     'elems', simp, ...
-%                     'boundary', dubs3( simp ), ...
-%                     'solve',      'np_fwd_solve', ...
-%                     'jacobian',   'np_calc_jacobian', ...
-%                     'system_mat', 'np_calc_system_mat' );
-                     
-clear vtx simp
-
 disp('step 2: create FEM model electrodes definitions');
 
-load(datareal,'gnd_ind','elec','zc','protocol','no_pl','sym');
-%elec : The electrodes matrix. 
-%np_pl : Number of electrode planes (in planar arrangements)
-%protocol : Adjacent or Opposite or Customized.
-%zc : Contact impedances of the electrodes
-%sym : Boolean entry for efficient forward computations 
-%sym='{n}';
-
-demo_mdl.gnd_node= gnd_ind;
-for i=1:length(zc)
-    demo_mdl.electrode(i).z_contact= zc(i);
-    demo_mdl.electrode(i).nodes=     unique( elec(i,:) );
-end
-
-demo_mdl.misc.sym     = sym;
-
-demo_mdl= eidors_obj('fwd_model', demo_mdl); % create object
+[gnd_ind, electrodes, sym, elec, protocol, no_pl] = get_model_elecs;
+demo_mdl.gnd_node=           gnd_ind;
+demo_mdl.electrode =         electrodes;
+demo_mdl.misc.sym =          sym;
 
 disp('step 3: create FEM model stimulation and measurement patterns');
 
-% get the current stimulation patterns
-[I,Ib] = set_3d_currents(protocol, ...
-                         elec, ...
-                         demo_mdl.nodes, ...
-                         demo_mdl.gnd_node, ...
-                         no_pl);
-% get the measurement patterns, only indH is used in this model
-%   here we only want to get the meas pattern from 'get_3d_meas',
-%   not the voltages, so we enter zeros
-[jnk,jnk,indH,indV,jnk] = get_3d_meas( ...
-                  elec, demo_mdl.nodes, ...
-                  zeros(size(I)), ... % Vfwd
-                  Ib, no_pl );
-n_elec= size(elec,1);
-n_meas= size(indH,1) / size(Ib,2);
-for i=1:size(Ib,2)
-    demo_mdl.stimulation(i).stimulation= 'mA';
-    demo_mdl.stimulation(i).stim_pattern= Ib(:,i);
-    idx= ( 1+ (i-1)*n_meas ):( i*n_meas );
-    meas_pat = sparse( (1:n_meas)'*[1,1], ...
-                       indH( idx, : ), ...
-                       ones(n_meas,2)*[1,0;0,-1], ...
-                       n_meas, n_elec );
-    demo_mdl.stimulation(i).meas_pattern= meas_pat;
-end
+[stimulations ] = get_model_stim( demo_mdl );
+demo_mdl.stimulation= stimulations;
 
-clear gnd_ind elec zc sym protocol no_pl I Ib
-clear indH indV indH_sz meas_pat idx jnk
-
-demo_mdl= eidors_obj('set', demo_mdl); %add new properties
+demo_mdl= eidors_obj('fwd_model', demo_mdl); %create object
 
 disp('step 4: simulate data for homogeneous medium');
 
@@ -117,15 +52,15 @@ homg_data=fwd_solve( demo_mdl, homg_img);
 disp('step 5: simulate data for inhomogeneous medium');
 %
 % create an inhomogeneous image
+% A,B are Indices of the elements to represent the inhomogeneity
 %
-load( datacom ,'A','B') %Indices of the elements to represent the inhomogeneity
+load( datacom_file ,'A','B')
 mat(A)= mat(A)+0.15;
 mat(B)= mat(B)-0.20;
 
 inhomg_img= eidors_obj('image', 'inhomogeneous image', ...
                        'elem_data', mat, ...
                        'fwd_model', demo_mdl );
-clear A B mat
 
 if ~isOctave
 %   show_fem( demo_mdl, 1);
@@ -170,6 +105,76 @@ if ~isOctave
     figure; image_levels( demo_img, levels );
 
 end
+
+function [vtx,simp,bdy] = get_model_elems;
+%bdy : the boundary surfaces (triangles)
+%vtx : the vertices of the model (coordinates of the nodes)
+%simp: the simplices of the model (connectivity in tetrahedral)
+load(datareal_file,'vtx','simp');
+bdy= dubs3( simp );
+
+
+
+function [gnd_ind, electrodes, sym, elec, protocol, no_pl] = get_model_elecs;
+%elec : The electrodes matrix. 
+%np_pl : Number of electrode planes (in planar arrangements)
+%protocol : Adjacent or Opposite or Customized.
+%zc : Contact impedances of the electrodes
+%sym : Boolean entry for efficient forward computations 
+%sym='{n}';
+
+load(datareal_file,'gnd_ind','elec','zc','protocol','no_pl','sym');
+
+for i=1:length(zc)
+    electrodes(i).z_contact= zc(i);
+    electrodes(i).nodes=     unique( elec(i,:) );
+end
+
+% get the current stimulation patterns
+function [stimulations] = get_model_stim( mdl );
+
+load(datareal_file,'protocol','no_pl','sym','elec');
+[I,Ib] = set_3d_currents(protocol, ...
+                         elec, ...
+                         mdl.nodes, ...
+                         mdl.gnd_node, ...
+                         no_pl);
+
+% get the measurement patterns, only indH is used in this model
+%   here we only want to get the meas pattern from 'get_3d_meas',
+%   not the voltages, so we enter zeros
+[jnk,jnk,indH,indV,jnk] = get_3d_meas( ...
+                  elec, mdl.nodes, ...
+                  zeros(size(I)), ... % Vfwd
+                  Ib, no_pl );
+n_elec= size(elec,1);
+n_meas= size(indH,1) / size(Ib,2);
+for i=1:size(Ib,2)
+    stimulations(i).stimulation= 'mA';
+    stimulations(i).stim_pattern= Ib(:,i);
+    idx= ( 1+ (i-1)*n_meas ):( i*n_meas );
+    meas_pat = sparse( (1:n_meas)'*[1,1], ...
+                       indH( idx, : ), ...
+                       ones(n_meas,2)*[1,0;0,-1], ...
+                       n_meas, n_elec );
+    stimulations(i).meas_pattern= meas_pat;
+end
+
+% Get filename for datareal
+function fname= datareal_file;
+   fname= 'datareal.mat';
+   if exist('OCTAVE_VERSION');
+       fname= file_in_loadpath(datareal);
+   end
+
+% Get filename for datacom
+function fname= datacom_file;
+   fname=  'datacom.mat';
+   if exist('OCTAVE_VERSION');
+       fname=  file_in_loadpath(datacom);
+   end
+
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % This is part of the EIDORS suite.
