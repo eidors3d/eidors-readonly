fae8d218eafee96b579c43c0e02fe0c6b6756185
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2005-10-10 19:34:23 +0000

    modified to remove explicit cache naming

--- trunk/eidors3d/calc_image_prior.m
+++ trunk/eidors3d/calc_image_prior.m
@@ -4,14 +4,14 @@ function image_prior = calc_image_prior( inv_model, varargin )
 %     crosscorrelation FEM element values
 % 
 % calc_image_prior can be called as
-%    image_prior= calc_image_prior( inv_model )
+%    image_prior= calc_image_prior( inv_model, ... )
 %
 % in each case it will call the inv_model.image_prior.func
 %
 % image_prior   is the calculated image prior
 % inv_model    is an inv_model structure
 %
-% $Id: calc_image_prior.m,v 1.6 2005-10-10 03:32:49 aadler Exp $
+% $Id: calc_image_prior.m,v 1.7 2005-10-10 19:34:23 aadler Exp $
 
 image_prior= eidors_obj('calc-or-cache', inv_model, ...
                  inv_model.image_prior.func, varargin{:} ); 
