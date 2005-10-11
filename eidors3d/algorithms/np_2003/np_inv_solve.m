8f111776ca8fdcfb7acdddbb8259602f42d5a863
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2005-10-11 15:26:08 +0000

    no explicit save to file
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@217 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors3d/algorithms/np_2003/np_inv_solve.m
+++ eidors3d/algorithms/np_2003/np_inv_solve.m
@@ -6,7 +6,7 @@ function img= np_inv_solve( inv_model, data1, data2)
 % data1      => differential data at earlier time
 % data2      => differential data at later time
 
-% $Id: np_inv_solve.m,v 1.16 2005-09-16 03:36:50 aadler Exp $
+% $Id: np_inv_solve.m,v 1.17 2005-10-11 15:26:08 aadler Exp $
 
 % The one_step reconstruction matrix is cached
 one_step_inv = eidors_obj('get-cache', inv_model, 'np_2003_one_step_inv');
@@ -22,7 +22,7 @@ else
                          'elem_data', ones( p.n_elem ,1), ...
                          'fwd_model', fwd_model );
 
-    J = calc_jacobian( fwd_model, homg_img, 'cache:np_calc_jacobian_homgimg');
+    J = calc_jacobian( fwd_model, homg_img );
 
     Reg = calc_image_prior( inv_model );
     tfac= calc_hyperparameter( inv_model );
