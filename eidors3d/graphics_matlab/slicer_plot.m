c48de69fa5192481219a335bfe24b3d71d38532a
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2005-10-12 14:02:39 +0000

    subfunctions of slicer_plot
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@228 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors3d/graphics_matlab/slicer_plot.m
+++ eidors3d/graphics_matlab/slicer_plot.m
@@ -109,7 +109,40 @@ axis([min(vtx(:,1)), max(vtx(:,1)), ...
 caxis([min(BB), max(BB)]);
 
 axis square;
-  
+
+
+function [CC] = solution_ext(BB,vtx,simp);
+%function [CC] = solution_ext(BB,vtx,simp);
+%
+%Auxiliary function that extracts a secondary NODE-wise solution CC 
+%based on the calculated ELEMENT-wise solution BB. This function is 
+%called from slicer_plot function.
+%
+%
+%
+%BB   = The calculated (element-wise) inverse solution 
+%vtx  = The vertices matrix
+%simp = The simplices matrix
+ 
+%Initialise the destination matrix CC
+CC = zeros(size(vtx,1),1);
+
+for uu=1:length(BB)
+   
+   sim_val = BB(uu); % Solution value for simplex number uu
+   
+   % We aim to distribute this solution value to the nodes involved in the current simplex.
+   
+   sim_nodes = simp(uu,:);
+   
+   for oo=1:length(sim_nodes)
+      
+      s_nd = sim_nodes(oo); % Each of the simplex's nodes 
+      
+      CC(s_nd) = CC(s_nd) + sim_val;
+   end
+end
+
   
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % This is part of the EIDORS suite.
