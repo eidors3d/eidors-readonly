bc2ea85ea2eebd88072cab87b83f11b7024cd757
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2005-10-24 15:34:53 +0000

    test for valid mex file

--- trunk/eidors3d/startup.m
+++ trunk/eidors3d/startup.m
@@ -1,18 +1,26 @@
 % Script to start EIDORS3D
 % Set path and variables correctly
-% $Id: startup.m,v 1.7 2005-06-04 18:59:44 aadler Exp $
+% $Id: startup.m,v 1.8 2005-10-24 15:34:53 aadler Exp $
 
 HOMEDIR=pwd;
 
 addpath( HOMEDIR );
 addpath([HOMEDIR, '/algorithms/np_2003']);
 addpath([HOMEDIR, '/algorithms/aa_1996']);
+addpath([HOMEDIR, '/algorithms/aa_2005']);
 addpath([HOMEDIR, '/models/aa_1996']);
 addpath([HOMEDIR, '/sample_data']);
 addpath([HOMEDIR, '/examples']);
 addpath([HOMEDIR, '/graphics_matlab']);
 addpath([HOMEDIR, '/graphics_vtk']);
 addpath([HOMEDIR, '/tests']);
+
+% test if eidors_var_id.cpp is a valid mexfile
+if exist('eidors_var_id')~=3
+  warning(sprintf([ ...
+     'you do not have a compiled mex file eidors_var_id.\n' ...
+     'Please compile it using: mex eidors_var_id.cpp']));
+end
 
 clear HOMEDIR;
 
