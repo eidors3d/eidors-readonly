f47fb225b921a02b553db33ae92e0801df175eb9
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2005-10-25 14:54:17 +0000

    Read EIT data from various mfrs
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@291 82006732-fb7e-4a22-9f28-86a212cce563

---
+++ eidors3d/interface/eidors_readdata.m
@@ -0,0 +1,83 @@
+function [vv,curr,volt]= eit_readdata( fname, format )
+% EIDORS readdata - read data files from various EIT equipment
+%    manufacturers
+%
+% Currently the list of supported file formats is:
+%    1. MCEIT (Goettingen / Viasys) "get" file format 
+%        format = "GET" or "MCEIT"
+%
+% Usage
+%  [vv,curr,volt]= eidors_readdata( fname, format )
+%     vv = measurements 208xmeas
+%     fname = file name
+%
+%  if format is unspecified, we attempt to autodetect
+%
+% $Id: eidors_readdata.m,v 1.1 2005-10-25 14:54:17 aadler Exp $
+
+% TODO:
+%   - output an eidors data object
+%   - test whether file format matches specified stimulation pattern
+
+if nargin < 2
+% unspecified file format, autodetect
+   dotpos = find(fname,'.');
+   if isempty( dotpos ) 
+      error('file format unspecified, can`t autodetect');
+   else
+      dotpos= dotpos(end);
+      format= fname( dotpos+1:end );
+   end
+end
+
+fmt= lower(format);
+if     strcmp(fmt, 'get') | strcmp(fmt, 'mceit')  
+   [vv,curr,volt] = mceit_readdata( fname );
+else
+   error('eidors_readdata: file "%s" format unknown', fmt);
+end
+   
+
+function [vv,curr,volt] = mceit_readdata( fname );
+   elec=16;
+   pos_i= [0,1];
+   ELS= rem(rem(0:elec^2-1,elec) - ...
+        floor((0:elec^2-1)/elec)+elec,elec)';
+   ELS=~any(rem( elec+[-1 0 [-1 0]+pos_i*[-1;1] ] ,elec)' ...
+		     *ones(1,elec^2)==ones(4,1)*ELS')';
+
+   fid= fopen(fname,'rb');
+   d= fread(fid,inf,'float');
+   fclose(fid);
+
+   if rem( length(d), 256) ~=0
+      error('File length strange');
+   end
+
+   dd= reshape( d, 256, length(d)/256);
+
+% measurements rotate with stimulation, we untwist them
+   twist= [               0+(1:13), ...
+                         13+(1:13), ...
+           39-(0:-1:0),  26+(1:12), ...
+           52-(1:-1:0),  39+(1:11), ...
+           65-(2:-1:0),  52+(1:10), ...
+           78-(3:-1:0),  65+(1: 9), ...
+           91-(4:-1:0),  78+(1: 8), ...
+          104-(5:-1:0),  91+(1: 7), ...
+          117-(6:-1:0), 104+(1: 6), ...
+          130-(7:-1:0), 117+(1: 5), ...
+          143-(8:-1:0), 130+(1: 4), ...
+          156-(9:-1:0), 143+(1: 3), ...
+          169-(10:-1:0),156+(1: 2), ...
+          182-(11:-1:0),169+(1: 1), ...
+          195-(12:-1:0), ...
+          208-(12:-1:0) ];
+    vv= zeros(256,size(dd,2));
+    vv(ELS,:)= dd(twist,:);
+   %vv= dd(1:208,:);
+
+   curr=0.00512*dd(209:224,:);  % Amps
+   volt=12*dd(225:240,:); %Vrms
+   %input impedance=voltage./current-440;	Ohm
+
