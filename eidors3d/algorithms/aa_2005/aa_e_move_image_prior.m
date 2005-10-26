76ee08ae9d1794fd837e1882f6bb60876e7cbd44
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2005-10-26 03:37:09 +0000

    return to Laplace -> add movement weighting parameter
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@304 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors3d/algorithms/aa_2005/aa_e_move_image_prior.m
+++ eidors3d/algorithms/aa_2005/aa_e_move_image_prior.m
@@ -3,22 +3,55 @@ function Reg= aa_e_move_image_prior( inv_model );
 % Reg= aa_e_move_image_prior( inv_model )
 % Reg        => output regularization term
 % inv_model  => inverse model struct
+% Parameters:
+%   inv_model.image_prior.parameters(1) -> relative weighting
+%     of movement vs image fraction of hyperparameter
+%     => Default = 100
 %
-% Use a diagonal weighting, just that
-% [Reg](i,i) = [H'*H](i,i) for H= Jacobian
+% For image portion, we use a Laplace prior, as 
+% -1 for each adjacent element, and 3 (in 2D) or 4 (in 3D)
+% for the element itself
+%
+% For the movmenent portion, we define a smoothness
+% constraint, such that Rij = -1 for adjacent electrodes
 
-% $Id: aa_e_move_image_prior.m,v 1.3 2005-10-25 17:53:06 aadler Exp $
+% $Id: aa_e_move_image_prior.m,v 1.4 2005-10-26 03:37:09 aadler Exp $
 
-fwd_model= inv_model.fwd_model;
+% relative strengths of conductivity and movement priors
+if isfield( inv_model.image_prior,'parameters')
+   hp_move= inv_model.image_prior.parameters(1);
+else
+   hp_move= 100;
+end
 
-homg_img= eidors_obj('image', 'homogeneous image', ...
-                     'elem_data', ones( size(fwd_model.elems,1) ,1), ...
-                     'fwd_model', fwd_model );
+pp= aa_fwd_parameters( inv_model.fwd_model );
 
-J= calc_jacobian( fwd_model, homg_img );
+% calc conductivity portion
+RegC = laplace_image_prior( inv_model );
 
-% We want: [Reg](i,i) = [H'*H](i,i) for H= Jacobian
-diag_elems= sum( J.^2 );
-idx= 1:length( diag_elems);
+% calc movement portion
+RegM = movement_image_prior( pp.n_dims, pp.n_elec );
 
-Reg= sparse( idx, idx, diag_elems);
+% zero blocks
+RegCM= sparse( pp.n_elem, pp.n_dims*pp.n_elec );
+
+Reg= [RegC,           RegCM;
+      RegCM', hp_move*RegM ];
+
+% For the movmenent portion, we define a smoothness
+% constraint, such that Rij = -1 for adjacent electrodes
+function RegM = movement_image_prior( dims, elecs );
+
+   % movement constraint in each dimention
+   idx =(0:elecs-1)';
+   im1= rem(idx-1+elecs,elecs);
+   ip1= rem(idx+1,elecs); 
+   mv= sparse([idx,idx,idx]+1,[im1,idx,ip1]+1,ones(elecs,1)*[-1,2.1,-1]);
+
+   RegM= spalloc(dims*elecs,dims*elecs, 3*dims*elecs);
+
+   for i=0:dims-1;
+     m_idx= idx + i*elecs + 1;
+     RegM( m_idx, m_idx ) = mv;
+   end
+
