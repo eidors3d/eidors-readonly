f9d25d97bbc59df4ce64fbcd0b70eb97f8bf2930
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2005-10-27 12:02:00 +0000

    allow printing via colormap also
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@313 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors3d/graphics_matlab/calc_colours.m
+++ eidors3d/graphics_matlab/calc_colours.m
@@ -10,9 +10,19 @@ function colours= calc_colours(img, scale)
 %
 % scale - colour value corresponding to maximum
 %       - default = autoscale
+%
+% Colour maps are controlled by the global variable
+%   eidors_colours. The following settings
+%
+%   eidors_colours.greylev = .2; % Grey level to colour to
+%   eidors_colours.sat_adj = .9; % max G,B when R=1
+%   eidors_colours.backgnd= [.5,.5,.15]; % colour for non image regions
+%   eidors_colours.mapped_colour= 0; % use colormap function
+%         if mapped_colour is non-zero, it indicates the colourmap
+%         size to use
 
 % TODO: create a global eidors_colours object to control behaviour
-% $Id: calc_colours.m,v 1.5 2005-10-27 03:27:37 aadler Exp $  
+% $Id: calc_colours.m,v 1.6 2005-10-27 12:02:00 aadler Exp $  
 
 pp=get_colours;
  
@@ -27,7 +37,7 @@ else
 end
 
 if isempty(elem_data)
-    colours = zeros(1,1,3);
+    colours = 'k'; %black
     return;
 end
 
@@ -41,21 +51,7 @@ if nargin <= 1
 elseif isempty(scale)
    scale =  max(abs(elem_data)) + eps;
 end
-scale_ed = elem_data / scale;
-
-F= 3*pp.sat_adj;
-red= F*abs(scale_ed+1/F) -1;
-red= red.*(red>0).*(red<1) + (red>=1);
-red(backgnd) = pp.backgnd(1);
-
-grn= F*abs(scale_ed    ) -1;
-grn= grn.*(grn>0).*(grn<1) + (grn>=1);
-grn(backgnd) = pp.backgnd(2);
-
-blu= F*abs(scale_ed-1/F) -1;
-blu= blu.*(blu>0).*(blu<1) + (blu>=1);
-blu(backgnd) = pp.backgnd(3);
-
+[red,grn,blu] = blu_red_axis( pp, elem_data / scale, backgnd );
 colours= ones(1, length(elem_data), 3);
 
 glev= abs(pp.greylev);
@@ -65,6 +61,21 @@ if pp.greylev < 0
 end
 
 
+function [red,grn,blu] = blu_red_axis( pp, scale_data, backgnd )
+   F= 3*pp.sat_adj;
+   red= F*abs(scale_data+1/F) -1;
+   red= red.*(red>0).*(red<1) + (red>=1);
+   red(backgnd) = pp.backgnd(1);
+
+   grn= F*abs(scale_data    ) -1;
+   grn= grn.*(grn>0).*(grn<1) + (grn>=1);
+   grn(backgnd) = pp.backgnd(2);
+
+   blu= F*abs(scale_data-1/F) -1;
+   blu= blu.*(blu>0).*(blu<1) + (blu>=1);
+   blu(backgnd) = pp.backgnd(3);
+
+
 function pp=get_colours;
    global eidors_colours;
 
@@ -72,6 +83,7 @@ function pp=get_colours;
       eidors_colours.greylev = .2;
       eidors_colours.sat_adj = .9;
       eidors_colours.backgnd= [.5,.5,.15];
+      eidors_colours.mapped_colour= 0;
    end
 
    pp= eidors_colours;
