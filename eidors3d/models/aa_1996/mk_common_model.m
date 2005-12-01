0c23f3c90e0a8e9f0a87370d92fa835d26640fab
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2005-12-01 09:17:28 +0000

    Thorax shape deformer
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@420 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors3d/models/aa_1996/mk_common_model.m
+++ eidors3d/models/aa_1996/mk_common_model.m
@@ -5,11 +5,7 @@ function inv_mdl= mk_common_model( str, varargin )
 % so that users do not need to re-write common code
 %
 % Usage: 
-%   mk_common_model('a2c',16)   - 2D circ model (64 elems) with 16 elecs
-%   mk_common_model('b2c',16)   - 2D circ model (256 elems)
-%   mk_common_model('c2c',16)   - 2D circ model (576 elems)
-%   mk_common_model('d2c',16)   - 2D circ model (1024 elems)
-%
+% 3D Models:
 %   mk_common_model('b3r1',16)  - circular ring with 16 electrodes
 %   mk_common_model('b3r2',16)  - two circular rings with 16 electrodes
 %
@@ -17,9 +13,16 @@ function inv_mdl= mk_common_model( str, varargin )
 %
 %   mk_common_model('n3r2',16)  - NP's 3D model with 2 ring electrodes
 %   mk_common_model('n3z',16)  - NP's 3D model with zigzag electrodes
+%
+% 2D Models:
+%   mk_common_model('a2c',16)   - 2D circ model (64 elems) with 16 elecs
+%   mk_common_model('b2c',16)   - 2D circ model (256 elems)
+%   mk_common_model('c2c',16)   - 2D circ model (576 elems)
+%   mk_common_model('d2c',16)   - 2D circ model (1024 elems)
+%
 
 % (C) 2005 Andy Adler. Licenced under the GPL Version 2
-% $Id: mk_common_model.m,v 1.11 2005-10-31 03:37:11 aadler Exp $
+% $Id: mk_common_model.m,v 1.12 2005-12-01 09:17:28 aadler Exp $
 
 options = {'no_meas_current','no_rotate_meas'};
 n_elec= 16; % default
@@ -161,3 +164,42 @@ function inv_mdl = mk_n3r2_model( n_elec, options );
    inv_mdl.reconst_type= 'difference';
    inv_mdl.fwd_model= fmdl;
    inv_mdl= eidors_obj('inv_model', inv_mdl);
+
+% Deform the boundary of the cylinder to make it like a torso
+% niv= 1.. 5 => Torso shape from T5 - T12
+function inv_mdl = deform_cylinder( inv_mdl, niv, y_expand, x_expand)
+    niv= reponse(2)-abs('0');
+    x_coord= [ ...
+      0,60,123,173,223,202,144,75,0,-75,-144,-202,-223,-173,-123,-60;
+      0,50,105,138,144,144,109,50,0,-50,-109,-144,-144,-138,-105,-50;
+      0,52, 99,133,148,141,110,61,0,-61,-110,-141,-148,-133,- 99,-52;
+      0,51, 92,129,148,136, 96,47,0,-47,- 96,-136,-148,-129,- 92,-51;
+      0,49, 92,128,148,141,111,64,0,-64,-111,-141,-148,-128,- 92,-49 ];
+    y_coord= [ ...
+      123,116, 91,42,-4,-67,-105,-119,-108,-119,-105,-67,-4,42, 91,116;
+      129,132,112,62, 3,-57,-101,-110,-107,-110,-101,-57, 3,62,112,132;
+      116,112, 92,53, 3,-48,- 88,-106,-105,-106,- 88,-48, 3,53, 92,112;
+      143,130, 99,63,14,-35,- 68,- 82,- 82,- 82,- 68,-35,14,63, 99,130;
+      136,128,103,68,23,-25,- 62,- 78,- 80,- 78,- 62,-25,23,68,103,128 ];
+
+    reidx= [13:16, 1:12];
+    geo= [x_coord(niv,reidx)',  ...
+          y_coord(niv,reidx)'];
+    a_max= size(geo,1);
+    ab_geo=sqrt(sum(([ geo; geo(1,:) ]').^2)');
+    nn= zeros(size(NODE));
+    for i=1:size(NODE,2);
+      angle = rem(a_max*atan2( NODE(2,i), ...
+            NODE(1,i) )/2/pi+a_max,a_max)+1;
+      fac=(  (floor(angle+1.001)- angle)* ...
+              ab_geo(floor(angle+.001)) + ...
+             (angle-floor(angle+.001))* ...
+              ab_geo(floor(angle+1.001))  );
+      nn(:,i)= NODE(:,i)* fac;
+    end  %for i=1:size
+    NODE=nn;
+
+  NODE(1,:)= NODE(1,:)*x_expand;
+  NODE(2,:)= NODE(2,:)*y_expand;
+  NODE(3,:)= NODE(3,:)*z_expand;
+
