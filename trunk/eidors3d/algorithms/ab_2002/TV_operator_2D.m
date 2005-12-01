2663cecb63368e76adb48a1e94c9409c21dd9e40
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2005-12-01 11:45:46 +0000

    Original contribution from Andrea Borsic

---
+++ trunk/eidors3d/algorithms/ab_2002/TV_operator_2D.m
@@ -0,0 +1,63 @@
+function TVop=tv_op(msh);
+% 03/12/00 By Andrea Borsic
+%
+% This function constructs Total Variation operator
+%
+% function TVop=tv_op(msh);
+
+
+num_tri=length(msh.TC);
+num_nod=length(msh.PC);
+
+list=[];
+
+%We build a selection array, to index the T matrix
+
+SEL=[ 1 2 3; 2 3 1];
+
+INC=spalloc(num_nod,num_nod,num_tri*3);
+
+for k=1:num_tri
+   
+   for j=1:3
+      
+      m=msh.TC(SEL(1,j),k);
+      n=msh.TC(SEL(2,j),k);
+      
+      if m<n % ( only the upper triangular )
+         
+         if (INC(m,n)==0) INC(m,n)=k;
+         else
+            len=sqrt((msh.PC(1,m)-msh.PC(1,n))^2+(msh.PC(2,m)-msh.PC(2,n))^2);
+				list(size(list,1)+1,:)=[len,k,INC(m,n)]; % Triangles and length of the shared edge are written into the list
+         end % if then else
+         
+      else
+         
+         if (INC(n,m)==0) INC(n,m)=k;
+         else
+            len=sqrt((msh.PC(1,m)-msh.PC(1,n))^2+(msh.PC(2,m)-msh.PC(2,n))^2);
+				list(size(list,1)+1,:)=[len,k,INC(n,m)]; % Triangles and length of the shared edge are written into the list
+         end % if then else
+         
+         
+      end % if m<n
+      
+   end % for j
+   
+end % for k
+
+TVop=spalloc(num_tri,num_tri,2*length(list));
+
+for i=1:length(list)
+   
+   TVop(i,list(i,2))=list(i,1);
+   
+   TVop(i,list(i,3))=-list(i,1);
+    
+end % for
+
+TVl=list(:,1);
+
+% Bye Bye
+
---
+++ trunk/eidors3d/algorithms/ab_2002/ab_calc_tv_prior.m
@@ -0,0 +1,10 @@
+function Reg= ab_calc_tv_prior( inv_model );
+% AB_CALC_TV_PRIOR calculate Total Variation image prior
+% Reg= ab_calc_tv_prior( inv_model )
+% Reg        => output regularization term
+% inv_model  => inverse model struct
+
+% (C) 2005 Andy Adler. Licenced under the GPL Version 2
+% $Id: ab_calc_tv_prior.m,v 1.1 2005-12-01 11:45:46 aadler Exp $
+
+% Andrea's code requires a msh 
