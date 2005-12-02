08f7581aa8dfd4a92239f203c2f9385ac9269224
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2005-12-02 09:11:21 +0000

    TV implementation with iteration

--- trunk/eidors3d/algorithms/aa_2005/aa_inv_total_var.m
+++ trunk/eidors3d/algorithms/aa_2005/aa_inv_total_var.m
@@ -1,15 +1,31 @@
-function img= aa_inv_conj_grad( inv_model, data1, data2)
-% AA_INV_CONJ_GRAD inverse solver based on the CG
-% inverse [Ref Shewchuck, 1994]
-% img= aa_inv_conj_grad( inv_model, data1, data2)
+function img= aa_inv_total_var( inv_model, data1, data2)
+% AA_INV_TOTAL_VARIANCE inverse solver for difference EIT
+% img= aa_inv_total_var( inv_model, data1, data2)
+%
 % img        => output image
 % inv_model  => inverse model struct
 % data1      => differential data at earlier time
 % data2      => differential data at later time
 %
+% ALGORITHM: for image at iteration k x{k}
+% Iterate:
+%   x{k+1} = (H'*W*H + l^2*R'*WB( x{k} )*R )\H'*W*y
+% For:
+%   WB( x ) = diag( 0.5 ./ sqrt( (l*R*x).^2 + Beta ) )
+% With Beta > 0 to approximate the L1 norm
+%
+% Comment on hyperparameter, we have l^2 in the iteration
+% formula and l in WB, so overall hyperparameter is l. This
+% makes the units work for the L1 norm.
+ 
+% REF: 
+% W. Clem Karl, "Regularization in Image Restoration
+% and Reconstruction," in Handbook of Image and Video
+% Processing, A. Bovic, Ed. San Diego, CA: Academic Press,
+% 2000, ch. 3.6, pp. 141-160.
 
 % (C) 2005 Andy Adler. Licenced under the GPL Version 2
-% $Id: aa_inv_total_var.m,v 1.1 2005-12-01 09:17:05 aadler Exp $
+% $Id: aa_inv_total_var.m,v 1.2 2005-12-02 09:11:21 aadler Exp $
 
 fwd_model= inv_model.fwd_model;
 pp= aa_fwd_parameters( fwd_model );
@@ -38,12 +54,17 @@ else
    dva= data1 - data2;
 end
 
-imax= 100; etol= 1e-3;
+if isfield(inv_model, 'parameters')
+    imax= inv_model.parameters.max_iterations;
+else
+    imax= 3 ;
+end
+etol= 1e-3;
 n_img= size(dva,2);
+
 sol = zeros( size(J,2), n_img );
 for i=1:n_img
-    % FIXME - ignore W for now
-   sol(:,i) = tv_inv( J, hp*R, dva(:,i), imax, etol );
+   sol(:,i) = tv_inv( J, W, hp*R, dva(:,i), imax, etol );
 end
 
 % create a data structure to return
@@ -52,5 +73,11 @@ img.elem_data = sol;
 img.inv_model= inv_model;
 img.fwd_model= fwd_model;
 
-function x= tv_inv( J, R, dva, imax, etol )
-   x= (J'*J + R)\J'*dva;
+function x= tv_inv( H, W, R, y, imax, etol )
+   n= size(R,1);
+   x= zeros(n,1);
+   Beta = 1e-8;
+   for iter= 1:imax
+      WB = spdiags( 0.5 ./ sqrt( (R*x).^2 + Beta ), 0, n, n );
+      x = (H'*W*H + R'*WB*R )\H'*W*y;
+   end
