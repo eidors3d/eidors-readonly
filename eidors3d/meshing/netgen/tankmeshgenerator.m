57c85be3616ee826e4a78cad60a3f34cce1cdcdd
billlion <billlion@82006732-fb7e-4a22-9f28-86a212cce563>
2005-12-05 16:36:07 +0000

    Exports new v3 style variable to file
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@449 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors3d/meshing/netgen/tankmeshgenerator.m
+++ eidors3d/meshing/netgen/tankmeshgenerator.m
@@ -1,8 +1,9 @@
 % Script to generate tank model interactively
 % Bill Lionheart 23/01/2005 (somewhere over Siberia)
 % Part of EIDORS 3D
+% Revised for new version 3.0 structure BL 05/12/2005
 display('EIDORS 3D tank generation script');
-display('You have to answe lots of tedious questions');
+display('You have to answer lots of tedious questions');
 display('Someone please make a GUI!');
 
 
@@ -20,9 +21,9 @@ no_of_planes = input('Number of planes? ');
 nelec = no_of_planes*elecs_per_plane;
 display(sprintf('Ok so you have %d electrodes in total? ',nelec));
 if nelec < 9
-  display('Thats not very many!');
+  display('That''s not very many!');
 elseif nelec <17
-  display('Come on this is 3D EIT. Its not the 1980s you know!');
+  display('Come on this is 3D EIT. It''s not the 1980s you know!');
 elseif nelec <65
   display('Ok sounds like enough!');
 else
@@ -125,17 +126,26 @@ fclose(fid);
 disp('Calling Netgen. Please wait.....');
 status= system(sprintf('ng -batchmode -geofile=%s  -meshfile=%s ',geofn,meshfn));
 if status~=0
-   error('Netgen call failed. Is netgen installed and on the search path?');
+   error('Netgen call failed. Is netgen installed and on the search path? see http://www.hpfem.jku.at/netgen/ for download');
 else
-   display('Netgen seems to have meshed your tank ok and written it to file')
+   display('Netgen seems to have meshed your tank ok and written it to file!');
+   display('..you just have to take your hats off to those guys at Johannes Kepler University, Linz,');
+   display('what a good job.');
 end
 disp(['Now reading back data from file: ' meshfn])
 [srf,vtx,fc,bc,simp,edg,mat_ind] = FEM_read_mesh_2(meshfn);
 disp([meshfn ' contains ' num2str(max(fc)) ' faces'])
 
+%Translate to v3 notation!
+tank_mdl.name = 'Tank model';
+tank_mdl.nodes= vtx;
+tank_mdl.elems= simp;
+tank_mdl.boundary= srf;
+
+
+
 disp('Now I need some help finding which faces are electrodes')
 
-disp('Do not choose and ground planes unless you have a vessel with a partly metal wall!')
 
 
 % Plot wire frame equivalent of mesh model
@@ -150,9 +160,33 @@ axis equal image; % Tightly fit square axes around plot
 mshaxs = axis; % Save present axes for use with faces
 pause(3)
 
-% Select the electrodes and ground planes (last electrode)
-[elecgnd,sels,sgnd] = FEM_define_electrodes_2(srf,vtx,bc,mshaxs);
+% Select the electrodes
+[elec,sels] = ng_tank_select_elec(srf,vtx,bc,mshaxs);
+
+size(elec)
+
+%[gnd_ind, electrodes, perm_sym, elec, protocol, no_pl] = get_model_elecs;
+
+
+while size(elec,1) ~= nelec 
+  display('That did''t work. Wrong number of electrodes! You need');
+  nelec
+  display('Have another try..');
+  [elec,sels] = ng_tank_select_elec(srf,vtx,bc,mshaxs);
+end
+
+for i=1:nelec
+    electrodes(i).z_contact= 1.0;  %well you can change that later!
+    electrodes(i).nodes=     unique( elec(i,:) );
+end
+
+perm_sym='{n}';
 
 
 
+tank_mdl.gnd_node=           1;
+tank_mdl.electrode =         electrodes;
+tank_mdl.misc.perm_sym =          perm_sym;
+
+save(fnstem,'tank_mdl')
 
