1ba4e2312386460fca61cf4c9da688dc394e1e1b
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2005-12-05 11:33:58 +0000

    functions for measurement inverse cov and R and RtR priors

--- trunk/eidors3d/calc_R_prior.m
+++ trunk/eidors3d/calc_R_prior.m
@@ -1,18 +1,18 @@
-function image_prior = calc_image_prior( inv_model, varargin )
+function R_prior = calc_R_prior( inv_model, varargin )
 % CALC_R_PRIOR: calculate regularization matrix R
 %   The image prior is matrix n_elem x ??? 
 % 
-% calc_image_prior can be called as
-%    image_prior= calc_image_prior( inv_model, ... )
+% calc_R_prior can be called as
+%    R_prior= calc_R_prior( inv_model, ... )
 %
 % in each case it will call the inv_model.image_prior.func
 %
-% image_prior   is the calculated image prior
+% R_prior      calculated regularization prior R
 % inv_model    is an inv_model structure
 
 % (C) 2005 Andy Adler. Licenced under the GPL Version 2
-% $Id: calc_R_prior.m,v 1.1 2005-12-02 11:59:17 aadler Exp $
+% $Id: calc_R_prior.m,v 1.2 2005-12-05 11:33:58 aadler Exp $
 
-image_prior= eidors_obj('calc-or-cache', inv_model, ...
-                 inv_model.image_prior.func, varargin{:} ); 
+R_prior= eidors_obj('calc-or-cache', inv_model, ...
+                 inv_model.R_prior.func, varargin{:} ); 
 
