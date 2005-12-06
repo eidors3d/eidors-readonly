36681ed390b63401b1ce1d0abb8c4189e1bc42a2
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2005-12-06 17:13:37 +0000

    calculate xyz centre of each simplex

---
+++ trunk/eidors3d/algorithms/ds_2005/center_of_simps.m
@@ -0,0 +1,44 @@
+function [center_simp]=center_of_simps(fwd_model)
+% CENTER_OF_SIMPS: Calculates the Center of Mass of the Simplicies.
+% [center_simp]=center_of_simps(fwd_model);
+%
+% center_simp = The center of mass of simps 
+%               [Nelems x 3] (x y z co-ordinates)
+%
+% (C) 2005 David Stephenson. Licensed under GPL Version 2
+% $Id: center_of_simps.m,v 1.1 2005-12-06 17:13:37 aadler Exp $
+
+vtx = fwd_model.nodes;
+simp= fwd_model.elems;
+
+for i=1:size(simp,1)
+    
+    x_sum=vtx(simp(i,1),1)+vtx(simp(i,2),1)+vtx(simp(i,3),1)+vtx(simp(i,4),1);
+    y_sum=vtx(simp(i,1),2)+vtx(simp(i,2),2)+vtx(simp(i,3),2)+vtx(simp(i,4),2);
+    z_sum=vtx(simp(i,1),3)+vtx(simp(i,2),3)+vtx(simp(i,3),3)+vtx(simp(i,4),3);
+    
+    x_center=x_sum/4;
+    y_center=y_sum/4;
+    z_center=z_sum/4;
+    
+    center_simp(i,1)=x_center;
+    center_simp(i,2)=y_center;
+    center_simp(i,3)=z_center;
+    
+    i=i+1;
+    
+end
+
+close(h);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This is part of the EIDORS suite.
+% Copyright (c) D.R Stephenson 2004
+% Copying permitted under terms of GNU GPL
+% See enclosed file gpl.html for details.
+% EIDORS 3D version XXX
+% MATLAB Version 6.5.0.180913a (R13)
+% MATLAB License Number: 1560
+% Operating System: Microsoft Windows XP Version 5.1 (Build 2600: Service Pack 1)
+% Java VM Version: Java 1.3.1_01 with Sun Microsystems Inc. Java HotSpot(TM) Client VM
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
