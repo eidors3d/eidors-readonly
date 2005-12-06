1297c947d0c4e090463bc22955586c0f634d484c
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2005-12-06 00:16:41 +0000

    generate animated gifs, too
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@466 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors3d/examples/moving_tank_objs.m
+++ eidors3d/examples/moving_tank_objs.m
@@ -1,26 +1,31 @@
 function moving_tank_objs
 % MOVING_TANK_OBJS: create movies of objects moving in tanks
 % Create moving objects and tanks
-% $Id: moving_tank_objs.m,v 1.3 2005-12-05 16:36:33 aadler Exp $
+% $Id: moving_tank_objs.m,v 1.4 2005-12-06 00:16:41 aadler Exp $
 load guardo_data.mat
 %global eidors_colours; eidors_colours.mapped_colour= 255;
 
 imdl= mk_common_model('b2c',16);
-imdl.hyperparameter.value= 1e-4;
+imdl.hyperparameter.value= 1e-2;
 imdl.RtR_prior.func= 'laplace_image_prior';
-imdl= rmfield(imdl,'image_prior');
-imdl.solve= 'inv_kalman_diff';
+if 0
+    imdl.solve= 'inv_kalman_diff';
+else
+    imdl.solve= 'aa_inv_solve';
+end
 
 vh= zc_h_demo4;
 vi= zc_demo4;
-% show_fem(inv_solve( imdl, vi(:,1), vh)); return
 imgs= inv_solve(imdl,vi,vh);
-mk_movie('back-forth.avi', imgs);
-return;
+%mk_movie('back-forth.gif', imgs);
+mk_movie2('back-forth.gif', imgs);
+
 vh= zc_h_demo3;
 vi= zc_demo3;
+imgs= inv_solve(imdl,vi,vh);
 
-mk_movie('turn.avi', imdl, vi, vh);
+%mk_movie('turn.avi', imgs);
+mk_movie2('turn.gif', imgs);
 
 
 
@@ -42,6 +47,25 @@ function mk_movie(fname, imgs)
    mov = close(mov);
    close(fig);
 
+function mk_movie2(fname, imgs)
+   mkdir('tmp_mk_movie2');
+   cmap= colormap;
+   for i=1:length(imgs)
+     img= show_slices(imgs(i));
+     imwrite(img,cmap, ...
+            sprintf('tmp_mk_movie2/img%05d.png',i), 'png');
+   end
+   retval= system(sprintf( ...
+       'convert -delay 25 tmp_mk_movie2/img*.png -loop 0 %s', ...
+       fname ));
+   if retval~=0
+       error('please ensure the imagemagick convert program is in your path');
+   end
+   if isunix
+       system('rm -r tmp_mk_movie2');
+   else
+       system('rm -r -d tmp_mk_movie2');
+   end
 
 
 
