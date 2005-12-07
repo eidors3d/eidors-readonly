13c0923995538a50f259ae2c03e24a5f488fa93d
billlion <billlion@82006732-fb7e-4a22-9f28-86a212cce563>
2005-12-07 13:03:03 +0000

    Moved from the grieve directory and prefix changed form FEM_ to ng_

---
+++ trunk/eidors3d/meshing/netgen/ng_extract_face.m
@@ -0,0 +1,27 @@
+function[fcsrf,fci] = ng_extract_face(srf,vtx,fc,fcnmb)
+
+% This function takes the wireframe model created by readmesh
+% and extracts one of the faces (fcnmb) into an indices
+% matrix (fci) mapped on to indices matrix srf which, in turn,
+% maps on to vtx. It also creates a matrix (fcsrf) which
+% directly maps this face into vtx.
+%
+% Version 2.4
+% B.D.Grieve - 23/01/2002
+%
+%
+% srf      = The boundary surfaces
+% vtx      = The vertices matrix
+% fc       = A one column matrix containing the face numbers
+% fcnmb    = The number of the face to be extracted
+% fcsrf    = The indices into the vtx matrix for this face
+% fci      = The indices into the srf matrix of this face
+
+fcsrf = [];
+fci = [];
+for loop1 = 1:size(fc,1)
+    if fc(loop1)==fcnmb
+        fcsrf = [fcsrf; srf(loop1,:)]; % Concatenate fcsrf to map directly with vtx for this face
+        fci = [fci; loop1]; % Concatenate fci with srf indices for this face
+    end
+end
