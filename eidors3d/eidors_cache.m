1a2d608fe7a78e6bfa2d03905006d1947309ba14
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2005-12-07 15:09:09 +0000

    control eidors caches
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@516 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors3d/eidors_cache.m
+++ eidors3d/eidors_cache.m
@@ -1,2 +1,24 @@
+function eidors_cache( command, varargin )
+% Control eidors_caching
+% Usage: eidors_cache( command, ... )
+%
+% USAGE:
+%   eidors_cache clear all
+%
+%   eidors_cache clear all
+%  
+%
+% (C) 2005 Andy Adler. Licensed under GPL version 2
+% $Id: eidors_cache.m,v 1.2 2005-12-07 15:09:09 aadler Exp $
+
+if nargin<1
+    return;
+end
+
 global eidors_objects;
-clear eidors_objects;
+if strcmp( command , 'clear')
+    eidors_objects= struct;
+    clear eidors_objects;
+else
+    error('command not understood');
+end
