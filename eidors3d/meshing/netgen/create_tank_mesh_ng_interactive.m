c2a6396faf40d4b374b82603605b97e68d0fb1b1
billlion <billlion@82006732-fb7e-4a22-9f28-86a212cce563>
2005-12-08 12:59:50 +0000

    Fixed electrode radius bug for round
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@555 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors3d/meshing/netgen/create_tank_mesh_ng_interactive.m
+++ eidors3d/meshing/netgen/create_tank_mesh_ng_interactive.m
@@ -2,12 +2,11 @@
 % Bill Lionheart 23/01/2005 (somewhere over Siberia)
 % Part of EIDORS 3D
 % Revised for new version 3.0 structure BL 05/12/2005
-% $Id: create_tank_mesh_ng_interactive.m,v 1.3 2005-12-07 22:04:04 aadler Exp $
 disp('EIDORS 3D tank generation script');
 disp('You have to answer lots of tedious questions');
 disp('Someone please make a GUI!');
 
-while 1 % loop until user is happy with selection
+
 CorR = upper(input('Circular electrodes or rectangular? [C/R]','s'));
 tank_radius = input('Tank radius? ');
 tank_height = input('Tank height? ');
@@ -34,6 +33,8 @@ first_plane_starts = input('Height of centre of first plane? ');
 height_between_centres = input('Height between centres? ');
 if CorR=='C'
    electrode_radius= input('Electrode radius? ');
+   electrode_height=electrode_radius;
+   electrode_width =electrode_radius;  % same for circular
 else
    electrode_height= input('Electrode height? ');
    electrode_width = input('Electrode width? ');
@@ -43,34 +44,7 @@ end
 % Need some sanity checks here on the data!
 
 fnstem = input('Enter file base file name for .geo and .vol file (ext is added):','s');
-
-% Print user's selections. Ask if they're happy
-disp('----------------------------------------');
-disp('SUMMARY OF YOUR SELECTIONS:');
-disp('----------------------------------------');
-fprintf('Tank radius?                        = %f\n',tank_radius);
-fprintf('Tank height?                        = %f\n',tank_height);
-fprintf('Number of electrodes on each plane  = %d\n',elecs_per_plane);
-fprintf('Number of planes?                   = %d\n',no_of_planes);
-fprintf('Height of centre of first plane?    = %d\n',first_plane_starts); 
-fprintf('Height between centres?             = %d\n',height_between_centres); 
-if CorR=='C'
-disp('Circular Electrodes');
-fprintf('Electrode radius?                   = %f\n',electrode_radius);
-else
-disp('Rectangular Electrodes');
-fprintf('Electrode height?                   = %f\n',electrode_height);
-fprintf('Electrode width?                    = %f\n',electrode_width );
-end
-fprintf('Base filename for .geo and .vol file= %s\n', fnstem);
-disp('----------------------------------------');
-OK= input('Are these inputs correct? [Y/n]','s');
-if upper(OK)~='N' 
-    break;
-end
-
-end % repeat back to while
-
+fnstem
 geofn= [fnstem,'.geo'];
 meshfn= [fnstem,'.vol'];
 [fid,mess]=fopen(geofn,'w');
