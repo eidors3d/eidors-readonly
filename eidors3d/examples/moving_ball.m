351934feb60cc8d22ce1f52ce7c69fe18c1ae311
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2005-12-08 23:16:18 +0000

    clean up code in moving ball
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@564 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors3d/examples/moving_ball.m
+++ eidors3d/examples/moving_ball.m
@@ -1,12 +1,12 @@
-inv3d= eidors_obj('inv_model', 'EIT inverse');
-inv3d.reconst_type= 'difference';
+inv3d= mk_common_model('n3r2',16);
 inv3d.jacobian_bkgnd.value = 1;
-inv3d.fwd_model.misc.perm_sym= '{y}';
-inv3d.hyperparameter.value = 1e-4;
+inv3d.hyperparameter.value = 1e-1;
 inv3d.solve=            'np_inv_solve';
 inv3d.R_prior.func=     'np_calc_image_prior';
 inv3d.np_calc_image_prior.parameters= [3 1]; %  deg=1, w=1
-inv3d.fwd_model= tank_mdls(1);
+inv3d.fwd_model.stimulation= mk_stim_patterns( 16,2,'{op}','{ad}',{'meas_current'});
+inv3d.fwd_model.nodes(:,[1,2])= inv3d.fwd_model.nodes(:,[1,2])*1.5;
 
-imgr= inv_solve(inv3d,tank_data(2),tank_data(1));
+ imgr= inv_solve(inv3d,tank_data(30),tank_data(1));
 
+ show_slices(imgr,linspace(.1,2.9,9)'*[inf,inf,1])
