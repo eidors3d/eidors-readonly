fbac119b9ab049b43e7e1aaa257cc54ba5364242
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2005-12-21 19:29:31 +0000

    Fixed NP bld_master to add sqrt 2 factor
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@585 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors3d/algorithms/np_2003/bld_master.m
+++ eidors3d/algorithms/np_2003/bld_master.m
@@ -82,7 +82,9 @@ slst = zeros(1,ns*dimen^2);
   slst(pat+8) = squeeze(sum([prod([JJ(1,1,:) ; JJ(2,2,:)]); prod([-JJ(2,1,:) ; JJ(1,2,:)])])) ./ dj; 
   
 
-LocJac = sparse(ilst,jlst,slst,dimen*ns,dimen*ns);
+%LocJac = sparse(ilst,jlst,slst,dimen*ns,dimen*ns);
+%Brad's correction - match Silvestri's and match equivalent resistor
+LocJac = sparse(ilst,jlst,slst,dimen*ns,dimen*ns)*sqrt(2);
 
 D = LocJac * D0;
 
