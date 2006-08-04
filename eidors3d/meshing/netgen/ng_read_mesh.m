dc335ff58634abc2163c135021c4cc4a94a0301b
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2006-08-04 20:02:47 +0000

    Clean up old code
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@669 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors3d/meshing/netgen/ng_read_mesh.m
+++ eidors3d/meshing/netgen/ng_read_mesh.m
@@ -24,7 +24,7 @@ function[srf,vtx,fc,bc,simp,edg,mat_ind] = ng_read_mesh(filename)
 % filename = Name of file containing NetGen .vol information
 % mat_ind  = Material index
 
-% $Id: ng_read_mesh.m,v 1.3 2006-08-04 20:02:05 aadler Exp $
+% $Id: ng_read_mesh.m,v 1.4 2006-08-04 20:02:47 aadler Exp $
 % (C) 2002-2006 (C) Licenced under the GPL
 
 %filename = input('Please enter the mesh filename [e.g. demo.vol]: ','s');
@@ -62,56 +62,3 @@ function mat= get_lines_with_numbers( fid, n_cols);
    mat= fscanf(fid,'%f',[n_cols,n_rows])';
 
 
-function oldjnk
-
-mdca = textread(filename,'%s');
-
-% Retrieve pointers and data length information from .vol file
-% Nested loops extract relevant information into arrays
-for loop3 = 1:size(mdca,1)
-    if strcmp(mdca(loop3),'surfaceelementsgi')
-        lngse = str2num(cat(2,mdca{loop3+1}));
-        startse = loop3+2;
-        % Put Surface Element data into the array srf
-        for loop1 = 0:lngse-1
-            for loop2 = 0:10
-                se(loop1+1,loop2+1) = str2num(cat(2,mdca{startse+loop1*11+loop2}));
-            end
-        end
-        disp([num2str(lngse) ' surface elements retrieved'])
-    end
-    if strcmp(mdca(loop3),'volumeelements')
-        lngve = str2num(cat(2,mdca{loop3+1}));
-        startve = loop3+2;
-        % Put Volume Element data into the array ve
-        for loop1 = 0:lngve-1
-            for loop2 = 0:5
-                ve(loop1+1,loop2+1) = str2num(cat(2,mdca{startve+loop1*6+loop2}));
-keyboard
-            end
-        end
-        disp([num2str(lngve) ' volume elements retrieved'])
-    end
-    if strcmp(mdca(loop3),'edgesegmentsgi2')
-        lnges = str2num(cat(2,mdca{loop3+1}));
-        startes = loop3+2;
-        % Put Edge Segment data into the array edg
-        for loop1 = 0:lnges-1
-            for loop2 = 0:11
-                es(loop1+1,loop2+1) = str2num(cat(2,mdca{startes+loop1*12+loop2}));
-            end
-        end
-        disp([num2str(lnges) ' edge segments retrieved'])
-    end
-    if strcmp(mdca(loop3),'points')
-        lngp = str2num(cat(2,mdca{loop3+1}));
-        startp = loop3+2;
-        % Put Points data into the array vtx
-        for loop1 = 0:lngp-1
-            for loop2 = 0:2
-                vtx(loop1+1,loop2+1) = str2num(cat(2,mdca{startp+loop1*3+loop2}));
-            end
-        end
-        disp([num2str(lngp) ' vertices retrieved'])
-    end
-end
