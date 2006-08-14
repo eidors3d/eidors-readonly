dbb0b4a7e32bb4e647e6d2ca3188e2ab9f0a2796
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2006-08-14 20:48:23 +0000

    use animate_reconstructions

--- trunk/eidors3d/examples/moving_tank_objs.m
+++ trunk/eidors3d/examples/moving_tank_objs.m
@@ -12,6 +12,10 @@ function imgr= moving_tank_objs(data_sel, inv_sel)
 %             30 => from netgen simulations
 %             40 => show netgen FEM simulation
 %
+%   if data_sel is a vector of EIT data, then it will
+%      be used for the reconstruction. The first measurement
+%      is the homogeneous, and the next are the data
+%
 % inv_sel
 %  2D reconstructions
 %   inv_sel = 1   => aa_inv_solve
@@ -19,13 +23,14 @@ function imgr= moving_tank_objs(data_sel, inv_sel)
 %   inv_sel = 2   => inv_kalman_diff
 % 
 % Create moving objects and tanks
-% $Id: moving_tank_objs.m,v 1.11 2006-08-12 00:01:24 aadler Exp $
+% $Id: moving_tank_objs.m,v 1.12 2006-08-14 20:48:23 aadler Exp $
 
 
 if nargin<1; data_sel = 1; end
 if nargin<2; inv_sel  = 1; end
 
-switch data_sel
+if isnumeric(data_sel) & size(data_sel)==[1,1]
+  switch data_sel
     case 10
         load montreal_data_1995;
         vh= zc_h_demo4;
@@ -45,14 +50,9 @@ switch data_sel
         filename= 'target-around10';
 
     case 30
-        load all_models_for_moving_ball
-        stim_pat = mk_stim_patterns(16,1,'{ad}','{ad}',{'no_meas_current'},1);
-        vh= do_simulation( tank_img_homg, stim_pat);
-        for i=1:2 %length(tank_img)
-            vi(:,i)= do_simulation( tank_img(i), stim_pat);
-        end
-        filename= 'ball-model';
-        % FIXME: something weird is happening here
+        load netgen_moving_ball
+        vh= vh;
+        vi= vi;
 
     case 40
         dirname= 'tmp_mk_movie2';
@@ -78,6 +78,11 @@ switch data_sel
 
     otherwise
         error('Don''t recognize data_sel');
+  end
+else
+  vh= data_sel(:,1);
+  vi= data_sel(:,2:end);
+  filename= 'user-data';
 end
 
 switch inv_sel
@@ -107,12 +112,13 @@ switch inv_sel
 
     case 4
         imdl= mk_common_model('b2c',16);
-        imdl.hyperparameter.value= 1e-2;
+        imdl.hyperparameter.value= 3e-2;
         time_steps= 1;
 
         imdl.RtR_prior= @time_smooth_prior;
+%       imdl.R_prior= @time_smooth_prior;
         imdl.time_smooth_prior.space_prior= @laplace_image_prior;
-        imdl.time_smooth_prior.time_weight= .5;
+        imdl.time_smooth_prior.time_weight= .2;
         imdl.time_smooth_prior.time_steps=  time_steps;
 
         imdl.solve= @time_prior_solve;
@@ -122,53 +128,8 @@ switch inv_sel
 end
 
 imgs= inv_solve(imdl,vi,vh);
-mk_movie2(filename, imgs);
+animate_reconstructions(filename, imgs);
 fprintf('file %s.gif created\n',filename);
-
-
-
-% create avi movie fname
-% imdl is reconstruction model
-% vi is inhomogeneous data sequence
-% vh is homogeneous data
-function mk_movie(fname, imgs)
-   fig=figure;
-   set(fig,'DoubleBuffer','on');
-   mov = avifile( [fname ,'.avi'] );%, 'Compression', 'RLE' );
-
-   for i=1:length(imgs)
-     show_slices(imgs(i));
-     F = getframe(gca);
-     mov = addframe(mov,F);
-   end
-   mov = close(mov);
-   close(fig);
-
-function mk_movie2(fname, imgs)
-   calc_colours('mapped_colour', 127);
-   dirname= 'tmp_mk_movie2';
-   rm_rf( dirname );
-   mkdir( dirname );
-
-   r_img= show_slices(imgs);
-   c_img = calc_colours( r_img);
-   out_img= reshape(c_img, size(r_img,1), size(r_img,2) ,[]);
-   cmap= colormap;
-
-   for i=1:length(imgs)
-     imwrite(out_img(:,:,i),cmap, ...
-            sprintf('%s/img%05d.png',dirname, i), 'png');
-   end
-
-   ld_lib_path= sys_dep;
-
-   retval= system(sprintf( ...
-       '%s convert -delay 25 %s/img*.png -loop 0 %s.gif', ...
-       ld_lib_path, dirname, fname ));
-   if retval~=0
-       error('please ensure the imagemagick convert program is in your path');
-   end
-   rm_rf(dirname);
 
 function rm_rf(dirname)
    if isdir(dirname)==0
@@ -180,26 +141,3 @@ function rm_rf(dirname)
    else
        system(['rmdir /s /q "',dirname,'"']);
    end
-
-
-% remove all electrodes except the first 16.
-% apply stimulation pattern and return data
-function vv= do_simulation( img, stim_pat)
-
-   img.fwd_model.electrode= ...
-      img.fwd_model.electrode(1:16);
-
-   img.fwd_model.stimulation= stim_pat;
-
-   vv= fwd_solve( img);
-
-% work around stupid matlab bugs
-function ld_lib_path= sys_dep;
-   ld_lib_path='';
-   if  strfind(system_dependent('getos'),'Linux')
-     s=ver;
-      if str2num(s.Version)>=7
-        %Version 7 under linux sets the LD_LIBRARY_PATH and that breaks external progs
-          ld_lib_path='LD_LIBRARY_PATH=;';
-      end      
-   end    
