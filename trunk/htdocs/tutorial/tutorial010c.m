658c965c86784c7caab0d2587f82060ac85bd38b
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2006-08-17 20:44:02 +0000

    Basic tutorial

--- trunk/htdocs/tutorial/tutorial010c.m
+++ trunk/htdocs/tutorial/tutorial010c.m
@@ -1,5 +1,5 @@
 % Reconstruct images
-% $Id: tutorial010c.m,v 1.1 2006-08-17 20:32:24 aadler Exp $
+% $Id: tutorial010c.m,v 1.2 2006-08-17 20:44:02 aadler Exp $
 
 subplot(131)
 show_fem(sim_img);
@@ -14,4 +14,8 @@ show_fem(rec_img)
 subplot(133)
 show_slices(rec_img,[inf,inf,2.0,1,1; ...
                      inf,inf,1.0,1,2]);
+
+pp=get(gcf,'paperposition');
+set(gcf,'paperposition',pp.*[1,1,1.4,1]);
 print -r75 -dpng tutorial010c.png
+set(gcf,'paperposition',pp.*[1,1,1,1]);
