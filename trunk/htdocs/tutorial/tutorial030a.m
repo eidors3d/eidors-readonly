041e10cb84092fb1c82a0fd77a5d4372e3bcc765
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2006-08-18 18:03:27 +0000

    stim patterns

--- trunk/htdocs/tutorial/tutorial030a.m
+++ trunk/htdocs/tutorial/tutorial030a.m
@@ -1,16 +1,17 @@
 % Explore Stimulation Patterns
-% $Id: tutorial030a.m,v 1.1 2006-08-18 17:36:19 aadler Exp $
+% $Id: tutorial030a.m,v 1.2 2006-08-18 18:03:27 aadler Exp $
 
 % 3D Model
 imdl_3d= mk_common_model('n3r2',16);
 fmdl= imdl_3d.fwd_model;
 
-% Show
+% Show opposite pattern
 fmdl.stimulation=mk_stim_patterns(16,2, '{op}','{op}', ...
              {'meas_current','no_redundant'} );
 subplot(121)
 display_meas(fmdl,'ya')
 
+% Show adjacent pattern
 fmdl.stimulation=mk_stim_patterns(16,2, '{ad}','{ad}', ...
              {'no_meas_current','do_redundant'} );
 subplot(122)
