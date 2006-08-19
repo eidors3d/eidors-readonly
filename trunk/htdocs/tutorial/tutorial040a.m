1b615378ab73102d2793e61a2df161be3e4a0de7
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2006-08-19 00:51:04 +0000

    tutorial #4 - netgen

--- trunk/htdocs/tutorial/tutorial040a.m
+++ trunk/htdocs/tutorial/tutorial040a.m
@@ -1,17 +1,27 @@
 % Use Netgen
-% $Id: tutorial040a.m,v 1.1 2006-08-18 19:33:25 aadler Exp $
+% $Id: tutorial040a.m,v 1.2 2006-08-19 00:51:04 aadler Exp $
 
 [tank_mdl,centres] = create_tank_mesh_ng( ...
-  15,     ... % tank_radius,
-  30,     ... % tank_height,
-  'C',    ... % CorR,  - circular or rectangular
-  4,      ... % log2_electrodes_per_plane,
-  2,      ... % no_of_planes,
-  10,     ... % first_plane_starts,
-  10,     ... % height_between_centres,
-  2,      ... % electrode_width, (radius for circ elecs)
-  3,      ... % electrode_height,
-  'my_mdl');  % fnstem - filename without extension
+  15,      ... % tank_radius,
+  30,      ... % tank_height,
+  'R',     ... % CorR,  - circular or rectangular
+  4,       ... % log2_electrodes_per_plane,
+  2,       ... % no_of_planes,
+  10,      ... % first_plane_starts,
+  10,      ... % height_between_centres,
+  2,       ... % electrode_width, (radius for circ elecs)
+  3,       ... % electrode_height,
+  'my_mdl',... % fnstem - filename without extension
+  0 );         % elec_mesh_density - increase mesh near electrodes
 
+subplot(121)
+show_fem(tank_mdl)
+view(-28,10)
 
-print -r75 -dpng tutorial040a.png;
+[tank_mdl2,centres] = create_tank_mesh_ng( ...
+  15, 30, 'C', 4, 2, 10, 10, 2, 3, 'my_mdl', 10 );
+subplot(122)
+show_fem(tank_mdl2)
+view(-28,10)
+
+print -r100 -dpng tutorial040a.png;
