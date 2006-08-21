b1091b6794efd2bfa70f5985b694612b8822d671
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2006-08-21 04:30:45 +0000

    update tutorial#12
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@743 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/tutorial120a.m
+++ htdocs/tutorial/tutorial120a.m
@@ -1,9 +1,10 @@
 % Compare 2D algorithms
-% $Id: tutorial120a.m,v 1.3 2006-08-21 03:45:57 aadler Exp $
+% $Id: tutorial120a.m,v 1.4 2006-08-21 04:30:45 aadler Exp $
 
 imb=  mk_common_model('c2c',16);
 
 e= size(imb.fwd_model.elems,1);
+bkgnd= 1;
 
 % Solve Homogeneous model
 img= eidors_obj('image','');
@@ -15,6 +16,11 @@ vh= fwd_solve( img );
 img.elem_data([25,37,49:50,65:66,81:83,101:103,121:124])=bkgnd * 2;
 img.elem_data([95,98:100,79,80,76,63,64,60,48,45,36,33,22])=bkgnd * 2;
 vi= fwd_solve( img );
+
+% Add 15dB noise
+vi_n= vi; 
+vi_n.meas = vi.meas + std(vi.meas - vh.meas)/10^(15/20) ...
+                     *randn(size(vi.meas));
 
 show_fem(img);
 axis square; axis off
--- htdocs/tutorial/tutorial120b.m
+++ htdocs/tutorial/tutorial120b.m
@@ -1,5 +1,5 @@
 % Compare 2D algorithms
-% $Id: tutorial120b.m,v 1.1 2006-08-21 03:45:57 aadler Exp $
+% $Id: tutorial120b.m,v 1.2 2006-08-21 04:30:45 aadler Exp $
 
 % Create Inverse Model
 inv2d= eidors_obj('inv_model', 'EIT inverse');
@@ -16,6 +16,7 @@ inv2d.hyperparameter.value = 1e-3;
 inv2d.RtR_prior=   @laplace_image_prior;
 inv2d.solve=       @np_inv_solve;
 imgr(1)= inv_solve( inv2d, vi, vh);
+imgn(1)= inv_solve( inv2d, vi_n, vh);
 
 % Automatic hyperparameter selection
 inv2d.hyperparameter.func = @aa_calc_noise_figure;
@@ -24,6 +25,7 @@ inv2d.hyperparameter.tgt_elems= 1:4;
 inv2d.RtR_prior=   @aa_calc_image_prior;
 inv2d.solve=       @aa_inv_solve;
 imgr(2)= inv_solve( inv2d, vi, vh);
+imgn(2)= inv_solve( inv2d, vi_n, vh);
 
 % Total variation using PDIPM
 inv2d.hyperparameter.value = 1e-2;
@@ -31,13 +33,20 @@ inv2d.solve=       @ab_tv_diff_solve;
 inv2d.R_prior=     @ab_calc_tv_prior;
 inv2d.parameters.max_iterations= 20;
 inv2d.parameters.term_tolerance= 1e-3;
-tvimg= inv_solve( inv2d, vi, vh);
 
 % TVimg will add the background value
+tvimg= inv_solve( inv2d, vi, vh);
 tvimg.elem_data = tvimg.elem_data - bkgnd;
-img(3)= inv_solve( inv2d, vi, vh);
+imgr(3)= tvimg;
+
+tvimg= inv_solve( inv2d, vi_n, vh);
+tvimg.elem_data = tvimg.elem_data - bkgnd;
+imgn(3)= tvimg;
 
 % Output image
+subplot(211)
 show_slices(imgr, [inf,inf,0,1,1]);
+subplot(212)
+show_slices(imgn, [inf,inf,0,1,1]);
 
 print -r100 -dpng tutorial120b.png;
