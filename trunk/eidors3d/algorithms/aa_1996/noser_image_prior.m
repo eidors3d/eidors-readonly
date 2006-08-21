81d4d85de23717be699bfaa8243a2dcab8d50c5c
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2006-08-21 18:02:01 +0000

    fixed syntax

--- trunk/eidors3d/algorithms/aa_1996/noser_image_prior.m
+++ trunk/eidors3d/algorithms/aa_1996/noser_image_prior.m
@@ -9,19 +9,19 @@ function Reg= noser_image_prior( inv_model );
 % setting inv_model.noser_image_prior.exponent=2
 
 % (C) 2005 Andy Adler. Licenced under the GPL Version 2
-% $Id: noser_image_prior.m,v 1.1 2006-08-21 17:56:28 aadler Exp $
+% $Id: noser_image_prior.m,v 1.2 2006-08-21 18:02:01 aadler Exp $
 
     img_bkgnd= calc_jacobian_bkgnd( inv_model );
     J = calc_jacobian( inv_model.fwd_model, img_bkgnd);
 
     exponent= 2;
-    if isfiels(inv_model,'noser_image_prior');
+    if isfield(inv_model,'noser_image_prior');
        exponent= inv_model.noser_image_prior.exponent;
     end
 
     l_prior= size(J,2);
 
     % Reg is spdiags(diag(J'*J),0, l_prior, l_prior);
-    diag_col sum(J.^exponent,1)';
+    diag_col= sum(J.^exponent,1)';
     Reg = spdiags( diag_col, 0, l_prior, l_prior);
 
