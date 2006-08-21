d33c08372f557d69daccce00b36709b7bd2ea989
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2006-08-21 21:00:45 +0000

    Icrime tutorial complete
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@758 82006732-fb7e-4a22-9f28-86a212cce563

---
+++ htdocs/tutorial/tutorial210g.m
@@ -0,0 +1,27 @@
+% Reconstruct images with cheating Laplace prior (large model)
+% $Id: tutorial210g.m,v 1.1 2006-08-21 21:00:45 aadler Exp $
+
+lmdl= mk_common_model('c2c');
+lmdl.RtR_prior= @tutorial210_cheat_laplace;
+lmdl.tutorial210_cheat_laplace.cheat_weight= .3;
+
+% Normal Tikhonov prior
+lmdl.tutorial210_cheat_laplace.cheat_elements= [];
+im_st(1)= inv_solve(lmdl, vi, vh);
+
+% Normal Tikhonov with sad face
+lmdl.tutorial210_cheat_laplace.cheat_elements=  ...
+     large_face.sad;
+im_st(2)= inv_solve(lmdl, vi, vh);
+
+% Normal Tikhonov with halarge_facey face
+lmdl.tutorial210_cheat_laplace.cheat_elements=  ...
+     large_face.happy;
+im_st(3)= inv_solve(lmdl, vi, vh);
+
+% Normal Tikhonov with half face
+lmdl.tutorial210_cheat_laplace.cheat_elements=  ...
+     large_face.halfy;
+im_st(4)= inv_solve(lmdl, vi, vh);
+
+show_slices( im_st, levels);
