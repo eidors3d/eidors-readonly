30590e0fc4eaa202b1633a8e81ce4d33b0c6d5d8
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2006-08-21 21:00:45 +0000

    Icrime tutorial complete

---
+++ trunk/htdocs/tutorial/tutorial210_cheat_laplace.m
@@ -0,0 +1,41 @@
+function Reg= tutorial210_cheat_laplace( inv_model )
+% Reg= cheat_laplace( inv_model )
+% Reg        => output regularization term
+% Parameters:
+%   elems    = inv_model.tutorial210_cheat_laplace.cheat_elements;
+%            => elements weights to modify
+%   weight   = inv_model.tutorial210_cheat_laplace.cheat_weight;
+%            => new weight to set elements to
+
+pp= aa_fwd_parameters( inv_model.fwd_model );
+
+ROI = zeros(1,pp.n_elem);
+ROI( inv_model.tutorial210_cheat_laplace.cheat_elements ) = 1;
+
+Iidx= [];
+Jidx= [];
+Vidx= [];
+for ii=1:pp.n_elem
+  el_adj = find_adjoin( ii, pp.ELEM );
+  for jj=el_adj(:)'
+      if (ROI(ii) + ROI(jj)) == 1 %one only
+         fac= inv_model.tutorial210_cheat_laplace.cheat_weight *.5;
+      else 
+         fac = .5;
+      end
+      Iidx= [Iidx,      ii, ii, jj, jj];
+      Jidx= [Jidx,      ii, jj, ii, jj];
+      Vidx= [Vidx, fac*([1, -1, -1,  1]) ];
+  end
+end
+Reg = sparse(Iidx,Jidx, Vidx, pp.n_elem, pp.n_elem );
+
+% find elems which are connected to elems ee
+function elems= find_adjoin(ee, ELEM)
+   nn= ELEM(:,ee);
+   [d,e]= size(ELEM);
+   ss= zeros(1,e);
+   for i=1:d
+     ss= ss+ any(ELEM==nn(i));
+   end
+   elems= find(ss==d-1);
---
+++ trunk/htdocs/tutorial/tutorial210f.m
@@ -0,0 +1,27 @@
+% Reconstruct images with cheating Laplace prior (small model)
+% $Id: tutorial210f.m,v 1.1 2006-08-21 21:00:45 aadler Exp $
+
+smdl= mk_common_model('b2c');
+smdl.RtR_prior= @tutorial210_cheat_laplace;
+smdl.tutorial210_cheat_laplace.cheat_weight= .3;
+
+% Normal Tikhonov prior
+smdl.tutorial210_cheat_laplace.cheat_elements= [];
+im_st(1)= inv_solve(smdl, vi, vh);
+
+% Normal Tikhonov with sad face
+smdl.tutorial210_cheat_laplace.cheat_elements=  ...
+    [small_face.eyes, small_face.sad];
+im_st(2)= inv_solve(smdl, vi, vh);
+
+% Normal Tikhonov with hasmall_facey face
+smdl.tutorial210_cheat_laplace.cheat_elements=  ...
+    [small_face.eyes, small_face.smile];
+im_st(3)= inv_solve(smdl, vi, vh);
+
+% Normal Tikhonov with half face
+smdl.tutorial210_cheat_laplace.cheat_elements=  ...
+    [small_face.eyes, small_face.rsmile, small_face.lsad];
+im_st(4)= inv_solve(smdl, vi, vh);
+
+show_slices( im_st, levels);
