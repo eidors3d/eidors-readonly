12f9f03ac029a9dd9e80d5fcba010fa31af9a456
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2006-08-21 20:31:51 +0000

    reconstruct with noise

---
+++ trunk/htdocs/tutorial/tutorial210c.m
@@ -0,0 +1,14 @@
+% Test image for different noise
+% $Id: tutorial210c.m,v 1.1 2006-08-21 20:31:51 aadler Exp $
+
+il_g= mk_common_model('c2c');
+
+num_tries=6;
+vi_n(1:num_tries) = vi;
+for i= 1:num_tries % matlab can't vectorize
+   noise = .002*randn(size(vi.meas));
+   vi_n(i).meas = vi_n(i).meas + noise;
+end
+
+levels= [0,0,0,1,1];
+show_slices( inv_solve( il_g, vi_n, vh ), levels);
