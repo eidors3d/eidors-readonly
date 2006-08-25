24bb423819eab43b9ace51ab0a48f2116ffdf167
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2006-08-25 00:10:23 +0000

    allow cropping of FEM models

---
+++ trunk/eidors3d/graphics_matlab/crop_model.m
@@ -0,0 +1,44 @@
+function crop_model( axis_handle, fcn_handle );
+% crop_model( axis_handle, fcn_handle );
+% Crop away parts of a fem model
+% 
+% if axis_handle==[], then use the current axis
+% examples:
+%   crop_model([],  inline('z==3','x','y','z'))
+%   crop_model(gca, inline('x+y>0','x','y','z'))
+
+% (C) 2006 Andy Adler. Licenced under the GPL Version 2
+% $Id: crop_model.m,v 1.1 2006-08-25 00:10:23 aadler Exp $
+
+if exist('OCTAVE_VERSION');
+   warning('show_fem does not support octave');
+   return
+end
+
+if isempty(axis_handle)
+   axis_handle= gca;
+end
+
+kk= get(axis_handle,'Children');
+% only the FEM frame
+%k=kk( find( kk== min(kk) ));
+
+for k= kk(:)'
+   try
+      x= get(k,'XData');
+      y= get(k,'YData');
+      z= get(k,'ZData');
+      c= get(k,'CData');
+      idx= ~all( feval(fcn_handle,x,y,z) );
+      if any(size(c)>[1,1])
+         set(k,'Xdata', x(:,idx), ...
+               'Ydata', y(:,idx), ...
+               'Zdata', z(:,idx), ...
+               'Cdata', c(:,idx));
+      else
+         set(k,'Xdata', x(:,idx), ...
+               'Ydata', y(:,idx), ...
+               'Zdata', z(:,idx));
+      end
+   end
+end
