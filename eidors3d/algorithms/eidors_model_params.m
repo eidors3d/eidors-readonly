0d297ba90afd46d04da07d3fe75a090756395be0
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2006-11-15 19:38:13 +0000

    add n_elem etc
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@839 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors3d/algorithms/eidors_model_params.m
+++ eidors3d/algorithms/eidors_model_params.m
@@ -3,16 +3,19 @@ function mdl = eidors_model_params( mdl );
 % Fill in default parameter values in EIDORS types
 %
 % (C) 2006 Andy Adler. Licensed under the GPL v2
-% $Id: eidors_model_params.m,v 1.1 2006-11-15 19:36:06 aadler Exp $
+% $Id: eidors_model_params.m,v 1.2 2006-11-15 19:38:13 aadler Exp $
 
 switch mdl.type;
-   case 'fwd_model';
+   case 'inv_model';
    case 'fwd_model';
       if ~isfield(obj,'normalize_measurements');
          mdl.normalize_measurements= 0;
       end
       mdl.elems=    double(mdl.elems);
       mdl.boundary= double(mdl.boundary);
+
       % fill in boundary if it doesn't exist
-      % fill in boundary n_elems, n_nodes
+      mdl.n_elem = size(mdl.elems,1);
+      mdl.n_node = size(mdl.nodes,1);
+      mdl.n_elec = length(mdl.electrode);
 end
