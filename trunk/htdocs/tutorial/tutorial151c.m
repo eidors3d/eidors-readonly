fe527786a0e9b11352c400264054064ecf0ef895
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2006-11-15 20:36:27 +0000

    fixes of eidors

--- trunk/htdocs/tutorial/tutorial151c.m
+++ trunk/htdocs/tutorial/tutorial151c.m
@@ -1,16 +1,17 @@
 % Difference imaging result
-% $Id: tutorial151c.m,v 1.1 2006-11-15 17:16:27 aadler Exp $
+% $Id: tutorial151c.m,v 1.2 2006-11-15 20:36:27 aadler Exp $
 
 % imdl is loaded from file tutorial151_model.mat
 imdl.reconst_type= 'difference';
 imdl.solve=        @np_inv_solve;
 imdl.jacobian_bkgnd.value= backgnd;
+imdl.hyperparameter.value= 1e-1;
 img_diff= inv_solve(imdl, v_homg, v_targ);
 
 clf;
 axes('position',[.1,.1,.65,.8]);
-   show_fem(sim_img); view(-33,20);
+   show_fem(img_diff); view(-33,20);
 axes('position',[.8,.1,.15,.8]);
-   show_slices(sim_img,5);
+   show_slices(img_diff,5);
 
-print -r75 -dpng tutorial151a.png;
+print -r75 -dpng tutorial151c.png;
