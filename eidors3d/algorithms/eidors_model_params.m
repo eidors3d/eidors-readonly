33d6a65fe75deb10a3b964d0b4e3ec314a5660f5
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2006-11-15 19:45:33 +0000

    bug fix
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@840 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors3d/algorithms/eidors_model_params.m
+++ eidors3d/algorithms/eidors_model_params.m
@@ -3,12 +3,16 @@ function mdl = eidors_model_params( mdl );
 % Fill in default parameter values in EIDORS types
 %
 % (C) 2006 Andy Adler. Licensed under the GPL v2
-% $Id: eidors_model_params.m,v 1.2 2006-11-15 19:38:13 aadler Exp $
+% $Id: eidors_model_params.m,v 1.3 2006-11-15 19:45:33 aadler Exp $
+
+% TODO - to caching
 
 switch mdl.type;
    case 'inv_model';
+      mdl.fwd_model= eidors_model_params( mdl.fwd_model );
+
    case 'fwd_model';
-      if ~isfield(obj,'normalize_measurements');
+      if ~isfield(mdl,'normalize_measurements');
          mdl.normalize_measurements= 0;
       end
       mdl.elems=    double(mdl.elems);
