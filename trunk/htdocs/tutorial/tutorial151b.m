81f0863b7a17b89095937a2b1f6f14b3b6944a13
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2006-11-15 17:02:45 +0000

    simulate voltages

---
+++ trunk/htdocs/tutorial/tutorial151b.m
@@ -0,0 +1,16 @@
+% Simulate data for difference and absolute imaging
+% $Id: tutorial151b.m,v 1.1 2006-11-15 17:02:45 aadler Exp $
+
+% Create homogeneous simulation Model
+backgnd= .02; 
+sim_img.elem_data= backgnd*ones(size(simmdl.elems,1),1);
+v_homg= fwd_solve(sim_img);
+
+% Create target simulation Model
+sim_img.elem_data(target1)= backgnd*2;
+sim_img.elem_data(target2)= backgnd/4;
+v_targ= fwd_solve(sim_img);
+
+clf; subplot(211);
+plot([v_homg.meas, v_targ.meas]);
+print -r75 -dpng tutorial151b.png;
