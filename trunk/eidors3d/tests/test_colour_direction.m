8422146ab5ad69e038c3801b0d1aaeba53641f9d
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2006-11-17 00:21:57 +0000

    test reconstructed colour directions

---
+++ trunk/eidors3d/tests/test_colour_direction.m
@@ -0,0 +1,25 @@
+% Test colour mapping
+
+% 2D Test
+imdl= mk_common_model('b2c',16);
+s_img= eidors_obj('image','');
+s_img.fwd_model= imdl.fwd_model;
+s_img.elem_data= ones(size(s_img.fwd_model.elems,1),1);
+v_homg = fwd_solve(s_img);
+s_img.elem_data(1)= 1.1;
+v_targ = fwd_solve(s_img);
+imgr1= inv_solve(imdl,v_homg,v_targ);
+show_fem(imgr1);
+disp('image should be red');
+
+% 3D Test
+imdl= mk_common_model('n3r2',[16 2]);
+s_img= eidors_obj('image','');
+s_img.fwd_model= imdl.fwd_model;
+s_img.elem_data= ones(size(s_img.fwd_model.elems,1),1);
+v_homg = fwd_solve(s_img);
+s_img.elem_data(1)= 1.1;
+v_targ = fwd_solve(s_img);
+imgr1= inv_solve(imdl,v_homg,v_targ);
+show_fem(imgr1);
+disp('image should be red');
