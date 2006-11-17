3cf5254a766df5e36688f6f851a03a9720a19aaf
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2006-11-17 03:57:30 +0000

    Absolute imaging
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@849 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/tutorial151d.m
+++ htdocs/tutorial/tutorial151d.m
@@ -1,15 +1,15 @@
 % Difference imaging result
-% $Id: tutorial151d.m,v 1.1 2006-11-17 03:23:29 aadler Exp $
+% $Id: tutorial151d.m,v 1.2 2006-11-17 03:57:30 aadler Exp $
 
 % imdl is loaded from file tutorial151_model.mat
 imdl.reconst_type= 'static';
 imdl.solve=        @tutorial151_nonlinearGN;
 imdl.parameters.term_tolerance= 1e-5;
-imdl.parameters.max_iterations= 5;
+imdl.parameters.max_iterations= 10;
 % special parameter for this model
 imdl.tutorial151_nonlinearGN.init_backgnd= backgnd;
 
-imdl.hyperparameter.value= 1e-2;
+imdl.hyperparameter.value= 3e-1;
 img_diff= inv_solve(imdl, v_targ);
 
 clf;
