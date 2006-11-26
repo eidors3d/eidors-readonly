0b717b8259ef1893609a8a3f7ba33aeb5982ff32
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2006-11-26 01:01:55 +0000

    script to calculate figures from netgen
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@884 82006732-fb7e-4a22-9f28-86a212cce563

---
+++ htdocs/tutorial/tutorial052a.m
@@ -0,0 +1,15 @@
+% Radial move ball in a single electrode plane
+% $Id: tutorial052a.m,v 1.1 2006-11-26 01:01:55 aadler Exp $
+
+electrodes_per_plane= 16;
+number_of_planes= 1;
+movement_pattern= 'radial_move';
+finelevel= '-veryfine';
+refine_electrodes= 20;
+tank_radius= 15;
+tank_height= 4;
+electrode_width = 0.25;
+electrode_height= 0.25;
+rect_or_circ_electrode= 'C';
+
+move_the_ball;
