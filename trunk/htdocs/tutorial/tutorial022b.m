9c5c7b8a61c6dfaff460b4238e32bcc72e55fd16
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2007-03-26 23:51:47 +0000

    rectangle tutorial

---
+++ trunk/htdocs/tutorial/tutorial022b.m
@@ -0,0 +1,9 @@
+% 2D image $Id: tutorial022b.m,v 1.1 2007-03-26 23:51:47 aadler Exp $
+
+img= eidors_obj('image','2D rectangle', ...
+      'elem_data', ones(size(mdl.elems,1),1) * conduc, ...
+      'fwd_model', mdl); 
+
+fsol= fwd_solve(img)
+%show_fem(mdl); axis('equal'); set(gca,'Ylim',[-.5,ww-.5]);
+%print -r100 -dpng tutorial022b.png
