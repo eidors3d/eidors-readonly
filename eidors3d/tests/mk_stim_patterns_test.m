79734f32497d1ee3e137eeff771e965ca22e788c
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2007-03-27 18:11:27 +0000

    fixed for latest patterns - why did measurments change from -1,1 to 1,-1?
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@905 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors3d/tests/mk_stim_patterns_test.m
+++ eidors3d/tests/mk_stim_patterns_test.m
@@ -2,7 +2,7 @@ function ok= mk_stim_patterns_test
 % Verify mk_stim_patterns function
 
 % (C) 2005 Andy Adler. Licenced under the GPL Version 2
-% $Id: mk_stim_patterns_test.m,v 1.2 2005-12-14 16:33:05 aadler Exp $
+% $Id: mk_stim_patterns_test.m,v 1.3 2007-03-27 18:11:27 aadler Exp $
 
 ok= 1;
 
@@ -43,80 +43,80 @@ function ok= test_adj(pat)
 
    ok=1;
    if length(pat) ~= 16
-      ok=0; warning('Fail at pt#01'); return; end
+      ok=0; eidors_msg('Fail at pt#01',1); return; end
 
    if ~strcmp( pat(1).stimulation, 'mA');
-      ok=0; warning('Fail at pt#02'); return; end
+      ok=0; eidors_msg('Fail at pt#02',1); return; end
 
    % Stim pattern # 1
    if pat(1).stim_pattern ~= [-1;1;zeros(14,1)]
-      ok=0; warning('Fail at pt#03'); return; end
+      ok=0; eidors_msg('Fail at pt#03',1); return; end
 
    meas= pat(1).meas_pattern;
 
    if any( size(meas)~= [13 16] )
-      ok=0; warning('Fail at pt#04'); return; end
+      ok=0; eidors_msg('Fail at pt#04',1); return; end
 
-   if any( meas(1,:)~= [0,0,1,-1,zeros(1,12)] )
-      ok=0; warning('Fail at pt#05'); return; end
+   if any( meas(1,:)~= [0,0,-1,1,zeros(1,12)] )
+      ok=0; eidors_msg('Fail at pt#05',1); return; end
 
-   if any( meas(13,:)~= [zeros(1,14),1,-1] )
-      ok=0; warning('Fail at pt#06'); return; end
+   if any( meas(13,:)~= [zeros(1,14),-1,1] )
+      ok=0; eidors_msg('Fail at pt#06',1); return; end
 
    % Stim pattern # 10
    if any( pat(10).stim_pattern ~= [zeros(9,1);-1;1;zeros(5,1)] )
-      ok=0; warning('Fail at pt#07'); return; end
+      ok=0; eidors_msg('Fail at pt#07',1); return; end
 
    meas= pat(10).meas_pattern;
 
    if any( size(meas)~= [13 16] )
-      ok=0; warning('Fail at pt#08'); return; end
+      ok=0; eidors_msg('Fail at pt#08',1); return; end
 
-   if any( meas(1,:)~= [1,-1,zeros(1,14)] )
-      ok=0; warning('Fail at pt#09'); return; end
+   if any( meas(1,:)~= [-1,1,zeros(1,14)] )
+      ok=0; eidors_msg('Fail at pt#09',1); return; end
 
-   if any( meas(13,:)~= [-1,zeros(1,14),1] )
-      ok=0; warning('Fail at pt#10'); return; end
+   if any( meas(13,:)~= [1,zeros(1,14),-1] )
+      ok=0; eidors_msg('Fail at pt#10',1); return; end
 
 function ok= test_adj_full(pat)
    eidors_msg('test adjacent current pattern (full)',2);
 
    ok=1;
    if length(pat) ~= 16
-      ok=0; warning('Fail at pt#11'); return; end
+      ok=0; eidors_msg('Fail at pt#11',1); return; end
 
    if ~strcmp( pat(1).stimulation, 'mA');
-      ok=0; warning('Fail at pt#12'); return; end
+      ok=0; eidors_msg('Fail at pt#12',1); return; end
 
    % Stim pattern # 1
    if pat(1).stim_pattern ~= [-1;1;zeros(14,1)]
-      ok=0; warning('Fail at pt#13'); return; end
+      ok=0; eidors_msg('Fail at pt#13',1); return; end
 
    meas= pat(1).meas_pattern;
 
    if any( size(meas)~= [16 16] )
-      ok=0; warning('Fail at pt#14'); return; end
+      ok=0; eidors_msg('Fail at pt#14',1); return; end
 
-   if any( meas(1,:)~= [1,-1,zeros(1,14)] )
-      ok=0; warning('Fail at pt#15'); return; end
+   if any( meas(1,:)~= [-1,1,zeros(1,14)] )
+      ok=0; eidors_msg('Fail at pt#15',1); return; end
 
-   if any( meas(13,:)~= [zeros(1,12),1,-1,0,0] )
-      ok=0; warning('Fail at pt#16'); return; end
+   if any( meas(13,:)~= [zeros(1,12),-1,1,0,0] )
+      ok=0; eidors_msg('Fail at pt#16',1); return; end
 
    % Stim pattern # 10
    if any( pat(10).stim_pattern ~= [zeros(9,1);-1;1;zeros(5,1)] )
-      ok=0; warning('Fail at pt#17'); return; end
+      ok=0; eidors_msg('Fail at pt#17',1); return; end
 
    meas= pat(10).meas_pattern;
 
    if any( size(meas)~= [16 16] )
-      ok=0; warning('Fail at pt#18'); return; end
+      ok=0; eidors_msg('Fail at pt#18',1); return; end
 
-   if any( meas(1,:)~= [1,-1,zeros(1,14)] )
-      ok=0; warning('Fail at pt#19'); return; end
+   if any( meas(1,:)~= [-1,1,zeros(1,14)] )
+      ok=0; eidors_msg('Fail at pt#19',1); return; end
 
-   if any( meas(13,:)~= [zeros(1,12),1,-1,0,0] )
-      ok=0; warning('Fail at pt#20'); return; end
+   if any( meas(13,:)~= [zeros(1,12),-1,1,0,0] )
+      ok=0; eidors_msg('Fail at pt#20',1); return; end
 
 
 function ok= test_adj_rotate(pat)
@@ -124,78 +124,78 @@ function ok= test_adj_rotate(pat)
 
    ok=1;
    if length(pat) ~= 16
-      ok=0; warning('Fail at pt#21'); return; end
+      ok=0; eidors_msg('Fail at pt#21',1); return; end
 
    if ~strcmp( pat(1).stimulation, 'mA');
-      ok=0; warning('Fail at pt#22'); return; end
+      ok=0; eidors_msg('Fail at pt#22',1); return; end
 
    % Stim pattern # 1
    if pat(1).stim_pattern ~= [-1;1;zeros(14,1)]
-      ok=0; warning('Fail at pt#23'); return; end
+      ok=0; eidors_msg('Fail at pt#23',1); return; end
 
    meas= pat(1).meas_pattern;
 
    if any( size(meas)~= [13 16] )
-      ok=0; warning('Fail at pt#24'); return; end
+      ok=0; eidors_msg('Fail at pt#24',1); return; end
 
-   if any( meas(1,:)~= [0,0,1,-1,zeros(1,12)] )
-      ok=0; warning('Fail at pt#25'); return; end
+   if any( meas(1,:)~= [0,0,-1,1,zeros(1,12)] )
+      ok=0; eidors_msg('Fail at pt#25',1); return; end
 
-   if any( meas(13,:)~= [zeros(1,14),1,-1] )
-      ok=0; warning('Fail at pt#26'); return; end
+   if any( meas(13,:)~= [zeros(1,14),-1,1] )
+      ok=0; eidors_msg('Fail at pt#26',1); return; end
 
    % Stim pattern # 10
    if any( pat(10).stim_pattern ~= [zeros(9,1);-1;1;zeros(5,1)] )
-      ok=0; warning('Fail at pt#27'); return; end
+      ok=0; eidors_msg('Fail at pt#27',1); return; end
 
    meas= pat(10).meas_pattern;
 
    if any( size(meas)~= [13 16] )
-      ok=0; warning('Fail at pt#28'); return; end
+      ok=0; eidors_msg('Fail at pt#28',1); return; end
 
-   if any( meas(1,:)~= [zeros(1,11),1,-1,zeros(1,3)] )
-      ok=0; warning('Fail at pt#29'); return; end
+   if any( meas(1,:)~= [zeros(1,11),-1,1,zeros(1,3)] )
+      ok=0; eidors_msg('Fail at pt#29',1); return; end
 
-   if any( meas(13,:)~= [zeros(1,7),1,-1,zeros(1,7)] )
-      ok=0; warning('Fail at pt#30'); return; end
+   if any( meas(13,:)~= [zeros(1,7),-1,1,zeros(1,7)] )
+      ok=0; eidors_msg('Fail at pt#30',1); return; end
 
 function ok= test_adj_no_redund(pat)
    eidors_msg('test adjacent current pattern (rotate)',2);
 
    ok=1;
    if length(pat) ~= 14
-      ok=0; warning('Fail at pt#31'); return; end
+      ok=0; eidors_msg('Fail at pt#31',1); return; end
 
    if ~strcmp( pat(1).stimulation, 'mA');
-      ok=0; warning('Fail at pt#32'); return; end
+      ok=0; eidors_msg('Fail at pt#32',1); return; end
 
    % Stim pattern # 1
    if pat(1).stim_pattern ~= [-1;1;zeros(14,1)]
-      ok=0; warning('Fail at pt#33'); return; end
+      ok=0; eidors_msg('Fail at pt#33',1); return; end
 
    meas= pat(1).meas_pattern;
 
    if any( size(meas)~= [13 16] )
-      ok=0; warning('Fail at pt#34'); return; end
+      ok=0; eidors_msg('Fail at pt#34',1); return; end
 
-   if any( meas(1,:)~= [0,0,1,-1,zeros(1,12)] )
-      ok=0; warning('Fail at pt#35'); return; end
+   if any( meas(1,:)~= [0,0,-1,1,zeros(1,12)] )
+      ok=0; eidors_msg('Fail at pt#35',1); return; end
 
-   if any( meas(13,:)~= [zeros(1,14),1,-1] )
-      ok=0; warning('Fail at pt#36'); return; end
+   if any( meas(13,:)~= [zeros(1,14),-1,1] )
+      ok=0; eidors_msg('Fail at pt#36',1); return; end
 
    % Stim pattern # 10
    if any( pat(10).stim_pattern ~= [zeros(9,1);-1;1;zeros(5,1)] )
-      ok=0; warning('Fail at pt#37'); return; end
+      ok=0; eidors_msg('Fail at pt#37',1); return; end
 
    meas= pat(10).meas_pattern;
 
    if any( size(meas)~= [5 16] )
-      ok=0; warning('Fail at pt#38'); return; end
+      ok=0; eidors_msg('Fail at pt#38',1); return; end
 
-   if any( meas(1,:)~= [zeros(1,11),1,-1,zeros(1,3)] )
-      ok=0; warning('Fail at pt#39'); return; end
+   if any( meas(1,:)~= [zeros(1,11),-1,1,zeros(1,3)] )
+      ok=0; eidors_msg('Fail at pt#39',1); return; end
 
-   if any( meas(5,:)~= [-1,zeros(1,14),1] )
-      ok=0; warning('Fail at pt#40'); return; end
+   if any( meas(5,:)~= [1,zeros(1,14),-1] )
+      ok=0; eidors_msg('Fail at pt#40',1); return; end
 
