12813f770f0fca69ab57095a892b5b6f6135705e
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2007-08-30 03:55:35 +0000

    EIDORS workshop - 23-29 Aug 2007 - Revision 135

---
+++ trunk/htdocs/tutorial/adv_image_reconst/move_2d02.m
@@ -0,0 +1,37 @@
+% Generate eidors planar finite element model
+mdl2dim = mk_common_model('b2c');
+% Set eidors_obj hyperparameter member.
+mdl2dim.hyperparameter.value= 0.01;
+
+clim= .088;
+
+% Solve inverse problem for mdl2dim eidors_obj model.
+img2dim = inv_solve(mdl2dim, vh, vi);
+img2dim.calc_colours.clim= clim;
+
+% Plot results for each algorithm
+subplot(1,2,1);
+show_fem_move(img2dim);
+calc_colours(img2dim,[],1); % do colourbar
+
+% Set eidors_obj hyperparameter member.
+mdlM = mdl2dim;
+% Place traditional jacobian in temporary member.
+mdlM.fwd_model.conductivity_jacobian = mdlM.fwd_model.jacobian;
+% Redefine jacobian member for movement & conductivity.
+mdlM.fwd_model.jacobian = 'aa_e_move_jacobian';
+mdlM.RtR_prior =     'aa_e_move_image_prior';
+mdlM.aa_e_move_image_prior.parameters = sqrt(1e2/1); 
+
+% Solve inverse problem for mdlM eidors_obj model.
+imgM = inv_solve(mdlM, vh, vi);
+imgM.calc_colours.clim= clim;
+
+% Plot results for each algorithm
+subplot(1,2,2);
+show_fem_move(imgM);
+
+calc_colours(imgM,[],1); % do colourbar
+
+set(gcf,'paperposition',[.25 2.5 8 4]);
+print -r125 -dpng move_2d02.png
---
+++ trunk/htdocs/tutorial/adv_image_reconst/temporal_solver04.m
@@ -0,0 +1,28 @@
+% Image reconstruction of moving objects $Id: temporal_solver04.m,v 1.1 2007-08-30 03:55:35 aadler Exp $
+
+time_steps=  3; ts_expand= 5;
+time_weight= .8;
+ts_vec= -time_steps:time_steps;
+
+image_select= .25*length(xyr_pt)+1:ts_expand:.75*length(xyr_pt)+1;
+
+% GN Solver
+ vi_sel = vi(:,image_select);
+ img= inv_solve( imdl_GN, vh, vi_sel);
+ animate_reconstructions('temporal_solver04a', img);
+
+% Temporal Solver
+ k=1;
+ for i= image_select
+   vi_sel= vi(:,i+ts_vec);
+   imgs= inv_solve( imdl_TS, vh, vi_sel);
+   img.elem_data(:,k)= imgs.elem_data(:,1+time_steps);
+   k=k+1;
+ end
+ animate_reconstructions('temporal_solver04c', img);
+
+
+% Kalman Solver
+ vi_sel = vi(:,image_select);
+ img= inv_solve( imdl_KS, vh, vi_sel);
+ animate_reconstructions('temporal_solver04d', img);
