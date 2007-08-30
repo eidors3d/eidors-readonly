7775748c1a19406cc63ecf5884ff475bf1e2746f
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2007-08-30 04:45:16 +0000

    define system matrix
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@974 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/data_structures/tutorial022a.m
+++ htdocs/tutorial/data_structures/tutorial022a.m
@@ -1,4 +1,4 @@
-% Create 2D model $Id: tutorial022a.m,v 1.1 2007-06-15 18:24:37 aadler Exp $
+% Create 2D model $Id: tutorial022a.m,v 1.2 2007-08-30 04:45:16 aadler Exp $
 
 nn= 84;     % number of nodes
 ww=4;       % width = 4
@@ -9,11 +9,12 @@ mdl.elems = delaunayn(mdl.nodes);
 mdl.gnd_node = 1;
 elec(1).nodes= [1:ww];      elec(1).z_contact= 0;
 elec(2).nodes= nn-[0:ww-1]; elec(2).z_contact= 0;
-stim.stim_pattern= [-1,1];
+stim.stim_pattern= [-1;1];
 stim.meas_pattern= [-1,1];
 mdl.stimulation= stim;
 mdl.electrode= elec;
 mdl.solve = @aa_fwd_solve;
+mdl.system_mat = @aa_calc_system_mat;
 
 show_fem(mdl); axis('equal'); set(gca,'Ylim',[-.5,ww-.5]);
 print -r100 -dpng tutorial022a.png
