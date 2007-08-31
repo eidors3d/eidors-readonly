4e3e273c1dd37bfb6725f925d49421cf3c8d0a0a
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2007-08-31 04:30:02 +0000

    update images and tutorials for 3.2
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@976 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/lung_EIT/tutorial310c.m
+++ htdocs/tutorial/lung_EIT/tutorial310c.m
@@ -1,8 +1,8 @@
 % Lung images
-% $Id: tutorial310c.m,v 1.1 2007-06-15 18:10:20 aadler Exp $
+% $Id: tutorial310c.m,v 1.2 2007-08-31 04:30:02 aadler Exp $
 
 load montreal_data_1995
-imdl_d.hyperparameter.value=2;
+imdl_d.hyperparameter.value=5e-2;
 img= inv_solve(imdl_d, zc_resp(:,22), zc_resp);
 
 clf; show_slices(img)
