7b0b7d59c89747bfa22e4eab9326ef7eeb11fa75
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2007-09-20 21:08:01 +0000

    build single plane
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1005 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/netgen/build_single_plane01.m
+++ htdocs/tutorial/netgen/build_single_plane01.m
@@ -1,5 +1,5 @@
 % Build model
-% $Id: build_single_plane01.m,v 1.1 2007-09-20 20:30:33 aadler Exp $
+% $Id: build_single_plane01.m,v 1.2 2007-09-20 21:08:01 aadler Exp $
 
 % Choose Parameters
 electrodes_per_plane= 16;
@@ -28,24 +28,46 @@ height_between_centres = first_plane_starts;
    fname, refine_electrodes  );
 
 % The msz file was created here can be reused later
-msz_file= 'tank_mdl.msz';
-
-% control mesh refinement: options are '-veryfine'; '-fine'; '';
-finelevel= '';
-if ~isempty(finelevel);
-   call_netgen([fname,'.geo'],[fname,'.vol'],msz_file, finelevel);
-end
-
-% Create Homog
+msz_file= [fname, '.msz'];
 stim_pat= mk_stim_patterns(electrodes_per_plane, number_of_planes, ...
               '{ad}','{ad}',{'meas_current'});
-[fmdl,mat_idxs]= ng_mk_fwd_model( ...
-  [fname,'.vol'], centres, [], stim_pat);
-n_elem= size(fmdl.elems,1);
-elem_data= ones(n_elem,1);
-img=eidors_obj('image','netgen_problem', ...
-               'fwd_model',fmdl, 'elem_data', elem_data);
-vh= fwd_solve( img);
 
-show_fem( fmdl);
-print -r100 -dpng build_single_plane01a.png;
+% control mesh refinement: options are '-veryfine'; '-fine'; '';
+for finemodels= 0:2
+  if     finemodels==0; finelevel= '';
+  elseif finemodels==1; finelevel= '-fine';
+  elseif finemodels==2; finelevel= '-veryfine';
+  end
+
+  if ~isempty(finelevel);
+      call_netgen([fname,'.geo'],[fname,'.vol'],msz_file, finelevel);
+  end
+
+  [fmdl,mat_idxs]= ng_mk_fwd_model( [fname,'.vol'], centres, [], stim_pat);
+
+  % Save some space
+  fmdl.elems=    uint16(fmdl.elems);
+  fmdl.boundary= uint16(fmdl.boundary);
+
+  if     finemodels==0; mdl_coarse= fmdl;
+  elseif finemodels==1; mdl_fine  = fmdl;
+  elseif finemodels==2; mdl_vfine = fmdl;
+  end
+
+  subplot(311);
+  show_fem( fmdl); view(0,14);
+
+  subplot(312);
+  show_fem( fmdl); view(0,0);
+  crop_model(gca, inline('y>0','x','y','z'))
+
+  subplot(313);
+  show_fem( fmdl); view(0,0);
+  crop_model(gca, inline('y>-10','x','y','z'))
+  set(gca,'Xlim',[-2,2],'Zlim',[-1,1]+first_plane_starts);
+
+  print('-r100','-dpng', ...
+        sprintf('build_single_plane01%c.png',96+finemodels));
+end
+   
+save single_plane_models mdl_coarse mdl_fine mdl_vfine
