99fd659a4a7cac4975b51a0b5aef3974fe35c06a
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2007-09-21 19:48:00 +0000

    model the mesh

--- trunk/eidors3d/algorithms/a_adler/mk_mesh_sample_array.m
+++ trunk/eidors3d/algorithms/a_adler/mk_mesh_sample_array.m
@@ -15,19 +15,53 @@ function [eptr, pt_coord]= mk_mesh_sample_array( varargin )
 % npoints - total number of points for rectangular mapping
 
 % (C) 2007 Andy Adler. License: GPL version 2 or version 3
-% $Id: mk_mesh_sample_array.m,v 1.2 2007-09-21 14:18:36 aadler Exp $
+% $Id: mk_mesh_sample_array.m,v 1.3 2007-09-21 19:48:00 aadler Exp $
 
-[params, mdls] = proc_input (varargin);
-
+[pp, mdls] = proc_input (varargin);
+xyz = interpxyz(pp.min_xyz , pp.max_xyz, pp.npoints);
+tri_pts = mk_tri_pts( mdls{1}, xyz);
 keyboard
 
+function xyz = interpxyz( xyzmin, xyzmax, n_interp);
+    xyz_delta= xyzmax - xyzmin;
+    % Allocate points to each dir nx,ny,nz so nx*ny*nz~=n_interp,
+    % and (nx,ny,nz) = k*xyzdelta
+    neq0 = xyz_delta~=0;
+    k= (prod(xyz_delta(neq0))/n_interp)^(1/sum(neq0));
+    n_xyz= [1,1,1]; n_xyz(neq0)= ceil(xyz_delta(neq0)/k);
+
+    xspace = linspace(xyzmin(1), xyzmax(1), n_xyz(1) );
+    yspace = linspace(xyzmin(2), xyzmax(2), n_xyz(2) );
+    zspace = linspace(xyzmin(3), xyzmax(3), n_xyz(3) );
+    [xx3,yy3,zz3] = ndgrid( xspace, yspace, zspace );
+    xyz= [xx3(:), yy3(:), zz3(:)];
+
+% calculate mapping of points xyz into triangle
+% fmdl is fwd_mode, xyz is points [x(:), y(:), z(:)]
+function tri_pts = mk_tri_pts( fmdl, xyz);
+    to3d = speye(size(fmdl.nodes,2), 3);
+    nodes = fmdl.nodes*to3d;
+    elems = fmdl.elems;
+    tri_pts= tsearchn(nodes, elems, xyz*to3d');
+
+    ff= find(~isnan(tri_pts));
+    n_ff= size(elems, 1);
+    csum = sparse(tri_pts(ff),1,1,n_ff, 1);
+    if any(csum<10)
+       warning(sprintf( ...
+         'mesh density is too low: min=%d', full(min(csum)) ))
+    end
+
 function [p, mdls] = proc_input (varargin);
-   if isnumeric( varargin{1}{end} )
-      p.npoints = varargin{1}{end};
-      mdls = varargin{1}(1:end-1);
+   % BLOODY STUPID IDIOTS AT MATLAB CANT KEEP COMPATIBLE
+   varargin = varargin{1};
+
+   if isnumeric( varargin{end} )
+      p.npoints = varargin{end};
+      mdls = varargin(1:end-1);
    else
       p.npoints = [];
-      mdls = varargin{1}{:};
+      mdls = varargin(:);
    end
 
    p.dims= 1;
@@ -56,6 +90,6 @@ function [p, mdls] = proc_input (varargin);
       p.min_xyz(idx) = min_n(idx);
    end
 
-   if ~isempty(p.npoints)
+   if isempty(p.npoints)
       p.npoints = 64^p.dims;
    end
