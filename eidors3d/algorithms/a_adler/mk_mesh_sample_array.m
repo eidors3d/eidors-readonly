81d85a4ff7ec367020a638642ac07eacce7e44a1
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2007-09-21 20:05:22 +0000

    looks ok
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1023 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors3d/algorithms/a_adler/mk_mesh_sample_array.m
+++ eidors3d/algorithms/a_adler/mk_mesh_sample_array.m
@@ -1,26 +1,77 @@
-function [eptr, pt_coord]= mk_mesh_sample_array( varargin )
+function [eptr, pt_elem]= mk_mesh_sample_array( varargin )
 % MK_MESH_SAMPLE_ARRAY - create rectangular coordinate map into mesh
-% [eptr, pt_coord]= mk_mesh_sample_array( mdl1, mdl2, ... , npoints);
+% [eptr, pt_elem]= mk_mesh_sample_array( mdl1, mdl2, ... , npoints);
 %
-% pt_coord - n_points x dims matrix of sample points
+% pt_elem - n_points x dims matrix of sample points
 % eptr -     element number containing the pt_coord
 % mdl1, mdl2 - fwd_model objects
 %
 % for example: [ep, pt] = mk_coarse_fine_mapping( m1, m2 )
 % ep= [x   y   z]  pt= [m1, m2 ]
-%      1   1   1         1  2 % pt in el#1 in m1, el#2 in m2
-%      1   1   2         2  2 % pt in el#2 in m1, el#2 in m2
-%      1   2   1         2  0 % pt in el#2 in m1, pt not in m2
+%      1   1   1        1   2  % pt in el#1 in m1, el#2 in m2
+%      1   1   2        2   2  % pt in el#2 in m1, el#2 in m2
+%      1   2   1        2  NaN % pt in el#2 in m1, pt not in m2
 %
 % npoints - total number of points for rectangular mapping
 
 % (C) 2007 Andy Adler. License: GPL version 2 or version 3
-% $Id: mk_mesh_sample_array.m,v 1.3 2007-09-21 19:48:00 aadler Exp $
+% $Id: mk_mesh_sample_array.m,v 1.4 2007-09-21 20:05:22 aadler Exp $
 
 [pp, mdls] = proc_input (varargin);
 xyz = interpxyz(pp.min_xyz , pp.max_xyz, pp.npoints);
-tri_pts = mk_tri_pts( mdls{1}, xyz);
-keyboard
+for mdl= mdls{:}'
+   xyz= [xyz; interpmdl( mdl )];
+end
+
+
+pt_elem= [];
+for mdl= mdls{:}'
+   tri_pts = mk_tri_pts( mdl, xyz);
+   pt_elem = [pt_elem, tri_pts];
+end
+
+% Add four interpolation points in each elem
+% 1- n1+n2+n3/3 = mid
+% 2- n1+mid/2 = n1/2 + (n1+n2+n3)/6 = (4*n1 + n2 + n3)/6;
+function xyz = interpmdl( fmdl );
+    to3d = speye(size(fmdl.nodes,2), 3);
+    nodes = fmdl.nodes*to3d;
+    elems = fmdl.elems;
+
+    xelems= reshape( nodes(elems,1), size(elems) );
+    yelems= reshape( nodes(elems,2), size(elems) );
+    zelems= reshape( nodes(elems,3), size(elems) );
+
+    if size(elems,2)==4 % pyramids
+       iv= [2;2;2;2]/8;
+       xyz=[      [xelems*iv, yelems*iv, zelems*iv]];
+
+       iv= [5;1;1;1]/8;
+       xyz=[xyz;  [xelems*iv, yelems*iv, zelems*iv]];
+
+       iv= [1;5;1;1]/8;
+       xyz=[xyz;  [xelems*iv, yelems*iv, zelems*iv]];
+
+       iv= [1;1;5;1]/8;
+       xyz=[xyz;  [xelems*iv, yelems*iv, zelems*iv]];
+
+       iv= [1;1;1;5]/8;
+       xyz=[xyz;  [xelems*iv, yelems*iv, zelems*iv]];
+    elseif size(elems,2)==3 % triangles
+       iv= [2;2;2]/6;
+       xyz=[      [xelems*iv, yelems*iv, zelems*iv]];
+
+       iv= [4;1;1]/6;
+       xyz=[xyz;  [xelems*iv, yelems*iv, zelems*iv]];
+
+       iv= [1;4;1]/6;
+       xyz=[xyz;  [xelems*iv, yelems*iv, zelems*iv]];
+
+       iv= [1;1;4]/6;
+       xyz=[xyz;  [xelems*iv, yelems*iv, zelems*iv]];
+    else
+       error('strange size elems'); 
+    end
 
 function xyz = interpxyz( xyzmin, xyzmax, n_interp);
     xyz_delta= xyzmax - xyzmin;
@@ -47,7 +98,7 @@ function tri_pts = mk_tri_pts( fmdl, xyz);
     ff= find(~isnan(tri_pts));
     n_ff= size(elems, 1);
     csum = sparse(tri_pts(ff),1,1,n_ff, 1);
-    if any(csum<10)
+    if any(csum<4)
        warning(sprintf( ...
          'mesh density is too low: min=%d', full(min(csum)) ))
     end
