1d71991ddf878fe38a960280fd16460b37028ac4
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2007-09-24 14:55:53 +0000

    remove unnecessary
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1029 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/netgen/build_single_plane02.m
+++ htdocs/tutorial/netgen/build_single_plane02.m
@@ -1,9 +1,8 @@
 % Test area of electrodes
-% $Id: build_single_plane02.m,v 1.3 2007-09-24 14:29:56 aadler Exp $
+% $Id: build_single_plane02.m,v 1.4 2007-09-24 14:55:53 aadler Exp $
 
 % verify np_fwd_parameters identifies
 for i= 1:electrodes_per_plane*number_of_planes
-   elec_areas= [];
       fprintf('\nelec#%02d (area,nodes):',i);
    for fmdl= { ng_mdl_16x1_coarse, ng_mdl_16x1_fine, ng_mdl_16x1_vfine }
       pp= np_fwd_parameters( fmdl{1} );
@@ -24,4 +23,3 @@ for i= 1:electrodes_per_plane*number_of_planes
 
    end
 end
-
