3f8f5c64bd282d700d5ac4c7a23aeff8833fbe98
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2007-09-24 14:29:56 +0000

    test mesh

--- trunk/htdocs/tutorial/netgen/build_single_plane02.m
+++ trunk/htdocs/tutorial/netgen/build_single_plane02.m
@@ -1,27 +1,27 @@
 % Test area of electrodes
-% $Id: build_single_plane02.m,v 1.2 2007-09-20 21:08:01 aadler Exp $
+% $Id: build_single_plane02.m,v 1.3 2007-09-24 14:29:56 aadler Exp $
 
 % verify np_fwd_parameters identifies
-pp= np_fwd_parameters( fmdl);
 for i= 1:electrodes_per_plane*number_of_planes
-   ee= reshape( pp.elec(i,:), 3, []);
-   ee(:, all(ee==0,1)) = [];
+   elec_areas= [];
+      fprintf('\nelec#%02d (area,nodes):',i);
+   for fmdl= { ng_mdl_16x1_coarse, ng_mdl_16x1_fine, ng_mdl_16x1_vfine }
+      pp= np_fwd_parameters( fmdl{1} );
+      ee= reshape( pp.elec(i,:), 3, []);
+      ee(:, all(ee==0,1)) = [];
 
-   Xs = reshape(fmdl.nodes(ee,1),3,[]);
-   Ys = reshape(fmdl.nodes(ee,2),3,[]);
-   Zs = reshape(fmdl.nodes(ee,3),3,[]);
-   h=patch(Xs,Ys,Zs, 'b');
-   set(h, 'FaceLighting','none', 'CDataMapping', 'direct' );
+   %  Xs = reshape(pp.vtx(ee,1),3,[]);
+   %  Ys = reshape(pp.vtx(ee,2),3,[]);
+   %  Zs = reshape(pp.vtx(ee,3),3,[]);
+   %  h=patch(Xs,Ys,Zs, 'b');
+   %  set(h, 'FaceLighting','none', 'CDataMapping', 'direct' );
 
-   elec_area= 0;
-   for j= 1: size(ee,2)
-      elec_area = elec_area + triarea3d( fmdl.nodes( ee(:,j),:) );
+      elec_area= 0; elec_nodes= size(ee,2);
+      for j= 1:elec_nodes
+         elec_area = elec_area + triarea3d( pp.vtx( ee(:,j),:) );
+      end
+      fprintf('[ %6.5f (%3d) ]',elec_area, elec_nodes);
+
    end
-
-   fprintf('elec#%d: area=%f\n',i,elec_area);
 end
-
-view(0,14);
-show_fem( fmdl);
-print -r100 -dpng build_single_plane01a.png;
 
