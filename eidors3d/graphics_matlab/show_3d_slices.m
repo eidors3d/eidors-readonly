ffb9385b0ad461dc9dc504095bd76b0913fbada9
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2007-09-28 02:02:30 +0000

    close to fixed - 3D plot shows
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1046 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors3d/graphics_matlab/show_3d_slices.m
+++ eidors3d/graphics_matlab/show_3d_slices.m
@@ -7,18 +7,27 @@ function show_3d_slices(img, varargin);
 % Default show 2 z_cuts and 1 x and 1 y cut
 
 % (C) 2007 Andy Adler. License: GPL version 2 or version 3
-% $Id: show_3d_slices.m,v 1.3 2007-09-28 01:29:31 aadler Exp $
+% $Id: show_3d_slices.m,v 1.4 2007-09-28 02:02:30 aadler Exp $
 
 cla;
 hold on
 
 [xyz_max, xyz_min, rimg, cimg, ...
-          x_cuts, y_cuts, z_cuts] = get_slices(img, varargin);
+          x_cuts, y_cuts, z_cuts] = get_slices(img, varargin{:});
 
 for zi= 1:length(z_cuts)
    M_trans= [1,0;0,1;0,0];
    M_add= [0,0,z_cuts(zi)];
-   surf_slice(rimg(:,:,zi), cimg(:,:,zi), xyz_min, xyz_max, ...
+   idx= zi;
+   surf_slice(rimg(:,:,idx), cimg(:,:,idx), xyz_min, xyz_max, ...
+              M_trans, M_add, 1);
+end
+
+for xi= 1:length(x_cuts)
+   M_trans= [0,0;1,0;0,1];
+   M_add= [x_cuts(xi),0,0];
+   idx= zi+xi;
+   surf_slice(rimg(:,:,idx), cimg(:,:,idx), xyz_min, xyz_max, ...
               M_trans, M_add, 1);
 end
 
@@ -30,21 +39,21 @@ function [xyz_max, xyz_min, rimg, cimg, ...
    xyz_min= min(img.fwd_model.nodes);
    if nargin==1;
       % Default show 2 z_cuts and 1 x and 1 y cut
-       x_cuts= linspace(xyz_min(3), xyz_max(3), 3); x_cuts([1,3])=[];
-       y_cuts= linspace(xyz_min(3), xyz_max(3), 3); y_cuts([1,4])=[];
+       x_cuts= linspace(xyz_min(1), xyz_max(1), 3); x_cuts([1,3])=[];
+       y_cuts= linspace(xyz_min(2), xyz_max(2), 3); y_cuts([1,3])=[];
        z_cuts= linspace(xyz_min(3), xyz_max(3), 4); z_cuts([1,4])=[];
    elseif nargin==2;
-       z_cuts= varargin{1}{1};
+       z_cuts= varargin{1};
        x_cuts= [];
        y_cuts= [];
    elseif nargin==3;
-       z_cuts= varargin{1}{1};
-       y_cuts= varargin{2}{1};
-       z_cuts= [];
-   elseif nargin==3;
-       z_cuts= varargin{1}{1};
-       y_cuts= varargin{2}{1};
-       z_cuts= varargin{3}{1};
+       z_cuts= varargin{1};
+       x_cuts= varargin{2};
+       y_cuts= [];
+   elseif nargin==4;
+       z_cuts= varargin{1};
+       x_cuts= varargin{2};
+       y_cuts= varargin{3};
    else 
        error('too many inputs');
    end
@@ -55,14 +64,18 @@ function [xyz_max, xyz_min, rimg, cimg, ...
 
    clim = [];
    ref_lev= 'use_global';
-   rimg= calc_slices( img, limts);
+   np= calc_colours('npoints');
+   rimg= NaN*ones(np+2,np+2,size(limts,1));
+   rimg(2:end-1,2:end-1,:)= calc_slices( img, limts);
    cimg = calc_colours( rimg, clim, 0, ref_lev );
 
 function surf_slice(rimg, cimg, xyz_min, xyz_max, M_trans, M_add, show_surf);
-   np= calc_colours('npoints');
+   np= calc_colours('npoints')+2;
 
-   [x,y]=meshgrid( linspace(xyz_min(1),xyz_max(1), np), ...
-                   linspace(xyz_min(2),xyz_max(2), np) );
+   lim_min= xyz_min*M_trans;
+   lim_max= xyz_max*M_trans;
+   [x,y]=meshgrid( linspace(lim_min(1),lim_max(1), np), ...
+                   linspace(lim_min(2),lim_max(2), np) );
 
    xyz= reshape([x(:),y(:)]*M_trans', np,np,3);
 
@@ -73,7 +86,7 @@ function surf_slice(rimg, cimg, xyz_min, xyz_max, M_trans, M_add, show_surf);
    if show_surf
       hh=surf(xyz(:,:,1)+M_add(1), ...
               xyz(:,:,2)+M_add(2), ...
-              xyz(:,:,3)+M_add(3), cimg);
+              xyz(:,:,3)+M_add(3), flipud(cimg));
       % WHY WOULD IT BE ANYTHING ELSE  - STUPID MATLAB !!!
       set(hh,'CDataMapping','direct', ...
              'EdgeAlpha',0);
