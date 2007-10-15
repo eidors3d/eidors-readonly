700aa0fd3e0f43e918ba82a909bdc6d4d2c20098
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2007-10-15 20:52:17 +0000

    solve with backproj

---
+++ trunk/htdocs/tutorial/EIDORS_basics/backproj_solve03.m
@@ -0,0 +1,23 @@
+% $Id: backproj_solve03.m,v 1.1 2007-10-15 20:52:17 aadler Exp $
+
+% Gauss Newton Solver
+inv_GN= eidors_obj('inv_model','GN_solver','fwd_model', img.fwd_model);
+inv_GN.reconst_type= 'difference';
+inv_GN.solve= @np_inv_solve;
+inv_GN.RtR_prior= @noser_image_prior;
+inv_GN.jacobian_bkgnd.value= 1;
+inv_GN.hyperparameter.value= 1e-3;
+
+imgr= inv_solve(inv_GN, vh,vi);
+subplot(121)
+show_fem(imgr);
+axis equal; axis off
+
+% Backprojection Solver
+inv_BP= eidors_obj('inv_model','BP_solver','fwd_model', img.fwd_model);
+inv_BP.reconst_type= 'difference';
+inv_BP.solve= @backproj_solve;
+inv_BP.backproj_solve.type= 'naive';
+imgr= inv_solve(inv_BP, vh,vi);
+
+
