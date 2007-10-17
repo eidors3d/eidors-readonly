bf74fbf947cdb01ddfe781cc181736855b03f3f1
daitao <daitao@82006732-fb7e-4a22-9f28-86a212cce563>
2007-10-17 17:25:09 +0000

    add some commends
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1071 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors3d/algorithms/t_dai/calc_covar_prior.m
+++ eidors3d/algorithms/t_dai/calc_covar_prior.m
@@ -2,7 +2,7 @@ function Reg= calc_covar_prior( inv_model )
 % CALC_COVAR_PRIOR image prior with distance-based interelement covar
 % This is a simplification of exponential_covar_prior.m
 % Reg= calc_covar_prior( inv_model )
-% Reg        => output regularization term
+% Reg        => output regularization 
 % inv_model  => inverse model struct
 % P_type--prior type
 % 1: elements are globally correlated
@@ -10,35 +10,34 @@ function Reg= calc_covar_prior( inv_model )
 % 3: only elements within electrode rings are correlated.
 
 % (C) 2007, Tao Dai and Andy Adler. Licenced under the GPL Version 2
-% $Id: calc_covar_prior.m,v 1.1 2007-10-17 00:37:31 daitao Exp $
+% $Id: calc_covar_prior.m,v 1.2 2007-10-17 17:25:09 daitao Exp $
 
 % get average x,y,z of each element
-ff= inv_model.fwd_model;
-nel= size(ff.elems,1);
+ff = inv_model.fwd_model;
+nel = size(ff.elems,1);
 eta = .1;%attenuation factor. eta is large when elems're spatially highly correlated
 
-dist= zeros(nel);
+dist = zeros(nel);
 
 z1 = ff.nodes(ff.electrode(1).nodes,3);%upper electrode ring
 z2 = ff.nodes(ff.electrode(end).nodes,3);%lower electrode ring
-%    ff.nodes(:,3)>z1||ff.nodes(:,3)<z2
 
-for dim= 1: size(ff.nodes,2);
-    coords= reshape(ff.nodes(ff.elems,dim),[size(ff.elems)]);%coord of four vertex of each elem
-    m_coords= mean( coords,2);%calc center coord
+for dim = 1: size(ff.nodes,2);
+    coords = reshape(ff.nodes(ff.elems,dim),[size(ff.elems)]);%coord of four vertex of each elem
+    m_coords = mean( coords,2);%calc center coord
     if dim == 3
-        temp = double((m_coords<max(z1,z2))&(m_coords>min(z1,z2)));
-        H = temp*temp';%(temp1*temp1')+(temp2*temp2');
-        switch inv_model.fourD_prior.P_type
-            case 1
+        temp = double((m_coords<max(z1,z2))&(m_coords>min(z1,z2)));%regions of interests
+        H = temp*temp';
+        switch inv_model.fourD_prior.P_type %Prior type
+            case 1 % all elements are correlated
                 H = eta*(ones(size(H)));
-            case 2
+            case 2 % elements of ROI are not correlated with elements of non_ROI
                 temp = double(m_coords>max(z1,z2));
                 H = H + temp*temp';
                 temp = double(m_coords<min(z1,z2));
                 H = H + temp*temp';
                 H = eta*(H+1e-6);
-            case 3
+            case 3 % elements are only correlated within ROI, non-ROI elements are highly attenuated
                 H = eta*(H+1e-6);
             otherwise
                 error('no such a 3-D prior type');
@@ -46,10 +45,10 @@ for dim= 1: size(ff.nodes,2);
     end
     difm = m_coords*ones(1,nel);
     difm = difm - difm';
-    dist= dist + difm.^2;%L^2=dx^2+dy^2+dz^2
+    dist= dist + difm.^2;%
 end
-dist = sqrt(dist);
+dist = sqrt(dist);%elements distance matrix
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 dist = dist/max(dist(:));
 
-Reg = exp(-dist ./ H);
+Reg = exp(-dist ./ H);% 3-D elements correlations matrix.
