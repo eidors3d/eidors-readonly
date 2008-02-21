f2c1de2d1a2f3ee0bf0756e2e0222ac434883aae
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-02-21 19:23:18 +0000

    add code for 2d fields - m_2d_fields is nick's old measfields
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1091 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors3d/algorithms/n_polydorides/np_calc_jacobian.m
+++ eidors3d/algorithms/n_polydorides/np_calc_jacobian.m
@@ -6,20 +6,26 @@ function J= np_calc_jacobian( fwd_model, img)
 % img = image background for jacobian calc
 
 % (C) 2005 Andy Adler. License: GPL version 2 or version 3
-% $Id: np_calc_jacobian.m,v 1.13 2007-08-30 03:37:04 aadler Exp $
+% $Id: np_calc_jacobian.m,v 1.14 2008-02-21 19:23:18 aadler Exp $
 
 p= np_fwd_parameters( fwd_model );
 
 s_mat= calc_system_mat( fwd_model, img );
 
-v_f = np_calc_3d_fields( fwd_model, img );
+dims= size(fwd_model.nodes,2)
+if dims==2
+elseif dims==3
+   v_f = np_calc_3d_fields( fwd_model, img );
 
-tol = 1e-5; %tolerance for the forward solver
+   tol = 1e-5; %tolerance for the forward solver
 
-% Calculating the Jacobian
-Vfwd = forward_solver(s_mat.E, p.I, tol, s_mat.perm);
-J = jacobian_3d_fields(Vfwd,s_mat.Ela,s_mat.D, p.elec, ...
-                       p.vtx,p.simp, img.elem_data, v_f, p.df);
+   % Calculating the Jacobian
+   Vfwd = forward_solver(s_mat.E, p.I, tol, s_mat.perm);
+   J = jacobian_3d_fields(Vfwd,s_mat.Ela,s_mat.D, p.elec, ...
+                          p.vtx,p.simp, img.elem_data, v_f, p.df);
+else
+   error('dims not 2 or 3');
+end
 
 % calculate normalized Jacobian if required
 if p.normalize
