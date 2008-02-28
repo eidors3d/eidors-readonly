f3217045b40e7747d1374051b634230458a46bb0
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-02-28 19:06:35 +0000

    more results
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1104 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors3d/tests/perturb_jacobian_test.m
+++ eidors3d/tests/perturb_jacobian_test.m
@@ -1,5 +1,5 @@
 % Perturbation Jacobians
-% $Id: perturb_jacobian_test.m,v 1.3 2008-02-27 02:06:39 aadler Exp $
+% $Id: perturb_jacobian_test.m,v 1.4 2008-02-28 19:06:35 aadler Exp $
 
 % imdl= mk_common_model('c2c2',16);
   imdl= mk_common_model('a3cr',16);
@@ -32,4 +32,30 @@
   norm(J_aa - J_aa_p,'fro')/norm(J_aa,'fro')
   norm(J_np - J_np_p,'fro')/norm(J_np,'fro')
   norm(J_np - J_aa_p,'fro')/norm(J_np,'fro')
+  norm(J_np - J_aa  ,'fro')/norm(J_np,'fro')
+
+% Demo model with EIDORS3D
+  imdl= mk_common_model('n3r2',16);
+  img= calc_jacobian_bkgnd(imdl);
+  for i=1:16; imdl.fwd_model.electrode(i).z_contact= .01;end
+
+% Calculate the normal Jacobian
+  img.fwd_model.jacobian=   @np_calc_jacobian;
+  img.fwd_model.system_mat= @np_calc_system_mat;
+  img.fwd_model.solve=      @np_fwd_solve;
+  J_np= calc_jacobian( img );
+
+% Calculate the perturbation Jacobian
+  vh= fwd_solve(img);
+
+  for el= 1:50:size(img.fwd_model.elems,1);
+     fprintf('\nelem#%03d: ',el);
+     for delta= [1e-4,1e-6,1e-8] % delta is perturb
+        img_i = img;
+        img_i.elem_data(el)= img_i.elem_data(el) + delta;
+        vi= fwd_solve(img_i);
+        J_p = (vi.meas - vh.meas)/delta; % perturb Jacobian
+        fprintf(' %8.6f', norm(J_p - J_np(:,el))/norm(J_np(:,el)));
+     end
+  end
 
