8878597fb1727462d64d3b5387215ed3c4d9afb2
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-03-03 15:50:24 +0000

    remove chol solver as per discussion 3 mar 2008

--- trunk/eidors3d/algorithms/n_polydorides/forward_solver.m
+++ trunk/eidors3d/algorithms/n_polydorides/forward_solver.m
@@ -16,51 +16,57 @@ function [V] = forward_solver(E,I,tol,pp,V);
 % d: number of current patterns
 [n_nodes,d] = size(I);
 
+% PCG solver is better if memory is tight. This limit, 100k nodes
+% seems to be good for machines with ~ 1GB of memory
+n_nodes_pcg = 1e5;
+
 if nargin < 6
-V = zeros(size(E,1),d);
+   V = zeros(size(E,1),d);
 end
 
 if isreal(E)==1
 
-    % USE \ operator for real case, to make it work
-    % FIXME: this needs to be tested to see if it is most
-    % efficient - AA 20 Feb 08
-    %V= E\I;
-    %return
+   if n_nodes < n_nodes_pcg
+       V= E\I;
+   else
 
-    if  pp ~= 1:size(I,1) %There is a colume permutation, hence Cholesky opted
-        %Permute the rows and columns to make the factors sparser
-        E = E(pp,pp);
-        In = I(pp,:);
-        rr(pp)=1:max(size(pp));  % this should be done only Once!
-                                 % actually much better just to do the
-                                 % renumbering when the mesh is generated!
-        %U = cholinc(E,tol/10);  % This is wrong as only incomplete Choleski.
-        U = chol(E);
-        q_c =  U' \ In;  
-        Vn = U \ q_c;
-        %De-permute the result for Cholesky
-        V = Vn(rr,:);
-    else
+       K = cholinc(E,tol*100);
+       %flags needed for quiet output
+       for i=1:d
+           [V(:,i),flag] = pcg(E,I(:,i),tol*norm(I(:,i)),n_nodes,K',K,V(:,i));
+       end
 
-        %Alternatively use pcg ********
-        K = cholinc(E,tol*100);
-        %flags needed for quiet output
-        for i=1:d
-            [V(:,i),flag] = pcg(E,I(:,i),tol*norm(I(:,i)),n_nodes,K',K,V(:,i));
-        end
+   end
 
-    end
+   % Cholesky solver. Gives poor results matching others
+   % so we no longer use it
+   if 0 
+       %Permute the rows and columns to make the factors sparser
+       E = E(pp,pp);
+       In = I(pp,:);
+       rr(pp)=1:max(size(pp));  % this should be done only Once!
+                                % actually much better just to do the
+                                % renumbering when the mesh is generated!
+       U = chol(E);
+       q_c =  U' \ In;  
+       Vn = U \ q_c;
+       %De-permute the result for Cholesky
+       V = Vn(rr,:);
+   end
 
 else % is complex
     
-  [L,U] = luinc(E,tol/10);
-
-  for y=1:d
-
-     V(:,y) = bicgstab(E,I(:,y),tol*norm(I(:,y)),1000,L,U);
-
-  end 
+   if n_nodes < n_nodes_pcg
+      V= E\I;
+   else
+      [L,U] = luinc(E,tol/10);
+      
+      for y=1:d
+      
+         V(:,y) = bicgstab(E,I(:,y),tol*norm(I(:,y)),1000,L,U);
+      
+      end 
+   end 
    
 end %is complex
 
