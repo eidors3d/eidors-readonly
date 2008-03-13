3688f5a148af70eaa469a102633ea7c9e36bb76a
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-03-13 22:08:30 +0000

    Many reconstructions
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1148 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/lung_EIT/tutorial410c.m
+++ htdocs/tutorial/lung_EIT/tutorial410c.m
@@ -1,5 +1,5 @@
 % Lung images
-% $Id: tutorial410c.m,v 1.1 2008-03-13 21:34:56 aadler Exp $
+% $Id: tutorial410c.m,v 1.2 2008-03-13 22:08:30 aadler Exp $
 
 raster_img= calc_slices(img);
 raster_img(isnan(raster_img))=0;
@@ -16,6 +16,4 @@ plot( ((1:s_ri(3))-1)*5, sig/sig(1))
 xlabel('minutes after drink')
 ylabel('normalized conductivity');
 
-
 print -r125 -dpng tutorial410c.png;
-
---
+++ htdocs/tutorial/lung_EIT/tutorial410d.m
@@ -0,0 +1,42 @@
+% Lung images
+% $Id: tutorial410d.m,v 1.1 2008-03-13 22:08:30 aadler Exp $
+
+load montreal_data_1995
+vh= zc_h_stomach_pre; % abdomen before fluid
+vi= zc_stomach_0_5_60min(:,1); % right after drink
+
+% GN solution - Gaussian prior
+imdl.RtR_prior= @gaussian_HPF_prior;
+imdl.solve=     @aa_inv_solve;
+imdl.hyperparameter.value=1e-2;
+img= inv_solve(imdl, vh, vi);
+
+subplot(221); show_fem(img); axis equal; axis off
+
+% GN solution - Noser prior
+imdl.RtR_prior= @noser_image_prior;
+imdl.solve=     @aa_inv_solve;
+imdl.hyperparameter.value=8e-2;
+img= inv_solve(imdl, vh, vi);
+
+subplot(222); show_fem(img); axis equal; axis off
+
+% GN solution - Noser prior
+imdl= rmfield(imdl,'RtR_prior');
+imdl.R_prior=   @ab_calc_tv_prior;
+imdl.solve=     @ab_tv_diff_solve;
+imdl.hyperparameter.value=1e-2;
+img= inv_solve(imdl, vh, vi);
+
+subplot(223); show_fem(img); axis equal; axis off
+
+% GN solution - Noser prior
+imdl.R_prior=   @ab_calc_tv_prior;
+imdl.solve=     @ab_tv_diff_solve;
+imdl.hyperparameter.value=1e-1;
+img= inv_solve(imdl, vh, vi);
+
+subplot(224); show_fem(img); axis equal; axis off
+
+
+print -r100 -dpng tutorial410d.png;
