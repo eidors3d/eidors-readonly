d5bf81e0adda60f26d119c85b7311ce384187023
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-03-13 21:34:56 +0000

    Stomach tutorial
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1145 82006732-fb7e-4a22-9f28-86a212cce563

---
+++ htdocs/tutorial/lung_EIT/tutorial410c.m
@@ -0,0 +1,21 @@
+% Lung images
+% $Id: tutorial410c.m,v 1.1 2008-03-13 21:34:56 aadler Exp $
+
+raster_img= calc_slices(img);
+raster_img(isnan(raster_img))=0;
+% define roi as whole image
+s_ri = size(raster_img);
+roi = ones(s_ri(1:2));
+
+for i= 1:s_ri(3)
+  sig(i)= sum(sum(raster_img(:,:,i) .* roi));
+end
+
+subplot(221)
+plot( ((1:s_ri(3))-1)*5, sig/sig(1))
+xlabel('minutes after drink')
+ylabel('normalized conductivity');
+
+
+print -r125 -dpng tutorial410c.png;
+
