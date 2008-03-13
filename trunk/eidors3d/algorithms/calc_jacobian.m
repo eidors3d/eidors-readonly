503f9a8145fb0555e9f9e4361904567b87aa4c6a
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-03-13 20:42:11 +0000

    calculate jacobian is now done on an inv_model

--- trunk/eidors3d/algorithms/calc_jacobian.m
+++ trunk/eidors3d/algorithms/calc_jacobian.m
@@ -1,16 +1,22 @@
-function jacobian = calc_jacobian( fwd_model, img, varargin)
-% CALC_JACOBIAN: calculate jacobian from a fwd_model object and an image
+function jacobian = calc_jacobian( fwd_model, img, varargin )
+% CALC_JACOBIAN: calculate jacobian from an inv_model
 % 
-%    jacobian= calc_jacobian( fwd_model, img, ...)
+%  J = calc_jacobian( fwd_model, img )
+%      calc Jacobian on fwd_model at conductivity given
+%      in image (fwd_model is for forward and reconstruction)
 %
-% jacobian  is a jacobian matrix
-%      returned by the function fwd_model.solve
+%  J = calc_jacobian( fwd_model, img, rec_model )
+%      calc Jacobian on fwd_model at conductivity given
+%      in image (rec_model is for image reconstruction)
 %
 % fwd_model is a fwd_model structure
-% img       is an image structure
+% rec_model is a fwd_model structure (but may not
+%       have elems or electrodes)
+% img       is an image structure, with 'elem_data' or
+%           'node_data' parameters
 
-% (C) 2005 Andy Adler. License: GPL version 2 or version 3
-% $Id: calc_jacobian.m,v 1.16 2007-08-30 03:37:04 aadler Exp $
+% (C) 2005-08 Andy Adler. License: GPL version 2 or version 3
+% $Id: calc_jacobian.m,v 1.17 2008-03-13 20:42:11 aadler Exp $
 
 if nargin==1
    img= fwd_model;
