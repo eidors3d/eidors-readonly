eb1f53eb1689de842be5a47902b52e634b60bcce
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-03-15 21:23:19 +0000

    do basics and adv
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1175 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors3d/tests/tutorials_test.m
+++ eidors3d/tests/tutorials_test.m
@@ -1,5 +1,5 @@
 % Rerun all the tutorials to see if they work
-% $Id: tutorials_test.m,v 1.1 2008-03-15 20:41:57 aadler Exp $
+% $Id: tutorials_test.m,v 1.2 2008-03-15 21:23:19 aadler Exp $
 
  cd ../../eidors-htdocs/tutorial
 
@@ -32,3 +32,64 @@ backproj_solve02
 clear; clf; close all; 
 tutorial120a
 backproj_solve03
+
+% Find and trim all output images
+!find -name '*.png' -exec convert -trim '{}' '{}' ';'
+
+% adv_image_reconst
+cd ../adv_image_reconst
+
+disp('basic_iterative');
+clear; clf; close all; 
+basic_iterative01
+basic_iterative02
+
+disp('total_variation');
+clear; clf; close all; 
+total_variation01
+total_variation02
+total_variation03
+total_variation04
+
+disp('move2d');
+clear; clf; close all; 
+move_2d01
+move_2d02
+
+disp('move3d');
+clear; clf; close all; 
+move_3d01
+move_3d02
+
+disp('temporal_solver');
+clear; clf; close all; 
+temporal_solver01
+temporal_solver02
+temporal_solver03
+temporal_solver04
+
+disp('TV_hyperparams');
+clear; clf; close all; 
+TV_hyperparams01
+TV_hyperparams02
+name_base= 'tv_hp_00_img-a1=%3.1f-a2=%3.1f.jpg';
+TV_hyperparams03
+fid= fopen('TV-params-NSR=0.html','w');
+TV_hyperparams04
+
+% add noise
+TV_hyperparams05
+name_base= 'tv_hp_20_img-a1=%3.1f-a2=%3.1f.jpg';
+TV_hyperparams03
+fid= fopen('TV-params-NSR=.01.html','w');
+TV_hyperparams04
+
+
+% Don't know what to do - asked Camille
+%vitro_2d01.m
+
+% Find and trim all output images
+!find -name '*.png' -exec convert -trim '{}' '{}' ';'
+return
+
+
