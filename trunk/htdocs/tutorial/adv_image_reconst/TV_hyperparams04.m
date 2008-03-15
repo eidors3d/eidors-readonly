9446b503155b8dd371cdd29868d60744b1182a83
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-03-15 01:16:58 +0000

    html excape

--- trunk/htdocs/tutorial/adv_image_reconst/TV_hyperparams04.m
+++ trunk/htdocs/tutorial/adv_image_reconst/TV_hyperparams04.m
@@ -1,19 +1,28 @@
-% Generate HTML frame to view $Id: TV_hyperparams04.m,v 1.1 2008-03-15 00:12:20 aadler Exp $
+% Generate HTML frame to view $Id: TV_hyperparams04.m,v 1.2 2008-03-15 01:16:58 aadler Exp $
 
-fprintf(fid,'<TABLE><TR><TH>');
+a=sprintf('%calpha;',38); % alpha
+m=sprintf('%cminus;',38); % alpha
+s=sprintf('%c',60); % less than 
+e=sprintf('%c',62); % greater than<'; e
+tr= [s,'TR',e]; etr= [s,'/TR',e];
+th= [s,'TH',e]; etr= [s,'/TH',e];
+sub=[s,'SUB',e];esub=[s,'/SUB',e];
+sup=[s,'SUP',e];esup=[s,'/SUP',e];
+
+
+fprintf(fid,[s,'TABLE',e,tr,th]);
 for alpha1= alpha1list
-   fprintf(fid,'<TH>&alpha;<sub>1</sub>=10<sup>&minus;%3.1f</sup>',alpha1);
+   fprintf(fid,[th,a,sub,'1',esub,'=10',sup,m,'%3.1f',esup],alpha1);
 end
 fprintf(fid,'\n');
 
 for alpha2= alpha2list
-   fprintf(fid,'\n<TR>\n<TH>&alpha;<sub>2</sub>=10<sup>&minus;%3.1f</sup>\n',alpha2);
+   fprintf(fid,['\n',tr,'\n',th,a,sub,'2',esub,'=10', ...
+                sup,m,'%3.1f',esup,'\n'],alpha2);
    for alpha1= alpha1list
-      fprintf(fid,'<TD><img src="%s">\n', sprintf(name_base,alpha1,alpha2) );
+      fprintf(fid,[td,s,'img src="%s"',e,'\n'], sprintf(name_base,alpha1,alpha2) );
    end
 end
 
-fprintf(fid,'\n</TABLE></BODY></HTML>\n');
+fprintf(fid,['\n',s,'/TABLE',e]\n');
 fclose(fid);
-
-
