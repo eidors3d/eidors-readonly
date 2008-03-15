a36e00bf367c49654b1310a2a4d4baa764519655
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-03-15 20:41:57 +0000

    test to run all the tutorials

---
+++ trunk/eidors3d/tests/tutorials_test.m
@@ -0,0 +1,34 @@
+% Rerun all the tutorials to see if they work
+% $Id: tutorials_test.m,v 1.1 2008-03-15 20:41:57 aadler Exp $
+
+ cd ../../eidors-htdocs/tutorial
+
+% EIDORS_basics
+cd EIDORS_basics
+clear; clf; close all; 
+disp('tutorial010');
+tutorial010a
+tutorial010b
+tutorial010c
+
+clear; clf; close all; 
+disp('tutorial110');
+tutorial110a
+
+clear; clf; close all; 
+disp('tutorial120');
+tutorial120a
+tutorial120b
+
+clear; clf; close all; 
+disp('tutorial130');
+tutorial130a
+tutorial130b
+
+clear; clf; close all; 
+disp('backproj_solve');
+backproj_solve01
+backproj_solve02
+clear; clf; close all; 
+tutorial120a
+backproj_solve03
