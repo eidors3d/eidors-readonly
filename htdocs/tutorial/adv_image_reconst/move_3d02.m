f1a94329b5d9e1f5f017ee403ebd03812028dfb2
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-03-15 21:10:38 +0000

    use new way to calc colours
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1169 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/adv_image_reconst/move_3d02.m
+++ htdocs/tutorial/adv_image_reconst/move_3d02.m
@@ -1,6 +1,5 @@
-% Reconstruct 3D movement $Id: move_3d02.m,v 1.1 2007-08-30 03:58:28 aadler Exp $
+% Reconstruct 3D movement $Id: move_3d02.m,v 1.2 2008-03-15 21:10:38 aadler Exp $
 
-calc_colours('backgnd',[.8 .8 .9]);
 img3dim= img;
 
 mdl3dim.RtR_prior = 'laplace_image_prior';
@@ -9,10 +8,12 @@ mdl3dim.hyperparameter.value = 3e-3;
 % Show slices of 3D model with true movement vectors
 subplot(1,3,1);
 img3dim.elem_data = img3dim.elem_data - 1;
+img3dim.calc_colours.backgnd=[.8 .8 .9];
 show_slices_move( img3dim, move );
 
 % Inverse solution of data without movement consideration
 img3dim= inv_solve(mdl3dim, vh, vi);
+img3dim.calc_colours.backgnd=[.8 .8 .9];
 subplot(1,3,2)
 show_slices_move( img3dim );
 
@@ -31,6 +32,7 @@ mdlM.RtR_prior = 'aa_e_move_image_prior';
 mdlM.aa_e_move_image_prior.parameters = move_vs_conduct;
 % Solve inversglobale problem and show slices
 imgM = inv_solve(mdlM, vh, vi);
+imgM.calc_colours.backgnd=[.8 .8 .9];
 subplot(1,3,3)
 show_slices_move( imgM );
 print -r100 -dpng move_3d02.png
