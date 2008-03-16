0e72f89bf6050908e95b2208a0ae6e07d588ed40
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-03-16 11:06:27 +0000

    better examples
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1193 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/dual_model/dual_model03.m
+++ htdocs/tutorial/dual_model/dual_model03.m
@@ -1,13 +1,9 @@
-% Simulate data $Id: dual_model03.m,v 1.1 2008-03-16 01:33:12 aadler Exp $
+% Simulate data $Id: dual_model03.m,v 1.2 2008-03-16 11:06:27 aadler Exp $
 
-img1= inv_solve(imdl(1), vh, vi);
-subplot(221)
-show_fem(img1);
-
-img2= inv_solve(imdl(2), vh, vi);
-c2f= img2.fwd_model.coarse2fine;
-img2.elem_data= c2f * img2.elem_data;
-subplot(222)
-show_fem(img2);
+for model= 1:3
+   img= inv_solve(imdl(model), vh, vi);
+   subplot(2,3,model)
+   show_fem(img);
+end
 
 print -r125 -dpng dual_model03a.png;
