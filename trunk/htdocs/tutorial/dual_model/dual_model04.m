8f0dc0d8dcba8e5fbd8a58f7b97f990ba5027e1f
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-03-16 21:16:08 +0000

    Add coarse_fine models

---
+++ trunk/htdocs/tutorial/dual_model/dual_model04.m
@@ -0,0 +1,32 @@
+% create fine meshes $Id: dual_model04.m,v 1.1 2008-03-16 21:16:07 aadler Exp $
+
+% create base model
+mdl_coarse=mk_common_model('a2c0',16);
+
+for model = 1:3
+   if model==1
+% Model 1: 64 elements
+      mdl_str= 'a2c0';
+   elseif model==2
+% Model 2: 256 elements
+      mdl_str= 'b2c0';
+   elseif model==3
+% Model 3: 576 elements
+      mdl_str= 'c2c0';
+   end
+
+   mdl_fine= mk_common_model(mdl_str,16);
+   mdl_fine.fwd_model.mk_coarse_fine_mapping.n_interp= 150;
+   mdl_fine.RtR_prior = @noser_image_prior;
+   mdl_fine.hyperparameter.value = 3e-2;
+
+   imdl(model)= mdl_fine;
+   imdl(model).fwd_model.coarse2fine = ...
+       mk_coarse_fine_mapping( mdl_fine.fwd_model, mdl_coarse.fwd_model);
+
+   subplot(2,3, model)
+   show_fem(mdl_fine.fwd_model);
+
+end
+
+print -r125 -dpng dual_model04a.png;
