6705995a9d76e9fa4c3b4986677c1443b3ba1fbb
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-03-18 19:22:17 +0000

    give better errors
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1210 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors3d/algorithms/eidors_model_params.m
+++ eidors3d/algorithms/eidors_model_params.m
@@ -3,11 +3,17 @@ function mdl = eidors_model_params( mdl );
 % Fill in default parameter values in EIDORS types
 %
 % (C) 2006 Andy Adler. Licensed under the GPL v2
-% $Id: eidors_model_params.m,v 1.5 2007-03-27 16:13:07 aadler Exp $
+% $Id: eidors_model_params.m,v 1.6 2008-03-18 19:22:17 aadler Exp $
 
 % TODO - to caching
 
-switch mdl.type;
+try
+   type=mdl.type;
+catch
+   error('eidors_model_params: object is not eidors object (no type)');
+end
+
+switch type;
    case 'inv_model';
       mdl.fwd_model= eidors_model_params( mdl.fwd_model );
 
