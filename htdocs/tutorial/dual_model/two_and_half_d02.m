11973f395bab90e7e3e2eaf5758be1f4c0bc70af
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-03-19 16:50:43 +0000

    use better hyperparam
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1221 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/dual_model/two_and_half_d02.m
+++ htdocs/tutorial/dual_model/two_and_half_d02.m
@@ -1,19 +1,18 @@
-% Solve 2D and 3D model $Id: two_and_half_d02.m,v 1.2 2008-03-19 00:04:28 aadler Exp $
+% Solve 2D and 3D model $Id: two_and_half_d02.m,v 1.3 2008-03-19 16:50:43 aadler Exp $
 
 % Original target
 subplot(131)
-show_fem(demo_img); view(-62,28)
+show_fem(inhomg_img); view(-62,28)
 
 % Create inverse Model: Classic
-imdl.type=       'inv_model';
-imdl.name=       'Nick Polydorides GN inverse';
+imdl= eidors_obj('inv_model','NPs GN inverse');
 imdl.solve=       @np_inv_solve;
-imdl.hyperparameter.value = 3e-1;
+imdl.hyperparameter.value = 1e-3;
 imdl.R_prior= @np_calc_image_prior;
 imdl.np_calc_image_prior.parameters= [3 1]; % see iso_f_smooth: deg=1, w=1
 imdl.jacobian_bkgnd.value= 1;
 imdl.reconst_type= 'difference';
-imdl.fwd_model= demo_img.fwd_model;
+imdl.fwd_model= f_mdl; % fine model
 
 % Classic (inverse crime) solver
 img1= inv_solve(imdl, vh, vi);
