5afbb960bb929839f8e373824f786b15de26cdf5
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-03-19 00:04:28 +0000

    new way to do coarse/fine

--- trunk/htdocs/tutorial/dual_model/two_and_half_d02.m
+++ trunk/htdocs/tutorial/dual_model/two_and_half_d02.m
@@ -1,7 +1,12 @@
-% Solve 2D and 3D model $Id: two_and_half_d02.m,v 1.1 2008-03-15 19:13:57 aadler Exp $
+% Solve 2D and 3D model $Id: two_and_half_d02.m,v 1.2 2008-03-19 00:04:28 aadler Exp $
+
+% Original target
+subplot(131)
+show_fem(demo_img); view(-62,28)
 
 % Create inverse Model: Classic
-imdl.name= 'Nick Polydorides GN inverse';
+imdl.type=       'inv_model';
+imdl.name=       'Nick Polydorides GN inverse';
 imdl.solve=       @np_inv_solve;
 imdl.hyperparameter.value = 3e-1;
 imdl.R_prior= @np_calc_image_prior;
@@ -14,22 +19,5 @@ imdl.fwd_model= demo_img.fwd_model;
 img1= inv_solve(imdl, vh, vi);
 subplot(132)
 show_fem(img1); view(-62,28)
-
-
-% Create inverse Model: Classic
-imdl.name= 'Coarse/Fine inverse';
-imdl.coarse_fine.solve = imdl.solve;
-imdl.solve = @coarse_fine_solve;
-% imdl.coarse_fine.mapping = speye(n_e3d); % original solver
-  imdl.coarse_fine.mapping = c2f;
-imdl= eidors_obj('inv_model', imdl);
-
-img2= inv_solve(imdl, vh, vi);
-subplot(133)
-show_fem(img2); view(-62,28)
-
-% Original
-subplot(131)
-show_fem(demo_img); view(-62,28)
 
 print -r125 -dpng two_and_half_d02a.png
