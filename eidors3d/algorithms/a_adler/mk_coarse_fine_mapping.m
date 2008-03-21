4fc1ffab1906f42c0a3f662f84d6f4a43ddfc4f0
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-03-21 01:24:03 +0000

    New mapping strategy - find completely contained elems, then partial elems
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1228 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors3d/algorithms/a_adler/mk_coarse_fine_mapping.m
+++ eidors3d/algorithms/a_adler/mk_coarse_fine_mapping.m
@@ -1,4 +1,4 @@
-function mapping= mk_coarse_fine_mapping( f_mdl, c_mdl );
+function [mapping,c_elems] = mk_coarse_fine_mapping( f_mdl, c_mdl );
 % MK_COARSE_FINE_MAPPING: create a mapping matrix from coarse to fine FEM
 % mapping= mk_coarse_fine_mapping( f_mdl, c_mdl );
 %
@@ -11,7 +11,7 @@ function mapping= mk_coarse_fine_mapping( f_mdl, c_mdl );
 %                 - number of points to interpolate in each dimension
 
 % (C) 2007 Andy Adler. License: GPL version 2 or version 3
-% $Id: mk_coarse_fine_mapping.m,v 1.12 2008-03-20 23:19:14 aadler Exp $
+% $Id: mk_coarse_fine_mapping.m,v 1.13 2008-03-21 01:24:03 aadler Exp $
 
 % Mapping depends f_mdl and c_mdl, but only on nodes and elems
 cc_mdl.elems = c_mdl.elems;
@@ -26,7 +26,7 @@ ff_mdl.nodes = f_mdl.nodes * fto3d;
 ff_mdl.type  = 'fwd_model';
 n_ff= size(f_mdl.elems, 1);
 
-mapping = eidors_obj('get-cache', ff_mdl, 'coarse_fine_mapping', cc_mdl);
+mapping = eidors_obj('get-cache', {ff_mdl,cc_mdl}, 'coarse_fine_mapping');
 if ~isempty(mapping)
     eidors_msg('mk_coarse_fine_mapping: using cached value', 3);
 else
@@ -38,9 +38,12 @@ else
     end
    
 
-    eidors_obj('set-cache', ff_mdl, 'coarse_fine_mapping', mapping, cc_mdl);
+    eidors_obj('set-cache', {ff_mdl,cc_mdl}, 'coarse_fine_mapping', mapping);
     eidors_msg('mk_coarse_fine_mapping: setting cached value', 3);
 end
+
+f_elems = all_contained_elems( ff_mdl, cc_mdl);
+c_elems = contained_elems_i( ff_mdl, cc_mdl, f_elems);
 
 function mapping= mk_mapping( ff_mdl, cc_mdl, n_interp);
     xyzmin = min([ff_mdl.nodes;cc_mdl.nodes]);
@@ -55,6 +58,62 @@ function mapping= mk_mapping( ff_mdl, cc_mdl, n_interp);
     csum = sparse(f_tria(ff),1,1,n_ff, 1);
     mapping = c2f./(csum*ones(1,n_cc));
 
+% find all elems of ff_mdl completely contained in cc_mdl
+function c_elems = all_contained_elems( fm, cm)
+    nd= size(fm.nodes,2); % n dims
+    [nf,ef]= size(fm.elems);
+    nc= size(cm.elems,1);
+    fm_pts = zeros(nf*ef,3);
+    % shrink pts slightly so they're not on the boundary
+    s_fac= 1-1e-6;
+    for dim= 1:nd
+       % fm_pts is local_nodes x elems x xyz
+       fm_pt= reshape(fm.nodes(fm.elems,dim),nf,ef);
+       fm_ctr= mean(fm_pt,2)*ones(1,ef);
+       fm_pt = s_fac*(fm_pt-fm_ctr) + fm_ctr;
+       fm_pts(:,dim) = fm_pt(:);
+    end
+
+    tsn= tsearchn(cm.nodes(:,1:2), cm.elems, fm_pts(:,1:2));
+    tsn= reshape( tsn, nf, ef);
+    % if node in fm is outside cm, then set it back inside
+    tsn= sort( tsn, 2); % send Nans to right
+    isn= find(isnan(tsn));
+    tsn(isn)= tsn( 1+rem(isn-1,nf ) );
+    c_elems= all(diff(tsn,1,2)==0,2);
+    c_elems= c_elems.* tsn(:,1);
+
+% interpolate over a triangle with n_interp points
+% generate a set of points to fairly cover the triangle
+function interp= triangle_interpolation(n_interp)
+    interp= zeros(0,3);
+    for i=0:n_interp
+       for j=0:n_interp-i
+          interp= [interp;i,j,n_interp-i-j];
+       end
+    end
+
+    interp= (interp+1/3)/(n_interp+1);
+
+% find fraction of elem contained in cm
+function c_elems = contained_elems_i( fm, cm, idx)
+   nc= size(cm.elems,1);
+   nf= size(fm.elems,1);
+
+   fidx= find(idx==0);
+   ridx= 1:nf; ridx(fidx)=[];
+   idx(fidx)=[];
+   c_elems = sparse(ridx,idx,1,nf,nc);
+
+   interp= triangle_interpolation( 4 );
+   l_interp= size(interp,1);
+   for i = fidx'
+      el_nodes= fm.nodes(fm.elems(i,:),:);
+      fm_pts = interp*el_nodes;
+      tsn= tsearchn(cm.nodes(:,1:2), cm.elems, fm_pts(:,1:2));
+      c_elems(i,:)= sparse(1,tsn,1,1,nc)/l_interp;
+   end
+      
 % Do 3D interpolation of region xyzmin= [x,y,z] to xyzmax
 %  with n_interp points in the minimum direction
 function xyz = interpxyz( xyzmin, xyzmax, n_interp);
