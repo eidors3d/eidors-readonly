6d0bef290dbceed8c76704a9a160d2e56dbfd69a
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-03-24 15:59:38 +0000

    faster simulator
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1235 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors3d/models/a_adler/simulate_3d_movement.m
+++ eidors3d/models/a_adler/simulate_3d_movement.m
@@ -12,7 +12,7 @@ function [vh,vi,xyr_pt]= simulate_3d_movement( n_sims, mdl_3d, rad_pr )
 %   model = fwd_model to simulate 
 %         (default use internal, or if model= []);
 %
-% $Id: simulate_3d_movement.m,v 1.2 2008-03-24 14:55:01 aadler Exp $
+% $Id: simulate_3d_movement.m,v 1.3 2008-03-24 15:59:38 aadler Exp $
 
 
 mdl_3d.solve=      'np_fwd_solve';
@@ -22,7 +22,6 @@ mdl_3d.misc.perm_sym=   '{n}';
 n_elems= size(mdl_3d.elems,1);
 
 
-disp('STEP 1A: simultion 3D - homogeneous');
 % create homogeneous image + simulate data
 sigma= ones( size(mdl_3d.elems,1) ,1);
 img= eidors_obj('image', 'homogeneous image', ...
@@ -31,35 +30,16 @@ img= eidors_obj('image', 'homogeneous image', ...
 vh = fwd_solve( img);
 % show_fem( homg_img);
 
-disp('STEP 1B: simultion 3D - inhomogeneous');
-target_conductivity= .2;
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% np= calc_colours('npoints');
-npx=64;
-npy=64;
-npz=64;
-nn= mdl_3d.nodes;
-[x,y,z]=ndgrid( linspace(min(nn(:,1)),max(nn(:,1)),npx), ...
-                linspace(min(nn(:,2)),max(nn(:,2)),npy), ...
-                linspace(min(nn(:,3)),max(nn(:,3)),npz));
-% z= meshgrid(linspace(-.5,.5,np));
-eptr= img_mapper3a(mdl_3d.nodes', mdl_3d.elems', npx, npy, npz);
-
-eptr_n= sparse( eptr(:)+1,1,1, n_elems+1, 1);
-eptr_n= full(eptr_n(2:end))+.01;%plus a small positive value to prevent providing 0
-
-n_sims = 200;%frames number
-
 rp= 1;%.05;%obj radius
 radius=10;% 2/3;%obj trajactory radius (x-y plane)
 z0 =5;% -.3;
 zt =25;% .3;%obj moves along zaxis from z0 to zt
-% rp= .05;%obj radius
-% radius= 2/3;%obj trajactory radius (x-y plane)
-% z0 = -.3;
-% zt = .3;%obj moves along zaxis from z0 to zt
 
+npx=64;
+npy=64;
+npz=64;
+[x,y,z] = calc_point_grid(mdl_3d.nodes', npx, npy, npz);
+clear pts;
 for i=1:n_sims
     f_frac= (i-1)/n_sims;
     if rem(i,20)==0; fprintf('simulating %d / %d \n',i,n_sims); end
@@ -69,11 +49,25 @@ for i=1:n_sims
     zp = z0 + (zt - z0) * f_frac;% object moves from bottow to top
     xyzr_pt(:,i)= [xp;-yp;zp;rp]; % -y because images and axes are reversed
 
-    ff= find( (x(:)-xp).^2 + (y(:)-yp).^2 + (z(:)-zp).^2 <= rp^2 )';%find points in obj
-    obj_n= sparse( eptr(ff)+1,1,1, n_elems+1, 1);
-    obj_n= full(obj_n(2:end));
+    ff= find( (x(:)-xp).^2 + (y(:)-yp).^2 + (z(:)-zp).^2 <= rp^2 )';
+    pts{i} = ff;
+end
+pts_all = unique( [pts{:}] );
+pts_all = pts_all(:);
+for i=1:n_sims
+   [jnk,idx_i]= intersect( pts_all, pts{i});
+   pts_idx{i}= idx_i;
+end
 
-    img.elem_data= 1 + target_conductivity * (obj_n./eptr_n);%inhom img
+[eptr,vol]= img_mapper3a(mdl_3d.nodes', mdl_3d.elems',  ...
+         x(pts_all), y(pts_all), z(pts_all));
+
+target_conductivity= .2;
+
+for i=1:n_sims
+    obj_n= sparse( eptr(pts_idx{i}),1,1, n_elems, 1);
+    img.elem_data= 1 + target_conductivity * obj_n./vol;
+%   show_fem(img); pause;
 
     vi(i)= fwd_solve( img );% measurement
 end
@@ -83,36 +77,40 @@ vh= [vh(:).meas];
 
 
 % modified img_mapper3 from calc_slices.m
-% find out indices of points that locate in elements
-function EPTR= img_mapper3a(NODE, ELEM, npx, npy,npz )
-
-   [x,y,z] = calc_point_grid(NODE, ELEM, npx, npy, npx);
-
-   EPTR=zeros(npy,npx,npz);
-
+% this is like tsearch, but doesn't require
+% delaunay triangularization. I also wrote it, so I like it :-)
+function [EPTR, VOL] = img_mapper3a(NODE, ELEM, x,y,z );
    % calc and see if one point is in one element
    elems = size(ELEM,2);
+
+   EPTR= zeros(prod(size(x)),1);
+   VOL= zeros(elems,1);
+
    for j= 1: elems
-       if rem(j,1000)==0; fprintf('mapping %d / %d \n',j,elems); end
+       if rem(j,5000)==0; fprintf('mapping %d / %d \n',j,elems); end
        xyz= NODE(:,ELEM(:,j))';
-       min_z= min(xyz(:,3)); max_z= max(xyz(:,3));
-       %     if (min_z>0 | max_z<0)
-       %         continue;
-       %     end
        min_x= min(xyz(:,1)); max_x= max(xyz(:,1));
        min_y= min(xyz(:,2)); max_y= max(xyz(:,2));
+       min_z= min(xyz(:,3)); max_z= max(xyz(:,3));
 
-       % Simplex volume is det([v2-v1,v3-v1, ...])
-       VOL= abs(det(xyz'*[-1,1,0,0;-1,0,1,0;-1,0,0,1]'));
+       % Simplex relative volume is det([v2-v1,v3-v1, ...])
+       VOL(j)= abs(det(xyz'*[-1,1,0,0;-1,0,1,0;-1,0,0,1]'));
 
+       xlmax= x<=max_x; if ~any(xlmax); continue; end
+       xgmin= x>=min_x; if ~any(xgmin); continue; end
+       ylmax= y<=max_y; if ~any(ylmax); continue; end
+       ygmin= y>=min_y; if ~any(ygmin); continue; end
+       zlmax= z<=max_z; if ~any(zlmax); continue; end
+       zgmin= z>=min_z; if ~any(zgmin); continue; end
        % come up with a limited set of candidate points which
        % may be within the simplex
-       endr=find( y(:)<=max_y & y(:)>=min_y ...
-                & x(:)<=max_x & x(:)>=min_x ...
-                & z(:)<=max_z & z(:)>=min_z);
+       endr=find( xlmax & xgmin & ylmax & ygmin & zlmax & zgmin);
+       ll=  prod(size(endr));
+       if isempty(ll);
+          continue;
+       end
 
        nn=  size(ELEM,1); %Simplex vertices
-       ll=  length(endr);
        vol=zeros(ll,nn);
        for i=1:nn
            i1= i; i2= rem(i,nn)+1; i3= rem(i+1,nn)+1;
@@ -123,11 +121,11 @@ function EPTR= img_mapper3a(NODE, ELEM, npx, npy,npz )
                x3.*y1.*z2 + x2.*y3.*z1 - x3.*y2.*z1;
        end
 
-       endr( sum(abs(vol),2) - VOL >1e-8 )=[];
+       endr( sum(abs(vol),2) - VOL(j) >1e-8 )=[];
        EPTR(endr)= j;
    end %for j=1:ELEM
 
-function [x,y,z] = calc_point_grid(NODE, ELEM, npx, npy, npx);
+function [x,y,z] = calc_point_grid(NODE, npx, npy, npz);
    xmin = min(NODE(1,:));    xmax = max(NODE(1,:));
    xmean= mean([xmin,xmax]); xrange= xmax-xmin;
 
