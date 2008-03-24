9ff6906769735429e990e3d99b351a9ebd21b63a
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-03-24 14:55:01 +0000

    refactor

--- trunk/eidors3d/models/a_adler/simulate_3d_movement.m
+++ trunk/eidors3d/models/a_adler/simulate_3d_movement.m
@@ -12,7 +12,7 @@ function [vh,vi,xyr_pt]= simulate_3d_movement( n_sims, mdl_3d, rad_pr )
 %   model = fwd_model to simulate 
 %         (default use internal, or if model= []);
 %
-% $Id: simulate_3d_movement.m,v 1.1 2008-03-24 14:45:34 aadler Exp $
+% $Id: simulate_3d_movement.m,v 1.2 2008-03-24 14:55:01 aadler Exp $
 
 
 mdl_3d.solve=      'np_fwd_solve';
@@ -77,70 +77,68 @@ for i=1:n_sims
 
     vi(i)= fwd_solve( img );% measurement
 end
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 vi= [vi(:).meas];
 vh= [vh(:).meas];
-save data_32_16x2_coarse vi vh 
-save eptr_32_16x2_coarse eptr xyzr_pt
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 
 % modified img_mapper3 from calc_slices.m
 % find out indices of points that locate in elements
 function EPTR= img_mapper3a(NODE, ELEM, npx, npy,npz )
-xmin = min(NODE(1,:));    xmax = max(NODE(1,:));
-xmean= mean([xmin,xmax]); xrange= xmax-xmin;
 
-ymin = min(NODE(2,:));    ymax = max(NODE(2,:));
-ymean= mean([ymin,ymax]); yrange= ymax-ymin;
+   [x,y,z] = calc_point_grid(NODE, ELEM, npx, npy, npx);
 
-zmin = min(NODE(3,:));    zmax = max(NODE(3,:));
-zmean= mean([zmin,zmax]); zrange= zmax-zmin;
+   EPTR=zeros(npy,npx,npz);
 
-range= max([xrange, yrange,zrange]);
-[x y z]=ndgrid( ...
-    linspace( xmean - range*0.5, xmean + range*0.5, npx ), ...
-    linspace( ymean + range*0.5, ymean - range*0.5, npy ),...
-    linspace( zmean - zrange*0.5, zmean + zrange*0.5, npz ));
+   % calc and see if one point is in one element
+   elems = size(ELEM,2);
+   for j= 1: elems
+       if rem(j,1000)==0; fprintf('mapping %d / %d \n',j,elems); end
+       xyz= NODE(:,ELEM(:,j))';
+       min_z= min(xyz(:,3)); max_z= max(xyz(:,3));
+       %     if (min_z>0 | max_z<0)
+       %         continue;
+       %     end
+       min_x= min(xyz(:,1)); max_x= max(xyz(:,1));
+       min_y= min(xyz(:,2)); max_y= max(xyz(:,2));
 
-EPTR=zeros(npy,npx,npz);
+       % Simplex volume is det([v2-v1,v3-v1, ...])
+       VOL= abs(det(xyz'*[-1,1,0,0;-1,0,1,0;-1,0,0,1]'));
 
-% calc and see if one point is in one element
-elems = size(ELEM,2);
-for j= 1: elems
-    if rem(j,1000)==0; fprintf('mapping %d / %d \n',j,elems); end
-    xyz= NODE(:,ELEM(:,j))';
-    min_z= min(xyz(:,3)); max_z= max(xyz(:,3));
-    %     if (min_z>0 | max_z<0)
-    %         continue;
-    %     end
-    min_x= min(xyz(:,1)); max_x= max(xyz(:,1));
-    min_y= min(xyz(:,2)); max_y= max(xyz(:,2));
+       % come up with a limited set of candidate points which
+       % may be within the simplex
+       endr=find( y(:)<=max_y & y(:)>=min_y ...
+                & x(:)<=max_x & x(:)>=min_x ...
+                & z(:)<=max_z & z(:)>=min_z);
 
-    % Simplex volume is det([v2-v1,v3-v1, ...])
-    VOL= abs(det(xyz'*[-1,1,0,0;-1,0,1,0;-1,0,0,1]'));
+       nn=  size(ELEM,1); %Simplex vertices
+       ll=  length(endr);
+       vol=zeros(ll,nn);
+       for i=1:nn
+           i1= i; i2= rem(i,nn)+1; i3= rem(i+1,nn)+1;
+           x1= xyz(i1,1)-x(endr); y1= xyz(i1,2)-y(endr); z1= xyz(i1,3)-z(endr);
+           x2= xyz(i2,1)-x(endr); y2= xyz(i2,2)-y(endr); z2= xyz(i2,3)-z(endr);
+           x3= xyz(i3,1)-x(endr); y3= xyz(i3,2)-y(endr); z3= xyz(i3,3)-z(endr);
+           vol(:,i)= x1.*y2.*z3 - x1.*y3.*z2 - x2.*y1.*z3 + ...
+               x3.*y1.*z2 + x2.*y3.*z1 - x3.*y2.*z1;
+       end
 
-    % come up with a limited set of candidate points which
-    % may be within the simplex
-    endr=find( y(:)<=max_y & y(:)>=min_y ...
-        & x(:)<=max_x & x(:)>=min_x ...
-        & z(:)<=max_z & z(:)>=min_z);
+       endr( sum(abs(vol),2) - VOL >1e-8 )=[];
+       EPTR(endr)= j;
+   end %for j=1:ELEM
 
-    nn=  size(ELEM,1); %Simplex vertices
-    ll=  length(endr);
-    vol=zeros(ll,nn);
-    for i=1:nn
-        i1= i; i2= rem(i,nn)+1; i3= rem(i+1,nn)+1;
-        x1= xyz(i1,1)-x(endr); y1= xyz(i1,2)-y(endr); z1= xyz(i1,3)-z(endr);
-        x2= xyz(i2,1)-x(endr); y2= xyz(i2,2)-y(endr); z2= xyz(i2,3)-z(endr);
-        x3= xyz(i3,1)-x(endr); y3= xyz(i3,2)-y(endr); z3= xyz(i3,3)-z(endr);
-        vol(:,i)= x1.*y2.*z3 - x1.*y3.*z2 - x2.*y1.*z3 + ...
-            x3.*y1.*z2 + x2.*y3.*z1 - x3.*y2.*z1;
-    end
+function [x,y,z] = calc_point_grid(NODE, ELEM, npx, npy, npx);
+   xmin = min(NODE(1,:));    xmax = max(NODE(1,:));
+   xmean= mean([xmin,xmax]); xrange= xmax-xmin;
 
-    endr( sum(abs(vol),2) - VOL >1e-8 )=[];
-    EPTR(endr)= j;
-end %for j=1:ELEM
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% save data_3d_4_32_r2e16 vi vh 
-% save eptr_4_32_r2e16 eptr xyzr_pt
+   ymin = min(NODE(2,:));    ymax = max(NODE(2,:));
+   ymean= mean([ymin,ymax]); yrange= ymax-ymin;
 
+   zmin = min(NODE(3,:));    zmax = max(NODE(3,:));
+   zmean= mean([zmin,zmax]); zrange= zmax-zmin;
+
+   range= max([xrange, yrange,zrange]);
+   [x y z]=ndgrid( ...
+       linspace( xmean - range*0.5, xmean + range*0.5, npx ), ...
+       linspace( ymean + range*0.5, ymean - range*0.5, npy ),...
+       linspace( zmean - zrange*0.5, zmean + zrange*0.5, npz ));
