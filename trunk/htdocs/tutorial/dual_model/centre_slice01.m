358c35a9b4239dc2cfa47c5659a0b6821d20e41d
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-03-25 02:12:05 +0000

    map slices to 2D reconst

--- trunk/htdocs/tutorial/dual_model/centre_slice01.m
+++ trunk/htdocs/tutorial/dual_model/centre_slice01.m
@@ -1,11 +1,11 @@
-% Simulate Moving Ball - Helix $Id: centre_slice01.m,v 1.2 2008-03-25 01:57:01 aadler Exp $
+% Simulate Moving Ball - Helix $Id: centre_slice01.m,v 1.3 2008-03-25 02:12:04 aadler Exp $
 
 % get ng_mdl_16x2_coarse from data_contrib section of web page
 n_sims= 20;
 load ng_mdl_16x2_coarse.mat
 [vh,vi,xyzr_pt]= simulate_3d_movement( n_sims, ng_mdl_16x2_coarse);
 
-show_fem(ng_mdl_16x2_fine)
+show_fem(ng_mdl_16x2_coarse)
 crop_model(gca, inline('x-z<-15','x','y','z'))
 view(-23,14)
 
@@ -18,6 +18,5 @@ for i=1:n_sims
    set(hh,'EdgeColor',[1,0,1],'FaceColor',[1,0,1]);
 end
 hold off
-
 
 print -r100 -dpng centre_slice01a.png;
