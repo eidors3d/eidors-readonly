ff00b658ed84217717add416d1e65a62de9c4799
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-03-27 19:52:46 +0000

    centre slice calcs

---
+++ trunk/htdocs/tutorial/dual_model/centre_slice04.m
@@ -0,0 +1,22 @@
+% 2D solver $Id: centre_slice04.m,v 1.1 2008-03-27 19:52:46 aadler Exp $
+
+c_mdl.mk_coarse_fine_mapping.f2c_offset = [0,0,-.3*scl];
+c_mdl.mk_coarse_fine_mapping.z_depth = 0.1*scl;
+c2f= mk_coarse_fine_mapping( f_mdl, c_mdl);
+imdl.fwd_model.coarse2fine = c2f;
+imgc0= inv_solve(imdl, vh, vi);
+% Show image of reconstruction in upper planes
+subplot(132)
+show_slices(imgc0);
+
+c_mdl.mk_coarse_fine_mapping.f2c_offset = [0,0,.3*scl];
+c_mdl.mk_coarse_fine_mapping.z_depth = 0.1*scl;
+c2f= mk_coarse_fine_mapping( f_mdl, c_mdl);
+imdl.fwd_model.coarse2fine = c2f;
+imgc1= inv_solve(imdl, vh, vi);
+
+% Show image of reconstruction in lower planes
+subplot(133)
+show_slices(imgc1);
+
+print -r150 -dpng centre_slice04a.png;
