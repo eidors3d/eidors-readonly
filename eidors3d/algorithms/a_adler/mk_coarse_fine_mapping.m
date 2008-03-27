10a39d48d9091086bd8ff521a861843f9f3d49c8
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-03-27 17:15:47 +0000

    before significant refactoring
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1256 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors3d/algorithms/a_adler/mk_coarse_fine_mapping.m
+++ eidors3d/algorithms/a_adler/mk_coarse_fine_mapping.m
@@ -18,19 +18,17 @@ function mapping = mk_coarse_fine_mapping( f_mdl, c_mdl );
 %
 % if c_mdl is 2D and f_mdl is 3D, then parameter
 %     c_mdl.mk_coarse_fine_mapping.z_depth (default = inf)
-% indicates the +/- z_depth which is reflected onto the
-% c_mdl (ie c_mdl is at z=0 +/- z_depth)
+%     indicates the +/- z_depth which elements in 2D are
+%     considered to be extruded in 3D
 
 % (C) 2007-2008 Andy Adler. License: GPL version 2 or version 3
-% $Id: mk_coarse_fine_mapping.m,v 1.19 2008-03-25 16:38:09 aadler Exp $
+% $Id: mk_coarse_fine_mapping.m,v 1.20 2008-03-27 17:15:47 aadler Exp $
 
-% Mapping depends only on nodes and elems
+% Mapping depends only on nodes and elems - remove the other stuff
 try; c_mdl= rmfield(c_mdl,'electrode');   end
 try; c_mdl= rmfield(c_mdl,'stimulation'); end
 try; f_mdl= rmfield(f_mdl,'electrode');   end
 try; f_mdl= rmfield(f_mdl,'stimulation'); end
-
-% TODO: mapping step-> map f_mdl onto c_mdl
 
 mapping = eidors_obj('get-cache', {f_mdl,c_mdl}, 'coarse_fine_mapping');
 if ~isempty(mapping)
@@ -47,13 +45,6 @@ else
     f_elems = all_contained_elems( f_mdl, c_mdl, z_depth);
     mapping = contained_elems_i( f_mdl, c_mdl, f_elems);
 
-    % look for f_mdl z out of range
-    [nf,ne] = size(f_mdl.elems);
-    f_elem_z= reshape(f_mdl.nodes( f_mdl.elems(:), 3), nf, ne);
-    oor= all( abs(f_elem_z) > z_depth , 2);
-
-    mapping = spdiags(~oor, 0, nf,nf) * mapping;
-
     eidors_obj('set-cache', {f_mdl,c_mdl}, 'coarse_fine_mapping', mapping);
     eidors_msg('mk_coarse_fine_mapping: setting cached value', 3);
 end
@@ -61,16 +52,15 @@ end
 
 % find all elems of ff_mdl completely contained in cc_mdl
 function c_elems = all_contained_elems( fm, cm, z_depth)
-    nd= size(fm.nodes,2); % n dims
     [nf,ef]= size(fm.elems);
-    nc= size(cm.elems,1);
+    [nc,ec]= size(cm.elems);
     fm_pts = zeros(nf*ef,3);
-    % shrink pts slightly so they're not on the boundary
+    % shrink pts slightly (s_fac) so they're not on the boundary
     % by shrinking, we avoid cases where an element is
     % only slighly intersecting another. This is beyond the
     % resolution of the next step (interpolation) anyway
     s_fac= .9; % .9999;
-    for dim= 1:nd
+    for dim= 1:ef-1 % ef-1 is dimensions in fm
        % fm_pts is local_nodes x elems x xyz
        fm_pt= reshape(fm.nodes(fm.elems,dim),nf,ef);
        fm_ctr= mean(fm_pt,2)*ones(1,ef);
@@ -78,16 +68,48 @@ function c_elems = all_contained_elems( fm, cm, z_depth)
        fm_pts(:,dim) = fm_pt(:);
     end
 
-    tsn= tsearchn(cm.nodes(:,1:2), cm.elems, fm_pts(:,1:2));
-    tsn= reshape( tsn, nf, ef);
+    tsn= search_fm_pts_in_cm(cm, fm_pts, z_depth);
+    % if all points are outside (NaN) then c_elems = NaN
+    % if all points are in one elem   then c_elems = 1
+    % if all points are in diff elems then c_elems = 0
+    c_elems= all(diff(tsn,1,2)==0,2);
+    c_elems(any(isnan(tsn),1))= NaN;
+
+return
+    % This is not quite correct, 
+
     % if node in fm is outside cm, then set it back inside
     % this way only elems in fm that are completely outside cm
-    % will cause a problem
+    % will be identified that way
     tsn= sort( tsn, 2); % send Nans to right
     isn= find(isnan(tsn));
     tsn(isn)= tsn( 1+rem(isn-1,nf ) );
-    c_elems= all(diff(tsn,1,2)==0,2);
     c_elems= c_elems.* tsn(:,1);
+
+function tsn= search_fm_pts_in_cm(cm, fm_pts, z_depth);
+    dc= size(cm.elems,2);  %coarse dim+1
+    [nf,df]= size(fm_pts); %fine dim+1
+
+    tsn= NaN*ones(size(fm_pts,1),1);
+    not_oor= ~any( isnan(fm_pts,2) );
+
+    if dc==3
+       if df==4
+       % look for f_mdl z not out of range 
+          not_oor= not_oor &  any( abs(fm_pts(:,3) ) <= z_depth , 2);
+       end
+
+       dims=1:2;
+
+    elseif ec==4 %cm is 3D
+       error('cant handle 3D coarse models (yet)'); 
+    else
+       error('coarse model must be 2 or 3D');
+    end
+
+    tsn(not_oor)= tsearchn(cm.nodes(:,dims), cm.elems, fm_pts(not_oor,dims));
+    tsn= reshape( tsn, nf, df);
+
 
 % interpolate over a triangle with n_interp points
 % generate a set of points to fairly cover the triangle
@@ -116,7 +138,11 @@ function interp= triangle_interpolation(n_interp, dim_fine)
     interp= (interp + 1/dim_fine )/(n_interp+1);
 
 % find fraction of elem contained in cm
-function c_elems = contained_elems_i( fm, cm, idx)
+% idx is the index into which the elem is contained
+% if idx >= 1, the element is completely with in that coarse elem
+% if idx == 0, the element is crosses several coarse elems
+% if idx == Nan, the element is outside the coarse model
+function c_elems = contained_elems_i( fm, cm, idx, z_depth)
    [nc,dc]= size(cm.elems);
    [nf,df]= size(fm.elems);
 
@@ -125,11 +151,10 @@ function c_elems = contained_elems_i( fm, cm, idx)
 
    c_e_i= []; c_e_j=[]; c_e_v=[];
 
-   if df==3
-      interp= triangle_interpolation( 4, df );
-   else
-      interp= triangle_interpolation( 3, df );
-   end
+   % lower density interpolation in higher dimentions, since 
+   % the added dimensions will give extra interpolation points.
+   interp= triangle_interpolation( 7-df, df );
+
    l_interp = size(interp,1);
    dims = 1:dc-1; % run calc over dimensions 1 to dc-1
 
@@ -138,8 +163,9 @@ function c_elems = contained_elems_i( fm, cm, idx)
    el_nodes= reshape(el_nodes, df, l_fidx*(df-1) );
    fm_pts = interp*el_nodes;
    fm_pts = reshape(fm_pts, l_fidx*l_interp, df-1);
-   % find which element in the coarse mesh we're in
-   tsn=     tsearchn(cm.nodes(:,dims), cm.elems, fm_pts(:,dims));
+
+   tsn= search_fm_pts_in_cm(cm, fm_pts, z_depth);
+
    tsn_idx= ones(l_interp,1)*fidx(:)';
    tsn_idx= tsn_idx(:)';
    % find and isolate Nans
@@ -150,12 +176,19 @@ function c_elems = contained_elems_i( fm, cm, idx)
    nan_weight= reshape(nan_idx, l_interp, l_fidx);
    nan_weight= l_interp - sum(nan_weight,1);
    
-   ridx= 1:nf; ridx(fidx)=[];
-   idx(fidx)=[];
+   in_idx= find((idx==0) | isnan(idx));
+   ridx= 1:nf; ridx(in_idx)=[];
+   idx(in_idx)=[];
+
+   % first term is contribution from f_elems in one c_elem
+   % next term is contribution from f_elems in many c_elems, weighted
    c_elems = sparse(ridx,idx,1,nf,nc) +  ...
              sparse(fidx,fidx,1./nan_weight,nf,nf) * ...
              sparse(tsn_idx,tsn,1,nf,nc);
    return
+   c_elems1= sparse(ridx,idx,1,nf,nc);
+   c_elems2= sparse(fidx,fidx,1./nan_weight,nf,nf);
+   c_elems3= sparse(tsn_idx,tsn,1,nf,nc);
    
 
 % non vectorized calculation
