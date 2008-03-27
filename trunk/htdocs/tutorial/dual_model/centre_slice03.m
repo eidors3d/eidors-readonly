6604bbae95223d289de77b9c9957364b0f104e22
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-03-27 20:56:01 +0000

    mods to image

--- trunk/htdocs/tutorial/dual_model/centre_slice03.m
+++ trunk/htdocs/tutorial/dual_model/centre_slice03.m
@@ -1,4 +1,4 @@
-% 2D solver $Id: centre_slice03.m,v 1.3 2008-03-27 19:52:46 aadler Exp $
+% 2D solver $Id: centre_slice03.m,v 1.4 2008-03-27 20:56:00 aadler Exp $
 
 
 % Set coarse as reconstruction model
@@ -7,9 +7,9 @@ c_mdl.mk_coarse_fine_mapping.f2c_offset = [0,0,scl];
 c_mdl.mk_coarse_fine_mapping.f2c_project = (1/scl)*speye(3);
 c2f= mk_coarse_fine_mapping( f_mdl, c_mdl);
 imdl.fwd_model.coarse2fine = c2f;
-imdl.RtR_prior = @Gaussian_HPF_prior;
+imdl.RtR_prior = @gaussian_HPF_prior;
 imdl.solve = @aa_inv_solve;
-imdl.hyperparameter.value= 0.03;
+imdl.hyperparameter.value= 0.01;
 
 imgc= inv_solve(imdl, vh, vi);
 
