62bdddc7e5473bbbe12cb5c98d7480f1a9320700
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-03-27 14:26:53 +0000

    refine mesh manually -> setting th required nodes. fix distmesh that doesn't mesh outside nodes

--- trunk/eidors3d/meshing/distmesh/dm_mk_common_model.m
+++ trunk/eidors3d/meshing/distmesh/dm_mk_common_model.m
@@ -9,7 +9,7 @@ function [fwd_mdl]= dm_mk_common_model( mdl_name, nnodes, elec_pattern, stim_pat
 %  fwd_mdl:           eidors format fwd_model
 
 % (C) 2008 Andy Adler. License: GPL version 2 or version 3
-% $Id: dm_mk_common_model.m,v 1.4 2008-03-10 18:12:35 aadler Exp $
+% $Id: dm_mk_common_model.m,v 1.5 2008-03-27 14:26:53 aadler Exp $
 
 
 if mdl_name == 'c2'
@@ -26,7 +26,8 @@ if elec_pattern == [16, 1]
                   '{ad}','{ad}',{},100);
 
    elec_width= .1;
-   elec_nodes = mk_2d_circ_elec_nodes( elecs_per_ring, elec_width);
+   [elec_nodes, refine_nodes] = ...
+       mk_2d_circ_elec_nodes( elecs_per_ring, elec_width);
 end
 
    h0= estimate_h0(bbox, nnodes);
@@ -37,8 +38,9 @@ end
       z_contact=z_contact+0.001;
    end
 
-   fwd_mdl= dm_mk_fwd_model( fd, h0, bbox, elec_nodes, stim_pattern, ...
-                           z_contact, ['dm_mk_common_model= ',mdl_name]);
+   fwd_mdl= dm_mk_fwd_model( fd, h0, bbox, elec_nodes, refine_nodes, ...
+                              stim_pattern, z_contact, ...
+                              ['dm_mk_common_model= ',mdl_name]);
 
 
 function  h0= estimate_h0(bbox, nnodes);
@@ -46,13 +48,25 @@ function  h0= estimate_h0(bbox, nnodes);
    area_est= prod(abs(diff(dims,1)));
    h0 = (area_est/nnodes)^(1/dims);
 
-function  elec_nodes = mk_2d_circ_elec_nodes( n_elecs, elec_width )
+% elec_nodes is a list of nodes attached to each electrode
+% refine_nodes is a matrix of nodes created to help electrodes
+function  [elec_nodes,refine_nodes] ...
+        = mk_2d_circ_elec_nodes( n_elecs, elec_width )
    radius=1;
    th_delta = elec_width/2/pi/radius;
 % electrodes start top and go clockwise
+   refine_nodes= [];
    for i=1:n_elecs
       th= (i-1)*2*pi/n_elecs;
-      th= th + th_delta*[-.5;0;.5];
-      this_e_node= radius*[sin(th),cos(th)];
+      the= th + th_delta*[-1;-.5;0;.5;1];
+      this_e_node= radius*[sin(the),cos(the)];
       elec_nodes{i} = this_e_node;
+
+      thr= th + th_delta*[-5;-2;-1;-.5;0;.5;1;2;5];
+      csthr= [sin(thr),cos(thr)];
+      refine_nodes= [refine_nodes; ...
+                   radius*csthr([1:2,8:9],:); ...
+               .98*radius*csthr([1:9],:); ...
+               .95*radius*csthr([1:2:9],:); ...
+               .90*radius*csthr([1:4:9],:)];
    end
--- trunk/eidors3d/meshing/distmesh/dm_mk_fwd_model.m
+++ trunk/eidors3d/meshing/distmesh/dm_mk_fwd_model.m
@@ -1,5 +1,5 @@
-function [fwd_mdl]= dm_mk_fwd_model( fd, h0, bbox, ...
-                     elec_nodes, stim_pattern, z_contact, name)
+function [fwd_mdl]= dm_mk_fwd_model( fd, h0, bbox, elec_nodes, ...
+                     refine_nodes, stim_pattern, z_contact, name)
 % DM_MK_FWD_MODEL: create a fwd_model object using distmesh
 % fwd_mdl= dm_mk_fwd_model( fd, fh, h0, bbox,...
 %                          elec_nodes, stim_pattern, z_contact, name);
@@ -17,54 +17,23 @@ function [fwd_mdl]= dm_mk_fwd_model( fd, h0, bbox, ...
 %  fwd_mdl:           eidors format fwd_model
 
 % (C) 2008 Andy Adler. License: GPL version 2 or version 3
-% $Id: dm_mk_fwd_model.m,v 1.8 2008-03-13 20:44:31 aadler Exp $
+% $Id: dm_mk_fwd_model.m,v 1.9 2008-03-27 14:26:53 aadler Exp $
 
 if nargin <8
    name = 'MDL from dm_mk_fwd_model';
 end
 fwd_mdl= create_refined_model(name, fd, h0, bbox, elec_nodes, ...
-                        stim_pattern, z_contact); 
+                        refine_nodes, stim_pattern, z_contact); 
 
 function fmdl= create_refined_model(name, fd, h0, bbox, elec_nodes, ...
-                        stim_pattern, z_contact); 
-global node_voltages_for_distmesh
-   node_voltages_for_distmesh=[];
+                        refine_nodes, stim_pattern, z_contact); 
 
    % fixed_node= [elec_nodes{:}]; - wish we could do it like this - matlab bug!!
    fixed_node= [];
    for i= 1:prod(size(elec_nodes)) 
-      this_elec= [fixed_node; elec_nodes{i}];
       fixed_node= [fixed_node; elec_nodes{i}];
    end
-   [vtx,simp] = call_distmesh(fd,h0,bbox,fixed_node);
-
-   srf= find_boundary(simp);
-   fmdl = construct_fwd_model(srf,vtx,simp, name, ...
-                          stim_pattern, elec_nodes, z_contact);
-
-   return
-   homg_img= eidors_obj('image','', 'fwd_model',fmdl, ...
-                        'elem_data',ones(size(simp,1),1));
-
-% horrid way to handle this - send this to distmesh
-   % 4. Describe each edge by a unique pair of nodes
-   pair=zeros(0,2);
-   dim=2;
-   localpairs=nchoosek(1:dim+1,2);
-   for ii=1:size(localpairs,1)
-     pair=[pair;simp(:,localpairs(ii,:))];
-   end
-   pair=unique(sort(pair,2),'rows');
-   
-   node_v = calc_all_node_voltages( homg_img);
-   pair_v = node_v(pair(:,1),:) - node_v(pair(:,2),:);
-   max_pair_v = max(abs(pair_v),[],2);
-   pair_d = sqrt(sum(( vtx(pair(:,1),:) - vtx(pair(:,2),:) ).^2,2));
-   pair_E = max_pair_v./pair_d;
-   pair_p = (vtx(pair(:,1),:) + vtx(pair(:,2),:) )/2;
-   node_voltages_for_distmesh.pair_E= (pair_E+30); % inv E field
-   node_voltages_for_distmesh.pair_p= pair_p; % posn
-
+   fixed_node= [fixed_node; refine_nodes];
    [vtx,simp] = call_distmesh(fd,h0,bbox,fixed_node);
 
    srf= find_boundary(simp);
@@ -111,26 +80,8 @@ function mdl= construct_fwd_model(srf,vtx,simp, name, ...
    mdl.system_mat=     'aa_calc_system_mat';
 
 function [vtx,simp] = call_distmesh(fd,h0,bbox,fixed_node);
-   [vtx,simp] = distmesh2d(fd,@huniform,h0,bbox,fixed_node);
+   [vtx,simp] = distmeshnd(fd,@huniform,h0,bbox,fixed_node);
 
 %  FH:        Scaled edge length function - decrease in refined areas
 function h= huniform(p);
-   global node_voltages_for_distmesh;
-   if isempty(node_voltages_for_distmesh)
-      h= ones(size(p,1),1);
-   else
-      h= abs(sqrt(sum(p.^2,2))-1);
-      h= h+.10;
-      h(h>.2)= .2;
-      
-   end
-   if 0
-      np= size(node_voltages_for_distmesh.pair_p,1);
-      op= ones(np,1);
-      h=  ones(size(p,1),1);
-      for i= 1:size(p,1)
-         df= sum( (op*p(i,:) - node_voltages_for_distmesh.pair_p).^2 ,2);
-         ff= find( df == min(df));
-         h(i)= mean( node_voltages_for_distmesh.pair_E(ff) );
-      end
-   end
+   h= ones(size(p,1),1);
