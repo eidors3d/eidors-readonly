2b91e7d3bc3ee7075eee243e5748ab1363c9ba75
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-03-27 19:47:25 +0000

    reconstruction of a central slice
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1264 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/dual_model/centre_slice01.m
+++ htdocs/tutorial/dual_model/centre_slice01.m
@@ -1,14 +1,12 @@
-% Simulate Moving Ball - Helix $Id: centre_slice01.m,v 1.5 2008-03-25 19:21:49 aadler Exp $
+% Simulate Moving Ball - Helix $Id: centre_slice01.m,v 1.6 2008-03-27 19:47:24 aadler Exp $
 
 % get ng_mdl_16x2_vfine from data_contrib section of web page
 n_sims= 20;
 load ng_mdl_16x2_vfine.mat; fmdl= ng_mdl_16x2_vfine;
-%imdl= mk_common_model('b3cr',[16,2]); fmdl= imdl.fwd_model;
 [vh,vi,xyzr_pt]= simulate_3d_movement( n_sims, fmdl);
 
 show_fem(fmdl)
 crop_model(gca, inline('x-z<-15','x','y','z'))
-%crop_model(gca, inline('x-z<-0.5','x','y','z'))
 view(-23,14)
 
 hold on
@@ -17,7 +15,7 @@ for i=1:n_sims
    xp=xyzr_pt(1,i); yp=xyzr_pt(2,i);
    zp=xyzr_pt(3,i); rp=xyzr_pt(4,i);
    hh=surf(rp*xs+xp, rp*ys+yp, rp*zs+zp);
-   set(hh,'EdgeColor',[1,0,1],'FaceColor',[1,0,1]);
+   set(hh,'EdgeColor',[.4,0,.4],'FaceColor',[.2,0,.2]);
 end
 hold off
 
--- htdocs/tutorial/dual_model/centre_slice02.m
+++ htdocs/tutorial/dual_model/centre_slice02.m
@@ -1,22 +1,53 @@
-% 2D solver $Id: centre_slice02.m,v 1.3 2008-03-25 19:21:49 aadler Exp $
+% 2D solver $Id: centre_slice02.m,v 1.4 2008-03-27 19:47:24 aadler Exp $
 
-imdl = mk_common_model('b3cr',[16,2]); f_mdl=imdl.fwd_model;
-%imdl.fwd_model = ng_mdl_16x2_coarse;
+% Create and show inverse solver
+imdl = mk_common_model('b3cr',[16,2]);
+
+load ng_mdl_16x2_coarse; fmdl = ng_mdl_16x2_coarse;
+
+imdl.fwd_model = f_mdl;
 
 % Create coarse model
 imdl2d= mk_common_model('b2c2',16);
 c_mdl= imdl2d.fwd_model;
-% if fine model is offset use:
-%c_mdl.mk_coarse_fine_mapping.f2c_offset = [0,0,15];
-%c_mdl.mk_coarse_fine_mapping.f2c_project = (1/15)*speye(3);
-c2f= mk_coarse_fine_mapping( f_mdl, c_mdl);
-imdl.fwd_model.coarse2fine = c2f;
-imdl.RtR_prior = @noser_image_prior;
-imdl.solve = @aa_inv_solve;
-imdl.hyperparameter.value= 0.1;
 
-img= inv_solve(imdl, vh, vi);
-imgc= img; imgc.fwd_model= c_mdl;
+% Show fine model
+show_fem(f_mdl);
+crop_model(gca, inline('x-z<-15','x','y','z'))
+view(-23,10)
 
-subplot(131)
-show_slices(imgc);
+scl= 15; % scale difference between c_mdl and f_mdl
+c_els= c_mdl.elems;
+c_ndsx= c_mdl.nodes(:,1)*scl;
+c_ndsy= c_mdl.nodes(:,2)*scl;
+c_ndsz= 0*c_ndsx; 
+
+layersep= .3;
+layerhig= .1;
+hold on
+% Lower resonstruction layer
+hh= trimesh(c_els, c_ndsx, c_ndsy, c_ndsz+(1-layersep)*scl);
+set(hh, 'EdgeColor', [1,0,0]);
+hh= trimesh(c_els, c_ndsx, c_ndsy, c_ndsz+(1-layersep-layerhig)*scl);
+set(hh, 'EdgeColor', [.3,.3,.3]);
+hh= trimesh(c_els, c_ndsx, c_ndsy, c_ndsz+(1-layersep+layerhig)*scl);
+set(hh, 'EdgeColor', [.3,.3,.3]);
+
+% Upper resonstruction layer
+hh= trimesh(c_els, c_ndsx, c_ndsy, c_ndsz+(1+layersep)*scl);
+set(hh, 'EdgeColor', [0,0,1]);
+hh= trimesh(c_els, c_ndsx, c_ndsy, c_ndsz+(1+layersep-layerhig)*scl);
+set(hh, 'EdgeColor', [.3,.3,.3]);
+hh= trimesh(c_els, c_ndsx, c_ndsy, c_ndsz+(1+layersep+layerhig)*scl);
+set(hh, 'EdgeColor', [.3,.3,.3]);
+
+for i=1:n_sims
+   xp=xyzr_pt(1,i); yp=xyzr_pt(2,i);
+   zp=xyzr_pt(3,i); rp=xyzr_pt(4,i);
+   hh=surf(rp*xs+xp, rp*ys+yp, rp*zs+zp);
+   set(hh,'EdgeColor',[.4,0,.4]);
+end
+
+hold off;
+
+print -r100 -dpng centre_slice02a.png;
