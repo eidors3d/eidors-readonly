447467388f72fc374638161c481c82985e30c749
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-03-27 19:47:25 +0000

    reconstruction of a central slice

--- trunk/htdocs/tutorial/dual_model/centre_slice03.m
+++ trunk/htdocs/tutorial/dual_model/centre_slice03.m
@@ -1,20 +1,22 @@
-% 2D solver $Id: centre_slice03.m,v 1.1 2008-03-25 19:21:49 aadler Exp $
+% 2D solver $Id: centre_slice03.m,v 1.2 2008-03-27 19:47:24 aadler Exp $
 
-c_mdl.mk_coarse_fine_mapping.f2c_offset = [0,0,0];
+c_mdl.mk_coarse_fine_mapping.f2c_offset = [0,0,-.3];
 c_mdl.mk_coarse_fine_mapping.z_depth = 0.1;
 c2f= mk_coarse_fine_mapping( f_mdl, c_mdl);
 imdl.fwd_model.coarse2fine = c2f;
 imgc0= inv_solve(imdl, vh, vi);
-imgc0.fwd_model= c_mdl;
+% Show image of reconstruction in upper planes
+subplot(132)
+show_slices(imgc0);
 
-c_mdl.mk_coarse_fine_mapping.f2c_offset = [0,0,.2];
+c_mdl.mk_coarse_fine_mapping.f2c_offset = [0,0,.3];
 c_mdl.mk_coarse_fine_mapping.z_depth = 0.1;
 c2f= mk_coarse_fine_mapping( f_mdl, c_mdl);
 imdl.fwd_model.coarse2fine = c2f;
 imgc1= inv_solve(imdl, vh, vi);
-imgc1.fwd_model= c_mdl;
 
-subplot(132)
-show_slices(imgc0);
+% Show image of reconstruction in lower planes
 subplot(133)
 show_slices(imgc1);
+
+print -r150 -dpng centre_slice03a.png;
