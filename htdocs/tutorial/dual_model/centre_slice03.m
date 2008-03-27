2ff99254d496c3dd9db61dcba05e75ce4223709d
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-03-27 19:52:46 +0000

    centre slice calcs
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1265 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/dual_model/centre_slice03.m
+++ htdocs/tutorial/dual_model/centre_slice03.m
@@ -1,22 +1,17 @@
-% 2D solver $Id: centre_slice03.m,v 1.2 2008-03-27 19:47:24 aadler Exp $
+% 2D solver $Id: centre_slice03.m,v 1.3 2008-03-27 19:52:46 aadler Exp $
 
-c_mdl.mk_coarse_fine_mapping.f2c_offset = [0,0,-.3];
-c_mdl.mk_coarse_fine_mapping.z_depth = 0.1;
+
+% Set coarse as reconstruction model
+imdl.rec_model= c_mdl;
+c_mdl.mk_coarse_fine_mapping.f2c_offset = [0,0,scl];
+c_mdl.mk_coarse_fine_mapping.f2c_project = (1/scl)*speye(3);
 c2f= mk_coarse_fine_mapping( f_mdl, c_mdl);
 imdl.fwd_model.coarse2fine = c2f;
-imgc0= inv_solve(imdl, vh, vi);
-% Show image of reconstruction in upper planes
-subplot(132)
-show_slices(imgc0);
+imdl.RtR_prior = @Gaussian_HPF_prior;
+imdl.solve = @aa_inv_solve;
+imdl.hyperparameter.value= 0.03;
 
-c_mdl.mk_coarse_fine_mapping.f2c_offset = [0,0,.3];
-c_mdl.mk_coarse_fine_mapping.z_depth = 0.1;
-c2f= mk_coarse_fine_mapping( f_mdl, c_mdl);
-imdl.fwd_model.coarse2fine = c2f;
-imgc1= inv_solve(imdl, vh, vi);
+imgc= inv_solve(imdl, vh, vi);
 
-% Show image of reconstruction in lower planes
-subplot(133)
-show_slices(imgc1);
-
-print -r150 -dpng centre_slice03a.png;
+subplot(131)
+show_slices(imgc);
