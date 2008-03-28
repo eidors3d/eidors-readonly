fd3354740a0e2ce1cf20a26d6a78f9ecaf0c458c
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-03-28 15:32:07 +0000

    create square mesh
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1273 82006732-fb7e-4a22-9f28-86a212cce563

---
+++ htdocs/tutorial/dual_model/square_mesh01.m
@@ -0,0 +1,21 @@
+% Make square mesh $Id: square_mesh01.m,v 1.1 2008-03-28 15:32:05 aadler Exp $
+
+% Create square mesh model
+imdl= mk_common_model('c2s',16);
+s_mdl= rmfield(imdl.fwd_model,{'electrode','stimulation'});
+
+% assign one parameter to each square
+e= size(s_mdl.elems,1);
+params= ceil(( 1:e )/2);
+s_mdl.coarse2fine = sparse(1:e,params,1,e,max(params));
+
+show_fem(s_mdl)
+
+% Show parameter numbers
+   numeros= reshape(sprintf('%3d',params),3,e)';
+   xc=mean(reshape(s_mdl.nodes(s_mdl.elems,1),e,3),2);
+   yc=mean(reshape(s_mdl.nodes(s_mdl.elems,2),e,3),2);
+   text(xc,yc,numeros,'FontSize',7, ...
+            'HorizontalAlignment','center');
+
+print -r100 -dpng square_mesh01a.png;
