9bfb14af04c0411e0f9af6ec96ab823d478e7e52
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-04-15 09:56:18 +0000

    do sims

--- trunk/htdocs/papers/paper-EIT2008/code/make_figures.m
+++ trunk/htdocs/papers/paper-EIT2008/code/make_figures.m
@@ -32,6 +32,8 @@ print -dpng -r100 fig1b.png
 
 
 imdl= mk_common_model('c2c2',16');
+imdl.RtR_prior= @gaussian_HPF_prior;
+iml.hyperparameter.value= 0.003;
 img= inv_solve(imdl,vh,vi);
 
 clf; subplot(121)
@@ -52,5 +54,89 @@ show_fem(img2)
 axis image
 print -dpng -r125 fig2c.png
 
+% MOVING BALL #2
+% Model parameters
+n_elec= 16;
+n_nodes= 5000;
+
+% Create electrodes
+refine_level=4; %electrode refinement level
+elec_width= .1;
+z_contact = 0.01;
+% elect positions
+th=linspace(0,2*pi,n_elec(1)+1)';th(end)=[];
+elec_posn= [sin(th),cos(th)];
+[elec_nodes, refine_nodes] = dm_mk_elec_nodes( elec_posn, ...
+       elec_width, refine_level);
+
+% Define circular medium
+fd=inline('sum(p.^2,2)-1','p');
+bbox = [-1,-1;1,1];
+smdl= dm_mk_fwd_model( fd, [], n_nodes, bbox, ...
+                          elec_nodes, refine_nodes, z_contact);
+
+% stimulation pattern: adjacent
+stim_pat= mk_stim_patterns(n_elec,1,'{ad}','{ad}',{},1);
+
+smdl.stimulation= stim_pat;
+himg= eidors_obj('image','','fwd_model',smdl,...
+                 'elem_data',ones(size(smdl.elems,1),1) );
+
+vh= fwd_solve(himg);
+
+% Create a moving object within the model
+trg_rad= 0.1;
+radius= 0.75;
+n_sims= 20
+contrast= 0.1;
+if n_nodes>2000
+   n_th_obj= 30;
+else
+   n_th_obj= 15;
+end
+
+th=linspace(0,2*pi,n_th_obj+1)';th(end)=[];
+clear vi;
+for i= 1:3;
+   thc= 2*pi*(i-1)/n_sims;
+   trg_ctr= radius*[cos(thc),-sin(thc)];
+
+   trg_refine_nodes= [refine_nodes; ...
+                trg_rad*sin(th)+trg_ctr(1), ...
+                trg_rad*cos(th)+trg_ctr(2) ];
+
+   tmdl= dm_mk_fwd_model( fd, [], n_nodes, bbox, elec_nodes, ...
+                          trg_refine_nodes, z_contact);
+   tmdl.stimulation = stim_pat;
+
+   % find elements in size target
+   mdl_pts = interp_mesh( tmdl );
+   ctr_pts = mdl_pts - ones(size(mdl_pts,1),1)*trg_ctr;
+   in_trg  =(sum(ctr_pts.^2,2) < trg_rad^2);
+
+   % Create target image object
+   timg= eidors_obj('image','','fwd_model',tmdl,...
+                    'elem_data',1 + in_trg*contrast);
+   vi(i)= fwd_solve(timg);
+
+   if i==1
+      clf; show_fem(timg);
+      axis image
+      print -dpng -r100 fig3a.png
+      
+      axis image
+      axis([0.5,1.1,-0.5,0.1]);
+      print -dpng -r100 fig3b.png
+
+   end
+end
+
+
+show_fem(inv_solve(imdl,vh,vi(1)));
+axis image; print -dpng -r125 fig4a.png
+show_fem(inv_solve(imdl,vh,vi(2)));
+axis image; print -dpng -r125 fig4b.png
+show_fem(inv_solve(imdl,vh,vi(3)));
+axis image; print -dpng -r125 fig4c.png
 !find -name 'fig*.png' -exec convert  -trim '{}' PNG8:'{}' ';'
 
