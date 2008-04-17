4e4c4f3b07810267298a2f4842721183ca3de00a
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-04-17 19:58:38 +0000

    bug with xlen != ylen
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1357 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors3d/models/a_adler/mk_grid_model.m
+++ eidors3d/models/a_adler/mk_grid_model.m
@@ -2,7 +2,7 @@ function fmdl= mk_grid_model(xvec, yvec);
 % MK_GRID_MODEL: Create reconstruction model on pixelated grid 
 
 % (C) 2008 Andy Adler. License: GPL version 2 or version 3
-% $Id: mk_grid_model.m,v 1.1 2008-04-17 19:44:47 aadler Exp $
+% $Id: mk_grid_model.m,v 1.2 2008-04-17 19:58:38 aadler Exp $
 
 xlen = length(xvec);
 ylen = length(yvec);
@@ -12,13 +12,13 @@ fmdl= eidors_obj('fwd_model', ...
 [x,y]= meshgrid( xvec, yvec);
     x=x';y=y';
     fmdl.nodes= [x(:),y(:)];
-    k= 1:ylen-1;
-    elem_frac = [ k;k+1;k+ylen; ...
-                  k+1;k+ylen;k+ylen+1];
+    k= 1:xlen-1;
+    elem_frac = [ k;k+1;k+xlen; ...
+                  k+1;k+xlen;k+xlen+1];
     elem_frac= reshape(elem_frac, 3,[])';
     fmdl.elems=  [];
     for j=0:ylen-2
-       fmdl.elems=  [fmdl.elems; elem_frac + ylen*j];
+       fmdl.elems=  [fmdl.elems; elem_frac + xlen*j];
     end
 
     fmdl.boundary = find_boundary( fmdl.elems);
