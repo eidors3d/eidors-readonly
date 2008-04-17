bf7d85c52505d898df5b61137bfbbb8a7b69a87c
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-04-17 16:12:05 +0000

    create mesh example
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1350 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/other_models/square_mesh01.m
+++ htdocs/tutorial/other_models/square_mesh01.m
@@ -1,4 +1,4 @@
-% $Id: square_mesh01.m,v 1.1 2008-04-17 15:55:39 aadler Exp $
+% $Id: square_mesh01.m,v 1.2 2008-04-17 16:12:05 aadler Exp $
 
 z_contact= 0.01;
 n_elec= 9;
@@ -6,8 +6,8 @@ nodes_per_elec= 5;
 elec_width= 0.2;
 elec_spacing= 1.0;
 
-xllim=-10; xrlim= 10; ydepth=-10;
-[x,y] = meshgrid( linspace(xllim,xrlim,40), linspace(ydepth,0,20) );
+xllim=-10; xrlim= 10; ydepth=-15;
+[x,y] = meshgrid( linspace(xllim,xrlim,40), linspace(ydepth,0,30) );
 vtx= [x(:),y(:)];
 
 xgrid=  linspace(-elec_width/2, +elec_width/2, nodes_per_elec)';
@@ -25,12 +25,12 @@ for i=1:n_elec
         [x0 + xgrid*2   , y0-elec_width*2+ 0*xgrid]];
 end
 
-mdl= mk_fmdl_from_nodes( vtx, elec_nodes, z_contact, 'sq_m1');
+fmdl= mk_fmdl_from_nodes( vtx, elec_nodes, z_contact, 'sq_m1');
 
 
 subplot(121)
-show_fem(mdl); axis image
+show_fem(fmdl); axis image
 subplot(122)
-show_fem(mdl); axis image; axis([-2 2 -1.5 0.5]);
+show_fem(fmdl); axis image; axis([-2 2 -1.5 0.5]);
 
-print -dpng -r150 square_mesh01a.m
+print -dpng -r150 square_mesh01a.png
