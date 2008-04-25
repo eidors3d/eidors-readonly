59f0de1b8b26e872aad05ab7c2ab457866d7e093
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-04-25 14:38:52 +0000

    more efficient boundary using sort

--- trunk/eidors3d/algorithms/n_polydorides/find_boundary.m
+++ trunk/eidors3d/algorithms/n_polydorides/find_boundary.m
@@ -8,18 +8,34 @@ function [srf] = find_boundary(simp);
 %        boundary simplices are of 1 lower dimention than simp
 %simp = The simplices matrix
 
-% $Id: find_boundary.m,v 1.13 2007-08-30 03:37:04 aadler Exp $
+% $Id: find_boundary.m,v 1.14 2008-04-25 14:38:52 aadler Exp $
 
 wew = size(simp,2) - 1;
 
 if wew==3 || wew==2
+   srf= find_2or3d_boundary_old(simp,wew);
    srf= find_2or3d_boundary(simp,wew);
 else
    eidors_msg('find_boundary: WARNING: not 2D or 3D simplices',1);
    srf=[]; return;
 end
 
-function srf= find_2or3d_boundary(simp,wew);
+function srf= find_2or3d_boundary(simp,dim);
+   % convert to integer to make sort faster
+   simp = uint32( simp ); % max 4b elements - enough for a while
+   localface = nchoosek(1:dim+1,dim);
+   srf_local= simp(:,localface');
+   srf_local= reshape( srf_local', dim, []);
+   srf_local= sort(srf_local)';
+   sort_srl = sortrows( srf_local );
+   same_srl = find( all( sort_srl(1:end-1,:) == sort_srl(2:end,:), 2) );
+   diff_srl = ones(size(srf_local,1),1);
+   diff_srl(same_srl) = 0;
+   diff_srl(same_srl+1) = 0;
+
+   srf= sort_srl( find(diff_srl),: );
+
+function srf= find_2or3d_boundary_old(simp,wew);
 els= size(simp,1);
 srf= [];
 % find elements on the boundary
