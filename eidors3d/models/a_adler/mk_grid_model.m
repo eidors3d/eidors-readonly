2f4918cdbb48286d4d3511284499ea9e6778982c
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-05-02 09:16:51 +0000

    fix parameters
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1378 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors3d/models/a_adler/mk_grid_model.m
+++ eidors3d/models/a_adler/mk_grid_model.m
@@ -13,7 +13,7 @@ function [cmdl, c2f]= mk_grid_model(fmdl, xvec, yvec, zvec);
 %  zvec - z edges (optional - to create 3D model)
 
 % (C) 2008 Andy Adler. License: GPL version 2 or version 3
-% $Id: mk_grid_model.m,v 1.4 2008-05-01 17:51:25 aadler Exp $
+% $Id: mk_grid_model.m,v 1.5 2008-05-02 09:16:51 aadler Exp $
 
 if nargin == 3
    cmdl = mk_2d_grid(xvec,yvec);
@@ -24,7 +24,40 @@ else
 end
 
 if ~isempty( fmdl)
-   c2f= mk_coarse_fine_mapping( fmdl, cmdl);
+   if nargin ==3
+      nef= size( fmdl.elems,1);
+      c2f= sparse(nef,0);
+      mdl_pts = interp_mesh( fmdl, 3);
+      x_pts = squeeze(mdl_pts(:,1,:));
+      y_pts = squeeze(mdl_pts(:,2,:));
+      for yi= 1:length(yvec)-1
+            in_y_pts = y_pts >= yvec(yi) & y_pts < yvec(yi+1);
+         for xi= 1:length(xvec)-1
+             in_x_pts =  x_pts >= xvec(xi) & x_pts < xvec(xi+1);
+             in_pts = mean( in_y_pts & in_x_pts , 2);
+             c2f = [c2f,sparse(in_pts)];
+         end
+      end
+   elseif nargin ==4
+%     c2f= mk_coarse_fine_mapping( fmdl, cmdl);
+      nef= size( fmdl.elems,1);
+      c2f= sparse(nef,0);
+      mdl_pts = interp_mesh( fmdl, 3);
+      x_pts = squeeze(mdl_pts(:,1,:));
+      y_pts = squeeze(mdl_pts(:,2,:));
+      z_pts = squeeze(mdl_pts(:,3,:));
+      for zi= 1:length(zvec)-1
+               in_z_pts = z_pts >= zvec(zi) & z_pts < zvec(zi+1);
+         for yi= 1:length(yvec)-1
+               in_y_pts = y_pts >= yvec(yi) & y_pts < yvec(yi+1);
+            for xi= 1:length(xvec)-1
+                in_x_pts =  x_pts >= xvec(xi) & x_pts < xvec(xi+1);
+                in_pts = mean( in_y_pts & in_x_pts & in_z_pts, 2);
+                c2f = [c2f,sparse(in_pts)];
+            end
+         end
+      end
+   end
 end
 
 function cmdl= mk_2d_grid(xvec, yvec);
@@ -33,8 +66,7 @@ function cmdl= mk_2d_grid(xvec, yvec);
    cmdl= eidors_obj('fwd_model', ...
             sprintf('Grid model %d x %d', xlen, ylen) );
 
-   [x,y]= meshgrid( xvec, yvec);
-   x=x';y=y';
+   [x,y]= ndgrid( xvec, yvec);
    cmdl.nodes= [x(:),y(:)];
    k= 1:xlen-1;
    elem_frac = [ k;k+1;k+xlen; ...
@@ -50,5 +82,41 @@ function cmdl= mk_2d_grid(xvec, yvec);
 % assign one single parameter to each square element
    e= size(cmdl.elems,1);
    params= ceil(( 1:e )/2);
+   cmdl.coarse2fine = sparse(1:e,params,1,e,max(params));
+
+function cmdl= mk_3d_grid(xvec, yvec, zvec);
+   xlen = length(xvec);
+   ylen = length(yvec);
+   zlen = length(zvec);
+   cmdl= eidors_obj('fwd_model', ...
+            sprintf('Grid model %d x %d x %d', xlen, ylen, zlen) );
+
+   [x,y,z]= ndgrid( xvec, yvec, zvec);
+   cmdl.nodes= [x(:),y(:),z(:)];
+   k= 1:xlen-1;
+   ac = xlen; up = xlen*ylen; % accross vs up
+   elem_frac = [ k;     k+1 ;  k+ac;   k+up;  ...
+                 k+1;   k+ac;  k+up;   k+up+1; ...
+                 k+ac;  k+up;  k+up+1; k+up+ac; ...
+                 k+1;   k+ac;  k+ac+1; k+up+1; ...
+                 k+ac;  k+ac+1;k+up+1; k+up+ac; ...
+                 k+ac+1;k+up+1;k+up+ac;k+up+ac+1];
+   elem_frac= reshape(elem_frac, 4,[])';
+
+   row_frac =  [];
+   for j=0:ylen-2
+      row_frac=  [row_frac; elem_frac + ac*j];
+   end
+
+   cmdl.elems=  [];
+   for k=0:zlen-2
+      cmdl.elems=  [cmdl.elems; row_frac + up*k];
+   end
+
+   cmdl.boundary = find_boundary( cmdl.elems);
+
+% assign one single parameter to each square element
+   e= size(cmdl.elems,1);
+   params= ceil(( 1:e )/6);
    cmdl.coarse2fine = sparse(1:e,params,1,e,max(params));
 
