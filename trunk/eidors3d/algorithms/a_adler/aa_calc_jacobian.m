0e781a6ffcd7efc35d71f53fae169c7deee4d856
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-05-09 22:38:05 +0000

    more efficient jacobian calcs and memory saving for  coarse2fine

--- trunk/eidors3d/algorithms/a_adler/aa_calc_jacobian.m
+++ trunk/eidors3d/algorithms/a_adler/aa_calc_jacobian.m
@@ -10,7 +10,7 @@ function J= aa_calc_jacobian( fwd_model, img)
 % img = image background for jacobian calc
 
 % (C) 2005 Andy Adler. License: GPL version 2 or version 3
-% $Id: aa_calc_jacobian.m,v 1.14 2008-05-08 14:35:40 aadler Exp $
+% $Id: aa_calc_jacobian.m,v 1.15 2008-05-09 22:38:00 aadler Exp $
 
 pp= aa_fwd_parameters( fwd_model );
 s_mat= calc_system_mat( fwd_model, img );
@@ -33,16 +33,18 @@ CC= sparse((1:d*e),pp.ELEM(:),ones(d*e,1), d*e, n);
 
 if isfield(fwd_model,'coarse2fine')
    DE = jacobian_calc(pp, zi2E, CC, sv, fwd_model.coarse2fine);
+   nparam= size(fwd_model.coarse2fine,2);
 else
    DE = jacobian_calc(pp, zi2E, CC, sv);
+   nparam= e;
 end
 
-J = zeros( pp.n_meas, e );
+J = zeros( pp.n_meas, nparam );
 idx=0;
 for j= 1:pp.n_stim
    meas_pat= fwd_model.stimulation(j).meas_pattern;
    n_meas  = size(meas_pat,1);
-   DEj = reshape( DE(:,j,:), pp.n_elec, e );
+   DEj = reshape( DE(:,j,:), pp.n_elec, nparam );
    J( idx+(1:n_meas),: ) = meas_pat*DEj;
    idx= idx+ n_meas;
 end
@@ -65,14 +67,134 @@ function DE = jacobian_calc(pp, zi2E, CC, sv, c2f);
 d= pp.n_dims+1;
 dfact= (d-1)*(d-2); % Valid for d<=3
 
-DE= zeros(pp.n_elec, pp.n_stim, pp.n_elem);
-for k= 1:pp.n_elem
-    a=  inv([ ones(d,1), pp.NODE( :, pp.ELEM(:,k) )' ]);
-    dSS_dEj= 2*a(2:d,:)'*a(2:d,:)/dfact/abs(det(a));
+do_c2f = ( nargin==5 );
 
-    idx= d*(k-1)+1 : d*k;
-    CC_idx = CC(idx,:);
-    dq= zi2E * CC_idx' * dSS_dEj * CC_idx * sv;
-    DE(:,:,k)= dq;
+if do_c2f
+   DE= zeros(pp.n_elec, pp.n_stim, size(c2f,2) );
+else
+   DE= zeros(pp.n_elec, pp.n_stim, pp.n_elem);
 end
 
+dSS_dEj= zeros(d,d,pp.n_elem);
+
+for k= 1:pp.n_elem
+    a=  inv([ ones(d,1), pp.NODE( :, pp.ELEM(:,k) )' ]);
+    F= a(2:d,:) * sqrt(2/dfact/abs(det(a)));
+%   dSS_dEj(:,:,k)= 2*a(2:d,:)'*a(2:d,:)/dfact/abs(det(a));
+    dSS_dEj(:,:,k)= F'*F; % Check transpose for complex?
+end
+
+if ~do_c2f
+   if 0
+      for k= 1:pp.n_elem
+          idx= d*(k-1)+1 : d*k;
+          CC_idx = CC(idx,:);
+          dq= zi2E * CC_idx' * dSS_dEj(:,:,k) * CC_idx * sv;
+          DE(:,:,k)= dq;
+       end
+    elseif 0
+      SS= calc_SS(pp);
+      FF= calc_FF(pp);
+      for k= 1:pp.n_elem
+          idx= d*(k-1)+1 : d*k;
+          de= pp.n_elem * d;
+          dd= zeros(de,1);dd(idx)=1;
+          dSS__dEj = SS * spdiags(dd,0, de, de);
+          dq= zi2E * CC' * dSS__dEj * CC * sv;
+          DE(:,:,k)= dq;
+       end
+    elseif 0
+      FC= calc_FF(pp)*CC;
+      zi2E_FCt = zi2E * FC';
+      FC_sv   = FC * sv;
+      for k= 1:pp.n_elem
+          idx= (d-1)*(k-1)+1 : (d-1)*k;
+          de= pp.n_elem * (d-1);
+          dd= zeros(de,1);dd(idx)=1;
+          dDD_dEj = spdiags(dd,0, de, de);
+          dq= zi2E_FCt * dDD_dEj * FC_sv;
+          DE(:,:,k)= dq;
+       end
+    else
+      FC= calc_FF(pp)*CC;
+      zi2E_FCt = zi2E * FC';
+      FC_sv   = FC * sv;
+      for k= 1:pp.n_elem
+          idx= (d-1)*(k-1)+1 : (d-1)*k;
+          dq= zi2E_FCt(:,idx) * FC_sv(idx,:);
+          DE(:,:,k)= dq;
+       end
+    end
+
+else
+   if 0
+      for k= 1:pp.n_elem
+          idx= d*(k-1)+1 : d*k;
+          CC_idx = CC(idx,:);
+          dq= zi2E * CC_idx' * dSS_dEj(:,:,k) * CC_idx * sv;
+          chg_col = c2f(k,:);
+          for j= find( chg_col );
+             DE(:,:,j) = DE(:,:,j) + chg_col(j)*dq;
+          end
+       end
+   elseif 0
+      FC= calc_FF(pp)*CC;
+      zi2E_FCt = zi2E * FC';
+      FC_sv   = FC * sv;
+      de= pp.n_elem * (d-1);
+      for k= 1:size(c2f,2);
+          chg_col = kron( c2f(:,k), ones(d-1,1));
+          dDD_dEj = spdiags(chg_col,0, de, de);
+          dq= zi2E_FCt * dDD_dEj * FC_sv;
+          DE(:,:,k)= dq;
+      end
+   else
+      FC= calc_FF(pp)*CC;
+      zi2E_FCt = zi2E * FC';
+      FC_sv   = FC * sv;
+      de= pp.n_elem * (d-1);
+      for k= 1:size(c2f,2);
+          ff = find( c2f(:,k) );
+          lff= length(ff)*(d-1);
+          ff1= ones(d-1,1) * ff(:)';
+          ffd= (d-1)*ff1 + (-(d-2):0)'*ones(1,length(ff));
+          dDD_dEj = spdiags(c2f(ff1,k), 0, lff, lff);
+          dq= zi2E_FCt(:,ffd) * dDD_dEj * FC_sv(ffd,:);
+          DE(:,:,k)= dq;
+      end
+   end
+end
+
+function SS= calc_SS( p) 
+d= p.n_dims+1;
+e= p.n_elem;
+n= p.n_node;
+
+SSiidx= floor([0:d*e-1]'/d)*d*ones(1,d) + ones(d*e,1)*(1:d) ;
+SSjidx= [1:d*e]'*ones(1,d);
+SSdata= zeros(d*e,d);
+% FIXME: test_2d_resitor gives wrong results (unless dfact = 4*(d-1))
+dfact = (d-2)*(d-1); % to match analytic solution 4*dims
+for j=1:e
+  a=  inv([ ones(d,1), p.NODE( :, p.ELEM(:,j) )' ]);
+  idx= d*(j-1)+1 : d*j;
+  SSdata(idx,1:d)= 2*a(2:d,:)'*a(2:d,:)/abs(det(a));
+end %for j=1:ELEMs 
+SS= sparse(SSiidx,SSjidx,SSdata/dfact);
+
+function FF= calc_FF( p) 
+d0= p.n_dims+0;
+d1= p.n_dims+1;
+e= p.n_elem;
+n= p.n_node;
+
+FFiidx= floor([0:d0*e-1]'/d0)*d1*ones(1,d1) + ones(d0*e,1)*(1:d1);
+FFjidx= [1:d0*e]'*ones(1,d1);
+FFdata= zeros(d0*e,d1);
+dfact = (d0-1)*d0;
+for j=1:e
+  a=  inv([ ones(d1,1), p.NODE( :, p.ELEM(:,j) )' ]);
+  idx= d0*(j-1)+1 : d0*j;
+  FFdata(idx,1:d1)= a(2:d1,:)* sqrt(2/dfact/abs(det(a)));
+end %for j=1:ELEMs 
+FF= sparse(FFiidx,FFjidx,FFdata)';
---
+++ trunk/eidors3d/tests/test_c2f_jacobian.m
@@ -0,0 +1,203 @@
+function test_c2f_jacobian
+% Test calc of jacobian given coarse to fine mapping
+%
+% (C) Andy Adler 2008. Licenced under GPL v2 or v3
+% $Id: test_c2f_jacobian.m,v 1.1 2008-05-09 22:38:05 aadler Exp $
+eidors_cache clear
+
+test1_np_3d
+test2_aa_2d
+test3_aa_3d
+test4_npaa_3d
+
+function test1_np_3d
+% Fine model
+imdl = mk_common_model('n3r2');
+f1mdl = imdl.fwd_model;
+
+% Create 2D FEM of all NODES with z=0
+n2d = f1mdl.nodes( (f1mdl.nodes(:,3) == 0), 1:2);
+e2d = delaunayn(n2d);
+c_mdl = eidors_obj('fwd_model','2d','elems',e2d,'nodes',n2d);
+
+% Create f_mdl with coarse2fine mapping
+c2f= mk_coarse_fine_mapping( f1mdl, c_mdl );
+f2mdl= f1mdl;
+f2mdl.coarse2fine = c2f;
+
+% Just in case - define reconstruction
+imdl.solve=       @np_inv_solve;
+imdl.hyperparameter.value = 1e-3;
+imdl.R_prior= @np_calc_image_prior;
+imdl.np_calc_image_prior.parameters= [3 1]; % see iso_f_smooth: deg=1, w=1
+imdl.jacobian_bkgnd.value= .6;
+imdl.reconst_type= 'difference';
+imdl.fwd_model= f1mdl; % fine model
+
+img = calc_jacobian_bkgnd( imdl);
+
+t=cputime;
+J1= np_calc_jacobian( f1mdl, img)*c2f;
+fprintf('np_calc - full: t=%f\n',  cputime-t  );
+n_J1 = norm(J1,'fro');
+
+t=cputime;
+J2= np_calc_jacobian( f2mdl, img);
+fprintf('np_calc - c2f: t=%f\n',  cputime-t  );
+
+n_J1_J2 = norm(J1-J2,'fro')/ n_J1;
+if n_J1_J2 > 1e-4; 
+   warning('J1-J2 is too big');
+end
+
+
+t=cputime;
+J2p= perturb_jacobian( f2mdl, img);
+fprintf('perturb_j - c2f: t=%f\n',  cputime-t  );
+
+n_J1_J2p = norm(J1-J2p,'fro')/ n_J1;
+if n_J1_J2p > 1e-4; 
+   warning('J1-J2p is too big');
+end
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function test2_aa_2d
+
+imdl= mk_common_model('a2c2',16);
+cmdl = imdl.fwd_model;
+imdl= mk_common_model('c2c0',16);
+f1mdl = imdl.fwd_model;
+
+c2f= mk_coarse_fine_mapping(f1mdl,cmdl);
+img= calc_jacobian_bkgnd(imdl);
+
+t=cputime;
+J1= aa_calc_jacobian( f1mdl, img)*c2f;
+fprintf('aa_calc - full: t=%f\n',  cputime-t  );
+n_J1 = norm(J1,'fro');
+
+f2mdl= f1mdl;
+f2mdl.coarse2fine = c2f;
+
+%%%%%%%%%% J2
+t=cputime;
+J2= aa_calc_jacobian( f2mdl, img);
+fprintf('aa_calc - c2f: t=%f\n',  cputime-t  );
+
+n_J1_J2 = norm(J1-J2,'fro')/ n_J1;
+if n_J1_J2 > 1e-4; 
+   warning('J1-J2 is too big');
+keyboard
+end
+
+
+%%%%%%%%%% J2p
+t=cputime;
+J2p= perturb_jacobian( f2mdl, img);
+fprintf('perturb_j - c2f: t=%f\n',  cputime-t  );
+
+n_J1_J2p = norm(J1-J2p,'fro')/ n_J1;
+
+if n_J1_J2p > 1e-4; 
+   warning('J1-J2p is too big');
+end
+
+%%%%%%%%%% J1p
+t=cputime;
+J1p= perturb_jacobian( f1mdl, img)*c2f;
+fprintf('perturb_j - full: t=%f\n',  cputime-t  );
+
+n_J1_J1p = norm(J1-J1p,'fro')/ n_J1;
+if n_J1_J1p > 1e-4; 
+   warning('J1-J1p is too big');
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function test3_aa_3d
+
+imdl= mk_common_model('a2c0',16);
+cmdl = imdl.fwd_model;
+imdl= mk_common_model('a3cr',16);
+f1mdl = imdl.fwd_model;
+
+c2f= mk_coarse_fine_mapping(f1mdl,cmdl);
+img= calc_jacobian_bkgnd(imdl);
+
+t=cputime;
+J1= aa_calc_jacobian( f1mdl, img)*c2f;
+fprintf('aa_calc - full: t=%f\n',  cputime-t  );
+n_J1 = norm(J1,'fro');
+
+f2mdl= f1mdl;
+f2mdl.coarse2fine = c2f;
+
+%%%%%%%%%% J2
+t=cputime;
+J2= aa_calc_jacobian( f2mdl, img);
+fprintf('aa_calc - c2f: t=%f\n',  cputime-t  );
+
+n_J1_J2 = norm(J1-J2,'fro')/ n_J1;
+if n_J1_J2 > 1e-4; 
+   warning('J1-J2 is too big');
+keyboard
+end
+
+
+%%%%%%%%%% J2p
+t=cputime;
+J2p= perturb_jacobian( f2mdl, img);
+fprintf('perturb_j - c2f: t=%f\n',  cputime-t  );
+
+n_J1_J2p = norm(J1-J2p,'fro')/ n_J1;
+
+if n_J1_J2p > 1e-4; 
+   warning('J1-J2p is too big');
+end
+
+function test4_npaa_3d
+% Fine model
+imdl = mk_common_model('n3r2');
+f1mdl = imdl.fwd_model;
+f1mdl.solve = @aa_fwd_solve;
+f1mdl.jacobian = @aa_calc_jacobian;
+f1mdl.system_mat = @aa_calc_system_mat;
+
+% Create 2D FEM of all NODES with z=0
+n2d = f1mdl.nodes( (f1mdl.nodes(:,3) == 0), 1:2);
+e2d = delaunayn(n2d);
+c_mdl = eidors_obj('fwd_model','2d','elems',e2d,'nodes',n2d);
+
+% Create f_mdl with coarse2fine mapping
+c2f= mk_coarse_fine_mapping( f1mdl, c_mdl );
+f2mdl= f1mdl;
+f2mdl.coarse2fine = c2f;
+
+imdl.jacobian_bkgnd.value= .6;
+img = calc_jacobian_bkgnd( imdl);
+
+t=cputime;
+J1= aa_calc_jacobian( f1mdl, img)*c2f;
+fprintf('aa_calc - full: t=%f\n',  cputime-t  );
+n_J1 = norm(J1,'fro');
+
+t=cputime;
+J2= aa_calc_jacobian( f2mdl, img);
+fprintf('aa_calc - c2f: t=%f\n',  cputime-t  );
+
+n_J1_J2 = norm(J1-J2,'fro')/ n_J1;
+if n_J1_J2 > 1e-4; 
+   warning('J1-J2 is too big');
+end
+
+
+t=cputime;
+J2p= perturb_jacobian( f2mdl, img);
+fprintf('perturb_j - c2f: t=%f\n',  cputime-t  );
+
+n_J1_J2p = norm(J1-J2p,'fro')/ n_J1;
+if n_J1_J2p > 1e-4; 
+   warning('J1-J2p is too big');
+end
+
