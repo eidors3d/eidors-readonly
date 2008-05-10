8834f148f58ae4b864603c8464f9e5b07f6ffec2
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-05-10 18:37:51 +0000

    test for non square resistor -- approximate

--- trunk/eidors3d/tests/test_2d_resistor.m
+++ trunk/eidors3d/tests/test_2d_resistor.m
@@ -1,9 +1,9 @@
 % Create 2D model of a cylindrical resistor
-% $Id: test_2d_resistor.m,v 1.6 2008-05-10 17:42:38 aadler Exp $
+% $Id: test_2d_resistor.m,v 1.7 2008-05-10 18:37:51 aadler Exp $
 
 nn= 12;     % number of nodes
 ww=2;       % width = 4
-if ~exist('conduc');conduc=   1;end  % conductivity in Ohm-meters
+if ~exist('conduc');conduc=  .4;end  % conductivity in Ohm-meters
 current= 4;  % Amps
 z_contact= 1e-2;
 scale = .35;
@@ -37,7 +37,7 @@ fprintf('Solver %s: %f\n', fsol.name, fsol.meas);
 
 % analytical solution
 wid_len= max(mdl.nodes) - min(mdl.nodes);
-R = wid_len(1) / wid_len(2) / conduc + 2*z_contact/scale;
+R = wid_len(2) / wid_len(1) / conduc + 2*z_contact/scale;
 
 V= current*R;
 fprintf('Solver %s: %f\n', 'analytic', V);
@@ -71,20 +71,50 @@ Jaa= calc_jacobian(mdl,img);
 
 
 % TEST RESISTOR THAT IS NOT RECTANGULAR
-s1=.6;s2=3*s1;
-m2= eidors_obj('fwd_model','not rect');
-m2.nodes= [s1/2,-1;-s1/2,-1;s1/2,0;-s1/2,0;s2/2,1;-s2/2,1];
-m2.elems= [1,2,3;2,3,4;3,4,5;4,5,6];
-m2.gnd_node= 3;
-m2.electrode(1).nodes= [1,2];
-m2.electrode(2).nodes= [3,4];
-m2.electrode(3).nodes= [5,6];
-[m2.electrode(:).z_contact]= deal(.0001);
-m2.stimulation(1).stim_pattern= [-1;0;1];
-m2.stimulation(1).meas_pattern= [1,-1,0;0,-1,1];
+nn= 12;     % number of nodes
+ww=2;       % width = 4
+if ~exist('conduc');conduc=  .4;end  % conductivity in Ohm-meters
+current= 4;  % Amps
+z_contact= 1e-2;
+scale = .35;
+mdl=mk_grid_model([],3+scale*(1:ww), scale*(1:nn/ww));
+xdelta= .05*mdl.nodes(1:ww:nn,2);
+mdl.nodes(1:ww:nn,1) = mdl.nodes(1:ww:nn,1) + xdelta;
+mdl.nodes(2:ww:nn,1) = mdl.nodes(2:ww:nn,1) - xdelta;
+mdl.gnd_node = 1;
+elec_nodes= [1:ww];
+elec(1).nodes= elec_nodes;      elec(1).z_contact= z_contact;
+elec(2).nodes= nn-elec_nodes+1; elec(2).z_contact= z_contact;
+stim.stim_pattern= [-1;1]*current;
+stim.meas_pattern= [-1,1];
+mdl.stimulation= stim;
+mdl.electrode= elec;
+show_fem(mdl);
+n_el = size(mdl.elems,1);
+img= eidors_obj('image','2D rectangle', 'fwd_model',mdl, ...
+      'elem_data', ones(n_el,1) * conduc );
 % AA_SOLVER
-m2.solve = @aa_fwd_solve;
-m2.system_mat = @aa_calc_system_mat;
-img= eidors_obj('image','','fwd_model',m2,'elem_data',[1,1,1,1]);
-fsol= fwd_solve(img);
+mdl.solve = @aa_fwd_solve;
+mdl.system_mat = @aa_calc_system_mat;
+fsol= fwd_solve(mdl,img);
 fprintf('Solver %s: %f\n', fsol.name, fsol.meas);
+% NP_SOLVER
+mdl.solve = @np_fwd_solve;
+mdl.system_mat = @np_calc_system_mat;
+fsol= fwd_solve(mdl,img);
+fprintf('Solver %s: %f\n', fsol.name, fsol.meas);
+
+% Analytic
+e1= mdl.nodes(mdl.electrode(1).nodes,:);
+d1= sqrt( sum(( [1,-1]*e1 ).^2 ));
+e2= mdl.nodes(mdl.electrode(2).nodes,:);
+d2= sqrt( sum(( [1,-1]*e2 ).^2 ));
+hig= max(mdl.nodes(:,2)) - min(mdl.nodes(:,2));
+% R = int_h (1/d) dh 
+% d= a+b*h; a= d1; b= (d2-d1)/hig
+% R = int 1/(a+b*h)*dh = log(a+bh)/b
+% R = [log(d2) - log(d1)]*hig/(d2-d1)
+% doesn't account for arc in conductivity pattern
+R = (log(d2)-log(d1))*hig/(d2-d1)/conduc + 2*z_contact/scale;
+V= current*R;
+fprintf('Solver %s: %f\n', 'analytic', V);
