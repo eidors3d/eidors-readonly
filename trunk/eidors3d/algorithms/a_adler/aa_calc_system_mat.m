cc851c2835009615907aeeec4a8e018457a9f5f4
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-05-11 01:11:00 +0000

    fixed for 3D

--- trunk/eidors3d/algorithms/a_adler/aa_calc_system_mat.m
+++ trunk/eidors3d/algorithms/a_adler/aa_calc_system_mat.m
@@ -9,15 +9,17 @@ function s_mat= aa_calc_system_mat( fwd_model, img)
 %   CC  = Connectivity Matrix
 
 % (C) 2005 Andy Adler. License: GPL version 2 or version 3
-% $Id: aa_calc_system_mat.m,v 1.17 2008-05-11 00:27:13 aadler Exp $
+% $Id: aa_calc_system_mat.m,v 1.18 2008-05-11 01:11:00 aadler Exp $
 
 FC= aa_system_mat_fields( fwd_model);
+lFC= size(FC,1);
 
 pp= aa_fwd_parameters( fwd_model);
-elem_sigma = kron( [img.elem_data(:);ones(pp.n_elec,1)], ...
-                    ones(pp.n_dims,1) );
-l_elem_s= length( elem_sigma);
-elem_sigma = spdiags( elem_sigma, 0, l_elem_s, l_elem_s);
+elem_sigma = kron( img.elem_data(:), ones(pp.n_dims,1) );
 
-s_mat.E= FC' * elem_sigma * FC;
+ES= ones(lFC,1);
+ES(1:length(elem_sigma))= elem_sigma;
+ES= spdiags(ES,0,lFC,lFC);
+
+s_mat.E= FC' * ES * FC;
 
