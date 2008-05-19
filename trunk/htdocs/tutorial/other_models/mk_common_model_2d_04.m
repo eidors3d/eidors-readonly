6dc5e1d71220b66cb2796a0601eca645b6bf174b
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-05-19 18:22:34 +0000

    distmesh models

---
+++ trunk/htdocs/tutorial/other_models/mk_common_model_2d_04.m
@@ -0,0 +1,20 @@
+% 2D models $Id: mk_common_model_2d_04.m,v 1.1 2008-05-19 18:22:32 aadler Exp $
+
+clf; subplot(221)
+
+imdl= mk_common_model('a2d0c',8);
+show_fem(imdl.fwd_model);
+print -dpng -r100 mk_common_model_2d_04a.png
+
+imdl= mk_common_model('b2d1c',16);
+show_fem(imdl.fwd_model);
+print -dpng -r100 mk_common_model_2d_04b.png
+
+imdl= mk_common_model('c2d2c',32);
+show_fem(imdl.fwd_model);
+print -dpng -r100 mk_common_model_2d_04c.png
+
+imdl= mk_common_model('e2d4c',16);
+show_fem(imdl.fwd_model);
+print -dpng -r100 mk_common_model_2d_04d.png
+
