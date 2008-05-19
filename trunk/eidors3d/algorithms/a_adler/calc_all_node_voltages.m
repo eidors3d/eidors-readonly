856e85d44e9ba147b42fba347c307ed125dd0862
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-05-19 17:15:18 +0000

    z_contact

--- trunk/eidors3d/algorithms/a_adler/calc_all_node_voltages.m
+++ trunk/eidors3d/algorithms/a_adler/calc_all_node_voltages.m
@@ -6,16 +6,16 @@ function node_v= calc_all_node_voltages( himg );
 % node_v     = n_nodes x n_stims voltage on each node
 
 % (C) 2007 Andy Adler. License: GPL version 2 or version 3
-% $Id: calc_all_node_voltages.m,v 1.3 2008-03-13 20:39:53 aadler Exp $
+% $Id: calc_all_node_voltages.m,v 1.4 2008-05-19 17:15:18 aadler Exp $
 
    % create one "measurement electrode" per node and
-   % stim patterns to match
+   % meas patterns to match
    n_nodes = size(himg.fwd_model.nodes,1);
    n_elecs = length(himg.fwd_model.electrode);
    ze= zeros(n_elecs,1);
    for i= 1:n_nodes
       himg.fwd_model.electrode(n_elecs + i).nodes= i;
-      himg.fwd_model.electrode(n_elecs + i).z_contact= 0;
+      himg.fwd_model.electrode(n_elecs + i).z_contact= 0.01;
    end
 
    n_stims = length(himg.fwd_model.stimulation);
