8348f26e55db5890b3f7fb77c825f7c03f902e48
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-05-19 14:39:46 +0000

    run_tutorials in each dir

---
+++ trunk/htdocs/tutorial/EIDORS_basics/run_tutorials.m
@@ -0,0 +1,33 @@
+% run all tutorials in this directory
+% $Id: run_tutorials.m,v 1.1 2008-05-19 14:39:46 aadler Exp $
+
+clear; clf; close all; 
+disp('tutorial010');
+tutorial010a
+tutorial010b
+tutorial010c
+
+clear; clf; close all; 
+disp('tutorial110');
+tutorial110a
+
+clear; clf; close all; 
+disp('tutorial120');
+tutorial120a
+tutorial120b
+
+clear; clf; close all; 
+disp('tutorial130');
+tutorial130a
+tutorial130b
+
+clear; clf; close all; 
+disp('backproj_solve');
+backproj_solve01
+backproj_solve02
+clear; clf; close all; 
+tutorial120a
+backproj_solve03
+
+% Find and trim all output images
+!find -name '*.png' -exec convert -trim '{}' '{}' ';'
