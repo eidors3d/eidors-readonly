f89f36416162026af76b0e0919ef3c4d89a42877
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-06-11 14:40:02 +0000

    use specified backprojection matrix to solve
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1442 82006732-fb7e-4a22-9f28-86a212cce563

---
+++ eidors3d/algorithms/a_adler/solve_use_matrix.m
@@ -0,0 +1,33 @@
+function img= solve_use_matrix( inv_model, data1, data2)
+% SOLVE_USE_MATRIX solve using reconstruction matrix
+% img= solve_use_matrix( inv_model, data1, data2)
+% img        => output image (or vector of images)
+% data1      => differential data at earlier time
+% data2      => differential data at later time
+% inv_model  => inverse model struct
+% inv_model.solve_use_matrix.RM  = rec_matrix
+% inv_model.solve_use_matrix.map = map to elements (optional)
+%    map is the size of the reconst model. contains
+%    zero if elem is unfilled, otherwise pointer to matrix row
+
+% (C) 2008 Andy Adler. License: GPL version 2 or version 3
+% $Id: solve_use_matrix.m,v 1.1 2008-06-11 14:40:02 aadler Exp $
+
+dv = calc_difference_data( data1, data2, inv_model.fwd_model);
+
+RM = inv_model.solve_use_matrix.RM;
+
+if isfield(inv_model.solve_use_matrix,'map')
+   map = inv_model.solve_use_matrix.map;
+else
+   map = 1:size(RM,1);
+end
+
+
+rec= inv_model.solve_use_matrix.RM * dv;
+sol = rec(map,:);
+
+% create a data structure to return
+img.name= 'solved by solve_use_matrix';
+img.elem_data = sol;
+img.fwd_model= inv_model.fwd_model;
