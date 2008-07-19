59c76ad00b4d9609d76fea26a677ce49528a7251
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-07-19 17:27:35 +0000

    show ROIs

--- trunk/htdocs/tutorial/lung_EIT/if_peep_trial04.m
+++ trunk/htdocs/tutorial/lung_EIT/if_peep_trial04.m
@@ -1,7 +1,26 @@
-% Display Times courses $Id: if_peep_trial04.m,v 1.1 2008-07-19 17:08:51 aadler Exp $
+% Display Times courses $Id: if_peep_trial04.m,v 1.2 2008-07-19 17:27:35 aadler Exp $
 
-   loop = 1;
-   subplot(2,1,loop);
-   img = i_injury;
+for loop = 1:2;
+   if loop == 1; img = i_injury;
+   else          img = i_treat;
+   end
 
+   subplot(2,2,loop);
+   time = (0:size(img.elem_data,2)-1)/13; % Frame rate = 13/s
 
+   raster= calc_slices( img );
+   ROIs  = raster(ylocn, xlocn, :);
+   ROIs  = permute(ROIs, [3,1,2]);
+
+   %Normalize to its maximum
+   for i=1:length(ylocn)
+      ROIs(:,i) = - ROIs(:,i) / max(abs(ROIs(:,i)));
+   end
+   plot(time, ROIs);
+   axis([0, max(time), -0.1, 1]);
+   legend('1','2','3','4',2)
+   xlabel('time (s)')
+   ylabel('normalized \Delta Z')
+end
+
+print -dpng -r150 if_peep_trial04.png
