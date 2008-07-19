d3f4181baf0be1bfafa7aae1531086786344841b
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-07-19 21:12:31 +0000

    Lung injury data
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1512 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/lung_EIT/if_lung_injury01.m
+++ htdocs/tutorial/lung_EIT/if_lung_injury01.m
@@ -1,9 +1,9 @@
-% Create Model $Id: if_lung_injury01.m,v 1.1 2008-07-19 20:58:23 aadler Exp $
+% Create Model $Id: if_lung_injury01.m,v 1.2 2008-07-19 21:12:29 aadler Exp $
 
 imdl= mk_common_model('c2c2',16);
 % Reverse electrodes to give 'clinical' view (looking toward patient head)
 imdl.fwd_model.electrode =  ...
-   imdl.fwd_model.electrode([9:16,1:8]);
+   imdl.fwd_model.electrode([9:-1:1,16:-1:10]);
 % Use normalized difference imaging
 imdl.fwd_model.normalize_measurements=1;
 
