a8b8cd7bad1ddd4b64f5f235a018f79a84679969
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-07-21 14:27:24 +0000

    Fix orientation

--- trunk/htdocs/tutorial/lung_EIT/if_lung_injury01.m
+++ trunk/htdocs/tutorial/lung_EIT/if_lung_injury01.m
@@ -1,9 +1,6 @@
-% Create Model $Id: if_lung_injury01.m,v 1.2 2008-07-19 21:12:29 aadler Exp $
+% Create Model $Id: if_lung_injury01.m,v 1.3 2008-07-21 14:27:24 aadler Exp $
 
 imdl= mk_common_model('c2c2',16);
-% Reverse electrodes to give 'clinical' view (looking toward patient head)
-imdl.fwd_model.electrode =  ...
-   imdl.fwd_model.electrode([9:-1:1,16:-1:10]);
 % Use normalized difference imaging
 imdl.fwd_model.normalize_measurements=1;
 
@@ -11,9 +8,9 @@ subplot(211);
 show_fem(imdl.fwd_model, [0,1,0]);
 axis equal; axis off
 
-hh=text(-1.1,0,'Right');
+hh=text(-1.15,0,'Right');
 set(hh,'Rotation',90,'HorizontalAlignment','Center');
-hh=text(0,+1.1,'Dorsal');
+hh=text(0,+1.15,'Ventral');
 set(hh,'HorizontalAlignment','Center');
 
 print -r150 -dpng if_lung_injury01.png
