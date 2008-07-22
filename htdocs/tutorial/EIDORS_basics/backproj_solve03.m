c8e497638ac1a5aa4dabf5ac566a7f2a8efe2044
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-07-22 20:26:02 +0000

    fix colours and fix backprojection direction
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1517 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/EIDORS_basics/backproj_solve03.m
+++ htdocs/tutorial/EIDORS_basics/backproj_solve03.m
@@ -1,4 +1,4 @@
-% $Id: backproj_solve03.m,v 1.3 2007-10-22 21:34:39 aadler Exp $
+% $Id: backproj_solve03.m,v 1.4 2008-07-22 20:26:02 aadler Exp $
 
 % Gauss Newton Solver
 inv_GN= eidors_obj('inv_model','GN_solver','fwd_model', img.fwd_model);
@@ -6,9 +6,10 @@ inv_GN.reconst_type= 'difference';
 inv_GN.solve= @np_inv_solve;
 inv_GN.RtR_prior= @noser_image_prior;
 inv_GN.jacobian_bkgnd.value= 1;
-inv_GN.hyperparameter.value= 1e-3;
+inv_GN.hyperparameter.value= 0.03;
 
 imgr= inv_solve(inv_GN, vh,vi);
+imgr.calc_colours.ref_level=0;
 subplot(131)
 show_fem(imgr);
 axis equal; axis off
@@ -20,15 +21,15 @@ inv_BP.solve= @backproj_solve;
 inv_BP.backproj_solve.type= 'naive';
 
 imgr= inv_solve(inv_BP, vh,vi);
-subplot(132)
-show_fem(imgr);
+imgr.calc_colours.ref_level=0;
+subplot(132); show_fem(imgr);
 axis equal; axis off
 
 inv_BP.backproj_solve.type= 'simple_filter';
 
 imgr= inv_solve(inv_BP, vh,vi);
-subplot(133)
-show_fem(imgr);
+imgr.calc_colours.ref_level=0;
+subplot(133); show_fem(imgr);
 axis equal; axis off
 
 print -r125 -dpng backproj_solve03a.png;
