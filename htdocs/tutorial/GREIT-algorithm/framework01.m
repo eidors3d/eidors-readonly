f1286dfe58c1be2fb635dcc430daa489f4770427
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-07-26 16:01:03 +0000

    Expand Jacobian calculation
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1538 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/GREIT-algorithm/framework01.m
+++ htdocs/tutorial/GREIT-algorithm/framework01.m
@@ -8,28 +8,6 @@ xvec= linspace( xyzmin(1), xyzmax(1), pixel_grid+1);
 yvec= linspace( xyzmin(2), xyzmax(2), pixel_grid+1);
 zvec= [0.6*xyzmin(3)+0.4*xyzmax(3), 0.4*xyzmin(3)+0.6*xyzmax(3)];
 
-[rmdl,c2f] = mk_grid_model(ng_mdl_16x1_fine, xvec, yvec, zvec);
-
-% CALCULATE JACOBIAN AND SAVE IT
-
-img= eidors_obj('image','GREIT-ng_mdl');
-img.fwd_model= ng_mdl_16x1_fine;
-img.fwd_model.coarse2fine = c2f;
-img.rec_model= rmdl;
-img.elem_data= ones(size(img.fwd_model,1));
-
-% ADJACENT STIMULATION PATTERNS
-img.fwd_model.stimulation= mk_stim_patterns(16, 1, ...
-             [0,1],[0,1], {'do_redundant', 'no_meas_current'}, 1);
-
-% SOLVERS
-img.fwd_model.system_mat= @aa_calc_system_mat;
-img.fwd_model.solve=      @aa_fwd_solve;
-img.fwd_model.jacobian=   @aa_calc_jacobian;
-
-J= calc_jacobian(img);
-save Jacobian J
-
 % SHOW MODEL CORRESPONDENCE
 
 clf;
