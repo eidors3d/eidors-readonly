62fbaea8507d39971330fdb74044fa63968f8143
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-07-26 17:33:46 +0000

    modify exponent
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1545 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/GREIT-algorithm/framework03.m
+++ htdocs/tutorial/GREIT-algorithm/framework03.m
@@ -4,7 +4,7 @@ load GREIT_Jacobian_ng_mdl_fine J map
 % Remove space outside FEM model
 J= J(:,map);
 % inefficient code - but for clarity
-diagJtJ = diag(J'*J);
+diagJtJ = diag(J'*J) .^ (0.7);
 
 R= spdiags( diagJtJ,0, length(diagJtJ), length(diagJtJ));
 
