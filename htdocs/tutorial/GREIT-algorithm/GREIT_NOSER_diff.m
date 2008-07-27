7ff63445b6e399f952b224a19d49b067ec375697
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-07-27 03:30:17 +0000

    norm algs
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1560 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/GREIT-algorithm/GREIT_NOSER_diff.m
+++ htdocs/tutorial/GREIT-algorithm/GREIT_NOSER_diff.m
@@ -1,4 +1,4 @@
-function [img,map]= GREIT_NOSER_ndiff( ref_meas, reconst_meas )
+function [img,map]= GREIT_NOSER_diff( ref_meas, reconst_meas )
 % Reconstruct GREIT images using NOSER algorithm
 %
 % (C) 2008 Andy Adler. Licenced under GPL v2 or v3
@@ -9,7 +9,7 @@ function [img,map]= GREIT_NOSER_ndiff( ref_meas, reconst_meas )
    % Expand ref_meas to the full size of reconst_meas
    num_meas = size(reconst_meas,2);
    ref_meas = ref_meas * ones(1,num_meas);
-   dv = ( reconst_meas - ref_meas ) ./ ref_meas; % CHANGE IS HERE:
+   dv =  reconst_meas - ref_meas;
 
    % reconst image
    ds = RM*dv;
@@ -17,8 +17,7 @@ function [img,map]= GREIT_NOSER_ndiff( ref_meas, reconst_meas )
    img= reshape(ds, 32,32,num_meas);
 
 function [RM,map] = calc_NOSER_RM
-   [J,map,vbkgnd] = GREIT_Jacobian_cyl;
-   J = J ./ (vbkgnd*ones(size(J,2))); % Normalized Jacobian
+   [J,map] = GREIT_Jacobian_cyl;
    RM = zeros(size(J'));
  
    % Remove space outside FEM model
@@ -27,5 +26,5 @@ function [RM,map] = calc_NOSER_RM
    diagJtJ = diag(J'*J);
    R= spdiags( diagJtJ,0, length(diagJtJ), length(diagJtJ));
 
-   hp = 3.0;
+   hp = 1.0;
    RM(map,:)= (J'*J + hp^2*R)\J';
