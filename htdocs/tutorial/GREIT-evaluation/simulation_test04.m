ba29d3a1fa5b80e4ae7915c655e393d25eebb4a2
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-07-27 22:07:30 +0000

    Refactor algorithms list
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1574 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/GREIT-evaluation/simulation_test04.m
+++ htdocs/tutorial/GREIT-evaluation/simulation_test04.m
@@ -1,4 +1,5 @@
-% Reconstruct some images $Id$
+% Calc Parameters $Id$
+
 imb.calc_colours.ref_level = 0; % select colour output
 imb.calc_colours.greylev   = 0.01; % black backgnd
 bkgnd= [.1,.5,.1]; imb.calc_colours.backgnd   = bkgnd;
@@ -16,15 +17,4 @@ ind= sub2ind([32,32],yc,xc);
 algs = get_list_of_algs;
 for i= 1:length(algs)
    img = feval(algs{i}, vh, vi(:,idx) );
-   hmi = calc_hm_set( img, 0.5 )+1;
-   qmi = calc_hm_set( img, 0.25 )+1;
 
-   imc= calc_colours(img, imb);
-   imc(out) = 1; hmi(out) = 3; qmi(out) = 3; % background
-   imc(ind) = 1; hmi(ind) = 3; qmi(ind) = 3; % target
-
-   imwrite(imc,colormap, sprintf('simulation_test03_%d.png',i),'png')
-   clrmap = [0,0,0;1,1,1;bkgnd];
-   imwrite(hmi,clrmap, sprintf('simulation_test03_h%d.png',i),'png')
-   imwrite(qmi,clrmap, sprintf('simulation_test03_q%d.png',i),'png')
-end
