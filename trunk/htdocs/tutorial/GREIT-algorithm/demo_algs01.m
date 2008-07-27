7905d606980d221b9da3a4a6d6aad1fd7f9649b3
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-07-27 00:12:33 +0000

    demo for algs

--- trunk/htdocs/tutorial/GREIT-algorithm/demo_algs01.m
+++ trunk/htdocs/tutorial/GREIT-algorithm/demo_algs01.m
@@ -1,10 +1,8 @@
-% Demo Data $Id$
+% Tank Data $Id:$
 
-% EXCLUDE MEASURES AT ELECTRODES
-[x,y]= meshgrid(1:16,1:16);
-idx= abs(x-y)>1 & abs(x-y)<15;
+% exclude measures at electrodes
+[x,y]= meshgrid(1:16,1:16); idx= abs(x-y)>1 & abs(x-y)<15;
 
-% LOAD SOME TEST DATA
 load iirc_data_2006
-v_reference= - real(v_reference(idx,:));
-v_rotate   = - real(v_rotate(idx,:));
+vh1= - real(v_reference(idx,1));
+vi1= - real(v_rotate(idx,1));
