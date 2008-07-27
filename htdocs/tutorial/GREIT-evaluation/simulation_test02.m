6b76e4d512f875a5122aaae4c82a116a21e9f014
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-07-27 17:38:34 +0000

    show simulations
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1567 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/GREIT-evaluation/simulation_test02.m
+++ htdocs/tutorial/GREIT-evaluation/simulation_test02.m
@@ -2,20 +2,17 @@
 load ng_mdl_16x1_fine;
 show_fem(ng_mdl_16x1_fine)
 crop_model(gca, inline('x-z<-15','x','y','z'))
-      view(-90,70)
-%     view(-90,20)
+      view(-90,50)
 
 hold on
-[xs,ys,zs]=sphere(10); spclr= [0,1,1];
-for i=1:5:size(xyzr_pt,2);
-   xp=xyzr_pt(1,i);
-   yp=xyzr_pt(2,i);
-   zp=xyzr_pt(3,i);
-   rp=xyzr_pt(4,i);
+[xs,ys,zs]=sphere(10); spclr= [0,.5,.5];
+for i=1:1:size(xyzr_pt,2);
+   xp=xyzr_pt(1,i); yp=xyzr_pt(2,i); zp=xyzr_pt(3,i); rp=xyzr_pt(4,i);
    hh=surf(rp*xs+xp, rp*ys+yp, rp*zs+zp);
-   set(hh,'EdgeColor',spclr,'FaceColor',spclr);
+   set(hh,'EdgeColor',[0,.4,.4],'FaceColor',[0,.8,.8]);
+   hh=text(xp,yp,zp+1,num2str(i)); 
+   set(hh,'FontSize',7,'FontWeight','bold','HorizontalAlignment','center');
 end
 hold off
 
-% print -dpng -r100 simulation_test02a.png
-
+print -dpng -r75 simulation_test02a.png
