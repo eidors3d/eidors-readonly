fb8688f0620ad22c9e77c0f696f83ab1c810f8fc
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-07-27 20:37:12 +0000

    Calculate sets
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1572 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/GREIT-evaluation/calc_hm_set.m
+++ htdocs/tutorial/GREIT-evaluation/calc_hm_set.m
@@ -1,19 +1,19 @@
-function hm_img = calc_hm_set(img)
+function hm_img = calc_hm_set(img,frac)
 % hm_img= CALC_HA_SET(img)
 % hm_img is 32x32xNimg. It is 1 inside the Half Ampl Set
+% frac is the fraction of maximum (0.5 or 0.25)
 
 % (C) 2008 Andy Adler. Licenced under GPL v2 or v3
 % $Id$
 
-[x,y]=meshgrid(linspace(-1,1,32),linspace(-1,1,32));
-map = x.^2+y.^2<1.1;
+[x,y]=meshgrid(linspace(-1,1,32),linspace(-1,1,32)); map = x.^2+y.^2<1.1;
 
 hm_img = logical(zeros(size(img)));
 for i=1:size(img,3);
    imi = img(:,:,i); imi= imi(map);
-   if max(imi) < min(imi); imi=-imi; end
+   if abs(max(imi)) < abs(min(imi)); imi=-imi; end
 
    hmi= logical(zeros(32));
-   hmi(map) = imi >= max(imi)/2;
+   hmi(map) = imi >= max(imi) * frac;
    hm_img(:,:,i) = hmi;
 end
--- htdocs/tutorial/GREIT-evaluation/simulation_test03.m
+++ htdocs/tutorial/GREIT-evaluation/simulation_test03.m
@@ -6,7 +6,7 @@ algs = {'GREIT_Sheffield_backproj', ...
 
 imb.calc_colours.ref_level = 0; % select colour output
 imb.calc_colours.greylev   = 0.01; % black backgnd
-imb.calc_colours.backgnd   = [.1,.5,.1]; %grey
+bkgnd= [.1,.5,.1]; imb.calc_colours.backgnd   = bkgnd;
 
 idx= 157;
 
@@ -15,12 +15,20 @@ xc=-xyzr_pt(1,idx) + xyzr_pt(4,idx) * sin(phi); xc= round(xc*15.5 + 16.5);
 yc=-xyzr_pt(2,idx) + xyzr_pt(4,idx) * cos(phi); yc= round(yc*15.5 + 16.5);
 ind= sub2ind([32,32],yc,xc);
 
+% Use this Map for reconstruction shape
+[x,y]=meshgrid(linspace(-1,1,32),linspace(-1,1,32)); out = x.^2+y.^2>1.1;
+
 for i= 1:length(algs)
-   [img,map] = feval(algs{i}, vh, vi(:,idx) );
+   img = feval(algs{i}, vh, vi(:,idx) );
+   hmi = calc_hm_set( img, 0.5 )+1;
+   qmi = calc_hm_set( img, 0.25 )+1;
 
    imc= calc_colours(img, imb);
-   imc(~map) = 1; % background
-   imc(ind) = 1;
+   imc(out) = 1; hmi(out) = 3; qmi(out) = 3; % background
+   imc(ind) = 1; hmi(ind) = 3; qmi(ind) = 3; % target
 
    imwrite(imc,colormap, sprintf('simulation_test03_%d.png',i),'png')
+   clrmap = [0,0,0;1,1,1;bkgnd];
+   imwrite(hmi,clrmap, sprintf('simulation_test03_h%d.png',i),'png')
+   imwrite(qmi,clrmap, sprintf('simulation_test03_q%d.png',i),'png')
 end
