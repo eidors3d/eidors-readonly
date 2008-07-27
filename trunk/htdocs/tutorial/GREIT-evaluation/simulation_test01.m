52a90cec601ccc71b89f08783e782572f32d9d06
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-07-27 20:19:26 +0000

    fixes

--- trunk/htdocs/tutorial/GREIT-evaluation/simulation_test01.m
+++ trunk/htdocs/tutorial/GREIT-evaluation/simulation_test01.m
@@ -10,6 +10,6 @@ else
            ng_mdl_16x1_fine, params, @simulation_radmove);
 
    %Change because of geometry of model is at 90 deg and radius is 15
-   xyzr_pt= xyzr_pt([2,1,3],:)/15; 
+   xyzr_pt= xyzr_pt([2,1,3,4],:)/15; 
    save sim_radmove_homog vh vi xyzr_pt
 end
