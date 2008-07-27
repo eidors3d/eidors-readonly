f29daf746d9546c3fd83fb2bb9a9438cefbb73dc
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-07-27 00:29:10 +0000

    show output

--- trunk/htdocs/tutorial/GREIT-algorithm/demo_algs02.m
+++ trunk/htdocs/tutorial/GREIT-algorithm/demo_algs02.m
@@ -2,10 +2,10 @@
 imb=  mk_common_model('c2c',16);
 
 img= calc_jacobian_bkgnd( imb );
-v(2).vh= fwd_solve( img );
+vv= fwd_solve( img ); v(2).vh= vv.meas;
 img.elem_data([25,37,49:50,65:66,81:83,101:103,121:124])= 1.1;
 img.elem_data([95,98:100,79,80,76,63,64,60,48,45,36,33,22])= 1.1;
-v(2).vi= fwd_solve( img );
+vv= fwd_solve( img ); v(2).vi= vv.meas;
 
 subplot(221);show_fem(img);
 axis square; axis off
--- trunk/htdocs/tutorial/GREIT-algorithm/demo_algs04.m
+++ trunk/htdocs/tutorial/GREIT-algorithm/demo_algs04.m
@@ -1,9 +1,23 @@
 % Demo algorithms $Id$
 
+% SELECT ALGORITHMS
 algs = {'GREIT_NOSER_ndiff', ...
         'GREIT_Sheffield_backproj' };
 
-alg = algs{1};
+% SELECT COLOUR OUTPUT
+imb.calc_colours.ref_level = 0;
+imb.calc_colours.greylev   = 0.01; % black backgnd
+imb.calc_colours.backgnd   = [.5,.5,.5]; %grey
 
-k=1;
-[img,map] = feval(alg, v(k).vh, v(k).vi );
+
+for i= 1:length(algs)
+   for k= 1:3
+      [img,map] = feval(algs{i}, v(k).vh, v(k).vi );
+
+      imc= calc_colours(img, imb);
+      imc(~map) = 1; % background
+
+      imwrite(imc,colormap, ...
+              sprintf('demo_algs04_%d%d.png',i,k),'png')
+    end
+end
