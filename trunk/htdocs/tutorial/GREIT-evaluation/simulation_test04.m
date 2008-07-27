2bcab0048669cad4a409a5721a8eabc7c1f9f6d2
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-07-27 22:48:19 +0000

    test simulations and parameters

--- trunk/htdocs/tutorial/GREIT-evaluation/simulation_test04.m
+++ trunk/htdocs/tutorial/GREIT-evaluation/simulation_test04.m
@@ -1,20 +1,16 @@
 % Calc Parameters $Id$
 
-imb.calc_colours.ref_level = 0; % select colour output
-imb.calc_colours.greylev   = 0.01; % black backgnd
-bkgnd= [.1,.5,.1]; imb.calc_colours.backgnd   = bkgnd;
+subplot(421); algs = get_list_of_algs;
 
-idx= 157;
-
-phi = linspace(0,2*pi,10);
-xc=-xyzr_pt(1,idx) + xyzr_pt(4,idx) * sin(phi); xc= round(xc*15.5 + 16.5);
-yc=-xyzr_pt(2,idx) + xyzr_pt(4,idx) * cos(phi); yc= round(yc*15.5 + 16.5);
-ind= sub2ind([32,32],yc,xc);
-
-% Use this Map for reconstruction shape
-[x,y]=meshgrid(linspace(-1,1,32),linspace(-1,1,32)); out = x.^2+y.^2>1.1;
-
-algs = get_list_of_algs;
 for i= 1:length(algs)
-   img = feval(algs{i}, vh, vi(:,idx) );
-
+   img = feval(algs{i}, vh, vi );
+   param= GREIT_sim_params( img, xyzr_pt);
+   for j=1:size(param,1)
+      plot(param(j,:)); set(gca,'XTickLabel',[])
+      if     j==1; set(gca,'YLim',[0,1.2]);
+      elseif j==2; set(gca,'YLim',[0,0.2]);
+      elseif j==3; set(gca,'YLim',[0,0.4]);
+      end
+      print('-dpng','-r100',sprintf('simulation_test04_%d%d.png',i,j));
+   end
+end
