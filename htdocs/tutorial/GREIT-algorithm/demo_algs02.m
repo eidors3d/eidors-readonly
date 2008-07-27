dcb5a484e65d5cde544b13c1756f4cbc2affcce9
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-07-27 00:12:33 +0000

    demo for algs
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1556 82006732-fb7e-4a22-9f28-86a212cce563

---
+++ htdocs/tutorial/GREIT-algorithm/demo_algs02.m
@@ -0,0 +1,12 @@
+% Simulated objects
+imb=  mk_common_model('c2c',16);
+
+img= calc_jacobian_bkgnd( imb );
+vh2= fwd_solve( img );
+img.elem_data([25,37,49:50,65:66,81:83,101:103,121:124])= 1.1;
+img.elem_data([95,98:100,79,80,76,63,64,60,48,45,36,33,22])= 1.1;
+vi2= fwd_solve( img );
+
+subplot(221);show_fem(img);
+axis square; axis off
+print -r75 -dpng demo_algs02a.png;
---
+++ htdocs/tutorial/GREIT-algorithm/demo_algs03.m
@@ -0,0 +1,5 @@
+% Human breathing $Id:$
+
+load montreal_data_1995
+vh3 = zc_resp(idx,1);
+vi3 = zc_resp(idx,22);
