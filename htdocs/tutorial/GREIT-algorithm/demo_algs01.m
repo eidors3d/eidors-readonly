2b34eb4edbd8b861b23adbf3995659591fa135b8
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-07-27 00:19:17 +0000

    show some output
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1557 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/GREIT-algorithm/demo_algs01.m
+++ htdocs/tutorial/GREIT-algorithm/demo_algs01.m
@@ -1,8 +1,8 @@
-% Tank Data $Id:$
+% Tank Data $Id$
 
 % exclude measures at electrodes
 [x,y]= meshgrid(1:16,1:16); idx= abs(x-y)>1 & abs(x-y)<15;
 
 load iirc_data_2006
-vh1= - real(v_reference(idx,1));
-vi1= - real(v_rotate(idx,1));
+v(1).vh= - real(v_reference(idx,1));
+v(1).vi= - real(v_rotate(idx,1));
