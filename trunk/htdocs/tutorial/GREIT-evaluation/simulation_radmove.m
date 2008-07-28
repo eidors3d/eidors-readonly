bd77a2f826aaa775aae09497ea74ee981902a38d
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-07-28 10:09:23 +0000

    Support 3D and 2D simulations

--- trunk/htdocs/tutorial/GREIT-evaluation/simulation_radmove.m
+++ trunk/htdocs/tutorial/GREIT-evaluation/simulation_radmove.m
@@ -1,8 +1,7 @@
 % Radial Movement - $Id$  
-% fcn defined by simulate_3d_movement
 function [xp,yp,zp]= simulation_radmove(f_frac, radius, z0,zt);
    rp= f_frac*radius; 
    cv= 2*pi*f_frac * 73; % move 8 times around circle
    xp= rp * cos(cv);
    yp= rp * sin(cv);
-   zp = mean([zt,z0]);
+   if nargin==4; zp = mean([zt,z0]); else; zp= 0; end
