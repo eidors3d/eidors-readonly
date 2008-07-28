efbc43368f255e3b64949b870997f2335c4c99bb
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-07-28 09:19:55 +0000

    framework
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1576 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/GREIT-algorithm/demo_algs04.m
+++ htdocs/tutorial/GREIT-algorithm/demo_algs04.m
@@ -1,16 +1,10 @@
 % Demo algorithms $Id$
 
-% SELECT ALGORITHMS
-algs = {'GREIT_Sheffield_backproj', ...
-        'GREIT_NOSER_ndiff', ...
-        'GREIT_NOSER_diff', ...
-       };
+algs= get_list_of_algs;
 
-% SELECT COLOUR OUTPUT
-imb.calc_colours.ref_level = 0;
+imb.calc_colours.ref_level = 0; % select colour output
 imb.calc_colours.greylev   = 0.01; % black backgnd
 imb.calc_colours.backgnd   = [.5,.5,.5]; %grey
-
 
 for i= 1:length(algs)
    for k= 1:4
@@ -19,7 +13,6 @@ for i= 1:length(algs)
       imc= calc_colours(img, imb);
       imc(~map) = 1; % background
 
-      imwrite(imc,colormap, ...
-              sprintf('demo_algs04_%d%d.png',i,k),'png')
+      imwrite(imc,colormap, sprintf('demo_algs04_%d%d.png',i,k),'png')
     end
 end
