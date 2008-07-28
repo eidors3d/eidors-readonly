c1f5071b002984dfa0396efdef846c7e0dda63ce
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-07-28 10:09:23 +0000

    Support 3D and 2D simulations
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1579 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/GREIT-evaluation/simulation_test01.m
+++ htdocs/tutorial/GREIT-evaluation/simulation_test01.m
@@ -1,15 +1,22 @@
 % simulate radial movement $Id$
+use_3d_model = 0; % 3D FEM is not working well
+
 if exist('sim_radmove_homog.mat','file')
    load sim_radmove_homog.mat vh vi xyzr_pt
 else
-   load ng_mdl_16x1_fine;
    params= [0.9,0.05,0.5,0.5]; %max_posn, targ_rad, z_0, z_t
-   ng_mdl_16x1_fine.stimulation = mk_stim_patterns(16,1,'{ad}','{ad}', ...
-              {'no_meas_current'}, 1);
-   [vh,vi,xyzr_pt]= simulate_3d_movement(200, ...
-           ng_mdl_16x1_fine, params, @simulation_radmove);
+   stim_pat = mk_stim_patterns(16,1,'{ad}','{ad}', {'no_meas_current'}, 1);
+   if use_3d_model;
+      load ng_mdl_16x1_fine; fmdl= ng_mdl_16x1_fine;
+      fmdl.stimulation = stim_pat;
+      [vh,vi,xyzr_pt]= simulate_3d_movement(200, fmdl, params, @simulation_radmove);
+      xyzr_pt= xyzr_pt([2,1,3,4],:)/15; %Change: mdl geometry at 90 deg; radius is 15
+   else;
+      imdl = mk_common_model('f2d3c',16); fmdl= imdl.fwd_model;
+      fmdl.stimulation = stim_pat;
+      [vh,vi,xyzr_pt]= simulate_2d_movement(200, fmdl, params, @simulation_radmove);
+      xyzr_pt = [-1,0,0;0,1,0;0,0,0;0,0,1]*xyzr_pt;
+   end
 
-   %Change because of geometry of model is at 90 deg and radius is 15
-   xyzr_pt= xyzr_pt([2,1,3,4],:)/15; 
    save sim_radmove_homog vh vi xyzr_pt
 end
