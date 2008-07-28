abf34255e7a14f70924564fd1e855a4bdcb16a83
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-07-28 11:18:36 +0000

    2d movement simulation code
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1585 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/GREIT-evaluation/simulate_2d_test02.m
+++ htdocs/tutorial/GREIT-evaluation/simulate_2d_test02.m
@@ -6,4 +6,4 @@ stim_pat= mk_stim_patterns(n_elec,1,'{ad}','{ad}',{},1);
 smdl.stimulation= stim_pat;
 himg= eidors_obj('image','','fwd_model',smdl, 'elem_data',ones(size(smdl.elems,1),1) );
 
-vh= fwd_solve(himg);
+vh= fwd_solve(himg); vh= vh.meas;
