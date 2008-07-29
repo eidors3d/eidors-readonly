c6faf32a2068e25702bfcfe043132a27eab5fc2f
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-07-29 11:44:18 +0000

    noise figure

--- trunk/htdocs/tutorial/GREIT-evaluation/simulation_test04.m
+++ trunk/htdocs/tutorial/GREIT-evaluation/simulation_test04.m
@@ -5,11 +5,22 @@ subplot(421); algs = get_list_of_algs;
 for i= 1:length(algs)
    img = feval(algs{i}, vh, vi );
    param= GREIT_sim_params( img, xyzr_pt);
+
+   noise = 0.01*std(vh)*randn(208,1000);
+   snr_y = mean(abs(vi-vh*ones(1,size(vi,2))),1) / mean(std(noise,[],1),2); 
+
+   im_n= feval(algs{i}, vh, vh*ones(1,size(noise,2)) + noise);
+   snr_x = mean(mean(abs(img),1),2) / mean(abs(im_n(:)));
+   param= [param; [snr_x(:)./snr_y(:)]' ];
+
    for j=1:size(param,1)
       plot(param(j,:)); set(gca,'XTickLabel',[])
-      if     j==1; set(gca,'YLim',[0,1.2]);
-      elseif j==2; set(gca,'YLim',[0,0.2]);
+      if     j==1; set(gca,'YLim',[0,1.3]);
+      elseif j==2; set(gca,'YLim',[-0.05,0.2]);
       elseif j==3; set(gca,'YLim',[0,0.4]);
+      elseif j==4; set(gca,'YLim',[0,0.5]);
+      elseif j==5; set(gca,'YLim',[0,1.0]);
+      elseif j==6; set(gca,'YLim',[0,4.0]);
       end
       print('-dpng','-r100',sprintf('simulation_test04_%d%d.png',i,j));
    end
