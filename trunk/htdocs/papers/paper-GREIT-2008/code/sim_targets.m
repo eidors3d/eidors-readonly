4f7e89eb98aef0ee2007d46cbff03aa2ced48e45
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-11-15 22:31:47 +0000

    appears to be working

--- trunk/htdocs/papers/paper-GREIT-2008/code/sim_targets.m
+++ trunk/htdocs/papers/paper-GREIT-2008/code/sim_targets.m
@@ -7,18 +7,30 @@ function sim_targets( savefile )
 
    params= [0.9,0.05,0.5,0.5]; %max_posn, targ_rad, z_0, z_t
 
-   [vh,vi,xyzr_pt]= simulate_3d_movement(200, fmdl, params, @simulation_radmove);
+   [vh,vi,xyzr_pt]= simulate_3d_movement(200, fmdl, params, ...
+         @simulation_random);
+%        @simulation_radmove);
+
    %Change: mdl geometry at 90 deg; radius is 15
    xyzr_pt= xyzr_pt([2,1,3,4],:)/15;
 
    save(savefile, 'vh','vi','xyzr_pt');
+
+function [xp,yp,zp]= simulation_random(f_frac, radius, z0,zt);
+   lim = (2*radius*(1-0.05-0.02))^2;
+   while 1
+     xp = 2*radius*(rand(1)-0.5);
+     yp = 2*radius*(rand(1)-0.5);
+     if xp^2 + yp^2 < lim; break;end
+   end
+   zp = mean([zt,z0]);
 
 function [xp,yp,zp]= simulation_radmove(f_frac, radius, z0,zt);
    rp= f_frac*radius; 
    cv= 2*pi*f_frac * 73;
    xp= rp * cos(cv);
    yp= rp * sin(cv);
-   if nargin==4; zp = mean([zt,z0]); else; zp= 0; end
+   zp = mean([zt,z0]);
 
 return
 % OLD CODE FOR 2D Simulations
