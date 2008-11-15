56a7ebb02aa3041bfdc1d311b3af0f5e205e18c0
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-11-15 16:04:25 +0000

    sim_targets

--- trunk/htdocs/papers/paper-GREIT-2008/code/sim_targets.m
+++ trunk/htdocs/papers/paper-GREIT-2008/code/sim_targets.m
@@ -1,17 +1,20 @@
 % simulate radial movement $Id$
-use_3d_model = 1;
 
 params= [0.9,0.05,0.5,0.5]; %max_posn, targ_rad, z_0, z_t
 stim_pat = mk_stim_patterns(16,1,'{ad}','{ad}', {'no_meas_current'}, 1);
-if use_3d_model;
-   load ng_tank; fmdl.stimulation = stim_pat;
-   [vh,vi,xyzr_pt]= simulate_3d_movement(200, fmdl, params, @simulation_radmove);
-   xyzr_pt= xyzr_pt([2,1,3,4],:)/15; %Change: mdl geometry at 90 deg; radius is 15
-else;
+
+load ng_cyl_mdl.mat
+fmdl.stimulation = stim_pat;
+
+[vh,vi,xyzr_pt]= simulate_3d_movement(200, fmdl, params, @simulation_radmove);
+%Change: mdl geometry at 90 deg; radius is 15
+xyzr_pt= xyzr_pt([2,1,3,4],:)/15;
+
+save sim_targets vh vi xyzr_pt
+
+return
+% OLD CODE FOR 2D Simulations
    imdl = mk_common_model('f2d3c',16); fmdl= imdl.fwd_model;
    fmdl.stimulation = stim_pat;
    [vh,vi,xyzr_pt]= simulate_2d_movement(200, fmdl, params, @simulation_radmove);
    xyzr_pt = [-1,0,0;0,1,0;0,0,0;0,0,1]*xyzr_pt;
-end
-
-save sim_targets vh vi xyzr_pt
