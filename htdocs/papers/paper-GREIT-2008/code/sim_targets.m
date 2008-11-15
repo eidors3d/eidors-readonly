77ed13c3b01e8d1b76f7e48a3a15f6dcc8033030
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-11-15 19:54:48 +0000

    put model inside sim_targets
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1653 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/papers/paper-GREIT-2008/code/sim_targets.m
+++ htdocs/papers/paper-GREIT-2008/code/sim_targets.m
@@ -1,17 +1,24 @@
 function sim_targets( savefile )
 % simulate radial movement $Id$
 
-params= [0.9,0.05,0.5,0.5]; %max_posn, targ_rad, z_0, z_t
-stim_pat = mk_stim_patterns(16,1,'{ad}','{ad}', {'no_meas_current'}, 1);
+   load ng_cyl_mdl.mat
+   stim_pat = mk_stim_patterns(16,1,'{ad}','{ad}', {'no_meas_current'}, 1);
+   fmdl.stimulation = stim_pat;
 
-load ng_cyl_mdl.mat
-fmdl.stimulation = stim_pat;
+   params= [0.9,0.05,0.5,0.5]; %max_posn, targ_rad, z_0, z_t
 
-[vh,vi,xyzr_pt]= simulate_3d_movement(200, fmdl, params, @simulation_radmove);
-%Change: mdl geometry at 90 deg; radius is 15
-xyzr_pt= xyzr_pt([2,1,3,4],:)/15;
+   [vh,vi,xyzr_pt]= simulate_3d_movement(200, fmdl, params, @simulation_radmove);
+   %Change: mdl geometry at 90 deg; radius is 15
+   xyzr_pt= xyzr_pt([2,1,3,4],:)/15;
 
-save(savefile, 'vh','vi','xyzr_pt');
+   save(savefile, 'vh','vi','xyzr_pt');
+
+function [xp,yp,zp]= simulation_radmove(f_frac, radius, z0,zt);
+   rp= f_frac*radius; 
+   cv= 2*pi*f_frac * 73;
+   xp= rp * cos(cv);
+   yp= rp * sin(cv);
+   if nargin==4; zp = mean([zt,z0]); else; zp= 0; end
 
 return
 % OLD CODE FOR 2D Simulations
