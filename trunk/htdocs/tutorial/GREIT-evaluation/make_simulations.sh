35b5f59f03659a6e7b0b5690d428a142654f1c3e
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-11-22 17:49:24 +0000

    upload the file version

--- trunk/htdocs/tutorial/GREIT-evaluation/make_simulations.sh
+++ trunk/htdocs/tutorial/GREIT-evaluation/make_simulations.sh
@@ -3,12 +3,12 @@
 
 nohup \
 matlab -nodisplay \
- -r "run ../../../eidors/startup;  \
+ -r "run ../../../eidors/startup;  tic; \
      params= [0.9,0.05,0.5,0.5]; \
      stim_pat = mk_stim_patterns(16,1,'{ad}','{ad}', {'no_meas_current'}, 1); \
      load ng_tank_75; fmdl.stimulation = stim_pat; \
      [vh,vi,xyzr_pt]= simulate_3d_movement(200, fmdl, params, @simulation_radmove); \
      xyzr_pt= xyzr_pt([2,1,3,4],:)/15; \
-     save sim_radmove_homog vh vi xyzr_pt \
+     save sim_radmove_homog vh vi xyzr_pt; \
 \
-     disp('FINISHED'); exit;"
+     toc; disp('FINISHED'); exit;"
