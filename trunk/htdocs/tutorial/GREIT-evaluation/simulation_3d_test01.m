ed0cfba6149fda138e850f12d2a1cb3d68bc0936
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-12-02 11:43:20 +0000

    Modify code and test images to make them larger

--- trunk/htdocs/tutorial/GREIT-evaluation/simulation_3d_test01.m
+++ trunk/htdocs/tutorial/GREIT-evaluation/simulation_3d_test01.m
@@ -8,12 +8,12 @@ else
    stim_pat = mk_stim_patterns(16,1,'{ad}','{ad}', {'no_meas_current'}, 1);
    if use_3d_model;
       load ng_tank; fmdl.stimulation = stim_pat;
-      [vh,vi,xyzr_pt]= simulate_3d_movement(200, fmdl, params, @simulation_radmove);
+      [vh,vi,xyzr_pt]= simulate_3d_movement(500, fmdl, params, @simulation_radmove);
       xyzr_pt= xyzr_pt([2,1,3,4],:)/15; %Change: mdl geometry at 90 deg; radius is 15
    else;
       imdl = mk_common_model('f2d3c',16); fmdl= imdl.fwd_model;
       fmdl.stimulation = stim_pat;
-      [vh,vi,xyzr_pt]= simulate_2d_movement(200, fmdl, params, @simulation_radmove);
+      [vh,vi,xyzr_pt]= simulate_2d_movement(500, fmdl, params, @simulation_radmove);
       xyzr_pt = [-1,0,0;0,1,0;0,0,0;0,0,1]*xyzr_pt;
    end
 
