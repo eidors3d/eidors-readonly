3a81e06bf5872e33303b900a5beada8ee0b29aaf
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-12-03 14:02:27 +0000

    added properties / split sim data

--- trunk/htdocs/papers/paper-GREIT-2008/code/make_sim_testdata.m
+++ trunk/htdocs/papers/paper-GREIT-2008/code/make_sim_testdata.m
@@ -1,5 +1,5 @@
-function make_testdata( savefile )
-% Make test data and save in file
+function make_sim_testdata( savefile )
+% Make simulated test data and save in file
 % 
 % (C) Andy Adler 2008 $Id$
 % Licensed under GNU GPL v2 or v3
@@ -29,22 +29,6 @@ K=K+1;
 test_v(K).vi = test_v(K-1).vi + noise/norm(noise)*sig * 10^(-6/20);
 test_v(K).vh = test_v(K-1).vh;
 
-% PHANTOM DATA
-K=K+1;
-load iirc_data_2006
-test_v(K).vh= - real(v_reference(idx,1));
-test_v(K).vi= - real(v_rotate(idx,1));
-
-load montreal_data_1995
-K=K+1;
-test_v(K).vh= double( zc_h_demo4(idx) );
-test_v(K).vi= double( zc_demo4(idx,1) );
-K=K+1;
-test_v(K).vh= double( zc_h_demo4(idx) );
-test_v(K).vi= double( zc_demo4(idx,5) );
-K=K+1;
-test_v(K).vh= double( zc_h_demo4(idx) );
-test_v(K).vi= double( zc_demo4(idx,10) );
 
 params= [0.9,0.05,0.5,0.5]; %max_posn, targ_rad, z_0, z_t
 stim_pat = mk_stim_patterns(16,1,'{ad}','{ad}', {'no_meas_current'}, 1);
@@ -60,44 +44,6 @@ test_v(K).vi = vi(:,3);
 K=K+1;
 test_v(K).vh = vh;
 test_v(K).vi = vi(:,2);
-
-
-%  HUMAN TIDAL BREATHING
-
-% Electrodes on back
-
-K=K+1;
-test_v(K).vh = double( zc_resp(idx,1) );
-test_v(K).vi = double( zc_resp(idx,22) );
-
-% Contrib EIT data - PIG
-vv= eidors_readdata('p1130107.get');
-vh = mean(vv(idx,1:40),2);
-K=K+1;
-test_v(K).vh = vh;
-%test_v(K).vi = vv(idx,1345); % PEEP 5 end-expi
-test_v(K).vi = vv(idx,387); % PEEP 10 end-expi incr
-K=K+1;
-test_v(K).vh = vh;
-%test_v(K).vi = vv(idx,1145); % PEEP 15 end-expi
-test_v(K).vi = vv(idx,1257); % PEEP 10 end-expi decr
-
-% Contrib EIT data - PIG
-vv= eidors_readdata('1-control.RAW');
-vh= mean(vv(idx,[37, 42, 46, 50, 54, 58, 62, 66, 70, 75]),2);
-vi= mean(vv(idx,[35, 39, 43, 47, 51, 64, 68, 72, 76, 80]),2);
-K=K+1;
-test_v(K).vh = vh; % FRC
-test_v(K).vi = vi; % VT
-
-vv= eidors_readdata('1-injury.RAW');
-vh= mean(vv(idx,[37, 41, 45, 58, 62, 66, 70, 74, 82, 87]),2);
-vi= mean(vv(idx,[34, 47, 51, 55, 59, 63, 76, 80, 84, 88]),2);
-K=K+1;
-test_v(K).vh = vh; % FRC
-test_v(K).vi = vi; % VT
-
-
 
 save(savefile,'test_v');
 
