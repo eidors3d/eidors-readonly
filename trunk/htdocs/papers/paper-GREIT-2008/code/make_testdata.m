3be73ae5bb8698b5533f493b2196614c666c7b46
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2008-12-03 14:33:22 +0000

    fixed testdata

--- trunk/htdocs/papers/paper-GREIT-2008/code/make_testdata.m
+++ trunk/htdocs/papers/paper-GREIT-2008/code/make_testdata.m
@@ -9,28 +9,7 @@ function make_testdata( savefile )
 
 K=1;
 
-% SIMULATED OBJECTS
-imb=  mk_common_model('c2c',16);
-
-img= calc_jacobian_bkgnd( imb );
-vv= fwd_solve( img );
-test_v(K).vh= vv.meas;
-
-img.elem_data([25,37,49:50,65:66,81:83,101:103,121:124])= 1.1;
-img.elem_data([95,98:100,79,80,76,63,64,60,48,45,36,33,22])= 1.1;
-vv= fwd_solve( img );
-test_v(K).vi= vv.meas;
-
-% Noise
-sig = norm(test_v(1).vi - test_v(1).vh);
-randn('seed',50);noise = randn(size(test_v(1).vh));
-
-K=K+1;
-test_v(K).vi = test_v(K-1).vi + noise/norm(noise)*sig * 10^(-6/20);
-test_v(K).vh = test_v(K-1).vh;
-
 % PHANTOM DATA
-K=K+1;
 load iirc_data_2006
 test_v(K).vh= - real(v_reference(idx,1));
 test_v(K).vi= - real(v_rotate(idx,1));
@@ -45,21 +24,6 @@ test_v(K).vi= double( zc_demo4(idx,5) );
 K=K+1;
 test_v(K).vh= double( zc_h_demo4(idx) );
 test_v(K).vi= double( zc_demo4(idx,10) );
-
-params= [0.9,0.05,0.5,0.5]; %max_posn, targ_rad, z_0, z_t
-stim_pat = mk_stim_patterns(16,1,'{ad}','{ad}', {'no_meas_current'}, 1);
-load ng_cyl_mdl; fmdl.stimulation = stim_pat;
-[vh,vi,xyzr_pt]= simulate_3d_movement(4, fmdl, params, @simulation_radmove);
-%xyzr_pt= xyzr_pt([2,1,3,4],:)/15; %Change: mdl geometry at 90 deg; radius is 15
-K=K+1;
-test_v(K).vh = vh;
-test_v(K).vi = vi(:,4);
-K=K+1;
-test_v(K).vh = vh;
-test_v(K).vi = vi(:,3);
-K=K+1;
-test_v(K).vh = vh;
-test_v(K).vi = vi(:,2);
 
 
 %  HUMAN TIDAL BREATHING
