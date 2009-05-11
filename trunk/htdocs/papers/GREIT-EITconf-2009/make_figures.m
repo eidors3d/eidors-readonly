d88d32bc08b0c0ba08733a492b5bbba430f167d9
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2009-05-11 18:37:49 +0000

    make figures for paper

--- trunk/htdocs/papers/GREIT-EITconf-2009/make_figures.m
+++ trunk/htdocs/papers/GREIT-EITconf-2009/make_figures.m
@@ -1,6 +1,58 @@
-function make_figures
-   pig_data;
+function make_figures(figno)
 
+   switch figno
+     case 1;
+       imdl = mk_common_model('c2c2',16);
+       imdl.fwd_model.normalize_measurements= 0;
+       imdl.hyperparameter.value = 0.0439;
+
+     case 2;
+       imdl = mk_common_model('c2c2',16);
+       imdl.fwd_model.normalize_measurements= 1;
+       imdl.hyperparameter.value = 0.05;
+
+     case 3;
+       mdl = fmdl3d;
+
+
+     otherwise
+       error('huh?')
+   end
+   calc_noise_figure( set_tgts( imdl ));
+   fprintf('hp=%1.8g\n', choose_noise_figure( set_tgts(imdl, 0.79)) );
+
+   [eelv,eilv] = pig_data;
+    vh = eelv.p20; 
+    vi = eilv.p20; 
+
+
+   img= inv_solve(imdl, vh, vi);
+   show_fem(img);
+
+   
+function imdl = set_tgts( imdl, nf )
+   imdl00 = mk_common_model('a2c0',16);
+   meas_sel = imdl00.fwd_model.meas_select;   
+   [eelv,eilv] = pig_data;
+    vh = eelv.p20(meas_sel); 
+    vi = eilv.p20(meas_sel); 
+   imdl.hyperparameter.tgt_data.meas_t1= vh;
+   imdl.hyperparameter.tgt_data.meas_t2= vi;
+
+   if nargin==2;
+      imdl.hyperparameter.noise_figure = nf;
+   end
+ 
+
+
+function mdl = fmdl3d
+   if ~exist('ng_mdl_16x1_coarse.mat','file')
+      !wget http://eidors3d.sf.net/data_contrib/netgen_moving_ball/ng_mdl_16x1_coarse.7z
+      !C:\progra~1\7-zip\7z e ng_mdl_16x1_coarse.7z ng_mdl_16x1_coarse.mat
+   end
+
+   load ng_mdl_16x1_coarse;
+   mdl = ng_mdl_16x1_coarse;
 
 
 function [eelv,eilv] = pig_data
