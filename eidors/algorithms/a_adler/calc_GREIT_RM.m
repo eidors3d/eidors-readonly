4d93f425c54de3952ba6ed9efd5c16e020d7e150
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2009-05-14 11:13:38 +0000

    Calc GREIT RM
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1817 82006732-fb7e-4a22-9f28-86a212cce563

---
+++ eidors/algorithms/a_adler/calc_GREIT_RM.m
@@ -0,0 +1,47 @@
+function RM= calc_GREIT_RM(vh,vi, xyr, weight, normalize)
+% CALCULATE GREIT reconstruction matrix
+%   RM= calc_GREIT_RM(vh,vi, xyr, weight, normalize)
+%
+% (C) 2009 Andy Adler. Licenced under GPL v2 or v3
+% $Id$
+
+   if normalize
+      Y = vi./(vh*ones(1,size(vi,2))) - 1; 
+   else
+      Y = vi - (vh*ones(1,size(vi,2)));
+   end
+
+   D = desired_soln( xyr, 0.25 );
+
+   if isempty(weight)
+       [RM] = calc_RM(Y,D, 5);
+   else
+       error('not coded yet');
+   end
+
+function RM = calc_RM(Y, D, noiselev)
+
+   noiselev = noiselev * mean(abs(Y(:)));
+   % Desired soln for noise is 0
+   Y = [Y, noiselev*eye(208)];
+   D = [D,          zeros(32^2,208)];
+
+   RM = D*Y'/(Y*Y');
+
+function PSF= desired_soln(xyc, radius)
+   xsz= 32; ysz= 32; sz= xsz * ysz;
+   lim= 1.00;
+   [x,y]= ndgrid(linspace(-lim,lim,xsz), linspace(-lim,lim,ysz));
+   spc = 2*lim/(xsz-1) * 0.5;
+   PSF = zeros(sz,size(xyc,2));
+   for i=1:size(xyc,2);
+      for dx = linspace(-spc, spc, 5)
+         for dy = linspace(-spc, spc, 5)
+            PSF(:,i) = PSF(:,i) +  1/25*( ...
+               (dx+x(:)-xyc(1,i)).^2 + (dy+y(:)-xyc(2,i)).^2 ...
+                        < radius^2 );
+         end
+      end
+%     PSF(:,i) = PSF(:,i)/sum(PSF(:,i));
+   end
+
