f202f32b782553f6f523d7541f91a18115da6a9f
sawickib <sawickib@82006732-fb7e-4a22-9f28-86a212cce563>
2009-06-11 01:32:03 +0000

    Higher order interpolation for inclusions
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1831 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/algorithms/b_sawicki/create_inclusion.m
+++ eidors/algorithms/b_sawicki/create_inclusion.m
@@ -1,12 +1,14 @@
-function inhomg_img = create_inclusion( homg_img, center, r, inclusion_mat)
+function inhomg_img = create_inclusion( homg_img, center, r, inclusion_mat, order)
 % USAGE: inhomg_img = create_inclusion( homg_img, inclusion_xyz, ...
-%                                           inclusion_r, inclusion_mat)
+%                                       inclusion_r, inclusion_mat, ...
+%                                       order)
 %
 % Parameters: 
 %      homg_img  -  image structure (fwd_model + elem_data)
 %      center    -  center of spherical inclusion
 %      r         -  radius of inclusion
 %      inclusion_mat   -  inclusion material
+%      order     -  interpolation order (default: 0)
 %
 % Function create inclusion by changing material properties for elements
 % which center is is inside of the sphere defined by (center and r).
@@ -18,24 +20,31 @@ function inhomg_img = create_inclusion( homg_img, center, r, inclusion_mat)
    name = homg_img.name;
    mat = homg_img.elem_data;
    fwd_model = homg_img.fwd_model;
-   dim = size(fwd_model.nodes, 2);
+   if nargin == 4
+     order = 0;
+   end
+   
+   % Calculate interpolation points 
+   mdl_pts = interp_mesh(fwd_model, order);
+   points_in_element = size(mdl_pts, 3);
   
-   for ei=1:size(fwd_model.elems)
-      elem_center = zeros(1,dim);
-      for vi=1:(dim+1)
-         elem_center = elem_center + ...
-         fwd_model.nodes(fwd_model.elems(ei,vi),:);
+   % Main loop
+   for ei=1:size(mdl_pts,1)
+      points_in_sphere = 0;
+      for vi=1:points_in_element
+         if norm(mdl_pts(ei,:,vi) - center) < r
+             points_in_sphere = points_in_sphere + 1;
+         end
       end
-      elem_center = elem_center/(dim+1);
-      if norm(elem_center - center) < r
-         mat(ei) = inclusion_mat;     
-      end
+      ratio = points_in_sphere/points_in_element;
+      mat(ei) = ratio*inclusion_mat + (1-ratio)*mat(ei);     
    end
 
+   % Create new image object
    inhomg_img= eidors_obj('image', name, ...
                           'elem_data', mat, ...
                           'fwd_model', fwd_model );
     
-  end
+  
   
   
