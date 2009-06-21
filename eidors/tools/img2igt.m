dc493356ae7bf78538d301ef885320bca2680930
nightflight84 <nightflight84@82006732-fb7e-4a22-9f28-86a212cce563>
2009-06-21 09:32:32 +0000

    documentation
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1846 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/tools/img2igt.m
+++ eidors/tools/img2igt.m
@@ -1,31 +1,34 @@
 function igt = img2igt(img)
-% IMG2IGT - returns an IGT-compatible measurement matrix from any EIDORS
-% IMG struct.
+%IMG2IGT returns an IGT-compatible image matrix from any EIDORS
+% IMG struct. 
+%
+% IGT = IMG2IGT(IMG) returns a vector NFrames-by-912.
+%
+% WARNING: When the mesh stored in fwd_model is not a resterised 32-by-32
+% matrix, this conversion results in a loss of quality.
+% 
+% See also EIDORS_SAVEIMG.
 
 % (C) 2009 by Bartlomiej Grychtol. Licensed under GPL v2 or v3
 % $Id$ 
 
-% loc.name = 'Dummy vector 1:856';
-% loc.elem_data = 1:856;
-% loc.fwd_model = img.fwd_model; 
-% loc.type = 'image';
-
+% render the image on a 32-by-32 grid
 img.calc_colours.npoints=32;
 greit_m = calc_slices(img);
 greit_m(isnan(greit_m)) = 0;
 
+% create an IGT mask
 ind=[8:25,39:58,70:91,101:124,132:157,163:190,194:223,225:800, ...
         802:831,835:862,868:893,901:924,934:955,967:986,1000:1017];
 igt_m = zeros(1,1024);
 igt_m(ind) = 1:912;
 igt_m = reshape(igt_m,32,32)';
 
+% create IGT vector
 igt=zeros(size(img.elem_data,2),912);
-
 for i = 1:32
     for j = 1:32
-        if igt_m(i,j)>0 %&& greit_m(i,j)>0
-            %igt(:,igt_m(i,j)) = img.elem_data(greit_m(i,j),:);
+        if igt_m(i,j)>0 
             igt(:,igt_m(i,j)) = greit_m(i,j,:);
         end
     end
