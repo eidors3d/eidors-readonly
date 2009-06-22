93c7dc1ade25ae366fa603b92335e610e8d1a0b7
nightflight84 <nightflight84@82006732-fb7e-4a22-9f28-86a212cce563>
2009-06-22 09:52:37 +0000

    work on eidors images

--- trunk/eidors/algorithms/a_adler/GREIT_noise_params.m
+++ trunk/eidors/algorithms/a_adler/GREIT_noise_params.m
@@ -1,10 +1,14 @@
-function params = GREIT_noise_params(imgs, alg, vh, vi )
-% params = GREIT_noise_params(imgs, homg_voltage, sig_voltage)
+function params = GREIT_noise_params(imdl, vh, vi )
+% params = GREIT_noise_params(imdl, homg_voltage, sig_voltage)
 %  params(1,:) = Noise Figure
 
 % (C) 2008 Andy Adler. Licensed under GPL v2 or v3
 % $Id$
 
+% NOTE THAT WE ASSUME A LINEAR ALGORITHM FOR THIS MEASURE
+
+imgs = inv_solve(imdl,vh,vi);
+imgs = imgs.elem_data;
 % There are better ways here
 noise = 0.01*std(vh)*randn(208,1000);
 vhn= vh*ones(1,size(noise,2));
@@ -14,7 +18,8 @@ vhn= vh*ones(1,size(noise,2));
  noise_y  = mean(std(noise./vhn ),2); 
 snr_y = mean(abs(signal_y),1) / noise_y;
 
-im_n= feval(alg, vh, vhn + noise);
+im_n= inv_solve(imdl, vh, vhn + noise);
+im_n = im_n.elem_data;
 %signal_x = mean(mean(abs(imgs),1),2);
  signal_x = mean(mean(   (imgs),1),2);
 %noise_x  = mean(abs(im_n(:)));
