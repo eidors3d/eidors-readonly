f5160a077d4ae64b4cc96fb01222daabb48d34ed
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2009-06-23 09:09:29 +0000

    add properties and refactor

--- trunk/eidors/meshing/distmesh/dm_2d_circ_pt_elecs.m
+++ trunk/eidors/meshing/distmesh/dm_2d_circ_pt_elecs.m
@@ -16,59 +16,10 @@ function fmdl = dm_2d_circ_pt_elecs( elec_pts, pfix, spacing);
 % (C) 2009 Andy Adler. License: GPL version 2 or version 3
 % $Id$
 
+bbox= [-1,-1;1,1];
+fmdl= dm_2d_pt_elecs( elec_pts, [], [0.15,10,0.05], @circle, [-1,-1;1,1] );
 
-params.base_spacing = spacing(1);
-params.refine_ratio = spacing(2);
-params.gradient     = spacing(3);
-epfix = vertcat(elec_pts{:});
-params.refine_pts   = epfix;
-
-[p,t] = distmeshnd(@circle,@dm_refine_points,params.base_spacing/2,  ...
-           [-1,-1;1,1],[pfix,epfix],params);
-
-bdy = find_boundary(t);
-ubn = unique(bdy(:));
-
-for i=1:length(elec_pts);
-   electrode(i).nodes     = get_elec_nodes(elec_pts{i}, p, ubn);
-   electrode(i).z_contact = 0.01; % nominal
-end
-
-
-fmdl.nodes= p;
-fmdl.elems= t;
-fmdl.boundary = bdy;
-fmdl.type = 'fwd_model';
 fmdl.name = sprintf('dm_2d_circ_pt_elec');
-fmdl.electrode = electrode;
-fmdl.gnd_node=           1;
-fmdl.np_fwd_solve.perm_sym =     '{n}';
-
 
 function d= circle(p,params);
   d = sqrt(sum(p.^2,2)) - 1; 
-
-% Find the nodes associated with a given electrode
-%   electrode(i).nodes = get_elec_nodes(elec_pts{i}, p, ubn);
-% electrode points are no further from each end point
-%   than the elecs are from each other
-function nodes= get_elec_nodes( epts, p, ubn);
-   tol = 1e-6;
-   nodes = ubn;
-   bp    = p(ubn,:); % Unique points on the boundary
-   lep = size(epts,1);
-   oep = ones(lep,1);
-   onp = ones(size(ubn,1),1);
-   for i=1:lep
-      el = epts(i,:);
-      % Distance to other electrodes
-      de = oep*el - epts;
-      de = sqrt( sum( de.^2,2 ));
-      mde= max(de);
-      % Distance to other points
-      dp = onp*el - bp;
-      dp = sqrt( sum( dp.^2,2 ));
-      % Zero outside points
-      nodes( dp > mde+tol ) = 0;
-   end
-   nodes = nodes( nodes>0 );
