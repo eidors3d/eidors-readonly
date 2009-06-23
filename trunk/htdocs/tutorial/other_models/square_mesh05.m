75c196887a282c2f577784436663ee48e3c02d54
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2009-06-23 19:21:55 +0000

    square mesh with point electrodes

--- trunk/htdocs/tutorial/other_models/square_mesh05.m
+++ trunk/htdocs/tutorial/other_models/square_mesh05.m
@@ -1,8 +1,8 @@
 % $Id$
 
 z_contact= 0.01;
-n_elec= 17;
 nodes_per_elec= 5;
+n_elec=17;
 elec_width= 0.2;
 elec_spacing= 1.0;
 
@@ -12,28 +12,47 @@ vtx= [x(:),y(:)];
 % Refine points close to electrodes - don't worry if points overlap
 [x,y] = meshgrid( -9:.25:9, -3:.25:0 );
 vtx= [vtx; x(:),y(:)];
+[x,y] = meshgrid( -9:.25:9, -15:.25:-11 );
+vtx= [vtx; x(:),y(:)];
 
 xgrid=  linspace(-elec_width/2, +elec_width/2, nodes_per_elec)';
 x2grid= elec_width* [-5,-4,-3,3,4,5]'/4;
 for i=1:n_elec
-% Electrode centre
   x0= (i-1-(n_elec-1)/2)*elec_spacing;
-  y0=0;
-  elec_nodes{i}= [x0+ xgrid, y0+0*xgrid];
+
+% Top electrode
+  y0  = zeros(size(xgrid));
+  y0_2= zeros(size(x2grid));
+% elec_nodes{2*i-1}= [x0+ xgrid, y0];
+  elec_nodes{2*i-1}= [x0, 0];
   vtx= [ vtx; ...
-        [x0 + x2grid   ,  y0             + 0*x2grid];
-        [x0 + xgrid*1.5,  y0-elec_width/2+ 0*xgrid];
-        [x0 + x2grid*1.5, y0-elec_width/2+ 0*x2grid];
-        [x0 + xgrid*2   , y0-elec_width  + 0*xgrid];
-        [x0 + xgrid*2   , y0-elec_width*2+ 0*xgrid]];
+        [x0 + x2grid    , y0_2              ];
+        [x0 + xgrid*1.5 , y0  - elec_width/2];
+        [x0 + x2grid*1.5, y0_2- elec_width/2];
+        [x0 + xgrid*2   , y0  - elec_width  ];
+        [x0 + xgrid*2   , y0  - elec_width*2]];
+
+% Bottom electrode
+  y0  = -13*ones(size(xgrid));
+  y0_2= -13*ones(size(x2grid));
+  elec_nodes{2*i}= [x0,-13]; % Only point electrodes insidq
+  vtx= [ vtx; ...
+        [x0 + x2grid    , y0_2              ];
+        [x0 + xgrid*1.5 , y0  - elec_width/2];
+        [x0 + x2grid*1.5, y0_2- elec_width/2];
+        [x0 + xgrid*1.5 , y0  + elec_width/2];
+        [x0 + x2grid*1.5, y0_2+ elec_width/2];];
 end
 
 fmdl= mk_fmdl_from_nodes( vtx, elec_nodes, z_contact, 'sq_m1');
+fmdl.solve=@aa_fwd_solve;
+fmdl.system_mat=@aa_calc_system_mat;
+fmdl.jacobian=@aa_calc_jacobian;
 
 
 subplot(121)
 show_fem(fmdl); axis image
 subplot(122)
-show_fem(fmdl); axis image; axis([-2 2 -2.5 0.5]);
+show_fem(fmdl); axis image; axis([-2 2 -14 -12]);
 
-print -dpng -r150 square_mesh01a.png
+print -dpng -r150 square_mesh05a.png
