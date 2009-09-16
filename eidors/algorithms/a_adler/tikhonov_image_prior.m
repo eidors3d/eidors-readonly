440ce3f60f1328d79b896ea8e82aa0de007d80e5
sawickib <sawickib@82006732-fb7e-4a22-9f28-86a212cce563>
2009-09-16 22:32:46 +0000

    Update Tikhonov work with dual models
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1920 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/algorithms/a_adler/tikhonov_image_prior.m
+++ eidors/algorithms/a_adler/tikhonov_image_prior.m
@@ -7,7 +7,13 @@ function Reg= tikhonov_image_prior( inv_model );
 % (C) 2005 Andy Adler. License: GPL version 2 or version 3
 % $Id$
 
-pp= aa_fwd_parameters( inv_model.fwd_model );
+%pp= aa_fwd_parameters( inv_model.fwd_model );
 
-Reg = speye( pp.n_elem );
+if isfield( inv_model.fwd_model, 'coarse2fine' )
+    no_dof = size(inv_model.fwd_model.coarse2fine,2);
+else
+    no_dof = size(inv_model.fwd_model.elems,1);
+end
+
+Reg = speye( no_dof );
 
