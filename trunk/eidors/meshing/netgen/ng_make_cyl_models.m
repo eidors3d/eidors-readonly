95e4d86f474817d1c7ebbf8df056ab5b4a0f9f3a
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2009-11-22 13:25:54 +0000

    make electrodes

--- trunk/eidors/meshing/netgen/ng_make_cyl_models.m
+++ trunk/eidors/meshing/netgen/ng_make_cyl_models.m
@@ -14,11 +14,10 @@ function fmdl = ng_make_cyl_models(cyl_shape, elec_pos, ...
 %  elec_pos = [x,y,z] centres of each electrode (N_elecs x 3)
 %
 % ELECTRODE SHAPES::
-%  elec_shape = {[width,height], [maxsz] } % Rectangular elecs
+%  elec_shape = [width,height, {maxsz}]  % Rectangular elecs
 %     OR
-%  elec_shape = {[radius], [maxsz] }  % Circular elecs
-%     OR
-%  elec_shape = {[radius,0], [maxsz] }  % Circular elecs
+%  elec_shape = [radius, {0, maxsz} ]  % Circular elecs
+%     maxsz  (OPT)  -> max size of mesh elems (default = courase mesh)
 %     maxsz  (OPT)  -> max size of mesh elems (default = courase mesh)
 %
 % Specify either a common electrode shape or for each electrode
@@ -26,9 +25,13 @@ function fmdl = ng_make_cyl_models(cyl_shape, elec_pos, ...
 %
 % USAGE EXAMPLES:
 % Simple 3D cylinder. Radius = 1. No electrodes
-%   fmdl= ng_make_cyl_models(3,{0}); 
+%   fmdl= ng_make_cyl_models(3,[0],[]); 
 % Simple 2D cylinder. Radius = 2. Set minsize to refine
-%   fmdl= ng_make_cyl_models([0,2,.2],{0}); 
+%   fmdl= ng_make_cyl_models([0,2,.2],[0],[]); 
+% 3D cylinder. Radius = 1. 2 planes of 8 elecs with radius 0.1
+%   fmdl= ng_make_cyl_models(3,[8,1,2],[0.1]); 
+% 3D cylinder. Radius = 1. 6 electrodes with elec refinement
+%   fmdl= ng_make_cyl_models(3,[7,1],[0.2,0,0.05]); 
 
 % (C) Andy Adler, 2009. Licenced under GPL v2 or v3
 % $Id$
@@ -36,21 +39,49 @@ function fmdl = ng_make_cyl_models(cyl_shape, elec_pos, ...
 fnstem = tempname;
 geofn= [fnstem,'.geo'];
 meshfn= [fnstem,'.vol'];
-fid=fopen(geofn,'w');
 
 [tank_height, tank_radius, tank_maxh, is2D] = parse_shape(cyl_shape);
+[elecs, centres] = parse_elecs( elec_pos, elec_shape,  ...
+                       tank_height, tank_radius );
 
-write_header(fid,tank_height,tank_radius,tank_maxh);
-   fprintf(fid,'tlo bigcyl;\n');
-fclose(fid);
-   call_netgen( geofn, meshfn);
-
-centres=[];
+write_geo_file(geofn, tank_height, tank_radius, tank_maxh, elecs);
+call_netgen( geofn, meshfn);
 
 fmdl = ng_mk_fwd_model( meshfn, centres, 'ng', []);
+%delete(geofn); delete(meshfn); % remove temp files
 if is2D
    fmdl = mdl2d_from3d(fmdl);
 end
+
+function write_geo_file(geofn, tank_height, tank_radius, tank_maxh, elecs)
+   fid=fopen(geofn,'w');
+   write_header(fid,tank_height,tank_radius,tank_maxh);
+
+   n_elecs = length(elecs);
+   %  elecs(i).pos   = [x,y,z]
+   %  elecs(i).shape = 'C' or 'R'
+   %  elecs(i).dims  = [radius] or [width,height]
+   %  elecs(i).maxh  = '-maxh=#' or '';
+   for i=1:n_elecs
+      name = sprintf('elec%04d',i);
+      pos = elecs(i).pos;
+      if elecs(i).shape == 'C'
+         write_circ_elec(fid,name, pos, pos,  ...
+               elecs(i).dims, tank_radius, elecs(i).maxh);
+      else
+   write_rect_elec(fid,name,c, dirn,hw,d,maxh)
+      end
+
+      fprintf(fid,'solid cyl%04d = bigcyl    and %s; \n',i,name);
+   end
+
+   % SHOULD tank_maxh go here?
+   fprintf(fid,'tlo bigcyl;\n');
+   for i=1:n_elecs
+      fprintf(fid,'tlo cyl%04d cyl -col=[1,0,0];\n ',i);
+   end
+
+   fclose(fid);
 
 function [tank_height, tank_radius, tank_maxh, is2D] = ...
               parse_shape(cyl_shape);
@@ -75,6 +106,67 @@ function [tank_height, tank_radius, tank_maxh, is2D] = ...
          tank_height = min(tank_height,2*tank_maxh);
       end
    end
+
+% ELECTRODE POSITIONS:
+%  elec_pos = [n_elecs_per_plane,z_planes] 
+%     OR
+%  elec_pos = [x,y,z] centres of each electrode (N_elecs x 3)
+%
+% ELECTRODE SHAPES::
+%  elec_shape = [width,height, {maxsz}]  % Rectangular elecs
+%     OR
+%  elec_shape = [radius, {0, maxsz} ]  % Circular elecs
+%     maxsz  (OPT)  -> max size of mesh elems (default = courase mesh)
+% 
+% OUTPUT:
+%  elecs(i).pos   = [x,y,z]
+%  elecs(i).shape = 'C' or 'R'
+%  elecs(i).dims  = [radius] or [width,height]
+%  elecs(i).maxh  = '-maxh=#' or '';
+function [elecs, centres] = parse_elecs( elec_pos, elec_shape, hig, rad );
+   % It never makes sense to specify only one elec
+   if size(elec_pos,1) == 1
+       % Parse elec_pos = [n_elecs_per_plane,z_planes] 
+      n_elecs= elec_pos(1); % per plane
+      th = linspace(0,2*pi, n_elecs+1)'; th(end)=[];
+      csth= rad*[sin(th),cos(th)]; % clockwise from TDC
+
+      centres=[];
+      on_elecs = ones(n_elecs, 1);
+      for i=2:length(elec_pos)
+        centres= [centres; [csth, on_elecs*elec_pos(i)]];
+      end
+   else
+      centres = elec_pos;
+   end
+   n_elecs= size(centres,1);
+
+   if size(elec_shape,1) == 1
+      elec_shape = ones(n_elecs,1) * elec_shape;
+   end
+   elec_shape = [elec_shape, zeros(n_elecs, 2)]; % add default zeros
+
+   elecs= struct; % empty
+   for i= 1:n_elecs
+     row = elec_shape(i,:);
+
+     elecs(i).pos  = centres(i,:);
+     if row(2) == 0
+        elecs(i).shape = 'C';
+        elecs(i).dims  = row(1);
+     else
+        elecs(i).shape = 'R';
+        elecs(i).dims  = row(1:2);
+     end
+
+     if row(3) == 0
+        elecs(i).maxh = '';
+     else
+        elecs(i).maxh = sprintf('-maxh=%f', row(3));
+     end
+   end
+   
+
 
 function write_header(fid,tank_height,tank_radius,maxsz);
    if maxsz==0; 
@@ -117,3 +209,47 @@ function mdl2 = mdl2d_from3d(mdl3)
       enodes(enodes==0) = []; % Remove 3D layers
       mdl2.electrode(i).nodes = enodes;
    end
+
+
+function write_rect_elec(fid,name,c, dirn,hw,d,maxh)
+% writes the specification for a netgen cuboid on fid, named name, centerd on c,
+% in the direction given by vector dirn,
+% hw = [height, width]  and depth d
+% direction is in the xy plane
+   h = hw(1); w= hw(2);
+   dirnp = [-dirn(2),dirn(1),0];
+   dirnp = dirnp/norm(dirnp);
+
+   bl = c - (d/2)* dirn + (w/2)*dirnp - [0,0,h/2];
+   tr = c + (d/2)* dirn - (w/2)*dirnp + [0,0,h/2];
+   fprintf(fid,'solid %s  =plane (%6.3f,%6.3f,%6.3f;0, 0, -1 )\n ', ...
+           name,bl(1),bl(2),bl(3));
+   fprintf(fid,'        and plane(%6.3f,%6.3f,%6.3f;%6.3f,%6.3f,%6.3f  )\n ', ...
+           bl(1),bl(2),bl(3),-dirn(1),-dirn(2),0);
+   fprintf(fid,'        and plane(%6.3f,%6.3f,%6.3f;%6.3f,%6.3f,%6.3f  )\n ', ...
+           bl(1),bl(2),bl(3),dirnp(1),dirnp(2),0);
+   fprintf(fid,'        and plane(%6.3f,%6.3f,%6.3f;0, 0, 1  )\n ', ...
+           tr(1),tr(2),tr(3));
+   fprintf(fid,'        and plane(%6.3f,%6.3f,%6.3f;%6.3f,%6.3f,%6.3f  )\n ', ...
+           tr(1),tr(2),tr(3),dirn(1),dirn(2),0);
+   fprintf(fid,'        and plane(%6.3f,%6.3f,%6.3f;%6.3f,%6.3f,%6.3f  )%s;\n ', ...
+           tr(1),tr(2),tr(3),-dirnp(1),-dirnp(2),0,maxh);
+
+function write_circ_elec(fid,name,c, dirn,rd,ln,maxh)
+% writes the specification for a netgen cylindrical rod on fid,
+%  named name, centerd on c,
+% in the direction given by vector d, radius rd  lenght ln
+% direction is in the xy plane
+% the direction vector
+   dirn(3) = 0; dirn = dirn/norm(dirn);
+
+   inpt = c - dirn.*(ln/2);
+   outpt =c + dirn.*(ln/2);
+
+   fprintf(fid,'solid %s  = ', name);
+   fprintf(fid,'  plane(%6.3f,%6.3f,%6.3f;%6.3f,%6.3f,%6.3f) and\n', ...
+         inpt(1),inpt(2),inpt(3),-dirn(1),-dirn(2),-dirn(3));
+   fprintf(fid,'  plane(%6.3f,%6.3f,%6.3f;%6.3f,%6.3f,%6.3f) and\n', ...
+         outpt(1),outpt(2),outpt(3),dirn(1),dirn(2),dirn(3));
+   fprintf(fid,' cylinder(%6.3f,%6.3f,%6.3f;%6.3f,%6.3f,%6.3f;%6.3f) %s;\n', ...
+         inpt(1),inpt(2),inpt(3),outpt(1),outpt(2),outpt(3), rd,maxh);
