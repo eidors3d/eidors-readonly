10fe545a075bdce2282fac5c35e1c22435706e96
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2009-11-29 16:02:38 +0000

    make figures / fix electrode node dimension
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@1985 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/papers/paper-FEM-2009/code/mk_figures.m
+++ htdocs/papers/paper-FEM-2009/code/mk_figures.m
@@ -13,39 +13,62 @@ end
 
 function mk_fig_1
   imdl = mk_common_model('c2c2',16);
-  stim= imdl.fwd_model.stimulation;
-  setax= [-0.4,1,-0.7,0.7];
   th= linspace(0,2*pi,50);
   cc= 0.2*cos(th)+0.5; ss= 0.2*sin(th);
 
   %figure;
-  set(gcf,'paperposition',[0.25 2.5 6 6]); clf
+  set(gcf,'paperposition',[0.25 2.5 6 6*(5/7)]); clf
+  setax= [-0.4,1,-0.5,0.5];
 
   axes('position',[0.1,0.5,0.4,0.4]);
-  [img, vi, vh, n_en] = ng_mdl(imdl, 1, 0)
+  [img, vic1, vhc1, n_en] = ng_mdl(imdl, 1, 0)
   show_fem(img); axis(setax);
   set(gca,'XTickLabel',[]);
   hh=line(cc,ss); set(hh,'Color',[0,0,1],'LineWidth',2); 
 
   axes('position',[0.52,0.5,0.4,0.4]);
-  [img, vi, vh, n_en] = ng_mdl(imdl, 0.05, 0)
+  [img, vif1, vhf1, n_en] = ng_mdl(imdl, 0.05, 0)
   show_fem(img); axis(setax);
   set(gca,'XTickLabel',[]);
   set(gca,'YTickLabel',[]);
   hh=line(cc,ss); set(hh,'Color',[0,0,1],'LineWidth',2); 
 
   axes('position',[0.1,0.08,0.4,0.4]);
-  [img, vi, vh, n_en] = ng_mdl(imdl, 1.00, 1)
+  [img, vic2, vhc2, n_en] = ng_mdl(imdl, 1.00, 1)
   show_fem(img); axis(setax);
   set(gca,'XTickLabel',[]);
   hh=line(cc,ss); set(hh,'Color',[0,0,1],'LineWidth',2); 
 
   axes('position',[0.52,0.08,0.4,0.4]);
-  [img, vi, vh, n_en] = ng_mdl(imdl, 0.05, 1)
+  [img, vif2, vhf2, n_en] = ng_mdl(imdl, 0.05, 1)
   show_fem(img); axis(setax);
   set(gca,'YTickLabel',[]);
   hh=line(cc,ss); set(hh,'Color',[0,0,1],'LineWidth',2); 
 
+  print -depsc2 ../figures/fig_2d_fems.eps
+
+  clf
+  axes('position',[0.1,0.5,0.4,0.4]);
+  img = inv_solve(imdl,vic1,vhc1);%show_fem(img);
+  set(gca,'XTickLabel',[]);
+  hh=line(cc,ss); set(hh,'Color',[0,0,1],'LineWidth',2); 
+
+  axes('position',[0.52,0.5,0.4,0.4]);
+  img = inv_solve(imdl,vif1,vhf1); show_fem(img);
+  set(gca,'XTickLabel',[]);
+  set(gca,'YTickLabel',[]);
+  hh=line(cc,ss); set(hh,'Color',[0,0,1],'LineWidth',2); 
+
+  axes('position',[0.1,0.08,0.4,0.4]);
+  img = inv_solve(imdl,vic2,vhc1); show_fem(img);
+  set(gca,'XTickLabel',[]);
+  hh=line(cc,ss); set(hh,'Color',[0,0,1],'LineWidth',2); 
+
+  axes('position',[0.52,0.08,0.4,0.4]);
+  img = inv_solve(imdl,vif2,vhf1); show_fem(img);
+  set(gca,'YTickLabel',[]);
+  hh=line(cc,ss); set(hh,'Color',[0,0,1],'LineWidth',2); 
+  
 
 
 % no obj if rad = 0
@@ -56,10 +79,18 @@ function [img, vi, vh, n_en] = ng_mdl(imdl, maxh, rad)
      extra={'ball','solid ball = cylinder(0.5,0,0;0.5,0,1;0.2) and orthobrick(-1,-1,0;1,1,0.05);'}
   end
 
-  fmdl= ng_mk_cyl_models([0,1,maxh],[16],[0.1,0,0.02],extra); 
+   p = {maxh,rad};
+   fmdl = eidors_obj('get-cache', p, 'ng_mk_cyl_models');
+   if isempty(fmdl)
+      fmdl= ng_mk_cyl_models([0,1,maxh],[16],[0.1,0,0.02],extra); 
+      eidors_obj('set-cache', p, 'ng_mk_cyl_models', fmdl);
+   end
+
   imdl = assign_mdl(imdl, fmdl);
   img = calc_jacobian_bkgnd(imdl);
+keyboard
   vh = fwd_solve(img);
+
 
   pts = interp_mesh(fmdl, 4);
   pts=(pts(:,1,:)-0.5).^2 + (pts(:,2,:)-0).^2;
@@ -77,3 +108,4 @@ function imdl = assign_mdl(imdl, fmdl);
   imdl.fwd_model.elems    = fmdl.elems;
   imdl.fwd_model.gnd_node = fmdl.gnd_node;
   imdl.fwd_model.electrode= fmdl.electrode;
+  imdl.fwd_model.boundary = find_boundary(fmdl.elems);
