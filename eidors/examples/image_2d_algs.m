0fc61a9c58f6fa389b4854672385b917a943d63e
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2009-12-28 17:36:57 +0000

    fixed example
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@2003 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/examples/image_2d_algs.m
+++ eidors/examples/image_2d_algs.m
@@ -4,52 +4,18 @@
 % (C) 2005 Andy Adler. License: GPL version 2 or version 3
 % $Id$
 
-eidors_msg('log_level',1); % 2 for most messages
-
-% 
-% Step 1: Create simple 16 electrode 2D model
-% 
-% get parameters for model from mk_circ_tank
-% param= mk_circ_tank(rings, levels, n_elec, n_planes )
 n_elec= 16;
 n_rings= 1;
  options = {'no_meas_current','no_rotate_meas'};
 
-params= mk_circ_tank(8, [], n_elec); 
+[st, ms]= mk_stim_patterns(n_elec, n_rings, '{ad}','{ad}', options, 10);
+inv2d= mk_common_model('c2c2',n_elec);
+inv2d.fwd_model.stimulation = st;
+inv2d.fwd_model.meas_select = ms;
 
-[st, els]= mk_stim_patterns(n_elec, n_rings, '{ad}','{ad}', options, 10);
-params.stimulation= st;
-params.meas_select= els;
-params.solve=      'aa_fwd_solve';
-params.system_mat= 'aa_calc_system_mat';
-params.jacobian=   'aa_calc_jacobian';
-params.normalize_measurements= 0;
-mdl_2d   = eidors_obj('fwd_model', params);
-
-% 
-% Step 3: Create inverse model
-% 
-inv2d.name= 'AA mdl with excluded measurements';
-%inv2d.solve=       'aa_inv_solve';
-inv2d.solve=       'aa_inv_conj_grad';
-inv2d.jacobian_bkgnd.value = 1;
-%inv2d.hyperparameter.value = 1e-2;
-inv2d.hyperparameter.func = 'choose_noise_figure';
-inv2d.hyperparameter.noise_figure= 1;
-inv2d.hyperparameter.tgt_elems= 1:4;
- inv2d.RtR_prior= 'laplace_image_prior';
-%inv2d.RtR_prior= 'gaussian_HPF_prior';
-inv2d.reconst_type= 'difference';
-inv2d.fwd_model= mdl_2d;
-inv2d= eidors_obj('inv_model', inv2d);
-
-% 
-% Step 3: Reconst and show image
-% 
 load eidors2d_bubble.mat
 d1= bubble2(1280+(-255:0));
 d2= bubble1(1280+(-255:0));
 
 img= inv_solve( inv2d, d1, d2);
 show_fem(img);
-
