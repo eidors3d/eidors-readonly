c5aba77f828bd436e1c47a6a607b04082fc71468
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2010-03-24 16:04:13 +0000

    add caching of models
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@2062 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/meshing/distmesh/dm_2d_circ_pt_elecs.m
+++ eidors/meshing/distmesh/dm_2d_circ_pt_elecs.m
@@ -1,5 +1,6 @@
 function fmdl = dm_2d_circ_pt_elecs( elec_pts, pfix, spacing);
-% DM_2D_CIRC_PT_ELECS: Create circle mesh (or radius 1) refined
+% DM_2D_CIRC_PT_ELECS: Create circle mesh (or radius 1) refined with electrodes
+% fmdl = dm_2d_circ_pt_elecs( elec_pts, pfix, spacing);
 %     at points on the electrodes
 % fmdl = dm_2d_circ_pt_elecs( elec_pts, pfix, ...
 %               [base_spacing, refine_ratio, gradient]);
@@ -7,16 +8,32 @@ function fmdl = dm_2d_circ_pt_elecs( elec_pts, pfix, spacing);
 %    normally only two points are specified (at start and end of electrode)
 %    eg elec_pts{1} = [-.1,1;.1,1];
 % pfix = any fixed points to provide to the model (default = [])
-% base_spacing - edge length away from refined nodes (eg 0.1)
-% refine_ratio - relative refinement near points (eg. 10)
-% gradient     - transition slope of refinement (eg 0.1)
+% param(1) = base_spacing - edge length away from refined nodes (eg 0.1)
+% param(2) = refine_ratio - relative refinement near points (eg. 10)
+% param(3) = gradient     - transition slope of refinement (eg 0.1)
 %
-% Example: = dm_2d_circ_pt_elecs( elec_pts, [], [0.15,10,0.05] );
+% Example: Three electrodes 2 on boundary and one internal
+%  elec_pts = {[1,0],[0,1;sin(0.2),cos(0.2)],[0.5,0.5]};
+%  fmdl= dm_2d_circ_pt_elecs( elec_pts, [], [0.15,10,0.05] );
+%
+% Example:
+%  n_elecs= 14; elec_width= 0.1; hw= elec_width/2;
+%  th = linspace(0,2*pi,n_elecs+1); th(end)=[];
+%  for i=1:n_elecs;
+%     ti = th(i) + [hw;-hw];
+%     elec_pts{i} = [sin(ti),cos(ti)];
+%  end
+%  fmdl= dm_2d_circ_pt_elecs( elec_pts, [], [0.10,10,0.02] );
+
 %
 % See also: dm_2d_pt_elecs
 
 % (C) 2009 Andy Adler. License: GPL version 2 or version 3
 % $Id$
+
+cache_obj = {elec_pts, spacing};
+fmdl= eidors_obj('get-cache',cache_obj, 'dm_2d_circ_pt_elecs');
+if ~isempty(fmdl); return; end % Object is cached
 
 params.base_spacing = spacing(1);
 params.refine_ratio = spacing(2);
@@ -26,6 +43,7 @@ bbox= [-1,-1;1,1];
 fmdl= dm_2d_pt_elecs( elec_pts, [], params, @circle, [-1,-1;1,1] );
 
 fmdl.name = sprintf('dm_2d_circ_pt_elec');
+eidors_obj('set-cache',cache_obj, 'dm_2d_circ_pt_elecs', fmdl);
 
 function d= circle(p,params);
   d = sqrt(sum(p.^2,2)) - 1; 
