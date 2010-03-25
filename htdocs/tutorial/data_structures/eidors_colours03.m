308cbc5c17682a6aa063ead186b240ca7567cc60
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2010-03-25 16:37:53 +0000

    new eidors colours
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@2074 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/data_structures/eidors_colours03.m
+++ htdocs/tutorial/data_structures/eidors_colours03.m
@@ -1,5 +1,6 @@
 % Show EIDORS colours $Id$
 subplot(221); img1= img;
+img1.calc_colours.cb_shrink_move = [0.5,0.8,.02];
 
 show_fem(img1,1);
 axis equal; axis off; print -r75 -dpng eidors_colours03a.png
