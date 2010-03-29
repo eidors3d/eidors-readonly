e1b7192bda4666cdf5cdfe003b7e4bfe35ed6e46
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2010-03-29 16:12:42 +0000

    working version
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@2107 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/graphics_matlab/show_current.m
+++ eidors/graphics_matlab/show_current.m
@@ -1,4 +1,4 @@
-function show_current( img, volt )
+function show_current( img, vv )
 % SHOW_CURRENT: show current or other quantity defined
 %  on nodes onto the image
 %
@@ -29,23 +29,23 @@ end
 if nargin==1; % We need to calculate
    img.fwd_solve.get_all_meas = 1;
    vh = fwd_solve(img);
-   volt = vh.meas(:,1);
+   vv = vh.volt(:,1);
 end 
 
 elem_ptr = mdl_slice_mapper( img.fwd_model, 'elem' );
 szep = size(elem_ptr);
 
-elemcur = zeros(size(fmdl.elems,1)+1,size(fmdl.nodes,2));
-vv =  vh.volt(:,1);
+Nel = size(img.fwd_model.elems,1);
+elemcur = zeros(Nel+1,dims);
 del = [1,-1,0;0,1,-1;-1,0,1];
 del2 = del'*del;
-for i=1:size(elemcur,1)-1;
+for i=1:Nel
   idx = img.fwd_model.elems(i,:);
   nod = img.fwd_model.nodes(idx,:);
   elemcur(i+1,:) = vv(idx)'*del2*nod;
 end
 
-[x,y] = grid_the_space( img.fmdl);
+[xp,yp] = grid_the_space( img.fwd_model);
 
 xc = reshape( elemcur(elem_ptr+1,1), szep);
 yc = reshape( elemcur(elem_ptr+1,2), szep);
@@ -80,14 +80,14 @@ function  [x,y] = grid_the_space( fmdl);
 function do_unit_test
    imdl= mk_common_model('d2d2c',8);
    img = calc_jacobian_bkgnd( imdl );
-   img.fwd_solve.mdl_slice_mapper.npx = 64;
-   img.fwd_solve.mdl_slice_mapper.npy = 64;
+   img.fwd_model.mdl_slice_mapper.npx = 64;
+   img.fwd_model.mdl_slice_mapper.npy = 64;
    show_current(img);
 
-xx = linspace(0,1,64);
-fmdl.mdl_slice_mapper.x_pts = xx;
-fmdl.mdl_slice_mapper.y_pts = xx;
-fmdl.mdl_slice_mapper.level = [inf,inf,0];
+   img.fwd_model.mdl_slice_mapper.x_pts = linspace(0,1,62);
+   img.fwd_model.mdl_slice_mapper.y_pts = linspace(0,1,56);
+   img.fwd_model.mdl_slice_mapper.level = [inf,inf,0];
 
-
-   img.fwd_solve.get_all_meas = 1; vh = fwd_solve(img);
+   img.fwd_solve.get_all_meas = 1;
+   vh = fwd_solve(img);
+   show_current(img, vh.volt(:,1));
