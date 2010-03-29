b97643a2ae4ee9b686e581d9fd41e8f027c0ab9e
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2010-03-29 16:07:06 +0000

    refactor mapper
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@2106 82006732-fb7e-4a22-9f28-86a212cce563

---
+++ eidors/graphics_matlab/show_current.m
@@ -0,0 +1,93 @@
+function show_current( img, volt )
+% SHOW_CURRENT: show current or other quantity defined
+%  on nodes onto the image
+%
+% show_current( img, volt )
+%   img -> img object 
+%   volt-> voltage on nodes (if not specified, img is solved via fwd_solve)
+%
+% The points are specified as either
+%   img.fwd_model.mdl_slice_mapper.npx   - number of points in horizontal direction
+%   img.fwd_model.mdl_slice_mapper.npy   - number of points in vertical 
+%    or
+%   img.fwd_model.mdl_slice_mapper.x_pts - vector of points in horizontal direction
+%   img.fwd_model.mdl_slice_mapper.y_pts - vector of points in vertical
+%
+% For 3D models, the slice may be specified as (see calc_slices for information)
+%   img.fwd_model.mdl_slice_mapper.level 
+
+% (C) 2010 Andy Adler. License: GPL version 2 or version 3
+% $Id$
+
+if isstr(img) && strcmp(img,'UNIT_TEST'); do_unit_test; return; end
+
+dims = size(img.fwd_model.nodes,2);
+if dims == 2;
+   img.fwd_model.mdl_slice_mapper.level = [inf,inf,0];
+end
+
+if nargin==1; % We need to calculate
+   img.fwd_solve.get_all_meas = 1;
+   vh = fwd_solve(img);
+   volt = vh.meas(:,1);
+end 
+
+elem_ptr = mdl_slice_mapper( img.fwd_model, 'elem' );
+szep = size(elem_ptr);
+
+elemcur = zeros(size(fmdl.elems,1)+1,size(fmdl.nodes,2));
+vv =  vh.volt(:,1);
+del = [1,-1,0;0,1,-1;-1,0,1];
+del2 = del'*del;
+for i=1:size(elemcur,1)-1;
+  idx = img.fwd_model.elems(i,:);
+  nod = img.fwd_model.nodes(idx,:);
+  elemcur(i+1,:) = vv(idx)'*del2*nod;
+end
+
+[x,y] = grid_the_space( img.fmdl);
+
+xc = reshape( elemcur(elem_ptr+1,1), szep);
+yc = reshape( elemcur(elem_ptr+1,2), szep);
+
+quiver(xp,yp,xc,yc,2);
+
+function  [x,y] = grid_the_space( fmdl);
+
+  xspace = []; yspace = [];
+  try 
+     xspace = fmdl.mdl_slice_mapper.x_pts;
+     yspace = fmdl.mdl_slice_mapper.y_pts;
+  end
+
+  if isempty(xspace)
+     npx  = fmdl.mdl_slice_mapper.npx;
+     npy  = fmdl.mdl_slice_mapper.npy;
+
+     xmin = min(fmdl.nodes(:,1));    xmax = max(fmdl.nodes(:,1));
+     xmean= mean([xmin,xmax]); xrange= xmax-xmin;
+
+     ymin = min(fmdl.nodes(:,2));    ymax = max(fmdl.nodes(:,2));
+     ymean= mean([ymin,ymax]); yrange= ymax-ymin;
+
+     range= max([xrange, yrange]);
+     xspace = linspace( xmean - range*0.5, xmean + range*0.5, npx );
+     yspace = linspace( ymean + range*0.5, ymean - range*0.5, npy );
+  end
+
+  [x,y]=meshgrid( xspace, yspace );
+
+function do_unit_test
+   imdl= mk_common_model('d2d2c',8);
+   img = calc_jacobian_bkgnd( imdl );
+   img.fwd_solve.mdl_slice_mapper.npx = 64;
+   img.fwd_solve.mdl_slice_mapper.npy = 64;
+   show_current(img);
+
+xx = linspace(0,1,64);
+fmdl.mdl_slice_mapper.x_pts = xx;
+fmdl.mdl_slice_mapper.y_pts = xx;
+fmdl.mdl_slice_mapper.level = [inf,inf,0];
+
+
+   img.fwd_solve.get_all_meas = 1; vh = fwd_solve(img);
