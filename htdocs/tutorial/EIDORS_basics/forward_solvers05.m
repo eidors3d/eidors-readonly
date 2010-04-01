5d837a85c95b3f965cefba9bf316efcf4471dfdd
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2010-04-01 13:57:13 +0000

    forward solvers
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@2120 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/EIDORS_basics/forward_solvers05.m
+++ htdocs/tutorial/EIDORS_basics/forward_solvers05.m
@@ -1,24 +1,25 @@
 % Forward solvers $Id$
 
-% Calculate a stimulation pattern
-stim = mk_stim_patterns(19,1,[0,9],[0,1],{},1);
 
 % Solve all voltage patterns
-img_2.fwd_model.stimulation = stim;
 img_2.fwd_solve.get_all_meas = 1;
-vh = fwd_solve(img_2);
+img_2.fwd_model.mdl_slice_mapper.npx = 64;
+img_2.fwd_model.mdl_slice_mapper.npy = 64;
 
-% Show first stim pattern
+% Show [0-3] stim pattern
 subplot(221);
-img_v = rmfield(img_2, 'elem_data');
-img_v.node_data = vh.volt(:,1);
-show_fem(img_v);
+stim = mk_stim_patterns(19,1,[0,3],[0,1],{},1);
+img_2.fwd_model.stimulation = stim;
+vh = fwd_solve(img_2);
+show_current(img_2,vh.volt(:,1));
+axis([-1,1,-1,1]);
 
-% Show 7th stim pattern
+% Show [2-9] stim pattern
 subplot(222);
-img_v = rmfield(img_2, 'elem_data');
-img_v.node_data = vh.volt(:,7);
-img_v.calc_colours.cb_shrink_move = [0.3,0.6,+0.03];
-show_fem(img_v,1);
+stim = mk_stim_patterns(19,1,[0,7],[0,1],{},1);
+img_2.fwd_model.stimulation = stim;
+vh = fwd_solve(img_2);
+show_current(img_2,vh.volt(:,3));
+axis([-1,1,-1,1]);
 
-print -dpng -r125 forward_solvers03a.png
+print -dpng -r125 forward_solvers05a.png
