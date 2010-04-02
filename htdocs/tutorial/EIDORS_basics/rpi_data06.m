ef0aef837d34d9430e18eb48ef10f0f4d269f6e8
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2010-04-02 18:18:23 +0000

    RPI data and examples
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@2152 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/EIDORS_basics/rpi_data06.m
+++ htdocs/tutorial/EIDORS_basics/rpi_data06.m
@@ -8,14 +8,12 @@ imdl.solve = @GN_abs_solve;
 
 subplot(221);
 
-for iter = [1,2,3,5];
+for iter = [1,2,3];
    imdl.parameters.max_iteration = iter;
    img = inv_solve(imdl , vi);
    img.calc_colours.cb_shrink_move = [0.5,0.8,0];
    show_fem(img,[1,1]); axis off; axis image
 
    %print -dpng -r125 rpi_data01a.png
-   print -depsc2  jnk.eps;
-   system(sprintf('LD_LIBRARY_PATH="" convert -density 125 jnk.eps rpi_data06%c.png', ...
-                  'a'-1+iter));
+   print -depsc2  jnk.eps; system(sprintf('LD_LIBRARY_PATH="" convert -density 125 jnk.eps rpi_data06%c.png', 'a'-1+iter));
 end
