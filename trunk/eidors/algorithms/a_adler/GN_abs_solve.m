5ccba6e57f7c135541f1d24c89128175ac5cdaab
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2010-04-02 18:04:49 +0000

    absolute solver

---
+++ trunk/eidors/algorithms/a_adler/GN_abs_solve.m
@@ -0,0 +1,74 @@
+function img= GN_abs_solve( inv_model, data1);
+% GN_ABS_SOLVER absolute solver using Gauss Newton approximation
+% img= gn_abs_solve( inv_model, data1, data2)
+% img        => output image (or vector of images)
+% inv_model  => inverse model struct
+% data1      => EIT data
+%
+% Parameters:
+%   inv_model.parameters.max_iteration = N_max iter
+
+% (C) 2010 Andy Adler. License: GPL version 2 or version 3
+% $Id$
+
+% Step 1: fit to background
+img = homogeneous_estimate( inv_model, data1 );
+
+hp  = calc_hyperparameter( inv_model );
+RtR = calc_RtR_prior( inv_model );
+W   = calc_meas_icov( inv_model );
+hp2RtR= hp*RtR;
+
+iters = 1;
+try 
+   iters = inv_model.parameters.max_iteration;
+end
+
+img0 = img;
+for i = 1:iters; 
+  vsim = fwd_solve( img ); 
+  dv = calc_difference_data( vsim , data1, img.fwd_model);
+  J = calc_jacobian( img );
+
+  RDx = hp2RtR*(img0.elem_data - img.elem_data);
+  dx = (J'*W*J + hp2RtR)\(J'*dv - RDx);
+
+  img = line_optimize(img, dx, data1);
+end
+
+function  img = line_optimize(imgk, dx, data1);
+% TODO, we can do a parabloic fit
+  flist = [0.03, 0.1, 0.3,  0.5, 0.9];
+  clim = mean(imgk.elem_data)/10; % prevent zero and negative conductivity
+  img = imgk;
+  for i = 1:length(flist);
+     img.elem_data = imgk.elem_data + flist(i)*dx;
+     img.elem_data(img.elem_data <= clim ) = clim;
+     vsim = fwd_solve( img );
+     dv = calc_difference_data( vsim , data1, img.fwd_model);
+     mlist(i) = norm(dv);
+  end
+
+  [jnk, i] = min(mlist)
+  img.elem_data = imgk.elem_data + flist(i)*dx;
+  img.elem_data(img.elem_data <= clim ) = clim;
+
+function img = homogeneous_estimate( imdl, data );
+   img = calc_jacobian_bkgnd( imdl );
+   vs = fwd_solve(img);
+
+   if isstruct(data)
+      data = data.meas;
+   else
+     meas_select = [];
+     try
+        meas_select = imdl.fwd_model.meas_select;
+     end
+     if length(data) == length(meas_select)
+        data = data(meas_select);
+     end
+   end
+
+   pf = polyfit(data,vs.meas,1);
+
+   img.elem_data = img.elem_data*pf(1);
