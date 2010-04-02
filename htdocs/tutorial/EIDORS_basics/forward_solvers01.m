f747fd41e975985ae7919f02d9bc40a83bf65a0e
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2010-04-02 10:46:54 +0000

    add selector function
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@2133 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/EIDORS_basics/forward_solvers01.m
+++ htdocs/tutorial/EIDORS_basics/forward_solvers01.m
@@ -10,11 +10,8 @@ subplot(221); show_fem(img_1);
 % Add a circular object at 0.2, 0.5
 % Calculate element membership in object
 img_2 = img_1;
-xctr = 0.2; yctr = 0.5; rad = 0.1;
-pts = interp_mesh( img_2.fwd_model, 4);
-dist = (pts(:,1,:)-xctr).^2 + (pts(:,2,:)-yctr).^2;
-member = mean( dist < rad^2 ,3);
-img_2.elem_data = 1 + member;
+select_fcn = inline('(x-0.2).^2+(y-0.5).^2<0.1^2','x','y','z');
+img_2.elem_data = 1 + elem_select(img_2.fwd_model, select_fcn);
 
 img_2.calc_colours.cb_shrink_move = [0.3,0.6,+0.03];
 subplot(222); show_fem(img_2,1);
