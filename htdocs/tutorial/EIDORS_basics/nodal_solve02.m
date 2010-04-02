47101a4421ba4fd3321a982e8fd8d9eacf64d07a
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2010-04-02 12:44:32 +0000

    Elem solver
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@2135 82006732-fb7e-4a22-9f28-86a212cce563

---
+++ htdocs/tutorial/EIDORS_basics/nodal_solve02.m
@@ -0,0 +1,19 @@
+% Element Solvers Target $Id$
+
+% Coarse model
+imdl1= mk_common_model('a2c0',16);
+imdl1.RtR_prior = @laplace_image_prior;
+imdl1.hyperparameter.value = 0.01;
+
+img1 = inv_solve(imdl1, vh, vi);
+show_fem(img1);
+print -dpng -r125 nodal_solve02a.png
+
+% Fine model
+imdl2= mk_common_model('c2c0',16);
+imdl2.RtR_prior = @laplace_image_prior;
+imdl2.hyperparameter.value = 0.01;
+
+img2 = inv_solve(imdl2, vh, vi);
+show_fem(img2);
+print -dpng -r125 nodal_solve02b.png
