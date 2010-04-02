bb02d8a2d8899cf921a14275aafaa80d2b2bca81
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2010-04-02 14:59:16 +0000

    colour bars

--- trunk/htdocs/tutorial/EIDORS_basics/rpi_data02.m
+++ trunk/htdocs/tutorial/EIDORS_basics/rpi_data02.m
@@ -16,7 +16,8 @@ vh =  real(ACT2006_homog); vi = real(ACT2000_phant);
 
 subplot(221);
 img = inv_solve(imdl, vh, vi);
-show_fem(img,[0,1]); axis off; axis image
+img.calc_colours.cb_shrink_move = [0.5,0.8,0];
+show_fem(img,[1,1]); axis off; axis image
 
 %print -dpng -r125 rpi_data01a.png
 print -depsc2  jnk.eps;!LD_LIBRARY_PATH="" convert -density 125 jnk.eps rpi_data02a.png
