5e54d5b33b2dfafa02d7f13bf62e422bdea78abb
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2010-04-02 18:05:15 +0000

    absolute solvers
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@2150 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/EIDORS_basics/rpi_data06.m
+++ htdocs/tutorial/EIDORS_basics/rpi_data06.m
@@ -1,11 +1,21 @@
 % Absolute reconstructions $Id$
 
-imdl.solve = @GN_abs_solver;
+imdl = mk_common_model('b2c2',32);
+imdl.fwd_model = fmdl;
+imdl.reconst_type = 'absolute';
+imdl.hyperparameter.value = 808;
+imdl.solve = @GN_abs_solve;
 
 subplot(221);
-imdl.parameters.max_iteration = 1;
-img = inv_solve(imdl , vi);
-show_fem(img,[0,1]); axis off; axis image
 
-%print -dpng -r125 rpi_data01a.png
-print -depsc2  jnk.eps;!LD_LIBRARY_PATH="" convert -density 125 jnk.eps rpi_data03a.png
+for iter = [1,2,3,5];
+   imdl.parameters.max_iteration = iter;
+   img = inv_solve(imdl , vi);
+   img.calc_colours.cb_shrink_move = [0.5,0.8,0];
+   show_fem(img,[1,1]); axis off; axis image
+
+   %print -dpng -r125 rpi_data01a.png
+   print -depsc2  jnk.eps;
+   system(sprintf('LD_LIBRARY_PATH="" convert -density 125 jnk.eps rpi_data06%c.png', ...
+                  'a'-1+iter));
+end
