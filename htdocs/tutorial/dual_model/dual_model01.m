8003d00575e9131129294a49eb1943fd256e6091
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2010-04-04 11:35:04 +0000

    more tutorial mods
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@2159 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/dual_model/dual_model01.m
+++ htdocs/tutorial/dual_model/dual_model01.m
@@ -1,6 +1,6 @@
 % Simulate data $Id$
 imdl= mk_common_model('c2c2',16);
-img=calc_jacobian_bkgnd(imdl);
+img= mk_image(imdl);
 vh=fwd_solve(img);
 idx=[365,328,292,259,227,198,170,145,121];
 img.elem_data([idx,idx+1,101,81])=1.1;
