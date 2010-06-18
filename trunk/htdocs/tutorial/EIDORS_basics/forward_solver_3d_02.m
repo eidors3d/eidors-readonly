a90fa052e2baa13321a7329c693c40f054e52983
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2010-06-18 08:25:25 +0000

    3D solvers

---
+++ trunk/htdocs/tutorial/EIDORS_basics/forward_solver_3d_02.m
@@ -0,0 +1,9 @@
+extra={'ball','solid ball = sphere(0,0.2,0.5;0.1);'};
+[fmdl,mat_idx]= ng_mk_cyl_models([1,0.3,0.05],[nelec,ring_vert_pos],[0.1,0.05,0.02], extra);
+fmdl.stimulation = stim;
+
+img= mk_image(fmdl, conduct);
+img.elem_data(mat_idx{2}) = 0.1;
+
+show_fem(img);
+print_convert forward_solver_3d_02a.png '-density 75'
