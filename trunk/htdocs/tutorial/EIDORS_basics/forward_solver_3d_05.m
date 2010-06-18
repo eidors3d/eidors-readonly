b23d6d99c4d6861f6c9ce94157d1c8c69348659b
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2010-06-18 08:50:42 +0000

    images and mods

---
+++ trunk/htdocs/tutorial/EIDORS_basics/forward_solver_3d_05.m
@@ -0,0 +1,16 @@
+img.fwd_model.electrode([2,13]) = img.fwd_model.electrode([13,2]); % flip electrodes
+
+img.elem_data(mat_idx{2}) = conduct; % Homogenous
+vh = fwd_solve(img);
+
+img.elem_data(mat_idx{2}) = 0.95*conduct; %Non-conductive inclusion
+vi = fwd_solve(img);
+
+img_v.node_data = vh.volt(:,1);
+
+show_slices(img_v,[0.25;0.35;0.45]*[inf,inf,1])
+print_convert forward_solver_3d_05a.png '-density 75'
+
+img_v.node_data = vh.volt(:,1) - vi.volt(:,1);
+show_slices(img_v,[0.25;0.35;0.45]*[inf,inf,1])
+print_convert forward_solver_3d_05b.png '-density 75'
