1719fa3d76803fb265b0e145f36f458c78e0bb5d
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2010-06-18 08:25:25 +0000

    3D solvers
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@2190 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/EIDORS_basics/forward_solver_3d_01.m
+++ htdocs/tutorial/EIDORS_basics/forward_solver_3d_01.m
@@ -1,43 +1,12 @@
-nelec= 7;
-fmdl= ng_mk_cyl_models([1,0.3,0.05],[nelec,0.2],[0.1,0.05,0.02]);
+nelec= 8; nrings= 2;
+ring_vert_pos = [0.2, 0.5]; 
+fmdl= ng_mk_cyl_models([1,0.3,0.05],[nelec,ring_vert_pos],[0.1,0.05,0.02]);
 
-stim = mk_stim_patterns(nelec,1,[0,1],[0,1],{'meas_current'},1);
+stim = mk_stim_patterns(nelec,nrings,[0,1],[0,1],{'meas_current'},1);
 fmdl.stimulation = stim;
+
 conduct = 1;
 img = mk_image( fmdl, conduct ); 
-img.fwd_solve.get_all_meas = 1;
-vh = fwd_solve(img);
 
-img_v = rmfield(img, 'elem_data');
-img_v.node_data = vh.volt(:,1);
-
-subplot(231); show_fem(img);
-subplot(232): plot(vh.meas)
-subplot(233);
-img_v.calc_colours.npoints=128;
-show_slices(img_v,[inf,inf,0.2])
-
-
-extra={'ball','solid ball = sphere(0,0,0.5;0.1);'};
-[fmdl,mat_idx]= ng_mk_cyl_models([1,0.3,0.05],[nelec,0.2],[0.1,0.05,0.02],extra);
-fmdl.stimulation = stim;
-
-img= mk_image(fmdl, conduct);
-img.fwd_solve.get_all_meas = 1;
-vh2 = fwd_solve(img);
-
-img.elem_data(mat_idx{2}) = 2;
-vi2 = fwd_solve(img);
-
-
-
-subplot(234); show_fem(img);
-subplot(235): plot([vi2.meas, 100*(vh2.meas-vi2.meas)])
-
-
-img_v = rmfield(img, 'elem_data');
-img_v.node_data = vi2.volt(:,1) - vh2.volt(:,1);
-
-subplot(236);
-img_v.calc_colours.npoints=128;
-show_slices(img_v,[inf,inf,0.2])
+show_fem(img);
+print_convert forward_solver_3d_01a.png '-density 75'
