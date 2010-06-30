d005e2750e9411590d0f60a44e15284222ecab78
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2010-06-30 12:09:54 +0000

    Fixes to make improve image quality

--- trunk/htdocs/tutorial/EIDORS_basics/rpi_data03.m
+++ trunk/htdocs/tutorial/EIDORS_basics/rpi_data03.m
@@ -5,6 +5,7 @@ imdl.fwd_model = fmdl;
 
 img = inv_solve(imdl , vh, vi);
 img.calc_colours.cb_shrink_move = [0.5,0.8,0.05];
+img.calc_colours.ref_level = 0;
 show_fem(img,[1,1]); axis off; axis image
 
 print_convert('rpi_data03a.png','-density 60');
