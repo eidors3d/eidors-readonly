203a12b1573fb9cd90b03b5e5c505bb667d7fca9
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2010-07-02 10:07:01 +0000

    use matrix goes to nodes

--- trunk/eidors/algorithms/a_adler/solve_use_matrix.m
+++ trunk/eidors/algorithms/a_adler/solve_use_matrix.m
@@ -9,6 +9,8 @@ function img= solve_use_matrix( inv_model, data1, data2)
 % inv_model.solve_use_matrix.map = map to elements (optional)
 %    map is the size of the reconst model. contains
 %    zero if elem is unfilled, otherwise pointer to matrix row
+% inv_model.solve_use_matrix.solve_nodes (default to elements)
+%    if 1, then solution goes to the img.node_data value
 
 % (C) 2008 Andy Adler. License: GPL version 2 or version 3
 % $Id$
@@ -31,5 +33,11 @@ sol = rec(map,:);
 try   img.name= ['solved by ', inv_model.name];
 catch img.name= ['solved by solve_use_matrix'];
 end
-img.elem_data = sol;
+
+solve_to = 'elem_data';
+try if inv_model.solve_use_matrix.solve_nodes == 1
+   solve_to = 'node_data';
+end; end
+
+img.(solve_to) = sol;
 img.fwd_model= inv_model.fwd_model;
