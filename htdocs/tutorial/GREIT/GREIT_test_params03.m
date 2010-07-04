e8383f36bec4ddd0e20742b25ec704185aab39db
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2010-07-04 14:41:32 +0000

    greit test parameters
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@2240 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/GREIT/GREIT_test_params03.m
+++ htdocs/tutorial/GREIT/GREIT_test_params03.m
@@ -1,24 +1,12 @@
 % GREIT params eval $Id$
 
 % Reconstruct GREIT Images
-i_gr = mk_common_gridmdl('GREITc1');
-imgr = inv_solve(i_gr, vh, vi);
+imdl_gr = mk_common_gridmdl('GREITc1');
+imgc{1} = inv_solve(imdl_gr, vh, vi);
 
-imgr.calc_colours.npoints = 32;
-params = eval_GREIT_fig_merit(imgr, xyzr);
+% Reconstruct backprojection Images
+imdl_bp = mk_common_gridmdl('backproj');
+imgc{2} = inv_solve(imdl_bp, vh, vi);
 
-plot(r, params(1,:)); axis([0,0.9,0,2.0]);  ylabel('AR');
-print_convert('GREIT_test_params03_ar.png',  '-density 60',0.3);
-
-plot(r, params(2,:)); axis([0,0.9,-0.15,0.15]);  ylabel('PE');
-print_convert('GREIT_test_params03_pe.png',  '-density 60',0.3);
-
-plot(r, params(3,:)); axis([0,0.9,0,0.4]);  ylabel('RES');
-print_convert('GREIT_test_params03_res.png', '-density 60',0.3);
-
-plot(r, params(4,:)); axis([0,0.9,0,0.3]);  ylabel('SD');
-print_convert('GREIT_test_params03_sd.png',  '-density 60',0.3);
-
-plot(r, params(5,:)); axis([0,0.9,0,0.6]);  ylabel('RNG');
-print_convert('GREIT_test_params03_rng.png', '-density 60',0.3);
+fname_base = 'GREIT_test_params04';
 
