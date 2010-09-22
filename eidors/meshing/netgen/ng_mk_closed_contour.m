f22a65bcbf462118bd5d649048e9ee4513af448b
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2010-09-22 19:17:17 +0000

    mean offset
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@2264 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/meshing/netgen/ng_mk_closed_contour.m
+++ eidors/meshing/netgen/ng_mk_closed_contour.m
@@ -56,6 +56,8 @@ if nargin < 4; extra_ng_code = {'',''}; end
 [fmdl, mat_idx] = ng_mk_ellip_models(ellip_shape, elec_pos, ...
                   elec_shape, extra_ng_code);
 fmdl.nodes(:,1:2) = fmdl.nodes(:,1:2)*params.rot;
+fmdl.nodes(:,1) = fmdl.nodes(:,1) + params.xy_mean(1);
+fmdl.nodes(:,2) = fmdl.nodes(:,2) + params.xy_mean(2);
 
 function [ellip_shape,params] = proc_shape(ellip_shape);
   height = ellip_shape{1}; 
@@ -68,6 +70,49 @@ function [ellip_shape,params] = proc_shape(ellip_shape);
 
   [u,d,v]= svd(cov(xy_shape));
   params.rot = [1,0;0,-1]*u;
-  ellip_ax = sqrt(2*[d(1,1),d(2,2)])  
-pause(1);
+  params.xy_mean = mean(xy_shape,1);
+
+  ellip_ax = sqrt(2*[d(1,1),d(2,2)]); 
   ellip_shape= [height, ellip_ax, maxh];
+
+function [C, xf, yf] = fourier_fit(xc,yc,Nlist, doplot);
+% Fourier Fit
+% [1, sin(t1), sin(2*t1), ... cos(t1) ] * [A1] = [R1]
+%            ...
+% [1, sin(tN), sin(2*tN), ... cos(tN) ] * [AM] = [R2]
+if nargin<4; doplot=0;end
+
+xm= mean(xc);
+ym= mean(yc);
+
+if doplot
+   plot(xc,yc,'b-*');
+end
+
+yc= yc-ym;
+xc= xc-xm;
+
+for N= Nlist
+   ang = atan2(yc(:),xc(:));
+   rad = sqrt(yc(:).^2 + xc(:).^2);
+   A = ones(length(ang), 2*N+1);
+   for i=1:N
+     A(:,i+1  ) = cos(i*ang);
+     A(:,i+1+N) = sin(i*ang);
+   end
+   C = A\rad;
+
+   ang = linspace(0,2*pi,50)';
+   A = ones(length(ang), 2*N+1);
+   for i=1:N
+     A(:,i+1  ) = cos(i*ang);
+     A(:,i+1+N) = sin(i*ang);
+   end
+   rad = A*C;
+   xf = rad.*cos(ang) + xm;
+   yf = rad.*sin(ang) + ym;
+
+   if doplot
+      hold on;plot(xf,yf,'r-*'); hold off;
+   end
+end
