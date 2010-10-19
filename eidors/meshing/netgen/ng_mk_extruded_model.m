355e013cb9be7befee24501353290395d71d6f79
nightflight84 <nightflight84@82006732-fb7e-4a22-9f28-86a212cce563>
2010-10-19 15:28:26 +0000

    move function to internal
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@2282 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/meshing/netgen/ng_mk_extruded_model.m
+++ eidors/meshing/netgen/ng_mk_extruded_model.m
@@ -46,7 +46,7 @@ write_geo_file(geofn, ptsfn, tank_height, tank_shape, ...
 call_netgen( geofn, meshfn, ptsfn);
 
 [fmdl,mat_idx] = ng_mk_fwd_model( meshfn, centres, 'ng', [],0.01,...
-    'ng_remove_electrodes');
+    @ng_remove_electrodes);
 
 %delete(geofn); delete(meshfn); delete(ptsfn); % remove temp files
 
@@ -925,5 +925,59 @@ function write_circ_elec(fid,name,c, dirn, rd, centroid, maxh)
         c(1)+dirn(1),c(2)+dirn(2),c(3)+dirn(3), rd, ...
         centroid(1), centroid(2), 0, -dirn(1), -dirn(2), dirn(3),maxh);
      
+function [srf,vtx,fc,bc,simp,edg,mat_ind] = ng_remove_electrodes...
+    (srf,vtx,fc,bc,simp,edg,mat_ind, N_elec)
+% NG_REMOVE_ELECTRODES: cleans up matrices read from a *.vol file
+% [srf,vtx,fc,bc,simp,edg,mat_ind]= ng_remove_electrodes...
+%     (srf,vtx,fc,bc,simp,edg,mat_ind, N_elec)
+%
+% Used to clean up external objects used to force electrode meshing in
+% ng_mk_extruded_model.
+%
+% (C) Bartlomiej Grychtol, 2010. Licenced under GPL v2 or v3
+% $Id$
+
+% total objects:
+N_obj = max(mat_ind);
+
+% electrode simps:
+e_simp_ind = mat_ind > (N_obj - N_elec);
+
+in = unique(simp(~e_simp_ind,:));
+out = unique(simp(e_simp_ind,:));
+boundary = intersect(in,out);
+out = setdiff(out,boundary);
+
+ext_srf_ind = ismember(srf,out);
+ext_srf_ind = ext_srf_ind(:,1) | ext_srf_ind(:,2) | ext_srf_ind(:,3);
+
+srf(ext_srf_ind,:) = [];
+bc(ext_srf_ind,:) = [];
+fc(ext_srf_ind,:) = [];
+simp = simp(~e_simp_ind,:);
+mat_ind = mat_ind(~e_simp_ind);
+
+% fix bc:
+n_unique = numel(unique(bc));
+missing = setdiff(1:n_unique, unique(bc));
+spare = setdiff(unique(bc), 1:n_unique); 
+for i = 1:length(missing)
+    bc( bc==spare(i) ) = missing(i);
+end
+
+% fic vtx:
+v = 1:size(vtx,1);
+unused_v = setdiff(v, union(unique(simp),unique(srf))); 
+v(unused_v) = [];
+for i = 1: length(vtx)
+%     simp_ind = find(simp == i);
+%     srf_ind = find( srf == i);
+    new_v_ind = find(v == i);
+    simp( simp == i ) = new_v_ind; 
+    srf( srf  == i ) = new_v_ind;
+end
+vtx(unused_v,:) = [];
+
+
 
      
