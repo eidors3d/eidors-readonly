3039759cc9cde5b475b11a44e4079fd7b6115ba1
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2010-10-29 14:48:47 +0000

    mods to make the final image

--- trunk/htdocs/tutorial/netgen/extrusion/pig_body02.m
+++ trunk/htdocs/tutorial/netgen/extrusion/pig_body02.m
@@ -7,9 +7,9 @@ electh= atan2(elec_pos(:,2),elec_pos(:,1))*180/pi; % electh=electh(1:3);
 [stim,meas_sel] = mk_stim_patterns(16,1,[0,1],[0,1],{'no_meas_current'}, 1);
 fmdl.stimulation = stim;
 
-fmdl.nodes = fmdl.nodes*diag([1,-1,1]); % Flip y axis image to plot
+fmdl.nodes = fmdl.nodes*diag([-1,-1,1]); % Flip x,y axis to match medical direction
 img=mk_image(fmdl,1);
-img.elem_data(mat_idx{2})= 0.5;
+img.elem_data(mat_idx{2})= 0.3;
 
 clf; show_fem(img); view(0,70);
 print_convert pig_body02.jpg
