4c96f283690f0a3f97bee41cf4b46bba98cbbe98
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2010-10-29 14:48:47 +0000

    mods to make the final image
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@2320 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/netgen/extrusion/pig_body05.m
+++ htdocs/tutorial/netgen/extrusion/pig_body05.m
@@ -4,7 +4,7 @@ img_v.fwd_model.mdl_slice_mapper.level = [inf,inf,1.0];
 
  q = show_current(img_v, vh.volt(:,1));
 
-sx = centroid(1) + linspace(-1,1,15)';
+sx =-centroid(1) - linspace(-1,1,15)';
 sy =-centroid(2) + linspace(-1,1,15)';
 hh=streamline(q.xp,q.yp, q.xc, q.yc,sx,sy);
 hh=streamline(q.xp,q.yp,-q.xc,-q.yc,sx,sy);
