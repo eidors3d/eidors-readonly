2b695df28158f4c1966fceac9a7cfb76b90913c0
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2010-10-29 14:53:12 +0000

    2D model

---
+++ trunk/htdocs/tutorial/netgen/extrusion/pig_body07.m
@@ -0,0 +1,6 @@
+fmdlr = ng_mk_extruded_model({0,trunk,[4,50],.1},[0,0],[0.1]);
+fmdlr.nodes = fmdlr.nodes*diag([-1,-1]);
+
+show_fem(fmdlr);
+clf; show_fem(fmdlr); view(0,70);
+print_convert pig_body07.jpg
