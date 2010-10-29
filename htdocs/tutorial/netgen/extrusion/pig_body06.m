c1e4629010d197e5a46609989e5ef8d8bea78a17
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2010-10-29 14:58:17 +0000

    images
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@2322 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/netgen/extrusion/pig_body06.m
+++ htdocs/tutorial/netgen/extrusion/pig_body06.m
@@ -6,4 +6,4 @@ set(gca,'YDir','normal');
 hh=streamline(q.xp,q.yp, q.xc, q.yc,sx,sy); set(hh,'Linewidth',2);
 hh=streamline(q.xp,q.yp,-q.xc,-q.yc,sx,sy); set(hh,'Linewidth',2);
 
-axis equal; axis tight; axis off; print_convert pig_body05a.jpg
+axis equal; axis tight; axis off; print_convert pig_body06a.jpg
