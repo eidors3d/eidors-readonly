f95b6b7794fee4952f7a9f8ca37f76a51f660092
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2010-10-31 14:29:18 +0000

    pig body

--- trunk/htdocs/tutorial/netgen/extrusion/pig_body05.m
+++ trunk/htdocs/tutorial/netgen/extrusion/pig_body05.m
@@ -1,8 +1,15 @@
-img_v.fwd_model.mdl_slice_mapper.npx = 128;
-img_v.fwd_model.mdl_slice_mapper.npy = 128;
+img_v.fwd_model.mdl_slice_mapper.npx = 1000;
+img_v.fwd_model.mdl_slice_mapper.npy = 1000;
 img_v.fwd_model.mdl_slice_mapper.level = [inf,inf,1.0];
 
+% Calculate at high resolution
  q = show_current(img_v, vh.volt(:,1));
+
+% Lower resolution to visualize
+img_v.fwd_model.mdl_slice_mapper.npx = 64;
+img_v.fwd_model.mdl_slice_mapper.npy = 64;
+show_current(img_v, vh.volt(:,1));
+
 
 sx =-centroid(1) - linspace(-1,1,15)';
 sy =-centroid(2) + linspace(-1,1,15)';
