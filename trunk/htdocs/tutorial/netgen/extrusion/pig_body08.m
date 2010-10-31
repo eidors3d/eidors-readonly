4a80cce376313d13ba6a8eada41f505214d312e3
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2010-10-31 11:50:21 +0000

    pig body

--- trunk/htdocs/tutorial/netgen/extrusion/pig_body08.m
+++ trunk/htdocs/tutorial/netgen/extrusion/pig_body08.m
@@ -3,10 +3,8 @@ img.elem_data( mat_idx{2} ) = 0.3;
 vh= fwd_solve(img);
 
 % Put a ball in the object center
-targ= mk_c2f_circ_mapping(fmdl, [-2.8;-2.8;1;0.3]);
+targ= mk_c2f_circ_mapping(fmdl, [-2.2;-1.2;1;0.3]);
 img.elem_data = img.elem_data + targ*.5;
-subplot(234); show_fem(img); view(2);
-title('Simulated change region');
 
 show_fem(img); view(0,90);
 print_convert pig_body08a.jpg
@@ -14,6 +12,8 @@ print_convert pig_body08a.jpg
 
 vi = fwd_solve(img);
 vi = add_noise( 3, vi, vh );
-plot([vh.meas, 100*(vi.meas - vh.meas)]);
+plot([vh.meas,  20*(vi.meas - vh.meas)]);
+axis tight;
+legend('meas ref','20x diff meas','Location','SouthWest');
 
 print_convert pig_body08b.jpg
