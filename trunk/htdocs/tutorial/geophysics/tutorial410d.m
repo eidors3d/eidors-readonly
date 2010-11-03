42267414a896a9d0d6e12d3b49fd0644991359fe
dgibert <dgibert@82006732-fb7e-4a22-9f28-86a212cce563>
2010-11-03 15:35:00 +0000

    show slices has changed

--- trunk/htdocs/tutorial/geophysics/tutorial410d.m
+++ trunk/htdocs/tutorial/geophysics/tutorial410d.m
@@ -7,6 +7,6 @@ show_fem(gallery_3D_img);
 subplot(122)
 gallery_3D_resist= gallery_3D_img; % Create resistivity image
 gallery_3D_resist.elem_data= 1./gallery_3D_img.elem_data;
-show_slices(gallery_3D_resist,[inf,inf,0],20,20);
+show_slices(gallery_3D_resist,[inf,inf,0]);
 
 %print -r125 -dpng tutorial410d.png;
