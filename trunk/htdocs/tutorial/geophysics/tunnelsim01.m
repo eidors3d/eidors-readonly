e18cab58ba0135e0b015ab5c64d1923a9a01f0b8
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2010-11-08 08:18:58 +0000

    fix view

--- trunk/htdocs/tutorial/geophysics/tunnelsim01.m
+++ trunk/htdocs/tutorial/geophysics/tunnelsim01.m
@@ -1,4 +1,4 @@
-% Create 3D model of a tunnel $Id:$ 
+% Create 3D model of a tunnel $Id$ 
 
 N_elec = 16;
 shape_str = ['solid incyl  = cylinder (0,0,0; 1,0,0; 1) -maxh=1.0; \n', ...
@@ -12,12 +12,12 @@ cth= cos(th); sth=sin(th); zth= zeros(size(th));
  elec_shape=[0.01];
  elec_obj = 'incyl';
  fmdl = ng_mk_gen_models(shape_str, elec_pos, elec_shape, elec_obj);
-show_fem( fmdl );
+subplot(221); show_fem( fmdl );
+
 view(90,0); print_convert tunnelsim01a.png
 view(0,0); print_convert tunnelsim01b.png
 
 crop_model([],  inline('x>=0.5','x','y','z'))
 crop_model([],  inline('x<=-0.5','x','y','z'))
 crop_model([],  inline('(y.^2+z.^2)>=1.3^2','x','y','z'))
-view(-90,50); print_convert tunnelsim01c.png
-
+view(-90,50); axis tight; print_convert tunnelsim01c.png
