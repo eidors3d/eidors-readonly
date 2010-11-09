f1bd0c95d2c4daf558ed7f6b1829291b12a28e54
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2010-11-09 08:00:19 +0000

    added caching to analytic mdl

--- trunk/eidors/models/a_adler/analytic_2d_circle.m
+++ trunk/eidors/models/a_adler/analytic_2d_circle.m
@@ -34,7 +34,7 @@ function V = analytic_2d_circle(I, params)
 % plot([vi,vsi]);
 
 
-if ~all(sum(I,1) == 0); error('net I must be 0'); end
+if ~all(abs(sum(I,1)) <1e-12); error('net I must be 0'); end
 [ll,nv] = size(I);
 
 IF= fft(I); IF(1)= []; % Don't include zero term
@@ -44,19 +44,26 @@ VF= zeros(size(I));
 VF(1+[ll2, ll-ll2],:)= [VF2;conj(VF2)];
 V = ifft(VF);
 
-if norm(imag(V))>1e-10; error('Imaginary output'); end
+if norm(imag(V))>1e-10; error('Unexpected Imaginary output - probably a bug'); end
 V= real(V);
 
 
 function [SM,ll2] = solve_matrix(ll, params)
+  ll1= ll-1; % IGNORE DC
+  ll2 = 1:ceil(ll1/2);
+
+  SM = eidors_obj('get-cache', {ll,params}, 'analytic_2d_circle');
+  if ~isempty(SM)
+     eidors_msg('analytic_2d_circle: using cached value', 3);
+     return
+  end
+  
    s_h = params(1);
    s_i = params(2);
    b   = params(3);
    a   = params(4);
    alp = params(5);
 
-   ll1= ll-1; % IGNORE DC
-   ll2 = 1:ceil(ll1/2);
    mll2= max(ll2);
    K = ll2(:);
    D = spdiags(K,0,mll2,mll2);
@@ -80,12 +87,16 @@ function [SM,ll2] = solve_matrix(ll, params)
 
 
 
-   T = zeros(mll2);
+   T = sparse(mll2,mll2);
    for m=ll2; for n=ll2
      sm= 0;
      for p=1:min(m,n)
 %      sm=sm+ nchoosek(m-1,p-1)*nchoosek(n,p)*ab2^p;
-       sm=sm+ nchoosek(m-1,p-1)*nchoosek(n,p)*a^(2*p)*b^(m+n-2*p);
+       % stop calculating nchoosek when too small, because 
+       dsm = nchoosek(m-1,p-1)*nchoosek(n,p)*a^(2*p)*b^(m+n-2*p);
+%       disp([m,n,p,log10(dsm)])
+       if dsm < 1e-11; break; end  
+       sm=sm + dsm;
      end
 %    T(i,j) = mu*(sm/m)* b^(m+n) * exp(1j*(m-n)*alp);
      T(m,n) = mu*(sm/m)* exp(1j*(m-n)*alp);
@@ -93,3 +104,6 @@ function [SM,ll2] = solve_matrix(ll, params)
 
    I= speye(mll2);
    SM = (I-T*D)\(I+T*D)*iD;
+
+   eidors_obj('set-cache', {ll,params}, 'analytic_2d_circle', SM);
+   eidors_msg('analytic_2d_circle: setting cached value', 3);
