0383fbbc5f83bb2a9d2f7b94affd88bd92ce9305
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2010-11-12 10:32:14 +0000

    work for non 16 electrodes
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@2384 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/algorithms/a_adler/calc_reciproc_error.m
+++ eidors/algorithms/a_adler/calc_reciproc_error.m
@@ -3,6 +3,9 @@ function meas_icov = calc_reciproc_error(inv_model, data )
 % meas_icov = calc_reciproc_error(inv_model, data )
 %
 % Calculate meas_icov from data
+% 
+% specify tau as 
+%    inv_model.calc_reciproc_error.tau
 
 % Reference: Real-time management of faulty electrodes in
 %  electrical impedance tomography AE Hartinger, R Guardo,
@@ -11,16 +14,23 @@ function meas_icov = calc_reciproc_error(inv_model, data )
 % (C) 2008 Andy Adler. License: GPL version 2 or version 3
 % $Id$
 
-% Only works for 16 electrode data
+try
+   tau == inv_model.calc_reciproc_error.tau;
+catch
+   tau = 2.5e-6;
+end
+
+% Use only with no_rotate
+Nel = length(inv_model.fwd_model.electrode);
 nframes= size(data,2);
 nmeas = size(data,1);
-if nmeas ~= 256
-   data2= zeros(256, nframes);
+if nmeas ~= Nel^2
+   data2= zeros(Nel^2, nframes);
    mselect = inv_model.fwd_model.meas_select;
    data2(mselect,:)= data;
-   data2= reshape( data2, 16,16,nframes);
+   data2= reshape( data2, Nel,Nel,nframes);
 else 
-   data2= reshape( data, 16,16,nframes);
+   data2= reshape( data, Nel,Nel,nframes);
 end
 
 ndata2= abs(data2)/max(abs(data2(:)));
@@ -28,7 +38,6 @@ ndata2r= permute(ndata2, [2,1,3]); % Transpose
 recerr= mean( ndata2 - ndata2r, 3);
 e2    = recerr.^2; % eqn 8 from paper
 
-tau = 2.5e-6;
 s2  = exp(e2/tau);
 
 if isfield(inv_model.fwd_model,'meas_select')
