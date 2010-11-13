ec5aff4df412cbe43c0e9f35138d37c1ed763580
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2010-11-13 09:33:31 +0000

    allow rm electrode list as paramter
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@2388 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/algorithms/a_adler/meas_icov_rm_elecs.m
+++ eidors/algorithms/a_adler/meas_icov_rm_elecs.m
@@ -1,16 +1,20 @@
 function meas_icov = meas_icov_rm_elecs( imdl, elec_list )
 % MEAS_ICOV_RM_ELECS: remove electrodes from consideration
 % meas_icov = meas_icov_rm_elecs( inv_model, elec_list )
+%
+% elec_list = numbers of elecs to remove
+%   or as imdl.meas_icov_rm_elecs.elec_list
 % 
 % Reference Accounting for erroneous electrode data in EIT
 % A. Adler Physiological Measurement, 25(1):227-238, 2004. 
-%
-% elec_list = numbers of elecs to remove
 
 % (C) 2008 Andy Adler. License: GPL version 2 or version 3
 % $Id$
 
-% TODO: put elec_list into a parameter
+if nargin< 2
+   elec_list = imdl.meas_icov_rm_elecs.elec_list;
+end
+
 meas_icov = [];
 for stim = imdl.fwd_model.stimulation(:)'
    mp = stim.meas_pattern;
