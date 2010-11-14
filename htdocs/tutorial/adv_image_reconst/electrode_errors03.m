9c5f32d9dd8f97ce91c0c22b4cf7e38379a00da1
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2010-11-14 09:07:49 +0000

    image reconstruction with electrode errors
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@2392 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/adv_image_reconst/electrode_errors03.m
+++ htdocs/tutorial/adv_image_reconst/electrode_errors03.m
@@ -1,6 +1,6 @@
 % Reciprocity error + Image $Id$
 
-imdl.calc_reciproc_error.tau = 3e-3;
+imdl.calc_reciproc_error.tau = 3e-4;
 imdl.meas_icov = calc_reciproc_error( imdl, vi );
 
 img = inv_solve(imdl, vh, vi(:,20)); show_fem(img,[0,1,0]);
