c47ba82d21ec4703e9a403255109bfbcacefbcd0
nightflight84 <nightflight84@82006732-fb7e-4a22-9f28-86a212cce563>
2011-03-01 09:36:21 +0000

    accept image structres with many images
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@2443 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/graphics_matlab/calc_slices.m
+++ eidors/graphics_matlab/calc_slices.m
@@ -13,6 +13,7 @@ function rimg = calc_slices( img, levels );
 % PARAMETERS:
 %   img.calc_slices.filter % Filter to be applied to images
 %      Example:    img.calc_slices.filter = ones(3)/9
+%   img.calc_slices.scale  % Scaling to apply to images
 %
 % rimg= np x np x I x L where np is 128 by default
 % np can be adjusted by calc_colours('npoints')
@@ -27,59 +28,66 @@ try   np = img(1).calc_colours.npoints;
 end
 
 % Assume all fwd_models are same dimension (all 3D or 2D no mixed dims)
-if n_dims(img(1))==2
-   levels= [Inf,Inf,0];
+if n_dims(img(1))==2 
    if nargin>1 && ~isempty(levels);
-       warning('specified levels ignored for 2D FEM');
+       if ~all(levels(1,:) == [inf,inf,0])
+          warning('specified levels ignored for 2D FEM');
+       end
    end
+   levels= [Inf,Inf,0];
 end
 
 
-
-rimg = calc_this_slice( img(1), levels, np);
+rimg = [];
+for i=1:length(img)
+   rimg = cat(3, rimg, calc_this_slice( img(i), levels, np) );
+end
 
 function rimg = calc_this_slice( img, levels, np)
-% If scalar levels then we just create that many cut planes on z-dimension
-fwd_model = img.fwd_model;
-if size(levels)== [1,1]
-   zmax= max(fwd_model.nodes(:,3));
-   zmin= min(fwd_model.nodes(:,3));
-   levels = linspace(zmin,zmax, levels+2);
-   levels = levels(2:end-1)'*[Inf,Inf,1];
-end
-num_levs= size(levels,1);
-if isfield(img,'elem_data')
-   [elem_data, n_images] = get_img_data(img);
-   rimg=zeros(np,np,n_images,num_levs);
+    % If scalar levels then we just create that many cut planes on z-dimension
+    fwd_model = img.fwd_model;
+    if size(levels)== [1,1]
+       zmax= max(fwd_model.nodes(:,3));
+       zmin= min(fwd_model.nodes(:,3));
+       levels = linspace(zmin,zmax, levels+2);
+       levels = levels(2:end-1)'*[Inf,Inf,1];
+    end
+    num_levs= size(levels,1);
+    if isfield(img,'elem_data')
+       [elem_data, n_images] = get_img_data(img);
+       rimg=zeros(np,np,n_images,num_levs);
 
-   for lev_no = 1:num_levs
-      level= levels( lev_no, 1:3 );
+       for lev_no = 1:num_levs
+          level= levels( lev_no, 1:3 );
 
-      rimg(:,:,:,lev_no) = calc_image_elems( elem_data, level, fwd_model, np);
-   end
-elseif isfield(img,'node_data')
-   node_data= [img.node_data];
-   if size(node_data,1)==1; node_data=node_data';end
-   n_images= size(node_data,2);
-   rimg=zeros(np,np,n_images,num_levs);
+          rimg(:,:,:,lev_no) = calc_image_elems( elem_data, level, fwd_model, np);
+       end
+    elseif isfield(img,'node_data')
+       node_data= [img.node_data];
+       if size(node_data,1)==1; node_data=node_data';end
+       n_images= size(node_data,2);
+       rimg=zeros(np,np,n_images,num_levs);
 
-   for lev_no = 1:num_levs
-      level= levels( lev_no, 1:3 );
+       for lev_no = 1:num_levs
+          level= levels( lev_no, 1:3 );
 
-      rimg(:,:,:,lev_no) = calc_image_nodes( node_data, level, fwd_model, np);
-   end
-else
-   error('img does not have a data field');
-end
+          rimg(:,:,:,lev_no) = calc_image_nodes( node_data, level, fwd_model, np);
+       end
+    else
+       error('img does not have a data field');
+    end
 
-% FILTER IMAGE
-try   filt = img.calc_slices.filter; 
-catch filt = []; end
+    % FILTER IMAGE
+    try   filt = img.calc_slices.filter; 
+    catch filt = 1; end
+    try   scal = img.calc_slices.scale; 
+    catch scal = 1; end
 
-if ~isempty(filt)
-   filt = filt/sum(filt(:));
-   rimg = filter_image(rimg, filt);
-end
+    
+    if filt*scal ~= 1
+       filt = scal * ( filt/sum(filt(:)) );
+       rimg = filter_image(rimg, filt);
+    end
 
 % Calculate an image by mapping it onto the node_ptr matrix
 % This makes a blocky image to nearest node -> no longer used
@@ -147,7 +155,7 @@ function  rimg = filter_image(rimg, filt);
    end; end
 
 function do_unit_test
-   img = calc_jacobian_bkgnd( mk_common_model('a2c2',8));
+   img = mk_image( mk_common_model('a2c2',8));
    img.calc_colours.npoints = 8; 
 
    imc = calc_slices(img);
@@ -200,7 +208,21 @@ function do_unit_test
    do_indiv_test('cs 3d 3', imn, imt);
 
 
+% multi image struct
+   img = mk_image( mk_common_model('a2c2',8));
+   img.calc_colours.npoints = 8; 
+   img(2) = img;
+   imc = calc_slices(img); 
+   imt = NaN*ones(8); imt(3:6,2:7) = 1; imt(2:7,3:6) = 1; 
+   do_indiv_test('cs mult 1', imc, cat(3,imt,imt));
 
+   imgb = mk_image( mk_common_model('b2c2',8));
+   imgb.calc_colours.npoints = 8; 
+   img(3)=imgb;
+   imc = calc_slices(img); 
+   do_indiv_test('cs mult 2', imc, cat(3,imt,imt,imt));
+
+   
 function do_indiv_test(txt,a,b, tol)
    if nargin < 4; tol = 0; end
    fprintf('%10s = ',txt);
