d69caa226d2ce63aa70eda6efa066f7bd66fa481
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2011-03-02 20:13:47 +0000

    matrix
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@2471 82006732-fb7e-4a22-9f28-86a212cce563

---
+++ htdocs/tutorial/GREIT/mk_GREIT_matrix01.m
@@ -0,0 +1,8 @@
+$Name$
+n_elecs = 16;
+fmdl = ng_mk_cyl_models([2 2 0.2] ,[n_elecs,1],[0.1]);
+fmdl.stimulation =  mk_stim_patterns(n_elecs,1,[0,1],[0,1],{'no_meas_current'}, 1);
+fmdl.normalize_measurements = 0;
+
+show_fem(fmdl); view(0,70);
+print_convert mk_GREIT_matrix01a.png '-density 60'
