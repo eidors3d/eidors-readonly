dbba6a42970ec806a19a54a5018826d60bde24f7
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2011-03-02 08:42:27 +0000

    add tests and docs

--- trunk/eidors/models/a_adler/num_nodes.m
+++ trunk/eidors/models/a_adler/num_nodes.m
@@ -1,8 +1,10 @@
-function num = n_nodes( mdl );
-% N_NODES: number of elemnts in a (fwd or inv model or image)
-% num_nodes = n_nodes( mdl );
+function num = num_nodes( mdl );
+% NUM_NODES: number of elemnts in a (fwd or inv model or image)
+% n_nodes = num_nodes( mdl );
 
 % $Id$
+
+if isstr(mdl) && strcmp(mdl,'UNIT_TEST'); do_unit_test; return; end
 
 switch mdl.type
   case 'image';      fmdl = mdl.fwd_model;
@@ -13,3 +15,18 @@ switch mdl.type
 end
 
 num = size(fmdl.nodes,1);
+
+function do_unit_test
+   mdl = mk_common_model('a2c2',8);
+   ne = num_nodes( mdl );
+   ok='fail'; if ne==41; ok='ok'; end; fprintf('test1: %10s\n',ok);
+
+   ne = num_nodes( mdl.fwd_model );
+   ok='fail'; if ne==41; ok='ok'; end; fprintf('test2: %10s\n',ok);
+
+   ne = num_nodes( mk_image( mdl ));
+   ok='fail'; if ne==41; ok='ok'; end; fprintf('test3: %10s\n',ok);
+
+   mdl = mk_common_model('n3r2',16);
+   ne = num_nodes( mk_image( mdl ));
+   ok='fail'; if ne==252; ok='ok'; end; fprintf('test4: %10s\n',ok);
