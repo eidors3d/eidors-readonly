8e5d75a7fc982fdbd810d6dcbd541d7bac3c800f
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2011-03-02 20:32:23 +0000

    calculate reconstruction matrix

--- trunk/htdocs/tutorial/GREIT/mk_GREIT_matrix01.m
+++ trunk/htdocs/tutorial/GREIT/mk_GREIT_matrix01.m
@@ -3,6 +3,7 @@ n_elecs = 16;
 fmdl = ng_mk_cyl_models([2 2 0.2] ,[n_elecs,1],[0.1]);
 fmdl.stimulation =  mk_stim_patterns(n_elecs,1,[0,1],[0,1],{'no_meas_current'}, 1);
 fmdl.normalize_measurements = 0;
+img = mk_image(fmdl,1); % Homogeneous background
 
 show_fem(fmdl); view(0,70);
 print_convert mk_GREIT_matrix01a.png '-density 60'
