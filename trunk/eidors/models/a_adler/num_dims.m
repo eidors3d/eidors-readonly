05d3b45ffbdfa8ee97deda1a3602a449cdcfa2c4
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2011-03-02 08:33:35 +0000

    rename to have less likely conflicts. Add tests

---
+++ trunk/eidors/models/a_adler/num_dims.m
@@ -0,0 +1,28 @@
+function num = mdl_dims( mdl );
+% MDL_DIMS: dimension of model space (are nodes in 2D or 3D space)
+%  Note, mdl_dims is not the same as the element dimentions
+% num_dims = n_dims( mdl );
+% num_dims = 2 if 2D, 3 if 3D
+
+% $Id$
+
+if isstr(mdl) && strcmp(mdl,'UNIT_TEST'); do_unit_test; return; end
+
+switch mdl.type
+  case 'image';      fmdl = mdl.fwd_model;
+  case 'inv_model';  fmdl = mdl.fwd_model;
+  case 'fwd_model';  fmdl = mdl;
+  otherwise;
+      error('can''t process model of type %s', mdl.type );
+end
+
+num = size(fmdl.nodes,2);
+
+function do_unit_test
+   mdl = mk_common_model('a2c2',8);
+   ne = mdl_dims( mdl );
+   ok='fail'; if ne==2; ok='ok'; end; fprintf('test1: %10s\n',ok);
+
+   mdl = mk_common_model('n3r2',16);
+   ne = num_elems( mk_image( mdl )) 
+   ok='fail'; if ne==3; ok='ok'; end; fprintf('test2: %10s\n',ok);
---
+++ trunk/eidors/models/a_adler/num_elems.m
@@ -0,0 +1,33 @@
+function num = num_elems( mdl );
+% NUM_ELEMS: number of elemnts in a (fwd or inv model or image)
+% num_elems = n_elems( mdl );
+
+% $Id$
+
+if isstr(mdl) && strcmp(mdl,'UNIT_TEST'); do_unit_test; return; end
+
+switch mdl.type
+  case 'image';      fmdl = mdl.fwd_model;
+  case 'inv_model';  fmdl = mdl.fwd_model;
+  case 'fwd_model';  fmdl = mdl;
+  otherwise;
+      error('can''t process model of type %s', mdl.type );
+end
+
+num = size(fmdl.elems,1);
+
+
+function do_unit_test
+   mdl = mk_common_model('a2c2',8);
+   ne = num_elems( mdl );
+   ok='fail'; if ne==64; ok='ok'; end; fprintf('test1: %10s\n',ok);
+
+   ne = num_elems( mdl.fwd_model );
+   ok='fail'; if ne==64; ok='ok'; end; fprintf('test2: %10s\n',ok);
+
+   ne = num_elems( mk_image( mdl ));
+   ok='fail'; if ne==64; ok='ok'; end; fprintf('test3: %10s\n',ok);
+
+   mdl = mk_common_model('n3r2',16);
+   ne = num_elems( mk_image( mdl )) 
+   ok='fail'; if ne==828; ok='ok'; end; fprintf('test4: %10s\n',ok);
