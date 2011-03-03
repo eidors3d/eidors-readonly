b95f72059900dcec67c92e0f9523e9b84035cd40
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2011-03-03 22:44:11 +0000

    working files

--- trunk/htdocs/tutorial/GREIT/pig_ex01.m
+++ trunk/htdocs/tutorial/GREIT/pig_ex01.m
@@ -22,41 +22,7 @@ fmdl.nodes = fmdl.nodes*diag([-1,-1,1]);
 
 img = mk_image(fmdl,1);
 img.elem_data( mat_idx{2} ) = 0.25;
-%% Train GREIT
-opt.imgsz = [64 64]; % 64-by-64 image (yes, we can do that now)
-opt.distr = 3; % non-random, uniform
-opt.Nsim = 500; % 500 hundred targets to train on, seems enough
-opt.target_size = 0.01; %small targets
-opt.target_offset = 0;
-opt.noise_figure = 0.5; % this is key!
-imdl=mk_GREIT_model(img, 0.25, [], opt);
 
-%% Read in the data
-ctrl = eidors_readdata('2-control.RAW');
-inj  = eidors_readdata('2-injury.RAW');
-
-vh_ctrl = mean(ctrl(:,80:120),2);
-ex_ctrl = ctrl(:,101);
-in_ctrl = ctrl(:,103);
-
-vh_inj = mean(inj(:,80:120),2);
-ex_inj = inj(:,99);
-in_inj = inj(:,101);
-%% Reconstruct
-img_in_ctrl = inv_solve(imdl,vh_ctrl,in_ctrl);
-img_ex_ctrl = inv_solve(imdl,vh_ctrl,ex_ctrl);
-img_ctrl = img_in_ctrl; 
-img_ctrl.elem_data = img_ctrl.elem_data - img_ex_ctrl.elem_data;
-figure
-show_fem(img_ctrl);
-
-img_in_inj= inv_solve(imdl,vh_inj,in_inj);
-img_ex_inj = inv_solve(imdl,vh_inj,ex_inj);
-img_inj = img_in_inj; 
-img_inj.elem_data = img_ctrl.elem_data - img_ex_inj.elem_data;
-figure
-show_fem(img_inj);
-
-%%
-print_convert('injury','-density 60');
-print_convert('control','-density 60');
+show_fem(img);
+print -dpng -r75 pig_ex_fmdl.png
+%print_convert pig_ex_fmdl.png '-density 75';
