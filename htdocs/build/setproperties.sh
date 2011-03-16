aeaee286ba706c7b2df191fc60c339d004e5f6b7
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2011-03-16 12:14:16 +0000

    move build dir
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@2501 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/build/setproperties.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-find ../..  \
-        -name \*.m \
-     -o -name \*.cpp \
-     -o -name \*.html \
-     -o -name \*.shtml \
-    | grep -v .svn \
-    | xargs svn propset svn:keywords "Date Author Id"
