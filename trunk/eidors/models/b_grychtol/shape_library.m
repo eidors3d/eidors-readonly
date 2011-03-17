2b5487d6a84379022bbdf486b2c674b6749c8ce7
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2011-03-17 16:39:04 +0000

    initial structure

--- trunk/eidors/models/b_grychtol/shape_library.m
+++ trunk/eidors/models/b_grychtol/shape_library.m
@@ -26,3 +26,31 @@ function out = shape_library(action, shape, varargin)
 
 % (C) Bartlomiej Grychtol, 2011. License: GPL version 2 or version 3
 % $Id:$
+
+
+switch action
+    case 'UNIT_TEST'
+        do_unit_test;
+        return
+    case 'list'
+        list_shapes;
+    case 'show'
+        show_shape;
+    case 'get'
+        get_shape;
+end
+
+function list_shapes
+disp('list_shapes');
+
+function show_shape
+disp('show_shape');
+
+function get_shape
+disp('get_shape');
+
+function do_unit_test
+    shape_library('list');
+    shape_library('list','pig_32kg');
+    shape_library('show','pig32kg');
+    shape_library('get','pig_32kg','trunk','lungs')
