e4f7050e56707af59c59a33e7b707ed88de4d189
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2011-04-10 13:49:15 +0000

    Refactor for arbitrary model shape
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@2542 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/algorithms/a_adler/eval_GREIT_fig_merit.m
+++ eidors/algorithms/a_adler/eval_GREIT_fig_merit.m
@@ -13,13 +13,16 @@ function params = eval_GREIT_fig_merit(imgs, xyzr_pt)
 
 % (C) 2009 Andy Adler. Licensed under GPL v2 or v3
 % $Id$
-
+mdl = imgs.fwd_model;
 imgs = calc_slices(imgs);
+map = ~isnan(squeeze(imgs(:,:,1))); %assume all imgs are the same shape
 imgs(isnan(imgs)) = 0;
+sz = size(imgs,1);
+[x,y,bb_min,bb_max]=prepare_grid(sz,mdl);
 
 N_imgs = size(imgs,3);
 for i= 1:N_imgs
-   [xmean,ymean,equiv_circ,map,qmi,img] = calc_cofg(imgs(:,:,i));
+   [xmean,ymean,equiv_circ,qmi,img] = calc_cofg(imgs(:,:,i),map,x,y);
    params(1,i) = calc_amplitude( img );
    params(2,i) = calc_posn_error( qmi, xmean, ymean, xyzr_pt(1:2,i) );
    params(3,i) = calc_resolution( qmi, map );
@@ -28,7 +31,13 @@ for i= 1:N_imgs
 end
 
 % TODO: Fix this when we start to care about units
-params(1,:) = params(1,:)/mean(params(1,1:10));
+ctr = bb_min + 0.5*(bb_max-bb_min);
+r = max(0.5*(bb_max-bb_min));
+ctr_pts = sum((xyzr_pt(1:2,:)-repmat(ctr',1,size(xyzr_pt,2))).^2) < (0.05*r)^2;
+%params(1,:) = params(1,:)/mean(params(1,1:10));  
+params(1,:) = params(1,:)/mean(params(1,ctr_pts));
+
+
 
 function ampl = calc_amplitude(img)
    ampl = sum(img(:));
@@ -46,13 +55,24 @@ function sd  = calc_shape_deform(qmi, equiv_circ)
 function rr = calc_ringing(img, qmi );
    ring_part =  img .* ( (img<0) & ~qmi);
    rr = -sum( ring_part(:) )/sum( img(:).*qmi(:) );
-
-function [xmean,ymean,equiv_circ,map,qmi,img] = calc_cofg(img);
+   
+function [x,y,bb_min,bb_max]=prepare_grid(sz,mdl)
+      % bounding box
+   bnd = unique(mdl.boundary);
+   bb_min = min(mdl.nodes(bnd,:));
+   bb_max = max(mdl.nodes(bnd,:));
+   
+   [x,y]=meshgrid(linspace(bb_min(1),bb_max(1),sz),linspace(bb_min(2),bb_max(2),sz)); 
+   
+function [xmean,ymean,equiv_circ,qmi,img] = calc_cofg(img,map,x,y);
 %  if abs(max(img(:))) < abs(min(img(:))); img= -img; end
    sz = size(img,1);
    qmi = calc_hm_set( img, 0.25 );
    if sum(img(:) & qmi(:))<0 ; keyboard ; end
-   [x,y]=meshgrid(linspace(-1,1,sz),linspace(-1,1,sz)); map = x.^2+y.^2<1.1;
+   
+   
+
+   %map = x.^2+y.^2<1.1;
    qmi = qmi.*map; img = img.*map;
 
    ss_qmi = sum(qmi(:));
