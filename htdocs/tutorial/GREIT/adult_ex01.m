28fc11fbbd359c8ce652f4d300c886d05ac31c75
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2011-04-28 15:14:42 +0000

    mods
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@2551 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/GREIT/adult_ex01.m
+++ htdocs/tutorial/GREIT/adult_ex01.m
@@ -1,6 +1,6 @@
 fmdl= mk_library_model('adult_male_16el_lungs');
 img = mk_image(fmdl, 1); % background conductivity
-img.elem_data([fmdl.mat_idx{2};fmdl.mat_idx{3}]) = 0.2; % lungs
+img.elem_data([fmdl.mat_idx{2};fmdl.mat_idx{3}]) = 3.0; % lungs
 subplot(211); show_fem(img);
 print_convert mk_library_model02a.png
 view(2);
