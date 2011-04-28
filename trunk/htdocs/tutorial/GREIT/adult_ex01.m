12de4b6146b44e39171cc009c34469e4fe64e120
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2011-04-28 18:47:07 +0000

    example complete

--- trunk/htdocs/tutorial/GREIT/adult_ex01.m
+++ trunk/htdocs/tutorial/GREIT/adult_ex01.m
@@ -1,7 +1,10 @@
 fmdl= mk_library_model('adult_male_16el_lungs');
+fmdl.electrode = fmdl.electrode([9:16,1:8]);
 img = mk_image(fmdl, 1); % background conductivity
-img.elem_data([fmdl.mat_idx{2};fmdl.mat_idx{3}]) = 3.0; % lungs
+% Note, I don't understand why it takes a conductivity increase
+%  here: This needs to be debugged further
+img.elem_data([fmdl.mat_idx{2};fmdl.mat_idx{3}]) = 1.3; % lungs
 subplot(211); show_fem(img);
-print_convert mk_library_model02a.png
-view(2);
-print_convert mk_library_model02b.png
+print_convert adult_ex01a.png
+show_fem(img,[0,1]); view(2); %electrode #'s
+print_convert adult_ex01b.png
