9c5075e0e2310f2b0a6daf9c2cd7b849d74e75c2
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2011-04-28 19:03:58 +0000

    2 layer tutorial
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@2553 82006732-fb7e-4a22-9f28-86a212cce563

---
+++ htdocs/tutorial/GREIT/mk_GREIT_mat_2layer02.m
@@ -0,0 +1,17 @@
+% $Id$
+opt.imgsz = [32 32];
+opt.distr = 3; % non-random, uniform
+opt.Nsim = 1000;
+opt.target_size = 0.05; % Target size (frac of medium)
+opt.noise_figure = 0.5; % Recommended NF=0.5;
+
+fmdl = ng_mk_ellip_models([2, 2,1.4,0.2] ,[n_elecs,layers],[0.1]);
+fmdl.stimulation =  stim;
+fmdl.normalize_measurements = 0;
+img = mk_image(fmdl,1);
+
+imdl = mk_GREIT_model(img, 0.25, [], opt);
+
+show_fem(fmdl); view(0,70);
+
+print_convert mk_GREIT_mat_2layer02a.png '-density 60'
