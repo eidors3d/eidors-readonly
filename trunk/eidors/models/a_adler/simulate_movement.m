75a5a015c0c2610e3283a98a9bec6ed2c05d9e4a
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2011-05-11 13:34:06 +0000

    don't simulate with normalized_measurements

--- trunk/eidors/models/a_adler/simulate_movement.m
+++ trunk/eidors/models/a_adler/simulate_movement.m
@@ -51,6 +51,10 @@ function [vh,vi,xyzr,c2f]= simulate_movement( img, xyzr );
    c2f = mk_c2f_circ_mapping( img.fwd_model, xyzr);
 
    img.fwd_model.coarse2fine = c2f;
+
+   % We don't want a normalized jacobian here
+   img.fwd_model.normalize_measurements = 0;
+
    J= calc_jacobian( img );
    J= move_jacobian_postprocess( J, img, Nt);
 
