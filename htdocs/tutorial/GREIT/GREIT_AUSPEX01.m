354c36b46d8ae1e681bfc4f4e3396bb10adc60d5
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2011-07-12 12:39:09 +0000

    AUSPEX
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@2653 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/GREIT/GREIT_AUSPEX01.m
+++ htdocs/tutorial/GREIT/GREIT_AUSPEX01.m
@@ -1,30 +1,20 @@
 n_elecs = 16;
-if 0
-   fmdl = ng_mk_cyl_models([2 2 0.2] ,[n_elecs,1],[0.1]);
-else
-   fmdl= mk_library_model('adult_male_16el');
-   fmdl.electrode = fmdl.electrode([9:16,1:8]);
-   fmdl.electrode = fmdl.electrode([1,16:-1:2]);
-   fmdl.nodes(:,2) = -fmdl.nodes(:,2);
-end
-   fmdl.stimulation =  mk_stim_patterns(n_elecs,1,[0,1],[0,1],{'rotate_meas','no_meas_current'}, 1);
+fmdl = ng_mk_cyl_models([1 1 0.1] ,[n_elecs,.5],[0.05]);
+fmdl.stimulation =  mk_stim_patterns(n_elecs,1,[0,1],[0,1],{'rotate_meas','no_meas_current'}, 1);
+fmdl.normalize_measurements = 1; %MUST FOR AUSPEX
 
-   fmdl.normalize_measurements = 1;
-   img = mk_image(fmdl,1); % Homogeneous background
+opt.imgsz = [32 32];
+opt.distr = 3; 
+opt.noise_figure = 0.5; 
+imdl = mk_GREIT_model(fmdl, 0.20, [], opt);
 
-   opt.imgsz = [32 32];
-   opt.distr = 3; 
-   opt.noise_figure = 0.5; 
-   imdl = mk_GREIT_model(img, 0.2, [], opt);
-
-% MODEL
-fmdl = imdl.rec_model;
-fmdl.nodes = fmdl.nodes/max(fmdl.nodes(:))*1.2*[1,0;0,-1];
+% DESTINATION AUSPEX MODEL
 cmdl = mk_common_gridmdl('b2d','backproj');
 cmdl = rmfield(cmdl.fwd_model,'coarse2fine');
-f2c  = mk_coarse_fine_mapping(cmdl, fmdl);
+% MODEL GEOMETRIES MUST MATCH
+f2c  = mk_coarse_fine_mapping(cmdl, imdl.rec_model);
 
 RM = f2c*imdl.solve_use_matrix.RM(1:size(f2c,2),:);
 
 ReconstrMatrix= - ( RM(1:2:end,:) + RM(2:2:end,:) )';
-save ReconstrMatrixGREIT ReconstrMatrix -V6;
+save ReconstrMatrixGREITc ReconstrMatrix -V6;
---
+++ htdocs/tutorial/GREIT/GREIT_AUSPEX03.m
@@ -0,0 +1,14 @@
+imdl = mk_common_model('c2c2',16);
+imdl.fwd_model.stimulation =  ...
+     mk_stim_patterns(16,1,[0,1],[0,1],{'rotate_meas','no_meas_current'},1);
+img = mk_image(imdl);     vh = fwd_solve( img );
+img.elem_data(290) = 1.1; vi = fwd_solve( img );
+
+fname = 'ReconstrMatrixGREITc.mat';
+load(fname);
+
+imdl = mk_common_gridmdl('b2d',ReconstrMatrix');
+
+imgr= inv_solve(imdl, vh, vi);
+show_fem(imgr);
+
