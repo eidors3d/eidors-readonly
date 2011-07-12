ee265c956a8b33c67379892284cc7434b1ab9309
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2011-07-12 18:18:14 +0000

    new syntax for columns in show_slices

--- trunk/htdocs/tutorial/strange_effects/opposite_meas01.m
+++ trunk/htdocs/tutorial/strange_effects/opposite_meas01.m
@@ -6,25 +6,29 @@ imdl.fwd_model = rmfield(imdl.fwd_model,'meas_select');
 % Simulate with AD / AD
 imdl.fwd_model.stimulation = mk_stim_patterns(16,1,'{ad}','{op}',{},1);
 [vi,vh] = simulate_2d_movement( 5, imdl.fwd_model, [.9,.05], 2);
-imgs= inv_solve(imdl, vh, vi); 
-subplot(411); show_slices(imgs,[0,inf,inf,1,1])
+imgs= inv_solve(imdl, vh, vi);
+imgs.show_slices.img_cols = 5;
+subplot(411); show_slices(imgs)
 
 % Simulate with OP / AD
 imdl.fwd_model.stimulation = mk_stim_patterns(16,1,'{op}','{ad}',{},1);
 [vi,vh] = simulate_2d_movement( 5, imdl.fwd_model, [.9,.05], 2);
 imgs= inv_solve(imdl, vh, vi); 
-subplot(412); show_slices(imgs,[0,inf,inf,1,1])
+imgs.show_slices.img_cols = 5;
+subplot(412); show_slices(imgs)
 
 % Simulate with AD / OP
 imdl.fwd_model.stimulation = mk_stim_patterns(16,1,'{ad}','{op}',{},1);
 [vi,vh] = simulate_2d_movement( 5, imdl.fwd_model, [.9,.05], 2);
 imgs= inv_solve(imdl, vh, vi); 
-subplot(413); show_slices(imgs,[0,inf,inf,1,1])
+imgs.show_slices.img_cols = 5;
+subplot(413); show_slices(imgs)
 
 % Simulate with OP / OP
 imdl.fwd_model.stimulation = mk_stim_patterns(16,1,'{op}','{op}',{},1);
 [vi,vh] = simulate_2d_movement( 5, imdl.fwd_model, [.9,.05], 2);
 imgs= inv_solve(imdl, vh, vi); 
-subplot(414); show_slices(imgs,[0,inf,inf,1,1])
+imgs.show_slices.img_cols = 5;
+subplot(414); show_slices(imgs)
 
 print -dpng -r100 opposite_meas01.png
