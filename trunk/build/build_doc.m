23708ccbff277cda2d0d0a917a24130717da7785
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2011-07-13 14:55:22 +0000

    frames + pretty introduction

--- trunk/build/build_doc.m
+++ trunk/build/build_doc.m
@@ -1,13 +1,13 @@
 urlwrite('http://www.artefact.tk/software/matlab/m2html/m2html.zip',...
     'm2html.zip');
-!unzip m2html
+unzip('m2html.zip');
 addpath([pwd '/m2html']);
 tpl = 'blue';
 !chmod u+w -R m2html
 !cp doc_template/index.html m2html/templates/frame
 !cp doc_template/matlabicon.gif m2html/templates/frame
 !cp doc_template/matlabicon.gif m2html/templates/blue
-if 1
+if 0
 m2html('mfiles','eidors', 'htmldir','doc','recursive','on',...
     'globalhypertextlinks', 'on','template','blue',...
     'helptocxml', 'on');
@@ -16,6 +16,7 @@ m2html('mfiles','eidors', 'htmldir','doc','recursive','on',...
     'globalhypertextlinks', 'on','template','frame','index','menu',...
     'helptocxml', 'on');
 end
+!cp doc_template/intro.html ../doc
 !rsync -r doc htdocs
 !rm -rf doc
 !rm -rf m2html
