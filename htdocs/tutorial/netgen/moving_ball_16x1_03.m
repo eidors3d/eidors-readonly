b2508c1c4aebe1c6c22a2da861e0978ff866bf61
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2011-07-13 07:38:33 +0000

    Not Needed any more
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@2678 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/netgen/moving_ball_16x1_03.m
+++
@@ -1,9 +0,0 @@
-% Show moving ball 
-% $Id$
-show_fem(ng_mdl_16x1_coarse);
-crop_model(gca, inline('z>9','x','y','z'))
-view(0,70)
-[xs,ys,zs] = sphere(n);
-
-surf(xs,ys,zs);
-print -r100 -dpng build_single_plane03a.png
