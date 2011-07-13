fee145199228401111a559bde47c7466261b9059
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2011-07-13 12:40:27 +0000

    fix contact impedance

--- trunk/htdocs/tutorial/data_structures/tutorial022b.m
+++ trunk/htdocs/tutorial/data_structures/tutorial022b.m
@@ -1,9 +1,5 @@
 % 2D image $Id$
 
-img= eidors_obj('image','2D rectangle', ...
-      'elem_data', ones(size(mdl.elems,1),1) * conduc, ...
-      'fwd_model', mdl); 
-
+img= mk_image(mdl, conduc);
 fsol= fwd_solve(img)
-%show_fem(mdl); axis('equal'); set(gca,'Ylim',[-.5,ww-.5]);
-%print -r100 -dpng tutorial022b.png
+% fsol is the voltage drop across the resistor
