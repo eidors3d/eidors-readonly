080d946e1ed23b6e8d48b65c7e382d331181678e
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2011-07-13 07:31:07 +0000

    change to use aa_calc_jacobian, np_calc_jacobian threw warnings
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@2676 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/distmesh/dm_geophys04.m
+++ htdocs/tutorial/distmesh/dm_geophys04.m
@@ -4,6 +4,7 @@
 % reconstruction model and fwd_model
 imdl= mk_common_model('c2c2',16);
 imdl.rec_model= cmdl;
+fmdl.jacobian = @aa_calc_jacobian;
 imdl.fwd_model= fmdl;
 
 c2f= mk_coarse_fine_mapping( fmdl, cmdl);
@@ -11,6 +12,7 @@ imdl.fwd_model.coarse2fine = c2f;
 imdl.RtR_prior = @gaussian_HPF_prior;
 imdl.solve = @aa_inv_solve;
 imdl.hyperparameter.value= 0.0001;
+
 
 imgs= inv_solve(imdl, vh, vi);
 
