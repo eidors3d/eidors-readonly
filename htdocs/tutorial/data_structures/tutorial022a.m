2a0b1023dc3add8ecce83f382960726705ffe153
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2011-07-13 12:40:27 +0000

    fix contact impedance
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@2701 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/data_structures/tutorial022a.m
+++ htdocs/tutorial/data_structures/tutorial022a.m
@@ -7,8 +7,8 @@ mdl= eidors_obj('fwd_model','2D rectangle');
 mdl.nodes = [floor( (0:nn-1)/ww );rem(0:nn-1,ww)]';
 mdl.elems = delaunayn(mdl.nodes);
 mdl.gnd_node = 1;
-elec(1).nodes= [1:ww];      elec(1).z_contact= 0;
-elec(2).nodes= nn-[0:ww-1]; elec(2).z_contact= 0;
+elec(1).nodes= [1:ww];      elec(1).z_contact= 0.01;
+elec(2).nodes= nn-[0:ww-1]; elec(2).z_contact= 0.01;
 stim.stim_pattern= [-1;1];
 stim.meas_pattern= [-1,1];
 mdl.stimulation= stim;
@@ -17,4 +17,4 @@ mdl.solve = @aa_fwd_solve;
 mdl.system_mat = @aa_calc_system_mat;
 
 show_fem(mdl); axis('equal'); set(gca,'Ylim',[-.5,ww-.5]);
-print -r100 -dpng tutorial022a.png
+print_convert tutorial022a.png '-density 75'
