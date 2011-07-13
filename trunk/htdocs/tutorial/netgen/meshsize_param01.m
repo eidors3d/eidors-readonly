8f35559b29d0564f2f1e5dc5709bbab776380e89
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2011-07-13 10:56:43 +0000

    new meshgen interface for meshsize_parameter

--- trunk/htdocs/tutorial/netgen/meshsize_param01.m
+++ trunk/htdocs/tutorial/netgen/meshsize_param01.m
@@ -1,10 +1,13 @@
 % Call Netgen $Id$ 
-electrodes_per_plane= 16;
-number_of_planes= 1;
-movement_pattern='radial_turn';
-tank_height= 10;
-finelevel=''; %finelevel='-fine'; Other option
-refine_electrodes=0;
-fno_max=1;
-maxh= ''; %don't use max h
-move_the_ball % CALL NETGEN
+stim = mk_stim_patterns(16,1,'{ad}','{ad}',{},1);
+fmdl = ng_mk_cyl_models([10,15],[16,5],[0.5]);
+fmdl.stimulation= stim;
+img = mk_image(fmdl,1);
+vh = fwd_solve(img);
+
+extra={'ball','solid ball = sphere(7.5,0,5;2);'};
+[fmdl,mat_idx] = ng_mk_cyl_models([10,15],[16,5],[0.5],extra);
+fmdl.stimulation= stim;
+img = mk_image(fmdl,1);
+img.elem_data(mat_idx{2}) = 1.1;
+vi = fwd_solve(img);
--- trunk/htdocs/tutorial/netgen/meshsize_param02.m
+++ trunk/htdocs/tutorial/netgen/meshsize_param02.m
@@ -7,7 +7,7 @@ th= linspace(0,2*pi,100); th(end)= [];
 xcirc= 0.5+0.1*cos(th); ycirc= 0.1*sin(th);
 
 subplot(121);
-show_fem(img); view(90,60);
+show_fem(img); view(0,60);
 
 subplot(122);
 show_fem(inv_solve( imdl, vh, vi(1)));
@@ -15,4 +15,4 @@ line(xcirc,ycirc,'Color',[0,0.5,0],'LineWidth',2);
 axis image
 ylabel(sprintf('No. Elems= %d', size(img.fwd_model.elems,1)));
 
-print -dpng -r100 meshsize_param02a.png
+print_convert meshsize_param02a.png '-density 100'
