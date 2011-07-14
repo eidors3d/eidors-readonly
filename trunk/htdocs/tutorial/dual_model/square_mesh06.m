5801c620df7d45c02a20b761a74f6fdfcbd3319e
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2011-07-14 14:37:16 +0000

    modify display

--- trunk/htdocs/tutorial/dual_model/square_mesh06.m
+++ trunk/htdocs/tutorial/dual_model/square_mesh06.m
@@ -11,11 +11,11 @@ imdl.hyperparameter.value= 0.01;
 imgc= inv_solve(imdl, vh, vi);
 
 % Show on the mesh
-subplot(121); show_fem(imgs);
-subplot(122); show_fem(imgc);
+subplot(121); show_fem(imgs); axis equal
+subplot(122); show_fem(imgc); axis equal
 print_convert square_mesh06a.png
 
 % Show on a grid 
-subplot(121); show_slices(imgs);
-subplot(122); show_slices(imgc);
+subplot(121); show_slices(imgs); axis equal
+subplot(122); show_slices(imgc); axis equal
 print_convert square_mesh06b.png
