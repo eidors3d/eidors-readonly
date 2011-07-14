3a07bc21bab6f4cec5fdc1261b7e184cd8de741a
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2011-07-14 15:56:29 +0000

    Fix movement jacobian and new print_convert
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@2758 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/adv_image_reconst/move_3d01.m
+++ htdocs/tutorial/adv_image_reconst/move_3d01.m
@@ -7,22 +7,12 @@ movement = 2;
 mdl3dim = mk_common_model( 'n3r2' );
 mdl3dim.fwd_model.nodes(:,3) = mdl3dim.fwd_model.nodes(:,3)/3;
 
-% Instantiate a homogeneous forward model.
-% ref_level is 1 since we use ones( ).
-sigma = ones( size(mdl3dim.fwd_model.elems,1) ,1);
-% Create eidors_obj type image
-img = eidors_obj('image', 'homogeneous image', ...
-    'elem_data', sigma, ...
-    'fwd_model', mdl3dim.fwd_model, ...
-    'ref_level', 1);
-% Solve the homogeneous forward problem
+img = mk_image(mdl3dim);
 vh = fwd_solve( img );
 
-% Set local inhomogeneites using eidors 3D model
 load('datacom.mat','A','B');
-sigma(A) = 1.2; 
-sigma(B) = 0.8;
-img.elem_data = sigma;
+img.elem_data(A) = 1.2; 
+img.elem_data(B) = 0.8;
 node0 = img.fwd_model.nodes;  % Node variable before movement
 node1 = node0;                % Node variable after movement
 z_axis = node1(:,3);
@@ -38,7 +28,7 @@ show_fem( img );
 xlabel('x'); ylabel('y');
 view(-44,22)
 
-print -r75 -dpng move_3d01.png
+print_convert  move_3d01.png '-density 75'
 
 % Do a 3D twist - we'll actually use this one
 centre = 1- movement/2;
@@ -51,4 +41,3 @@ vi = fwd_solve( img );
 noise = noiselev*std( vh.meas - vi.meas )*randn( size(vi.meas) );
 vi.meas = vi.meas + noise;
 move = node1 - node0;
-
--- htdocs/tutorial/adv_image_reconst/move_3d02.m
+++ htdocs/tutorial/adv_image_reconst/move_3d02.m
@@ -17,22 +17,22 @@ img3dim.calc_colours.backgnd=[.8 .8 .9];
 subplot(1,3,2)
 show_slices_move( img3dim );
 
+% This is also available by this code:
+% mdlM = select_imdl(mdl3dim, {'Elec Move GN'});
+
 % Inverse solution of data with movement consideration
 move_vs_conduct = 20;  % Movement penalty (symbol mu in paper)
-% Define a eidglobalors_obj Movement model solved by electrode movement 
-% algorithms.
+
 mdlM = mdl3dim;
 mdlM.fwd_model.conductivity_jacobian = mdlM.fwd_model.jacobian;
 
-% this is a perturbation type jacobian
-%mdlM.fwd_model.jacobian = 'aa_e_move_jacobian';
-
-mdlM.fwd_model.jacobian = @calc_move_jacobian; % faster / more accurate
-mdlM.RtR_prior = 'aa_e_move_image_prior';
+mdlM.fwd_model.jacobian = @aa_e_move_jacobian; % perturbation type jacobian
+mdlM.RtR_prior = @aa_e_move_image_prior;
 mdlM.aa_e_move_image_prior.parameters = move_vs_conduct;
+
 % Solve inversglobale problem and show slices
 imgM = inv_solve(mdlM, vh, vi);
 imgM.calc_colours.backgnd=[.8 .8 .9];
 subplot(1,3,3)
 show_slices_move( imgM );
-print -r100 -dpng move_3d02.png
+print_convert  move_3d02.png '-density 100'
