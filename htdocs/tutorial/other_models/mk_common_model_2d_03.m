3a90ab7ec8334ad0fbd244360b8807dadf92bfa3
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2011-07-14 22:38:07 +0000

    common models
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@2791 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/other_models/mk_common_model_2d_03.m
+++ htdocs/tutorial/other_models/mk_common_model_2d_03.m
@@ -4,16 +4,16 @@ clf; subplot(221)
 
 imdl= mk_common_model('c2t2',16);
 show_fem(imdl.fwd_model);
-print -dpng -r100 mk_common_model_2d_03a.png
+print_convert mk_common_model_2d_03a.png
 
 imdl= mk_common_model('b2t3',8);
 show_fem(imdl.fwd_model);
-print -dpng -r100 mk_common_model_2d_03b.png
+print_convert mk_common_model_2d_03b.png
 
 imdl= mk_common_model('b2T2',8);
 show_fem(imdl.fwd_model);
-print -dpng -r100 mk_common_model_2d_03c.png
+print_convert mk_common_model_2d_03c.png
 
 imdl= mk_common_model('c2T3',16);
 show_fem(imdl.fwd_model);
-print -dpng -r100 mk_common_model_2d_03d.png
+print_convert mk_common_model_2d_03d.png
--- htdocs/tutorial/other_models/mk_common_model_2d_04.m
+++ htdocs/tutorial/other_models/mk_common_model_2d_04.m
@@ -4,17 +4,17 @@ clf; subplot(221)
 
 imdl= mk_common_model('a2d0c',8);
 show_fem(imdl.fwd_model);
-print -dpng -r100 mk_common_model_2d_04a.png
+print_convert mk_common_model_2d_04a.png
 
 imdl= mk_common_model('b2d1c',16);
 show_fem(imdl.fwd_model);
-print -dpng -r100 mk_common_model_2d_04b.png
+print_convert mk_common_model_2d_04b.png
 
 imdl= mk_common_model('c2d2c',32);
 show_fem(imdl.fwd_model);
-print -dpng -r100 mk_common_model_2d_04c.png
+print_convert mk_common_model_2d_04c.png
 
 imdl= mk_common_model('e2d4c',16);
 show_fem(imdl.fwd_model);
-print -dpng -r100 mk_common_model_2d_04d.png
+print_convert mk_common_model_2d_04d.png
 
