31064a48d7a0e89779dd5722e8e64f17f6d09e22
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2011-07-14 17:10:57 +0000

    set to axis equal

--- trunk/htdocs/tutorial/netgen/extrusion/pig_body09.m
+++ trunk/htdocs/tutorial/netgen/extrusion/pig_body09.m
@@ -3,7 +3,7 @@ imdl.fwd_model.electrode = imdl.fwd_model.electrode([1,16:-1:2]);
 imdl.fwd_model.normalize_measurements= 1;
 imr= inv_solve(imdl, vh, vi);
 
-show_fem(imr); axis tight; axis off
+show_fem(imr); axis tight; axis off; axis equal
 
 print_convert pig_body09a.jpg
 
