a23b4914cfdd68b3c4367a9967d76c0c60fd39ce
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2011-07-14 16:32:47 +0000

    crop figures nicely

--- trunk/htdocs/tutorial/distmesh/simple_2d_model01.m
+++ trunk/htdocs/tutorial/distmesh/simple_2d_model01.m
@@ -4,11 +4,11 @@
 imdm=mk_common_model('a2d1c',16);
 subplot(121)
 show_fem(imdm.fwd_model);
-axis on; axis equal
+axis on; axis equal; axis([-1.1 1.1 -1.1 1.1]);
 
 % close in view
 subplot(122)
 show_fem(imdm.fwd_model);
 axis equal; axis([-.1 .1 .85 1.05]);
 
-print -dpng -r125 simple_2d_model01a.png
+print_convert simple_2d_model01a.png '-density 125'
