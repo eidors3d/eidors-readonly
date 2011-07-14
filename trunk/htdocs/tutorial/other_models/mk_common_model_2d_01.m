1621a95821736617aa1a12bb7ffa882ebfef74dc
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2011-07-14 22:38:07 +0000

    common models

--- trunk/htdocs/tutorial/other_models/mk_common_model_2d_01.m
+++ trunk/htdocs/tutorial/other_models/mk_common_model_2d_01.m
@@ -4,16 +4,16 @@ clf; subplot(221)
 
 imdl= mk_common_model('a2c0',16);
 show_fem(imdl.fwd_model);
-print -dpng -r100 mk_common_model_2d_01a.png
+print_convert  mk_common_model_2d_01a.png
 
 imdl= mk_common_model('b2c0',16);
 show_fem(imdl.fwd_model);
-print -dpng -r100 mk_common_model_2d_01b.png
+print_convert  mk_common_model_2d_01b.png
 
 imdl= mk_common_model('c2c0',16);
 show_fem(imdl.fwd_model);
-print -dpng -r100 mk_common_model_2d_01c.png
+print_convert  mk_common_model_2d_01c.png
 
 imdl= mk_common_model('d2c0',16);
 show_fem(imdl.fwd_model);
-print -dpng -r100 mk_common_model_2d_01d.png
+print_convert  mk_common_model_2d_01d.png
--- trunk/htdocs/tutorial/other_models/mk_common_model_2d_02.m
+++ trunk/htdocs/tutorial/other_models/mk_common_model_2d_02.m
@@ -4,16 +4,16 @@ clf; subplot(221)
 
 imdl= mk_common_model('c2c0',16);
 show_fem(imdl.fwd_model);
-print -dpng -r100 mk_common_model_2d_02a.png
+print_convert mk_common_model_2d_02a.png
 
 imdl= mk_common_model('c2C0',16);
 show_fem(imdl.fwd_model);
-print -dpng -r100 mk_common_model_2d_02b.png
+print_convert mk_common_model_2d_02b.png
 
 imdl= mk_common_model('c2c2',8);
 show_fem(imdl.fwd_model);
-print -dpng -r100 mk_common_model_2d_02c.png
+print_convert mk_common_model_2d_02c.png
 
 imdl= mk_common_model('c2C2',8);
 show_fem(imdl.fwd_model);
-print -dpng -r100 mk_common_model_2d_02d.png
+print_convert mk_common_model_2d_02d.png
