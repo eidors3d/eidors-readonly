4b58ce7483bd3b0055e77421f9124d105a24e280
cgomezlaberge <cgomezlaberge@82006732-fb7e-4a22-9f28-86a212cce563>
2012-01-31 19:16:42 +0000

    start of description
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@2836 82006732-fb7e-4a22-9f28-86a212cce563

---
+++ htdocs/tutorial/lung_EIT/cg_ards_recruitment_01.m
@@ -0,0 +1,46 @@
+clear
+addpath EITCalcFrequencySpectrum
+addpath EITCalcLungRoi
+addpath EITCalcTimeSignal
+addpath EITFilterData
+addpath EITReadData
+addpath EITReconstructImages
+addpath Special
+
+% PROCESS EACH STEP OF THE PROTOCOL
+basename = './DATA';
+filename = 'STUDYNAME/SUBJECT_1/YYYYMMDD/Eit/Viasys/1001_b.get'; 
+range =[]; maneuver='increment'; PEEP=14; dP=5;
+process_and_save(basename, filename, range, maneuver, PEEP, dP);
+    
+filename = 'STUDYNAME/SUBJECT_1/YYYYMMDD/Eit/Viasys/1001_c1.get'; 
+range =[]; maneuver='increment'; PEEP=15; dP=15;
+process_and_save(basename, filename, range, maneuver, PEEP, dP);
+    
+filename = 'STUDYNAME/SUBJECT_1/YYYYMMDD/Eit/Viasys/1001_c2.get'; 
+range =[]; maneuver='increment'; PEEP=20; dP=15;
+process_and_save(basename, filename, range, maneuver, PEEP, dP);
+    
+filename = 'STUDYNAME/SUBJECT_1/YYYYMMDD/Eit/Viasys/1001_c3.get'; 
+range =[]; maneuver='increment'; PEEP=25; dP=15;
+process_and_save(basename, filename, range, maneuver, PEEP, dP);
+    
+filename = 'STUDYNAME/SUBJECT_1/YYYYMMDD/Eit/Viasys/1001_c4.get'; 
+range =[]; maneuver='increment'; PEEP=30; dP=15;
+process_and_save(basename, filename, range, maneuver, PEEP, dP);
+    
+filename = 'STUDYNAME/SUBJECT_1/YYYYMMDD/Eit/Viasys/1001_d1.get'; 
+range =[1,765]; maneuver='decrement'; PEEP=20; dP=6;
+process_and_save(basename, filename, range, maneuver, PEEP, dP);
+    
+filename = 'STUDYNAME/SUBJECT_1/YYYYMMDD/Eit/Viasys/1001_d2.get'; 
+range =[]; maneuver='decrement'; PEEP=18; dP=6;
+process_and_save(basename, filename, range, maneuver, PEEP, dP);
+    
+filename = 'STUDYNAME/SUBJECT_1/YYYYMMDD/Eit/Viasys/1001_d3.get'; 
+range =[368,780]; maneuver='decrement';  PEEP=16; dP=5;
+process_and_save(basename, filename, range, maneuver, PEEP, dP);
+    
+filename = 'STUDYNAME/SUBJECT_1/YYYYMMDD/Eit/Viasys/1001_d4.get'; 
+range =[411,780]; maneuver='decrement'; PEEP=14; dP=4;
+process_and_save(basename, filename, range, maneuver, PEEP, dP);
