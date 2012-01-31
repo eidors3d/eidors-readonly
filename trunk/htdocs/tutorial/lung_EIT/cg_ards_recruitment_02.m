8e65442ef3a1ae0a57cda95e207996be92e27d9f
cgomezlaberge <cgomezlaberge@82006732-fb7e-4a22-9f28-86a212cce563>
2012-01-31 19:16:42 +0000

    start of description

---
+++ trunk/htdocs/tutorial/lung_EIT/cg_ards_recruitment_02.m
@@ -0,0 +1,7 @@
+%% SHOW EXAMPLE RESULTS FROM ONE STEP
+load SUBJECT_1-c1.mat
+EITCalcTimeSignal(db_c1.eitdata);
+EITCalcFrequencySpectrum(db_c1.eitdata);
+EITDisplayImages(db_c1.eitimages);
+EITDisplayImages(db_c1.eitimages,1,'mtd');
+clear
