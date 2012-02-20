2777503bf1bcbf3a73ce3be23dc6b1a7b4f87316
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2012-02-20 07:51:20 +0000

    electrode errors with GREIT

---
+++ trunk/htdocs/tutorial/adv_image_reconst/electrode_errors05.m
@@ -0,0 +1,7 @@
+% Basic GREIT reconst $Id$
+
+imgr= inv_solve(imdl, zc_resp(:,1), zc_resp(:,22));
+imgr.calc_colours.ref_level=0; show_fem(imgr,[0,1]);
+
+axis equal; axis([-1,1.05,-.8,.8]);
+print_convert electrode_errors05a.jpg
