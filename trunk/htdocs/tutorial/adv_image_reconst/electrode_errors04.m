b8b921f89fd77660805d804b82ce0c5e43dfa2ec
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2012-02-20 10:13:20 +0000

    add GREIT electrode errors

--- trunk/htdocs/tutorial/adv_image_reconst/electrode_errors04.m
+++ trunk/htdocs/tutorial/adv_image_reconst/electrode_errors04.m
@@ -6,9 +6,9 @@ fmdl.electrode = fmdl.electrode([9:16,1:8]);
 [stim,msel] = mk_stim_patterns(16,1,[0,1],[0,1],{'no_meas_current'},1);
 fmdl.stimulation = stim;
 fmdl.normalize_measurements=1;
+fmdl.meas_select = msel;
 img = mk_image(fmdl, 1);
 img.elem_data([fmdl.mat_idx{2};fmdl.mat_idx{3}]) = 0.3; % lungs
 clear opt; opt.imgsz = [32 32]; opt.square_pixels=1;
 opt.noise_figure = .5;
 imdl=mk_GREIT_model(img, 0.25, [], opt);
-imdl.fwd_model.meas_select = msel;
