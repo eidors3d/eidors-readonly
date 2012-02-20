0b63deba999e6a9e8d3e2bb10691541307424bed
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2012-02-20 09:58:15 +0000

    tests for rm elecsa

--- trunk/eidors/algorithms/a_adler/meas_icov_rm_elecs.m
+++ trunk/eidors/algorithms/a_adler/meas_icov_rm_elecs.m
@@ -1,9 +1,13 @@
-function meas_icov = meas_icov_rm_elecs( imdl, elec_list )
+function meas_icov = meas_icov_rm_elecs( imdl, elec_list)
 % MEAS_ICOV_RM_ELECS: remove electrodes from consideration
 % meas_icov = meas_icov_rm_elecs( inv_model, elec_list )
 %
-% elec_list = numbers of elecs to remove
-%   or as imdl.meas_icov_rm_elecs.elec_list
+% PARAMETERS:
+% - elec_list = numbers of elecs to remove
+%     or as imdl.meas_icov_rm_elecs.elec_list
+%
+% - imdl.meas_icov_rm_elecs.exponent - exponent
+% - imdl.meas_icov_rm_elecs.SNR      - SNR to add (default inf)
 %
 % meas_icov_rm_elecs can also accept a fwd_model parameter
 % 
@@ -12,6 +16,8 @@ function meas_icov = meas_icov_rm_elecs( imdl, elec_list )
 
 % (C) 2008 Andy Adler. License: GPL version 2 or version 3
 % $Id$
+
+if isstr(imdl) && strcmp(imdl,'UNIT_TEST'); do_unit_test; return; end
 
 switch imdl.type,
   case 'inv_model'; fmdl = imdl.fwd_model;
@@ -22,6 +28,15 @@ end
 if nargin< 2
    elec_list = imdl.meas_icov_rm_elecs.elec_list;
 end
+
+     NSR = 0;
+try; NSR = (imdl.meas_icov_rm_elecs.SNR)^(-1);
+end
+
+    exponent = 1;
+try;exponent = imdl.meas_icov_rm_elecs.exponent;
+end
+ 
 
 meas_icov = [];
 for stim = fmdl.stimulation(:)'
@@ -38,4 +53,65 @@ for stim = fmdl.stimulation(:)'
 end
 
 n = length(meas_icov);
-meas_icov = spdiags( meas_icov, 0, n,n );
+
+meas_icov(meas_icov == 0) = NSR;
+meas_icov = spdiags( meas_icov.^exponent, 0, n,n );
+
+function do_unit_test
+   imdl = mk_common_model('a2c0',4);
+   covar = meas_icov_rm_elecs(imdl,1);
+   unit_test_cmp('ire # 1', covar, zeros(size(covar))); 
+
+   imdl = mk_common_model('a2c0',8);
+   covar = meas_icov_rm_elecs(imdl,1);
+   ff =    find( diag(covar)~= 1);
+   ffcmp = [1;2;3;4;5;10;11;15;16;20;21;25;26;30;31;36;37;38;39;40];
+   unit_test_cmp('ire # 2', ff,ffcmp);
+
+   covar = meas_icov_rm_elecs(imdl.fwd_model,1);
+   unit_test_cmp('ire # 3', ff,ffcmp);
+
+   imdl.meas_icov_rm_elecs.elec_list = 1;
+   covar = meas_icov_rm_elecs(imdl);
+   unit_test_cmp('ire # 4', ff,ffcmp);
+   ff =    find( diag(covar)~= 1);
+
+   
+   covar = meas_icov_rm_elecs(imdl,[1,2]);
+   ff =    find( diag(covar)==1);
+   ffcmp = [12;13;14;18;19;23;24;28;29;33;34;35];
+   unit_test_cmp('ire # 5', ff,ffcmp);
+
+   covar = meas_icov_rm_elecs(imdl,[1,2,3]);
+   ff =    find( diag(covar)==1);
+   ffcmp = [18;19;24;29;34;35];
+   unit_test_cmp('ire # 6', ff,ffcmp);
+
+   covar = meas_icov_rm_elecs(imdl,[1,2,3,4]);
+   ff =    find( diag(covar)==1);
+   ffcmp = [24;35];
+   unit_test_cmp('ire # 7', ff,ffcmp);
+
+% NOW CHECK OTHER MODES
+   imdl.meas_icov_rm_elecs.elec_list = 1:4;
+   imdl.meas_icov_rm_elecs.exponent = 1;
+   imdl.meas_icov_rm_elecs.SNR      = 0;
+   covar=  diag( meas_icov_rm_elecs(imdl) );
+   ffcmp = [24;35];
+   unit_test_cmp('ire # 8', find(covar==1),ffcmp);
+
+   imdl.meas_icov_rm_elecs.SNR      = 100;
+   covar=  diag( meas_icov_rm_elecs(imdl) );
+   unit_test_cmp('ire # 9', find(covar==1),ffcmp);
+   ff =    find( covar~=1);
+   unit_test_cmp('ire #10', covar(ff),1/100);
+
+   imdl.meas_icov_rm_elecs.exponent = -1;
+   covar=  diag( meas_icov_rm_elecs(imdl) );
+   unit_test_cmp('ire #11', find(covar==1),ffcmp);
+   ff =    find( covar~=1);
+   unit_test_cmp('ire #12', covar(ff),100);
+    
+   
+
+
