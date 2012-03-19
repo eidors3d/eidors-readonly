ebf23c32cc358a07a535270e5ba26ffc528c4676
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2012-03-19 14:31:04 +0000

    Added cashing
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@2893 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/algorithms/a_adler/get_elem_volume.m
+++ eidors/algorithms/a_adler/get_elem_volume.m
@@ -17,32 +17,42 @@ NODE = fwd_model.nodes';
 ELEM = fwd_model.elems';
 [d,e]= size(ELEM);
 
-VOL=zeros(e,1);
-ones_d = ones(1,d);
-d1fac = prod( 1:d-1 );
-if d > size(NODE,1)
-   for i=1:e
-       this_elem = NODE(:,ELEM(:,i)); 
-       VOL(i)= abs(det([ones_d;this_elem])) / d1fac;
-   end
-elseif d == 3 % 3D nodes in 2D mesh
-   for i=1:e
-       this_elem = NODE(:,ELEM(:,i)); 
-       d12= det([ones_d;this_elem([1,2],:)])^2;
-       d13= det([ones_d;this_elem([1,3],:)])^2;
-       d23= det([ones_d;this_elem([2,3],:)])^2;
-       VOL(i)= sqrt(d12 + d13 + d23 ) / d1fac;
-   end
-elseif d == 2 % 3D nodes in 1D mesh (ie resistor mesh)
-   for i=1:e
-       this_elem = NODE(:,ELEM(:,i)); 
-       d12= det([ones_d;this_elem([1],:)])^2;
-       d13= det([ones_d;this_elem([2],:)])^2;
-       d23= det([ones_d;this_elem([3],:)])^2;
-       VOL(i)= sqrt(d12 + d13 + d23 ) / d1fac;
-   end
-else
-   error('mesh size not understood when calculating volumes')
+
+% cashing 
+c_obj = {NODE, ELEM};
+VOL = eidors_obj('get-cache', c_obj, 'elem_volume');
+if isempty(VOL)
+    VOL=zeros(e,1);
+    ones_d = ones(1,d);
+    d1fac = prod( 1:d-1 );
+    if d > size(NODE,1)
+        for i=1:e
+            this_elem = NODE(:,ELEM(:,i));
+            VOL(i)= abs(det([ones_d;this_elem])) / d1fac;
+        end
+    elseif d == 3 % 3D nodes in 2D mesh
+        for i=1:e
+            this_elem = NODE(:,ELEM(:,i));
+            d12= det([ones_d;this_elem([1,2],:)])^2;
+            d13= det([ones_d;this_elem([1,3],:)])^2;
+            d23= det([ones_d;this_elem([2,3],:)])^2;
+            VOL(i)= sqrt(d12 + d13 + d23 ) / d1fac;
+        end
+    elseif d == 2 % 3D nodes in 1D mesh (ie resistor mesh)
+        for i=1:e
+            this_elem = NODE(:,ELEM(:,i));
+            d12= det([ones_d;this_elem([1],:)])^2;
+            d13= det([ones_d;this_elem([2],:)])^2;
+            d23= det([ones_d;this_elem([3],:)])^2;
+            VOL(i)= sqrt(d12 + d13 + d23 ) / d1fac;
+        end
+    else
+        error('mesh size not understood when calculating volumes')
+    end
+    % caching
+    eidors_cache('boost_priority', -4); % low priority
+    eidors_obj('set-cache', c_obj, 'elem_volume', VOL);
+    eidors_cache('boost_priority', +4); % restore priority
 end
 
 if isfield(fwd_model,'coarse2fine')
