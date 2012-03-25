4e2d01d327cfc3250e5b37acbfc6872936ae007d
michaelcrabb30 <michaelcrabb30@82006732-fb7e-4a22-9f28-86a212cce563>
2012-03-25 19:48:01 +0000

    Edited examples in /algorithms/m_crabb
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@2908 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/algorithms/m_crabb/example_fwd3d.m
+++ eidors/algorithms/m_crabb/example_fwd3d.m
@@ -1,27 +1,49 @@
-% ensure dev/m_crabb/forward_problem is on the path
-
+%Make common model, and make an image
 imdl = mk_common_model('n3r2',16);
 fmdl = imdl.fwd_model;
 img0 = mk_image(fmdl,1);
-v0 = fwd_solve(img0); v0=v0.meas;
 
+%Forward solve using eidors solver (linear)
+v0 = fwd_solve(img0); v0e=v0.meas;
+
+%Solvers (including mc_fem_modify and element type)
 fmdl.solve = @mc_fwd_solve;
 fmdl.system_mat = @mc_calc_system_mat;
 fmdl.jacobian   = @mc_calc_jacobian;
 fmdl.fem_modify = @mc_fem_modify;
-fmdl.mc_type    = 'tet4'; % linear case
+
+%Forward solve using mc solvers (linear, quadratic)
+
+%Linear FEM
+fmdl.mc_type    = 'tet4';
 img1 = mk_image(fmdl,1);
-v1 = fwd_solve(img1); v1=v1.meas;
+img1.fwd_solve.get_all_meas = 1; %Internal voltage
+v1 = fwd_solve(img1); v1e=v1.meas;
 
-fmdl.mc_type    = 'tet10'; % linear case
+%Quadratic FEM
+fmdl.mc_type    = 'tet10';
 img2 = mk_image(fmdl,1);
-img2.fwd_solve.get_all_meas = 1;
-v2 = fwd_solve(img2); v2all = v2.volt; v2=v2.meas;
-img2n = rmfield(img2,'elem_data');
-vtxidx = 1:size(fmdl.nodes,1);
-img2n.node_data = v2all(vtxidx,1);
+img2.fwd_solve.get_all_meas = 1; %Internal voltage
+v2 = fwd_solve(img2); v2e=v2.meas;
 
-
-plot([v0,v1,v2,[v1-v0,v2-v0]*1]);
-legend('0','1','2','1-0','2-0');
+%Electrode voltages
+figure; plot([v0e,v1e,v2e,[v0e-v1e,v2e-v0e]*2]);
+legend('0','1','2','1-0','2-0')
 xlim([1,100]);
+
+
+%Internal voltage for linear
+v1all = v1.volt; 
+img1n = rmfield(img1,'elem_data');
+img1n.node_data = v1all(1:size(fmdl.nodes,1),1);
+figure; show_slices(img1n,[inf,inf,2.5]);
+
+%Internal voltage or quadratic
+v2all = v2.volt; 
+img2n = rmfield(img2,'elem_data');
+img2n.node_data = v2all(1:size(fmdl.nodes,1),1);
+figure; show_slices(img2n,[inf,inf,2.5]);
+
+%Internal voltage difference of linear-quadratic 
+img12n=img1n; img12n.node_data=v1all(1:size(fmdl.nodes,1),1)-v2all(1:size(fmdl.nodes,1),1);
+figure; show_slices(img12n,[inf,inf,2.5]);
