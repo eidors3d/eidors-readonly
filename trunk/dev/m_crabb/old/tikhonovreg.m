11727f2e927a185fb8f0d7f10da283946adee65a
michaelcrabb30 <michaelcrabb30@82006732-fb7e-4a22-9f28-86a212cce563>
2012-03-25 12:21:43 +0000

    Updating dev/m_crabb

--- trunk/dev/m_crabb/old/tikhonovreg.m
+++
@@ -1,8 +0,0 @@
-function [x]= Tikhonov(A,b,alpha)
-%Tikhnonov regularised solution of Ax=b with alpha the regular parameter
-%x=A'*(A*A'+a^2*I)^-1*b
-
-B = A*A' + alpha^2*eye(size(A,1)); %(A*A'+a^2*I)
-C=B\b; %(A*A'+a^2*I)^-1*b
-
-x=A'*C; %x=A'*(A*A'+a^2*I)^-1*b
