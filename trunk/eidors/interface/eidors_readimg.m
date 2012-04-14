cbc47e2fe5c48dbfa4b49e78b35721aa3f043f5c
anon-eidors <anon-eidors@82006732-fb7e-4a22-9f28-86a212cce563>
2012-04-14 16:44:38 +0000

    read images generated by Dixtal software

--- trunk/eidors/interface/eidors_readimg.m
+++ trunk/eidors/interface/eidors_readimg.m
@@ -4,10 +4,12 @@ function img = eidors_readimg( fname, format )
 %  img = eidors_readimg( fname, format )
 %
 % Currently the list of supported file formats is:
-%    - NATIVE "e3d" file format
-%        format = "e3d" or "NATIVE"
 %    - MCEIT (Goettingen / Viasys) "igt" file format 
 %        format = "IGT" or "MCEIT"
+%    - DIXTAL (Goettingen / Viasys) "imt" file format 
+%        format = "DIXTAL-IMG"
+%    - NATIVE "e3d" file format
+%        format = "e3d" or "NATIVE"
 %
 % Usage
 % img = eidors_readimg( fname, format )
@@ -39,6 +41,8 @@ fmt= lower(format);
 switch fmt
     case {'igt','mceit'}
         img = mceit_readimg( fname );
+    case {'dixtal-img'}
+        img = read_dixtal_img( fname, 1, [] ); % Guess that FS=50
     case {'e3d','native'}
         img = native_readimg( fname );
     otherwise
@@ -97,4 +101,76 @@ else
 end
 
 
+
+function [dixtalImages]=read_dixtal_img(file_name,skipHeader,Fs);
+% [dixtalImages]=OpenDixtalImages(file_name,skipHeader,Fs)
+%
+% Read the Dixtal images at file_name
+% input: 
+%       - file_name:  Name and place of the dixtal file
+%       - skipHeader: skip Dixtal header
+%       - Fs:         sampling frequency (50 if empty)
+% 
+% output:
+%       - dixtalImages: structure with the following fields:
+%           Fs:      sampling frequency
+%           datas:   analogue channel 2
+%           images:  Dixtal images
+
+if(isempty(Fs)==0)
+    dixtalImages.Fs = Fs;
+else
+    dixtalImages.Fs = 50;
+end
+
+fid=fopen(file_name,'r','ieee-be');
+[fname, mode, mformat] = fopen(fid);
+
+%if skip header is selceted, skip the header
+if (skipHeader == 1)
+    for i=1:42
+        tline = fgetl(fid);
+    end
+end
+a=fread(fid,'*float32','ieee-be');
+fclose(fid);
+
+% Reshape the Images
+offset = 1*32+4;
+imageL = 32*32 + offset;
+long = floor(length(a)/(imageL));
+images = reshape(a(1:long*imageL),imageL,long);
+% extracts data chanels
+[dixtalImages.datas,dixtalImages.images] = ExtractDixtalDatas(images);
+
+function [dixtalDatas,images]=ExtractDixtalDatas(images);
+% [dixtalAnalogs,images]=ExtractDixtalDatas(fname)
+% Extract the analog recording and the ECG from the reconstructed images of
+%       - dixtalDatas structure with the following fields:
+%           analog1: analogue channel 1
+%           analog2: analogue channel 2
+%           tbc_ecg: ECG correctness TO BE CONFIRMED
+%           tbc_timeStamp: Timestamp correctness TO BE CONFIRMED
+%           tbc_sync1: synchronization signal correctness TO BE CONFIRMED
+%           tbc_sync2: synchronization signal correctness TO BE CONFIRMED
+% !All fields starting with tbc_ are first guess signal is saved to not loos
+% the data but shall be investigated!
+%       - images: EIT images without the Dixtal data
+
+
+% read the datas
+datas=[];
+imageL = size(images,2);
+for i=1:36
+    datas(i,:) = images(32*32+i:imageL:end,:);
+end
+%remove the datas form the image
+images = images(1:32*32,:);
+
+dixtalDatas.analog1 = reshape(datas(8:12,:),1,5*size(datas,2));
+dixtalDatas.analog2 = reshape(datas(14:18,:),1,5*size(datas,2));
+dixtalDatas.tbc_ecg = reshape(datas(20:24,:),1,5*size(datas,2));
+dixtalDatas.tbc_sync1 = datas(1,:);
+dixtalDatas.tbc_sync2 = datas(25,:);
+dixtalDatas.tbc_timeStamp = datas(2,:);
 
