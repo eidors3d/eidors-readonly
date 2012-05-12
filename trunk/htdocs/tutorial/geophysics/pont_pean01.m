376bb546193682f41e22faa45bf31842be72f6ea
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2012-05-12 14:53:57 +0000

    results from Pont Pean - next need to test code

--- trunk/htdocs/tutorial/geophysics/pont_pean01.m
+++ trunk/htdocs/tutorial/geophysics/pont_pean01.m
@@ -10,30 +10,3 @@
   fmdl.stimulation = stim_meas_list( data(:,3:6) - 40100);
 
 show_fem(fmdl);
-
-%Reconstruction model
-[cmdl]= mk_grid_model([], 2.5+[-50,-20,0:10:320,340,370], ...
-                             -[0:2.5:10, 15:5:25,30:10:80,100,120]);
-c2f = mk_coarse_fine_mapping( fmdl, cmdl);
-
-fmdl.coarse2fine = c2f;
-imdl= eidors_obj('inv_model','test');
-imdl.fwd_model= fmdl;
-imdl.rec_model= cmdl;
-   imdl.reconst_type = 'difference';
-   imdl.RtR_prior = @laplace_image_prior;
-   imdl.solve = @aa_inv_solve;
-   imdl.hyperparameter.value = 0.1;
-   imdl.fwd_model.normalize_measurements = 1;
-   imdl.jacobian_bkgnd.value = 0.03;
-
-% Difference image vs simulated data
-gps = load('Mine_20FEV2004.gps');
-data= load('Mine_20FEV2004_LI.tomel');
-vr = data(:,9);
-img = mk_image( imdl );
-vh = fwd_solve(img); vh = vh.meas;
-
-imgr = inv_solve(imdl, vh, vr);
-
-show_fem(imgr);
---
+++ trunk/htdocs/tutorial/geophysics/pont_pean02.m
@@ -0,0 +1,15 @@
+%Reconstruction model
+[cmdl]= mk_grid_model([], 2.5+[-50,-20,0:10:320,340,370], ...
+                             -[0:2.5:10, 15:5:25,30:10:80,100,120]);
+c2f = mk_coarse_fine_mapping( fmdl, cmdl);
+
+fmdl.coarse2fine = c2f;
+imdl= eidors_obj('inv_model','test');
+imdl.fwd_model= fmdl;
+imdl.rec_model= cmdl;
+   imdl.reconst_type = 'difference';
+   imdl.RtR_prior = @laplace_image_prior;
+   imdl.solve = @aa_inv_solve;
+   imdl.hyperparameter.value = 0.1;
+   imdl.fwd_model.normalize_measurements = 1;
+   imdl.jacobian_bkgnd.value = 0.03;
