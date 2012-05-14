151104ce691cac4eeaa3551b1da786a52db91a8a
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2012-05-14 19:30:56 +0000

    don't need - print to jpg

--- trunk/htdocs/tutorial/EIDORS_basics/view_3D_surf02.m
+++ trunk/htdocs/tutorial/EIDORS_basics/view_3D_surf02.m
@@ -5,6 +5,6 @@ colours = calc_colours(imgv,[]);
 patch('Faces',fmdl.boundary,'Vertices',fmdl.nodes, 'facecolor','interp', ...
       'facevertexcdata',colours,'CDataMapping','direct'); 
 
-print_convert view_3D_surf02a.png '-density 75'
+print_convert view_3D_surf02a.jpg '-density 75'
 view(0,0)
-print_convert view_3D_surf02b.png '-density 75'
+print_convert view_3D_surf02b.jpg '-density 75'
--- trunk/htdocs/tutorial/EIDORS_basics/view_3D_surf05.m
+++ trunk/htdocs/tutorial/EIDORS_basics/view_3D_surf05.m
@@ -7,6 +7,5 @@ show_fem(fmdl);
 patch('Faces',fmdl.boundary,'Vertices',fmdl.nodes, 'facecolor','interp', ...
       'facevertexcdata',colours,'CDataMapping','direct'); 
 
-print_convert view_3D_surf05a.png '-density 75'
 view(0,0)
-print_convert view_3D_surf05b.png '-density 75'
+print_convert view_3D_surf05b.jpg '-density 300 -resize 33%% -quality 75'
