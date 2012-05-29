40cdcb7c9b03e33ba8530d8b02556b1f5be0946c
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2012-05-29 15:08:41 +0000

    more unit tests

--- trunk/eidors/meshing/netgen/ng_mk_gen_models.m
+++ trunk/eidors/meshing/netgen/ng_mk_gen_models.m
@@ -3,10 +3,7 @@ function [fmdl,mat_idx] = ng_mk_gen_models(shape_str, elec_pos,  elec_shape, ele
 %[fmdl,mat_idx] = ng_mk_gen_models(shape_str, elec_pos, elec_shape);
 %
 % INPUT:
-% shape_str = {height, [x_radius, y_radius, [maxsz]]}
-%    if height = 0 -> calculate a 2D shape
-%    x_radius, y_radius (OPT)  -> elliptical eccentricity in x,y directions(default = 1)
-%    maxsz  (OPT)  -> max size of mesh elems (default = course mesh)
+% shape_str = string of netgen *.geo file code, with mainobj as the main object
 %
 % ELECTRODE POSITIONS:
 %  elec_pos = [x,y,z,nx,ny,nz] centres of each electrode (N_elecs x 6)
@@ -468,6 +465,72 @@ switch tn
  elec_shape=[0.1];
  elec_obj = {'top','top','top','cyl','cyl','cyl'};
  fmdl = ng_mk_gen_models(shape_str, elec_pos, elec_shape, elec_obj);
+
+   case 9;
+ shape_str = ['solid top    = plane(0,0,0;0,0,1);\n' ...
+              'solid ball   = sphere(-1.25,0,-1;0.5); tlo ball;\n' ...
+              'solid mainobj= top and orthobrick(-2,-1,-2;2,1,0) and not ball -maxh=0.5;\n'];
+ elec_pos = linspace( -1.5,1.5,5)';
+ elec_pos = [  elec_pos, elec_pos*[0,0,0,0], elec_pos*0+1];
+ elec_shape=[0.3];
+ elec_obj = 'top';
+ [fmdl,mat_idx] = ng_mk_gen_models(shape_str, elec_pos, elec_shape, elec_obj);
+ img = mk_image( fmdl, 1);
+ img.elem_data(mat_idx{2}) = 1.1; 
+ 
+ fmdl = img; % so that the code shows the image
+
+  case 10;
+ shape_str = ['solid top    = plane(0,0,0;0,0,1);\n' ...
+              'solid mainobj= top and orthobrick(-3,-3,-2;3,3,0) -maxh=0.5;\n'];
+ [elec_pos_x,elec_pos_y] = meshgrid(linspace( -1.5,1.5,5),linspace(-2,2,7));
+ elec_pos = [  elec_pos_x(:), elec_pos_y(:), ones(size(elec_pos_x(:)))*[0,0,0,1] ];
+ elec_shape=[0.2];
+ elec_obj = 'top';
+ [fmdl,mat_idx] = ng_mk_gen_models(shape_str, elec_pos, elec_shape, elec_obj);
+
+   case 11;
+ shape_str = ['solid cyl    = cylinder (0,0,0; 0,0,1; 1.0); \n', ...
+              'solid tank   = orthobrick(-2,-2,0;2,2,0.4) and cyl; \n', ...
+              'solid fish   = ellipsoid(0.2,0.2,0.2;0.2,0,0;0,0.1,0;0,0,0.1); tlo fish;\n', ...
+              'solid mainobj= tank and not fish -maxh=0.3;\n'];
+ n_elec = 7;
+ th = linspace(0,2*pi,n_elec+1)'; th(end) = [];
+ cs = [cos(th), sin(th)];
+ elec_pos = [  cs, 0.2+0*th, cs, 0*th];
+ elec_shape=[0.05];
+ for i=1:n_elec; elec_obj{i} = 'cyl'; end
+ i=i+1;elec_pos(i,:) = [ 0  ,0.2,0.2,-1,0,0]; elec_obj{i} = 'fish';
+ i=i+1;elec_pos(i,:) = [ 0.4,0.2,0.2, 1,0,0]; elec_obj{i} = 'fish';
+ fmdl = ng_mk_gen_models(shape_str, elec_pos, elec_shape, elec_obj);
+
+   case 12;
+shape_str = ['solid top     = ellipsoid(0,0,0; 0,0,1; 1,0,0; 0,1,0); \n' ...
+    'solid mainobj= top and orthobrick(-2,-2,0;2,2,2) -maxh=0.1;\n'];
+deg2rad = pi/180;
+th = (-70:20:70)'*deg2rad;
+ elec_pos = [0*th,sin(th),cos(th),0*th,sin(th),cos(th); ...
+             sin(th),0*th,cos(th),sin(th),0*th,cos(th)];
+ elec_shape=[0.05];
+ elec_obj = 'top';
+fmdl = ng_mk_gen_models(shape_str, elec_pos, elec_shape, elec_obj);
+
+   case 13;
+ shape_str = ['solid cyl    = cylinder (0,0,0; 0,1,0; 1); \n', ...
+              'solid bottom = plane(0, 0,0;0,-1,0);\n' ...
+              'solid top    = plane(0,10,0;0, 1,0);\n' ...
+              'solid cut1   = plane(0, 4,0;0,-1,0);\n' ...
+              'solid cut2   = plane(0, 6,0;0, 1,0);\n' ...
+              'solid ball   = cyl and cut1 and cut2;  tlo ball;\n' ...
+              'solid mainobj= ( top and (not cut2) and cyl ) or ' ...
+                      '(bottom      and (not cut1) and cyl ) -maxh=0.8;\n'];
+ elec_pos = [ 0, 10,  0, 0,  1,  0; 
+              0,  0,  0, 0, -1,  0]; 
+ elec_shape=[1.0];
+ elec_obj = {'top','bottom'};
+ [fmdl,mat_idx] = ng_mk_gen_models(shape_str, elec_pos, elec_shape, elec_obj);
+ fmdl = mk_image(fmdl,1); 
+ fmdl.elem_data(mat_idx{2}) = 1.1;
    otherwise;
      error('huh?')
 end
