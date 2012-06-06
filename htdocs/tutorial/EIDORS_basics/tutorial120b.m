6acfba665146027eccf1e19b26683ed7a4d6b6ba
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2012-06-06 20:53:19 +0000

    update references to old ab_* functions in tutorials
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@3069 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/EIDORS_basics/tutorial120b.m
+++ htdocs/tutorial/EIDORS_basics/tutorial120b.m
@@ -45,8 +45,8 @@ inv2d.hyperparameter = rmfield(inv2d.hyperparameter,'func');
 
 % Total variation using PDIPM
 inv2d.hyperparameter.value = 1e-5;
-inv2d.solve=       @ab_tv_diff_solve;
-inv2d.R_prior=     @ab_calc_tv_prior;
+inv2d.solve=       @TV_diffusivity_solve;
+inv2d.R_prior=     @calc_TV_prior;
 inv2d.parameters.max_iterations= 10;
 inv2d.parameters.term_tolerance= 1e-3;
 
--- htdocs/tutorial/adv_image_reconst/TV_hyperparams02.m
+++ htdocs/tutorial/adv_image_reconst/TV_hyperparams02.m
@@ -8,8 +8,8 @@ invtv.reconst_type= 'difference';
 invtv.jacobian_bkgnd.value= 1;
 
 invtv.fwd_model=                  imdl.fwd_model;
-invtv.solve=                      @ab_tv_diff_solve;
-invtv.R_prior=                    @ab_calc_tv_prior;
+invtv.solve=                      @TV_diffusivity_solve;
+invtv.R_prior=                    @calc_TV_prior;
 invtv.parameters.term_tolerance=  1e-6;
 invtv.parameters.keep_iterations= 1;
 invtv.parameters.max_iterations=  maxit;
