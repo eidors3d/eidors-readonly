97d87a69828d15f1d3507f2d7bb03c48376bf3c6
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2012-06-07 14:40:23 +0000

    Copyright change

--- trunk/dev/n_lesparre/GN_absLogResist.m
+++ trunk/dev/n_lesparre/GN_absLogResist.m
@@ -8,7 +8,7 @@ function img= GN_absLogCond_solve( inv_model, data1);
 % Parameters:
 %   inv_model.parameters.max_iterations = N_max iter
 
-% (C) 2010 Andy Adler. License: GPL version 2 or version 3
+% (C) 2012 Nolwenn Lesparre. License: GPL version 2 or version 3
 % $Id: GN_abs_solve.m 2596 2011-06-18 14:18:06Z aadler $
 
 % Step 1: fit to background
