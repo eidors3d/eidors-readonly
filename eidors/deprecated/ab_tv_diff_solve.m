f4cda46159b44b50e1e24f865ee2b37484abaef5
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2012-06-07 20:18:20 +0000

    deprecated versions of Andrea's renamed TV solvers
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@3091 82006732-fb7e-4a22-9f28-86a212cce563

---
+++ eidors/deprecated/ab_tv_diff_solve.m
@@ -0,0 +1,87 @@
+function img= ab_tv_diff_solve( inv_model, data1, data2)
+% AB_TV_DIFF_SOLVE inverse solver for Andrea Borsic's
+%   Total Variation solver for use with difference EIT
+% img= ab_tv_diff_solve( inv_model, data1, data2)
+% img        => output image
+% inv_model  => inverse model struct
+% data1      => differential data at earlier time
+% data2      => differential data at later time
+% Parameters
+%   alpha1
+%   alpha2
+%   want_dual_variable  (set to 1 if you want access to dual)
+% Termination parameters
+%  max_iters =  inv_model.parameters.max_iteration (default 10)
+%      Max number of iterations before stopping
+%  min change = inv_model.parameters.min_change   (default 0)
+%      Min Change in objective fcn (norm(y-Jx)^2 + hp*TV(x)) before stopping
+
+% (C) 2002-2009 Andrea Borsic and Andy Adler. License: GPL version 2 or version 3
+% $Id$
+warning('EIDORS:deprecated','AB_TV_DIFF_SOLVE is deprecated as of 06-Jun-2012. Use TV_diffusivity_solve instead.');
+
+
+p= get_params(inv_model);
+
+dva = calc_difference_data( data1, data2, inv_model.fwd_model);
+% TEST CODE -> Put elsewhere
+back_val = get_good_background(inv_model, data1);
+inv_model.jacobian_bkgnd.value= back_val;
+
+sol= [];
+for i=1:size(dva,2)
+   [soln,dual_x]=primaldual_tvrecon_lsearch(inv_model, dva(:,i), ...
+        p.maxiter,p.alpha1,p.alpha2, p.tol, p.beta, p.min_change);
+
+   if ~p.keepiters
+      soln=soln(:,end);
+   end
+
+   sol=[sol, soln];
+end
+
+img.name= 'solved by ab_tv_diff_solve';
+img.elem_data = sol;
+img.fwd_model= inv_model.fwd_model;
+try if inv_model.ab_tv_diff_solve.want_dual_variable
+   img.dual_data = dual_x;
+end; end
+
+function p = get_params(inv_model);
+   
+   try   p.alpha1= inv_model.ab_tv_diff_solve.alpha1;
+   catch p.alpha1= 1e-2;
+   end
+
+   try   p.beta= inv_model.ab_tv_diff_solve.beta;
+   catch p.beta= 1e-4;
+   end
+
+   p.alpha2= calc_hyperparameter( inv_model);
+
+   try   p.min_change = inv_model.parameters.min_change;
+   catch p.min_change = 0;
+   end
+
+   try   p.maxiter = inv_model.parameters.max_iterations;
+   catch p.maxiter= 10;
+   end
+
+   try   p.keepiters = inv_model.parameters.keep_iterations;
+   catch p.keepiters= 0;
+   end
+
+   p.tol = 0; % TODO
+
+function back_val = get_good_background(inv_mdl, data1);
+
+   % Create homogeneous model
+   IM= eidors_obj('image','');
+   IM.fwd_model= inv_mdl.fwd_model;
+   s= ones(size(IM.fwd_model.elems,1),1);
+   IM.elem_data= s;
+
+   vsim= fwd_solve( IM);
+   back_val=abs( data1\vsim.meas );
+   back_val=1;
+
---
+++ eidors/deprecated/ab_tv_lagged_diff.m
@@ -0,0 +1,78 @@
+function img=ab_tv_lagged_diff( inv_model, data1, data2)
+% AB_TV_LAGGED_DIFF Lagged Diffusivity Inverse Solver
+% img= ab_inv_solve( inv_model, data1, data2)
+% img        => output image (or vector of images)
+% inv_model  => inverse model struct
+% data1      => differential data at earlier time
+% data2      => differential data at later time
+%
+% Parameters:
+%  max_iters =  inv_model.parameters.max_iteration (default 10)
+%      Max number of iterations before stopping
+%  min change = inv_model.parameters.min_change   (default 0)
+%      Min Change in objective fcn (norm(y-Jx)^2 + hp*TV(x)) before stopping
+%  beta      =  inv_model.TV_lagged_diffusivity.beta   (default 1e-3)
+% beta is the parameter that smooths the TV functional
+
+% (C) 2008 Andrea Borsic. License: GPL version 2 or version 3
+% $Id$
+warning('EIDORS:deprecated','AB_TV_LAGGED_DIFF is deprecated as of 06-Jun-2012. Use TV_LAGGED_DIFFUSIVITY instead.');
+try    max_iter = inv_model.parameters.max_iterations;
+catch  max_iter = 10;
+end
+
+try    min_change = inv_model.parameters.min_change;
+catch  min_change = 0;
+end
+
+try    beta = inv_model.aa_tv_lagged_diff.beta; 
+catch  beta = 1e-3;
+end
+
+
+fwd_model= inv_model.fwd_model;
+d=calc_difference_data( data1, data2, inv_model.fwd_model);
+
+L=calc_R_prior( inv_model );
+
+img_bkgnd=calc_jacobian_bkgnd( inv_model );
+J=calc_jacobian( fwd_model, img_bkgnd);
+
+alpha=calc_hyperparameter( inv_model );
+
+
+delta_sigma = zeros(size(J,2),1); % we start from no initial difference
+
+Obj_Fcn = inf; %initial value
+
+for k=1:max_iter
+ 
+    dv =  J*delta_sigma - d;
+
+% STOP IF OBJECTIVE FCN IS NOT CHANGING by more than min_change
+    Obj_Fcnk = norm(dv)^2 + alpha*sum(abs( delta_sigma ));
+    delta_ObjFcn = abs(Obj_Fcnk/Obj_Fcn - 1);
+%   fprintf('%d %g %g %g\n',k, Obj_Fcnk, Obj_Fcn, delta_ObjFcn);
+    if delta_ObjFcn < min_change; 
+       eidors_msg('Lagged_diff: Breaking at iteration %d',k,2);
+       break;
+    end
+    Obj_Fcn = Obj_Fcnk;
+    
+    E= sqrt((L*delta_sigma).^2+beta);
+    inv_E= spdiags( 1./E, 0, length(E), length(E));
+
+    phi1=J'*dv+ alpha*L'*inv_E*L*delta_sigma;
+    phi2=J'*J + alpha*L'*inv_E*L;
+
+    upd=-(phi2)\phi1;
+    
+    delta_sigma=delta_sigma+upd;
+
+    
+end % for k
+
+% create a data structure to return
+img.name = 'ab_tv_lagged_diff';
+img.elem_data = delta_sigma;
+img.fwd_model = fwd_model;
