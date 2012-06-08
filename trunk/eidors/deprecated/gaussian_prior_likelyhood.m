efc23fb52d0abce482dc4fd33def3669d6aab42c
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2012-06-08 15:17:56 +0000

    gaussian_prior_likelyhood => prior_gaussian_likelihood

--- trunk/eidors/deprecated/gaussian_prior_likelyhood.m
+++ trunk/eidors/deprecated/gaussian_prior_likelyhood.m
@@ -1,4 +1,4 @@
-function likelihood= gaussian_prior_likelyhood( inv_model, x, y, J )
+function likelihood= gaussian_prior_likelyhood( varargin )
 % Parameters for image
 %   inv_model.gaussian_prior_likelihood.img_mean -> image mean
 %   inv_model.gaussian_prior_likelihood.R_prior -> L*L' = inv(image covariance)
@@ -13,15 +13,4 @@ function likelihood= gaussian_prior_likelyhood( inv_model, x, y, J )
 % $Id$
 
 warning('EIDORS:deprecated','GAUSSIAN_PRIOR_LIKELYHOOD is deprecated as of 08-Jun-2012. Use PRIOR_GAUSSIAN_LIKELIHOOD instead.');
-
-x_m  = inv_model.gaussian_prior_likelihood.img_mean;
-L_x  = inv_model.gaussian_prior_likelihood.R_prior;
-p_x  = inv_model.gaussian_prior_likelihood.img_exp;
-L_n  = inv_model.gaussian_prior_likelihood.Noise;
-p_n  = inv_model.gaussian_prior_likelihood.data_exp;
-
-img_residual=  x - x_m;
-data_residual= y - J*x;
-
-likelihood= exp(- norm(L_n * data_residual, p_n) ...
-                - norm(L_x * img_residual,  p_x));
+likelihood = prior_gaussian_likelihood(varargin{:});
