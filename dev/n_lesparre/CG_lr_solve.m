f95c5d43e1bada95bb9bfacb31b652101dc55059
nolwenn85 <nolwenn85@82006732-fb7e-4a22-9f28-86a212cce563>
2012-06-13 13:30:09 +0000

    start adding tests
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@3150 82006732-fb7e-4a22-9f28-86a212cce563

--- dev/n_lesparre/CG_lr_solve.m
+++ dev/n_lesparre/CG_lr_solve.m
@@ -26,6 +26,8 @@ function img= CG_lr_solve( inv_model, data)
 % (C) 2012 Nolwenn Lesparre. License: GPL version 2 or version 3
 % $Id: CG_lr_solve.m 2596 2012-06-06 14:18:06Z nlesparre $
 
+if isstr(inv_model) && strcmp(inv_model,'UNIT_TEST'); do_unit_test; return; end
+
 img = calc_jacobian_bkgnd( inv_model );
 
 % Possibility to use a different parameterisation between the inverse and
@@ -178,3 +180,44 @@ tol=svj(ist);
 % set(gca,'fontsize',30,'fontname','Times'); drawnow;
 end  
 
+function do_unit_test
+% Forward Model
+ shape_str = ['solid top    = plane(0,0,0;0,1,0);\n' ...
+              'solid mainobj= top and orthobrick(-100,-200,-100;425,10,100) -maxh=20.0;\n'];
+gps = load('Mine_20FEV2004.gps');
+ elec_pos = gps(:,2:4); e0 = elec_pos(:,1)*0;
+ elec_pos = [  elec_pos, e0, e0+1, e0 ]; 
+ elec_shape=[0.5,.5,.5];
+ elec_obj = 'top';
+ [fmdl,mat_idx] = ng_mk_gen_models(shape_str, elec_pos, elec_shape, elec_obj);
+
+% Load data and positions (unused in this tutorial)
+data= load('Mine_20FEV2004_LI.tomel');
+  fmdl.stimulation = stim_meas_list( data(:,3:6) - 40100);
+
+show_fem(fmdl);
+%Reconstruction model
+[cmdl]= mk_grid_model([], 2.5+[-50,-20,0:10:320,340,370], ...
+                             -[0:2.5:10, 15:5:25,30:10:80,100,120]);
+c2f = mk_coarse_fine_mapping( fmdl, cmdl);
+
+fmdl.coarse2fine = c2f;
+imdl= eidors_obj('inv_model','test');
+imdl.fwd_model= fmdl;
+imdl.rec_model= cmdl;
+   imdl.reconst_type = 'difference';
+   imdl.RtR_prior = @prior_laplace;
+   imdl.solve = @inv_solve_diff_GN_one_step;
+   imdl.hyperparameter.value = 0.1;
+   imdl.fwd_model.normalize_measurements = 1;
+   imdl.jacobian_bkgnd.value = 0.03;
+% Difference image vs simulated data
+vr = data(:,9);
+img = mk_image( imdl );
+vh = fwd_solve(img); vh = vh.meas;
+
+imgr = inv_solve(imdl, vh, vr);
+
+show_fem(imgr);
+axis equal; axis tight
+end
