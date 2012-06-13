831d9be862491217d54c16c9b33e6e665bf5cf2e
nolwenn85 <nolwenn85@82006732-fb7e-4a22-9f28-86a212cce563>
2012-06-13 14:47:49 +0000

    mods to c2f so all elements are included
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@3154 82006732-fb7e-4a22-9f28-86a212cce563

--- dev/n_lesparre/CG_lr_solve.m
+++ dev/n_lesparre/CG_lr_solve.m
@@ -30,11 +30,12 @@ if isstr(inv_model) && strcmp(inv_model,'UNIT_TEST'); do_unit_test; return; end
 
 img = calc_jacobian_bkgnd( inv_model );
 
+
 % Possibility to use a different parameterisation between the inverse and
 % the forward problems. In that case, a coarse2fine matrix rely the
 % elements to reconstruct the medium conductivity
 if isfield(inv_model.fwd_model,'coarse2fine')
-    nc = size(img.fwd_model.coarse2fine,2);
+    nc = size(inv_model.fwd_model.coarse2fine,2);
     img.elem_data = mean(img.elem_data)*ones(nc,1);
 end
 
@@ -45,16 +46,18 @@ else
 end
 
 if ~isfield(inv_model.parameters,'perturb')
-    img.parameters.perturb= [0.0001 0.001 0.01];
+    inv_model.parameters.perturb= logspace(-5,-1,5);
 end
 
 if ~isfield(inv_model.parameters,'lambda')
-    img.parameters.lambda= logspace(-5,0,500);
+    inv_model.parameters.lambda= logspace(-5,0,500);
 end
 
 if ~isfield(inv_model.parameters,'normalisation')
-    img.parameters.normalisation= 1;
+    inv_model.parameters.normalisation= eye(size(data,1));
 end
+
+img.parameters= inv_model.parameters;
 
 for k= 1:iters
     % Estimate residuals between data and estimation
@@ -85,7 +88,7 @@ for k= 1:iters
     end
     
     % Estimate the perturbation direction
-    tol= svdAnalysisLcurvecrit(data,J);
+    tol= svdAnalysisLcurvecrit(data,img,J);
     delta_params= pinv(J,tol)*residuals;
     
     if isfield(inv_model.parameters,'fixed_background') && inv_model.parameters.fixed_background==1
@@ -104,12 +107,12 @@ end
 
 function  img = line_optimize(imgk, dx, data1)
 img = imgk;
-perturb= img.fwd_model.parameters.perturb;
+perturb= img.parameters.perturb;
 
 % Compute the forward model for eah perturbation step
 mlist= perturb*0;
 for i = 1:length(perturb);
-    img.params_mapping.params= imgk.params_mapping.params + perturb(i)*dx;
+    img.logRes= imgk.logRes + perturb(i)*dx;
     vsim = fwd_solve( img );
     dv = vsim.meas-data1;
     dv= img.parameters.normalisation*dv;
@@ -119,11 +122,18 @@ end
 pf = polyfit(perturb, mlist, 2);
 fmin = -pf(2)/pf(1)/2; % poly minimum
 fmin(fmin<0)= min(perturb);
+p= logspace(perturb(1),perturb(end),30);
+cp= pf(1)*p.^2+pf(2)*p+pf(3);
+
 if pf(1)*fmin^2+pf(2)*fmin+pf(3)>min(mlist) || fmin>max(perturb)
     [mk,ik]= min(cp);
     fmin= p(ik);
 end
-img.fwd_model.perturb= [fmin/4 fmin/2 fmin fmin*2 fmin*4] ;
+if fmin>min(mlist); 
+    [mk,ik]= min(mlist);
+    fmin= perturb(ik);
+end
+img.parameters.perturb= [fmin/4 fmin/2 fmin fmin*2 fmin*4] ;
 
 % figure; semilogx(perturb,mlist,'xk',fmin,pf(1)*fmin^2+pf(2)*fmin+pf(3),'or'); hold on
 % semilogx(p,pf(1)*p.^2+pf(2)*p+pf(3),'k','linewidth',2); axis tight
@@ -149,7 +159,7 @@ beta= U'*data;     %  adjust data
 beta= beta(1:length(svj));
 
 % Estimate the parameter of regularization
-lambda= img.fwd_model.parameters.lambda;
+lambda= img.parameters.lambda;
 % lambda= logspace(-4,-1,500);
 % lambda= logspace(-6,-2,500);
 
@@ -170,14 +180,14 @@ resi= sqrt(RES2); xlambda = sqrt(XL2);
 [m,ist]= min(abs(svj-lambda(ik)));
 tol=svj(ist);
 
-% figure;
-% loglog(resi,xlambda,'k',resi(ik),xlambda(ik),'or','linewidth',2); axis tight; hold on
-% yl= get(gca,'ylim');
-% ylabel('Roughness','fontsize',30,'fontname','Times')
-% xlabel('Residuals','fontsize',30,'fontname','Times')
-% ylim(yl);
-% title(['Best solution lambda=' num2str(lambda(ik),'%1.2e')],'fontsize',30,'fontname','Times')
-% set(gca,'fontsize',30,'fontname','Times'); drawnow;
+figure;
+loglog(resi,xlambda,'k',resi(ik),xlambda(ik),'or','linewidth',2); axis tight; hold on
+yl= get(gca,'ylim');
+ylabel('Roughness','fontsize',30,'fontname','Times')
+xlabel('Residuals','fontsize',30,'fontname','Times')
+ylim(yl);
+title(['Best solution lambda=' num2str(lambda(ik),'%1.2e')],'fontsize',30,'fontname','Times')
+set(gca,'fontsize',30,'fontname','Times'); drawnow;
 end  
 
 function do_unit_test
@@ -198,27 +208,42 @@ gps = load('Mine_20FEV2004.gps');
 
 show_fem(fmdl);
 %Reconstruction model
-[cmdl]= mk_grid_model([], 2.5+[-50,-20,0:10:320,340,370], ...
-                             -[0:2.5:10, 15:5:25,30:10:80,100,120]);
+[cmdl]= mk_grid_model([],  [-101,-50,-20,0:10:320,340,370,426], ...
+                          -[-0.1:2.5:10, 15:5:25,30:10:80,100,120,201]);
 c2f = mk_coarse_fine_mapping( fmdl, cmdl);
-
+%c2f_sum = sum(c2f');
 fmdl.coarse2fine = c2f;
 imdl= eidors_obj('inv_model','test');
 imdl.fwd_model= fmdl;
+
+% Test Originals GN solver
 imdl.rec_model= cmdl;
-   imdl.reconst_type = 'difference';
-   imdl.RtR_prior = @prior_laplace;
-   imdl.solve = @inv_solve_diff_GN_one_step;
-   imdl.hyperparameter.value = 0.1;
-   imdl.fwd_model.normalize_measurements = 1;
-   imdl.jacobian_bkgnd.value = 0.03;
+imdl.reconst_type = 'difference';
+imdl.RtR_prior = @prior_laplace;
+imdl.solve = @inv_solve_diff_GN_one_step;
+imdl.hyperparameter.value = 0.1;
+imdl.fwd_model.normalize_measurements = 1;
+imdl.jacobian_bkgnd.value = 0.03;
+
 % Difference image vs simulated data
 vr = data(:,9);
 img = mk_image( imdl );
 vh = fwd_solve(img); vh = vh.meas;
 
-imgr = inv_solve(imdl, vh, vr);
+imgGN = inv_solve(imdl, vh, vr);
 
-show_fem(imgr);
-axis equal; axis tight
+% NOW USE CG_lr_solve
+imdl.rec_model= cmdl;
+imdl.reconst_type = 'absolute';
+imdl.solve = @CG_lr_solve;
+imdl.fwd_model.normalize_measurements = 1;
+imdl.jacobian_bkgnd.value = 0.03;
+imdl.parameters.default = [];
+inv_model.parameters.lambda= logspace(-4,-2,500);
+imgCG = inv_solve(imdl, vr);
+
+figure
+subplot(211); show_fem(imgGN); axis equal
+subplot(212); show_fem(imgCG); axis equal
+
 end
