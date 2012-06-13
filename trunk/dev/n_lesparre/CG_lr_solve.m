bad88a2ce567861add204c23cba12ecba9f82a02
nolwenn85 <nolwenn85@82006732-fb7e-4a22-9f28-86a212cce563>
2012-06-13 13:32:05 +0000

    path to get data for test

--- trunk/dev/n_lesparre/CG_lr_solve.m
+++ trunk/dev/n_lesparre/CG_lr_solve.m
@@ -181,10 +181,12 @@ tol=svj(ist);
 end  
 
 function do_unit_test
+unzip('../../htdocs/data_contrib/dg_geophysical_EIT/Mine_20FEV2004.zip')
+data= load('Mine_20FEV2004_LI.tomel');
+gps = load('Mine_20FEV2004.gps');
 % Forward Model
  shape_str = ['solid top    = plane(0,0,0;0,1,0);\n' ...
               'solid mainobj= top and orthobrick(-100,-200,-100;425,10,100) -maxh=20.0;\n'];
-gps = load('Mine_20FEV2004.gps');
  elec_pos = gps(:,2:4); e0 = elec_pos(:,1)*0;
  elec_pos = [  elec_pos, e0, e0+1, e0 ]; 
  elec_shape=[0.5,.5,.5];
@@ -192,7 +194,6 @@ gps = load('Mine_20FEV2004.gps');
  [fmdl,mat_idx] = ng_mk_gen_models(shape_str, elec_pos, elec_shape, elec_obj);
 
 % Load data and positions (unused in this tutorial)
-data= load('Mine_20FEV2004_LI.tomel');
   fmdl.stimulation = stim_meas_list( data(:,3:6) - 40100);
 
 show_fem(fmdl);
