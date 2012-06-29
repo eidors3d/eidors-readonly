8c735f586a3f327b7ef5e1e5b2339d817d5b395c
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2012-06-29 21:12:41 +0000

    new system command

---
+++ trunk/eidors/interface/system_cmd.m
@@ -0,0 +1,19 @@
+function status = system_cmd( cmd );
+% SYSTEM_CMD: issue system commands, and try to compensate for
+%    strange differences between systems.
+%
+% status = system_cmd( cmd );
+
+% (C) 2012 Andy Adler, License GPL v2 or v3
+% $Id$
+
+if strfind(system_dependent('getos'),'Linux')
+  %Version 7 under linux sets the LD_LIBRARY_PATH and that breaks netgen    
+  ldpath ='LD_LIBRARY_PATH=;';
+  % Problems are a) some newer matlab versions dont' need it,
+  %   b) what if people are working in tcsh?
+
+  cmd = ['sh -c ''LD_LIBRARY_PATH; ',cmd,' '' '];
+end
+
+status = system(cmd);
