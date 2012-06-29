9bbcb2d3d20dc548e97a66c2b3427e3137274630
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2012-06-29 19:59:27 +0000

    fixes to use new code

--- trunk/htdocs/tutorial/dual_model/two_and_half_d02.m
+++ trunk/htdocs/tutorial/dual_model/two_and_half_d02.m
@@ -2,17 +2,11 @@
 
 % Original target
 subplot(141)
-show_fem(inhomg_img); view(-62,28)
+show_fem(img); view(-62,28)
 
 % Create inverse Model: Classic
-imdl= eidors_obj('inv_model','NPs GN inverse');
-imdl.solve=       @np_inv_solve;
-imdl.hyperparameter.value = 1e-3;
-imdl.R_prior= @np_calc_image_prior;
-imdl.np_calc_image_prior.parameters= [3 1]; % see iso_f_smooth: deg=1, w=1
-imdl.jacobian_bkgnd.value= 1;
-imdl.reconst_type= 'difference';
-imdl.fwd_model= f_mdl; % fine model
+imdl= select_imdl(f_mdl, {'Basic GN dif'});
+imdl.hyperparameter.value = 1e-2;
 
 % Classic (inverse crime) solver
 img1= inv_solve(imdl, vh, vi);
