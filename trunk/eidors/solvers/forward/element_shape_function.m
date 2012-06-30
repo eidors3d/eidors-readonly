300a6e9fc30ae7b195febd4735a7538b445affbb
michaelcrabb30 <michaelcrabb30@82006732-fb7e-4a22-9f28-86a212cce563>
2012-06-30 17:45:31 +0000

    Added element shape functions for mass matrix in element_shape_function.m

--- trunk/eidors/solvers/forward/element_shape_function.m
+++ trunk/eidors/solvers/forward/element_shape_function.m
@@ -1,44 +1,84 @@
-function map = element_shape_function(type,x,y,z)
-%MAPFUNC  A simple transformation to map from local coordinates of unit 
-%triangle and tetrahedra to global coordinates (straight edged elements!)
-%MAP = mapfunc(TYPE,X,Y,Z)
+function shape = element_shape_function(type,x,y,z)
+%ELEMENT_SHAPE_FUNCTION  - Evaluation of shape functions
+%values each point
 %
-%USE: xglobal=x1*map(1)+x2*map(2)+x3*map(3)
-%     yglobal=y1*map(1)+y2*map(2)+y3*map(3)
 %
 %INPUT:
 %1. X, Y, Z - local coordinates
 %2. TYPE - string describing different element types
-%   'tri3','tri6','tri10' - Coordinate mapping triangle 
-%   'tet4','tet10' - Coordinate mapping tetrahedron
+%   'tri3' - Linear, 2 node 1D shape function 
+%   'tri 6'- Quadratic, 3 node 1D shape function
+%   'tet4' - Linear, 3 node triangle shape function
+%   'tet10' - Quadratic, 6 node triangle shape function
 %
 %OUTPUT
-%1. MAPFUNC - vector of map functions
+%1. MAPFUNC - vector of shape functions on this element
 %
 %M Crabb - 29.06.2012
 
-if(strcmp(type(1:3),'tri'))
-    map = maptri3(x,y,z);
-elseif(strcmp(type(1:3),'tet'))
-    map = maptet4(x,y,z);
+if(strcmp(type,'tri3'))
+    shape = elemshapetri3(x,y);
+elseif(strcmp(type,'tri6'))
+    shape = elemshapetri6(x,y);
+elseif(strcmp(type,'tri10'))
+    shape = elemshapetri10(x,y);
+elseif(strcmp(type,'tet4'))
+    shape = elemshapetet4(x,y);
+elseif(strcmp(type,'tet10'))
+    shape = elemshapetet10(x,y);
 else
     error('Incorrect number of input arguments')
 end
 
 
-function map = maptri3(x,y,z)
+function map = elemshapetri3(x,y)
 map(1) = 1-x-y;
 map(2) = x;
 map(3) = y;
 end
 
-function map = maptet4(x,y)
+function map = elemshapetri6(x,y)
 map(1) = (1-x-y)*(1-2*x-2*y);
 map(2) = x*(2*x-1);
 map(3) = y*(2*y-1);
 map(4) = 4*x*(1-x-y);
-map(5) = 4*y*(1-x-y);
-map(6) = 4*x*y;
+map(5) = 4*x*y;
+map(6) = 4*y*(1-x-y);
 end
+
+function map = elemshapetri10(x,y)
+map(1) = 0.5*(1-x-y)*(1-3*x-3*y)*(2-3*x-3*y);
+map(2) = 0.5*x*(3*x-2)*(3*x-1);
+map(3) = 0.5*y*(3*y-2)*(3*y-1);
+map(4) = 4.5*x*(1-x-y)*(2-3*x-3*y);
+map(5) = 4.5*x*(1-x-y)*(3*x-1);
+map(6) = 4.5*x*y*(3*x-1);
+map(7) = 4.5*x*y*(3*y-1);
+map(8) = 4.5*y*(1-x-y)*(3*y-1);
+map(9) = 4.5*y*(1-x-y)*(2-3*x-3*y);
+map(10) = 27*x*y*(1-x-y);
+end
+
+
+function map = elemshapetet4(x,y,z)
+map(1) = (1-x-y-z);
+map(2) = x;
+map(3) = y;
+map(4) = z;
+end
+
+function map = elemshapetet10(x,y,z)
+map(1) = (1-x-y-z)*(1-2*x-2*y-2*z);
+map(2) = x*(2*x-1);
+map(3) = y*(2*y-1);
+map(4) = z*(2*z-1);
+map(5) = 4*x*(1-x-y-z);
+map(6) = 4*y*(1-x-y-z);
+map(7) = 4*z*(1-x-y-z);
+map(8) = 4*x*y;
+map(9) = 4*y*z;
+map(10) = 4*x*z;
+end
+
 
 end
