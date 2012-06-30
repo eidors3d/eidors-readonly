ffe56e99e9e62d50fa54a47217c334c4d51c9fd6
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2012-06-30 14:20:14 +0000

    real and complex images

---
+++ trunk/htdocs/tutorial/EIDORS_basics/real_complex02.m
@@ -0,0 +1,13 @@
+% Create reconstruction model + solve
+imdl = mk_common_model('b2d1c',16);
+imgr = inv_solve(imdl, vh, vi);
+
+imgr.calc_colours.clim = 0.02;
+subplot(221); show_fem(imgr);
+title 'real conductivity change'
+
+imgr.calc_colours.component = 'imag';
+subplot(222); show_fem(imgr);
+title 'imag conductivity change'
+
+print_convert real_complex02a.png 
