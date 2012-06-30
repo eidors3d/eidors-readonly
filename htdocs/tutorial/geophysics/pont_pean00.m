429b6098cbf54b183fb3945a3f9c371d5010a702
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2012-06-30 18:18:44 +0000

    fixes to run tutorial tests
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@3278 82006732-fb7e-4a22-9f28-86a212cce563

---
+++ htdocs/tutorial/geophysics/pont_pean00.m
@@ -0,0 +1,3 @@
+urlwrite( ...
+'http://eidors3d.sourceforge.net/data_contrib/dg_geophysical_EIT/Mine_20FEV2004.zip', 'Mine_20FEV2004.zip');
+zipfilecontents = unzip('Mine_20FEV2004.zip');
--- htdocs/tutorial/geophysics/pont_pean01.m
+++ htdocs/tutorial/geophysics/pont_pean01.m
@@ -1,15 +1,16 @@
-% Forward Model
- shape_str = ['solid top    = plane(0,0,0;0,1,0);\n' ...
-              'solid mainobj= top and orthobrick(-100,-200,-100;425,10,100) -maxh=20.0;\n'];
- elec_pos = gps(:,2:4); e0 = elec_pos(:,1)*0;
- elec_pos = [  elec_pos, e0, e0+1, e0 ]; 
- elec_shape=[0.5,.5,.5];
- elec_obj = 'top';
- fmdl = ng_mk_gen_models(shape_str, elec_pos, elec_shape, elec_obj);
-
-% Load data and positions (unused in this tutorial)
+% Load data and positions
 gps = load('Mine_20FEV2004.gps');
 data= load('Mine_20FEV2004_LI.tomel');
-  fmdl.stimulation = stim_meas_list( data(:,3:6) - 40100);
+
+% Forward Model
+shape_str = ['solid top    = plane(0,0,0;0,1,0);\n' ...
+             'solid mainobj= top and orthobrick(-100,-200,-100;425,10,100) -maxh=20.0;\n'];
+elec_pos = gps(:,2:4); e0 = elec_pos(:,1)*0;
+elec_pos = [  elec_pos, e0, e0+1, e0 ]; 
+elec_shape=[0.5,.5,.5];
+elec_obj = 'top';
+fmdl = ng_mk_gen_models(shape_str, elec_pos, elec_shape, elec_obj);
+
+fmdl.stimulation = stim_meas_list( data(:,3:6) - 40100);
 
 show_fem(fmdl);
