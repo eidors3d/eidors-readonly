15c53fb392fc12501f9672a5caed22c40c0fa1af
michaelcrabb30 <michaelcrabb30@82006732-fb7e-4a22-9f28-86a212cce563>
2012-06-30 16:48:44 +0000

    Clean up system matrix calculation and remove 2nd jacobian

--- trunk/eidors/solvers/forward/calc_system_mat_higher_order.m
+++ trunk/eidors/solvers/forward/calc_system_mat_higher_order.m
@@ -1,6 +1,7 @@
 function [s_mat]=calc_system_mat_higher_order(fwd_model,img)
 %Assemble the total stiffness matrix : s_mat.E=At;
 %M Crabb - 29.06.2012
+%TODO - Sparse assignment of the matrices
 
 %Find no. of electrodes and no. of ndoes
 elecstruc=fwd_model.electrode; nelecs=size(elecstruc,2);
@@ -46,6 +47,9 @@ elemstruc=fwd_model.elem; nelems=size(elemstruc,2);
 eletype=fwd_model.approx_type; 
 [weight,xcoord,ycoord,zcoord]=element_gauss_points(eletype);
 
+%Initialise global stiffness matrix
+Agal=zeros(nnodes,nnodes); %sparse updating non zero slow
+
 %Loop over the elements and calculate local Am matrix
 for i=1:nelems
     %Find the list of node numbers for each element
@@ -75,24 +79,19 @@ for i=1:nelems
             (jacobianelem\element_d_shape_function(eletype,xcoord(kk),ycoord(kk),zcoord(kk)))*magjacelem;
     end
     %This is element stiffness matrix (and multiply by its conductivity)
-    elemstruc(i).stiff=Ammat*img.elem_data(i); 
-end
-
-
-%Initialise global stiffness matrix
-Agal=zeros(nnodes,nnodes);
-
-%Assemble global stiffness matrix (Silvester's book!!)
-for k=1:nelems %loop over elements
-    nnodeselems=size(elemstruc(k).nodes,2); %No. of nodes per element
-    for i=1:nnodeselems %loop over nodes in element
-        for j=1:nnodeselems %loop over nodes in element
-            Agal(elemstruc(k).nodes(i),elemstruc(k).nodes(j)) = ...
-            Agal(elemstruc(k).nodes(i),elemstruc(k).nodes(j)) + ...
-            (elemstruc(k).stiff(i,j));
+    stiff=Ammat*img.elem_data(i); 
+    
+    %Assemble global stiffness matrix (Silvester's book!!)
+    nnodeselems=size(elemstruc(i).nodes,2); %No. of nodes per element
+    for ii=1:nnodeselems %loop over nodes in element
+        for jj=1:nnodeselems %loop over nodes in element
+            Agal(elemstruc(i).nodes(ii),elemstruc(i).nodes(jj)) = ...
+            Agal(elemstruc(i).nodes(ii),elemstruc(i).nodes(jj)) +  stiff(ii,jj);
         end
-    end
+    end   
+    
 end
+ 
 end
 
 %COMPLETE ELECTRODE MATRICES
@@ -111,18 +110,9 @@ elecstruc=fwd_model.electrode; nelecs=size(elecstruc,2);
 boundstruc=fwd_model.bound; nbounds=size(boundstruc,2);
 nodestruc=fwd_model.nodes; nnodes=size(nodestruc,1); nodedim=size(nodestruc,2);
 
-
 %Connect boundary/electrode -Put boundary into old matrix strucutre
 for i=1:nbounds
     boundstrucold(i,:)=boundstruc(i).nodes;
-end
-%Loop over electrodes and store in elecstruc the boundary numbers and areas
-for ii=1:nelecs
-    %Andy Adler's function, outputs rows of mdl.boundary of electrode
-    [bdy_idx,bdy_area]=find_electrode_bdy(boundstrucold(:,1:nodedim),nodestruc,elecstruc(ii).nodes);
-    
-    %Store boundary numbers, and corresponding areas, in mdl.electrode(ii)
-    elecstruc(ii).boundidx=bdy_idx; elecstruc(ii).area=bdy_area;  
 end
 
 %Find fem type and find quadrature points/weights for integration over
@@ -131,86 +121,72 @@ eletype=fwd_model.approx_type;
 [weight,xcoord,ycoord]=boundary_gauss_points(eletype);
 
 
-%Loop over boundarys and calculate local Aw/Az matrices
-%TODO - Loop over elecstruc.boundidx, which has boudnary numbers to save time!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-for ii=1:nbounds
-    %List by row of coordinates of on the boundaryNodal coordinates on the boundary
-    thisb=nodestruc(boundstruc(ii).nodes,:);
-    
-    %Find the magnitude Jacobian of the mapping in 2D/3D
-    %NB:Scalings are consistent with reference element shape
-    if(nodedim==2)
-        %Jacobian = 0.5*|(x2-x1)| (x1,x2 vector of coords)
-        diff21=thisb(2,:)-thisb(1,:);
-        magjacbound=0.5*sqrt(diff21(1)^2+diff21(2)^2);
-    elseif(nodedim==3)
-        %Jacobian = |(x3-x1)x(x3-x2)| (x1,x2,x3 vector of coords)
-        diffprod=cross(thisb(3,:)-thisb(1,:),thisb(3,:)-thisb(2,:));
-        magjacbound=sqrt(diffprod(1)^2+diffprod(2)^2+diffprod(3)^2);
-    end
-
-    %Initialise Azlocmat/Awlocmat and find local matrices
-    Azmat=0; Awmat=0;
-    for kk=1:size(weight,2)
-        Azmat = Azmat + weight(kk)* ...
-            (boundary_shape_function(eletype,xcoord(kk),ycoord(kk)))'* ...
-            (boundary_shape_function(eletype,xcoord(kk),ycoord(kk)))*magjacbound;
-        Awmat = Awmat + weight(kk)* ...
-            (boundary_shape_function(eletype,xcoord(kk),ycoord(kk)))*magjacbound;
-    end              
-    %Multiply by the boundary scaling factor and store in fwd_model.boundary
-    boundstruc(ii).Azmat=Azmat; boundstruc(ii).Awmat=Awmat; 
-end
-
 %1. Initialise global Az/Aw/Ad matrices and assemble a la Silvester
-Az=zeros(nnodes,nnodes); Aw=zeros(nnodes,nelecs); Ad=zeros(nelecs,nelecs);
-%Loop over the electrodes
-for ke=1:nelecs
-    %Find boundary numbers (and areas) and contact impedance of electrode
-    elecbound=elecstruc(ke).boundidx;
-    elecimped=elecstruc(ke).z_contact;
-    elecboundarea=elecstruc(ke).area;
+Az=zeros(nnodes,nnodes); Aw=zeros(nnodes,nelecs); Ad=zeros(nelecs,nelecs); %sparse updating non zero slow
 
+%Loop over the electrodes
+for ke=1:nelecs    
+    %The boundary numbers and areas, outputs rows of mdl.boundary of electrode
+    [bdy_idx,bdy_area]=find_electrode_bdy(boundstrucold(:,1:nodedim),nodestruc,elecstruc(ke).nodes);
+    
+    %Store boundary numbers, and corresponding areas
+    boundidx_ke=bdy_idx; area_ke=bdy_area;
+    
+    %Find contact impedance of electrode
+    elecimped=elecstruc(ke).z_contact;   
+           
     %Find total electrode area (absolute values)
     elecarea=0;
-    for i=1:size(elecboundarea,2)
-        elecarea = elecarea + abs(elecboundarea(i));
+    for i=1:size(area_ke,2)
+        elecarea = elecarea + abs(area_ke(i));
     end
     
     %Form the matrix Ad
-    Ad(ke,ke)=elecarea/elecimped;
+    Ad(ke,ke)=elecarea/elecimped; 
     
-    %Loop over each boundary of electrode
-    for kb=1:size(elecbound,1) 
+    
+    %Loop over boundarys and calculate Aw/Az matrices
+    for ii=1:length(boundidx_ke)
+        %List by row of coordinates of on the boundaryNodal coordinates on the boundary
+        thisb=nodestruc(boundstruc(boundidx_ke(ii)).nodes,:);
+    
+        %Find the magnitude Jacobian of the mapping in 2D/3D
+        %NB:Scalings are consistent with reference element shape
+        if(nodedim==2)
+            %Jacobian = 0.5*|(x2-x1)| (x1,x2 vector of coords)
+            diff21=thisb(2,:)-thisb(1,:);
+            magjacbound=0.5*sqrt(diff21(1)^2+diff21(2)^2);
+        elseif(nodedim==3)
+            %Jacobian = |(x3-x1)x(x3-x2)| (x1,x2,x3 vector of coords)
+            diffprod=cross(thisb(3,:)-thisb(1,:),thisb(3,:)-thisb(2,:));
+            magjacbound=sqrt(diffprod(1)^2+diffprod(2)^2+diffprod(3)^2);
+        end
+
+        %Initialise Azlocmat/Awlocmat and find local matrices
+        Azmat=0; Awmat=0;
+        for kk=1:size(weight,2)
+            Azmat = Azmat + weight(kk)* ...
+                (boundary_shape_function(eletype,xcoord(kk),ycoord(kk)))'* ...
+                (boundary_shape_function(eletype,xcoord(kk),ycoord(kk)))*magjacbound;
+            Awmat = Awmat + weight(kk)* ...
+                (boundary_shape_function(eletype,xcoord(kk),ycoord(kk)))*magjacbound;
+        end         
+        
         %Node numbers for this boundary
-        boundnodes=boundstruc(elecbound(kb)).nodes;
+        boundnodes=boundstruc(boundidx_ke(ii)).nodes;
+                
         %Loop over the nodes on the boundary and form Az/Aw matrices
         for i=1:size(boundnodes,2)
             for j=1:size(boundnodes,2)
                 %Form Az matrix in inner loop
-                Az(boundnodes(i),boundnodes(j)) = Az(boundnodes(i),boundnodes(j)) + ...
-                    boundstruc(elecbound(kb)).Azmat(i,j)/elecimped;
+                Az(boundnodes(i),boundnodes(j)) = Az(boundnodes(i),boundnodes(j)) + Azmat(i,j)/elecimped;
             end
             %Form Aw matrix in outer loop
-            Aw(boundnodes(i),ke) = Aw(boundnodes(i),ke) - ...
-                boundstruc(elecbound(kb)).Awmat(i);
-        end
+            Aw(boundnodes(i),ke) = Aw(boundnodes(i),ke) - Awmat(i)/elecimped;
+        end        
+
     end
-    %Now divide by the imnpedance of electrode to get Aw
-    Aw(:,ke)=Aw(:,ke)/elecimped;
+       
 end
 
-
-%OLD STORAGE - NOT REQUIRED ANYMORE
-if 0
-    %Store the new boundary structure in the forward model
-    fwd_model.bound=boundstruc;
- 
-    %Store the matrices in fwd_model.solver
-    fwd_model.solver.Aw=Aw; fwd_model.solver.Az=Az; fwd_model.solver.Ad=Ad;
-
-    %Put the forward model back into the image
-    img.fwd_model=fwd_model;
 end
-end
-
