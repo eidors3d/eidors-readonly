6bb1c45c9fb51a118d12d6da987768abd4e2184b
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2012-06-30 12:18:33 +0000

    Surface 3D array simulation

--- trunk/htdocs/tutorial/geophysics/surface_sim03.m
+++ trunk/htdocs/tutorial/geophysics/surface_sim03.m
@@ -1,14 +1,11 @@
-if 0
 shape_str = ['solid top    = plane(0,0,0;0,0,1);\n' ...
              'solid block  = orthobrick(-4,-4,-2;4,4,2) -maxh=0.3;\n' ...
              'solid mainobj= top and block;\n'];
-[fmdl,mat_idx] = ng_mk_gen_models(shape_str, elec_pos, elec_shape, elec_obj);
-fmdl.stimulation = mk_stim_patterns(size(fmdl.electrode,2), 1, ... %rings
-    [0,7], [0,7], {'no_meas_current'},1);
-end
+fmdl = ng_mk_gen_models(shape_str, elec_pos, elec_shape, elec_obj);
+fmdl.stimulation = stim;
 
 % inverse model: a smaller region directly over the electrodes with coarser mesh
-shape_str = 'solid mainobj= orthobrick(-5,-5,-2.0;5,5,-0.2) -maxh=0.5;';
+shape_str = 'solid mainobj= orthobrick(-3,-3,-2.0;3,3,-0.2) -maxh=0.5;';
 cmdl = ng_mk_gen_models(shape_str, [], [], '');
 
 % define the mapping between the two meshes
@@ -17,8 +14,13 @@ c2f= mk_coarse_fine_mapping( fmdl, cmdl);
 % set the reconstruction parameters
 inv3d= select_imdl(fmdl, {'Basic GN dif'});
 inv3d.solve= @inv_solve_diff_GN_one_step;
-inv3d.hyperparameter.value = 1e-1;
-inv3d.RtR_prior= @laplace_image_prior;
-inv3d.RtR_prior= @prior_noser;
+inv3d.hyperparameter.value = 1e-3;
+inv3d.RtR_prior= @prior_laplace;
+%inv3d.RtR_prior= @prior_tikhonov;
 inv3d.fwd_model.coarse2fine = c2f;
 inv3d.rec_model = cmdl;
+
+hh= show_fem(cmdl); set(hh,'EdgeColor',[0,0,1]);
+hold on; show_fem(fmdl); hold off
+view(-16,22); set(gca,'Projection','perspective')
+print_convert surface_sim03a.png '-density 100'
