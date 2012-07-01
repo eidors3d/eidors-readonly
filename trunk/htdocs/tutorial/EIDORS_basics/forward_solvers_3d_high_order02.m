84c9bea26e75921faf029105702dfdecf4e8f30f
michaelcrabb30 <michaelcrabb30@82006732-fb7e-4a22-9f28-86a212cce563>
2012-07-01 15:25:35 +0000

    High order tutorials added 3D

---
+++ trunk/htdocs/tutorial/EIDORS_basics/forward_solvers_3d_high_order02.m
@@ -0,0 +1,18 @@
+%Get internal linear voltage distribution for first stimulation
+v1all = v1.volt; 
+img1n = rmfield(img1,'elem_data');
+img1n.node_data = v1all(1:size(fmdl.nodes,1),1); %add first stim data
+
+%Plot the distribution
+figure; subplot(121); show_slices(img1n,[inf,inf,2.5]);
+eidors_colourbar(img1n);
+
+%Get internal voltage distribution for difference eidors/high order
+img11n=img1n; 
+img11n.node_data=v1all(1:size(fmdl.nodes,1),1)-v0all(1:size(fmdl.nodes,1),1);
+
+%Plot the difference of two linear approximations
+subplot(122); show_slices(img11n,[inf,inf,2.5]);
+eidors_colourbar(img11n);
+
+print_convert forward_solvers_3d_high_order02a.png
