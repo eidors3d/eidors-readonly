79e12b45ff3835acb9a06cb8f0c468774c7fb909
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2012-07-01 21:48:17 +0000

    mdl_normalize

--- trunk/htdocs/tutorial/GREIT/adult_ex02.m
+++ trunk/htdocs/tutorial/GREIT/adult_ex02.m
@@ -1,6 +1,6 @@
 [stim,msel] = mk_stim_patterns(16,1,[0,1],[0,1],{'no_meas_current'},1);
 img.fwd_model.stimulation = stim;
-img.fwd_model.normalize_measurements=1;
+img.fwd_model = mdl_normalize(img.fwd_model, 1);
 opt.imgsz = [32 32];
 opt.distr = 3; % non-random, uniform
 opt.Nsim = 500; % 500 hundred targets to train on, seems enough
