139abddc9f09c7e17d39ceb6bc3cd0b85b0e83c6
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2012-07-01 21:50:51 +0000

    mdl_normalize

--- trunk/htdocs/tutorial/netgen/extrusion/pig_body09.m
+++ trunk/htdocs/tutorial/netgen/extrusion/pig_body09.m
@@ -1,6 +1,6 @@
 imdl = mk_common_model('c2c2',16);
 imdl.fwd_model.electrode = imdl.fwd_model.electrode([1,16:-1:2]);
-imdl.fwd_model.normalize_measurements= 1;
+imdl.fwd_model = mdl_normalize(imdl.fwd_model, 1);
 imr= inv_solve(imdl, vh, vi);
 
 show_fem(imr); axis tight; axis off; axis equal
