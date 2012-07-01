7fd509f83e8e17b9962a07a9fd3c1e3a198269af
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2012-07-01 21:25:30 +0000

    mdl_normalize

--- trunk/htdocs/tutorial/lung_EIT/tutorial410a.m
+++ trunk/htdocs/tutorial/lung_EIT/tutorial410a.m
@@ -6,7 +6,7 @@ imdl= mk_common_model('c2t3',16);
 
 
 % most EIT systems image best with normalized difference
-imdl.fwd_model.normalize_measurements= 1;
+imdl.fwd_model = mdl_normalize(imdl.fwd_model, 1);
 imdl.RtR_prior= @prior_gaussian_HPF;
 
 % electrodes start on back (dorsal), then do this
