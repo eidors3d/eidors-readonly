8c551079b8a8877a4858f7d2de1aaeb06f281be6
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2012-07-01 21:29:35 +0000

    mdl_normalize
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@3344 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/adv_image_reconst/electrode_errors04.m
+++ htdocs/tutorial/adv_image_reconst/electrode_errors04.m
@@ -5,7 +5,7 @@ fmdl.electrode = fmdl.electrode([9:16,1:8]);
 
 [stim,msel] = mk_stim_patterns(16,1,[0,1],[0,1],{'no_meas_current'},1);
 fmdl.stimulation = stim;
-fmdl.normalize_measurements=1;
+fmdl = mdl_normalize(fmdl, 1);
 fmdl.meas_select = msel;
 img = mk_image(fmdl, 1);
 img.elem_data([fmdl.mat_idx{2};fmdl.mat_idx{3}]) = 0.3; % lungs
