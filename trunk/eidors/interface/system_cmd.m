58432db5c8c54358a02929effd90b19480cac549
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2012-07-02 21:58:00 +0000

    octave protect

--- trunk/eidors/interface/system_cmd.m
+++ trunk/eidors/interface/system_cmd.m
@@ -7,13 +7,15 @@ function status = system_cmd( cmd );
 % (C) 2012 Andy Adler, License GPL v2 or v3
 % $Id$
 
-if strfind(system_dependent('getos'),'Linux')
-  %Version 7 under linux sets the LD_LIBRARY_PATH and that breaks netgen    
-  ldpath ='LD_LIBRARY_PATH=;';
-  % Problems are a) some newer matlab versions dont' need it,
-  %   b) what if people are working in tcsh?
+if ~exist('OCTAVE_VERSION')
+   if strfind(system_dependent('getos'),'Linux')
+     %Version 7 under linux sets the LD_LIBRARY_PATH and that breaks netgen    
+     ldpath ='LD_LIBRARY_PATH=;';
+     % Problems are a) some newer matlab versions dont' need it,
+     %   b) what if people are working in tcsh?
 
-  cmd = ['sh -c ''LD_LIBRARY_PATH=""; ',cmd,' '' '];
+     cmd = ['sh -c ''LD_LIBRARY_PATH=""; ',cmd,' '' '];
+   end
 end
 
 status = system(cmd);
