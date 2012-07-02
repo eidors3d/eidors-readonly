14b8a7b619cf487fb5a0d3ed39e1cd04f9acaeff
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2012-07-02 11:58:03 +0000

    Add a NOSER example
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@3387 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/geophysics/surface_sim04.m
+++ htdocs/tutorial/geophysics/surface_sim04.m
@@ -7,3 +7,16 @@ print_convert surface_sim04a.png '-density 100'
 levels= [inf,inf,-0.4,1,1; inf,inf,-1.0,2,1; inf,inf,-1.6,3,1];
 show_slices(imgr,levels)
 print_convert surface_sim04b.png '-density 150'
+
+% Try a different prior
+inv3d.RtR_prior= @prior_noser;
+inv3d.prior_use_fwd_not_rec = 1;
+inv3d.hyperparameter.value = 1e-1;
+
+imgr = inv_solve(inv3d, vh, vi);
+
+show_fem(imgr); view(-16,22); set(gca,'Projection','perspective')
+print_convert surface_sim04c.png '-density 100'
+
+show_slices(imgr,levels)
+print_convert surface_sim04d.png '-density 150'
