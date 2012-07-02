58cba251b6736dfeac4ad521a5f5c577d53dcc7f
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2012-07-02 13:46:33 +0000

    make graphics nicer and control colourbar

--- trunk/htdocs/tutorial/EIDORS_basics/forward_solvers_2d_high_order02.m
+++ trunk/htdocs/tutorial/EIDORS_basics/forward_solvers_2d_high_order02.m
@@ -4,13 +4,15 @@ img1n = rmfield(img1,'elem_data');
 img1n.node_data = v1all(1:size(fmdl.nodes,1),1); %add first stim data
 
 %Plot the distribution
-figure; subplot(121); show_fem(img1n,1);
+img1n.calc_colours.cb_shrink_move = [0.5,0.5,0];
+clf; subplot(221); show_fem(img1n,1);
 
 %Get internal voltage distribution for difference eidors/high order
 img11n=img1n; 
 img11n.node_data=v1all(1:size(fmdl.nodes,1),1)-v0all(1:size(fmdl.nodes,1),1);
 
 %Plot the difference of two linear approximations
-subplot(122); show_fem(img11n,1);
+img11n.calc_colours.cb_shrink_move = [0.5,0.5,0];
+subplot(222); show_fem(img11n,1);
 
 print_convert forward_solvers_2d_high_order02a.png
