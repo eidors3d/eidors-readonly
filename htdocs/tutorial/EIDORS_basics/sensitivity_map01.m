7c22c70ac89956848f4e10779afd74490858e9d6
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2012-07-02 14:04:24 +0000

    Normalize sensitivity by volume, and clean code
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@3397 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/EIDORS_basics/sensitivity_map01.m
+++ htdocs/tutorial/EIDORS_basics/sensitivity_map01.m
@@ -2,14 +2,15 @@
 
 imdl= mk_common_model('f2d1c',16); % 'j2d1c'
 J= calc_jacobian(calc_jacobian_bkgnd(imdl));
-img = mk_image(imdl, J(5,:)');
+Sens = J(5,:)'./get_elem_volume(imdl.fwd_model);
+img = mk_image(imdl, Sens);
 
 img.calc_colours.npoints= 256;
 img.calc_slices.filter = conv2(ones(3),ones(3));
-img.calc_colours.clim = .0004;
+img.calc_colours.clim = 0.5;
 show_slices(img);
 print_convert('sensitivity_map01a.png','-density 60');
 
 img.calc_colours.cb_shrink_move = [0.5,0.8,0];
-show_fem(img,[1,1]);
+clf;show_fem(img,[1,1]);
 print_convert('sensitivity_map01b.png','-density 60');
