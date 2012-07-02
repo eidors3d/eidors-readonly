95eb39610959cfb515ee2ef3bbcc672e78745e86
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2012-07-02 09:08:43 +0000

    if_peep_trial with GREIT on pig shape

--- trunk/htdocs/tutorial/lung_EIT/if_peep_trial01.m
+++ trunk/htdocs/tutorial/lung_EIT/if_peep_trial01.m
@@ -1,17 +1,18 @@
 % Create Model $Id$
 
-imdl= mk_common_model('c2c2',16);
-% Use normalized difference imaging
-imdl.fwd_model = mdl_normalize(imdl.fwd_model, 1);
-%imdl= mk_common_gridmdl('GREITc1');
+fmdl = mk_library_model('pig_23kg_16el');
+[fmdl.stimulation fmdl.meas_select] = mk_stim_patterns(16,1,'{ad}','{ad}');
+fmdl = mdl_normalize(fmdl, 1);  % Use normalized difference imaging
+opt.noise_figure = 0.5; opt.imgsz = [64 64];
+imdl = mk_GREIT_model(fmdl, 0.25, [], opt);
 
-subplot(211);
+% subplot(211);
 show_fem(imdl.fwd_model, [0,1,0]);
 axis equal; axis off
 
 hh=text(-1.1,0,'Right');
 set(hh,'Rotation',90,'HorizontalAlignment','Center');
-hh=text(0,+1.1,'Ventral');
+hh=text(0,-1.15,'Ventral');
 set(hh,'HorizontalAlignment','Center');
 
 print_convert if_peep_trial01.png
