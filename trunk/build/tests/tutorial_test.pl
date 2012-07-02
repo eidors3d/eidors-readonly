ec3da49228b1d688346c2f72ea3a95343be80b09
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2012-07-02 23:10:36 +0000

    tutorials test

--- trunk/build/tests/tutorial_test.pl
+++ trunk/build/tests/tutorial_test.pl
@@ -15,34 +15,12 @@ foreach (sort keys %tuts) {
     open F, "ls $cmd| sort|" or die $!;
     while ( <F> ) {
        print $_;
-       m{^.*/(.*).m$}; $octcmd .= " $1;";
+       m{^.*/(.*).m$}; $octcmd .= "disp([\"############## $1 ############\"]); $1;";
     }
     $octcmd .="'";
    print $octcmd;
    
-   system $octcmd; 
-last
-}
+   system "$octcmd";
 
 
-__DATA__
-my @files = File::Find::Rule->file()
-              ->name( "*.m" ) ->in( "../../htdocs/tutorial" );
 
-my %tuts;
-for my $file (@files) {
-   $tuts{$1}++ if $file =~ /(.*)[0-9][0-9].*\.m/;
-}
-  
-foreach (keys %tuts) {
-    my $files = `ls "$_*"`; 
-    print "$_\n";
-}
-find ../../htdocs/tutorial/ -name *.m | sort | \
-perl -ne'$tuts{$1}++ if /(.*)[0-9][0-9].*\.m/;' \
-   -e'END{foreach (keys %tuts){' \
-   -e' $files = `ls "$_*"`;' \
-   -e' print "$_\n";'  \
-   -e'};}'
-
- # `ls $_*.m`;'
