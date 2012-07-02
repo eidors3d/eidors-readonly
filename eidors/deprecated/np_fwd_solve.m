ee611dfb73ffca464fa55164de76d153623c2eb2
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2012-07-02 11:50:28 +0000

    tests using a 3D resistor vs analytic
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@3386 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/deprecated/np_fwd_solve.m
+++ eidors/deprecated/np_fwd_solve.m
@@ -13,6 +13,8 @@ function data= np_fwd_solve( fwd_model, img)
 % (C) 2005 Andy Adler. License: GPL version 2 or version 3
 % $Id$
 
+if isstr(fwd_model) && strcmp(fwd_model,'UNIT_TEST'); do_unit_test; return; end
+
 warning('EIDORS:deprecated','NP_FWD_SOLVE is deprecated as of 07-Jun-2012. Use FWD_SOLVE_1ST_ORDER instead.');
 
 p= np_fwd_parameters( fwd_model );
@@ -44,3 +46,61 @@ end; end
 try; if img.fwd_solve.get_all_nodes== 1
    data.volt = Vfwd;               % all, including CEM nodes
 end; end
+
+function do_unit_test
+   test_3d_resistor
+
+function test_3d_resistor
+   ll=5; % length
+   ww=1; % width
+   hh=1; % height
+   conduc= .13;  % conductivity in Ohm-meters
+   current= 4;  % Amps
+   z_contact= 9e-1;
+   scale = .46;
+   nn=0;
+   for z=0:ll; for x=0:ww; for y=0:hh
+      nn=nn+1;
+      mdl.nodes(nn,:) = [x,y,z];
+   end; end; end
+
+   mdl= mk_grid_model([],0:ww,0:hh,0:ll);
+   mdl.nodes= mdl.nodes*scale;
+   mdl= rmfield(mdl,'coarse2fine');
+   mdl.boundary= find_boundary(mdl.elems);
+   mdl.gnd_node = 1;
+   mdl.normalize_measurements = 0;
+   elec_nodes= [1:(ww+1)*(hh+1)];
+   elec(1).nodes= elec_nodes;      elec(1).z_contact= z_contact;
+   elec(2).nodes= nn-elec_nodes+1; elec(2).z_contact= z_contact;
+   stim.stim_pattern= [-1;1]*current;
+   stim.meas_pattern= [-1,1];
+   mdl.stimulation= stim;
+   mdl.electrode= elec;
+%  show_fem(mdl);
+
+   % analytical solution
+   R = ll / ww / hh / scale/ conduc + 2*z_contact/scale^2;
+
+   V= current*R;
+%  fprintf('Solver %s: %f\n', 'analytic', V);
+
+   mdl.solve = @np_fwd_solve;
+   mdl.system_mat = @np_calc_system_mat;
+%  mdl.misc.perm_sym= '{n}'; %%% Check it works without
+   img= mk_image(mdl, conduc);
+
+   fsol= fwd_solve(img);
+%  fprintf('Solver %s: %f\n', fsol.name, fsol.meas);
+   unit_test_cmp('np_fwd_solve vs analytic', fsol.meas, V, 1e-11);
+
+
+
+   mdl.solve = @fwd_solve_1st_order;
+   mdl.system_mat = @system_mat_1st_order;
+
+   img= mk_image(mdl, conduc);
+
+   fsol= fwd_solve(img);
+%  fprintf('Solver %s: %f\n', fsol.name, fsol.meas);
+   unit_test_cmp('new solver 2d vs analytic', fsol.meas, V, 1e-11);
