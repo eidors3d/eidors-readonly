f3bc23db41fb3ca4b1b2f18b35ac5541ecccde65
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2012-07-02 11:24:27 +0000

    solver tests against analytic
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@3383 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/solvers/forward/system_mat_1st_order.m
+++ eidors/solvers/forward/system_mat_1st_order.m
@@ -73,6 +73,17 @@ function do_unit_test
 
    S2 = system_mat_1st_order(img.fwd_model,img);
    unit_test_cmp('sys_mat2', S2.E(1,:), 2*[4,-1,-1,-1,-1,zeros(1,36)],1e-14);
+
+   imdl=  mk_common_model('a2C2',16);
+   img=  mk_image(imdl);
+   S1 = system_mat_1st_order(img.fwd_model,img);
+   unit_test_cmp('sys_mat1', S1.E(1,:), [4,-1,-1,-1,-1,zeros(1,52)],1e-14);
+
+   img.elem_data([1:16]) = 2;
+   img.calc_colours.clim = 4; show_fem(img,[0,0,3]);
+
+   S2 = system_mat_1st_order(img.fwd_model,img);
+   unit_test_cmp('sys_mat2', S2.E(1,:), 2*[4,-1,-1,-1,-1,zeros(1,52)],1e-14);
    
    idx = 41-(0:15);
    unit_test_cmp('sys_mat4', S2.E(idx,idx),   S1.E(idx,idx),1e-14);
@@ -92,6 +103,7 @@ function do_unit_test
       unit_test_cmp('sys_mat: error', 1,1);
    end
 
+   imdl=  mk_common_model('a2c2',16);
    cmdl = mk_circ_tank(4,[],0);
    c2f = mk_coarse_fine_mapping( img.fwd_model, cmdl); % spy(c2f)
    img.fwd_model.coarse2fine = c2f;
@@ -106,3 +118,102 @@ function do_unit_test
    S4 = system_mat_1st_order(img.fwd_model,img);
    unit_test_cmp('c2f #2', S1.E, S4.E,1e-14);
 
+   test_2d_resistor
+   test_3d_resistor
+
+function test_2d_resistor
+   nn= 12;     % number of nodes
+   ww=2;       % width = 4
+   conduc=  .4;% conductivity in Ohm-meters
+   current= 4;  % Amps
+   z_contact= 9e-1;
+   scale = .35;
+   mdl=mk_grid_model([],3+scale*(1:ww), scale*(1:nn/ww));
+   mdl= rmfield(mdl,'coarse2fine'); % don't calc this.
+
+   mdl.gnd_node = 1;
+   elec_nodes= [1:ww];
+   elec(1).nodes= elec_nodes;      elec(1).z_contact= z_contact;
+   elec(2).nodes= nn-elec_nodes+1; elec(2).z_contact= z_contact;
+   stim.stim_pattern= [-1;1]*current;
+   stim.meas_pattern= [-1,1];
+   mdl.stimulation= stim;
+   mdl.electrode= elec;
+   mdl.normalize_measurements = 0;
+   %show_fem(mdl);
+
+   % analytical solution
+   wid_len= max(mdl.nodes) - min(mdl.nodes);
+   R = wid_len(2) / wid_len(1) / conduc + 2*z_contact/scale;
+
+   V= current*R;
+   %fprintf('Solver %s: %f\n', 'analytic', V);
+
+   % AA_SOLVER
+   mdl.solve = @fwd_solve_1st_order;
+   mdl.system_mat = @system_mat_1st_order;
+   img = mk_image(mdl, conduc);
+   fsol= fwd_solve(img);
+   %fprintf('Solver %s: %f\n', fsol.name, fsol.meas);
+   unit_test_cmp('test 2d vs analytic', fsol.meas, V, 1e-11);
+
+   % NP_SOLVER
+   mdl.solve = @np_fwd_solve;
+   mdl.system_mat = @np_calc_system_mat;
+   img = mk_image(mdl, conduc);
+   fsol= fwd_solve(img);
+   %fprintf('Solver %s: %f\n', fsol.name, fsol.meas);
+   unit_test_cmp('np_fwd_solve vs analytic', fsol.meas, V, 1e-11);
+
+function test_3d_resistor
+   ll=5; % length
+   ww=1; % width
+   hh=1; % height
+   conduc= .13;  % conductivity in Ohm-meters
+   current= 4;  % Amps
+   z_contact= 9e-1;
+   scale = .46;
+   nn=0;
+   for z=0:ll; for x=0:ww; for y=0:hh
+      nn=nn+1;
+      mdl.nodes(nn,:) = [x,y,z];
+   end; end; end
+
+   mdl= mk_grid_model([],0:ww,0:hh,0:ll);
+   mdl.nodes= mdl.nodes*scale;
+   mdl= rmfield(mdl,'coarse2fine');
+   mdl.boundary= find_boundary(mdl.elems);
+   mdl.gnd_node = 1;
+   mdl.normalize_measurements = 0;
+   elec_nodes= [1:(ww+1)*(hh+1)];
+   elec(1).nodes= elec_nodes;      elec(1).z_contact= z_contact;
+   elec(2).nodes= nn-elec_nodes+1; elec(2).z_contact= z_contact;
+   stim.stim_pattern= [-1;1]*current;
+   stim.meas_pattern= [-1,1];
+   mdl.stimulation= stim;
+   mdl.electrode= elec;
+%  show_fem(mdl);
+
+   % analytical solution
+   R = ll / ww / hh / scale/ conduc + 2*z_contact/scale^2;
+
+   V= current*R;
+%  fprintf('Solver %s: %f\n', 'analytic', V);
+
+   mdl.solve = @fwd_solve_1st_order;
+   mdl.system_mat = @system_mat_1st_order;
+
+   img= mk_image(mdl, conduc);
+
+   fsol= fwd_solve(img);
+%  fprintf('Solver %s: %f\n', fsol.name, fsol.meas);
+   unit_test_cmp('test 2d vs analytic', fsol.meas, V, 1e-11);
+
+   mdl.solve = @np_fwd_solve;
+   mdl.system_mat = @np_calc_system_mat;
+   mdl.misc.perm_sym= '{n}';
+   img= mk_image(mdl, conduc);
+
+   fsol= fwd_solve(img);
+%  fprintf('Solver %s: %f\n', fsol.name, fsol.meas);
+   unit_test_cmp('np_fwd_solve vs analytic', fsol.meas, V, 1e-11);
