5a329b4dd7910845fe18d722542ace14a533ce5b
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2012-07-03 11:57:19 +0000

    Update tutorial to standard GREIT

--- trunk/htdocs/tutorial/GREIT/neonate_ex01.m
+++ trunk/htdocs/tutorial/GREIT/neonate_ex01.m
@@ -1,5 +1,11 @@
 % Inverse model
-imdl = mk_common_gridmdl('GREITc1');
+fmdl = mk_library_model('neonate_16el');
+[fmdl.stimulation fmdl.meas_select] = mk_stim_patterns(16,1,'{ad}','{ad}');
+fmdl = mdl_normalize(fmdl,1);
+img = mk_image(fmdl,1);
+opt.noise_figure = 0.5; opt.imgsz = [64 64];
+imdl = mk_GREIT_model(img, 0.25, [], opt);
+% imdl = mk_common_gridmdl('GREITc1');
 
 % Data: eidors3d.sf.net/data_contrib/if-neonate-spontaneous/if-neonate-spontaneous.zip
 vv= eidors_readdata('P04P-1016.get');
--- trunk/htdocs/tutorial/GREIT/neonate_ex02.m
+++ trunk/htdocs/tutorial/GREIT/neonate_ex02.m
@@ -1,9 +1,17 @@
-vh = mean(vv,2);        % reference is average
-vi = vv(:,[45,70,173]); %3 inspirations
+% solve with reference to the mean
+imgall = inv_solve(imdl,mean(vv,2),vv);
+[ins, exp] = find_frc(imgall,[],13,[],2); % find breaths
 
-img = inv_solve(imdl,vh,vi);
 
-img.show_slices.img_cols = 3;
+% use expirations as reference
+vh = mean(vv(:,exp),2);  % reference is average
+img = inv_solve(imdl,vh,vv);
+img.elem_data = img.elem_data(:,ins); %only show inspirations
+calc_colours('defaults');
+calc_colours('ref_level',0);
+calc_colours('backgnd',[1 1 1]);
+calc_colours('greylev', 0.001);
+img.show_slices.img_cols = 4;
 img.show_slices.sep      = 2;
 img.calc_colours.ref_level=0;
 show_slices(img);
