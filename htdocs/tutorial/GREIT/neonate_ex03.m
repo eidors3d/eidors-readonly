3990c6f351a0b117318e677e8e46c908b8ac11c1
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2012-07-03 11:57:19 +0000

    Update tutorial to standard GREIT
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@3454 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/GREIT/neonate_ex03.m
+++ htdocs/tutorial/GREIT/neonate_ex03.m
@@ -1,26 +1,30 @@
-vh = mean(vv,2);        % reference is average
-img = inv_solve(imdl,vh,vv);
-img.calc_colours.ref_level=0;
-img.calc_colours.npoints  =32;
-
-% Yposns of where to plot
-yposns = [10:5:25];
+% positions of where to plot
+yposns = [20 20 45 45];
+xposns = [20 45 20 45];
 
 % Show image
 clf; axes('position',[0.05,0.5,0.25,0.45]);
-img1= img; img1.elem_data = img.elem_data(:,45);
+img1= imgall; img1.elem_data = imgall.elem_data(:,45);
 show_slices(img1);
-hold on;
-plot(10,yposns,'s','LineWidth',5);
+hold all;
+for i = 1:4
+    plot(xposns(i),yposns(i),'s','LineWidth',5);
+end
 hold off;
 
 % Show plots
-imgs = calc_slices(img);
-axes('position',[0.30,0.6,0.65,0.25]);
+imgs = calc_slices(imgall);
+axes('position',[0.32,0.6,0.63,0.28]);
 
 imgs = permute(imgs,[3,1,2]);
 taxis =  (0:size(imgs,1)-1)/13; % frame rate = 13
-plot(taxis,imgs(:,yposns,10),'LineWidth',2);
-axis tight
+hold all
+for i = 1:4
+    plot(taxis,imgs(:,yposns(i),xposns(i)),'LineWidth',2);
+end
+hold off
+set(gca,'ytick',[]);
+xlim([0 16]);
+
 
 print_convert neonate_ex03a.png
