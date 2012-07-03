531afa8edf784b79b89d59da861e604c306d2731
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2012-07-03 09:04:41 +0000

    Support for overloading. Overload sparse
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@3445 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/startup.m
+++ eidors/startup.m
@@ -1,3 +1,7 @@
 % CUSTOM EIDORS STARTUP FILE
 % this must be a script so that octave can source it
+
+% we will be overloading built-in functions. Disable the warning.
+warning off MATLAB:dispatcher:nameConflict
 eidors_startup
+warning on MATLAB:dispatcher:nameConflict
