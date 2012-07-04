9d2016652b32ee1adb040684ef5bb6987b9a81d7
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2012-07-04 09:39:43 +0000

    improved sparse handing
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@3489 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/overloads/sparse.m
+++ eidors/overloads/sparse.m
@@ -1,11 +1,16 @@
-function S = sparse(X, varargin)
+function S = sparse(varargin)
 %SPARSE Create sparse matrix (EIDORS overload).
 %   S = SPARSE(X) converts a sparse or full matrix to sparse form by
 %   squeezing out any zero elements.
 %
 %  See also SPARFUN/SPARSE
+%
+% Sparse doesn't work for uint* inputs, so we need to preconvert to double
 
 % (C) 2011 Bartlomiej Grychtol. License: GPL v2 or v3.
 % $Id$
 
-S = builtin('sparse',double(X),varargin{:});
+for i= 1:nargin
+  varargin{i} = double(varargin{i});
+end
+S = builtin('sparse',varargin{:});
