722283c4752aff05db1d65c3178e78599d298995
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2012-07-05 14:32:51 +0000

    avoid info.xml errors in older versions of Matlab
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@3508 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/startup.m
+++ eidors/startup.m
@@ -3,5 +3,17 @@
 
 % we will be overloading built-in functions. Disable the warning.
 warning off MATLAB:dispatcher:nameConflict
-eidors_startup
+try
+    eidors_startup
+catch
+    [message id] = lasterr;
+    if strcmp(id,'MATLAB:infoXmlValidationError')
+        % An old version of MATLAB that we don't know how to write
+        % info.xml for
+    else
+        error.message = message;
+        error.identifier = id;
+        rethrow(error);
+    end
+end
 warning on MATLAB:dispatcher:nameConflict
