7eafd9fd413effc79339f12c9247a1dbb1202224
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2012-07-05 16:26:47 +0000

    post 3.6 development
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@3518 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/eidors_obj.m
+++ eidors/eidors_obj.m
@@ -75,7 +75,7 @@ switch type
       set_cache_obj( name, varargin{:} );
       obj_id= []; % quiet matlab errors
    case 'eidors_version'
-      obj_id= '3.6 ($Date$)';  % Update for New eidors version
+      obj_id= '3.6+ ($Date$)';  % Update for New eidors version
    case 'interpreter_version'
       obj_id= test_versions;
 % TODO: Add these functions
