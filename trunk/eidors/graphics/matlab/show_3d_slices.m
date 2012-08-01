7e5a196bff43d5d173ffaf113894e3d798aec046
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2012-08-01 11:17:34 +0000

    Integrate new visualisation code in the main tree

--- trunk/eidors/graphics/matlab/show_3d_slices.m
+++ trunk/eidors/graphics/matlab/show_3d_slices.m
@@ -6,17 +6,24 @@ function show_3d_slices(img, varargin);
 %  y_cuts = planes in y to do a cut
 % Default show 2 z_cuts and 1 x and 1 y cut
 
-% (C) 2007 Andy Adler. License: GPL version 2 or version 3
+% (C) 2007-2012 Andy Adler & Bartlomiej Grychtol. 
+% License: GPL version 2 or version 3
 % $Id$
 
 if isstr(img) && strcmp(img,'UNIT_TEST'); do_unit_test; return; end
-
+try 
+    pp.ref_level = img.calc_colours.ref_level; 
+    pp.clim      = img.calc_colours.clim;
+catch
+    [jnk,ref_lev,max_scale] = scale_for_display( img.elem_data);
+    img.calc_colours.ref_level = ref_lev;
+    img.calc_colours.clim = max_scale;
+end
 try 
     np = img.calc_colours.npoints;
 catch
     np = calc_colours('npoints');
 end
-
 %  show_fem(img.fwd_model);
 mdl_min = min(img.fwd_model.nodes);
 mdl_max = max(img.fwd_model.nodes);
@@ -27,57 +34,27 @@ xvec = linspace(mdl_min(1), mdl_max(1),np(1)+1);
 yvec = linspace(mdl_min(2), mdl_max(2),np(2)+1);
 zvec = linspace(mdl_min(3), mdl_max(3),np(3)+1);
 
-img.fwd_model.mdl_slice_mapper.x_pts  = xvec(1:end-1) + .5*diff(xvec);
-img.fwd_model.mdl_slice_mapper.y_pts  = yvec(1:end-1) + .5*diff(yvec);
+
 for i= 1:length(z_cuts)
-    gmdl = mk_grid_model([],xvec,yvec);
-    gmdl.nodes(:,3) = z_cuts(i);
-    level = [inf inf z_cuts(i)];
-    img.fwd_model.mdl_slice_mapper.level = level; 
-    pic  = calc_slices(img,level)';
-    show_slice(gmdl,pic,img);
+    show_slice(img,[inf inf z_cuts(i)]);
     hold on
 end
 
-img.fwd_model.mdl_slice_mapper.x_pts  = yvec(1:end-1) + .5*diff(yvec);
-img.fwd_model.mdl_slice_mapper.y_pts  = zvec(1:end-1) + .5*diff(zvec);
 for i= 1:length(x_cuts)
-    gmdl = mk_grid_model([],yvec,zvec);
-    gmdl.nodes(:,2:3) = gmdl.nodes;
-    gmdl.nodes(:,1) = x_cuts(i);
-    level = [x_cuts(i) inf inf];
-    img.fwd_model.mdl_slice_mapper.level = level;
-    pic  = calc_slices(img,level)';
-    show_slice(gmdl,pic,img);
+    show_slice(img,[x_cuts(i) inf inf]);
+    hold on
 end
 
-img.fwd_model.mdl_slice_mapper.x_pts  = xvec(1:end-1) + .5*diff(xvec);
-img.fwd_model.mdl_slice_mapper.y_pts  = zvec(1:end-1) + .5*diff(zvec);
 for i= 1:length(y_cuts)
-    gmdl = mk_grid_model([],xvec,zvec);
-    gmdl.nodes(:,[1 3]) = gmdl.nodes;
-    gmdl.nodes(:,2) = y_cuts(i);
-    level = [inf y_cuts(i) inf];
-    img.fwd_model.mdl_slice_mapper.level = level;
-    pic  = calc_slices(img,level)';
-    show_slice(gmdl,pic,img);
+    show_slice(img,[inf y_cuts(i) inf]);
+    hold on
 end
 hold off
 
-function show_slice(gmdl,pic,img)
-    ff   = find(isnan(pic(:)));
-    gmdl.elems([2*ff, 2*ff-1],:)= [];
-    gmdl.coarse2fine([2*ff, 2*ff-1],:)= [];
-    gmdl.coarse2fine(:,ff)= [];
-    gmdl = rmfield(gmdl,'boundary');
-    opt.boundary = true;
-    gmdl.fwd_model = fix_model(gmdl, opt);
-    gim  = mk_image(gmdl,1);
-    gim.elem_data(1:2:end) = pic(~isnan(pic));
-    gim.elem_data(2:2:end) = pic(~isnan(pic));
-    try gim.calc_colours = img.calc_colours; end
-    try gim.calc_slices = img.calc_slices; end
-    show_fem(gim);
+function show_slice(img, level)
+    slc = mdl_slice_mesher(img,level);
+    try slc.calc_colours = img.calc_colours; end
+    show_fem(slc);
 
     
 function [x_cuts, y_cuts, z_cuts] =  get_cuts(img, varargin)
@@ -105,22 +82,6 @@ function [x_cuts, y_cuts, z_cuts] =  get_cuts(img, varargin)
    end
    
 
-
-function draw_line_around(cimg, rimg, x,y, M_trans, M_add);
-% The MATLAB contour functions are inflexible crap. We
-% Need to completely break them to get it to work
-% [x_ax;y_ax;z_ax] = M_trans * [x_ax_matlab;y_ax_matlab] + M_add
-% For x,z plane at y level 4 we have
-% M_trans = [1 0;0 0;0 1]; M_add= [0;4;0];
-
-   cimg(isnan(rimg))= -1e50;
-   [jnk,hh]=contour(x,y,cimg, [-1e49,-1e49]);
-   Contour_paths = M_trans*[ get(hh,'Xdata'), ...
-                             get(hh,'Ydata') ]';
-   set(hh,'EdgeColor',[0,0,0], ...
-          'Xdata', Contour_paths(1,:) + M_add(1), ...
-          'Ydata', Contour_paths(2,:) + M_add(2), ...
-          'Zdata', Contour_paths(3,:) + M_add(3));
 
 function do_unit_test
    imdl = mk_common_model('n3r2',[16,2]);
