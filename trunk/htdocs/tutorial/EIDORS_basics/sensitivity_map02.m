254b97b481abec31bed1b179f5b81c2bb3f3348a
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2012-08-01 12:04:39 +0000

    Fix to use new graphics

--- trunk/htdocs/tutorial/EIDORS_basics/sensitivity_map02.m
+++ trunk/htdocs/tutorial/EIDORS_basics/sensitivity_map02.m
@@ -3,13 +3,14 @@
 fwd_model= ng_mk_cyl_models([2,1,.07],[16,1.0],[0.1]);
 fwd_model.stimulation = mk_stim_patterns(16,1,[0,1],[0,1],{},1);
 J= calc_jacobian( mk_image(fwd_model,1) );
-Sens = J(5,:)'./get_elem_volume(imdl.fwd_model);
-img = mk_image(imdl, Sens');
+Sens = J(5,:)'./get_elem_volume(fwd_model);
+img = mk_image(fwd_model, Sens');
 
 img.calc_colours.clim= 3e-2;
 
 img.calc_colours.npoints= 256;
-img.calc_slices.filter = conv2(ones(3),ones(3));
+% img.calc_slices.filter = conv2(ones(3),ones(3));
+img.calc_colours.transparency_thresh = -1;
 show_3d_slices(img,[0.7,1.0],[0.5],[]);
 view(-70,22);
 print_convert('sensitivity_map02a.png','-density 60');
@@ -17,6 +18,7 @@ view(-70,62);
 print_convert('sensitivity_map02b.png','-density 60');
 
 np = interp_mesh(img.fwd_model);
+img.calc_colours.transparency_thresh = .15;
 img.elem_data(np(:,3)>1.05)= 0;
 show_fem(img);
 
