431adc4bcaeb98d68fc8c9e94d3056e72af06f3b
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2012-08-02 12:25:09 +0000

    fix GREIT square_pixels bug and weird show_fem problems

--- trunk/htdocs/tutorial/adv_image_reconst/electrode_errors02.m
+++ trunk/htdocs/tutorial/adv_image_reconst/electrode_errors02.m
@@ -3,11 +3,11 @@
 imdl.meas_icov = meas_icov_rm_elecs( imdl, 13);
 
 img = inv_solve(imdl, vh, vi(:,20));
-show_fem(img,[0,1,0]);
+show_fem(img,[0,1,0]); axis off
 print_convert electrode_errors02a.jpg
 
-imdl.meas_icov = meas_icov_rm_elecs( imdl, [13,5]);
 
+imdl.meas_icov = meas_icov_rm_elecs( imdl, [13,5]);
 img = inv_solve(imdl, vh, vi(:,20));
-show_fem(img,[0,1,0]);
+show_fem(img,[0,1,0]); 
 print_convert electrode_errors02b.jpg
