d93541de90ee824177f56fb97d00a5bdefeb66c8
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2012-08-15 12:35:59 +0000

    work in progress. need work on Jacobian for optics

--- trunk/dev/l_cao/test.m
+++ trunk/dev/l_cao/test.m
@@ -9,9 +9,15 @@ end
 
 
 for i = 1:length(fmdl.stimulation);
-   fmdl.stimulation(i).stim_pattern(fmdl.stimulation(i).stim_pattern < 0 ) = 0;
+   fmdl.stimulation(i).stim_pattern(fmdl.stimulation(i).stim_pattern > 0 ) = 0;
+   fmdl.stimulation(i).stim_pattern(fmdl.stimulation(i).stim_pattern < 0 ) = 10;
+   fmdl.stimulation(i).meas_pattern = sparse(diag(ones(16,1)));
+   fmdl.stimulation(i).meas_pattern(i,i) = 0;
+   fmdl.stimulation(i).meas_pattern(i,:) = [];
 end
-imdl.fwd_model = fmdl;
+fmdl.meas_select = true(256,1);
+fmdl.meas_select(1:17:256) = false;
+
 %Default EIDORS solver
 %Make image of unit conductivity
 img0 = mk_image(fmdl,0.15);
@@ -25,16 +31,25 @@ v0e=v0.meas; v0all=v0.volt;
 fmdl.solve = @fwd_solve_higher_order;
 % fmdl.system_mat = @system_mat_higher_order; 
 fmdl.system_mat = @calc_system_mat_opt;
+fmdl.jacobian = @jacobian_adjoint_opt;
+imdl.fwd_model = fmdl;
 
 %Add element type
 fmdl.approx_type    = 'tri3'; % linear
 %Make an image and get voltages using high order solver
-img1 = mk_image(fmdl,1);
+img1 = mk_image(fmdl,0.15);
 img1.fwd_solve.get_all_meas = 1; %Internal voltage
 v1 = fwd_solve(img1); 
 v1e=v1.meas; v1all=v1.volt;
 
 
 %Plot electrode voltages and difference
-clf;subplot(211); plot([v0e,v1e,[v0e-v1e]*100]);
-legend('0','1','(1-0) x 100'); xlim([1,100]);
+clf;subplot(211); plot([v0e,v1e]);
+legend('0','1'); xlim([1,256]);
+
+v0 = fwd_solve(img1);
+img1.elem_data(361) = 0.5;
+v1 = fwd_solve(img1);
+rimg = inv_solve(imdl,v1,v0);
+clf
+show_fem(rimg,[0 0 1]);
