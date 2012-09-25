01857fa77affb427049512e0bef3db9cf205662a
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2012-09-25 19:14:42 +0000

    absolute code
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@3594 82006732-fb7e-4a22-9f28-86a212cce563

---
+++ htdocs/tutorial/strange_effects/jacobian_absolute.m
@@ -0,0 +1,5 @@
+function J = jacobian_absolute( fwd_model, img);
+    vh = fwd_solve(img);
+    flip = sign(vh.meas);
+    flip = spdiags(flip, 0, length(flip), length(flip));
+    J = flip*jacobian_adjoint(fwd_model,img);
