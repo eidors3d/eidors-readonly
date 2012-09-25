2aff51531878bb0730ae2b75088133631cc1b0dd
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2012-09-25 18:39:39 +0000

    absolute value code
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@3593 82006732-fb7e-4a22-9f28-86a212cce563

---
+++ htdocs/tutorial/strange_effects/absolute_value03.m
@@ -0,0 +1,15 @@
+% create inverse model
+imdl = mk_common_model('c2c2',16);
+imdl.fwd_model.stimulation = stim;
+imdl.fwd_model.meas_select = msel;
+imdl.hyperparameter.value = 0.01;
+
+% reconstruct normally
+show_fem( inv_solve( imdl, vh, vi));
+print_convert absolute_value03a.png
+
+% reconstruct abs
+vha = abs(vh);
+via = abs(vi);
+show_fem( inv_solve( imdl, vha, via));
+print_convert absolute_value03b.png
