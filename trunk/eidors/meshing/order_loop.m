02581ac73827429837d6461d563a409c04f655a1
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2012-10-17 12:21:23 +0000

    order_loop + docs + unit test + moved to main tree

---
+++ trunk/eidors/meshing/order_loop.m
@@ -0,0 +1,65 @@
+function p = order_loop(pp,clk)
+%ORDER_LOOP Order a list of points on a loop
+% P = ORDER_LOOP(PP) orders clockwise a matrix PP (N x D) of N points in D 
+% dimensions that constitute a continues loop, under the assumption that 
+% the distance between two neighbouring points is always smaller than 
+% distance between any non-neighbouring points.
+
+% P = ORDER_LOOP(PP, CLK) orders the loop clockwise if CLK == 1 and
+% counter-clockwise if CLK == 0.
+
+% (C) 2012 Bartlomiej Grychtol. 
+% License: GPL version 2 or version 3
+% $Id$
+
+if ischar(pp) && strcmp(pp,'UNIT_TEST'); do_unit_test; return, end;
+
+if nargin == 1
+   clk = 1;
+end
+
+D = distmat(pp) + diag(inf*ones(length(pp),1));
+p = zeros(size(pp));
+p(1,:) = pp(1,:);
+idx1 = 1;
+for i = 1:length(pp)-1
+   [jnk,idx2] = min(D(idx1,:));
+   p(i+1,:) = pp(idx2,:);
+   D(:,idx1) = [];
+   D(idx1,:) = [];
+   pp(idx1,:) = [];
+   if idx2 > idx1
+      idx1 = idx2-1;
+   else
+      idx1 = idx2;
+   end
+end
+ctr = mean(p);
+tmp = p - repmat(ctr,length(p),1);
+th = unwrap(cart2pol(tmp(:,1),tmp(:,2)));
+
+if th(1) < th(end)
+   %counter-clockwise
+   p = flipud(p);
+end
+
+if clk == -1
+   p = flipud(p);
+end
+
+
+function do_unit_test
+t = linspace(0,2*pi,101); t(end) = [];
+p1(:,1) = sin(t);
+p1(:,2) = 4*cos(t);
+n = randperm(100);
+p2 = p1(n,:);
+p3 = order_loop(p2);
+exp = circshift(p1,-n(1));
+if all(all(p3 == exp ))
+   disp('OK');
+else
+   disp('Fail');
+end
+
+
