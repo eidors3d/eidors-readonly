a45cdef889c874827e2b59fe2f068057548b9b0f
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2012-11-18 22:19:43 +0000

    fix pictures
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@3634 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/strange_effects/conformal1_03.m
+++ htdocs/tutorial/strange_effects/conformal1_03.m
@@ -2,24 +2,26 @@
 
 nodes = img.fwd_model.nodes; mn = min(nodes); mx= max(nodes);
 img.fwd_model.mdl_slice_mapper.x_pts = linspace(mn(1),mx(1),50);
-img.fwd_model.mdl_slice_mapper.y_pts = linspace(mn(2),mx(2),10);
+img.fwd_model.mdl_slice_mapper.y_pts = linspace(mn(2),mx(2),15);
 img.calc_colours.clim = 5;
 
-show_fem(img); axis image;
+hh=show_fem(img); axis image;
+set(hh,'EdgeColor',[0.5,0.5,0.5]);
 hold on;
 show_current( img); 
 hold off;
 
-print_convert conformal1_03a.png '-density 125'
+print_convert conformal1_03a.png '-density 150'
 
 nodes = img2.fwd_model.nodes; mn = min(nodes); mx= max(nodes);
 img2.fwd_model.mdl_slice_mapper.x_pts = linspace(mn(1),mx(1),50);
 img2.fwd_model.mdl_slice_mapper.y_pts = linspace(mn(2),mx(2),50);
 img2.calc_colours.clim = 5;
 
-show_fem(img2); axis image;
+hh= show_fem(img2); axis image;
+set(hh,'EdgeColor',[0.5,0.5,0.5]);
 hold on;
 show_current( img2); 
 hold off;
 
-print_convert conformal1_03a.png '-density 125'
+print_convert conformal1_03b.png '-density 125'
