ac4f8bc436a0a9c3a80ccec7db62ff2956cb7144
michaelcrabb30 <michaelcrabb30@82006732-fb7e-4a22-9f28-86a212cce563>
2012-11-18 19:15:12 +0000

    Anisotropic code changes - This could be buggedsvn add anisotropy/
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@3632 82006732-fb7e-4a22-9f28-86a212cce563

---
+++ dev/m_crabb/forward/anisotropy/forward_anisotropy.m
@@ -0,0 +1,44 @@
+%Make an inverse model and extract forward model
+imdl = mk_common_model('n3r2',[16,1]);
+fmdl = imdl.fwd_model;
+
+%Assign a matrix to each elem data
+n_elem=size(fmdl.elems,1); 
+for i=1:n_elem 
+    %Symmetric conductivity tensor Cartesian coordinates
+    elem_data(i,1,1,1) =1;
+    elem_data(i,1,1,2) =0; elem_data(i,1,2,1) =0;
+    elem_data(i,1,1,3) =0; elem_data(i,1,3,1) =0;
+    elem_data(i,1,2,2) =1;
+    elem_data(i,1,2,3) =0; elem_data(i,1,3,2) =0;
+    elem_data(i,1,3,3) =1;   
+end
+
+%High-order EIDORS solver
+%Change default eidors solvers
+fmdl.system_mat = @system_mat_higher_order_anisotropy;
+
+%Add element type
+fmdl.approx_type    = 'tet4'; % linear
+%Make an image and get voltages using high order solver
+img1 = mk_image_anisotropy(fmdl,elem_data);
+img1.fwd_solve.get_all_meas = 1; %Internal voltage
+v1 = fwd_solve(img1); 
+v1e=v1.meas; v1all=v1.volt;
+
+%Plot electrode voltages and difference
+figure; plot([v1e]);
+legend('1'); 
+
+%Plot the voltage distribution
+v1all = v1all; 
+img1n = rmfield(img1,'elem_data');
+img1n.node_data = v1all(1:size(fmdl.nodes,1),1); %add first stim data
+figure; show_fem(img1n,1);
+
+%Plot the current distribution
+img_v = img1;
+img_v.fwd_model.mdl_slice_mapper.npx = 64;
+img_v.fwd_model.mdl_slice_mapper.npy = 64;
+img_v.fwd_model.mdl_slice_mapper.level = [inf,inf,1.0];
+figure; show_current(img_v, v1.volt(:,1));
---
+++ dev/m_crabb/forward/anisotropy/mk_image_anisotropy.m
@@ -0,0 +1,52 @@
+function img= mk_image_anisotropy(mdl, elem_data, name)
+% MK_IMAGE: create eidors image object
+%   img= mk_image(mdl, elem_data, name)
+%
+% Utility function to create an eidors_image object:
+% Usage 1:
+%   img = mk_image( inv_model )  -> uses jacobian backgnd for conductivity
+%
+% Usage 2: mdl can be a fwd_model or inv_model
+%   img = mk_image( mdl, 1 ) -> uniform image with conductivity 1
+%   img = mk_image( mdl, 2*ones(n_elems,1) ) -> uniform with c=2
+%   img = mk_image( mdl, 1, 'This name') -> Specify a 'name' attribute
+% 
+% Usage 3: create image from previous image, override conductity
+%  img = mk_image( other_image, 2 ) -> image with c=2
+
+% (C) 2008-10 Andy Adler. Licenced under GPL version 2 or 3
+% $Id: mk_image.m 3095 2012-06-08 13:37:44Z aadler $
+
+if isstr(mdl) && strcmp(mdl,'UNIT_TEST'); do_unit_test; return; end
+
+if nargin<3; name = 'Created by mk_image'; end
+if nargin<2; 
+   try
+     elem_data = mdl.jacobian_bkgnd.value;
+   catch
+     error('mk_image: for one parameter, needs a mdl.jacobian_bkgnd field');
+   end
+end
+
+switch mdl.type
+   case 'inv_model'; mdl = mdl.fwd_model;
+   case 'fwd_model'; mdl = mdl; % keep model
+   case 'image';     mdl = mdl.fwd_model;
+   otherwise; error('mk_image: no inv_model, fwd_model or image object');
+end
+
+img = eidors_obj('image',name);
+img.fwd_model = mdl;
+img.elem_data = elem_data;
+
+% TESTS:
+function do_unit_test
+   imdl = mk_common_model('a2c2',8);
+   im0 = mk_image( imdl );
+   unit_test_cmp('im1',im0.elem_data, ones(64,1) );
+
+   im0 = mk_image( imdl, 2 );
+   unit_test_cmp('im2',im0.elem_data, 2*ones(64,1) );
+
+   im0 = mk_image( imdl.fwd_model, 3*ones(64,1) );
+   unit_test_cmp('im3',im0.elem_data, 3*ones(64,1) );
