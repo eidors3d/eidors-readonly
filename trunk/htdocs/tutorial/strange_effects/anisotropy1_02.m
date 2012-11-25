29708a5bd6ff137dd0143a704f2b1f8af6bf954c
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2012-11-25 14:10:54 +0000

    fix contours

--- trunk/htdocs/tutorial/strange_effects/anisotropy1_02.m
+++ trunk/htdocs/tutorial/strange_effects/anisotropy1_02.m
@@ -1,21 +1,20 @@
-
-% deformation
+% Deformation $Id$
 th = fmdl.nodes(:,1)/yt*(pi);
 y = fmdl.nodes(:,2); y = y.*(th<=0) - y.*(th>0);
 th = (th-pi/2).*(th<=0) + (pi/2-th).*(th>0);
 [x,y] = pol2cart(th, y+4);
 y = y+8.*(fmdl.nodes(:,1)<=0);
-img2 = img; img2.fwd_model.nodes = [2*x, y];
+img2 = img; img2.fwd_model.nodes = [1.5*x, y];
 
-show_fem(img2); axis image
 img2.fwd_solve.get_all_meas = 1;
 vh = fwd_solve(img2);
 
-img2.fwd_model.mdl_slice_mapper.npx = 200;
-img2.fwd_model.mdl_slice_mapper.npy = 100;
-q=  show_current(img2,vh.volt);
-hh=show_fem(img2);
+img_v = rmfield(img2,'elem_data');
+img_v.node_data = vh.volt;
+hh=show_fem(img_v);
 set(hh,'EdgeColor',[1,1,1]*.75);
+
+q=  show_current(img2,vh.volt);
 hold on;
 sx =  linspace(1.1,6.9,15); sy =  0*sx;
 sy = -linspace(1.1,6.9,15); sx =  0*sy;
