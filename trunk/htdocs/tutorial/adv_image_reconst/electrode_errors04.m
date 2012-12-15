2d9b868a6613c1ef906e2b89239f491cd3d24ac2
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2012-12-15 07:18:33 +0000

    fix OS detection

--- trunk/htdocs/tutorial/adv_image_reconst/electrode_errors04.m
+++ trunk/htdocs/tutorial/adv_image_reconst/electrode_errors04.m
@@ -1,7 +1,7 @@
 % Reject electrodes + Image $Id$
 
 fmdl= mk_library_model('adult_male_16el_lungs');
-fmdl.electrode = fmdl.electrode([9:16,1:8]);
+% fmdl.electrode = fmdl.electrode([9:16,1:8]);
 
 [stim,msel] = mk_stim_patterns(16,1,[0,1],[0,1],{'no_meas_current'},1);
 fmdl.stimulation = stim;
