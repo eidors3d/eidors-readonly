a42341f5093eefe962271cc42531e50437c55300
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2013-01-10 19:15:34 +0000

    multi-physics inv_solve_abs_GN + apparent resistivity first draft
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@3722 82006732-fb7e-4a22-9f28-86a212cce563

---
+++ dev/physics/jacobian_apparent_resistivity.m
@@ -0,0 +1,53 @@
+function data =jacobian_apparent_resistivity(fwd_model, img)
+
+% $Id$
+
+% correct input paralemeters if function was called with only img
+if isstr(fwd_model) && strcmp(fwd_model,'UNIT_TEST'); do_unit_test; return; end
+
+if nargin == 1
+   img= fwd_model;
+else
+   warning('EIDORS:DeprecatedInterface', ...
+      ['Calling JACOBIAN_APPARENT_RESISTIVITY with two arguments is deprecated and will cause' ...
+       ' an error in a future version. First argument ignored.']);
+end
+fwd_model= img.fwd_model;
+
+if mdl_normalize(fwd_model)
+   error('Cannot calculate apparent resistivity for normalized difference data');
+end
+
+
+solver = @eidors_default;
+try
+   solver = fwd_model.jacobian_apparent_resistivity.jacobian;
+end
+
+img.fwd_model.jacobian = solver;
+
+J = calc_jacobian(img);
+
+fctr = get_factor(img);
+
+J = fctr * J;
+
+function fctr = get_factor(img)
+fctr = NaN;
+try
+   fctr = img.fwd_model.apparent_resistivity_factor;
+end
+
+if isstr(fctr), fctr = str2func(fcstr); end;
+
+if isa(fctr, 'function_handle')
+   fctr = feval(fctr, img);
+end
+
+if isnan(fctr)
+   vh = fwd_solve(mk_image(img.fwd_model,1));
+   n = length(vh.meas);
+   fctr = spdiags(1./vh.meas,0,n,n);
+end
+
+
