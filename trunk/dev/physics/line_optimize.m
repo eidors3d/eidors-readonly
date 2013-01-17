3ef81f300b15ded119a07dcfa04457c745825f6a
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2013-01-17 16:18:28 +0000

    Add opt.objective_func to line_optimize, fix objective function in GN solver

--- trunk/dev/physics/line_optimize.m
+++ trunk/dev/physics/line_optimize.m
@@ -1,4 +1,40 @@
-function [img fmin res] = line_optimize(imgk, dx, data1, opt)
+function [img fmin res] = line_optimize(imgk, dx, data0, opt)
+%LINE_OPTIMIZE Cheap line optimizer
+% [img fmin res] = line_optimize(imgk, dx, data0, opt)
+% img     : output image
+% fmin    : optimal step size
+% res     : value of the objective function
+% imgk    : starting image
+% dx      : step direction
+% data0   : data to fit
+% opt     : options structure
+%
+% Options:
+%   opt.perturb  : vector of step sizes to try (default: [0.1 0.5 1.0])
+%   opt.min_value: lower limit of img values (default: -Inf)
+%   opt.max_value: upper limit of img values (default: Inf)
+%   opt.objective_function: @my_objective_fun
+%       handle to an objective with the following signature
+%           val = my_objective_fun(data0, data, img0, img, opt)
+%       where:
+%           data0   : data to fit
+%           data    : data simulated on the current image
+%           img0    : starting image
+%           img     : current image
+%           opt     : the entire option structure (can be used to pass
+%           additional parameters)
+%       The default objective function is:
+%           val = norm(calc_difference_data(data0,data, img0.fwd_model));
+%
+% Note that the value of fmin will be limited by the maximum and minimum
+% given in the opt.perturb vector.
+%
+% See also: INV_SOLVE_ABS_GN
+
+% (C) 2010-2013 Copyright Bartłomiej Grychtol, Andy Adler & Nolwenn Lesparre.
+% License: GPL version 2 or 3.
+% $Id$
+
 
 if nargin <4 || isempty(opt)
    opt = struct;
@@ -6,15 +42,11 @@ end
 
 opt = parse_options(opt);
 
-imgk = physics_data_mapper(imgk);
-
 img = imgk;
 for i = 1:length(opt.perturb)
    img = calc_perturb(imgk,opt.perturb(i),dx, opt);
    vsim = fwd_solve(img);
-   % aren't we better off just calculating the difference ourselves?
-   dv = calc_difference_data( vsim, data1, img.fwd_model);
-   mlist(i) = norm(dv);
+   mlist(i) = feval(opt.objective_func,data0,vsim,imgk,img,opt);
 end
 
 % perform quadratic line fit in log space
@@ -48,11 +80,12 @@ end
 % RETURN VALUES
 img  = calc_perturb(imgk,fmin,dx,opt);
 vsim = fwd_solve(img);
-res  = calc_difference_data( vsim, data1, img.fwd_model);
+res  = feval(opt.objective_func,data0,vsim,imgk,img,opt);
 
 
 
 function img = calc_perturb(imgk, p, dx, opt)
+   imgk = physics_data_mapper(imgk);
    img = imgk;
    img.elem_data = imgk.elem_data + p*dx;
    img = apply_limits(img,opt);
@@ -64,6 +97,11 @@ img.elem_data(img.elem_data > opt.max_value) = opt.max_value;
 img.elem_data(img.elem_data < opt.min_value) = opt.min_value;
 
 
+function val = default_obj_fun(data0, data, img0, img, opt)
+dv = calc_difference_data(data0,data,img0.fwd_model);
+val = norm(dv);
+
+
 function opt = parse_options(opt)
 if ~isfield(opt,'perturb')
    opt.perturb = [0.1 0.5 1.0];
@@ -73,7 +111,10 @@ if ~isfield(opt,'min_value')
    opt.min_value = -Inf;
 end
 
-
 if ~isfield(opt,'max_value')
    opt.max_value = Inf;
+end
+
+if ~isfield(opt,'objective_func')
+    opt.objective_func = @default_obj_fun;
 end
