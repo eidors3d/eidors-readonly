00eb54ca84d7021a963ef64a77ed181a1d64c19b
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2013-02-07 16:01:51 +0000

    Developments for 2.5D
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@3763 82006732-fb7e-4a22-9f28-86a212cce563

--- dev/a_adler/system_mat_2_5D_1st_order.m
+++ dev/a_adler/system_mat_2_5D_1st_order.m
@@ -1,17 +1,27 @@
-function s_mat= aa_calc_system_mat( fwd_model, img)
-% AA_CALC_SYSTEM_MAT: SS= aa_calc_system_mat( fwd_model, img)
-% Calc system matrix for Andy Adler's EIT code
+function s_mat= system_mat_2_5D_1st_order( fwd_model, img)
+% SYSTEM_MAT_2_5D_1ST_ORDER: 2.5D system matrix
+% SS= SYSTEM_MAT_2_5D_1ST_ORDER( FWD_MODEL, IMG)
 % fwd_model = forward model
 % img       = image background for system matrix calc
 % s_mat = CC' * SS * conductivites * CC;
 % where:
 %   SS  = Unconnected system Matrix
 %   CC  = Connectivity Matrix
+%
+% Parameters
+%   fwd_model.system_mat_2_5D_1st_order.k
 
 % (C) 2005 Andy Adler. Licenced under the GPL Version 2
 % $Id: aa_calc_system_mat.m,v 1.5 2006/03/15 21:54:20 aadler Exp $
 
-p= aa_fwd_parameters( fwd_model );
+p= fwd_model_parameters( fwd_model );
+
+try
+k= fwd_model.system_mat_2_5D_1st_order.k;
+catch
+k=0;
+end
+
 
 d= p.n_dims+1;
 e= p.n_elem;
@@ -24,7 +34,9 @@ for j=1:e
   a=  inv([ ones(d,1), p.NODE( :, p.ELEM(:,j) )' ]);
   area = 1/dfact/abs(det(a));
   idx= d*(j-1)+1 : d*j;
-  SS(idx,idx)= area*a(2:d,:)'*a(2:d,:);
+  SSe1= a(2:d,:)'*a(2:d,:); 
+  SSe2= (ones(3) + eye(3))/12;
+  SS(idx,idx)= area*(SSe1 - k^2*SSe2);
 end %for j=1:ELEMs 
 
 CC= sparse((1:d*e),p.ELEM(:),ones(d*e,1), d*e, n);
