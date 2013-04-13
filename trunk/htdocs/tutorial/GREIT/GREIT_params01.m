c688b6542cf1bf95682c43ed75b6c4a7f2bcb6aa
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2013-04-13 14:13:16 +0000

    remove warning

--- trunk/htdocs/tutorial/GREIT/GREIT_params01.m
+++ trunk/htdocs/tutorial/GREIT/GREIT_params01.m
@@ -15,7 +15,7 @@ xyzr = [Xpos; Ypos; Zpos; r*ones(1,Npos)];
 [vh,vi] = simulate_movement(imgs, xyzr);
 
 % 3D inverse model
-imdl= mk_common_model('n3r2',16);
+imdl= mk_common_model('n3r2',32);
 mdl= ng_mk_cyl_models([2,1,0.1],[8,0.8,1.2],[0.05]); 
 mdl.stimulation = mk_stim_patterns(16,1,[0,1],[0,1],{},1);
 imdl.fwd_model = mdl; 
