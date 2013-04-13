a9051a33dcaee3859bf305e5b4bff1be3d690ce6
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2013-04-13 14:14:21 +0000

    remove warning
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@3827 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/GREIT/test_params03.m
+++ htdocs/tutorial/GREIT/test_params03.m
@@ -1,5 +1,5 @@
 % 3D inverse model
-imdl= mk_common_model('n3r2',16);
+imdl= mk_common_model('n3r2',[16,2]);
 mdl= ng_mk_cyl_models([2,1,0.1],[8,0.8,1.2],[0.05]); 
 mdl.stimulation = mk_stim_patterns(16,1,[0,1],[0,1],{},1);
 imdl.fwd_model = mdl; 
