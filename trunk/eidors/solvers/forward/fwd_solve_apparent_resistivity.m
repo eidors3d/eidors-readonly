5a9a2eae2ceead513ae4e1b191e587a5d05f96d1
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2013-04-16 15:46:17 +0000

    Added docs and unit tests

--- trunk/eidors/solvers/forward/fwd_solve_apparent_resistivity.m
+++ trunk/eidors/solvers/forward/fwd_solve_apparent_resistivity.m
@@ -1,8 +1,13 @@
 function data =fwd_solve_apparent_resistivity(fwd_model, img)
+% fwd_solve_apparent_resistivity: fwd_solve output as apparent resistivity
+%  This function is a wrapper to the fwd_solve; however, the output
+%  is converted into apparent resistivity units, rather than in
+%  voltage units
 
 % $Id$
 
 % correct input paralemeters if function was called with only img
+
 if isstr(fwd_model) && strcmp(fwd_model,'UNIT_TEST'); do_unit_test; return; end
 
 if nargin == 1
@@ -49,3 +54,18 @@ if isnan(fctr)
 end
 
 
+function do_unit_test
+   imdl = mk_common_model('a2c2',8); 
+   img = mk_image(imdl);
+   vrh = fwd_solve( img );
+   img.elem_data(1) = 1.1;
+   vri = fwd_solve( img );
+
+   img = mk_image(imdl);
+   img.fwd_model.solve = @fwd_solve_apparent_resistivity;
+   vah = fwd_solve( img );
+   img.elem_data(1) = 1.1;
+   vai = fwd_solve( img );
+
+   unit_test_cmp('homog is ones', vah.meas, ones(size(vah.meas)), 1e-10);
+   unit_test_cmp('ratio is same', vai.meas./vah.meas, vri.meas./vrh.meas, 1e-10);
