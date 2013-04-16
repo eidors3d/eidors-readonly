cb872a8a8de3d39951c8db2788e16916f41bcbe3
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2013-04-16 14:53:12 +0000

    clear so that we can run in a script
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@3839 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/GREIT-algorithm/demo_algs01.m
+++ htdocs/tutorial/GREIT-algorithm/demo_algs01.m
@@ -3,6 +3,6 @@
 % exclude measures at electrodes
 [x,y]= meshgrid(1:16,1:16); idx= abs(x-y)>1 & abs(x-y)<15;
 
-load iirc_data_2006
+load iirc_data_2006; clear v
 v(1).vh= - real(v_reference(idx,1));
 v(1).vi= - real(v_rotate(idx,1));
