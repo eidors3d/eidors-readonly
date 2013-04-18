fcf11c8ab8b7236d3b3465d2540c81f8a94d0c94
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2013-04-18 17:43:28 +0000

    fix images in pig_body tutorial

--- trunk/htdocs/tutorial/netgen/extrusion/pig_body09.m
+++ trunk/htdocs/tutorial/netgen/extrusion/pig_body09.m
@@ -2,7 +2,7 @@ imdl = mk_common_model('c2c2',16);
 imdl.fwd_model.electrode = imdl.fwd_model.electrode([1,16:-1:2]);
 imdl.fwd_model = mdl_normalize(imdl.fwd_model, 1);
 imr= inv_solve(imdl, vh, vi);
-
+clf
 show_fem(imr); axis tight; axis off; axis equal
 
 print_convert pig_body09a.jpg
