51a8f9fd7fe6b54869050396c6d941463f2f3127
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2013-04-18 14:21:55 +0000

    Fix pseudosection for diople-dipole

--- trunk/eidors/graphics/matlab/show_pseudosection.m
+++ trunk/eidors/graphics/matlab/show_pseudosection.m
@@ -18,111 +18,143 @@ function fwd_model= show_pseudosection( fwd_model, data)
 
 if ischar(fwd_model) && strcmp(fwd_model,'UNIT_TEST'); do_unit_test; return; end 
 
-
-fwd_model = process_options(fwd_model);
-
-orientation = fwd_model.show_pseudosection.orientation;
-if iscell(orientation) 
-    if strcmp(orientation{2},'yz')
-    fwd_model.nodes= fwd_model.nodes(:,[2 3 1]);
-    elseif strcmp(orientation{2},'xz')
-     fwd_model.nodes= fwd_model.nodes(:,[1 3 2]);
-    end
-    orientation= orientation{1};
-end
+[fwd_model, orientation] = process_options( fwd_model );
 
 switch(upper(orientation))
-    case 'HORIZONTALDOWNWARD';  [depth,location]= plotPseudoSectionProfileDown(fwd_model,data);
-    case 'VERTICAL';            [depth,location]= plotPseudoSectionProfileVert(fwd_model,data);
-    case 'CIRCULAROUTSIDE';     [depth,location]= plotPseudoSectionCircularOut(fwd_model,data);
-    case 'CIRCULARINSIDE';      [depth,location]= plotPseudoSectionCircularIn(fwd_model,data);
-  otherwise;
-    error('No orientation of type "%s" available', upper(orientation));
+    case 'HORIZONTALDOWNWARD';
+       [depth,location]= plotPseudoSectionProfileDown(fwd_model,data);
+    case 'VERTICAL';
+       [depth,location]= plotPseudoSectionProfileVert(fwd_model,data);
+    case 'CIRCULAROUTSIDE';
+       [depth,location]= plotPseudoSectionCircularOut(fwd_model,data);
+    case 'CIRCULARINSIDE';
+       [depth,location]= plotPseudoSectionCircularIn(fwd_model,data);
+    otherwise;
+       error('No orientation of type "%s" available', upper(orientation));
 end
 colormap('jet'); colorbar;
+
 
 fwd_model.show_pseudosection.depth= depth;
 fwd_model.show_pseudosection.location= location;
 
 end
 
-function fwd_model = process_options(fwd_model)
-try
-    fwd_model.show_pseudosection.point_size;
-catch
-    if size(fwd_model.electrode,2) <= 16
-        fwd_model.show_pseudosection.point_size= 500;
-    elseif size(fwd_model.electrode,2) <= 32
-        fwd_model.show_pseudosection.point_size= 200;
-    else
-        fwd_model.show_pseudosection.point_size= 50;
-    end
+function [fwd_model, orientation] = process_options( fwd_model );
+   try 
+      fwd_model.show_pseudosection.point_size
+   catch 
+      if size(fwd_model.electrode,2) <= 16
+          fwd_model.show_pseudosection.sizepoint= 500;
+      elseif size(fwd_model.electrode,2) <= 32
+          fwd_model.show_pseudosection.sizepoint= 200;
+      else
+          fwd_model.show_pseudosection.sizepoint= 50;
+      end
+   end
+
+   try
+      fwd_model.show_pseudosection.orientation;
+   catch
+      fwd_model.show_pseudosection.orientation = 'horizontaldownward';
+   end
+
+   orientation = fwd_model.show_pseudosection.orientation;
+   if iscell(orientation) 
+       if strcmp(orientation{2},'yz')
+          fwd_model.nodes= fwd_model.nodes(:,[2 3 1]);
+       elseif strcmp(orientation{2},'xz')
+          fwd_model.nodes= fwd_model.nodes(:,[1 3 2]);
+       end
+       orientation= orientation{1};
+   end
 end
 
-try
-    fwd_model.show_pseudosection.orientation;
-catch
-    fwd_model.show_pseudosection.orientation = 'horizontaldownward';
-end
-end
 
-
-
-function [a,xps]= plotPseudoSectionProfileDown(fmdl,data)
+function [zps,xps]= plotPseudoSectionProfileDown(fmdl,data)
    fs= 20;
    
    [elec_posn,elecNumber]= electrodesPosition(fmdl);
+   xposition_elec= reshape(elec_posn(elecNumber,1),[],4);
+   xps= mean(xposition_elec,2);
    
-   xps= (elec_posn(elecNumber(:,1),1)+elec_posn(elecNumber(:,2),1))/2;
-   a= abs(elecNumber(:,1)-elecNumber(:,2));
-   de= elec_posn(1,1)-elec_posn(2,1);
+   AB= abs(xposition_elec(:,2)-xposition_elec(:,1));
+   MN= abs(xposition_elec(:,4)-xposition_elec(:,3));
+   AN= abs(xposition_elec(:,4)-xposition_elec(:,1));
+   BM= abs(xposition_elec(:,3)-xposition_elec(:,2));
+   AM= abs(xposition_elec(:,3)-xposition_elec(:,1));
+   BN= abs(xposition_elec(:,4)-xposition_elec(:,2));
    
-   % Identiy reciprocal data(elecNumber(:,1)-elecNumber(:,2)) > abs(elecNumber(:,3)-elecNumber(:,4))
-   R= find(abs(elecNumber(:,1)-elecNumber(:,2)) < abs(elecNumber(:,3)-elecNumber(:,4)));
+   a= max([AB MN AN BM AM BN ],[],2);
+   zps= -a/3;
    
-   if ~isempty(R)
-       xps(R)= (elec_posn(elecNumber(R,3),4)+elec_posn(elecNumber(R,4),4))/2;
-       a(R)= abs(elecNumber(R,3)-elecNumber(R,4));
-   end
-   zps= -abs(a*de/2);
-      
+%    a= abs(elecNumber(:,1)-elecNumber(:,2));
+%    de= elec_posn(1,1)-elec_posn(2,1);
+%    
+%    % Identiy reciprocal data(elecNumber(:,1)-elecNumber(:,2)) > abs(elecNumber(:,3)-elecNumber(:,4))
+%    R= find(abs(elecNumber(:,1)-elecNumber(:,2)) < abs(elecNumber(:,3)-elecNumber(:,4)));
+%    
+%    if ~isempty(R)
+%        xps(R)= (elec_posn(elecNumber(R,3),4)+elec_posn(elecNumber(R,4),4))/2;
+%        a(R)= abs(elecNumber(R,3)-elecNumber(R,4));
+%    end
+     
    P= zps+1i*xps;
    [Pu,iu,ju]= unique(P);
    
-   xu= xps(iu);
-   zu= zps(iu);
-   du= iu*0;
-   for i= 1:length(Pu)
-       du(i)= mean(data(ju==i));
+   if length(Pu) < length(P)
+       xu= xps(iu);
+       zu= zps(iu);
+       du= iu*0;
+       for i= 1:length(Pu)
+           du(i)= mean(data(ju==i));
+       end
+   else
+       xu= xps;
+       zu= zps;
+       du= data;
    end
-   
-   scatter(xu,zu,fmdl.show_pseudosection.point_size,(du),'filled','MarkerEdgeColor','k');
+       
+   scatter(xu,zu,fmdl.show_pseudosection.sizepoint,(du),'filled','MarkerEdgeColor','k');
    xlabel('Distance (m)','fontsize',fs,'fontname','Times');
    ylabel('Pseudo-depth (m)','fontsize',fs,'fontname','Times')
     axis equal; axis tight;
    set(gca,'fontsize',fs,'fontname','Times')
 end
 
-function [a,zps]= plotPseudoSectionProfileVert(fmdl,data)
+function [xps,zps]= plotPseudoSectionProfileVert(fmdl,data)
    fs= 20;
    
    [elec_posn,elecNumber]= electrodesPosition(fmdl);
   
-   zps= (elec_posn(elecNumber(:,1),3)+elec_posn(elecNumber(:,2),3))/2;
-   a= abs(elec_posn(elecNumber(:,1),3)-elec_posn(elecNumber(:,2),3));
-%    a= abs(elecNumber(:,1)-elecNumber(:,2));
-%    de= abs(elec_posn(1,3)-elec_posn(2,3));
+   zposition_elec= reshape(elec_posn(elecNumber,3),[],4);
+   zps= mean(zposition_elec,2);
    
-   % Identiy reciprocal data(elecNumber(:,1)-elecNumber(:,2)) > abs(elecNumber(:,3)-elecNumber(:,4))
-   R= find(abs(elecNumber(:,1)-elecNumber(:,2)) < abs(elecNumber(:,3)-elecNumber(:,4)));
+   AB= abs(zposition_elec(:,2)-zposition_elec(:,1));
+   MN= abs(zposition_elec(:,4)-zposition_elec(:,3));
+   AN= abs(zposition_elec(:,4)-zposition_elec(:,1));
+   BM= abs(zposition_elec(:,3)-zposition_elec(:,2));
+   AM= abs(zposition_elec(:,3)-zposition_elec(:,1));
+   BN= abs(zposition_elec(:,4)-zposition_elec(:,2));
    
-   if ~isempty(R)
-       zps(R)= (elec_posn(elecNumber(R,3),3)+elec_posn(elecNumber(R,4),3))/2;
-       a(R)= abs(elec_posn(elecNumber(R,3),3)-elec_posn(elecNumber(R,4),3));
-%        a(R)= abs(elecNumber(R,3)-elecNumber(R,4));
-   end
+   a= max([AB MN AN BM AM BN ],[],2);
    xps= a/3;
-%    xps= abs(a*de/2);
+   
+%    zps= (elec_posn(elecNumber(:,1),3)+elec_posn(elecNumber(:,2),3))/2;
+%    a= abs(elec_posn(elecNumber(:,1),3)-elec_posn(elecNumber(:,2),3));
+% %    a= abs(elecNumber(:,1)-elecNumber(:,2));
+% %    de= abs(elec_posn(1,3)-elec_posn(2,3));
+%    
+%    % Identiy reciprocal data(elecNumber(:,1)-elecNumber(:,2)) > abs(elecNumber(:,3)-elecNumber(:,4))
+%    R= find(abs(elecNumber(:,1)-elecNumber(:,2)) < abs(elecNumber(:,3)-elecNumber(:,4)));
+%    
+%    if ~isempty(R)
+%        zps(R)= (elec_posn(elecNumber(R,3),3)+elec_posn(elecNumber(R,4),3))/2;
+%        a(R)= abs(elec_posn(elecNumber(R,3),3)-elec_posn(elecNumber(R,4),3));
+% %        a(R)= abs(elecNumber(R,3)-elecNumber(R,4));
+%    end
+%    xps= a/3;
+% %    xps= abs(a*de/2);
       
    P= zps+1i*xps;
    [Pu,iu,ju]= unique(P);
@@ -134,7 +166,7 @@ function [a,zps]= plotPseudoSectionProfileVert(fmdl,data)
        du(i)= mean(data(ju==i));
    end
    
-   scatter(xu,zu,fmdl.show_pseudosection.point_size,(du),'filled','MarkerEdgeColor','k'); 
+   scatter(xu,zu,fmdl.show_pseudosection.sizepoint,(du),'filled','MarkerEdgeColor','k'); 
    xlabel('Pseudo distance (m)','fontsize',fs,'fontname','Times');
    if zps(1)<0
        ylabel('Depth (m)','fontsize',fs,'fontname','Times')
@@ -148,11 +180,12 @@ end
 function [r_point,th_point]= plotPseudoSectionCircularIn(fmdl,data)
 fs= 20;
 [elec_posn,elecNumber] = electrodesPosition(fmdl);
-[r_point,th_point,r] = polarPosition(elecNumber,elec_posn);
+[r_point,th_point,r,xc,yc] = polarPosition(elecNumber,elec_posn);
 
 r_point= (r-r_point/pi);%*9*pi*r/10+pi*r/10;
 
 [x_point,y_point]= pol2cart(th_point,r_point);
+x_point= x_point+xc; y_point= y_point+yc;
 
 P= x_point+1i*y_point;
 [Pu,iu,ju]= unique(P);
@@ -164,7 +197,7 @@ for i= 1:length(Pu)
     du(i)= mean(data(ju==i));
 end
    
-scatter(xu,zu,fmdl.show_pseudosection.point_size,(du),'filled','MarkerEdgeColor','k');  
+scatter(xu,zu,fmdl.show_pseudosection.sizepoint,(du),'filled','MarkerEdgeColor','k');  
 xlabel('X (m)','fontsize',fs,'fontname','Times');
 ylabel('Y (m)','fontsize',fs,'fontname','Times')
 axis equal; axis tight;
@@ -175,9 +208,10 @@ end
 function [r_point,th_point]= plotPseudoSectionCircularOut(fmdl,data)
 fs= 20;
 [elec_posn,elecNumber] = electrodesPosition(fmdl);
-[r_point,th_point,r] = polarPosition(elecNumber,elec_posn);
+[r_point,th_point,r,xc,yc] = polarPosition(elecNumber,elec_posn);
 r_point= (r+r_point);
 [x_point,y_point]= pol2cart(th_point,r_point+r);
+x_point= x_point+xc; y_point= y_point+yc;
 
 P= x_point+1i*y_point;
 [Pu,iu,ju]= unique(P);
@@ -189,7 +223,7 @@ for i= 1:length(Pu)
     du(i)= mean(data(ju==i));
 end
    
-scatter(xu,zu,fmdl.show_pseudosection.point_size,(du),'filled','MarkerEdgeColor','k');  colorbar
+scatter(xu,zu,fmdl.show_pseudosection.sizepoint,(du),'filled','MarkerEdgeColor','k');  colorbar
 xlabel('X (m)','fontsize',fs,'fontname','Times');
 ylabel('Y (m)','fontsize',fs,'fontname','Times')
 axis equal; axis tight;
@@ -211,43 +245,103 @@ function [elec_posn,elecNumber] = electrodesPosition(fmdl)
    end
    elecNumber= [A B M N];
    
-   elec_posn= zeros(length(fmdl.electrode),3);
+   elec_posn= zeros(length(fmdl.electrode),size(fmdl.nodes,2));
    for i=1:length(fmdl.electrode)
    elec_posn(i,:)= mean(fmdl.nodes(fmdl.electrode(1,i).nodes,:),1);
    end
    
 end
 
-function [r_point,th_point,r] = polarPosition(elecNumber,elec_posn)
+function [r_point,th_bary,r,xc,yc] = polarPosition(elecNumber,elec_posn)
 
 TH= elecNumber*0;
 R= elecNumber*0;
-for i= 1:4
- [TH(:,i),R(:,i)]= cart2pol(elec_posn(elecNumber(:,i),1),elec_posn(elecNumber(:,i),2));
-end
 
-th_point= (TH(:,3)+TH(:,4))/2;
-idx_discontinuity= find((TH(:,3)>pi/2 & TH(:,4)<-pi/2) | (TH(:,3)<-pi/2 & TH(:,4)>pi/2));
-th_point(idx_discontinuity)= (TH(idx_discontinuity,3)+2*pi+TH(idx_discontinuity,4))/2;
-th_point(th_point>pi)= th_point(th_point>pi)-2*pi;
-th_point(th_point<-pi)= th_point(th_point<-pi)+2*pi;
+xposition_elec= reshape(elec_posn(elecNumber,1),[],4);
+yposition_elec= reshape(elec_posn(elecNumber,2),[],4);
+rx= (max(xposition_elec(:))-min(xposition_elec(:)))/2;
+ry= (max(yposition_elec(:))-min(yposition_elec(:)))/2;
+rmean= (rx+ry)/2;
+f = inline('((x-a(1)).^2)/a(2).^2 + ((y-a(3)).^2)/a(2).^2 -1', 'a','x','y');
+af = lsqnonlin(f, [mean(xposition_elec(:)),rmean,mean(yposition_elec(:)),rmean], [], [], [], xposition_elec, yposition_elec);
+xc= af(1); yc= af(3); r = af(2);
+
+barycenter_x= mean(xposition_elec,2);
+barycenter_y= mean(yposition_elec,2);
+
+[th_bary,r_bary]= cart2pol(barycenter_x-xc,barycenter_y-yc);
+
+for i= 1:4
+    [TH(:,i),R(:,i)]= cart2pol(elec_posn(elecNumber(:,i),1)-xc,elec_posn(elecNumber(:,i),2)-yc);
+end 
+
+Arc_length_AB= abs(TH(:,2)-TH(:,1));
+% idx_discontinuity= find((TH(:,2)>=pi/2-0.07 & TH(:,1)<-pi/2+0.07));
+idx_discontinuity= find((TH(:,2)>=pi/2 & TH(:,1)<0));
+Arc_length_AB(idx_discontinuity)= 2*pi+TH(idx_discontinuity,1)-TH(idx_discontinuity,2);
+% idx_discontinuity= find((TH(:,1)>=pi/2-0.07 & TH(:,2)<-pi/2+0.07));
+idx_discontinuity= find((TH(:,1)>=pi/2 & TH(:,2)<0));
+Arc_length_AB(idx_discontinuity)= 2*pi+TH(idx_discontinuity,2)-TH(idx_discontinuity,1);
+Arc_length_AB(Arc_length_AB>=pi+0.005)= Arc_length_AB(Arc_length_AB>=pi+0.005)-pi;
+Arc_length_AB= Arc_length_AB.*(R(:,1)+R(:,2))/2;
+
+Arc_length_MN= abs(TH(:,4)-TH(:,3));
+idx_discontinuity= find((TH(:,4)>=pi/2 & TH(:,3)<-pi/2));
+Arc_length_MN(idx_discontinuity)= 2*pi+TH(idx_discontinuity,3)-TH(idx_discontinuity,4);
+idx_discontinuity= find((TH(:,3)>=pi/2 & TH(:,4)<-pi/2));
+Arc_length_MN(idx_discontinuity)= 2*pi+TH(idx_discontinuity,4)-TH(idx_discontinuity,3);
+Arc_length_MN(Arc_length_MN>=pi+0.005)= Arc_length_MN(Arc_length_MN>=pi+0.005)-pi;
+Arc_length_MN= Arc_length_MN.*(R(:,3)+R(:,4))/2;
 
 Arc_length_AM= abs(TH(:,3)-TH(:,1));
-idx_discontinuity= find((TH(:,3)>pi/2 & TH(:,1)<-pi/2));
+idx_discontinuity= find((TH(:,3)>=pi/2 & TH(:,1)<-pi/2));
 Arc_length_AM(idx_discontinuity)= 2*pi+TH(idx_discontinuity,1)-TH(idx_discontinuity,3);
-idx_discontinuity= find((TH(:,1)>pi/2 & TH(:,3)<-pi/2));
+idx_discontinuity= find((TH(:,1)>=pi/2 & TH(:,3)<-pi/2));
 Arc_length_AM(idx_discontinuity)= 2*pi+TH(idx_discontinuity,3)-TH(idx_discontinuity,1);
+Arc_length_AM(Arc_length_AM>=pi+0.005)= Arc_length_AM(Arc_length_AM>=pi+0.005)-pi;
 Arc_length_AM= Arc_length_AM.*(R(:,1)+R(:,3))/2;
 
+Arc_length_AN= abs(TH(:,4)-TH(:,1));
+idx_discontinuity= find((TH(:,4)>=pi/2 & TH(:,1)<-pi/2));
+Arc_length_AN(idx_discontinuity)= 2*pi+TH(idx_discontinuity,1)-TH(idx_discontinuity,4);
+idx_discontinuity= find((TH(:,1)>=pi/2 & TH(:,4)<-pi/2));
+Arc_length_AN(idx_discontinuity)= 2*pi+TH(idx_discontinuity,4)-TH(idx_discontinuity,1);
+Arc_length_AN(Arc_length_AN>=pi+0.005)= Arc_length_AN(Arc_length_AN>=pi+0.005)-pi;
+Arc_length_AN= Arc_length_AN.*(R(:,1)+R(:,4))/2;
+
 Arc_length_NB= abs(TH(:,4)-TH(:,2));
-idx_discontinuity= find((TH(:,4)>pi/2 & TH(:,2)<-pi/2));
+idx_discontinuity= find((TH(:,4)>=pi/2 & TH(:,2)<-pi/2));
 Arc_length_NB(idx_discontinuity)= 2*pi+TH(idx_discontinuity,2)-TH(idx_discontinuity,4);
-idx_discontinuity= find((TH(:,2)>pi/2 & TH(:,4)<-pi/2));
+idx_discontinuity= find((TH(:,2)>=pi/2 & TH(:,4)<-pi/2));
 Arc_length_NB(idx_discontinuity)= 2*pi+TH(idx_discontinuity,4)-TH(idx_discontinuity,2);
+Arc_length_NB(Arc_length_NB>=pi+0.005)= Arc_length_NB(Arc_length_NB>=pi+0.005)-pi;
 Arc_length_NB= Arc_length_NB.*(R(:,2)+R(:,4))/2;
 
-r= mean(R(:));
-r_point= (Arc_length_AM+Arc_length_NB)/2;
+Arc_length_BM= abs(TH(:,3)-TH(:,2));
+idx_discontinuity= find((TH(:,3)>=pi/2 & TH(:,2)<-pi/2));
+Arc_length_BM(idx_discontinuity)= 2*pi+TH(idx_discontinuity,2)-TH(idx_discontinuity,3);
+idx_discontinuity= find((TH(:,2)>=pi/2 & TH(:,3)<-pi/2));
+Arc_length_BM(idx_discontinuity)= 2*pi+TH(idx_discontinuity,3)-TH(idx_discontinuity,2);
+Arc_length_BM(Arc_length_BM>=pi+0.005)= Arc_length_BM(Arc_length_BM>=pi+0.005)-pi;
+Arc_length_BM= Arc_length_BM.*(R(:,2)+R(:,3))/2;
+
+% u= [[Arc_length_AB Arc_length_MN Arc_length_AM Arc_length_AN
+% Arc_length_NB Arc_length_BM]*180/pi, elecNumber(:,1)-elecNumber(:,2)];
+% u= [[Arc_length_AB Arc_length_MN Arc_length_AM Arc_length_AN Arc_length_NB Arc_length_BM]*180/pi/0.27, elecNumber(:,1)-elecNumber(:,2)];
+% THd= TH*180/pi;
+% au= [max([Arc_length_AB Arc_length_MN Arc_length_AM Arc_length_AN Arc_length_NB Arc_length_BM]*180/pi/0.27,[],2), elecNumber(:,1)-elecNumber(:,2)];
+a= max([Arc_length_AB Arc_length_MN Arc_length_AM Arc_length_AN Arc_length_NB Arc_length_BM],[],2);
+r_point= a/2;
+
+ 
+
+% th_bary= (TH(:,3)+TH(:,4))/2;
+% idx_discontinuity= find((TH(:,3)>=pi/2 & TH(:,4)<-pi/2) | (TH(:,3)<-pi/2 & TH(:,4)>=pi/2));
+% th_bary(idx_discontinuity)= (TH(idx_discontinuity,3)+2*pi+TH(idx_discontinuity,4))/2;
+% th_bary(th_point>=pi+0.005)= th_bary(th_point>=pi+0.005)-2*pi;
+% th_bary(th_point<-pi)= th_bary(th_point<-pi)+2*pi;
+% r= mean(R(:));
+% r_point= (Arc_length_AM+Arc_length_NB)/2;
 end
 
 function do_unit_test
