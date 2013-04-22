12b63206179dd2ea3d19b9e568576a4bcd9753c9
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2013-04-22 14:33:28 +0000

    remove warnings
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@3971 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/strange_effects/jacobian_absolute.m
+++ htdocs/tutorial/strange_effects/jacobian_absolute.m
@@ -2,4 +2,4 @@ function J = jacobian_absolute( fwd_model, img);
     vh = fwd_solve(img);
     flip = sign(vh.meas);
     flip = spdiags(flip, 0, length(flip), length(flip));
-    J = flip*jacobian_adjoint(fwd_model,img);
+    J = flip*jacobian_adjoint(img);
