288955bb5736a5da8f171bb51bce5991a74a4420
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2013-06-03 13:37:37 +0000

    add traingle measures

--- trunk/dev/b_grychtol/calc_mesh_quality.m
+++ trunk/dev/b_grychtol/calc_mesh_quality.m
@@ -2,39 +2,56 @@ function [Q mdl] = calc_mesh_quality(mdl)
 %CALC_MESH_QUALITY Various measures of mesh quality.
 %  [Q MDL] = CALC_MESH_QUALITY(MDL) calculates several measures of mesh 
 %  quality for the EIDORS fwd_model struct MDL and returns them in Q:
-%     Q.NSR    - inradius to circumradius ratio (normalized shape ratio)
+%   Q.tet.
+%     NSR    - inradius to circumradius ratio (normalized shape ratio)
 %                 n*r / R
 %                where r = inradius; R = circumradius; n = dimensions [3,4]
-%     Q.mu     - inradius to longest edge ratio (L)                     [5]
+%     mu     - inradius to longest edge ratio (L)                     [5]
 %                 r / L
-%     Q.tau    - shortest to longest edge ratio                         [5]
+%     tau    - shortest to longest edge ratio                         [5]
 %                 l/L
-%     Q.reg    - "regularity" of tetrahedron                      [6,p.151]
+%     reg    - "regularity" of tetrahedron                      [6,p.151]
 %                 4r/H
 %                where H is the longest altitude
-%     Q.zeta   - measure relating volume and area                       [7]
+%     zeta   - measure relating volume and area                       [7]
 %                 V^4 / ( SUM( Ai^2 ) ^3 )
 %                where V = volume; Ai = area of face i
-%     Q.eta    - measure realting volume and edge length                [7]
+%     eta    - measure realting volume and edge length                [7]
 %                 V^(2/3) / SUM( li^2 )
 %                where li = length of edge i
-%     Q.alpha  - ratio of volume to mean edge length                    [6]
+%     alpha  - ratio of volume to mean edge length                    [6]
 %                 V / (1/6 SUM( Ai ))
 %                where Ai = area of face i
-%     Q.gamma  - ratio of volume to RMS edge length                     [8]
+%     gamma  - ratio of volume to RMS edge length                     [8]
 %                 V / SQRT( 1/6 SUM( Ai^2 ) )
+%     min_angle - minimum dihedral angle
 %
-%  All the above measures are scaled to be bounded between 0 (worst) and 
-%  1 (best), where a regular tetrahedron scores 1, and non-dimensional.
+%  Q.tri.
+%     NSR   - inradius to circumradius ratio (normalized shape ratio)
+%                 n*r / R
+%                where r = inradius; R = circumradius; n = dimensions [3,4]
+%     mu    - inradius to longest edge ratio (L)                        [9]
+%                 r / L       = tri_mu(mdl);
+%     eta   - measure relating triangle area and edge length        [10,11]
+%                 A / SUM( li^2) 
+%     theta - measure relating shortest altitude and edge length        [2]
+%                 h / SQRT( SUM ( li^2 ) )
+%     iota  - measure relating triangle area and edge length           [12]
+%                 A / SUM( li ) ^2
+%     kappa - ratio of shortest altitude to longest edge             [2,13]
+%                 h / L
+%     min_angle - minimum angle
+%
+%  All the above measures are non-dimensional and scaled to be bounded 
+%  between 0 (worst) and 1 (best), where a regular tetrahedron/triangle
+%  scores 1.
 %
 %  Measures where implemented based on review articles of V. Phathasarahy
 %  (1994) [1] and D. Field (2000) [2]. The references above indicate the
 %  original sources, although those were not consulted.
 %
 %  Additionally, CALC_MESH_QUALITY returns the original MDL with several
-%  useful fields added, which can also be used to assess quality:
-%    MDL.dihedral_angles (NE x 6) 
-%    MDL.solid_angles    (NE x 4)
+%  useful fields added.
 %
 %  REFERENCES:
 %  [1] Parthasarathy V (1994) "A comparison of tetrahedron quality measures" 
@@ -57,10 +74,25 @@ function [Q mdl] = calc_mesh_quality(mdl)
 %  [8] Parthasarathy V, Kodiyalam (1991) "A constrained optimization
 %      approach to finite element mesh smoothing" Finite Elem Anal Des
 %      9:309-20
+%  [9] Perronnet A (1992) "Triangulation par arbre-4 de triangles equilateraux 
+%      at maximisation de la qualite" Publication du Laboratoire d’Analyse
+%      Numerique, Universite Pierr it Marie Curie et Centre National de la
+%      Recherche Scientifique
+% [10] Bhatia RP, Lawrence KL (1990) "Two-dimensional finite element mesh 
+%      generation based on stripwise automatic triangulation" Comput Struct
+%      36:309–319.
+% [11] Bank RE, Xu J (1996) "An algorithm for coarsening unstructured 
+%      meshes" Numer Math 73:1–36
+% [12] Watabayshi GY, Galt JA (1986) "An optimized triangular mesh system 
+%      from random points" in "Numerical Grid Generation in Computational 
+%      Fluid Dynamics",Hauser J, Taylor C. (eds). Pineridge Press: Swansea,
+%      1986; 437–438
+% [13] Suhara J, Fukuda J (1972) "Automatic mesh generation for finite 
+%      element analysis" on "Advances in Computational Methods in 
+%      Structural Mechanics and Design" Oden JT, Clough RW, Yamamoto Y (eds).
+%      University of Alabamal Press: Tuscaloosa, AL, 1972; 607–624
 %
 % See also FIX_MODEL
-
-% TODO: Add surface mesh measures
 
 % (C) 2013 Bartlomiej Grychtol. License: GPL v2 or v3.
 % $Id$
@@ -71,19 +103,21 @@ if ischar(mdl) && strcmp(mdl, 'UNIT_TEST'), do_unit_test; return; end
 opt.elem2edge = 1;
 opt.face2elem = 1;
 opt.boundary  = 1;
+opt.boundary_face = 1;
 opt.normals   = 1;
 opt.face_centre = 1;
 opt.inner_normal = 1;
-
 mdl = fix_model(mdl, opt);
+
+% tet properties
 mdl.edge_length = edge_length(mdl);
 mdl.max_edge_length = max(mdl.edge_length(mdl.elem2edge),[],2);
 mdl.min_edge_length = min(mdl.edge_length(mdl.elem2edge),[],2);
 mdl.elem_vol    = elem_volume(mdl);
 mdl.face_area   = face_area(mdl);
 mdl.elem_area   = elem_area(mdl);
-mdl.rad_inscr   = inscribed_radius(mdl);
-mdl.rad_circ    = circumscribed_radius(mdl);
+mdl.rad_insphere      = insphere_radius(mdl);
+mdl.rad_circumsphere  = circumsphere_radius(mdl);
 mdl.tet_alt     = tet_altitudes(mdl);
 % Note: The measure based on the minimum solid angle (sin(1/2 min angle)
 % is bound between 0 and 1, but both 0 and 1 are degenerate, so measure is
@@ -91,21 +125,59 @@ mdl.tet_alt     = tet_altitudes(mdl);
 mdl.solid_angle    = solid_angles(mdl);
 mdl.dihedral_angle = dihedral_angles(mdl);
 
-% Quality measures
-Q.NSR          = normalized_shape_ratio(mdl);
+% tri properties
+mdl.face2edge        = calc_face2edge(mdl);
+mdl.rad_incircle     = incircle_radius(mdl);
+mdl.rad_circumcircle = circumcircle_radius(mdl);
+mdl.tri_alt          = tri_altitudes(mdl);
+mdl.tri_angle        = tri_angles(mdl);
+
+% TRI Quality measures
+Q.tri.NSR          = tri_normalized_shape_ratio(mdl);
+Q.tri.mu           = tri_mu(mdl);
+Q.tri.eta          = tri_eta(mdl);
+Q.tri.theta        = tri_theta(mdl);
+Q.tri.iota         = tri_iota(mdl);
+Q.tri.kappa        = tri_kappa(mdl);
+Q.tri.min_angle    = tri_min_angle(mdl);
+
+% only report measures for boundary triangles
+f = fields(Q.tri);
+for i = 1:length(f)
+   Q.tri.(f{i}) = Q.tri.(f{i})(mdl.boundary_face);
+end
+
+
+
+% TET Quality measures
+Q.tet.NSR          = tet_normalized_shape_ratio(mdl);
 % this measure is not very sensitive and not bound between 0 and 1
 % Q.omega        = omega(mdl);
-Q.mu           = mu(mdl);
-Q.tau          = tau(mdl);
-Q.reg          = R_measure(mdl);
+Q.tet.mu           = mu(mdl);
+Q.tet.tau          = tau(mdl);
+Q.tet.reg          = R_measure(mdl);
 % These two measures from Field's paper are not scale invariant
 % Q.R_star       = R_star(mdl);
 % Q.S            = S_measure(mdl);
-Q.zeta         = zeta(mdl);
-Q.eta          = eta(mdl);
-Q.alpha        = alpha(mdl);
-Q.gamma        = gamma(mdl);
+Q.tet.zeta         = zeta(mdl);
+Q.tet.eta          = eta(mdl);
+Q.tet.alpha        = alpha(mdl);
+Q.tet.gamma        = gamma(mdl);
+Q.tet.min_angle    = tet_min_angle(mdl);
 
+function a = tet_min_angle(mdl)
+a = min(mdl.dihedral_angle,[],2) / acos(1/3);
+
+
+function a = tri_min_angle(mdl)
+a = 3*min(mdl.tri_angle,[],2)/pi;
+
+function k = tri_kappa(mdl)
+k = 2/sqrt(3) * min(mdl.tri_alt,[],2) ...
+               ./ max(mdl.edge_length(mdl.face2edge),[],2);
+
+function i = tri_iota(mdl)
+i = 12*sqrt(3)*mdl.face_area ./ sum(mdl.edge_length(mdl.face2edge),2).^2;
 
 function G = gamma(mdl)
 G = 6 * sqrt(2) * mdl.elem_vol ./ sqrt(mean(mdl.edge_length(mdl.elem2edge).^2,2)).^3 ;
@@ -115,6 +187,9 @@ A = 6 * sqrt(2) * mdl.elem_vol ./ mean(mdl.edge_length(mdl.elem2edge),2).^3 ;
 
 function E = eta(mdl)
 E = 12 * (3*mdl.elem_vol).^(2/3) ./ sum(mdl.edge_length(mdl.elem2edge).^2,2);
+
+function E = tri_eta(mdl)
+E = 4 * sqrt(3) * mdl.face_area ./ sum(mdl.edge_length(mdl.face2edge).^2,2);
 
 function Z = zeta(mdl)
 Z = 3^7 * mdl.elem_vol.^4 ./ ...
@@ -127,29 +202,28 @@ function R = R_star(mdl)
 R = sqrt(2)*mdl.elem_vol ./ sum(mdl.edge_length(mdl.elem2edge),2);
 
 function R = R_measure(mdl)
-R = 4*mdl.rad_inscr ./ max(mdl.tet_alt,[],2);
+R = 4*mdl.rad_insphere ./ max(mdl.tet_alt,[],2);
 
-function H = tet_altitudes(mdl)
-elem_sorted = sort(mdl.elems,2);
-v = [4 3 2 1]; % which vertex is not on the face
-for i = 1:4 % loop over vertices
-      Nf = mdl.normals(mdl.elem2face(:,i),:);
-      Cf = mdl.face_centre(mdl.elem2face(:,i),:);
-      Pe = mdl.nodes(elem_sorted(:,v(i)), :);
-      H(:,i) = abs(my_dot(Nf, Pe - Cf));
-end
 function t = tau(mdl)
 t = mdl.min_edge_length ./ mdl.max_edge_length;
 
-
 function m = mu(mdl)
-m = 2*sqrt(6)*mdl.rad_inscr./mdl.max_edge_length;
+m = 2*sqrt(6)*mdl.rad_insphere./mdl.max_edge_length;
+
+function m = tri_mu(mdl)
+m = 2*sqrt(3)*mdl.rad_incircle./ max(mdl.edge_length(mdl.face2edge),[],2);
+
+function t = tri_theta(mdl)
+t = 2*min(mdl.tri_alt,[],2) ./ sqrt(sum(mdl.edge_length(mdl.face2edge).^2,2));
 
 function O = omega(mdl)
-O = sqrt(3)*mdl.max_edge_length ./ (2*sqrt(2)*mdl.rad_circ);
+O = sqrt(3)*mdl.max_edge_length ./ (2*sqrt(2)*mdl.rad_circumsphere);
 
-function NSR = normalized_shape_ratio(mdl)
-NSR = 3 * (mdl.rad_inscr ./ mdl.rad_circ);
+function NSR = tet_normalized_shape_ratio(mdl)
+NSR = 3 * (mdl.rad_insphere ./ mdl.rad_circumsphere);
+
+function NSR = tri_normalized_shape_ratio(mdl)
+NSR = 2*mdl.rad_incircle./mdl.rad_circumcircle;
 
 function A = dihedral_angles(mdl)
 v = nchoosek(1:4,2); % choose 2 faces
@@ -159,7 +233,7 @@ for i = 1:6
       N2 = mdl.normals(mdl.elem2face(:,v(i,2)), :) ...
             .* repmat(sign(mdl.inner_normal(:,v(i,2))-0.5),1,3);
       C  = my_cross(N1, N2);
-      D  = my_dot  (N1, N2);
+      D  = -my_dot  (N1, N2);
       A(:,i) = atan2( sqrt(sum(C.^2,2)), D);
 end
 
@@ -182,8 +256,18 @@ idx = A<0;
 A(idx) = A(idx) + pi;
 A = 2*A;
 
+function t = tri_angles(mdl)
+v = 1:3;
+L = mdl.edge_length(mdl.face2edge);
+L2 = L.^2;
+for i = 1:3
+   t(:,i) = acos( (L2(:,v(1)) + L2(:,v(2)) - L2(:,v(3)) ) ...
+                    ./(2 .* L(:,v(1)) .* L(:,v(2)) ) );
+   v = circshift(v,[1 2]);
+end
 
-function R = circumscribed_radius(mdl)
+
+function R = circumsphere_radius(mdl)
 ne = size(mdl.elems,1);
 E = mdl.elems';
 idx = 1:numel(E); idx(1:4:end) = [];
@@ -203,8 +287,41 @@ R = 0.5 * sqrt(sum(V.^2,2));
 C = mdl.nodes(E(1:4:end),:) + 0.5*V; % center
 
 
-function R = inscribed_radius(mdl)
+function H = tet_altitudes(mdl)
+elem_sorted = sort(mdl.elems,2);
+v = [4 3 2 1]; % which vertex is not on the face
+for i = 1:4 % loop over vertices
+      Nf = mdl.normals(mdl.elem2face(:,i),:);
+      Cf = mdl.face_centre(mdl.elem2face(:,i),:);
+      Pe = mdl.nodes(elem_sorted(:,v(i)), :);
+      H(:,i) = abs(my_dot(Nf, Pe - Cf));
+end
+
+function H = tri_altitudes(mdl)
+s = sum(mdl.edge_length(mdl.face2edge),2) / 2;
+S = repmat(s,1,3);
+nmrtr = 2*sqrt(s .* prod(S - mdl.edge_length(mdl.face2edge),2));
+H = repmat(nmrtr,1,3) ./ mdl.edge_length(mdl.face2edge) ;
+
+
+
+function R = circumcircle_radius(mdl)
+R = 0.25*prod(mdl.edge_length(mdl.face2edge),2) ./ mdl.face_area; 
+
+function R = incircle_radius(mdl)
+R = 2*mdl.face_area ./ sum(mdl.edge_length(mdl.face2edge),2);
+
+function R = insphere_radius(mdl)
 R = 3* mdl.elem_vol ./ mdl.elem_area;
+
+function f2e = calc_face2edge(mdl)
+%faces and edges are both column-wise sorted
+nf = length(mdl.faces);
+list(1:3:3*nf,:) = mdl.faces(:,1:2);
+list(2:3:3*nf,:) = mdl.faces(:,[1 3]);
+list(3:3:3*nf,:) = mdl.faces(:,2:3);
+[jnk f2e] = ismember(list, mdl.edges, 'rows');
+f2e = reshape(f2e,3,[])';
 
 function A = elem_area(mdl)
 A = sum(mdl.face_area(mdl.elem2face),2);
@@ -214,7 +331,7 @@ F = mdl.faces';
 A = mdl.nodes(mdl.faces(:,2),:) - mdl.nodes(mdl.faces(:,1),:);
 B = mdl.nodes(mdl.faces(:,3),:) - mdl.nodes(mdl.faces(:,1),:);
 A = sqrt(sum(my_cross(A,B).^2,2))/2;
-A = A';
+A = A;
 
 function V = elem_volume(mdl)
 E = mdl.elems';
@@ -265,14 +382,28 @@ end
 real = mk_library_model('pig_23kg_16el');
 [Q real] = calc_mesh_quality(real);
 
-f = fields(Q);
+figure(1); clf; set(1,'Name','Tet quality');
+f = fields(Q.tet);
 for i = 1:length(f)
    subplot(3,3,i)
-   hist(Q.(f{i}),100);
+   hist(Q.tet.(f{i}),100);
    xlabel(f{i});
+   axis tight
    xlim([0 1]);
 end
-subplot(3,3,9)
-hist(180*real.dihedral_angle(:)/pi,100)
-xlabel('dihedral angle');
-xlim([0 180])
+% subplot(3,3,9)
+% hist(180*real.dihedral_angle(:)/pi,100)
+% xlabel('dihedral angle');
+% xlim([0 180])
+
+figure(2); clf; set(2,'Name','Surface triangle quality');
+f = fields(Q.tri);
+for i = 1:length(f)
+   subplot(3,3,i)
+   hist(Q.tri.(f{i}),100);
+   xlabel(f{i});
+   axis tight
+   xlim([0 1]);
+end
+
+
