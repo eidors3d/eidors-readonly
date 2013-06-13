75c718e3c28731714b25ff5bddc258d2df54dc18
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2013-06-13 20:45:55 +0000

    speedup to merge_mesh
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@4202 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/models/merge_meshes.m
+++ eidors/models/merge_meshes.m
@@ -22,14 +22,19 @@ else
    th = varargin{end};
    shapes = varargin(1:end-1);
 end
-% Use a for loop, vectorised approach can run out of memory
-l0 = length(M1.nodes);
+if ~isfield(M1, 'boundary');
+   M1.boundary = find_boundary(M1);
+end
 if ~isfield(M1, 'mat_idx')
    M1.mat_idx = {1:length(M1.elems)};
 end
+% Use a for loop, vectorised approach can run out of memory
 for i = 1:length(shapes)
    l1 = length(M1.nodes);
    M2 = shapes{i};
+   if ~isfield(M2, 'boundary');
+      M2.boundary = find_boundary(M2);
+   end
    % make sure boundaries are the same dimension
    if size(M1.boundary,2) == 2 && size(M2.boundary,2)==3
       % M1 is 2D, M2 is 3D
@@ -37,15 +42,29 @@ for i = 1:length(shapes)
    elseif size(M1.boundary,2) == 3 && size(M2.boundary,2)==2
       M2.boundary = M2.elems;
    end
+   
+   useM1 = nodes_in_bounding_box(M2,M1,th);
+   useM1 = find(useM1);
+   useM2 = nodes_in_bounding_box(M1,M2,th);
+   len_useM1 = length(useM1);
+   
+   if isempty(useM1) || all(~useM2(:))
+      M1 = naive_join(M1,M2);
+      continue
+   end
+   
    nodes_to_add = [];
    n_new_nodes = 0;
    match = 0 * (1:length(M2.nodes)); 
-   for n = 1:length(M2.nodes)
-      D = M1.nodes(1:l0,:) - repmat(M2.nodes(n,:),l0,1);
-      D = sqrt(sum(D.^2,2));
-      [val p] = min(D);
-      if val < th
-         match(n) = p;
+   for n = 1:length(M2.nodes);
+      val = Inf;
+      if useM2(n)
+         D = M1.nodes(useM1,:) - repmat(M2.nodes(n,:),len_useM1,1);
+         D = sum(D.^2,2); % sqrt(sum(D.^2,2)); % don't need to sqrt
+         [val p] = min(D);
+      end
+      if val < th^2    % but then need th^2
+         match(n) = useM1(p); % matching node of M1
       else
          match(n) = l1 + n_new_nodes+1;
          n_new_nodes = n_new_nodes + 1;
@@ -61,6 +80,25 @@ end
 
 out =  M1;
 % rmfield(M1,'boundary');
+
+function M1 = naive_join(M1,M2)
+LN = length(M1.nodes);
+LE = length(M1.elems);
+M1.nodes = [M1.nodes; M2.nodes];
+M1.elems = [M1.elems; LN+M2.elems];
+M1.mat_idx = [M1.mat_idx {LE+(1:length(M2.elems))}];
+M1.boundary = [M1.boundary; LN+M2.boundary];
+   
+
+function use = nodes_in_bounding_box(M2,M1,th)
+   % limit to nodes in M1 that are within the bounding box of M2
+   maxM2 = max(M2.nodes)+th;
+   minM2 = min(M2.nodes)-th;
+   use = true(length(M1.nodes),1);
+   for i = 1:size(M1.nodes,2);
+      use = use & M1.nodes(:,i) < maxM2(i) & M1.nodes(:,i) > minM2(i);
+   end
+
 
 function run_unit_test
 subplot(221)
