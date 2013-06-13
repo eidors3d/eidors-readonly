0030fb0a3605d3472c33756b2134e85ab9bd23f7
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2013-06-13 16:56:02 +0000

    Matlab Parser for OEIT, with some attempt to create default fields

--- trunk/dev/open_eit/converter/matlab_xml_test.m
+++ trunk/dev/open_eit/converter/matlab_xml_test.m
@@ -1,5 +1,73 @@
-function s= matlab_xml_test( filename)
+function obj= matlab_xml_test( filename)
    if nargin==0; filename = 'sample.xml'; end
+   try
+      tree = xmlread(filename);
+   catch
+      error('Failed to read XML file %s.',filename);
+   end
+
+   obj = print_recurse( tree, 0);
+
+function obj = print_recurse( node, level);
+   if level>5; obj.name ='empty'; return;end
+   
+   obj = struct();
+
+   [name, atts,  kids] = non_text_nodes( node);
+   obj.name = name;
+   % deftault fields
+   obj.comment = '';
+   switch name % this is a hack
+      case 'electrode'; obj.contact_impedance = '???';
+   end
+
+   for i=1:level; fprintf('>'); end
+   fprintf('[ %s: ',name);
+   for i=1:length(atts);
+      fprintf('%s="%s" ',atts(i).Name, atts(i).Value);
+      obj.(atts(i).Name) = atts(i).Value;
+   end
+   fprintf(']\n');
+   for i=1:length(kids);
+     obj_i = print_recurse( kids{i}, level+1);
+     on = obj_i.name;
+     if strcmp(on, 'empty'); continue; end
+     if strcmp(on, 'user_data'); continue; end
+
+     try   ll = length(obj.( on ) );
+     catch ll = 0;
+     end
+     obj.( obj_i.name )( ll+1 ) = orderfields( obj_i);
+   end
+
+   switch name % this is a hack
+      case 'acquisition';
+         if ~isfield( obj,'stim_list');
+            obj.stim_list.stim.elec.id = '??';
+            obj.stim_list.stim.elec.gain = '??';
+         end
+   end
+   obj = orderfields( obj);
+
+
+function [name, atts,  kids] = non_text_nodes( node)
+
+   name = char( node.getNodeName );
+   atts = parseAttributes(node);
+
+   cnodes =  node.getChildNodes;
+   k = 1; kids = {};
+   for i=1:cnodes.getLength;
+      cnode_i = cnodes.getChildNodes.item(i-1);
+      if strcmp(cnode_i.getNodeName, '#text'); continue; end 
+      kids{k} = cnode_i;
+      k=k+1;
+   end
+ 
+
+
+
+function old_stuff
    s = parseXML( filename );
 
 s.Children(2).Children(4).Children(2).Attributes(1)
