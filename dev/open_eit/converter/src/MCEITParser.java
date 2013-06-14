28616a8f2c62b81919a0818707ace343f7b9e2de
f3te <f3te@82006732-fb7e-4a22-9f28-86a212cce563>
2013-06-14 20:48:26 +0000

    updated to changes made on sample.xml and new schema changes
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@4205 82006732-fb7e-4a22-9f28-86a212cce563

--- dev/open_eit/converter/src/MCEITParser.java
+++ dev/open_eit/converter/src/MCEITParser.java
@@ -1,7 +1,9 @@
 import java.io.BufferedInputStream;
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
@@ -10,8 +12,8 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
-/*
- * MCEITParser 
+/**
+ * MCEITParser
  * Chengbo He June 2013
  * Carleton University
  */
@@ -54,13 +56,13 @@ public class MCEITParser implements OeitLegacyParser {
         bin.close();
         fin.close();
         
-        int[] data = new int[256*(length/256)];
+        int[] data = new int[length];
         
         /*
          *  Rearranges data and reshapes to a 1D array
          */
         int a = 0, b = 0;
-        for (int i = 0; i< 256*(length/256)-1; i++){
+        for (int i = 0; i< length-1; i++){
     		if ((i+3)%16==0 && a<16*(length/256)){
     			data[i]= input[208+a/(length/256)][a%(length/256)];
     			data[i+1] = input[224+a/(length/256)][a%(length/256)];
@@ -77,6 +79,7 @@ public class MCEITParser implements OeitLegacyParser {
          * Read in XML file here, using DOM and prints data
          */
 		showData(data);
+		writeToOeit(data,length,fileName);
 		
         //input = untwist(input);
         mceitInfo.setLength(length);
@@ -95,9 +98,10 @@ public class MCEITParser implements OeitLegacyParser {
 	
 		//Get NodeLists here
 		Document doc = XMLReader.returnDOM("sample.xml"); // Returns DOM
-		NodeList measTasks = doc.getElementsByTagName("meas_task");
+		NodeList measTasks = doc.getElementsByTagName("acquisition");
 		NodeList measTypeList = doc.getElementsByTagName("meas_type");
 		NodeList electrodeList = doc.getElementsByTagName("electrode");
+		NodeList stimTypeList = doc.getElementsByTagName("stim_type");
 		
 		String stimType = null;
 		String measType = null;
@@ -115,12 +119,11 @@ public class MCEITParser implements OeitLegacyParser {
 			/*
 			 * --STIM--
 			 */
-			NodeList stimTypeList = doc.getElementsByTagName("stim_type");
 			Element stimE = (Element)stim.item(0);
 			stimType = stimE.getAttribute("type");
-			NodeList elect0 = stimE.getElementsByTagName("e");
-			String firstOrdinal0 = ((Element)elect0.item(0)).getAttribute("ordinal");
-			String secondOrdinal0 = ((Element)elect0.item(1)).getAttribute("ordinal");
+			NodeList elect0 = stimE.getElementsByTagName("elec");
+			String firstOrdinal0 = ((Element)elect0.item(0)).getAttribute("id");
+			String secondOrdinal0 = ((Element)elect0.item(1)).getAttribute("id");
 			String firstGain0 = ((Element)elect0.item(0)).getAttribute("gain");
 			String secondGain0 = ((Element)elect0.item(1)).getAttribute("gain");
 			
@@ -132,12 +135,12 @@ public class MCEITParser implements OeitLegacyParser {
 
 			for (int k = 0; k<electrodeList.getLength(); k++){
 				Element electrode = (Element)electrodeList.item(k);
-				if (firstOrdinal0.equals(electrode.getAttribute("ordinal"))){
+				if (firstOrdinal0.equals(electrode.getAttribute("id"))){
 					name01 = electrode.getAttribute("name");
 					type01 = electrode.getAttribute("type");
 					position01 = electrode.getAttribute("position");
 				}
-				if (secondOrdinal0.equals(electrode.getAttribute("ordinal"))){
+				if (secondOrdinal0.equals(electrode.getAttribute("id"))){
 					name02 = electrode.getAttribute("name");
 					type02 = electrode.getAttribute("type");
 					position02 = electrode.getAttribute("position");
@@ -164,9 +167,9 @@ public class MCEITParser implements OeitLegacyParser {
 			 */
 			Element measE = (Element) meas.item(0);
 			measType = measE.getAttribute("type");
-			NodeList elect = measE.getElementsByTagName("e");
-			String firstOrdinal = ((Element)elect.item(0)).getAttribute("ordinal");
-			String secondOrdinal = ((Element)elect.item(1)).getAttribute("ordinal");
+			NodeList elect = measE.getElementsByTagName("elec");
+			String firstOrdinal = ((Element)elect.item(0)).getAttribute("id");
+			String secondOrdinal = ((Element)elect.item(1)).getAttribute("id");
 			String firstGain = ((Element)elect.item(0)).getAttribute("gain");
 			String secondGain = ((Element)elect.item(1)).getAttribute("gain");
 			
@@ -178,12 +181,12 @@ public class MCEITParser implements OeitLegacyParser {
 
 			for (int k = 0; k<electrodeList.getLength(); k++){
 				Element electrode = (Element)electrodeList.item(k);
-				if (firstOrdinal.equals(electrode.getAttribute("ordinal"))){
+				if (firstOrdinal.equals(electrode.getAttribute("id"))){
 					name1 = electrode.getAttribute("name");
 					type1 = electrode.getAttribute("type");
 					position1 = electrode.getAttribute("position");
 				}
-				if (secondOrdinal.equals(electrode.getAttribute("ordinal"))){
+				if (secondOrdinal.equals(electrode.getAttribute("id"))){
 					name2 = electrode.getAttribute("name");
 					type2 = electrode.getAttribute("type");
 					position2 = electrode.getAttribute("position");
@@ -191,7 +194,7 @@ public class MCEITParser implements OeitLegacyParser {
 			}
 			
 			String demod_freq = null, physical_property2 = null, 
-					offset_gain = null, signal_gain = null;
+					meas_gain = null;
 			/*
 			 * GET MEAS TYPES INFO
 			 */
@@ -200,9 +203,20 @@ public class MCEITParser implements OeitLegacyParser {
 				if (measType.equals(measurement.getAttribute("name"))){
 					demod_freq = measurement.getAttribute("demod_frequency");
 					physical_property2 = measurement.getAttribute("physical_property");
-					offset_gain = measurement.getAttribute("offset_gain");
-					signal_gain = measurement.getAttribute("signal_gain");
+					meas_gain = measurement.getAttribute("gain");
 				}
+			}
+			
+			/*
+			 * GET FIELD INFO
+			 */
+			NodeList fieldList = ((Element)measTypeList.item(i)).getElementsByTagName("field");
+			String[] fieldName = new String[fieldList.getLength()];
+			String[] fieldType = new String[fieldList.getLength()];
+			
+			for (int k = 0; k<fieldList.getLength(); k++){
+				fieldName[k] = ((Element)fieldList.item(k)).getAttribute("name");
+				fieldType[k] = ((Element)fieldList.item(k)).getAttribute("type");
 			}
 			
 			/*
@@ -212,42 +226,55 @@ public class MCEITParser implements OeitLegacyParser {
 					+ "Start:" + start
 					+ " Stop:" + stop
 					+" <<<STIMULATION>>> "
-					+ "Ordinals:" + firstOrdinal0 + "("
+					+ "Ordinals:" + firstOrdinal0 + "{"
 					+ "name:" + name01
 					+ " gain:" + firstGain0
 					+ " type:" + type01
-					+ " position:" + position01 + ")"
-					+ " " + secondOrdinal0 + "("
+					+ " position:" + position01 + "}"
+					+ " " + secondOrdinal0 + "{"
 					+ "name:" + name02
 					+ " gain:" + secondGain0
 					+ " type:" + type02
-					+ " position:" + position02 + ")"
-					+ " type:" + stimType + "("
+					+ " position:" + position02 + "}"
+					+ " type:" + stimType + "{"
 					+ "amplitude:" + amplitude
 					+ " frequency:" + frequency
 					+ " physical_property:" + physical_property
-					+ " wave:" + wave + ")"
+					+ " wave:" + wave + "}"
 					+ " <<<MEASUREMENT>>> " 
-					+ "Ordinals:" + firstOrdinal + "("
+					+ "Ordinals:" + firstOrdinal + "{"
 					+ "name:" + name1
 					+ " gain:" + firstGain
 					+ " type:" + type1
-					+ " position:" + position1 + ")"
-					+ " " + secondOrdinal + "("
+					+ " position:" + position1 + "}"
+					+ " " + secondOrdinal + "{"
 					+ "name:" + name2
 					+ " gain:" + secondGain
 					+ " type:" + type2
-					+ " position:" + position2 + ")"
-					+ " type:" + measType  + "("
+					+ " position:" + position2 + "}"
+					+ " type:" + measType  + "{"
 					+ "demod_freq:" + demod_freq
 					+ " physical_property:" + physical_property2
-					+ " offset_gain:" + offset_gain
-					+ " signal_gain:" + signal_gain + ")"
+					+ " measurement_type_gain:" + meas_gain + "}"
+					+ " <<<FIELDS>>> "
+					+ "name:" + fieldName[0]
+					+ " type:" + fieldType[0]
 					+ " <<<VALUE>>> "
 					+ "raw:0x" + Integer.toHexString(0x1000000 | data[i]).substring(1)
 					+ " interpreted:" + data[i]);
 		}
 		//return data;
+	}
+	public void writeToOeit(int data[], int length, String fileName) throws IOException{
+		String oeitFileName = fileName.substring(0,fileName.length()-4);	
+		
+		DataOutputStream os = new DataOutputStream(new FileOutputStream(oeitFileName + ".oeit"));
+			
+			for (int i = 0; i < length; i++){
+				os.writeInt(data[i]);
+			}
+			
+			os.close();
 	}
 	
 	public double[][] transfer104To208(double[][] array104){ //Unnecessary right now
