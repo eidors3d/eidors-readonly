384825795cdbfddfef8505d7302fd64f5e3ddd73
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2013-06-21 09:24:24 +0000

    forward solver for movement as multi-physics
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@4248 82006732-fb7e-4a22-9f28-86a212cce563

---
+++ dev/physics/fwd_solve_elec_move.m
@@ -0,0 +1,38 @@
+function data = fwd_solve_elec_move(jnk, img)
+
+img = convert_legacy_movement_img(img);
+
+if isfield(img, 'movement')
+   img = apply_movement(img);
+   img = rmfield(img,'movement');
+end
+try
+   solver = img.fwd_model.fwd_solve_elec_move.data_solver;
+catch
+   solver = 'eidors_default';
+end
+img.fwd_model.solve = solver;
+data = fwd_solve(img);
+
+function img = apply_movement(img)
+
+n_elec  = length(img.fwd_model.electrode);
+n_nodes = length(img.fwd_model.nodes);
+n_dim   = mdl_dim(img.fwd_model);
+
+try 
+   node_move = img.movement.node_data;
+catch
+   node_move = zeros(n_nodes, n_dim );
+end
+
+if isfield(img.movement, 'electrode_data');
+   move = reshape( img.movement.electrode_data , n_elec, n_dim );
+   for i = 1:n_elec
+      enodes = img.fwd_model.electrode(i).nodes;
+      lenodes = length(enodes);
+      node_move(enodes,:) = node_move(enodes,:) + repmat(move(i,:),lenodes,1);
+   end
+end
+
+img.fwd_model.nodes = img.fwd_model.nodes + node_move;
