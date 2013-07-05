9281c99b790e0e6edaf07ec1d02e86a35a6abdcd
michaelcrabb30 <michaelcrabb30@82006732-fb7e-4a22-9f28-86a212cce563>
2013-07-05 14:52:35 +0000

    Change call_netgen.m back to original
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@4285 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/meshing/netgen/call_netgen.m
+++ eidors/meshing/netgen/call_netgen.m
@@ -39,7 +39,7 @@ end
 % Netgen executable filename
 cache_path = eidors_cache('cache_path');
 if  islinux
-   ng_name = '/opt/netgen/bin/netgen';
+   ng_name = '';
 else
    ng_name = [cache_path,'/ng'];
 end
