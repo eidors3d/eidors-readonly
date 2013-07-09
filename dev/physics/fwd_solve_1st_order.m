d5aa2cf106a52b38a1f298ab773ede0537998870
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2013-07-09 05:44:42 +0000

    support multiple measurement quantities
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@4287 82006732-fb7e-4a22-9f28-86a212cce563

--- dev/physics/fwd_solve_1st_order.m
+++ dev/physics/fwd_solve_1st_order.m
@@ -25,6 +25,17 @@ elseif  strcmp(getfield(warning('query','EIDORS:DeprecatedInterface'),'state'),'
 end
 fwd_model= img.fwd_model;
 
+try 
+    measurement = fwd_model.measured_quantity;
+catch
+    measurement = 'voltage';
+end
+
+if ~ismember(measurement, supported_measurement)
+    error('EIDORS:MeasurementNotSupported', '%s does not support %s',...
+    'FWD_SOLVE_1ST_ORDER',measurement);
+end
+
 img = physics_data_mapper(img);
 if ~ismember(img.current_physics, supported_physics)
     error('EIDORS:PhysicsNotSupported', '%s does not support %s', ...
@@ -66,9 +77,15 @@ catch err
    end
 end
 
+if ~strcmp(measurement, 'voltage')
+    [vv] = convert_measurement(vv,img,measurement);
+end
+    
 
 % create a data structure to return
 data.meas= vv;
+
+data.measured_quantity = measurement;
 data.time= NaN; % unknown
 data.name= 'solved by fwd_solve_1st_order';
 try; if img.fwd_solve.get_all_meas == 1
@@ -78,6 +95,55 @@ try; if img.fwd_solve.get_all_nodes== 1
    data.volt = v;                % all, including CEM nodes
 end; end
 
+function [vv] = convert_measurement(vv,img,measurement)
+switch measurement
+    case 'log_voltage'
+        vv = log(vv);
+    case 'apparent_resistivity'
+        fctr = get_factor(img);
+        vv = fctr*vv;
+    case 'log_apparent_resistivity'
+        fctr = get_factor(img);
+        vv = log(fctr*vv);
+end
+
+function fctr = get_factor(img)
+fctr = NaN;
+try
+   fctr = img.fwd_model.apparent_resistivity_factor;
+end
+
+if ischar(fctr), fctr = str2func(fcstr); end;
+
+if isa(fctr, 'function_handle')
+   fctr = feval(fctr, img);
+end
+
+if isnan(fctr)
+   img.fwd_model.measured_quantity = 'voltage';
+   %TODO: Should we use 1, or the actual image?
+   vh = fwd_solve(mk_image(img.fwd_model,1));
+   n = length(vh.meas);
+   fctr = spdiags(1./vh.meas,0,n,n);
+end
+
+
 function do_unit_test
    img = mk_image( mk_common_model('b2c2',16),1);
+   img.elem_data = 0.1 + rand(size(img.elem_data));
    vh = fwd_solve_1st_order(img);
+   img.fwd_model.measured_quantity = 'log_voltage';
+   vl = fwd_solve_1st_order(img);
+   subplot(131)
+   plot(vh.meas,vl.meas,'.'); 
+   
+   img.fwd_model.measured_quantity = 'apparent_resistivity';
+   vr = fwd_solve_1st_order(img);
+   subplot(132)
+   plot(vh.meas,vr.meas,'.'); 
+   
+   img.fwd_model.measured_quantity = 'log_apparent_resistivity';
+   vlr = fwd_solve_1st_order(img);
+   subplot(133)
+   plot(vr.meas,vlr.meas,'.'); 
+   
