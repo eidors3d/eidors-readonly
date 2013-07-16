3fd307e4e1548266e4412775aabafbc3ddeed76c
nolwenn85 <nolwenn85@82006732-fb7e-4a22-9f28-86a212cce563>
2013-07-16 07:38:22 +0000

    Addition of log10 estimations for voltages and apparent resistivity
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@4293 82006732-fb7e-4a22-9f28-86a212cce563

--- dev/physics/supported_measurement.m
+++ dev/physics/supported_measurement.m
@@ -1,7 +1,9 @@
 function list = supported_measurement
    list = {'voltage'
            'log_voltage'
+           'log10_voltage'
            'apparent_resistivity'
            'log_apparent_resistivity'
+           'log10_apparent_resistivity'
            };
 end
