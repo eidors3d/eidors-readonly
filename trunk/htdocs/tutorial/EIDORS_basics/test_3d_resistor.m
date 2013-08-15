1bb12f210dfaa7a4e3fc7677941f69701e86c044
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2013-08-15 14:41:00 +0000

    non uniform spacing

--- trunk/htdocs/tutorial/EIDORS_basics/test_3d_resistor.m
+++ trunk/htdocs/tutorial/EIDORS_basics/test_3d_resistor.m
@@ -1,22 +1,21 @@
 % Create 3D model of a Rectangular resistor
 % $Id$
 
-ll=5*1; % length (# elements)
-ww=1*2; % width (# elements)
-hh=1*3; % height (# elements)
+scale = .56; %meters
+ll=0:scale:5;
+ww=( 0:scale:2 ).^1.2;
+hh=( 0:scale:3 ).^0.8;
 conduc= .13;  % conductivity in Ohm-meters
 current= 4;  % Amps
 z_contact= 1e-2; % Ohms
-scale = .46; %
 
-mdl= mk_grid_model([],0:ww,0:hh,0:ll);
+mdl= mk_grid_model([],ww,hh,ll);
 mdl= rmfield(mdl,'coarse2fine');
-mdl.nodes= mdl.nodes*scale;
 mdl.boundary= find_boundary(mdl.elems);
 mdl.gnd_node = 1;
-elec_nodes= [1:(ww+1)*(hh+1)];
-elec(1).nodes= elec_nodes;      elec(1).z_contact= z_contact;
-elec(2).nodes= nn-elec_nodes+1; elec(2).z_contact= z_contact;
+elec_nodes= [1:length(ww)*length(hh)];
+elec(1).nodes= elec_nodes;                  elec(1).z_contact= z_contact;
+elec(2).nodes= num_nodes(mdl)-elec_nodes+1; elec(2).z_contact= z_contact;
 stim.stim_pattern= [-1;1]*current;
 stim.meas_pattern= [-1,1];
 mdl.stimulation= stim;
@@ -24,7 +23,10 @@ mdl.electrode= elec;
 show_fem(mdl);
 
 % analytical solution
-Block_R =  ll / ww / hh / scale/ conduc;
+Len = max(ll) - min(ll);
+Wid = max(ww) - min(ww);
+Hig = max(hh) - min(hh);
+Block_R =  Len / ( Wid * Hig ) / conduc;
 Contact_R = z_contact;
 R = Block_R + 2*Contact_R;
 
