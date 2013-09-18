91b1f803d677e6feb1efd3695ca2de400ed37a5f
alistair_boyle <alistair_boyle@82006732-fb7e-4a22-9f28-86a212cce563>
2013-09-18 21:22:15 +0000

    Fix material reordering for NetGen meshes.
    There is legacy code that reorders the material indices,
    placing the largest material (by element count) at the first
    index. For those who know which material is which based on
    the NetGen .geo file, this reordering breaks the model since
    it can not be easily undone. This change saves a version of
    the material indices in fwd_model unmodified. For backwards
    compatibility, the reordering remains as the function's
    second output variable.
    
    In general, this material reordering aught to be removed in
    some future commit since it can break if the region is
    broken into eqyal parts or a region with very high element
    density exists.
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@4339 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/meshing/netgen/ng_mk_fwd_model.m
+++ eidors/meshing/netgen/ng_mk_fwd_model.m
@@ -1,8 +1,8 @@
-function [fwd_mdl, mat_ind_reordered]= ...
+function [fwd_mdl, mat_idx_reordered]= ...
              ng_mk_fwd_model( ng_vol_filename, centres, ...
                               name, stim_pattern, z_contact, postprocmesh)
 % NG_MK_FWD_MODEL: create a fwd_model object from a netgen vol file
-% [fwd_mdl, mat_indices]= ...
+% [fwd_mdl, mat_idx_reordered]= ...
 %      ng_mk_fwd_model( ng_vol_filename, centres, ...
 %                       name, stim_pattern, z_contact)
 %
@@ -16,16 +16,17 @@ function [fwd_mdl, mat_ind_reordered]= ...
 %  z_contact:         vector or scalar electrode contact impedance
 %
 %  fwd_mdl:           eidors format fwd_model
-%  mat_ind_reordered: cell array of material indices from eidors
+%  mat_idx_reordered: cell array of material indices from eidors
 %                     reordered so that the material with
 %                     the most elements is placed first in
 %                     the list.  This is supposed to be the
 %                     main region but sometimes breaks when
 %                     there is a small region that is highly
 %                     refined.
-%                     -- mat_indices is DEPRECIATED (2013-09-18),
+%                     -- mat_ind_reordered is DEPRECIATED (2013-09-18),
 %                        the cell array is now stored inside
-%                        fwd_mdl as fwd_mdl.mat_ind_reordered
+%                        fwd_mdl as fwd_mdl.mat_ind_reordered and the
+%                        original list is stored as fwd_mdl.mat_ind
 % (C) 2006 Andy Adler. (C) 2013 Alistair Boyle. License: GPL version 2 or version 3
 % $Id$
 
@@ -48,6 +49,8 @@ fwd_mdl= construct_fwd_model(srf,vtx,simp,bc, name, ...
                              stim_pattern, centres, z_contact,fc);
 
 [fwd_mdl.mat_idx, fwd_mdl.mat_idx_reordered] = mk_mat_indices(mat_ind);
+
+mat_idx_reordered = fwd_mdl.mat_idx_reordered;
 
 if ~isfield(fwd_mdl,'normalize_measurements')
    fwd_mdl.normalize_measurements = 0;
@@ -92,38 +95,45 @@ mdl.system_mat= 'eidors_default';
 
 fwd_mdl= eidors_obj('fwd_model', mdl);
 
-% Output cell array of indices into each material type
-%   array order is the order of the specified material,
-%   except that the largest material (background) is placed
-%   first.
+% Output mat_ind cell array of indices into each material
+% typei. Array order is the order of the specified material
+% (netgen 'tlo' statements in the .geo file).
+% For mat_ind_reordered, the largest material (possibly the
+% background if it has the most elements) is placed first.
 function [mat_idx, mat_idx_reordered] = mk_mat_indices( mat_ind);
   % find length of mat_indices 
   % test example: mat_ind=[10 12 14 14 12 12 14 12];
 
   if isempty(mat_ind)
      mat_idx = [];
-      return
+     mat_idx_reordered = [];
+     return
   end
   mat_indices = unique( mat_ind );
   for i= 1:length(mat_indices);
      mat_idx{i}= find(mat_ind == mat_indices(i));
+  end
+% from here to end-of-function ... DEPRECIATED
+% (old code --- hack that sometimes breaks)
+  % put the largest material first
+  for i= 1:length(mat_indices);
      mat_idx_l(i) = length( mat_idx{i} );
   end
-  % put the largest material first
   [jnk, max_l] = max(mat_idx_l);
   new_idx = 1:length(mat_indices); new_idx(max_l) = [];
   ver= eidors_obj('interpreter_version');
   if ~ver.isoctave && ver.ver < 7
 % STUPID MATLAB WORK AROUND
-     clear jnk;ii=1;for i=[max_l, new_idx];
-         jnk{ii} = mat_idx{i}; ii=ii+1;
+     mat_idx_reordered = {};
+     ii=1;
+     for i=[max_l, new_idx];
+         mat_idx_reordered{ii} = mat_idx{i};
+         ii=ii+1;
      end
-     mat_idx = jnk;
   else
-     [mat_idx{:}] = mat_idx{[max_l, new_idx]};
+     mat_idx_reordered = cell(length(mat_idx),1);
+     [mat_idx_reordered{:}] = mat_idx{[max_l, new_idx]};
   end
-
-  mat_idx_reordered = mat_idx;
 
 function gnd_node=    find_centre_node(vtx);
   %distance from zero
