6e44b60808a9f98a2aa2d7093714f942ca20af5f
tschlebusch <tschlebusch@82006732-fb7e-4a22-9f28-86a212cce563>
2013-10-05 19:03:49 +0000

    Extension of 2D parametric reconstruction to 3D

---
+++ trunk/dev/t_schlebusch/apply_circle_parametrization_3d.m
@@ -0,0 +1,13 @@
+function img = apply_circle_parametrization_3d(img,dir)
+
+if nargin == 1;
+   dir = 0;
+end
+if dir==0
+   circ = @(r,x,y,z) 1 - 0.5 * elem_select(img.fwd_model,inline(sprintf('(x-%f).^2 + (y-%f).^2 + (z-%f).^2 <= %f^2',x,y,z,r),'x','y','z'));
+   p = img.conductivity.params;
+   img.elem_data = circ(p(1),p(2),p(3),p(4));
+   img.current_physics = 'conductivity';
+else
+   warning('reverse mapping is not possible');
+end
---
+++ trunk/dev/t_schlebusch/test_params_3d.m
@@ -0,0 +1,38 @@
+%% create forward model:
+fmdl= ng_mk_cyl_models([0.3,0.15,0.01],[16,[0.1,0.2]],[0.01]); 
+% Create stimulation pattern:
+stim =  mk_stim_patterns(16,2,[0,1],[0,1], {'no_meas_current'}, 1);
+fmdl.stimulation = stim;
+
+imdl = select_imdl(fmdl,{'Basic GN abs'});
+
+img = mk_image(fmdl,1,'conductivity');
+
+vh  = fwd_solve(img);
+
+circ= @(r,x,y,z,v) 1 - v * elem_select(fmdl,inline(sprintf('(x-%f).^2 + (y-%f).^2 + (z-%f).^2 <= %f^2',x,y,z,r),'x','y','z'));
+
+img.conductivity.elem_data = circ(0.08, 0, 0, 0.1, 0.5); % r, x, y, z, cond
+vi  = fwd_solve(img);
+vi = add_noise(40,vi);
+
+%show_fem(img)
+
+%% Reconstruction
+bkgnd.conductivity.params = [0.05, 0, 0, 0.15]'; % r x y z
+bkgnd.physics_data_mapper = 'conductivity';
+bkgnd.conductivity.func   = @apply_circle_parametrization_3d;
+bkgnd.physics_param_mapper = {'conductivity.params'};
+imdl.jacobian_bkgnd = bkgnd;
+imdl.fwd_model.jacobian = @jacobian_perturb;
+imdl.RtR_prior = @prior_tikhonov;
+imdl.hyperparameter.value = 1e-9;
+imdl.fwd_model.jacobian_perturb.delta  = 0.03;
+imdl.parameters.max_iterations = 50 ;
+imdl.parameters.show_iterations  = 1;
+
+rimg = inv_solve(imdl, vi);
+show_fem(rimg);
+eidors_colourbar(rimg);
+disp(rimg.conductivity.params);
+
