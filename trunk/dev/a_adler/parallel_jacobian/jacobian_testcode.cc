5f243bae3934c95d097aa3fa5da86e7594691fa1
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2014-02-21 19:53:15 +0000

    working C test version

--- trunk/dev/a_adler/parallel_jacobian/jacobian_testcode.cc
+++ trunk/dev/a_adler/parallel_jacobian/jacobian_testcode.cc
@@ -1,22 +1,16 @@
-#include "mex.h"
-#include "matrix.h"
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+#include "defvars.h"
 
 /*
-function DE = next_make_c(sz, zi2E_FCt, FC_sv);
-   DE= zeros(sz(1),sz(2),sz(3) );
-   for k= 1: sz(3);
-       idx= (sz(4)-1)*(k-1)+1 : (sz(4)-1)*k;
-       dq= zi2E_FCt(:,idx) * FC_sv(idx,:);
-       DE(:,:,k)= dq;
-   end
-*/
 void mexFunction( int nlhs, mxArray *plhs[],
                   int nrhs, const mxArray *prhs[] )
 {
   double *x,*y;
   mwSize mrows,ncols;
-  
-  /* Check for proper number of arguments. */
+
   if(nrhs!=3) {
     mexErrMsgTxt("Three inputs required.");
   } else if(nlhs>1) {
@@ -28,40 +22,30 @@ void mexFunction( int nlhs, mxArray *plhs[],
   {  
     mexErrMsgTxt("1x4 expected.");
   }
+*/
 
-  double * sz = mxGetPr(prhs[0]);
-/*
-  for (int k=0; k< 4; k++) {
-     printf("v%d = %f\n", k, sz[k]);
-  }
-*/
+  double * sz = S_sz;
   
-  /* Create matrix for the return argument.
-  plhs[0] = mxCreateDoubleMatrix(2,1, mxREAL);
-*/
-  mwSize dims[] = {sz[0],sz[1],sz[2]};
-  plhs[0] = mxCreateNumericArray(3, dims, mxDOUBLE_CLASS, mxREAL);
-  double * DE = mxGetPr(plhs[0]);
-printf("DE [%d x %d x %d]\n", dims[0],dims[1],dims[2]);
+  int dims[] = {sz[0],sz[1],sz[2]};
+  double DE [DE_SZ]; // test eq to S_DE
+  printf("DE [%d x %d x %d]\n", dims[0],dims[1],dims[2]);
   
-/*
-   d1 = sz(4)-1;
-   for k= 1: sz(3);
-       idx= d1*(k-1) + (1:d1);
-       dq= zi2E_FCt(:,idx) * FC_sv(idx,:);
-       DE(:,:,k)= dq;
-   end
-*/
+//   d1 = sz(4)-1;
+//   for k= 1: sz(3);
+//       idx= d1*(k-1) + (1:d1);
+//       dq= zi2E_FCt(:,idx) * FC_sv(idx,:);
+//       DE(:,:,k)= dq;
+//   end
+
   int d1 = (int) sz[3] - 1;
-  double * M1 = mxGetPr( prhs[1] ); // todo: check it is right size;
-  double * M2 = mxGetPr( prhs[2] ); // todo: check it is right size;
-  int R = mxGetM(prhs[1]); 
-  int L = mxGetN(prhs[1]); 
+  double * M1 = S_zi2E_FCt;
+  double * M2 = S_FC_sv;
+  int R = GetM_S_zi2E_FCt;
+  int L = GetN_S_zi2E_FCt;
 printf("M1[%d x %d] M2[%d x %d] d1=%d\n",
-       mxGetM(prhs[1]), mxGetN(prhs[1]), 
-       mxGetM(prhs[2]), mxGetN(prhs[2]), d1);
+       GetM_S_zi2E_FCt, GetN_S_zi2E_FCt,
+       GetM_S_FC_sv,    GetN_S_FC_sv,     d1);
 
-        
   int E = (int) sz[0]; // assume sz[1] == sz[0]
   int E2= E*E;
   for (int l=0; l< sz[2]; l++) {
@@ -70,9 +54,7 @@ printf("M1[%d x %d] M2[%d x %d] d1=%d\n",
      for (int j=0; j< R; j++) {
         double sum= 0.0;
         for (int k=0; k< d1; k++) {
-/*
-   DE(i,j,l) = sum_k M1(i, l*d1 + k) * M2(l*d1+k, j)
-*/
+// DE(i,j,l) = sum_k M1(i, l*d1 + k) * M2(l*d1+k, j)
           int m1o=  i + R*(ofs + k);
           int m2o=  ofs + k + L*j;
 if (i<=1 && l==0 && j<=1)
@@ -83,5 +65,11 @@ printf("i=%d j=%d k=%d m1[%d]=%f m2[%d]=%f\n",i,j,k, m1o, M1[m1o], m2o, M2[m2o])
      }
      }
   }
-   printf("fini\n");
+
+  double diff = 0.0;
+  for(int i=0; i<DE_SZ; i++) {
+    diff += fabs( DE[i] - S_DE[i]); 
+  }
+
+  printf("fini. Diff (should be 0) is %g\n",diff);
 }
