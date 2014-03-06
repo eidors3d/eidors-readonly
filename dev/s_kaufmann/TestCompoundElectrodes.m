390005314b027432b8ea32176477afeb1e4b81cd
elektrickser <elektrickser@82006732-fb7e-4a22-9f28-86a212cce563>
2014-03-06 12:24:42 +0000

    Tried to generate stim pattern for TestCompoundElectrodes.m
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@4451 82006732-fb7e-4a22-9f28-86a212cce563

--- dev/s_kaufmann/TestCompoundElectrodes.m
+++ dev/s_kaufmann/TestCompoundElectrodes.m
@@ -15,7 +15,7 @@
 
 %% Clear Workspace and Command Window, start time measurment & EIDORS
 clear;                      % Clear variables
-close all;                  % Close all windows
+%close all;                  % Close all windows
 MyEIDORSStartup;            % Starts EIDORS
 
 %eidors_cache('clear');      % Clear Cache
@@ -41,6 +41,56 @@ FEMimg = mk_image(fmdl, 1);
 
 % Show FEM forward model
 figure(1); set(gcf, 'Name', 'FEM Forward Model'); show_fem(FEMimg, [0, 1, 0]); title('Forward Model');
+
+%% Generate Stim Pattern
+Pattern.NumberOfElectrodeRings = 1;
+Pattern.AdjacentSkip = 7;
+%Pattern.AdjacentSkip = 1;
+
+%Pattern.RedundantMeasurementType = 'no_redundant';                       % 'do_redundant';
+Pattern.RedundantMeasurementType = 'do_redundant';                      % 'do_redundant';
+%Pattern.MeasureOnCurrentCarryingElectrodesType = 'no_meas_current';     % 'meas_current';
+Pattern.MeasureOnCurrentCarryingElectrodesType = 'meas_current';         % 'meas_current';
+Pattern.CurrentAmplitude = 5e-3;
+
+fprintf('\n\nPattern Settings:\n---------\n');
+fprintf('                       AdjacentSkip: %i\n', Pattern.AdjacentSkip);
+fprintf('                       %s, %s\n', Pattern.RedundantMeasurementType, Pattern.MeasureOnCurrentCarryingElectrodesType);
+fprintf('\n\n');
+
+[fmdl.stimulation2, fmdl.meas_sel2] = mk_stim_patterns(Electrodes.NumberOf, 2*Pattern.NumberOfElectrodeRings, [0 Pattern.AdjacentSkip], [0 Pattern.AdjacentSkip], {Pattern.RedundantMeasurementType Pattern.MeasureOnCurrentCarryingElectrodesType}, Pattern.CurrentAmplitude);
+[fmdl.stimulation, fmdl.meas_sel] = mk_stim_patterns(Electrodes.NumberOf, Pattern.NumberOfElectrodeRings, [0 Pattern.AdjacentSkip], [0 Pattern.AdjacentSkip], {Pattern.RedundantMeasurementType Pattern.MeasureOnCurrentCarryingElectrodesType}, Pattern.CurrentAmplitude);
+
+for i=1:length(fmdl.stimulation)
+    fmdl.stimulation(i).stim_pattern = full(fmdl.stimulation(i).stim_pattern);
+    fmdl.stimulation(i).stim_pattern = [fmdl.stimulation(i).stim_pattern; zeros(16, 1)];
+    fmdl.stimulation(i).stim_pattern = sparse(fmdl.stimulation(i).stim_pattern);
+    
+    fmdl.stimulation(i).meas_pattern = full(fmdl.stimulation(i).meas_pattern);
+    fmdl.stimulation(i).meas_pattern = [zeros(16,16), fmdl.stimulation(i).meas_pattern];
+    fmdl.stimulation(i).meas_pattern = [zeros(16,32); fmdl.stimulation(i).meas_pattern];
+    fmdl.stimulation(i).meas_pattern = sparse(fmdl.stimulation(i).meas_pattern);
+end
+fmdl.stimulation3 = fmdl.stimulation2(1:16);
+
+% Check im stim_pattern and stimulation is equal
+for i=1:16
+    if(all(fmdl.stimulation3(i).stim_pattern == fmdl.stimulation(i).stim_pattern) && all(fmdl.stimulation3(i).stimulation == fmdl.stimulation(i).stimulation))
+        fprintf('%i ok\n',i);
+    else
+        fprintf('%i not ok\n',i);
+    end;
+end;
+
+for i=1:16
+    full(fmdl.stimulation(i).meas_pattern)
+    full(fmdl.stimulation3(i).meas_pattern)
+end;
+
+ShowPattern(fmdl.stimulation, fmdl.meas_sel);
+
+% DISPLAY_MEAS: display measurements on mesh
+figure(2); display_meas(fmdl, 'ya', 0, 1);
 
 %% Goodbye Message
 Script.tElapsed=toc(Script.tStart);
