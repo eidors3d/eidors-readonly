e656642856d0b2b245224ed4cbb3bfea4711c307
elektrickser <elektrickser@82006732-fb7e-4a22-9f28-86a212cce563>
2014-03-10 11:49:34 +0000

    Updated CompoundElectrodes/TestCompoundElectrodes.m - Added reconstruction functionality - Added CalculateReciprocityAccuracy.m
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@4458 82006732-fb7e-4a22-9f28-86a212cce563

--- dev/s_kaufmann/MyEIDORSStartup.m
+++ dev/s_kaufmann/MyEIDORSStartup.m
@@ -1,23 +1,15 @@
-try 
-    % Check if EIDORS runs already
-    mk_stim_patterns(1, 1, 1, 1, '', 1);
-    
-    % When we are here EIDORS runs and we can return
-    return;
-catch
-    %% Save Path and jump to EIDORS path
-    curdir = cd;
-    cd C:\Repos\eidors\eidors
+%% Save Path and jump to EIDORS path
+curdir = cd;
+cd C:\Repos\eidors\eidors
 
-    %% Disable Warnings
-    warning off MATLAB:dispatcher:nameConflict
+%% Disable Warnings
+warning off MATLAB:dispatcher:nameConflict
 
-    %% Start EIDORS and increase cash
-    eidors_startup({'s_kaufmann'})
-    eidors_cache('cache_size', 4*1024*1024*1024);
-    eidors_cache
+%% Start EIDORS and increase cash
+eidors_startup({'s_kaufmann'})
+eidors_cache('cache_size', 20*1024*1024*1024);
+eidors_cache
 
-    %% Restore old path
-    cd(curdir)
-    clear curdir
-end;
+%% Restore old path
+cd(curdir)
+clear curdir
