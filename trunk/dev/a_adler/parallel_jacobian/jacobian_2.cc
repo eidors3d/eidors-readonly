6cb8dd1fcbba06389231b31e5f91691c72c0dc9d
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2014-03-20 15:11:00 +0000

    New code from Prateek

--- trunk/dev/a_adler/parallel_jacobian/jacobian_2.cc
+++ trunk/dev/a_adler/parallel_jacobian/jacobian_2.cc
@@ -1,6 +1,8 @@
 // Jacobian.cpp : Defines the entry point for the console application.
 //
+#if defined(_OPENMP)
 #include <omp.h>
+#endif
 #include <iostream>
 
 using std::cout;
@@ -8,6 +10,7 @@ using std::endl;
 
 #include <sstream>
 using std::istringstream;
+using std::ostringstream;
 
 #include <stdio.h>
 #include <math.h>
@@ -145,7 +148,9 @@ void beta( double *DE, const int maxrep )
     const double * sz = S_sz;
     const int dims[] = {sz[0],sz[1],sz[2]};
 
+#if defined(_OPENMP)
     cout << "Found " << omp_get_num_procs() << " processors" << endl;
+#endif
     cout << "DE [" << dims[0] << " x " << dims[1] << " x " << dims[2] << "]" << endl;
 
     const int d1 = (int) sz[3] - 1;
@@ -177,6 +182,7 @@ void beta( double *DE, const int maxrep )
             {
                 for (int j=0, Lj = 0, Ej = 0, m2o = ofs; j< R; j++, m2o += L, Ej += E )
                 {
+                    // ostringstream ostr;
                     double sum= 0.0;
                     for (int k=0, rk=0; k< d1; k++, rk += R )
                     {
@@ -184,7 +190,9 @@ void beta( double *DE, const int maxrep )
                         //                        printf("i=%d j=%d k=%d m1[%d]=%f m2[%d]=%f\n",i,j,k, m1o+rk, M1[m1o+rk], m2o+k, M2[m2o+k]);
                         sum += M1[m1o+rk] * M2[m2o+k];
                         ++mult;
+                        // ostr << "(" << (m1o+rk) << "," << (m2o+k) << ")";
                     }
+                    // cout << (i + Ej + E2l) << "=" << ostr.str() << endl;
                     DE[i + Ej + E2l] = sum;
                 }
             }
@@ -193,6 +201,68 @@ void beta( double *DE, const int maxrep )
 
     cout << "Mult: " << mult << endl;
 }
+
+
+void gamma( double *DE, const int maxrep )
+{
+#include "defvars.h"
+// X1=INT(A2/64)*16+MOD(MOD(A2,64),8)
+// Y1==INT(A2/64)*2+INT(MOD(A2,64)/8)*128
+// X2=X1+8
+// Y2=Y1+1
+
+#if defined(_OPENMP)
+    cout << "Found " << omp_get_num_procs() << " processors" << endl;
+#endif
+    const double * M1 = S_zi2E_FCt;
+    const double * M2 = S_FC_sv;
+
+    for( int rep = 0; rep < maxrep; ++rep )
+    {
+        /*
+         * Adjust the "k", and "j" loop to optimize for available
+         * thread/processors
+         */
+#define MAX 512
+#define KMAX (64)
+#define JMAX ((MAX)/(KMAX))           // 8
+#define ITER(k,j) (((k)<<3)|(j))
+
+#pragma omp parallel for
+        for( int k = 0; k < KMAX; ++k )
+        {
+            for( int j = 0; j < JMAX; ++j )
+            {
+                // const int x1 = (( i >> 6 ) << 4 ) + (( i & 63 ) & 7);
+                // const int y1 = (( i >> 6 ) << 1 ) + (( ( i & 63 ) >> 3 ) << 7);
+
+                const int   iter = ITER(k,j);
+
+                      int      x1 = ( ( iter & ~7 ) << 1 );
+                const int      y1 = ( ( iter & ~7 ) >> 2 ) + ( ( iter &  7 ) << 7 );
+                      int      x2 = x1 + 8;
+                const int      y2 = y1 + 1;
+                const double m2y1 = M2[y1];
+                const double m2y2 = M2[y2];
+
+                /*
+                 * Unroll loop for i ( 8 iterations )
+                 * M2[y1], and M2[y2] are loop invariant
+                 */
+                int   i = iter << 3;
+                DE[i++] = M1[x1++]*m2y1+M1[x2++]*m2y2;
+                DE[i++] = M1[x1++]*m2y1+M1[x2++]*m2y2;
+                DE[i++] = M1[x1++]*m2y1+M1[x2++]*m2y2;
+                DE[i++] = M1[x1++]*m2y1+M1[x2++]*m2y2;
+                DE[i++] = M1[x1++]*m2y1+M1[x2++]*m2y2;
+                DE[i++] = M1[x1++]*m2y1+M1[x2++]*m2y2;
+                DE[i++] = M1[x1++]*m2y1+M1[x2++]*m2y2;
+                DE[i++] = M1[x1++]*m2y1+M1[x2++]*m2y2;
+            }
+        }
+    }
+}
+
 int atoi( char *str )
 {
    int ret;
@@ -214,9 +284,9 @@ int main(int argc, char* argv[])
     double DE [DE_SZ];
 
     std::cout << "Calling original(rep=" << rep << ")" << std::endl;
-    clock_t t0 = clock();
+    time_t t0 = time( 0 );
     original( DE, rep );
-    cout << "Time: " << ((double) clock() - t0 )/CLOCKS_PER_SEC << endl;
+    cout << "Time: " << ( time(0) - t0 ) << endl;
 
     double diff = 0.0;
     for(int i=0; i<DE_SZ; i++)
@@ -229,9 +299,9 @@ int main(int argc, char* argv[])
 
     std::cout << std::endl;
     std::cout << "Calling alpha(rep=" << rep << ")" << std::endl;
-    t0 = clock( );
+    t0 = time( 0 );
     alpha( DEalpha, rep );
-    cout << "Time: " << ((double) clock() - t0 )/CLOCKS_PER_SEC << endl;
+    cout << "Time: " << ( time(0) - t0 ) << endl;
 
     diff = 0.0;
     for(int i=0; i<DE_SZ; i++)
@@ -244,9 +314,17 @@ int main(int argc, char* argv[])
 
     std::cout << std::endl;
     std::cout << "Calling beta(rep=" << rep << ")" << std::endl;
-    t0 = clock( );
+    t0 = time( 0 );
     beta( DEbeta, rep );
-    cout << "Time: " << ((double) clock() - t0 )/CLOCKS_PER_SEC << endl;
+    cout << "Time: " << ( time(0) - t0 ) << endl;
+
+    double DEgamma [DE_SZ];
+
+    std::cout << std::endl;
+    std::cout << "Calling gamma(rep=" << rep << ")" << std::endl;
+    t0 = time( 0 );
+    gamma( DEgamma, rep );
+    cout << "Time: " << ( time(0) - t0 ) << endl;
 
     diff = 0.0;
     for(int i=0; i<DE_SZ; i++)
@@ -272,6 +350,15 @@ int main(int argc, char* argv[])
         diff += fabs( DE[i] - DEbeta[i]); 
     }
     std::cout << "Sum fabs(delta beta): " << diff << std::endl;
+
+    std::cout << std::endl;
+    std::cout << "Comparing original vs gamma" << std::endl;
+    diff = 0.0;
+    for(int i=0; i<DE_SZ; i++)
+    {
+        diff += fabs( DE[i] - DEgamma[i]); 
+    }
+    std::cout << "Sum fabs(delta gamma): " << diff << std::endl;
 
     return 0;
 }
