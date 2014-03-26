188a05eebc21b19c7fad5c7704bac7f72b73ec5f
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2014-03-26 13:56:05 +0000

    add any_cuts script

--- trunk/eidors/graphics/matlab/show_3d_slices.m
+++ trunk/eidors/graphics/matlab/show_3d_slices.m
@@ -1,9 +1,10 @@
 function show_3d_slices(img, varargin);
-% show_3d_slices(img, z_cuts, x_cuts, y_cuts)
+% show_3d_slices(img, z_cuts, x_cuts, y_cuts, any_cuts)
 % Show a 3d view of an object with many slices through it
 %  z_cuts = planes in z to do a cut
 %  x_cuts = planes in x to do a cut
 %  y_cuts = planes in y to do a cut
+%  any_cuts = cut planes intercepts as Nx3 matrix (see mdl_slice_mapper)
 % Default show 2 z_cuts and 1 x and 1 y cut
 
 % (C) 2007-2012 Andy Adler & Bartlomiej Grychtol. 
@@ -49,7 +50,7 @@ mdl_min = min(img.fwd_model.nodes);
 mdl_max = max(img.fwd_model.nodes);
 mdl_rng = mdl_max - mdl_min;
 np = round(mdl_rng/min(mdl_rng) * np);
-[x_cuts, y_cuts, z_cuts] = get_cuts(img,varargin{:});
+[x_cuts, y_cuts, z_cuts, any_cuts] = get_cuts(img,varargin{:});
 xvec = linspace(mdl_min(1), mdl_max(1),np(1)+1);
 yvec = linspace(mdl_min(2), mdl_max(2),np(2)+1);
 zvec = linspace(mdl_min(3), mdl_max(3),np(3)+1);
@@ -69,6 +70,11 @@ for i= 1:length(y_cuts)
     show_slice(img,[inf y_cuts(i) inf]);
     hold on
 end
+
+for i= 1:size(any_cuts,1)
+    show_slice(img,any_cuts(i,:));
+    hold on
+end
 hold off
 
 warning(qfi.state,'EIDORS:FirstImageOnly');
@@ -79,9 +85,10 @@ function show_slice(img, level)
     show_fem(slc);
 
     
-function [x_cuts, y_cuts, z_cuts] =  get_cuts(img, varargin)
+function [x_cuts, y_cuts, z_cuts, any_cuts] =  get_cuts(img, varargin)
    mdl_max= max(img.fwd_model.nodes);
    mdl_min= min(img.fwd_model.nodes);
+   any_cuts = [];
    if nargin==1;
       % Default show 2 z_cuts and 1 x and 1 y cut
        x_cuts= linspace(mdl_min(1), mdl_max(1), 3); x_cuts([1,3])=[];
@@ -99,6 +106,11 @@ function [x_cuts, y_cuts, z_cuts] =  get_cuts(img, varargin)
        z_cuts= varargin{1};
        x_cuts= varargin{2};
        y_cuts= varargin{3};
+   elseif nargin==5;
+       z_cuts= varargin{1};
+       x_cuts= varargin{2};
+       y_cuts= varargin{3};
+       any_cuts= varargin{4};
    else 
        error('too many inputs');
    end
@@ -118,13 +130,15 @@ function do_unit_test
    calc_colours('transparency_thresh', 0.25);
    show_3d_slices(imgr,[1.5,2],[],[]);
 
-   subplot(221);  show_3d_slices(imgr,[1,2],0,0.5);
-   subplot(222);  show_3d_slices(img ,[1,2],0,0.5);
+   subplot(231);  show_3d_slices(imgr,[1,2],0,0.5);
+   subplot(232);  show_3d_slices(img ,[1,2],0,0.5);
+   cuts = [inf, -2.5, 1.5; inf, 10, 1.5];
+   subplot(233);  show_3d_slices(img ,[],[],[],cuts );
    
    imgr.calc_colours.transparency_thresh = -1;
    img.calc_colours.transparency_thresh = -1;
-   subplot(223);  show_3d_slices(imgr,[1,2],0.3,0.7);
-   subplot(224);  
+   subplot(234);  show_3d_slices(imgr,[1,2],0.3,0.7);
+   subplot(235);  
    show_fem(img.fwd_model);
    hold on
    show_3d_slices(img ,[1,2],0.3,0.7);
