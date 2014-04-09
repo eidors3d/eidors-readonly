679182e50146c427a46691d6d227ae6b33abdf16
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2014-04-09 12:59:44 +0000

    Much faster Jacobian code
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@4493 82006732-fb7e-4a22-9f28-86a212cce563

--- dev/a_adler/parallel_jacobian/jacobian_2.cc
+++ dev/a_adler/parallel_jacobian/jacobian_2.cc
@@ -3,22 +3,25 @@
 #if defined(_OPENMP)
 #include <omp.h>
 #endif
-#include <iostream>
 
+#include <iostream>
 using std::cout;
 using std::endl;
+
+#include <string>
+using std::string;
 
 #include <sstream>
 using std::istringstream;
 using std::ostringstream;
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include <time.h>
 
-void original( double *DE, const int maxrep )
+void original( double *S_sz, double *S_zi2E_FCt, double *S_FC_sv, int GetM_S_zi2E_FCt, int GetN_S_zi2E_FCt, int GetM_S_FC_sv, int GetN_S_FC_sv, double *DE )
 {
-#include "defvars.h"
 
 /*
 void mexFunction( int nlhs, mxArray *plhs[],
@@ -44,7 +47,7 @@ void mexFunction( int nlhs, mxArray *plhs[],
   
   int dims[] = {sz[0],sz[1],sz[2]};
 //  double DE [DE_SZ]; // test eq to S_DE
-  printf("DE [%d x %d x %d]\n", dims[0],dims[1],dims[2]);
+  // printf("DE [%d x %d x %d]\n", dims[0],dims[1],dims[2]);
   
 //   d1 = sz(4)-1;
 //   for k= 1: sz(3);
@@ -58,14 +61,12 @@ void mexFunction( int nlhs, mxArray *plhs[],
   double * M2 = S_FC_sv;
   int R = GetM_S_zi2E_FCt;
   int L = GetN_S_zi2E_FCt;
-printf("M1[%d x %d] M2[%d x %d] d1=%d\n",
-       GetM_S_zi2E_FCt, GetN_S_zi2E_FCt,
-       GetM_S_FC_sv,    GetN_S_FC_sv,     d1);
+//printf("M1[%d x %d] M2[%d x %d] d1=%d\n",
+//       GetM_S_zi2E_FCt, GetN_S_zi2E_FCt,
+//       GetM_S_FC_sv,    GetN_S_FC_sv,     d1);
 
   int E = (int) sz[0]; // assume sz[1] == sz[0]
   int E2= E*E;
-for( int rep = 0; rep < maxrep; ++rep )
-{
   for (int l=0; l< sz[2]; l++) {
      int ofs = d1*l;
      for (int i=0; i< R; i++) {
@@ -83,7 +84,6 @@ for( int rep = 0; rep < maxrep; ++rep )
      }
      }
   }
-}
 
   //double diff = 0.0;
   //for(int i=0; i<DE_SZ; i++) {
@@ -93,14 +93,13 @@ for( int rep = 0; rep < maxrep; ++rep )
   //printf("fini. Diff (should be 0) is %g\n",diff);
 }
 
-void alpha( double *DE, const int maxrep )
+void alpha( double *S_sz, double *S_zi2E_FCt, double *S_FC_sv, int GetM_S_zi2E_FCt, int GetN_S_zi2E_FCt, int GetM_S_FC_sv, int GetN_S_FC_sv, double *DE )
 {
-#include "defvars.h"
 
     const double * sz = S_sz;
     const int dims[] = {sz[0],sz[1],sz[2]};
 
-    cout << "DE [" << dims[0] << " x " << dims[1] << " x " << dims[2] << "]" << endl;
+    // cout << "DE [" << dims[0] << " x " << dims[1] << " x " << dims[2] << "]" << endl;
 
     const int d1 = (int) sz[3] - 1;
     const double * M1 = S_zi2E_FCt;
@@ -108,16 +107,14 @@ void alpha( double *DE, const int maxrep )
     const int R = GetM_S_zi2E_FCt;
     const int L = GetN_S_zi2E_FCt;
 
-    cout << "M1[" << GetM_S_zi2E_FCt << " x " << GetN_S_zi2E_FCt << "]"
-        << " M2[" << GetM_S_FC_sv << " x " << GetN_S_FC_sv << "] d1=" << d1 << endl;
+    // cout << "M1[" << GetM_S_zi2E_FCt << " x " << GetN_S_zi2E_FCt << "]"
+    //     << " M2[" << GetM_S_FC_sv << " x " << GetN_S_FC_sv << "] d1=" << d1 << endl;
 
     const int E = (int) sz[0]; // assume sz[1] == sz[0]
     const int E2= E*E;
     const int Rd1 = R * d1;
 
-    long long mult = 0;
-    for( int rep = 0; rep < maxrep; ++rep )
-    {
+    // long long mult = 0;
         for (int l=0, ofs=0, E2l=0, Rofs = 0; l< sz[2]; l++, ofs += d1, E2l += E2, Rofs += Rd1 )
         {
             for (int i=0, m1o = Rofs; i< R; i++, m1o++)
@@ -130,28 +127,26 @@ void alpha( double *DE, const int maxrep )
                         //                    if (i<=1 && l==0 && j<=1)
                         //                        printf("i=%d j=%d k=%d m1[%d]=%f m2[%d]=%f\n",i,j,k, m1o+rk, M1[m1o+rk], m2o+k, M2[m2o+k]);
                         sum += M1[m1o+rk] * M2[m2o+k];
-                        ++mult;
+                        // ++mult;
                     }
                     DE[i + Ej + E2l] = sum;
                 }
             }
         }
-    }
 
-    cout << "Mult: " << mult << endl;
+    // cout << "Mult: " << mult << endl;
 }
 
-void beta( double *DE, const int maxrep )
+void beta( double *S_sz, double *S_zi2E_FCt, double *S_FC_sv, int GetM_S_zi2E_FCt, int GetN_S_zi2E_FCt, int GetM_S_FC_sv, int GetN_S_FC_sv, double *DE )
 {
-#include "defvars.h"
 
     const double * sz = S_sz;
     const int dims[] = {sz[0],sz[1],sz[2]};
 
 #if defined(_OPENMP)
-    cout << "Found " << omp_get_num_procs() << " processors" << endl;
+    // cout << "Found " << omp_get_num_procs() << " processors" << endl;
 #endif
-    cout << "DE [" << dims[0] << " x " << dims[1] << " x " << dims[2] << "]" << endl;
+    // cout << "DE [" << dims[0] << " x " << dims[1] << " x " << dims[2] << "]" << endl;
 
     const int d1 = (int) sz[3] - 1;
     const double * M1 = S_zi2E_FCt;
@@ -159,19 +154,17 @@ void beta( double *DE, const int maxrep )
     const int R = GetM_S_zi2E_FCt;
     const int L = GetN_S_zi2E_FCt;
 
-    cout << "M1[" << GetM_S_zi2E_FCt << " x " << GetN_S_zi2E_FCt << "]"
-        << " M2[" << GetM_S_FC_sv << " x " << GetN_S_FC_sv << "] d1=" << d1 << endl;
+    // cout << "M1[" << GetM_S_zi2E_FCt << " x " << GetN_S_zi2E_FCt << "]"
+    //     << " M2[" << GetM_S_FC_sv << " x " << GetN_S_FC_sv << "] d1=" << d1 << endl;
 
     const int E = (int) sz[0]; // assume sz[1] == sz[0]
     const int E2= E*E;
     const int Rd1 = R * d1;
 
-    long long mult = 0;
-    for( int rep = 0; rep < maxrep; ++rep )
-    {
+    // long long mult = 0;
         int l;
         int maxL = sz[2];
-#pragma omp parallel for reduction(+:mult)
+#pragma omp parallel for
         for (l=0; l < maxL; l++ )
         {
             const int ofs = l * d1;
@@ -189,7 +182,7 @@ void beta( double *DE, const int maxrep )
                         //                    if (i<=1 && l==0 && j<=1)
                         //                        printf("i=%d j=%d k=%d m1[%d]=%f m2[%d]=%f\n",i,j,k, m1o+rk, M1[m1o+rk], m2o+k, M2[m2o+k]);
                         sum += M1[m1o+rk] * M2[m2o+k];
-                        ++mult;
+                        // ++mult;
                         // ostr << "(" << (m1o+rk) << "," << (m2o+k) << ")";
                     }
                     // cout << (i + Ej + E2l) << "=" << ostr.str() << endl;
@@ -197,69 +190,135 @@ void beta( double *DE, const int maxrep )
                 }
             }
         }
-    }
 
-    cout << "Mult: " << mult << endl;
+    // cout << "Mult: " << mult << endl;
 }
 
 
-void gamma( double *DE, const int maxrep )
+void gamma( double *S_sz, double *S_zi2E_FCt, double *S_FC_sv, int GetM_S_zi2E_FCt, int GetN_S_zi2E_FCt, int GetM_S_FC_sv, int GetN_S_FC_sv, double *DE )
 {
-#include "defvars.h"
 // X1=INT(A2/64)*16+MOD(MOD(A2,64),8)
 // Y1==INT(A2/64)*2+INT(MOD(A2,64)/8)*128
 // X2=X1+8
 // Y2=Y1+1
 
-#if defined(_OPENMP)
-    cout << "Found " << omp_get_num_procs() << " processors" << endl;
-#endif
+// X1=INT(A2/196)*42+MOD(MOD(A2,196),14)
+// Y1==INT(A2/196)*3+INT(MOD(A2,196)/14)*23529
+// X2=X1+14
+// Y2=Y1+1
+// X3=X1+28
+// Y3=Y1+2
+
     const double * M1 = S_zi2E_FCt;
     const double * M2 = S_FC_sv;
-
-    for( int rep = 0; rep < maxrep; ++rep )
-    {
-        /*
-         * Adjust the "k", and "j" loop to optimize for available
-         * thread/processors
-         */
-#define MAX 512
-#define KMAX (64)
-#define JMAX ((MAX)/(KMAX))           // 8
-#define ITER(k,j) (((k)<<3)|(j))
+    int maxI = S_sz[0] * S_sz[1] * S_sz[2];
 
 #pragma omp parallel for
-        for( int k = 0; k < KMAX; ++k )
+        for( int index = 0; index < 1463140/14; ++index )
         {
-            for( int j = 0; j < JMAX; ++j )
-            {
-                // const int x1 = (( i >> 6 ) << 4 ) + (( i & 63 ) & 7);
-                // const int y1 = (( i >> 6 ) << 1 ) + (( ( i & 63 ) >> 3 ) << 7);
+             //       int      x1 = (( i >> 6 ) << 4 ) + (( i & 63 ) & 7);
+             // const int      y1 = (( i >> 6 ) << 1 ) + (( ( i & 63 ) >> 3 ) * GetN_S_zi2E_FCt );
+             //       int      x2 = x1 + 8;
+             // const int      y2 = y1 + 1;
 
-                const int   iter = ITER(k,j);
-
-                      int      x1 = ( ( iter & ~7 ) << 1 );
-                const int      y1 = ( ( iter & ~7 ) >> 2 ) + ( ( iter &  7 ) << 7 );
-                      int      x2 = x1 + 8;
+                      int       i = index * 14;
+                      int      x1 = (i/196)*42 + ((i%196)%14);
+                const int      y1 = (i/196)*3  + ((i%196)/14) * 23529;
+                      int      x2 = x1 + 14;
                 const int      y2 = y1 + 1;
+                      int      x3 = x1 + 28;
+                const int      y3 = y1 + 2;
+
                 const double m2y1 = M2[y1];
                 const double m2y2 = M2[y2];
+                const double m2y3 = M2[y3];
 
-                /*
-                 * Unroll loop for i ( 8 iterations )
-                 * M2[y1], and M2[y2] are loop invariant
-                 */
-                int   i = iter << 3;
-                DE[i++] = M1[x1++]*m2y1+M1[x2++]*m2y2;
-                DE[i++] = M1[x1++]*m2y1+M1[x2++]*m2y2;
-                DE[i++] = M1[x1++]*m2y1+M1[x2++]*m2y2;
-                DE[i++] = M1[x1++]*m2y1+M1[x2++]*m2y2;
-                DE[i++] = M1[x1++]*m2y1+M1[x2++]*m2y2;
-                DE[i++] = M1[x1++]*m2y1+M1[x2++]*m2y2;
-                DE[i++] = M1[x1++]*m2y1+M1[x2++]*m2y2;
-                DE[i++] = M1[x1++]*m2y1+M1[x2++]*m2y2;
-            }
+                DE[i] = M1[x1]*m2y1  +M1[x2]*m2y2  +M1[x3]*m2y3  ;
+                DE[++i] = M1[++x1]*m2y1  +M1[++x2]*m2y2  +M1[++x3]*m2y3  ;
+                DE[++i] = M1[++x1]*m2y1  +M1[++x2]*m2y2  +M1[++x3]*m2y3  ;
+                DE[++i] = M1[++x1]*m2y1  +M1[++x2]*m2y2  +M1[++x3]*m2y3  ;
+
+                DE[++i] = M1[++x1]*m2y1  +M1[++x2]*m2y2  +M1[++x3]*m2y3  ;
+                DE[++i] = M1[++x1]*m2y1  +M1[++x2]*m2y2  +M1[++x3]*m2y3  ;
+                DE[++i] = M1[++x1]*m2y1  +M1[++x2]*m2y2  +M1[++x3]*m2y3  ;
+                DE[++i] = M1[++x1]*m2y1  +M1[++x2]*m2y2  +M1[++x3]*m2y3  ;
+
+                DE[++i] = M1[++x1]*m2y1  +M1[++x2]*m2y2  +M1[++x3]*m2y3  ;
+                DE[++i] = M1[++x1]*m2y1  +M1[++x2]*m2y2  +M1[++x3]*m2y3  ;
+                DE[++i] = M1[++x1]*m2y1  +M1[++x2]*m2y2  +M1[++x3]*m2y3  ;
+                DE[++i] = M1[++x1]*m2y1  +M1[++x2]*m2y2  +M1[++x3]*m2y3  ;
+
+                DE[++i] = M1[++x1]*m2y1  +M1[++x2]*m2y2  +M1[++x3]*m2y3  ;
+                DE[++i] = M1[++x1]*m2y1  +M1[++x2]*m2y2  +M1[++x3]*m2y3  ;
+
+                // cout << i << " " << x1 << "," << y1 << "," << x2 << "," << y2 << "," << x3 << "," << y3 << endl;
         }
+}
+
+void delta( double *S_sz, double *S_zi2E_FCt, double *S_FC_sv, int GetM_S_zi2E_FCt, int GetN_S_zi2E_FCt, int GetM_S_FC_sv, int GetN_S_FC_sv, double *DE )
+{
+// X1=INT(A2/64)*16+MOD(MOD(A2,64),8)
+// Y1==INT(A2/64)*2+INT(MOD(A2,64)/8)*128
+// X2=X1+8
+// Y2=Y1+1
+
+    const double * M1 = S_zi2E_FCt;
+    const double * M2 = S_FC_sv;
+    int maxI = (S_sz[0] * S_sz[1] * S_sz[2])/8;
+
+    for( int i = 0; i < maxI; ++i )
+    {
+        int iter = i << 3;
+
+              int      x1 = (( iter >> 6 ) << 4 ) + (( iter & 63 ) & 7);
+        const int      y1 = (( iter >> 6 ) << 1 ) + (( ( iter & 63 ) >> 3 ) * GetN_S_zi2E_FCt );
+              int      x2 = x1 + 8;
+        const int      y2 = y1 + 1;
+        const double m2y1 = M2[y1];
+        const double m2y2 = M2[y2];
+
+        DE[iter++] = M1[x1++]*m2y1+M1[x2++]*m2y2;
+        DE[iter++] = M1[x1++]*m2y1+M1[x2++]*m2y2;
+        DE[iter++] = M1[x1++]*m2y1+M1[x2++]*m2y2;
+        DE[iter++] = M1[x1++]*m2y1+M1[x2++]*m2y2;
+        DE[iter++] = M1[x1++]*m2y1+M1[x2++]*m2y2;
+        DE[iter++] = M1[x1++]*m2y1+M1[x2++]*m2y2;
+        DE[iter++] = M1[x1++]*m2y1+M1[x2++]*m2y2;
+        DE[iter++] = M1[x1++]*m2y1+M1[x2++]*m2y2;
+    }
+}
+
+void epsilon( double *S_sz, double *S_zi2E_FCt, double *S_FC_sv, int GetM_S_zi2E_FCt, int GetN_S_zi2E_FCt, int GetM_S_FC_sv, int GetN_S_FC_sv, double *DE )
+{
+// X1=INT(A2/64)*16+MOD(MOD(A2,64),8)
+// Y1==INT(A2/64)*2+INT(MOD(A2,64)/8)*128
+// X2=X1+8
+// Y2=Y1+1
+
+    const double * M1 = S_zi2E_FCt;
+    const double * M2 = S_FC_sv;
+    int maxI = (S_sz[0] * S_sz[1] * S_sz[2])/8;
+
+#pragma omp parallel for
+    for( int i = 0; i < maxI; ++i )
+    {
+        int iter = i << 3;
+
+              int      x1 = (( iter >> 6 ) << 4 ) + (( iter & 63 ) & 7);
+        const int      y1 = (( iter >> 6 ) << 1 ) + (( ( iter & 63 ) >> 3 ) * GetN_S_zi2E_FCt );
+              int      x2 = x1 + 8;
+        const int      y2 = y1 + 1;
+        const double m2y1 = M2[y1];
+        const double m2y2 = M2[y2];
+
+        DE[iter++] = M1[x1++]*m2y1+M1[x2++]*m2y2;
+        DE[iter++] = M1[x1++]*m2y1+M1[x2++]*m2y2;
+        DE[iter++] = M1[x1++]*m2y1+M1[x2++]*m2y2;
+        DE[iter++] = M1[x1++]*m2y1+M1[x2++]*m2y2;
+        DE[iter++] = M1[x1++]*m2y1+M1[x2++]*m2y2;
+        DE[iter++] = M1[x1++]*m2y1+M1[x2++]*m2y2;
+        DE[iter++] = M1[x1++]*m2y1+M1[x2++]*m2y2;
+        DE[iter++] = M1[x1++]*m2y1+M1[x2++]*m2y2;
     }
 }
 
@@ -275,90 +334,73 @@ int atoi( char *str )
    return ret;
 }
 
+
+void runTrial( string name, void (*fn)( double *S_sz, double *S_zi2E_FCt, double *S_FC_sv, int GetM_S_zi2E_FCt, int GetN_S_zi2E_FCt, int GetM_S_FC_sv, int GetN_S_FC_sv, double *DE ), double *S_sz, double *S_zi2E_FCt, double *S_FC_sv, int GetM_S_zi2E_FCt, int GetN_S_zi2E_FCt, int GetM_S_FC_sv, int GetN_S_FC_sv, double *DE, const int maxrep )
+{
+    std::cout << std::endl;
+    std::cout << "Calling " << name << " (rep=" << maxrep << ")" << std::endl;
+    time_t t0 = time( 0 );
+    for( int i = 0; i < maxrep; ++i )
+    {
+        fn( S_sz, S_zi2E_FCt, S_FC_sv, GetM_S_zi2E_FCt, GetN_S_zi2E_FCt, GetM_S_FC_sv, GetN_S_FC_sv, DE );
+    }
+    cout << "Time: " << ( time(0) - t0 ) << endl;
+}
+
+void runCompare( int size, string ref, double *refData, string sample, double *sampleData )
+{
+    double diff = 0.0;
+    for(int i=0; i<size; i++)
+    {
+        diff += fabs( refData[i] - sampleData[i]); 
+    }
+    std::cout << "Diff between " << ref << " and " << sample << " is " << diff << std::endl;
+}
+
 int main(int argc, char* argv[])
 {
-    #include "defvars.h"
 
     const int rep = (argc==2) ? atoi( argv[1] ) : 1;
 
-    double DE [DE_SZ];
+// #include "defvars-short.h"
+#include "defvars.h"
 
-    std::cout << "Calling original(rep=" << rep << ")" << std::endl;
-    time_t t0 = time( 0 );
-    original( DE, rep );
-    cout << "Time: " << ( time(0) - t0 ) << endl;
-
-    double diff = 0.0;
-    for(int i=0; i<DE_SZ; i++)
+    struct
     {
-        diff += fabs( DE[i] - S_DE[i]); 
-    }
-    std::cout << "Sum fabs(delta): " << diff << std::endl;
-
-    double DEalpha [DE_SZ];
-
-    std::cout << std::endl;
-    std::cout << "Calling alpha(rep=" << rep << ")" << std::endl;
-    t0 = time( 0 );
-    alpha( DEalpha, rep );
-    cout << "Time: " << ( time(0) - t0 ) << endl;
-
-    diff = 0.0;
-    for(int i=0; i<DE_SZ; i++)
+        string name;
+        void (*fn)( double *S_sz,
+                    double *S_zi2E_FCt,
+                    double *S_FC_sv,
+                    int GetM_S_zi2E_FCt,
+                    int GetN_S_zi2E_FCt,
+                    int GetM_S_FC_sv,
+                    int GetN_S_FC_sv,
+                    double *DE );
+        double *data;
+    } table[] =
     {
-        diff += fabs( DEalpha[i] - S_DE[i]); 
-    }
-    std::cout << "Sum fabs(delta): " << diff << std::endl;
+        { "Original C", original, (double *)malloc( sizeof(double) * DE_SZ ) },
+        { "Alpha",      alpha,    (double *)malloc( sizeof(double) * DE_SZ ) },
+        { "Beta",       beta,     (double *)malloc( sizeof(double) * DE_SZ ) },
+        { "Gamma",      gamma,    (double *)malloc( sizeof(double) * DE_SZ ) },
+        //{ "Delta",      delta,    (double *)malloc( sizeof(double) * DE_SZ ) },
+        //{ "Epsilon",    epsilon,  (double *)malloc( sizeof(double) * DE_SZ ) },
+    };
 
-    double DEbeta [DE_SZ];
-
-    std::cout << std::endl;
-    std::cout << "Calling beta(rep=" << rep << ")" << std::endl;
-    t0 = time( 0 );
-    beta( DEbeta, rep );
-    cout << "Time: " << ( time(0) - t0 ) << endl;
-
-    double DEgamma [DE_SZ];
-
-    std::cout << std::endl;
-    std::cout << "Calling gamma(rep=" << rep << ")" << std::endl;
-    t0 = time( 0 );
-    gamma( DEgamma, rep );
-    cout << "Time: " << ( time(0) - t0 ) << endl;
-
-    diff = 0.0;
-    for(int i=0; i<DE_SZ; i++)
+    for( int i = 0; i < ((sizeof(table))/(sizeof(table[0]))); ++i )
     {
-        diff += fabs( DEbeta[i] - S_DE[i]); 
+        runTrial( table[i].name, table[i].fn, S_sz, S_zi2E_FCt, S_FC_sv, GetM_S_zi2E_FCt, GetN_S_zi2E_FCt, GetM_S_FC_sv, GetN_S_FC_sv, table[i].data, rep );
+        runCompare( DE_SZ, "Matlab", S_DE, table[i].name, table[i].data );
+        if( i != 0 )
+        {
+            runCompare( DE_SZ, table[0].name, table[0].data, table[i].name, table[i].data );
+        }
     }
-    std::cout << "Sum fabs(delta): " << diff << std::endl;
 
-    std::cout << std::endl;
-    std::cout << "Comparing original vs alpha" << std::endl;
-    diff = 0.0;
-    for(int i=0; i<DE_SZ; i++)
+    for( int i = 0; i < ((sizeof(table))/(sizeof(table[0]))); ++i )
     {
-        diff += fabs( DE[i] - DEalpha[i]); 
+        free( table[i].data );
     }
-    std::cout << "Sum fabs(delta alpha): " << diff << std::endl;
-
-    std::cout << std::endl;
-    std::cout << "Comparing original vs beta" << std::endl;
-    diff = 0.0;
-    for(int i=0; i<DE_SZ; i++)
-    {
-        diff += fabs( DE[i] - DEbeta[i]); 
-    }
-    std::cout << "Sum fabs(delta beta): " << diff << std::endl;
-
-    std::cout << std::endl;
-    std::cout << "Comparing original vs gamma" << std::endl;
-    diff = 0.0;
-    for(int i=0; i<DE_SZ; i++)
-    {
-        diff += fabs( DE[i] - DEgamma[i]); 
-    }
-    std::cout << "Sum fabs(delta gamma): " << diff << std::endl;
 
     return 0;
 }
