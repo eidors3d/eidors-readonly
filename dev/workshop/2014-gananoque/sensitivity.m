b20f08ec20d3c644c1988f1e2a11d5f667566ef3
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2014-04-22 18:16:58 +0000

    code to play with
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@4518 82006732-fb7e-4a22-9f28-86a212cce563

--- dev/workshop/2014-gananoque/sensitivity.m
+++ dev/workshop/2014-gananoque/sensitivity.m
@@ -1,4 +1,16 @@
 % fmdl= ng_mk_cyl_models(3,[7,1],[0.2,0,0.05]); 
+if 0
+imdl = mk_common_model('f2c2',16);
+fmdl = imdl.fwd_model;
+else
+    fmdl = ng_mk_cyl_models([.5],[16,0.25],[.05,0,.01]);
+end
+
+fmdl.solve = @fwd_solve_higher_order;
+fmdl.system_mat = @system_mat_higher_order;
+fmdl.approx_type    = 'tet4'; % linear
+fmdl.approx_type    = 'tet10'; %Quadratic
+
 slices= [];
 for pl=1:3
     subplot(2,2,pl); 
@@ -8,8 +20,7 @@ for pl=1:3
         case 3; CSKIP = 6; MSKIP= 6;
     end
 
-imdl = mk_common_model('f2c2',16);
-fmdl = imdl.fwd_model;
+
 fmdl.stimulation = mk_stim_patterns(16,1,[0,CSKIP],[0,MSKIP],{},1);
 
 img = mk_image(fmdl,1);
@@ -21,12 +32,13 @@ for i=1:size(J,2)
 end
 sens.elem_data = sqrt(s1)./get_elem_volume(fmdl);
 
-%sens.calc_colours.clim = 3;
+sens.calc_colours.clim = 20;
 sens.calc_colours.ref_level = 0;
 %show_fem(sens,[0 1]);
-show_slices(sens)
-eidors_colourbar(sens);
-sli = calc_slices(sens);
+show_slices(sens,1)
+show_3d_slices(sens)
+%eidors_colourbar(sens);
+sli = calc_slices(sens,1);
 slices = [slices,sli(:,32)./max(sli(:,32))];
 end
 %eidors_colourbar(sens);
