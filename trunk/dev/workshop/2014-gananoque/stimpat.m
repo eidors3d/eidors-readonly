15a7b53e0f2676974ef5063fb5ba48a2c3488174
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2014-04-22 13:49:29 +0000

    add Inez's data and update stim code

--- trunk/dev/workshop/2014-gananoque/stimpat.m
+++ trunk/dev/workshop/2014-gananoque/stimpat.m
@@ -3,17 +3,23 @@ imdl = mk_common_model('b2c2',16);
 fmdl = imdl.fwd_model;
 show_fem(fmdl, [0, 1])
 %% simulate data
-sp1 = mk_stim_patterns(16, 1,...
+[sp1, meas_sel] = mk_stim_patterns(16, 1,...
     [0 1], [0 1], {'rotate_meas'});
 fmdl.stimulation = sp1;
 himg = mk_image(fmdl,1);
 vv = fwd_solve(himg);
+clf
 plot(vv.meas);
-axis tight
-sp2 = mk_stim_patterns(16, 1,...
+
+[sp2, meas_sel] = mk_stim_patterns(16, 1,...
     [0 1], [0 1], {'no_rotate_meas'});
 fmdl.stimulation = sp2;
 himg = mk_image(fmdl,1);
 vv = fwd_solve(himg);
 hold all
 plot(vv.meas);
+
+real_data = double(zc_h_demo3);
+real_data = real_data(meas_sel);
+plot(real_data/1e7)
+axis tight
