8c29f5f25d7108d12277ae51937cd74080c0f27c
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2014-04-22 15:43:47 +0000

    sensitivity comparison

--- trunk/dev/workshop/2014-gananoque/sensitivity.m
+++ trunk/dev/workshop/2014-gananoque/sensitivity.m
@@ -1,11 +1,24 @@
 % fmdl= ng_mk_cyl_models(3,[7,1],[0.2,0,0.05]); 
+pl= 1; for SKIP = [1,2,6];
+    subplot(1,3,pl); pl=pl+1;
+
 imdl = mk_common_model('f2c2',16);
 fmdl = imdl.fwd_model;
-show_fem(fmdl);
+fmdl.stimulation = mk_stim_patterns(16,1,[0,SKIP],[0,1],{},1);
+
 img = mk_image(fmdl,1);
 J = calc_jacobian(img)';
 sens = img;
-sens.elem_data = J(:,5)./get_elem_volume(fmdl);
+s1 = zeros(size(J,1),1);
+for i=1:size(J,2)
+    s1 = s1 + J(:,i).^2;
+end
+sens.elem_data = sqrt(s1)./get_elem_volume(fmdl);
 
-show_fem(sens);
-eidors_colourbar(sens);
+sens.calc_colours.clim = 3;
+sens.calc_colours.ref_level = 0;
+%show_fem(sens,[0 1]);
+show_slices(sens)
+
+end
+%eidors_colourbar(sens);
