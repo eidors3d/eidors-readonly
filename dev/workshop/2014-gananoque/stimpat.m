0cbeac56255fd17c63696eac8630c574ab18be2d
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2014-04-22 13:34:12 +0000

    update
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@4505 82006732-fb7e-4a22-9f28-86a212cce563

--- dev/workshop/2014-gananoque/stimpat.m
+++ dev/workshop/2014-gananoque/stimpat.m
@@ -2,9 +2,18 @@ load montreal_data_1995
 imdl = mk_common_model('b2c2',16);
 fmdl = imdl.fwd_model;
 show_fem(fmdl, [0, 1])
-
-sp1 = mk_stim_patterns(16, 1, [0 1], [0 1], {'rotate_meas'});
-fmdl.stim = sp1;
+%% simulate data
+sp1 = mk_stim_patterns(16, 1,...
+    [0 1], [0 1], {'rotate_meas'});
+fmdl.stimulation = sp1;
 himg = mk_image(fmdl,1);
 vv = fwd_solve(himg);
+plot(vv.meas);
+axis tight
+sp2 = mk_stim_patterns(16, 1,...
+    [0 1], [0 1], {'no_rotate_meas'});
+fmdl.stimulation = sp2;
+himg = mk_image(fmdl,1);
+vv = fwd_solve(himg);
+hold all
 plot(vv.meas);
