c46977cf386f11eb01bd0301d4236d4687e40407
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2014-04-22 15:25:19 +0000

    plot sensitivity

--- trunk/dev/workshop/2014-gananoque/sensitivity.m
+++ trunk/dev/workshop/2014-gananoque/sensitivity.m
@@ -1,2 +1,11 @@
-fmdl= ng_mk_cyl_models(3,[7,1],[0.2,0,0.05]); 
+% fmdl= ng_mk_cyl_models(3,[7,1],[0.2,0,0.05]); 
+imdl = mk_common_model('f2c2',16);
+fmdl = imdl.fwd_model;
 show_fem(fmdl);
+img = mk_image(fmdl,1);
+J = calc_jacobian(img)';
+sens = img;
+sens.elem_data = J(:,5)./get_elem_volume(fmdl);
+
+show_fem(sens);
+eidors_colourbar(sens);
