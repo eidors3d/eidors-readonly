d41b1d00987cc7f66a9691f0621c3c8903b1c290
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2014-04-27 16:40:06 +0000

    Sparse matrix efficiency tests
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@4527 82006732-fb7e-4a22-9f28-86a212cce563

---
+++ dev/a_adler/testcode/sparse_test.m
@@ -0,0 +1,42 @@
+function sparse_test()
+% this tests the speed of sparse matrix multiplies with a high sparsity level
+
+for sz = [16 32 64 128 256 512 1024 2048 4096]
+rhs = rand(10e4,1e3);
+lhs = sparse(sz,10e4);
+lhs(:,end-(sz-1):end) = eye(sz);
+%lhs(1,end-(sz/2):end) = -1.5;
+fprintf('size(lhs)= %dx%d\n',size(lhs));
+
+tic; for i=1:100
+   ans = lhs * rhs;              % MATLAB MULTIPLY
+end;
+tbase = toc;
+fprintf('MATLAB MULTIPLY  %fs x1.0\n', tbase);
+
+tic; for i=1:100
+   ans = rhs(end-15:end, :);     % DIRECT SELECT
+end;
+t = toc;
+fprintf('DIRECT SELECT    %fs x%0.1f\n', t, tbase/t);
+
+tic; for i=1:100
+   idx= any(lhs);
+   ans = lhs(:,idx)*rhs(idx,:);  % SELECT WITH IDX
+end;
+t = toc;
+fprintf('SELECT WITH IDX  %fs x%0.1f\n', t, tbase/t);
+
+tic; for i=1:100
+   idx= find(any(lhs));
+   ans = lhs(:,idx)*rhs(idx,:);  % SELECT WITH FIND
+end;
+t = toc;
+fprintf('SELECT WITH FIND %fs x%0.1f\n', t, tbase/t);
+end
+
+%TIMES ARE:
+%Elapsed time is 0.866917 seconds.
+%Elapsed time is 0.005294 seconds.
+%Elapsed time is 0.014516 seconds.
+%Elapsed time is 0.014434 seconds.
