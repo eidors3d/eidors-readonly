f0809e62e3e35eb498d4a2377a63aeaac1dcf2a1
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2014-05-02 20:37:57 +0000

    fix noser for movement jacobian

--- trunk/eidors/solvers/inverse/prior_noser.m
+++ trunk/eidors/solvers/inverse/prior_noser.m
@@ -13,6 +13,15 @@ function Reg= prior_noser( inv_model );
 
     img_bkgnd= calc_jacobian_bkgnd( inv_model );
     J = calc_jacobian(img_bkgnd);
+    
+    % if we deal with movement Jacobian, it will be too big, so we chop it
+    if isfield(img_bkgnd.fwd_model,'coarse2fine')
+       n_els = size(img_bkgnd.fwd_model.coarse2fine,2);
+    else
+       n_els = size(img_bkgnd.fwd_model.elems,1);
+    end
+    J = J(:,1:n_els);
+       
 
     exponent= 0.5;
     if isfield(inv_model,'prior_noser');
