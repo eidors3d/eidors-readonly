1fbe3b9de0bc38f6f26cb5a291747d4db4ddbcfb
alistair_boyle <alistair_boyle@82006732-fb7e-4a22-9f28-86a212cce563>
2015-01-16 07:57:20 +0000

    inv_solve_abs: Conjugate Gradient and Gauss-Newton wrappers
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@4658 82006732-fb7e-4a22-9f28-86a212cce563

---
+++ dev/a_boyle/inv_solve_abs_gn.m
@@ -0,0 +1,25 @@
+function img= inv_solve_abs_gn( inv_model, data1);
+%function img= inv_solve_abs_gn( inv_model, data1);
+% INV_SOLVE_ABS_GN
+% This function calls INV_SOLVE_ABS_CORE to find a Gauss-Newton
+% iterative solution.
+%
+% img = inv_solve_abs_gn( inv_model, data1 )
+%   img        => output image data (or vector of images)
+%   inv_model  => inverse model struct
+%   data1      => measurements
+%
+% See INV_SOLVE_ABS_CORE for arguments, options and parameters.
+%
+% (C) 2014 Alistair Boyle
+% License: GPL version 2 or version 3
+% $Id$
+
+%--------------------------
+% UNIT_TEST?
+if isstr(inv_model) && strcmp(inv_model,'UNIT_TEST'); img = do_unit_test; return; end
+
+img = inv_solve_abs_core(inv_model, data1);
+
+function pass = do_unit_test()
+   pass = inv_solve_abs_core('UNIT_TEST', 'inv_solve_abs_gn');
