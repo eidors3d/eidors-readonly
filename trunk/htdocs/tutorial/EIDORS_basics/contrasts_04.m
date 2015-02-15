3bacd5947a47bd20b6919f4ed8790f73334d52f9
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2015-02-15 20:49:43 +0000

    larger range of sims

--- trunk/htdocs/tutorial/EIDORS_basics/contrasts_04.m
+++ trunk/htdocs/tutorial/EIDORS_basics/contrasts_04.m
@@ -2,7 +2,7 @@ sz = 5; img_idx = 'b';
 for ellipse_x = [0.5,1,2];
    img = contrasts_04_modeller( sz, ellipse_x); 
    targ = img.fwd_model.mat_idx{1};
-   for contrast = linspace( -2,2,5);
+   for contrast = linspace( -3,3,7);
       img.elem_data( targ ) = exp(contrast);
       vv = fwd_solve(img);
       img_name = sprintf('04%c',img_idx); img_idx= img_idx+1;
