ff04379b7c6d684b88715d25e265da2f4fd0c2b4
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2015-02-15 19:54:13 +0000

    contrasts and perturbations
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@4676 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/EIDORS_basics/contrasts_02.m
+++ htdocs/tutorial/EIDORS_basics/contrasts_02.m
@@ -3,10 +3,10 @@ img.fwd_model.system_mat = @system_mat_1st_order;
 [img.fwd_model.electrode(:).z_contact] = deal(1000); % Large
 
 img.fwd_solve.get_all_meas = 1;
-vh = fwd_solve(img);
+vv = fwd_solve(img);
 imgv= rmfield(img,'elem_data');
-imgv.node_data = vh.volt;
-imgv.calc_colours.ref_level = mean(vh.volt);
+imgv.node_data = vv.volt;
+imgv.calc_colours.ref_level = mean(vv.volt);
 show_fem(imgv);
 
 print_convert contrasts_02a.png
