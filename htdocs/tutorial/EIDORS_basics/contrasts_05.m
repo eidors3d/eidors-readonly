bb04563dc65f2c168bf04950e35d85dde963adc4
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2015-02-15 18:33:41 +0000

    Start code for contrasts with subtracted background
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@4675 82006732-fb7e-4a22-9f28-86a212cce563

---
+++ htdocs/tutorial/EIDORS_basics/contrasts_05.m
@@ -0,0 +1,12 @@
+sz = 5; img_idx = 'b';
+for ellipse_x = [0.5,1,2];
+   img = contrasts_04_modeller( sz, ellipse_x); 
+   targ = img.fwd_model.mat_idx{1};
+   for contrast = linspace( -2,2,5);
+      img.elem_data( targ ) = exp(contrast);
+      vh = fwd_solve(img);
+      img_name = sprintf('04%c',img_idx); img_idx= img_idx+1;
+      contrasts_03;
+   end
+end
+
