b6152fd245d7a16dcecad0920cc7cb5abf3c2ba3
alistair_boyle <alistair_boyle@82006732-fb7e-4a22-9f28-86a212cce563>
2015-02-16 04:21:13 +0000

    stim_meas_list: stim_meas_list can now reverse itself and give back an array of stim/meas pairs
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@4678 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/models/stim_meas_list.m
+++ eidors/models/stim_meas_list.m
@@ -20,7 +20,20 @@ function [stim, meas_sel]= stim_meas_list( sp_mp , Nelec, current, gain);
 % (C) 2010 Andy Adler. License: GPL version 2 or version 3
 % $Id$
 
-if isstr(sp_mp) && strcmp(sp_mp,'UNIT_TEST'); do_unit_test; return; end
+if isstr(sp_mp) && strcmp(sp_mp,'UNIT_TEST'); stim=do_unit_test; return; end
+
+if isstruct(sp_mp)
+  stim = sp_mp;
+  meas_sel = [];
+  nst = length(stim);
+  nvt = 0;
+  for i = 1:nst;
+    nvt = nvt + size(stim(i).stim_pattern, 2) * size(stim(i).meas_pattern, 1);
+  end
+  stim = flatten_stim(stim, nst, nvt);
+  stim = stim(:,[2 1 4 3]);
+  return
+end
 
 if nargin <2; Nelec = max(sp_mp(:)); end
 if nargin <3; current = 1;           end
@@ -49,10 +62,58 @@ for i=1:Npat
    end
 end
 
-function  do_unit_test
+% take in a stim/meas struct
+% return a matrix of stim/meas pairs, per row with drive current 'i' and measurement gain 'g' calculated
+% [ +s -s +m -m i g ]
+function stim_flat = flatten_stim(stim, nst, nvt)
+  stim_flat = zeros(nvt, 6);
+  idx = 1;
+  % TODO calculate 'gain' when it matched (m+ == - m-)
+  % TODO calculate 'gain' when it is unmatched (m+ ~= m-)
+  % TODO calculate 'current' when it matched (s+ == - s-)
+  % TODO calculate 'current' when it is unmatched (s+ ~= s-)
+  for i = 1:nst
+      nmp= size(stim(i).meas_pattern, 1); % number of measurement patterns for this stim pair
+      [sp, jnk, spv]= find(stim(i).stim_pattern>0);
+      [sn, jnk, snv]= find(stim(i).stim_pattern<0);
+      [jnk, mp, mpv]= find(stim(i).meas_pattern>0); mp = mp(jnk);
+      [jnk, mn, mnv]= find(stim(i).meas_pattern<0); mn = mn(jnk);
+      % expand s+/s- to match the size of m+/m-
+      sp  = zeros(nmp,1)+sp;
+      sn  = zeros(nmp,1)+sn;
+      spv = zeros(nmp,1)+spv;
+      snv = zeros(nmp,1)+snv;
+      stim_flat(idx:idx+nmp-1,:) = ...
+        [ sp sn ... % stim pairs
+            mp mn spv mpv];  % meas pairs
+      idx = idx + nmp;
+  end
+
+function  ok = do_unit_test
+   ok = 1;
    imdl = mk_common_model('a2c0',16);
    img = mk_image(imdl);
-   img.fwd_model.stimulation = stim_meas_list([1,2,3,4;1,2,4,5],16);
+   list_in = [1,2,3,4;1,2,4,5];
+   img.fwd_model.stimulation = stim_meas_list(list_in,16);
+   list_out = stim_meas_list(img.fwd_model.stimulation);
+   ok = match(list_in, list_out, ok, 'pattern#1');
    vh = fwd_solve(img);
-   img.fwd_model.stimulation = stim_meas_list([6,7,3,4;1,2,4,5],16);
+   list_in = [6,7,3,4;1,2,4,5];
+   img.fwd_model.stimulation = stim_meas_list(list_in,16);
+   list_out = stim_meas_list(img.fwd_model.stimulation);
+   ok = match(list_in, list_out, ok, 'pattern#2');
    vh = fwd_solve(img);
+   if ok
+      disp PASS
+   else
+      disp FAIL
+   end
+
+function ok= match( pat1, pat2, ok, desc)
+    okl =  all(pat1(:) == pat2(:));
+    if okl
+      fprintf('stim_meas_list: pass -- %s\n',desc);
+    else
+      fprintf('stim_meas_list: fail -- %s\n',desc);
+      ok = 0;
+    end
