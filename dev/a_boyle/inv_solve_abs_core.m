cea8cf95212e9d0bd7c6d22dea2000594232534e
alistair_boyle <alistair_boyle@82006732-fb7e-4a22-9f28-86a212cce563>
2015-03-15 16:29:32 +0000

    minor: clean up iteration count at exit
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@4714 82006732-fb7e-4a22-9f28-86a212cce563

--- dev/a_boyle/inv_solve_abs_core.m
+++ dev/a_boyle/inv_solve_abs_core.m
@@ -341,8 +341,9 @@ if isfield(inv_model, 'rec_model')
   img.fwd_model = inv_model.rec_model;
 end
 if opt.verbose > 1
-   fprintf('  %d fwd_solves required for this solution in %d iterations\n', ...
-           opt.fwd_solutions, k);
+   if k==2; itrs=''; else itrs='s'; end
+   fprintf('  %d fwd_solves required for this solution in %d iteration%s\n', ...
+           opt.fwd_solutions, k-1, itrs);
 end
 % convert data for output
 img = map_img(img, opt.elem_output);
