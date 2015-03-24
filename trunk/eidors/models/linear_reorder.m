b2ce620381e85894aa1f8ffb29f60bca6263bf7f
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2015-03-24 15:41:33 +0000

    refactor linear_reorder before starting tests to speed up

--- trunk/eidors/models/linear_reorder.m
+++ trunk/eidors/models/linear_reorder.m
@@ -1,5 +1,5 @@
 function [fwd_model] = linear_reorder(fwd_model,ccw)
-%function [fwd_model] = linear_reorder(fwd_model,ccw)
+%[fwd_model] = linear_reorder(fwd_model,ccw)
 %Function to reorder local nodes (counter)clockwise per element
 %Input:  - fwd_model structure
 %        - ccw = -1 (default) - counter clockwise OR 1 - clockwise   
@@ -16,42 +16,45 @@ if (nargin==1)
     ccw=-1; %Default specify counter-clockwise nodes
 end
 
-nodecoords = fwd_model.nodes; %Cache coorindates of nodes [nnodesxnodedim]
-elementnodes = fwd_model.elems; %Cache matrix of elements [eletotalxelenode]
-
-eletotal = size(elementnodes,1); %No. of elements
-elenode = size(elementnodes,2); %No. of nodes per element
-nodedim = size(fwd_model.nodes,2);
-midpoint = mean(fwd_model.nodes(unique(fwd_model.elems),:));
-
-for e=1:eletotal; %Loop over all elements
-    %Row vector of global nodes [1xelenode]
-    enodes = elementnodes(e,:); 
-    %Matrix of nodal positions [elenodexdim] (Linear dimension==elenode-1) 
-    nd = nodecoords(enodes,:);
-    
-    % surface meshes need tweaking. Use the midpoint to fit the 3D formula.
-    % This will not work for non simply-connected surfaces.
-    if elenode == 3 && nodedim == 3
-       nd = [nd; midpoint]; 
-    end
-    %Calculate area of triangle/volume defined by the elements nodes
-    %In 2D this is area and in 3D this is volume
-    area= det([ones(length(nd),1),nd]);
-    areasign=sign(area); 
-    
-    %If sign is (pos) neg swap two nodes (last two will suffice..)
-    if(areasign == ccw) %Swap last two entries of enodes 
-        temp=enodes(elenode-1);
-        enodes(elenode-1)=enodes(elenode);
-        enodes(elenode) = temp;
-        %elementnodes(e,:)=enodes; %Put back into elementnodes matrix
-    end
-    elementnodes(e,:)=enodes; %Put enodes back into elementnodes matrix
+fwd_model = old_do_reorder(fwd_model, ccw);
 end
-fwd_model.elems=elementnodes; %Reassign fwd_model.elems
 
-%End LinearReorder function
+function fwd_model = old_do_reorder(fwd_model, ccw)
+   nodecoords = fwd_model.nodes; %Cache coorindates of nodes [nnodesxnodedim]
+   elementnodes = fwd_model.elems; %Cache matrix of elements [eletotalxelenode]
+
+   eletotal = size(elementnodes,1); %No. of elements
+   elenode = size(elementnodes,2); %No. of nodes per element
+   nodedim = size(fwd_model.nodes,2);
+   midpoint = mean(fwd_model.nodes(unique(fwd_model.elems),:));
+
+   for e=1:eletotal; %Loop over all elements
+       %Row vector of global nodes [1xelenode]
+       enodes = elementnodes(e,:); 
+       %Matrix of nodal positions [elenodexdim] (Linear dimension==elenode-1) 
+       nd = nodecoords(enodes,:);
+       
+       % surface meshes need tweaking. Use the midpoint to fit the 3D formula.
+       % This will not work for non simply-connected surfaces.
+       if elenode == 3 && nodedim == 3
+          nd = [nd; midpoint]; 
+       end
+       %Calculate area of triangle/volume defined by the elements nodes
+       %In 2D this is area and in 3D this is volume
+       area= det([ones(length(nd),1),nd]);
+       areasign=sign(area); 
+       
+       %If sign is (pos) neg swap two nodes (last two will suffice..)
+       if(areasign == ccw) %Swap last two entries of enodes 
+           temp=enodes(elenode-1);
+           enodes(elenode-1)=enodes(elenode);
+           enodes(elenode) = temp;
+           %elementnodes(e,:)=enodes; %Put back into elementnodes matrix
+       end
+       elementnodes(e,:)=enodes; %Put enodes back into elementnodes matrix
+   end
+   fwd_model.elems=elementnodes; %Reassign fwd_model.elems
+end
 
 function do_unit_test
    imdl = mk_common_model('n3r2',[16,2]); fmdl = imdl.fwd_model;
@@ -69,24 +72,32 @@ function do_unit_test
    fm1 = linear_reorder(fmdl);
    vol1= test_linear_reorder( fm1 );
    ok = all(vol1>0);
-   fprintf('test1 2d: OK=%d\n',ok);
+   fprintf('test3 2d: OK=%d\n',ok);
 
    fm1 = linear_reorder(fmdl, 1);
    vol1= test_linear_reorder( fm1 );
    ok = all(vol1<0);
-   fprintf('test2 2d: OK=%d\n',ok);
+   fprintf('test4 2d: OK=%d\n',ok);
 
+   imdl = mk_common_model('d3cr',[16,2]); fmdl = imdl.fwd_model;
+tic
+   vol1= test_linear_reorder( fmdl );
+toc
+   ok = all(vol1<0);
+   fprintf('test5 3d: OK=%d\n',ok);
+end
    
 
 function vol = test_linear_reorder(fwd_model)
 
-dim=size(fwd_model.nodes,2); elee=size(fwd_model.elems,1);
+   dim=size(fwd_model.nodes,2); elee=size(fwd_model.elems,1);
 
-for e=1:elee
-    b=fwd_model.elems(e,:);  [v]=fwd_model.nodes(b,:);
-        for i=1:dim
-            vv1(i,:)=v(i+1,:)-v(1,:);
-        end
-    vol(e)=det([vv1]);
+   for e=1:elee
+       b=fwd_model.elems(e,:);  [v]=fwd_model.nodes(b,:);
+           for i=1:dim
+               vv1(i,:)=v(i+1,:)-v(1,:);
+           end
+       vol(e)=det([vv1]);
+   end
 end
 
