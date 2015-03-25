d512c49ea5cf1589ad9ea367e41430fe97c30392
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2015-03-25 14:41:43 +0000

    Fixed tests and added comments to clarify the testing
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@4782 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/tests/test_3d_resistor.m
+++ eidors/tests/test_3d_resistor.m
@@ -6,7 +6,7 @@ ww=1*2; % width
 hh=1*3; % height
 conduc= .13;  % conductivity in Ohm-meters
 current= 4;  % Amps
-z_contact= 1e-2;
+z_contact= 1e-1;
 scale = .46;
 mdl= eidors_obj('fwd_model','3D rectangle');
 nn=0;
@@ -45,13 +45,6 @@ mdl.electrode= elec;
 show_fem(mdl);
 mdl = mdl_normalize(mdl,0);
 
-% analytical solution
-Block_R =  ll / ww / hh / scale/ conduc;
-Contact_R = z_contact;
-R = Block_R + 2*Contact_R;
-
-V= current*R;
-fprintf('Solver %s: %f\n', 'analytic', V);
 
 
 mdl.solve = @fwd_solve_1st_order;
@@ -65,9 +58,27 @@ img= eidors_obj('image','3D rectangle', ...
 fsol= fwd_solve(img);
 fprintf('Solver %s: %f\n', fsol.name, fsol.meas);
 
+% analytical solution
+Block_R =  ll / ww / hh / scale/ conduc;
+Contact_R = z_contact/(ww*hh)/scale^2;
+% Contact R reflects z_contact / (width/scale)^2. Here we need to use
+%  the scale, since this is not reflected in the size of the
+%  FEM as created by the grid. This is different to the test_2d_resistor,
+%  where the FEM is created scaled, so that the ww
+%  don't need to be scaled by the scale parameter.
+R = Block_R + 2*Contact_R;
 
-mdl.solve = @np_fwd_solve;
-mdl.system_mat = @np_calc_system_mat;
+V= current*R;
+fprintf('Solver %s: %f\n', 'analytic', V);
+fprintf('Solver %s: %f\n', 'analytic (no z_contact)', V - 2*Contact_R*current);
+
+if 0 % OLD CODE
+   mdl.solve = @np_fwd_solve;
+   mdl.jacobian = @np_calc_jacobian;
+else
+   mdl.solve = @fwd_solve_1st_order;
+   mdl.jacobian= @jacobian_adjoint;
+end
 mdl.misc.perm_sym= '{n}';
 mdl.normalize_measurements = 0;
 img= eidors_obj('image','3D rectangle', ...
@@ -79,14 +90,19 @@ fprintf('Solver %s: %f\n', fsol.name, fsol.meas);
 
 
 % NOW CALCULATE THE ANALYTICAL JACOBIAN
-mdl.solve = @np_fwd_solve;
-mdl.jacobian = @np_calc_jacobian;
+if 0 % OLD CODE
+   mdl.solve = @np_fwd_solve;
+   mdl.jacobian = @np_calc_jacobian;
+else
+   mdl.solve = @fwd_solve_1st_order;
+   mdl.jacobian= @jacobian_adjoint;
+end
 img.fwd_model = mdl;
 img.elem_data= ones(n_el,1) * conduc ;
 Jnp= calc_jacobian(img);
 
 mdl.jacobian = @jacobian_perturb;
-Jp1= calc_jacobian(mdl,img);
+Jp1= calc_jacobian(img);
 
 img.elem_data= ones(n_el,1) * conduc ;
 Jp2= zeros(size(Jnp));
@@ -100,6 +116,8 @@ end
    
 mdl.solve = @fwd_solve_1st_order;
 mdl.jacobian = @jacobian_adjoint;
-Jaa= calc_jacobian(mdl,img);
+Jaa= calc_jacobian(img);
 
-[Jaa;Jnp;Jp1;Jp2]
+fprintf('Jacobians: Cols by Jaa, Jnp, Jp1, Jp2\n');
+JJ = [Jaa;Jnp;Jp1;Jp2];
+disp(JJ(:,1:6))
