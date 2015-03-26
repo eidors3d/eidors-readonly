b9a60bf3812813254c7e3889963748ad61d898e1
bgrychtol-ipa <bgrychtol-ipa@82006732-fb7e-4a22-9f28-86a212cce563>
2015-03-26 14:48:26 +0000

    working 3DGREIT
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@4787 82006732-fb7e-4a22-9f28-86a212cce563

---
+++ dev/pamb/calc_GREIT_RM.m
@@ -0,0 +1,82 @@
+function [RM, PJt, M] = calc_GREIT_RM(vh,vi, xyc, radius, weight, options)
+% CALCULATE GREIT reconstruction matrix
+%   RM= calc_GREIT_RM(vh,vi, xyc, radius, weight, normalize)
+% 
+% Input:
+%   vh     = homogeneous (reference) training measurements 
+%   vi     = inhomogeneous training measurements 
+%   xyc    = x,y position of targets 2xN
+%   radius = requested resolution matrix
+%      if scalar - apply resolution to all images:  recommend 0.25 for 16 elecs
+%   weight = weighting matrix
+%      if scalar   = weighting of noise vs signal
+%      if 32^2 x N = weighting of each image output
+%   options.normalize = 
+%            0 -> regular difference EIT
+%            1 -> normalized difference EIT
+%   options.meshsz = [xmin xmax ymin ymax] size of mesh
+%      defaults to [-1 1 -1 1]
+%   options.imgsz  = [xsz ysz] size of the reconstructed image in pixels
+%   options.noise_covar [optional]
+%      covariance matrix of data noise
+%   options.desired_solution_fn
+%      specify a function to calculate the desired image. 
+%      It must have the signature:
+%      D = my_function( xyc, radius, options);
+% 
+%
+% (C) 2009 Andy Adler. Licenced under GPL v2 or v3
+% $Id$
+
+   if ~isstruct(options)
+       options.normalize = options;
+   end
+   opt = parse_options(options);
+
+   if opt.normalize
+      Y = vi./(vh*ones(1,size(vi,2))) - 1; 
+   else
+      Y = vi - (vh*ones(1,size(vi,2)));
+   end
+   if ~isfield(opt, 'desired_solution_fn')
+      D = default_GREIT_desired_soln( xyc, radius, opt);
+   else
+      D = feval(opt.desired_solution_fn, xyc, radius, opt);
+   end
+
+   if size(weight)==[1,1] % Can't use isscalar for compatibility with M6.5
+       [RM, PJt, M] = calc_RM(Y,D,weight, opt);
+   else
+       error('not coded yet');
+   end
+
+function [RM, PJt, M] = calc_RM(Y, D, noiselev, opt)
+
+   noiselev = noiselev * mean(abs(Y(:)));
+   % Desired soln for noise is 0
+   N_meas = size(Y,1);
+
+   % This implements RM = D*Y'/(J*Sx*J + Sn);
+   Sn = speye(N_meas) .* opt.noise_covar; % Noise covariance
+   PJt= D*Y';
+   M  = (Y*Y' + noiselev^2*Sn);
+   RM =  left_divide(M',PJt')';    %PJt/M;
+   % This implements RM = D*Y'/(Y*Y');
+   if 0
+      Y = [Y, noiselev*eye(N_meas)];
+      D = [D,          zeros(size(D,1),N_meas)];
+
+      RMold = D*Y'/(Y*Y');
+      if norm(RM-RMold,'fro')/norm(RM,'fro') > 1e-10; warning('not OK'); end
+   end
+
+
+
+   function opt = parse_options(opt)
+       if ~isfield(opt, 'normalize'), opt.normalize = 1; end
+       if ~isfield(opt, 'meshsz'),    opt.meshsz = [-1 1 -1 1]; end
+       if ~isfield(opt, 'imgsz'),     opt.imgsz = [32 32]; end
+       if ~isfield(opt, 'noise_covar'),
+                     opt.noise_covar = 1;
+       end
+%   options.data_covariance [optional]
---
+++ dev/pamb/greit_test.m
@@ -0,0 +1,42 @@
+fmdl= ng_mk_cyl_models([3,2,.4],[16,1,2],[.1,0,.025]);
+show_fem(fmdl);
+fmdl.stimulation = mk_stim_patterns(16,2,'{ad}','{ad}');
+imdl = select_imdl(fmdl,{'Basic GN dif'});
+%%
+opt.imgsz = [32 32 3*32];
+% opt.zvec = [0 .75:.5:2.25 3];
+opt.cube_voxels= true;
+opt.prune_model= false;
+imdl = mk_voxel_volume(imdl,opt);
+
+% prune manually
+rmdl = imdl.rec_model;
+D = sqrt(sum(rmdl.nodes(:,1:2).^2,2)) <= 2;
+E = all(D(reshape(rmdl.elems',4*6,[])'),2);
+E2= logical(kron(E,true(6,1)));
+rmdl.elems = rmdl.elems(E2,:);
+rmdl.coarse2fine = rmdl.coarse2fine(E2,E);
+rmdl = rmfield(rmdl, 'boundary');
+rmdl.inside = E2;
+imdl.rec_model = rmdl;
+
+%%
+[x, y, z] = ndgrid(linspace(-2,2,20),linspace(-2,2,20),.5:.25:2.5);
+idx = ( x(:).^2 + y(:).^2 ) < 1.8^2;
+gopt.distr = [x(idx) y(idx) z(idx)]';
+gopt.desired_solution_fn = @new_desired_soln;
+% gopt.target_size
+imdl = mk_GREIT_model(imdl,0.15,0.2, gopt);
+%%
+imgh = mk_image(fmdl,1);
+vh = fwd_solve(imgh);
+%%
+imgi = imgh;
+select_fun = inline('(x-.5).^2+(y-.5).^2+(z-1).^2<=0.1^2','x','y','z');
+imgi.elem_data = imgh.elem_data + elem_select(fmdl,select_fun);
+vi = fwd_solve(imgi);
+%%
+rimg = inv_solve(imdl,vh,vi);
+show_fem(rimg);
+
+% TODO: desired solution cannot assume equally spaced points
