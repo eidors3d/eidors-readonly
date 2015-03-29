07c4514bcca05d9162d769581d85c5a9421bdb00
bgrychtol-ipa <bgrychtol-ipa@82006732-fb7e-4a22-9f28-86a212cce563>
2015-03-29 11:55:10 +0000

    improve handling of image arrays

--- trunk/eidors/algorithms/calc_hm_set.m
+++ trunk/eidors/algorithms/calc_hm_set.m
@@ -7,8 +7,8 @@ function hm_img = calc_hm_set(img,frac)
 % (C) 2008 Andy Adler. Licenced under GPL v2 or v3
 % $Id$
 
-sz = size(img,1);
-[x,y]=meshgrid(linspace(-1,1,sz),linspace(-1,1,sz)); map = x.^2+y.^2<1.1;
+sz = size(img);
+[x,y]=meshgrid(linspace(-1,1,sz(1)),linspace(-1,1,sz(2))); map = x.^2+y.^2<1.1;
 
 hm_img = logical(zeros(size(img)));
 for i=1:size(img,3);
