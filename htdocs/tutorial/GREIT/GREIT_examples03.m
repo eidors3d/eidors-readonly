5614f4446d0f10b1fee1046d6f2c7ffaf94738cd
bgrychtol-ipa <bgrychtol-ipa@82006732-fb7e-4a22-9f28-86a212cce563>
2015-03-29 15:17:16 +0000

    update GREIT tutorials to current best practice
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@4823 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/GREIT/GREIT_examples03.m
+++ htdocs/tutorial/GREIT/GREIT_examples03.m
@@ -4,10 +4,14 @@
 show_fem( inv_solve( i_gr, vh, vn) ); axis equal;
 print_convert 'GREIT_examples03a.png' '-density 50';
 
+% current GREIT
+show_fem( inv_solve( i_grc, vh, vn) ); axis equal;
+print_convert 'GREIT_examples03b.png' '-density 50';
+
 % Sheffield Backprojection
 show_fem( inv_solve( i_bp, vh, vn) ); axis equal;
-print_convert 'GREIT_examples03b.png' '-density 50';
+print_convert 'GREIT_examples03c.png' '-density 50';
 
 % 2D Gauss Newton Inverse
 show_fem( inv_solve( i_gn, vh, vn) ); axis equal;
-print_convert 'GREIT_examples03c.png' '-density 50';
+print_convert 'GREIT_examples03d.png' '-density 50';
