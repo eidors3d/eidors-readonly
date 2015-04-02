3b41933c256e20ba7b0baf55adbb9ce42f405d1f
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2015-04-02 15:34:44 +0000

    Fix calling of eidors_colourbar. Matlab makes STUPID STUPID changes to the graphics engine

--- trunk/htdocs/tutorial/data_structures/eidors_colours02.m
+++ trunk/htdocs/tutorial/data_structures/eidors_colours02.m
@@ -1,20 +1,20 @@
 % Show EIDORS colours $Id$
-subplot(221);
+clf; subplot(221); clf;
 img1= img;
-img1.calc_colours.cb_shrink_move = [0.5,0.8,.02];
+img1.calc_colours.cb_shrink_move = [0.5,0.8,-.10];
 
-subplot(221);
+clf; subplot(221);
 show_fem(img1,1);
 axis equal; axis off; axis tight; 
 print_convert eidors_colours02a.png '-density 75'
 
-subplot(221);
+clf; subplot(221);
 img1.calc_colours.greylev= -0.35;
 show_fem(img1,1);
 axis equal; axis off; axis tight; 
 print_convert eidors_colours02b.png '-density 75'
 
-subplot(221);
+clf; subplot(221);
 img1.calc_colours.greylev= 0.35;
 show_fem(img1,1);
 axis equal; axis off; axis tight; 
--- trunk/htdocs/tutorial/data_structures/eidors_colours04.m
+++ trunk/htdocs/tutorial/data_structures/eidors_colours04.m
@@ -1,19 +1,19 @@
 % Show EIDORS colours $Id$
-subplot(221); img1= img;
-img1.calc_colours.cb_shrink_move = [0.5,0.8,.02];
+clf; subplot(221); img1= img;
+img1.calc_colours.cb_shrink_move = [0.5,0.8,-.10];
 
-subplot(221);
+clf; subplot(221);
 show_fem(img1,1);
 axis equal; axis off; axis tight;
 print_convert eidors_colours04a.png '-density 75'
 
-subplot(221);
+clf; subplot(221);
 img1.calc_colours.sat_adj= 0.99;
 show_fem(img1,1);
 axis equal; axis off; axis tight;
 print_convert eidors_colours04b.png '-density 75'
 
-subplot(221);
+clf; subplot(221);
 img1.calc_colours.sat_adj= 0.8;
 show_fem(img1,1);
 axis equal; axis off; axis tight;
