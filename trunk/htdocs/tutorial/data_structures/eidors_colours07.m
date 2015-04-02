f8c35b52ccd27cf2cbee147d2feea3d5bceb8846
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2015-04-02 15:34:51 +0000

    more fixes to work around matlab graphics changes

--- trunk/htdocs/tutorial/data_structures/eidors_colours07.m
+++ trunk/htdocs/tutorial/data_structures/eidors_colours07.m
@@ -1,7 +1,7 @@
 % Show lung images $Id$
 img = inv_solve(imdl, zc_resp(:,1), zc_resp(:,20));
 img.calc_colours.ref_level= 0;
-img.calc_colours.cb_shrink_move = [0.5,0.8,.02];
+img.calc_colours.cb_shrink_move = [0.5,0.8,-.10];
 
 clf; subplot(221);
 img.calc_colours.greylev = 0.01;
@@ -9,19 +9,19 @@ show_fem(img,[1,1]);
 axis equal; axis off; axis tight;
 print_convert eidors_colours07a.png '-density 75'
 
-subplot(221);
+clf; subplot(221);
 img.calc_colours.greylev =  0.3;
 show_fem(img,[1,1]);
 axis equal; axis off; axis tight;
 print_convert eidors_colours07b.png '-density 75'
 
-subplot(221);
+clf; subplot(221);
 img.calc_colours.greylev = -0.01;
 show_fem(img,[1,1]);
 axis equal; axis off; axis tight;
 print_convert eidors_colours07c.png '-density 75'
 
-subplot(221);
+clf; subplot(221);
 img.calc_colours.cmap_type = 'draeger';
 show_fem(img,[1,1]);
 axis equal; axis off; axis tight;
