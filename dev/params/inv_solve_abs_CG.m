b1b6004145397774a556e820d4128b753f64af70
alistair_boyle <alistair_boyle@82006732-fb7e-4a22-9f28-86a212cce563>
2015-04-11 02:18:54 +0000

    rename files for consistent case with trunk; part 2 of 2
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@4881 82006732-fb7e-4a22-9f28-86a212cce563

--- dev/params/inv_solve_abs_CG.m
+++ dev/params/inv_solve_abs_CG.m
@@ -1,10 +1,10 @@
-function img= inv_solve_abs_cg( inv_model, data1);
-%function img= inv_solve_abs_cg( inv_model, data1);
+function img= inv_solve_abs_CG( inv_model, data1);
+%function img= inv_solve_abs_CG( inv_model, data1);
 % INV_SOLVE_ABS_CG
 % This function calls INV_SOLVE_ABS_CORE to find a Conjugate-Gradient
 % iterative solution.
 %
-% img = inv_solve_abs_cg( inv_model, data1 )
+% img = inv_solve_abs_CG( inv_model, data1 )
 %   img        => output image data (or vector of images)
 %   inv_model  => inverse model struct
 %   data1      => measurements
@@ -29,7 +29,8 @@ end
 
 img = inv_solve_abs_core(inv_model, data1);
 
-function dx = CG_update(J, W, hp2RtR, dv, de)
+function dx = CG_update(J, W, hp2, RtR, dv, de)
+%  hp2RtR = hp2*RtR;
 %  % the actual update
 %  dx = (J'*W*J + hp2RtR)\(J'*dv + hp2RtR*de);
   dx = J'*dv;
@@ -78,13 +79,5 @@ function beta = beta_reset_polak_ribiere(dx_k, dx_km1, sx_km1)
     beta = 0;
   end
 
-% Dai-Yuan
-% cite: http://en.wikipedia.org/wiki/Nonlinear_conjugate_gradient_method
-%                dx_k^T    dx_k
-% beta = - -------------------------------
-%          s_{k-1}^T ( dx_k - dx_{k-1} )
-function beta = beta_dai_yuan(dx_k, dx_km1, sx_km1)
-  beta = -dx_k'*dx_k/(sx_km1'*(dx_k-dx_km1));
-
 function pass = do_unit_test()
-   pass = inv_solve_abs_core('UNIT_TEST', 'inv_solve_abs_cg');
+   pass = inv_solve_abs_core('UNIT_TEST', 'inv_solve_abs_CG');
