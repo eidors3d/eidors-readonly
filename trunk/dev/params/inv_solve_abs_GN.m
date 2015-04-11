fef0d1c7b54bc5ff4d3bbf32b149f71254ec073c
alistair_boyle <alistair_boyle@82006732-fb7e-4a22-9f28-86a212cce563>
2015-04-11 02:18:54 +0000

    rename files for consistent case with trunk; part 2 of 2

--- trunk/dev/params/inv_solve_abs_GN.m
+++ trunk/dev/params/inv_solve_abs_GN.m
@@ -1,10 +1,10 @@
-function img= inv_solve_abs_gn( inv_model, data1);
-%function img= inv_solve_abs_gn( inv_model, data1);
+function img= inv_solve_abs_GN( inv_model, data1);
+%function img= inv_solve_abs_GN( inv_model, data1);
 % INV_SOLVE_ABS_GN
 % This function calls INV_SOLVE_ABS_CORE to find a Gauss-Newton
 % iterative solution.
 %
-% img = inv_solve_abs_gn( inv_model, data1 )
+% img = inv_solve_abs_GN( inv_model, data1 )
 %   img        => output image data (or vector of images)
 %   inv_model  => inverse model struct
 %   data1      => measurements
@@ -22,4 +22,4 @@ if isstr(inv_model) && strcmp(inv_model,'UNIT_TEST'); img = do_unit_test; return
 img = inv_solve_abs_core(inv_model, data1);
 
 function pass = do_unit_test()
-   pass = inv_solve_abs_core('UNIT_TEST', 'inv_solve_abs_gn');
+   pass = inv_solve_abs_core('UNIT_TEST', 'inv_solve_abs_GN');
