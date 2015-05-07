75a66966fc9d525f1e336b5107ef56f3ce286b52
alistair_boyle <alistair_boyle@82006732-fb7e-4a22-9f28-86a212cce563>
2015-05-07 17:54:23 +0000

    examples and unit tests for inv_solve_abs_{CG,GN}
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@4915 82006732-fb7e-4a22-9f28-86a212cce563

--- dev/a_boyle/inv_solve_abs_GN.m
+++ dev/a_boyle/inv_solve_abs_GN.m
@@ -9,6 +9,16 @@ function img= inv_solve_abs_GN( inv_model, data1);
 %   inv_model  => inverse model struct
 %   data1      => measurements
 %
+% Example:
+%  imdl=mk_common_model('a2c');
+%  imdl.reconst_type='absolute'; % ***
+%  imdl.solve=@inv_solve_abs_GN; % ***
+%  fimg=mk_image(imdl,1);
+%  fimg.elem_data(5:10)=1.1;
+%  vi=fwd_solve(fimg);
+%  img=inv_solve(imdl,vi); % ***
+%  show_fem(img,1);
+%
 % See INV_SOLVE_ABS_CORE for arguments, options and parameters.
 %
 % (C) 2014 Alistair Boyle
@@ -71,4 +81,16 @@ function imdl = deprecate_imdl_opt(imdl,opt)
    imdl = rmfield(imdl, opt);
 
 function pass = do_unit_test()
-   pass = inv_solve_abs_core('UNIT_TEST', 'inv_solve_abs_GN');
+   pass=1;
+   imdl=mk_common_model('a2c');
+   imdl.reconst_type='absolute'; % ***
+   imdl.solve=@inv_solve_abs_GN; % ***
+   fimg=mk_image(imdl,1);
+   fimg.elem_data(5:10)=1.1;
+   vi=fwd_solve(fimg);
+   img=inv_solve(imdl,vi); % ***
+   clf; subplot(121); show_fem(fimg,1); title('forward model');
+        subplot(122); show_fem(img,1);  title('reconstruction');
+   try unit_test_cmp('fwd vs. reconst', fimg.elem_data, img.elem_data, 0.08);
+   catch me; disp(me.message); pass=0; end
+%   pass = pass & inv_solve_abs_core('UNIT_TEST', 'inv_solve_abs_GN');
