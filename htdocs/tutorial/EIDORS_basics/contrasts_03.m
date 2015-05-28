0ec8a81da721179677b60816cf8cbba41bc8fcaf
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2015-05-28 20:50:34 +0000

    Cleaner contrast pictures -- see streamlines better
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@5058 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/EIDORS_basics/contrasts_03.m
+++ htdocs/tutorial/EIDORS_basics/contrasts_03.m
@@ -4,9 +4,16 @@
    imgc.fwd_model.mdl_slice_mapper.level = [inf,inf,0];
    imgc.calc_colours.ref_level = 1;
    q = show_current(imgc,vv.volt);
-   hh=show_fem(imgc);
+
+   fm1 = img.fwd_model;
+   fm1.elems = fm1.elems(fm1.mat_idx{1},:);
+   bdy= find_boundary(fm1);
+
+   hh=show_fem(img.fwd_model);
    set(hh,'EdgeColor',[1,1,1]*.75);
    hold on;
+   plot( reshape(fm1.nodes(bdy,1),size(bdy))', ...
+         reshape(fm1.nodes(bdy,2),size(bdy))','k','LineWidth',2);
 
    sy = linspace(-2,2,20); sx= 0*sy - sz;
    hh=streamline(q.xp,q.yp, q.xc, q.yc,-sx,sy); set(hh,'Linewidth',2);
