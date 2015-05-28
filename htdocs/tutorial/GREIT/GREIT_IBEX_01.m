b3cdccfac9f04e0c4d767f3295f9d27f7757ffaa
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2015-05-28 16:42:52 +0000

    Save EIDORS images to the IBEX software format
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@5053 82006732-fb7e-4a22-9f28-86a212cce563

---
+++ htdocs/tutorial/GREIT/GREIT_IBEX_01.m
@@ -0,0 +1,15 @@
+fmdl= mk_library_model('adult_male_16el_lungs');
+fmdl.electrode = fmdl.electrode([9:16,1:8]);
+img = mk_image(fmdl, 1); % background conductivity
+img.elem_data(fmdl.mat_idx{2}) = 0.3001; % lungs
+img.elem_data(fmdl.mat_idx{3}) = 0.3002; % lungs
+ROI = calc_slices(img,[inf,inf,0.5]);
+llung_ROI = ~isnan(ROI) & (ROI==0.3001);
+rlung_ROI = ~isnan(ROI) & (ROI==0.3002);
+thorax_ROI= ~isnan(ROI); % include lungs, too
+
+subplot(131); imagesc(thorax_ROI); axis image
+subplot(132); imagesc(rlung_ROI);  axis image
+subplot(133); imagesc(llung_ROI);  axis image
+print_convert('GREIT_IBEX_01a.png');
+
