ad2104caefec529f7e197f63cfcfa70517a74cd2
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2015-06-02 05:41:13 +0000

    Shorten lines

--- trunk/htdocs/tutorial/EIDORS_basics/error_rates_contact_impedance02.m
+++ trunk/htdocs/tutorial/EIDORS_basics/error_rates_contact_impedance02.m
@@ -9,8 +9,18 @@ mdl.electrode(2).z_contact = z_c(z_cj);
 img.fwd_model.electrode(2).z_contact=z_c(z_cj);
 
 %Solve forward problem with linear, quadratic, cubic and find H1-error
-tic; fprintf(1,'H1 Lin'); [lin_errL2(1,z_cj),lin_errH1(1,z_cj),lin_errH1tot(1,z_cj),lin_errI(1,z_cj),lin_errUS(1,z_cj),lin_errUM(1,z_cj),lin_errUSM(1,z_cj),lin_timing_solver(1,z_cj),lin_DOF(1,z_cj)]=error_2D_squ_CEM(img,'tri3',1); toc;
-tic; fprintf(1,'H1 Quad'); [quad_errL2(1,z_cj),quad_errH1(1,z_cj),quad_errH1tot(1,z_cj),quad_errI(1,z_cj),quad_errUS(1,z_cj),quad_errUM(1,z_cj),quad_errUSM(1,z_cj),quad_timing_solver(1,z_cj),quad_DOF(1,z_cj)]=error_2D_squ_CEM(img,'tri6',1); toc;
+tic;
+  fprintf(1,'H1 Lin');
+  [lin_errL2(1,z_cj),lin_errH1(1,z_cj),lin_errH1tot(1,z_cj),lin_errI(1,z_cj), ...
+   lin_errUS(1,z_cj),lin_errUM(1,z_cj),lin_errUSM(1,z_cj), ...
+   lin_timing_solver(1,z_cj),lin_DOF(1,z_cj)]= error_2D_squ_CEM(img,'tri3',1);
+toc;
+tic;
+  fprintf(1,'H1 Quad');
+  [quad_errL2(1,z_cj),quad_errH1(1,z_cj),quad_errH1tot(1,z_cj),quad_errI(1,z_cj), ...
+  quad_errUS(1,z_cj),quad_errUM(1,z_cj),quad_errUSM(1,z_cj), ...
+  quad_timing_solver(1,z_cj),quad_DOF(1,z_cj)]= error_2D_squ_CEM(img,'tri6',1);
+toc;
 tic; fprintf(1,'H1 Cub'); [cub_errL2(1,z_cj),cub_errH1(1,z_cj),cub_errH1tot(1,z_cj),cub_errI(1,z_cj),cub_errUS(1,z_cj),cub_errUM(1,z_cj),cub_errUSM(1,z_cj),cub_timing_solver(1,z_cj),cub_DOF(1,z_cj)]=error_2D_squ_CEM(img,'tri10',1); toc;           
 
 %Loop over different refinements
