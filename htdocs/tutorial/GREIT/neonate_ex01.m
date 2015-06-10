00640d42b800c1f9b90dd44aa0a3df716d57ee73
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2015-06-10 20:12:34 +0000

    refactor electrode code
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@5100 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/GREIT/neonate_ex01.m
+++ htdocs/tutorial/GREIT/neonate_ex01.m
@@ -1,11 +1,19 @@
 % Inverse model
-fmdl = mk_library_model('neonate_16el');
+%fmdl = mk_library_model('neonate_16el_lungs');
+elec_pos = [16,1,.5]; elec_shape=[0.05,0,0,0,60]; maxsz=0.08; nfft=27;
+elec_pos = [16,1,.5]; elec_shape=[0.05,0,0,0,60]; maxsz=0.08; nfft=27;
+%fmdl = mk_library_model({'neonate','boundary','left_lung','right_lung'},elec_pos, elec_shape, maxsz,nfft);
+ fmdl = mk_library_model({'neonate','boundary'},elec_pos, elec_shape, maxsz,nfft);
+
 [fmdl.stimulation fmdl.meas_select] = mk_stim_patterns(16,1,'{ad}','{ad}');
 fmdl = mdl_normalize(fmdl,1);
-img = mk_image(fmdl,1);
-opt.noise_figure = 0.5; opt.imgsz = [64 64];
-imdl = mk_GREIT_model(img, 0.25, [], opt);
-% imdl = mk_common_gridmdl('GREITc1');
 
-% Data: eidors3d.sf.net/data_contrib/if-neonate-spontaneous/if-neonate-spontaneous.zip
-vv= eidors_readdata('P04P-1016.get');
+img = mk_image(fmdl,1); img.elem_data(vertcat(fmdl.mat_idx{2:3})) = 0.3;
+img.calc_colours.ref_level=1;
+
+show_fem(img);
+return
+opt.square_pixels = 1;
+opt.noise_figure = 0.5; opt.imgsz = [64 64];
+imdl = mk_GREIT_model(img, 0.20, [], opt);
+
--- htdocs/tutorial/GREIT/neonate_ex02.m
+++ htdocs/tutorial/GREIT/neonate_ex02.m
@@ -1,12 +1,14 @@
+% Data: eidors3d.sf.net/data_contrib/if-neonate-spontaneous/if-neonate-spontaneous.zip
+vv= eidors_readdata('P04P-1016.get');
+
 % solve with reference to the mean
 imgall = inv_solve(imdl,mean(vv,2),vv);
-[ins, exp] = find_frc(imgall,[],13,[],2); % find breaths
-
+[insp, expi] = find_frc(imgall,[],13,[],2); % find breaths
 
 % use expirations as reference
-vh = mean(vv(:,exp),2);  % reference is average
+vh = mean(vv(:,expi),2);  % reference is average
 img = inv_solve(imdl,vh,vv);
-img.elem_data = img.elem_data(:,ins); %only show inspirations
+img.elem_data = img.elem_data(:,insp); %only show inspirations
 calc_colours('defaults');
 calc_colours('ref_level',0);
 calc_colours('backgnd',[1 1 1]);
