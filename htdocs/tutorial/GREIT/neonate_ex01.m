7c93b5f395252e3de5d139158f8d05dcc673f5bb
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2015-06-11 10:35:53 +0000

    Banana-shaped minima region
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@5102 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/GREIT/neonate_ex01.m
+++ htdocs/tutorial/GREIT/neonate_ex01.m
@@ -2,8 +2,9 @@
 %fmdl = mk_library_model('neonate_16el_lungs');
 elec_pos = [16,1,.5]; elec_shape=[0.05,0,0,0,60]; maxsz=0.08; nfft=27;
 elec_pos = [16,1,.5]; elec_shape=[0.05,0,0,0,60]; maxsz=0.08; nfft=27;
-%fmdl = mk_library_model({'neonate','boundary','left_lung','right_lung'},elec_pos, elec_shape, maxsz,nfft);
- fmdl = mk_library_model({'neonate','boundary'},elec_pos, elec_shape, maxsz,nfft);
+elec_pos = [16,1,.5]; elec_shape=[0.2,0.4,0.01,0,60]; maxsz=0.08; nfft=27;
+ fmdl = mk_library_model({'neonate','boundary','left_lung','right_lung'},elec_pos, elec_shape, maxsz,nfft);
+%fmdl = mk_library_model({'neonate','boundary'},elec_pos, elec_shape, maxsz,nfft);
 
 [fmdl.stimulation fmdl.meas_select] = mk_stim_patterns(16,1,'{ad}','{ad}');
 fmdl = mdl_normalize(fmdl,1);
---
+++ htdocs/tutorial/strange_effects/minima_region_01.m
@@ -0,0 +1,21 @@
+imb=  mk_common_model('a2c2',16);
+img= mk_image(imb,1); %show_fem(img,[0,0,1]);
+vv = fwd_solve(img); vv = vv.meas;
+p1 = [54,55]; p2 = [41,40];
+
+sp = logspace(-2.5,2.5,40);
+for i = length(sp):-1:1
+   for j = length(sp):-1:1
+      img.elem_data(p1) = sp(i);
+      img.elem_data(p2) = sp(j);
+      v2 = fwd_solve(img); 
+      M(i,j) = norm(vv-v2.meas);
+   end
+end
+img.elem_data(p1) = 10^(+.1);
+img.elem_data(p2) = 10^(-.1);
+subplot(121);show_fem(img);
+clim = [min(M(:)),max(M(:))]*(eye(2)-[1,0;1,0]/20);
+subplot(122);imagesc(log10(sp),log10(sp),M,clim); axis image
+
+print_convert minima_region_01a.png
