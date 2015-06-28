5da2f93c9f04165b841bf938d9ba71972cdcd9ff
alistair_boyle <alistair_boyle@82006732-fb7e-4a22-9f28-86a212cce563>
2015-06-28 16:03:21 +0000

    mash_nodes: clean up

--- trunk/dev/a_boyle/mash_nodes.m
+++ trunk/dev/a_boyle/mash_nodes.m
@@ -1,5 +1,5 @@
-function   mdl = mash_nodes(mdl, method, interp_dim, upd_dim, elec_true, elec_err)
-% function mdl = mash_nodes(mdl, method, interp_dim, upd_dim, elec_true, [elec_err])
+function   mdl = mash_nodes(mdl, method, idm, dim, elec_true, elec_err)
+% function mdl = mash_nodes(mdl, method, idm, dim, elec_true, [elec_err])
 %
 % Mashes the nodes in the X, Y, or Z direction to
 % minimize the error in the electrode locations while
@@ -23,9 +23,8 @@ function   mdl = mash_nodes(mdl, method, interp_dim, upd_dim, elec_true, elec_er
 %        - 'shift_surface' leave the negative boundary
 %        alone and shift the positive boundary around
 %        to minimize the error
-% interp_dim - the dimension in which to interpolate
-% upd_dim - the dimension in which to minimize the
-%        error
+% idm - the dimension in which to index the error (monotonic with electrodes)
+% dim - the dimension in which to minimize the error
 % elec_true - the true electrode locations
 % elec_err - the error in electrode location, single
 %        vector (default: error in the upd_dim
@@ -36,61 +35,68 @@ function   mdl = mash_nodes(mdl, method, interp_dim, upd_dim, elec_true, elec_er
 % while(elec_err > eps)
 %  elec_nodes = [fmdl.electrode(:).nodes];
 %  elec_err = elec_true - fmdl.nodes(elec_nodes,:);
-%  cmdl = mash_nodes(cmdl, 'shift_all',     2, elec_true, elec_err(:,2)); % Y (downslope)
-%  cmdl = mash_nodes(cmdl, 'shift_surface', 1, elec_true, elec_err(:,3)); % Z (vertical)
+%  cmdl = mash_nodes(cmdl, 'shift_all',     1, 2, elec_true(:,[2,3]), elec_err(:,[2,3])); % Y (downslope)
+%  cmdl = mash_nodes(cmdl, 'shift_surface', 1, 1, elec_true(:,[2,3]), elec_err(:,[2,3])); % Z (vertical)
 %  % NOTE that X and Y for cmdl are flipped when plotted in figures but must
 %  % remain messed up for dimension order since the coarse2fine mapping function
 %  % gives a bad result otherwise
-%  fmdl = mash_nodes(fmdl, 'shift_middle',  1, elec_true, elec_err(:,1)); % X (cross-slope)
-%  fmdl = mash_nodes(fmdl, 'shift_all',     2, elec_true, elec_err(:,2)); % Y (downslope)
-%  fmdl = mash_nodes(fmdl, 'shift_surface', 3, elec_true, elec_err(:,3)); % Z (vertical)
+%  fmdl = mash_nodes(fmdl, 'shift_middle',  2, 1, elec_true); % X (cross-slope)
+%  fmdl = mash_nodes(fmdl, 'shift_all',     2, 2, elec_true); % Y (downslope)
+%  fmdl = mash_nodes(fmdl, 'shift_surface', 2, 3, elec_true); % Z (vertical)
 % end
 %
 % (C) 2014 A. Boyle
-  if nargin < 5
-     if isfield(mdl, 'electrode')
-        error('electrodes not available on this model, must supply positional errror');
-     else
-        % this code assumes a single node per electrode --- Point Electrode Model
-        % MATLAB voodoo: convert struct array to array
-        elec_nodes = [mdl.electrode(:).nodes];
-        elec_err = elec_true - mdl.nodes(elec_nodes,:);
-        elec_err = elec_err(:,upd_dim);
-     end
+  if nargin < 6
+     elec_err = mdl_electrode_error(mdl, elec_true);
   end
+  err = elec_err(:,dim);
 
-  y = mdl.nodes(:,interp_dim);
-  yi = [min(y); ...
-        mean([min(y) min(elec_true(:,2))]); ...
-        elec_true(:,interp_dim);
-        mean([max(y) max(elec_true(:,2))]); ...
-        max(y)];
-  yv = [0; 0; elec_err; 0; 0];
-
+  % add borders for electrode positions at
+  % the volume boundary and 50% of the edge to electrode distance
+  xq = mdl.nodes(:,idm);
+  x = [min(xq); ...
+       mean([min(xq) min(elec_true(:,idm))]); ...
+       elec_true(:,idm);
+       mean([max(xq) max(elec_true(:,idm))]); ...
+       max(xq)];
+  v = [0; 0; err; 0; 0];
+plot(err);
   % scale error to match the electrode locations
-  yy = interp1(yi, yv, y, 'linear', 'extrap');
+  vq = interp1(x, v, xq, 'linear', 'extrap');
 
   switch method
     case 'shift_all'
-      mdl.nodes(:,upd_dim) = mdl.nodes(:,upd_dim) + yy;
+      vqs = 1;
     case 'shift_middle'
-      x = mdl.nodes(:,upd_dim);
-      xr = range(x);
-      xm = xr/2 + min(x); % middle
-      xs = 1-(x-xm)./(xr/2); % scale the shift depending on x's distance from midline
-      mdl.nodes(:,upd_dim) = x + yy.*xs;
+      yq = mdl.nodes(:,dim);
+      yqr = max(yq)-min(yq); % range
+      yqm = (max(yq) + min(yq))/2; % middle = (max + min)/2
+      vqs = 1-abs(yq-yqm)./(yqr/2); % scale the shift depending on x's distance from midline
     case 'shift_surface' % assumes positive surface
-      z = mdl.nodes(:,upd_dim);
-      zr = range(z);
-      zm = min(z); % min
-      zs = (z - zm)/zr; % scale by distance from surface
-      mdl.nodes(:,upd_dim) = z + yy.*zs;
+      yq = mdl.nodes(:,dim);
+      yqr = max(yq) - min(yq);
+      yqm = min(yq); % min
+      vqs = abs(yq - yqm)./yqr; % scale by distance from surface
     otherwise
       error(['unrecognized method: ',method]);
   end
+  mdl.nodes(:,dim) = mdl.nodes(:,dim) + (vq .* vqs);
 end
 
-% the (max-min) range of a variable's values
-function r=range(a)
-  r=max(a(:))-min(a(:));
+% calculate the error in electrode position for the fwd_model
+function err = mdl_electrode_error(mdl, xyz)
+   if ~isfield(mdl, 'electrode')
+      error('electrodes not available on this model, must supply positional errror');
+   end
+
+   nel=length(mdl.electrode); % number of electrodes
+   nd=size(mdl.nodes,2); % number of dimensions
+
+   eu = ones(nel,nd)*NaN; % init
+   for i=1:length(mdl.electrode)
+      n = mdl.electrode(i).nodes; % nodes per electrode
+      eu(i,:) = mean(mdl.nodes(n,:)); % approx centre of each electrode
+      % Note: 'eu' could be a *bit* more accurate by dropping the interior nodes on the CEM, but this code is simpler
+   end
+   err = xyz - eu;
 end
