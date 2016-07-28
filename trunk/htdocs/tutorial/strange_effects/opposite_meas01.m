3717ed9f64732188c4292bf94c3b25e8b532bb3e
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2016-07-28 19:48:47 +0000

    fix ref_level for colours

--- trunk/htdocs/tutorial/strange_effects/opposite_meas01.m
+++ trunk/htdocs/tutorial/strange_effects/opposite_meas01.m
@@ -7,28 +7,28 @@ imdl.fwd_model = rmfield(imdl.fwd_model,'meas_select');
 imdl.fwd_model.stimulation = mk_stim_patterns(16,1,'{ad}','{op}',{},1);
 [vi,vh] = simulate_2d_movement( 5, imdl.fwd_model, [.9,.05], 2);
 imgs= inv_solve(imdl, vh, vi);
-imgs.show_slices.img_cols = 5;
+imgs.show_slices.img_cols = 5; imgs.calc_colours.ref_level = 0;
 subplot(411); show_slices(imgs)
 
 % Simulate with OP / AD
 imdl.fwd_model.stimulation = mk_stim_patterns(16,1,'{op}','{ad}',{},1);
 [vi,vh] = simulate_2d_movement( 5, imdl.fwd_model, [.9,.05], 2);
 imgs= inv_solve(imdl, vh, vi); 
-imgs.show_slices.img_cols = 5;
+imgs.show_slices.img_cols = 5; imgs.calc_colours.ref_level = 0;
 subplot(412); show_slices(imgs)
 
 % Simulate with AD / OP
 imdl.fwd_model.stimulation = mk_stim_patterns(16,1,'{ad}','{op}',{},1);
 [vi,vh] = simulate_2d_movement( 5, imdl.fwd_model, [.9,.05], 2);
 imgs= inv_solve(imdl, vh, vi); 
-imgs.show_slices.img_cols = 5;
+imgs.show_slices.img_cols = 5; imgs.calc_colours.ref_level = 0;
 subplot(413); show_slices(imgs)
 
 % Simulate with OP / OP
 imdl.fwd_model.stimulation = mk_stim_patterns(16,1,'{op}','{op}',{},1);
 [vi,vh] = simulate_2d_movement( 5, imdl.fwd_model, [.9,.05], 2);
 imgs= inv_solve(imdl, vh, vi); 
-imgs.show_slices.img_cols = 5;
+imgs.show_slices.img_cols = 5; imgs.calc_colours.ref_level = 0;
 subplot(414); show_slices(imgs)
 
-print -dpng -r100 opposite_meas01.png
+print_convert opposite_meas01.png
