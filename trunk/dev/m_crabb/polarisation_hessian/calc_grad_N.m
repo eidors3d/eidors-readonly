452103f68e7b14e4e72ba5965289646d18265357
michaelcrabb30 <michaelcrabb30@82006732-fb7e-4a22-9f28-86a212cce563>
2016-08-24 16:14:42 +0000

    Updated Taylor and polarisation Hessians

---
+++ trunk/dev/m_crabb/polarisation_hessian/calc_grad_N.m
@@ -0,0 +1,24 @@
+function [ DN ] = calc_grad_N( x, z )
+%CALC_GRAD_N 
+%
+% Gradient of N(x,z) wrt z
+% Single measure loc x, vector of pts z
+
+
+% 2D case
+if size(z,2)==2 && size(x,2)==2
+    
+    % Greens function contribution
+    RR = repmat(x, size(z,1)) - z;
+    DN = RR./(2*pi*repmat(sqrt(sum(RR.^2,2)),1,2));
+    
+    % Correction term on for boundary
+    
+
+else
+    error('mismatched dimensions for x/z or not in 2D')
+end
+
+
+end
+
--- trunk/dev/m_crabb/polarisation_hessian/saturation_plot.m
+++ trunk/dev/m_crabb/polarisation_hessian/saturation_plot.m
@@ -5,9 +5,11 @@ fmdl = imdl.fwd_model; %Extract model
 %Stimulation - 16 elecs adjacent current/adjacent voltage
 stim = mk_stim_patterns(16,1,'{ad}','{ad}');
 fmdl.stimulation = stim; %Add to model
+fmdl.approx_type='tri3';
 
 %Make an image and show image
 img = mk_image(fmdl,1);
+img_b=img;
 figure; show_fem(img,[0,1,3])
 
 %Solve forward model
@@ -18,10 +20,10 @@ figure; plot(vh.meas);
 pixel_group = [327,364,328,292,259,291];
 
 %Conductivity values
-cond_vals = logspace(-2,2,100);
+cond_vals = logspace(0,.5,10);
 
 for jj=1:length(cond_vals)
-    jj
+    jj;
     img.elem_data([pixel_group]) =cond_vals(jj);
     vi = fwd_solve(img); %Image with pixel perturbation
     norm_vi_diff(jj) = norm(vi.meas-vh.meas);
@@ -29,7 +31,35 @@ for jj=1:length(cond_vals)
 end
 
 
+%Calculate Jacobian and see difference between linear approximation
+J=calc_jacobian(img_b);
+J_pixel_group = J(:,pixel_group);
+J_pixel_group_sum = sum(J(:,pixel_group),2);
+
+for jj=1:length(cond_vals)
+    jj;
+    dv =J_pixel_group_sum*(cond_vals(jj)-1);
+    norm_vi_diff_lin(jj)= norm(dv);                
+end
+
+%Calculate Hessian and see difference between quadratic approximation
+img.fwd_model.approx_type='tri3';
+H = calc_hessian_select(img_b.fwd_model,img_b,pixel_group);
+
+H_pixel_group = H(:,1:length(pixel_group),1:length(pixel_group));
+H_pixel_group_sum = sum(sum( H_pixel_group ,2 ),3);
+
+for jj=1:length(cond_vals)
+    jj;
+    dv =J_pixel_group_sum*(cond_vals(jj)-1) + 0.5*H_pixel_group_sum*(cond_vals(jj)-1)*(cond_vals(jj)-1);
+    norm_vi_diff_quad(jj)= norm(dv);                
+end
 
 
-figure; semilogx(cond_vals,norm_vi_diff);
-ylabel('|| Vi - V0||'); xlabel('Pertubation in ith pixel');
+figure; semilogx(cond_vals,norm_vi_diff,'r*');
+hold on; semilogx(cond_vals,norm_vi_diff_lin,'b*');
+hold on; semilogx(cond_vals,norm_vi_diff_quad,'g*');
+legend('Non-linear perturbation','Linear approximation','Quadratic approximation')
+ylabel('|| Vi - V0||'); 
+xlabel('Pertubation in ith pixel');
+
