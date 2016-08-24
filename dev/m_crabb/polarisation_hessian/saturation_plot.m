ba5652eb06dc51514c4f846838a8422ce1aa736a
michaelcrabb30 <michaelcrabb30@82006732-fb7e-4a22-9f28-86a212cce563>
2016-08-24 16:55:53 +0000

    Updated Taylor and polarisation Hessians
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@5242 82006732-fb7e-4a22-9f28-86a212cce563

--- dev/m_crabb/polarisation_hessian/saturation_plot.m
+++ dev/m_crabb/polarisation_hessian/saturation_plot.m
@@ -9,33 +9,35 @@ fmdl.approx_type='tri3';
 
 %Make an image and show image
 img = mk_image(fmdl,1);
-img_b=img;
-figure; show_fem(img,[0,1,3])
+img_b=img; %Background to calculate Jacobians etc
+%figure; show_fem(img,[0,1,3])
 
-%Solve forward model
+%Solve forward model on cond=1
 vh = fwd_solve(img);
-figure; plot(vh.meas); 
+%figure; plot(vh.meas); 
 
 %Pixk group of pixels to perturb
 pixel_group = [327,364,328,292,259,291];
+img.elem_data([pixel_group])=2;
+figure; show_fem(img,[1,0,0]);
 
 %Conductivity values
-cond_vals = logspace(0,.5,10);
+cond_vals = logspace(0,.1,25); %0.1 is approx 1.25 contrast
 
+%Non-linear voltage difference
 for jj=1:length(cond_vals)
     jj;
     img.elem_data([pixel_group]) =cond_vals(jj);
     vi = fwd_solve(img); %Image with pixel perturbation
-    norm_vi_diff(jj) = norm(vi.meas-vh.meas);
-    
+    norm_vi_diff(jj) = norm(vi.meas-vh.meas);    
 end
-
 
 %Calculate Jacobian and see difference between linear approximation
 J=calc_jacobian(img_b);
 J_pixel_group = J(:,pixel_group);
 J_pixel_group_sum = sum(J(:,pixel_group),2);
 
+%Linearised voltage difference
 for jj=1:length(cond_vals)
     jj;
     dv =J_pixel_group_sum*(cond_vals(jj)-1);
@@ -45,21 +47,28 @@ end
 %Calculate Hessian and see difference between quadratic approximation
 img.fwd_model.approx_type='tri3';
 H = calc_hessian_select(img_b.fwd_model,img_b,pixel_group);
-
 H_pixel_group = H(:,1:length(pixel_group),1:length(pixel_group));
 H_pixel_group_sum = sum(sum( H_pixel_group ,2 ),3);
 
+%Quadratic voltage difference
 for jj=1:length(cond_vals)
     jj;
     dv =J_pixel_group_sum*(cond_vals(jj)-1) + 0.5*H_pixel_group_sum*(cond_vals(jj)-1)*(cond_vals(jj)-1);
     norm_vi_diff_quad(jj)= norm(dv);                
 end
 
-
-figure; semilogx(cond_vals,norm_vi_diff,'r*');
-hold on; semilogx(cond_vals,norm_vi_diff_lin,'b*');
-hold on; semilogx(cond_vals,norm_vi_diff_quad,'g*');
+%Plot the norm of voltage differences as function of contrast
+figure; semilogx(cond_vals,norm_vi_diff,'g*');
+hold on; semilogx(cond_vals,norm_vi_diff_lin,'r*');
+hold on; semilogx(cond_vals,norm_vi_diff_quad,'b*');
 legend('Non-linear perturbation','Linear approximation','Quadratic approximation')
-ylabel('|| Vi - V0||'); 
+ylabel('|| Vi - V1||'); 
+xlabel('Pertubation in ith pixel');
+
+%Plot norm of voltage differences of linear and quadratic away from non-linear as function of contrast 
+figure; semilogx(cond_vals,abs(norm_vi_diff-norm_vi_diff_lin),'r*');
+hold on; semilogx(cond_vals,abs(norm_vi_diff-norm_vi_diff_quad),'b*');
+legend('Linear-Nonlinear','Quadratic-Nonlinear')
+ylabel('||V_nonlin-V1||-||V_approx-V1||'); 
 xlabel('Pertubation in ith pixel');
 
