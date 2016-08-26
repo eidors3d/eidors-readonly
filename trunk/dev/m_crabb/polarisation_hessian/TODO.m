c125af2716ef905c60109e4e7fd796595bff3092
michaelcrabb30 <michaelcrabb30@82006732-fb7e-4a22-9f28-86a212cce563>
2016-08-26 16:04:53 +0000

    Add TODO on Polarisation/Hessian

---
+++ trunk/dev/m_crabb/polarisation_hessian/TODO.m
@@ -0,0 +1,13 @@
+%MIKE
+%0. Compare free space and Neumann greens
+%1. Use true Dz (N(x,z) in phess_obj
+%2. Find error in Taylor Hessian
+%3. Make objective Hessian with
+
+
+%FRISS
+%0. Change ellement select in first order expansion i.e. optimise over
+%interior region + make a mesh
+%1. TaylorPhessian of obejctive comparison
+%2. Inverse solve with BFGS ..
+%3. Compare BFGS Hessian to Taylor Hessina
