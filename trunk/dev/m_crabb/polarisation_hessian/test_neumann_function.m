6b870dd47005bef2640ebfaefeb8c6c8a3fb553f
michaelcrabb30 <michaelcrabb30@82006732-fb7e-4a22-9f28-86a212cce563>
2016-10-10 14:54:54 +0000

    neumann function test

--- trunk/dev/m_crabb/polarisation_hessian/test_neumann_function.m
+++ trunk/dev/m_crabb/polarisation_hessian/test_neumann_function.m
@@ -10,13 +10,18 @@ fmdl.approx_type='tri3';
 
 img=mk_image(fmdl,1);
 
-N0 = calc_neumann_function(img.fwd_model,img);
+N0 = calc_neumann_func(img.fwd_model,img);
 DN0 =calc_grad_potential(img,N0);
 
 n_elems = size(fmdl.elems,1);
 n_nodes = size(fmdl.nodes,1);
-N1=zeros(n_nodes,n_elems); %greens function with a source in each element
-DN1 = zeros(n_elems,2,n_elems); %gradient (takes nodal volt to elemen Dvolt) of each greens in source in each element
+%Greens function with a source in each element
+%First argument is potential at nodes and second argument is delta function
+%source supported in elements
+N1=zeros(n_nodes,n_elems); 
+%Gradient (takes nodal volt to elemen Dvolt) of each greens in source in
+%each element
+DN1 = zeros(n_elems,2,n_elems); 
 
 for ii=1:n_elems
     xii = fmdl.elem_centre(ii,:);
@@ -34,15 +39,20 @@ for ii=1:n_elems
     end    
 end
 
+%Pick which element
+
+for elem_indices=[4,139,576]
+
 img0 = img;
-img0.elem_data = DN0(:,1,4);
+img0.elem_data = DN0(:,1,elem_indices);
 
 img1 = img;
-img1.elem_data = DN1(:,1,4);
+img1.elem_data = DN1(:,1,elem_indices);
 
-img2=img
-img2.elem_data = (DN0(:,1,4)-DN1(:,1,4))./DN0(:,1,4);
+img2=img;
+img2.elem_data = (DN0(:,1,elem_indices)-DN1(:,1,elem_indices))./DN1(:,1,elem_indices);
 
+%{
 figure; plot3(img.fwd_model.nodes(:,1),img.fwd_model.nodes(:,2),N0(:,4),'r*')
 hold on; plot3(img.fwd_model.nodes(:,1),img.fwd_model.nodes(:,2),N1(:,4),'b*')
 
@@ -55,4 +65,13 @@ subplot(122); show_fem(img1); colorbar;
 
 figure; 
 show_fem(img2); colorbar; 
+%}
+
+figure; 
+hist(img2.elem_data,1000)
+title(sprintf('Error for Greens function source at (x,y) = (%1.2f,%1.2f)',...
+    fmdl.elem_centre(elem_indices,1),fmdl.elem_centre(elem_indices,2)))
+end
+
+
 %hold on; plot3(img.fwd_model.nodes(:,1),img.fwd_model.nodes(:,2),DU1(:,1,1),'r*')
