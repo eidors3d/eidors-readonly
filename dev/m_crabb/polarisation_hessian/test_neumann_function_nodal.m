70517bb263cf2362006f6ae6cfe25db7a61f91e3
michaelcrabb30 <michaelcrabb30@82006732-fb7e-4a22-9f28-86a212cce563>
2016-10-12 15:46:18 +0000

    Updated greens function files
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@5282 82006732-fb7e-4a22-9f28-86a212cce563

--- dev/m_crabb/polarisation_hessian/test_neumann_function_nodal.m
+++ dev/m_crabb/polarisation_hessian/test_neumann_function_nodal.m
@@ -10,6 +10,8 @@ fmdl.approx_type='tri3';
 
 img=mk_image(fmdl,1);
 
+%NE0 = calc_neumann_func(img.fwd_model,img);
+%DNE0 =calc_grad_potential(img,N0);
 
 %NO(x,z) for point source at nodal location z i.e. each column is vector of
 %nodal potentias for a delta function source at node z
@@ -45,41 +47,53 @@ for ii=1:n_nodes
     end    
 end
 
-%Pick which element
-
-%for elem_indices=[4,139,576]
-for elem_indices=[1,16,63]
+%Pick which solutions to plot
+for potential_indices=[1]
 
     
-img0 = img;
-img0.elem_data = DN0(:,1,elem_indices);
+img0x = img;img0y = img;
+img0x.elem_data = squeeze(DN0(potential_indices,1,:));
+img0y.elem_data = squeeze(DN0(potential_indices,2,:));
 
-img1 = img;
-img1.elem_data = DN1(:,1,elem_indices);
 
-img2=img;
-img2.elem_data = (DN0(:,1,elem_indices)-DN1(:,1,elem_indices));
+img1x = img;img1y = img;
+img1x.elem_data = squeeze(DN1(potential_indices,1,:));
+img1y.elem_data = squeeze(DN1(potential_indices,2,:));
+ 
 
-%%{
-figure; plot3(img.fwd_model.nodes(:,1),img.fwd_model.nodes(:,2),N0(:,elem_indices),'r*')
-hold on; plot3(img.fwd_model.nodes(:,1),img.fwd_model.nodes(:,2),N1(:,elem_indices),'b*')
+img2x=img; img2y=img;
+img2x.elem_data = squeeze((DN0(potential_indices,1,:)-DN1(potential_indices,1,:))...
+    ./abs(DN0(potential_indices,1,:)));
+img2y.elem_data = squeeze((DN0(potential_indices,2,:)-DN1(potential_indices,2,:))...
+    ./abs(DN0(potential_indices,2,:)));
 
-figure; plot3(img.fwd_model.nodes(:,1),img.fwd_model.nodes(:,2),(N0(:,elem_indices)-N1(:,elem_indices)),'r*')
+
+%{
+figure; plot3(img.fwd_model.nodes(:,1),img.fwd_model.nodes(:,2),N0(:,potential_indices),'r*')
+hold on; plot3(img.fwd_model.nodes(:,1),img.fwd_model.nodes(:,2),N1(:,potential_indices),'b*')
+figure; plot3(img.fwd_model.nodes(:,1),img.fwd_model.nodes(:,2),...
+    (N0(:,potential_indices)-N1(:,potential_indices)),'r*')
 %}
 
 figure; 
-subplot(121); show_fem(img0); colorbar; 
-subplot(122); show_fem(img1); colorbar;
+subplot(321); show_fem(img0x,[1,0,0]);% colorbar; 
+subplot(322); show_fem(img0y,[1,0,0]); %colorbar;
+subplot(323); show_fem(img1x,[1,0,0]);% colorbar; 
+subplot(324); show_fem(img1y,[1,0,0]); %colorbar;
+subplot(325); show_fem(img2x,[1,0,0]); %colorbar; 
+subplot(326); show_fem(img2y,[1,0,0]); %colorbar; 
 
-figure; 
-show_fem(img2); colorbar; 
 %}
 
 figure; 
-hist(img2.elem_data,100000)
-title(sprintf('Error for Greens function source at (x,y) = (%1.2f,%1.2f)',...
-    fmdl.elem_centre(elem_indices,1),fmdl.elem_centre(elem_indices,2)))
+subplot(121);hist(img2x.elem_data,10000)
+title(sprintf('Error for gradx Greens function source at (x,y) = (%1.2f,%1.2f)',...
+    fmdl.elem_centre(potential_indices,1),fmdl.elem_centre(potential_indices,2)))
+subplot(122);hist(img2y.elem_data,10000)
+title(sprintf('Error for grady Greens function source at (x,y) = (%1.2f,%1.2f)',...
+    fmdl.elem_centre(potential_indices,1),fmdl.elem_centre(potential_indices,2)))
 end
 
-
 %hold on; plot3(img.fwd_model.nodes(:,1),img.fwd_model.nodes(:,2),DU1(:,1,1),'r*')
+
+
