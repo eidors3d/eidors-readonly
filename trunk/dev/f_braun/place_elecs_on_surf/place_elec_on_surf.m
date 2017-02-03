aa46d392d2510434af49870c8af47f27b7137ce5
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2017-02-03 13:42:57 +0000

    move required code into function

--- trunk/dev/f_braun/place_elecs_on_surf/place_elec_on_surf.m
+++ trunk/dev/f_braun/place_elecs_on_surf/place_elec_on_surf.m
@@ -816,6 +816,39 @@ for i = 1:size(elec_pos,1)
 
 end
 
+function [C,A] = manifold_patches(F)
+%% Taken from: http://www.alecjacobson.com/weblog/?p=3618
+% Credits: Alec Jacobson, No licence given, assume public domain
+  % Compute connected components of facets connected by manifold edges.
+  %
+  % Inputs:
+  %   F  #F by simplex-size list of facets
+  % Outputs:
+  %   C  #F list of component ids
+  %
+
+  % simplex size
+  ss = size(F,2);
+  assert(ss == 3);
+
+  % List of all "half"-edges: 3*#F by 2
+  allE = [F(:,[2 3]); F(:,[3 1]); F(:,[1 2])];
+  % Sort each row
+  sortallE = sort(allE,2);
+  % IC(i) tells us where to find sortallE(i,:) in uE: 
+  % so that sortallE(i,:) = uE(IC(i),:)
+  [uE,~,IC] = unique(sortallE,'rows');
+  % uE2F(e,f) = 1 means face f is adjacent to unique edge e
+  uE2F = sparse(IC(:),repmat(1:size(F,1),1,ss)',1);
+  % kill non-manifold edges
+  uE2F(sum(uE2F,2)>2,:) = 0;
+  % Face-face Adjacency matrix
+  A = uE2F'*uE2F;
+  % All ones
+  A = A>0;
+  % Connected components are patches
+  %C = components(A); % alternative to graphconncomp from matlab_bgl
+  [~,C] = graphconncomp(A);
 
 function do_unit_test
 xy= [ -0.89 -0.74 -0.21  0.31  0.79  0.96  0.67  0.05 -0.36 -0.97;
