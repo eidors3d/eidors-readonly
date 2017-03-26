3d03904db22239cf95df973cfa0af57c7c7621e8
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2017-03-26 15:12:29 +0000

    working code and text for 2D

--- trunk/eidors/models/mk_hollow_electrode.m
+++ trunk/eidors/models/mk_hollow_electrode.m
@@ -9,14 +9,74 @@ function fmdl = mk_hollow_electrode(fmdl, elec_idx)
 % This function is useful for internal electrodes created by
 %   meshing software in which we don't want to calculate current
 %   flow within the electrode
+%
+% Limitations: currently works for 2D fems only
 
-% (C) 2017 Andy Adler. License: GPL v2 or v3. $Id$
+% (C) 2017 Andy Adler and Bartek Grychtol. License: GPL v2 or v3. $Id$
 
 if ischar(fmdl) && strcmp(fmdl,'UNIT_TEST'); do_unit_test; return; end
 
+if nargin==1; elec_idx = 1:length(fmdl.electrode); end
+elec_idx = elec_idx(:)'; % Row vector to index for loop
+
+elidx=[];
+for i = elec_idx
+   elidx = [elidx, fmdl.electrode(i).nodes];
+   
+end
+
+ELNODES = zeros(size(fmdl.nodes,1),1);
+ELNODES(elidx) = 1;
+% find all electrode elems
+ELELEM = sum(ELNODES(fmdl.elems),2)==3;
+% remove internal electrode elements
+fmdl.elems = fmdl.elems(~ELELEM,:);
+% used nodes
+unodes = unique(fmdl.elems);
+% remove internal electrode nodes
+for i = elec_idx
+   idx = ismember(fmdl.electrode(i).nodes,unodes);
+   fmdl.electrode(i).nodes = fmdl.electrode(i).nodes(idx);
+end
+
+nodemap = zeros(size(fmdl.nodes,1),1);
+nodemap(unodes) = 1:numel(unodes);
+% remove unused nodes
+fmdl.nodes = fmdl.nodes(unodes,:);
+%remap elems
+fmdl.elems = nodemap(fmdl.elems);
+%remap elecs
+for i = 1:length(fmdl.electrode)
+   nidx =  nodemap( fmdl.electrode(i).nodes(idx) );
+   if any(i==elec_idx);
+      npts = fmdl.nodes(nidx,:);
+      [npts_o, nnidx] = order_loop(npts);
+      fmdl.electrode(i).nodes = nidx(nnidx);
+   else % if not in specified index, don't reorder loop
+      fmdl.electrode(i).nodes = nidx;
+   end
+end
+
+
+fmdl.boundary = find_boundary(fmdl);
+
+[~,idx] = min(sum(fmdl.nodes.^2,2));
+fmdl.gnd_node = idx(1);
+
+
 function do_unit_test
     fmdl = unit_test_model1;
-    subplot(221); show_fem(fmdl); view(0,60);
+    subplot(221); show_fem(fmdl); axis([-.1,.3,0.4,0.8])
+    title 'original model - filled electrodes';
+    fmdl1= mk_hollow_electrode(fmdl,[1,2,5]);
+    
+    subplot(222); show_fem(fmdl1); axis([-.1,.3,0.4,0.8])
+    title 'original model - hollow electrode #1';
+
+    fmdl2= mk_hollow_electrode(fmdl);
+    subplot(223); show_fem(fmdl2); axis([-.1,.3,0.4,0.8])
+    title 'original model - all hollow electrode';
+    
 
 function fmdl = unit_test_model1
    R=0.65; Nel = 16;
