1962d2bfeac0058d93ad5b821269e656d1224abd
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2017-04-07 13:55:47 +0000

    Calc_R_Prior tests and dev work
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@5386 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/solvers/calc_R_prior.m
+++ eidors/solvers/calc_R_prior.m
@@ -19,6 +19,7 @@ function R_prior = calc_R_prior( inv_model, varargin )
 % (C) 2005-2008 Andy Adler. License: GPL version 2 or version 3
 % $Id$
 
+if isstr(inv_model) && strcmp(inv_model,'UNIT_TEST'); do_unit_test; return; end
 
 inv_model = rec_or_fwd_model( inv_model);
 
@@ -82,3 +83,19 @@ function inv_model = rec_or_fwd_model( inv_model);
          inv_model= rmfield(inv_model,'rec_model');
       end
    end
+
+function do_unit_test
+   imdl = mk_common_model('a2c2',16);
+
+   priors={'prior_tikhonov',
+           'prior_laplace'};
+
+   for i = 1:length(priors);
+      imdl.RtR_prior = feval(priors{i},imdl);
+
+      R   = calc_R_prior(imdl);
+      RtR = calc_RtR_prior(imdl);  
+      unit_test_cmp(['P:',priors{i}], R'*R, RtR, 1e-10);
+   end
+
+
