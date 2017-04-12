5901353d3e06595005c403549e0f738a39693e9a
alistair_boyle <alistair_boyle@82006732-fb7e-4a22-9f28-86a212cce563>
2017-04-12 05:22:03 +0000

    2.5D tutorial extended to cover Fourier methods
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@5392 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/dual_model/two_and_half_d02.m
+++ htdocs/tutorial/dual_model/two_and_half_d02.m
@@ -1,14 +1,24 @@
-% Solve 2D and 3D model $Id$
+% test c2f
+rimg = mk_image(cmdl,1);
+rimg.fwd_model = imdl.fwd_model;
+rimg.rec_model = imdl.rec_model;
+rimg.elem_data = 1 + ...
+   elem_select( rimg.rec_model, '(x-0.3).^2+(y+0.1).^2<0.15^2' ) - ...
+   elem_select( rimg.rec_model, '(x+0.4).^2+(y-0.2).^2<0.2^2' )*0.5;
+fimg = mk_image(fmdl,1);
+fimg.elem_data = 1 + ...
+   elem_select( rimg.fwd_model, '(x-0.3).^2+(y+0.1).^2<0.15^2' ) - ...
+   elem_select( rimg.fwd_model, '(x+0.4).^2+(y-0.2).^2<0.2^2' )*0.5;
+clf; % figure 1
+subplot(131); show_fem(mk_image(imdl.rec_model,rimg.elem_data)); axis square; axis off;
+title('coarse');
+subplot(132); show_fem(mk_image(imdl.fwd_model,c2f*rimg.elem_data)); axis square; axis off;
+title('coarse-to-fine');
+subplot(133); show_fem(fimg); axis square; axis off; title('fwd fine');
+print_convert two_and_half_d02a.png '-density 75';
 
-% Original target
-subplot(141)
-show_fem(img); view(-62,28)
-
-% Create inverse Model: Classic
-imdl= select_imdl(f_mdl, {'Basic GN dif'});
-imdl.hyperparameter.value = .1;
-
-% Classic (inverse crime) solver
-img1= inv_solve(imdl, vh, vi);
-subplot(142)
-show_fem(img1); view(-62,28)
+% Simulate data - homogeneous
+himg = mk_image(fmdl,1);
+vh = fwd_solve_2p5d_1st_order(himg);
+% Simulate data - inhomogeneous
+vi = fwd_solve_2p5d_1st_order(fimg);
