fe1eae92a643791b0517e2d24d712a7ce76fe071
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2017-04-25 15:45:35 +0000

    clean up code and add zoomed image
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@5420 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/EIDORS_basics/forward_solvers_3d_high_order03.m
+++ htdocs/tutorial/EIDORS_basics/forward_solvers_3d_high_order03.m
@@ -8,7 +8,7 @@ v2e=v2.meas; v2all=v2.volt;
 
 %Electrode voltages and difference for linear, quadratic and cubic
 figure; plot([v1e,v2e,[v2e-v0e]*1]);
-legend('1','2','(2-0) x 10',4)
+legend('1','2','(2-0) x 10','Location','NorthEast')
 xlim([1,100]);
 
 print_convert forward_solvers_3d_high_order03a.png
