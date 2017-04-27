83930cab28a8ba0fdea8c701f439e278c72a7bb5
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2017-04-27 18:10:01 +0000

    show code and Jacobian
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@5434 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/EIDORS_basics/forward_solvers_3d_high_order05.m
+++ htdocs/tutorial/EIDORS_basics/forward_solvers_3d_high_order05.m
@@ -1,20 +1,33 @@
-%Difference between quadratic and linear approximation internal voltage
-J0 = calc_jacobian( img0 );
-J1 = calc_jacobian( img1 );
-J2 = calc_jacobian( img2 );
+imdl = mk_common_model('b3cr',16); img = mk_image(imdl.fwd_model,1);
+vv=fwd_solve(img);      v0e=vv.meas;
+JJ=calc_jacobian(img);  J04=JJ(4,:)';
 
-for i=1:6;
-   imgout = img0;
-   switch i; case 1; imgout.elem_data = J0(4,:);
-             case 2; imgout.elem_data = J1(4,:);
-             case 3; imgout.elem_data = J2(4,:);
-             case 4; continue
-             case 5; imgout.elem_data = J0(4,:) - J1(4,:);
-             case 6; imgout.elem_data = J0(4,:) - J2(4,:);
-   end
-   subplot(2,3,i);
-   show_slices(imgout,[inf,inf,2.5]);
-   eidors_colourbar(imgout);
+%High-order EIDORS solver %Change default eidors solvers
+img.fwd_model.solve = @fwd_solve_higher_order;
+img.fwd_model.system_mat = @system_mat_higher_order;
+img.fwd_model.jacobian = @jacobian_adjoint_higher_order;
+
+vve=[]; JJ4=[];
+for i= 1:2; switch i;
+   case 1; img.fwd_model.approx_type = 'tet4'; % linear
+   case 2; img.fwd_model.approx_type = 'tet10'; % quadratic
+   end %switch
+   vv=fwd_solve(img);      vve(:,i)=vv.meas;
+   JJ=calc_jacobian(img);  JJ4(:,i)=JJ(4,:)';
 end
+
+subplot(311);
+plot([v0e,vve,(v0e*[1,1]-vve)*10]);
+legend('Default','linear','quadratic','(1-0)x10','(2-0)x10');
+xlim([1,100]);
+
+imgJJ=img; imgJJ.elem_data = JJ4;
+imgJJ.show_slices.img_cols = 2;
+
+level = [inf,inf,0.3];
+subplot(312); show_slices(imgJJ,level); eidors_colourbar(imgJJ);
+
+imgJJ.elem_data = JJ4 - J04*[1,1];
+subplot(313); show_slices(imgJJ,level); eidors_colourbar(imgJJ);
 
 print_convert forward_solvers_3d_high_order05a.png
