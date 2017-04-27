52b13d0078a8ef9a33e5211b34bba2094ecd0e1c
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2017-04-27 18:10:01 +0000

    show code and Jacobian

--- trunk/htdocs/tutorial/EIDORS_basics/forward_solvers_2d_high_order05.m
+++ trunk/htdocs/tutorial/EIDORS_basics/forward_solvers_2d_high_order05.m
@@ -1,19 +1,33 @@
-%Difference between quadratic and linear approximation internal voltage
-J0 = calc_jacobian( img0 );
-J1 = calc_jacobian( img1 );
-%J2 = calc_jacobian( img2 ); -- curently doesn't work
-%J3 = calc_jacobian( img3 ); 
-img0j  = img0; img0j.elem_data = J0(4,:);
-img01j = img0; img01j.elem_data = J1(4,:) - J0(4,:);
+imdl = mk_common_model('c2C',16); img = mk_image(imdl.fwd_model,1);
+vv=fwd_solve(img);      v0e=vv.meas;
+JJ=calc_jacobian(img);  J04=JJ(4,:)';
 
-%Plot the difference 
-clf;
-subplot(221); show_fem(img0j,1);
-subplot(223); show_fem(img0j,1); axis([-0.2, 0.6, 0.3, 1.1]);
+%High-order EIDORS solver %Change default eidors solvers
+img.fwd_model.solve = @fwd_solve_higher_order;
+img.fwd_model.system_mat = @system_mat_higher_order;
+img.fwd_model.jacobian = @jacobian_adjoint_higher_order;
 
+vve=[]; JJ4=[];
+for i= 1:3; switch i;
+   case 1; img.fwd_model.approx_type = 'tri3'; % linear
+   case 2; img.fwd_model.approx_type = 'tri6'; % quadratic
+   case 3; img.fwd_model.approx_type = 'tri10'; % cubic;
+   end %switch
+   vv=fwd_solve(img);      vve(:,i)=vv.meas;
+   JJ=calc_jacobian(img);  JJ4(:,i)=JJ(4,:)';
+end
 
-%Plot the difference 
-subplot(222); show_fem(img01j,1);
-subplot(224); show_fem(img01j,1); axis([-0.2, 0.6, 0.3, 1.1]);
+subplot(311);
+plot([v0e,vve,(v0e*[1,1,1]-vve)*10]);
+legend('Default','linear','quadratic','cubic','(1-0)x10','(2-0)x10','(3-0)x10');
+xlim([1,100]);
+
+imgJJ=img; imgJJ.elem_data = JJ4;
+imgJJ.show_slices.img_cols = 3;
+
+subplot(312); show_slices(imgJJ); eidors_colourbar(imgJJ);
+
+imgJJ.elem_data = JJ4 - J04*[1,1,1];
+subplot(313); show_slices(imgJJ); eidors_colourbar(imgJJ);
 
 print_convert forward_solvers_2d_high_order05a.png
