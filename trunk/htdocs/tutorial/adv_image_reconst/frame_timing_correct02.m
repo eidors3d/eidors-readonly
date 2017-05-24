de9494a00fe86937e81e82afbd6c804c2a88cb04
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2017-05-24 13:49:13 +0000

    frame timing tutorial complete

--- trunk/htdocs/tutorial/adv_image_reconst/frame_timing_correct02.m
+++ trunk/htdocs/tutorial/adv_image_reconst/frame_timing_correct02.m
@@ -2,6 +2,7 @@ Original = eidors_readdata('nm18.get','GET-RAW');
 Orig_ref = mean(Original,2); % reference frame - average of all frames
 Orig_imag=inv_solve(inv_mdl,Original(:,691:717),Orig_ref); %reconstruct difference images
 
-Corrected = eidors_readdata('nm18_corrected.get','GET-RAW');  % If using GUI
+%Corrected = eidors_readdata('nm18_corrected.get','GET-RAW');  % If using GUI
+Corrected = frame_timing_correct( Original, 'FFT');
 Corr_ref = mean(Corrected,2); % reference frame - average of all frames
 Corr_imag=inv_solve(inv_mdl,Corrected(:,691:717),Corr_ref); %reconstruct difference images
