4dc4df03c11077477ff9030c31d83c385d65f001
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2017-05-24 13:41:35 +0000

    timing tutorial - use GET-RAW with eidors_readdata
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@5489 82006732-fb7e-4a22-9f28-86a212cce563

---
+++ htdocs/tutorial/adv_image_reconst/frame_timing_correct02.m
@@ -0,0 +1,7 @@
+Original = eidors_readdata('nm18.get','GET-RAW');
+Orig_ref = mean(Original,2); % reference frame - average of all frames
+Orig_imag=inv_solve(inv_mdl,Original(:,691:717),Orig_ref); %reconstruct difference images
+
+Corrected = eidors_readdata('nm18_corrected.get','GET-RAW');  % If using GUI
+Corr_ref = mean(Corrected,2); % reference frame - average of all frames
+Corr_imag=inv_solve(inv_mdl,Corrected(:,691:717),Corr_ref); %reconstruct difference images
