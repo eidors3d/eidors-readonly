6a2ab17dccd6be08b899162a585700b69f6b189e
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2017-06-13 14:27:43 +0000

    Rewrite to use new absolute solver
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@5533 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/EIDORS_basics/compare_3D_abs_GN_01.m
+++ htdocs/tutorial/EIDORS_basics/compare_3D_abs_GN_01.m
@@ -1,14 +1,18 @@
 %Homogeneous and inclusion conductivity
 cond_h=1.0; cond_inc=2.0;
+cc.ref_level = cond_h; cc.clim = 0.6; cc.cb_shrink_move = [.3,.8,.00];
+
+e_idx = [ 1, 4, 5, 8, 9,12,13,16,17,20,21,24,25,28,29,32; % square
+          2, 3, 6, 7,10,11,14,15,18,19,22,23,26,27,30,31]';
 
 %3D forward model without inclusion 
 mdl_h= ng_mk_cyl_models([1 .7],[16,.25,.75],[0.075,0.3]);
-mdl_h.normalize_measurements=0;
+mdl_h.electrode(e_idx) =  mdl_h.electrode;
 
 %3D forward model with inclusion 
 extra={'ball','solid ball = sphere(0.2,0.2,0.5;0.2);'};
-[mdl_i,mat_idx_i]= ng_mk_cyl_models([1 .7],[16,.25,.75],[0.075,0.3],extra);
-mdl_i.normalize_measurements=0;
+mdl_i= ng_mk_cyl_models([1 .7],[16,.25,.75],[0.075,0.3],extra);
+mdl_i.electrode(e_idx) =  mdl_i.electrode;
 
 %Stimulation patterns and add to models
 stim=mk_stim_patterns(16,2,'{ad}','{ad}');
@@ -16,7 +20,9 @@ mdl_h.stimulation=stim; mdl_i.stimulation=stim;
 
 %Create two images
 img_h= mk_image(mdl_h,cond_h); 
-img_i= mk_image(mdl_i,cond_h); img_i.elem_data(mat_idx_i{2}) = cond_inc;
+img_i= mk_image(mdl_i,cond_h); img_i.elem_data(mdl_i.mat_idx{2}) = cond_inc;
+img_h.calc_colours = cc;
+img_i.calc_colours = cc;
 
 %Now get "real" voltages and add noise
 v_i=fwd_solve(img_i); v_i_n = add_noise( 20, v_i ); v_h=fwd_solve(img_h);
@@ -26,7 +32,6 @@ v_i=fwd_solve(img_i); v_i_n = add_noise( 20, v_i ); v_h=fwd_solve(img_h);
 clf; axes('position',[0.2,0.2,0.6,0.6]);
 
 show_3d_slices(img_i,0.6,0.3,0.3); view(-24,12);
-img_i.calc_colours.cb_shrink_move = [.3,.8,.00];
 eidors_colourbar(img_i);
 print_convert compare_3D_abs_GN_01a.png
 
