6f201a1d32ad409d9e2c27142db754b8aa1d5e32
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2017-06-14 12:31:50 +0000

    improved code and tests
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@5534 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/models/elec_rearrange.m
+++ eidors/models/elec_rearrange.m
@@ -6,7 +6,7 @@ function elec_idx = elec_rearrange( pattern, newarrange )
 %     fmdl.electrode = fmdl.electrode( elec_idx );
 %
 % pattern    = [Elecs/row, rows]  (eg [16,2])
-%   or a structure with (p.n_elecs, p.layers)
+%   or a structure with (p.n_elecs, p.n_layers)
 %
 % newarrange = 'square' 
 %              'zigzag' / 'odd_even' 
@@ -19,18 +19,18 @@ function elec_idx = elec_rearrange( pattern, newarrange )
 
    if isnumeric(pattern)
       p.n_elecs = pattern(1);
-      p.layers  = pattern(2);
+      p.n_layers= pattern(2);
    elseif isstruct(pattern)
       p = pattern;
    else
       error('Can''t interpret "pattern" input');
    end
 
-   if p.layers >= 3;
-      error('elec_rearrange: can''t interpret p.layers >=2');
+   if p.n_layers >= 3;
+      error('elec_rearrange: can''t interpret n_layers >=2');
    end
 
-   idx = 1:(p.n_elecs*p.layers);
+   idx = reshape(1:(p.n_elecs*p.n_layers),p.n_layers,p.n_elecs);
    switch newarrange
      case 'none';      return
      case 'odd_even';
@@ -44,11 +44,27 @@ function elec_idx = elec_rearrange( pattern, newarrange )
   elec_idx = idx(:);
 
 function do_unit_test
-   idx = elec_rearrange([8,2],'square');
-   elec= [1:16]; elec = elec(idx); elec= reshape(elec,[8,2]);
-   sqV = [1,2;4,3;5,6;8,7;9,10;12,11;13,14;16,15];
-   unit_test_cmp('Sq [8,2]', elec, sqV);
+   sqV = [1,2;4,3;5,6;8,7;9,10;12,11;13,14;16,15]';
 
+
+   elec= reshape([1:16],8,2)';
+   idx = elec_rearrange(size(elec'),'square');
+   idx = reshape(idx,size(elec'))';
+   unit_test_cmp('Sq [8,2]', idx, sqV);
+
+   p.n_elecs = 8; p.n_layers = 2;
+   idx = elec_rearrange(p,'square');
+   idx = reshape(idx,size(elec'))';
+   unit_test_cmp('Sq [8,2]', idx, sqV);
+
+   elec= [1:32]';
+   idx = elec_rearrange(size(elec),'square');
+   idx= reshape(idx,[16,2])';
+   unit_test_cmp('Sq [16,2]', idx(:,1:8), sqV);
+
+   imdl = mk_common_model('n3r2',[16,2]); fmdl = imdl.fwd_model;
    idx = elec_rearrange([16,2],'square');
-   elec= [1:32]; elec = elec(idx); elec= reshape(elec,[16,2]);
-   unit_test_cmp('Sq [16,2]', elec(1:8,:), sqV);
+   fmdl.electrode(idx) = fmdl.electrode;
+   show_fem_enhanced(fmdl,[0,1]);
+   view(10,82);
+   
