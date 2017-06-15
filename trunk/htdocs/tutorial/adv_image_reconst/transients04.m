61786fa3e02686d15dfb811e4b4c8bc877d900be
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2017-06-15 11:34:31 +0000

    Specify only real component of current shown

--- trunk/htdocs/tutorial/adv_image_reconst/transients04.m
+++ trunk/htdocs/tutorial/adv_image_reconst/transients04.m
@@ -4,6 +4,7 @@ if norm(real(vt(:))) < 1e-10*norm(imag(vt(:))); error('ifft'); end
 
 subplot(211); 
 
+imgn = rmfield(img,'elem_data');
 imgn.node_data = real(vt);
 imgn.show_slices.img_cols = 10;
 imgn.get_img_data.frame_select = round(0.3*tpts):round(0.79*tpts);
