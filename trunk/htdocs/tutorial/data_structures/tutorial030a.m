b158f5fb70fd1159c44073fc91116aa6d213ee26
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2017-06-16 16:30:20 +0000

    use updated function show_stim_meas_pattern instead of display_meas

--- trunk/htdocs/tutorial/data_structures/tutorial030a.m
+++ trunk/htdocs/tutorial/data_structures/tutorial030a.m
@@ -9,12 +9,12 @@ fmdl= imdl_3d.fwd_model;
 fmdl.stimulation=mk_stim_patterns(16,2, '{op}','{op}', ...
              {'meas_current','no_redundant'} );
 subplot(121)
-display_meas(fmdl,'ya')
+show_stim_meas_pattern(fmdl,'ya')
 
 % Show adjacent pattern
 fmdl.stimulation=mk_stim_patterns(16,2, '{ad}','{ad}', ...
              {'no_meas_current','do_redundant'} );
 subplot(122)
-display_meas(fmdl,'ya')
+show_stim_meas_pattern(fmdl,'ya')
 
 print_convert tutorial030a.png '-density 75';
