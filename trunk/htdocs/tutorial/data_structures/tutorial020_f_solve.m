994d10caf4ee9cbf6161f7cb592955076ce0c7d3
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2017-06-16 16:20:15 +0000

    accept one parameter => consistent with new eidors input parameters

--- trunk/htdocs/tutorial/data_structures/tutorial020_f_solve.m
+++ trunk/htdocs/tutorial/data_structures/tutorial020_f_solve.m
@@ -1,4 +1,4 @@
-function data =tutorial020_f_solve( f_mdl, img )
+function data =tutorial020_f_solve( img )
 % Forward Model for a resistor
 % For each stimulation there is I1 into Node1
 %  Node2 is connected to gnd with Zcontact
@@ -11,15 +11,16 @@ function data =tutorial020_f_solve( f_mdl, img )
 %  V= IR    => [V1;V2;V3] = [I1;I2*I3]*(R + 2*Zcontact)
 
   R= img.elem_data;
+  stim = img.fwd_model.stimulation;
 
-  n_stim= length( f_mdl.stimulation );
+  n_stim= length( stim );
   V= zeros(n_stim, 1);
 
   for i=1:n_stim
-    I        = f_mdl.stimulation(i).stim_pattern;
-    meas_pat = f_mdl.stimulation(i).meas_pattern;
+    I        = stim(i).stim_pattern;
+    meas_pat = stim(i).meas_pattern;
 
-    zc       = f_mdl.electrode( find(I) ).z_contact;
+    zc       = img.fwd_model.electrode( find(I) ).z_contact;
 
     V(i)     = meas_pat * I * ( R + 2*zc);
   end
