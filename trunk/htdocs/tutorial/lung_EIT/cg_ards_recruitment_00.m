863a704c2c19a1da46ac9e499008296f694bd2a4
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2017-06-18 15:28:26 +0000

    modify files so that automatic testing works

--- trunk/htdocs/tutorial/lung_EIT/cg_ards_recruitment_00.m
+++ trunk/htdocs/tutorial/lung_EIT/cg_ards_recruitment_00.m
@@ -2,3 +2,6 @@ zipfilecontents = unzip(...
     '../../data_contrib/cg-2012-ards-recruitment/cg_data_2012_p1.zip');
 zipfilecontents = [zipfilecontents, unzip(...
     '../../data_contrib/cg-2012-ards-recruitment/cg_2012_ards_recruitment_code.zip')];
+zipfilecontents{end+1} = 'zipfilecontents.mat';
+save zipfilecontents.mat zipfilecontents
+clear
