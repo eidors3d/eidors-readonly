63c5d7a2816397be1106ccbcd7084f9b27c34851
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2017-06-18 16:22:11 +0000

    reduce maxh to 50 because netgen5.3 fails with the older value of 40

--- trunk/htdocs/tutorial/geophysics/stimPatt_PseudoSec01.m
+++ trunk/htdocs/tutorial/geophysics/stimPatt_PseudoSec01.m
@@ -1,7 +1,7 @@
 % Forward Model
 shape_str = ['solid top    = plane(0,0,0;0,0,1);\n' ...
-             'solid mainobj= top and orthobrick(-100,-100,-200;410,100,0) -maxh=40.0;\n'];
-n_elec= 64; 
+             'solid mainobj= top and orthobrick(-100,-100,-200;410,100,0) -maxh=50.0;\n'];
+n_elec= 54; 
 e0 = linspace(0,310,n_elec)';
 elec_pos = [e0,0*e0,0*e0,0*e0,0*e0,1+0*e0];
 elec_shape= [0.005,0,1];
