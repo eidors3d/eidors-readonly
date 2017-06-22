425a8a8492c4f670fd1c70e162efa2418456d07d
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2017-06-22 02:47:29 +0000

    New way to call image selection. Use rows in image
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@5600 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/GREIT/adult_ex03.m
+++ htdocs/tutorial/GREIT/adult_ex03.m
@@ -1,8 +1,11 @@
 load montreal_data_1995
 img= inv_solve(imdl, zc_resp(:,1), zc_resp);
-img.calc_colours.ref_level=0;
+
+img.show_slices.img_cols=9;
 show_slices(img);
 print_convert adult_ex03b.png
-img.elem_data= img.elem_data(:,22);
+
+img.calc_colours.ref_level = 0;
+img.get_img_data.frame_select = 22;
 show_fem(img);
 print_convert adult_ex03a.png
