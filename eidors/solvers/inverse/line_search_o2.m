4b87317501b70c92266028cbc7c5cf75c3b7befe
alistair_boyle <alistair_boyle@82006732-fb7e-4a22-9f28-86a212cce563>
2017-07-07 18:35:00 +0000

    inv_solve_core: spatio-temporal solver support
    See
    A. Boyle, 2017, "Spatio-Temporal Regularization over Many Frames",
    EIT2017, Dartmouth College, Hanover, NH, USA
    
     - temporal hyperparameter support
     - PCG inner-iterations solver for temporal regularization
     - LLt=eye works now; basic support for non-regularized temporal
       solutions
     - GN_residual support for temporal regularization over many frames;
       requires the same Kronecker identity to go from Kronecker product to
       matrix multiplies and a vec() operation
     - line_search_omn2: fixed input arguments which led to miscounting
       retries and a failure to exit on bad line searches
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@5612 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/solvers/inverse/line_search_o2.m
+++ eidors/solvers/inverse/line_search_o2.m
@@ -1,9 +1,13 @@
-function  [alpha, img, dv, opt] = line_search_o2(imgk, dx, data1, img1, N, W, hp2RtR, dv0, opt)
-% function  [alpha, img, dv, opt] = line_search_o2(imgk, dx, data1, img1, N, W, hp2RtR, dv0, opt)
+function  [alpha, img, dv, opt] = line_search_o2(imgk, dx, data1, img1, N, W, hps2RtR, hpt2LLt, dv0, opt)
+% function  [alpha, img, dv, opt] = line_search_o2(imgk, dx, data1, img1, N, W, hps2RtR, dv0, opt)
+% function  [alpha, img, dv, opt] = line_search_o2(imgk, dx, data1, img1, N, W, hps2RtR, hpt2LLt, dv0, opt)
 % line search function with a fitted polynomial of O(2) -- a second order polynomial
 % (C) 2013 Alistair Boyle
 % License: GPL version 2 or version 3
 
 pf_max = 2; % max poly-fit order = 2 --> second-order polynomial
 retry = 0; % first any only call through this function, no recursion
-[alpha, img, dv, opt] = line_search_onm2(imgk, dx, data1, img1, N, W, hp2RtR, dv0, opt, retry, pf_max);
+if nargin == 9
+   hpt2LLt = 1;
+end
+[alpha, img, dv, opt] = line_search_onm2(imgk, dx, data1, img1, N, W, hps2RtR, hpt2LLt, dv0, opt, retry, pf_max);
