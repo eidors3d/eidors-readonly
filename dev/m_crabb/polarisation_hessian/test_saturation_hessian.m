d7d111fc1e7fa42bfb6f15bebb39341f01e0ad97
michaelcrabb30 <michaelcrabb30@82006732-fb7e-4a22-9f28-86a212cce563>
2017-07-25 17:50:02 +0000

    Diagonal Hessian only code
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@5625 82006732-fb7e-4a22-9f28-86a212cce563

--- dev/m_crabb/polarisation_hessian/test_saturation_hessian.m
+++ dev/m_crabb/polarisation_hessian/test_saturation_hessian.m
@@ -1,5 +1,5 @@
 %Make an ivnerse model - standard foward model inside
-imdl = mk_common_model('c2C',16);
+imdl = mk_common_model('b2C',16);
 fmdl = imdl.fwd_model; %Extract model
 
 %Stimulation - 16 elecs adjacent current/adjacent voltage
@@ -13,7 +13,7 @@ cond_vals = linspace(cond_lower,cond_upper,100);
 
 %Pixk group of pixels to perturb
 pixel_group = [327,364,328,292,259,291];
-%pixel_group = [311,364];
+pixel_group = 1:length(fmdl.elems(:,1));
 
 %Make an image on unit conductivity and solve
 img = mk_image(fmdl,cond_bkg); img_b=img; 
@@ -29,7 +29,15 @@ J_pixel_group_sum = sum(J(:,pixel_group),2);
 %Calculate Hessian and see difference between quadratic approximation
 img.fwd_model.approx_type='tri3';
 H = calc_hessian(img_b.fwd_model,img_b,pixel_group);
+H_diag = calc_hessian_diag(img_b.fwd_model,img_b,pixel_group);
+
+%Put into matrix
 H_pixel_group = H(:,1:length(pixel_group),1:length(pixel_group));
+H_pixel_group_diag = H_diag(:,1:length(pixel_group),1:length(pixel_group));
+
+figure; subplot(121); imagesc(squeeze(H_diag(1,:,:)));
+subplot(122); imagesc(squeeze(H(1,:,:)))
+
 %for ff=1:size(H_pixel_group,1)
 %    H_pixel_group_sum(ff,1) = ones(length(pixel_group),1)'*...
 %        reshape(H_pixel_group(ff,:,:),length(pixel_group),length(pixel_group))*ones(length(pixel_group),1);
