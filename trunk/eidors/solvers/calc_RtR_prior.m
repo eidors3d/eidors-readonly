67f1f60edece0105f75661b09f8c355d0fa3dd51
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2018-02-24 21:35:47 +0000

    working version, except doesn't play with eidors_defaults

--- trunk/eidors/solvers/calc_RtR_prior.m
+++ trunk/eidors/solvers/calc_RtR_prior.m
@@ -27,33 +27,33 @@ function RtR_prior = calc_RtR_prior( inv_model )
 % If a function to calculate RtR_prior is not provided,
 % RtR = R_prior' * R_prior;
 
-% (C) 2005-2008 Andy Adler. License: GPL version 2 or version 3
+% (C) 2005-2018 Andy Adler. License: GPL version 2 or version 3
 % $Id$
+
+if ischar(inv_model) && strcmp(inv_model,'UNIT_TEST'); do_unit_test; return; end
 
 inv_model = rec_or_fwd_model( inv_model);
 
 if isfield(inv_model,'RtR_prior')
-   if isnumeric(inv_model.RtR_prior)
-      RtR_prior = inv_model.RtR_prior;
-   else
-      try inv_model.RtR_prior = str2func(inv_model.RtR_prior); end
-      RtR_prior= eidors_cache( inv_model.RtR_prior, inv_model );
-   end
+   RtR_prior= eidors_cache(@get_RtR_prior, inv_model, 'calc_RtR_prior' );
 elseif isfield(inv_model,'R_prior')
    RtR_prior = eidors_cache(@calc_from_R_prior, inv_model, 'calc_RtR_prior');
 else
    error('calc_RtR_prior: neither R_prior nor RtR_prior provided');
 end
 
-if isfield(inv_model.fwd_model,'coarse2fine')
-   c2f= inv_model.fwd_model.coarse2fine;
-   if size(RtR_prior,1)==size(c2f,1)
-%     we need to take into account coarse2fine - using a reasonable tol
-      eidors_msg('calc_RtR_prior: using coarse2fine to model RtR');
-      f2c= c2f'; %pinv(c2f,1e-6);
-      RtR_prior=f2c*RtR_prior*c2f;
+function RtR_prior = c2f_RtR_prior( inv_model, RtR_prior);
+   if isfield(inv_model.fwd_model,'coarse2fine')
+      c2f= inv_model.fwd_model.coarse2fine;
+      if size(RtR_prior,1)==size(c2f,1)
+   %     we need to take into account coarse2fine - using a reasonable tol
+         eidors_msg('calc_RtR_prior: using coarse2fine to model RtR');
+         f2c= c2f'; %pinv(c2f,1e-6);
+         RtR_prior=f2c*RtR_prior*c2f;
+
+         % now update the object in the cache
+      end
    end
-end
 
 function RtR_prior = calc_from_R_prior(inv_model)
 
@@ -66,7 +66,19 @@ function RtR_prior = calc_from_R_prior(inv_model)
       R= eidors_cache( inv_model.R_prior, inv_model );
    end
 
-   RtR_prior = R'*R;
+   RtR_prior = c2f_RtR_prior(inv_model, R'*R );
+
+
+
+function RtR_prior = get_RtR_prior( inv_model)
+   if isnumeric(inv_model.RtR_prior)
+      RtR_prior = inv_model.RtR_prior;
+   else
+      try inv_model.RtR_prior = str2func(inv_model.RtR_prior); end
+      RtR_prior= feval( inv_model.RtR_prior, inv_model );
+   end
+
+   RtR_prior = c2f_RtR_prior( inv_model, RtR_prior);
    
    
 
@@ -86,3 +98,27 @@ function inv_model = rec_or_fwd_model( inv_model);
          inv_model= rmfield(inv_model,'rec_model');
       end
    end
+
+
+function do_unit_test
+   imdl = mk_common_model('a2c2',16);;
+   imdl.RtR_prior = @prior_tikhonov;
+   RtR = calc_RtR_prior(imdl);
+   unit_test_cmp('RtR=prior_tik',RtR,speye(size(RtR,1)),1e-14);
+   imdl.RtR_prior = @prior_laplace;
+   RtR = calc_RtR_prior(imdl);
+   unit_test_cmp('RtR=prior_lapl',RtR(1:3,1:3),[6,-2,0;-2,6,-2;0,-2,6],1e-14);
+
+   imdl = rmfield(imdl,'RtR_prior');
+   imdl.R_prior = @prior_tikhonov;
+   RtR = calc_RtR_prior(imdl);
+   unit_test_cmp('R=prior_tik',RtR,speye(size(RtR,1)),1e-14);
+   imdl.R_prior = @prior_TV;
+   RtR = calc_RtR_prior(imdl);
+   unit_test_cmp('R=prior_TV',RtR(1:3,1:3)*16,[4,-1,0;-1,4,-1;0,-1,4],1e-14);
+
+   imdl = mk_common_model('b2c2',16);
+   grid = linspace(-1,1,16);
+   [imdl.rec_model, imdl.fwd_model.coarse2fine] = ...
+        mk_grid_model( imdl.fwd_model, grid, grid);
+   RtR = calc_RtR_prior(imdl);
