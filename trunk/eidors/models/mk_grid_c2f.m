dc7f316024c9d672c3ab8bc8532150af3c8e4efc
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2018-02-27 11:31:26 +0000

    start refactoring again based on x cuts

--- trunk/eidors/models/mk_grid_c2f.m
+++ trunk/eidors/models/mk_grid_c2f.m
@@ -122,38 +122,35 @@ function [c2f, m]= do_mk_grid_c2f(fmdl0,rmdl0,opt0)
        max_iter = floor(n_elems/step);
        pmopt.final_msg = '';
        ctr = interp_mesh(fmdl0);
-       [~,zidx] = sort(ctr(:,3)); % sort by x - real solution is travelling salesman
-       [~,~,zl] = ndgrid(opt0.xvec(1:end-1),opt0.yvec(1:end-1),opt0.zvec(1:end-1)); zl=zl(:);
-       [~,~,zu] = ndgrid(opt0.xvec(2:end-0),opt0.yvec(2:end-0),opt0.zvec(2:end-0)); zu=zu(:);
+       [~,xidx] = sort(ctr(:,1)); % sort by x - real solution is travelling salesman
+       [xl] = ndgrid(opt0.xvec(1:end-1),opt0.yvec(1:end-1),opt0.zvec(1:end-1)); xl=xl(:);
+       [xu] = ndgrid(opt0.xvec(2:end-0),opt0.yvec(2:end-0),opt0.zvec(2:end-0)); xu=xu(:);
        progress_msg('Progress:',0,max_iter+1,pmopt);
        progress = 0;
        for k = 0:max_iter;
           eidx = true(n_elems,1);
-          eidx( zidx((k*step+1):min((k+1)*step,n_elems)) ) = false;
+          eidx( xidx((k*step+1):min((k+1)*step,n_elems)) ) = false;
           fmdl = fmdl0; fmdl.elems(eidx,:) = [];
                         fmdl.edge2elem(:,eidx)= [];
                         fmdl.node2elem(:,eidx)= [];
                         fmdl.elem2face(eidx,:)= [];
-          zvals = fmdl.nodes(fmdl.elems(:),3);
-          ridx = true(size(zu));
-          ridx(zl>max(zvals)) = false;
-          ridx(zu<min(zvals)) = false;
           opt = opt0;   opt.nTet = sum(eidx==0);
-          opt.zvec( opt.zvec > max(zvals) ) = [];
-          opt.zvec( opt.zvec < min(zvals) ) = [];
-          opt.xplane = opt0.xplane(find(opt.zvec),:);
-          opt.yplane = opt0.yplane(find(opt.zvec),:);
-
-          rmdl = rmdl0;
-          idx = rmdl0.coarse2fine * ridx > 0;
-          rmdl.elems = rmdl0.elems(idx,:);
-keyboard
-
+if 0
+          xvals = fmdl.nodes(fmdl.elems(:),1);
+          ridx = true(size(xu));
+          ridx(xl>max(xvals)) = false;
+          ridx(xu<min(xvals)) = false;
+          rmdl = rmdl0; 
+             idx = rmdl0.coarse2fine * ridx > 0;
+             rmdl.elems = rmdl0.elems(idx,:);
+          opt.xvec( opt.xvec > max(xvals) ) = [];
+          opt.xvec( opt.xvec < min(xvals) ) = [];
+end
 
           eidors_msg('log_level',eidors_msg('log_level')-2);
-          c2f(~eidx,:) = separable_calculations(fmdl,rmdl,opt);
+          c2f(~eidx,:) = separable_calculations(fmdl,rmdl0,opt);
           eidors_msg('log_level',eidors_msg('log_level')+2);
-          progresS_msg(k+1, max_iter+1);
+          progress_msg(k+1, max_iter+1);
        end
        progress_msg(Inf);
     else % save_memory > 0
@@ -733,9 +730,9 @@ function [intpts, tri2edge, tri2intpt, edge2intpt] = get_tet_intersection_points
         in = in & ~reshape(any(bsxfun(@eq,z(:),v),2),size(in));
         M = bsxfun(@lt, z(:),v);
         M = xor(M(:,1:end-1), M(:,2:end));
-        edge_num1= reshape(uint32(sum(bsxfun(@times,uint32(M),uint32(1:SZ(op))),2)), size(z));
+%       edge_num1= reshape(uint32(sum(bsxfun(@times,uint32(M),uint32(1:SZ(op))),2)), size(z));
         edge_num = reshape(uint32(M*(1:SZ(op))'), size(z));
-if ~(all(all(edge_num1==edge_num))); keyboard; end
+%       if ~(all(all(edge_num1==edge_num))); keyboard; end
         in = in & edge_num > 0;
         if nnz(in) == 0, continue, end
         edge_num(~in) = 0;
