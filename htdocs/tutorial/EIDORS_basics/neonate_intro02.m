0f7749e770218882e91fb4a9b4236d4710958080
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2018-03-20 21:30:05 +0000

    image reconstruction
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@5711 82006732-fb7e-4a22-9f28-86a212cce563

--- htdocs/tutorial/EIDORS_basics/neonate_intro02.m
+++ htdocs/tutorial/EIDORS_basics/neonate_intro02.m
@@ -1,7 +1,6 @@
 vv= eidors_readdata('P04P-1016.get'); vi=vv(:,45); vh=vv(:,61);
 imr = inv_solve(imdl,vh,vi);
 
-
 clf; axes('position',[0.05,0.5,0.25,0.45]);
 imr.calc_colours = struct('ref_level',0,'greylev',0.2,'backgnd',[1,1,1]);
 show_slices(imr);
