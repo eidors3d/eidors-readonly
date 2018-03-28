632cdf63fa826e8d5dd25f9bc166f90962eb0ea2
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2018-03-28 15:46:51 +0000

    option keep3D working ... fixed unit tests
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@5724 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/models/solve_RM_2Dslice.m
+++ eidors/models/solve_RM_2Dslice.m
@@ -11,6 +11,10 @@ function imdl = solve_RM_2Dslice(imdl, sel_fcn)
 %
 % One could cut the z=1 slice as
 %    imdl2= solve_RM_2Dslice(imdl, 1.0);
+% Or using 
+%    sel_fcn = inline('abs(z-1.0)<0.2'); OR
+%    sel_fcn = 'abs(z-1.0)<0.2 | abs(z-2.0)<0.2' ; % two planes
+%    imdl2= solve_RM_2Dslice(imdl, sel_fcn);
 % 
 % Note that the reconstruction planes are between the
 % cut planes specified in vopt.zvec
@@ -27,9 +31,12 @@ function imdl = solve_RM_2Dslice(imdl, sel_fcn)
 if isstr(imdl) && strcmp(imdl,'UNIT_TEST'); do_unit_test; return; end
 
 % Options
-vert_tol = .001;
-try
+vert_tol = .001; try
   vert_tol = imdl.solve_RM_2Dslice.vert_tol;
+end
+
+keep3D=0; try
+  keep3D = imdl.solve_RM_2Dslice.keep3D;
 end
 
 ctr = interp_mesh(imdl.rec_model); ct3= ctr(:,3);
@@ -37,7 +44,10 @@ ctr = interp_mesh(imdl.rec_model); ct3= ctr(:,3);
 if isnumeric( sel_fcn )
    ff = abs(ct3-sel_fcn) < vert_tol;
 else
-   error huh?
+    if ischar(sel_fcn) %  we have a string, create a function
+      sel_fcn = inline(sel_fcn, 'z');
+    end
+    ff = feval(sel_fcn,ct3);
 end
 imdl.rec_model.coarse2fine(~ff,:) = [];
 imdl.rec_model.elems(~ff,:) = [];
@@ -46,8 +56,11 @@ fb = any(imdl.rec_model.coarse2fine,1);
 imdl.rec_model.coarse2fine(:,~fb) = [];
 imdl.fwd_model.coarse2fine(:,~fb) = [];
 imdl.solve_use_matrix.RM(~fb,:) = [];
-imdl.rec_model.boundary = find_boundary(imdl.rec_model);
 
+if keep3D; 
+   imdl.rec_model.boundary = find_boundary(imdl.rec_model);
+   return;
+end
 
 imdl.rec_model.nodes(:,3) = [];
 imdl.rec_model.elems(:,4) = [];
@@ -87,28 +100,39 @@ function do_unit_test
 
    img.show_slices.img_cols = 1;
    slice1 = calc_slices(img);
-   subplot(131); show_fem(fmdl); title 'fmdl'
-   subplot(132); show_slices(img,[inf,inf,2]); title '3D slice';
-   imdl = solve_RM_2Dslice(imdl, 2.0);
+   subplot(231); show_fem(fmdl); title 'fmdl'
+   subplot(234); show_fem(img);  title '3D'
+   subplot(232); show_slices(img,[inf,inf,2]); title '3D slice';
+   imd2 = solve_RM_2Dslice(imdl, 2.0);
 
-   unit_test_cmp('RM size', size(imdl.solve_use_matrix.RM), [856,928]);
-   unit_test_cmp('RM', imdl.solve_use_matrix.RM(1,1:2), ...
+   unit_test_cmp('RM size', size(imd2.solve_use_matrix.RM), [856,928]);
+   unit_test_cmp('RM', imd2.solve_use_matrix.RM(1,1:2), ...
        [-13.546930204647675   9.664897892828284], 1e-10);
-   img = inv_solve(imdl, vh, vi);
+   img = inv_solve(imd2, vh, vi);
    unit_test_cmp('img size', size(img.elem_data), [856,5]);
    [mm,ll] =max(img.elem_data(:,1));
    unit_test_cmp('img', [mm,ll], ...
        [0.031608449353163, 453], 1e-10);
-
-   img.show_slices.img_cols = 1;
-   subplot(133); show_slices(img); title '2D';
    slice2 = calc_slices(img);
 
    unit_test_cmp('slice Nan', isnan(slice1), isnan(slice2))
    slice1(isnan(slice1))= 0;
    slice2(isnan(slice2))= 0;
    unit_test_cmp('slice value', slice1, slice2, 1e-10)
+
    
+   imdl.solve_RM_2Dslice.keep3D = 1;
+   imd3 = solve_RM_2Dslice(imdl, 1.0);
+   img = inv_solve(imd3, vh, vi);
+   subplot(235); show_fem(img);
+   
+   sel_fcn = inline('abs(z-1.0)<0.2','z');
+   imd3 = solve_RM_2Dslice(imdl, sel_fcn);
+
+   sel_fcn = 'abs(z-1.0)<0.2 | abs(z-2.0)<0.2' ;
+   imd3 = solve_RM_2Dslice(imdl, sel_fcn);
+   img = inv_solve(imd3, vh, vi);
+   subplot(236); show_fem(img);
 
 function fmdl = mystim_square(fmdl);
    [~,fmdl] = elec_rearrange([16,2],'square', fmdl);
