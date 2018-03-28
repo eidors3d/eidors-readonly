c9d05b713abf6c3f0d133dcd138e37aef6fbb9e5
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2018-03-28 11:57:45 +0000

    Set svn properties
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@5720 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/solvers/inverse/prior_laplace_old.m
+++ eidors/solvers/inverse/prior_laplace_old.m
@@ -22,7 +22,7 @@ function Reg= prior_laplace_old( inv_model );
 % previous implementation requires searching for faces in the mesh
 
 % (C) 2005 Andy Adler. License: GPL version 2 or version 3
-% $Id: $
+% $Id$
 
 if ischar(inv_model) && strcmp(inv_model,'UNIT_TEST'); do_unit_test; return; end
 
