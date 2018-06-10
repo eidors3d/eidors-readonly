fe2eba281c5b156ef894788c731aab9f841bead5
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2018-06-10 08:17:18 +0000

    add zeros to lines when there are none
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@5813 82006732-fb7e-4a22-9f28-86a212cce563

--- pubs/conferences/EIT2018/EIDORS3.9.1-EIT2018/git-loc.sh
+++ pubs/conferences/EIT2018/EIDORS3.9.1-EIT2018/git-loc.sh
@@ -10,15 +10,15 @@ for ver in `seq 1 $LASTREV`; do
    (cd $REPO && git checkout $GITVER);
    DATE=`(cd $REPO && git show -s --format=%ct $GITVER) | perl -pe'chomp'`;
    find $REPO -type f -name \*.m -print0 | xargs -0 perl \
-       -e  'BEGIN{my $lines = 0};' \
+       -e  'BEGIN{$lines = 0};' \
        -ne 'next if /^\s*%/; next if /^\s*$/; $lines++;' \
        -e  'END{print "  $lines,";}' >> $OUTF ; # all (/)
    find $REPO/htdocs -type f -name \*.m -print0 | xargs -0 perl \
-       -e  'BEGIN{my $lines = 0};' \
+       -e  'BEGIN{$lines = 0};' \
        -ne 'next if /^\s*%/; next if /^\s*$/; $lines++;' \
        -e  'END{print "  $lines,";}' >> $OUTF ; # htdocs
    find $REPO/dev -type f -name \*.m -print0 | xargs -0 perl \
-       -e  'BEGIN{my $lines = 0};' \
+       -e  'BEGIN{$lines = 0};' \
        -ne 'next if /^\s*%/; next if /^\s*$/; $lines++;' \
        -e  'END{print "  $lines,";}' >> $OUTF ; # dev
    echo "  $ver,    $DATE; " >> $OUTF;
