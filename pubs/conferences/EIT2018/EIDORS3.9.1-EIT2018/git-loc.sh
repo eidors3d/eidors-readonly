9cbc1246bd234796771c62dbec5842b0d236ed43
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2018-06-11 07:38:41 +0000

    data log
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@5816 82006732-fb7e-4a22-9f28-86a212cce563

--- pubs/conferences/EIT2018/EIDORS3.9.1-EIT2018/git-loc.sh
+++ pubs/conferences/EIT2018/EIDORS3.9.1-EIT2018/git-loc.sh
@@ -4,6 +4,7 @@ REPO=${1:-"$HOME/docs/eidors-git"}
 OUTF=datafile.m
 LASTREV=$(cd $REPO && git svn log --oneline HEAD^^^^^..HEAD | head -1 | cut -f1 -d'|' | sed 's/r//')
 echo 'loc = [ % all(/)  htdocs   dev   version  date' > $OUTF
+echo "for ver in 1 ... $LASTREV; do  ..."
 for ver in `seq 1 $LASTREV`; do 
    echo "$REPO: VER=$ver";
    GITVER=`(cd $REPO && git svn find-rev r$ver)`;
@@ -12,22 +13,22 @@ for ver in `seq 1 $LASTREV`; do
    find $REPO -type f -name \*.m -print0 | xargs -0 perl \
        -e  'BEGIN{$lines = 0};' \
        -ne 'next if /^\s*%/; next if /^\s*$/; $lines++;' \
-       -e  'END{printf("  %08d,",$lines);}' >> $OUTF ; # all (/)
+       -e  'END{printf("  %06d,",$lines);}' >> $OUTF ; # all (/)
    find $REPO/eidors -type f -name \*.m -print0 | xargs -0 perl \
        -e  'BEGIN{$lines = 0};' \
        -ne 'next if /^\s*%/; next if /^\s*$/; $lines++;' \
        -e  'END{print "  $lines,";}' >> $OUTF ; # htdocs
-       -e  'END{printf("  %08d,",$lines);}' >> $OUTF ; # eidors
+       -e  'END{printf("  %06d,",$lines);}' >> $OUTF ; # eidors
    find $REPO/htdocs -type f -name \*.m -print0 | xargs -0 perl \
        -e  'BEGIN{$lines = 0};' \
        -ne 'next if /^\s*%/; next if /^\s*$/; $lines++;' \
        -e  'END{print "  $lines,";}' >> $OUTF ; # htdocs
-       -e  'END{printf("  %08d,",$lines);}' >> $OUTF ; # htdocs
+       -e  'END{printf("  %06d,",$lines);}' >> $OUTF ; # htdocs
    find $REPO/dev -type f -name \*.m -print0 | xargs -0 perl \
        -e  'BEGIN{$lines = 0};' \
        -ne 'next if /^\s*%/; next if /^\s*$/; $lines++;' \
        -e  'END{print "  $lines,";}' >> $OUTF ; # dev
-       -e  'END{printf("  %08d,",$lines);}' >> $OUTF ; # dev
+       -e  'END{printf("  %06d,",$lines);}' >> $OUTF ; # dev
    echo "  $ver,    $DATE; " >> $OUTF;
 done
 echo "];" >> $OUTF
