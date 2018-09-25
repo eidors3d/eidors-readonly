b8b03a8f62d6402b058e4686177b93de97160ec6
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2018-09-25 14:10:14 +0000

    Released by Beat Mueller to the community under GPL (v2 or later)
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@5828 82006732-fb7e-4a22-9f28-86a212cce563

---
+++ dev/a_adler/splitEITfile.m
@@ -0,0 +1,93 @@
+function splitEITfile(filename, newFilename, range)
+% split EIT file
+%
+% filename:     name of file
+% newFilename:  store file here
+% range:        [startFrame endFrame]
+%
+% header is same as filename
+% if endFrame is bigger than number of frames, only available frames are 
+% copied
+% only consider fileformat 4
+
+    fid = fopen(filename, 'r', 'ieee-le', 'UTF-8');
+    
+    format_version = fread(fid, 1, 'int32', 'ieee-le');
+    
+    if format_version ~= 4
+        error('wrong file version')
+    end
+    
+    header_size = fread(fid, 1, 'uint32', 'ieee-le');
+    
+    tsStart = fread(fid, 1, 'uint64', 'ieee-le');
+    tsEnd = fread(fid, 1, 'uint64', 'ieee-le');
+    nFrames = fread(fid, 1, 'uint32', 'ieee-le');
+    
+    % check range:
+    if length(range) ~= 2
+        error('range has wrong size');
+    end
+    
+    if range(1) > nFrames || range(1) > range(2)
+        error('range not well defined');
+    end
+    
+    % check header_size
+    if header_size < 2664
+        disp('assume header size of 2664');
+        header_size = 2664;
+    end
+    
+    % create new file and copy input
+    fNewId = fopen(newFilename, 'w', 'ieee-le', 'UTF-8');
+    fseek(fid, 0, 'bof');
+    
+    tmp = fread(fid, header_size, 'int8', 'ieee-le');
+    fwrite(fNewId, tmp, 'int8', 'ieee-le');
+    
+     i = 0;
+     storeFrames = 0;
+     copiedFrames = 0;
+     while fseek(fid, 1,'cof') ~= -1
+        fseek(fid, -1,'cof');
+        % need to have payload
+        tStampsAbs = fread(fid,1,'int64','ieee-le'); 
+        ft = fread(fid,1,'int32','ieee-le'); 
+        pl = fread(fid,1,'int32','ieee-le');
+        
+        if ft == 0
+            i = i + 1;
+            
+            if i >= range(1) && i <= range(2)
+                storeFrames = 1;
+                copiedFrames = copiedFrames + 1;
+            else
+                storeFrames = 0;
+            end
+        end
+        
+        if storeFrames == 1
+            % put it into new Files
+            fwrite(fNewId, tStampsAbs, 'int64','ieee-le'); 
+            fwrite(fNewId, ft, 'int32', 'ieee-le');
+            fwrite(fNewId, pl, 'int32', 'ieee-le');
+            if pl > 0
+                payLoad = fread(fid, pl, 'int8', 'ieee-le');
+                fwrite(fNewId, payLoad, 'int8', 'ieee-le');
+            end
+        elseif pl > 0
+            % go to the next frame
+            fseek(fid,pl,'cof'); 
+        elseif pl < 0
+            disp('payloadsize < 0. try to continue');
+        end
+    end      
+    % readjust number of Frame
+    fseek(fNewId, 4 + 4 + 8 + 8, 'bof');
+    fwrite(fNewId, copiedFrames, 'uint32', 'ieee-le');
+
+
+    fclose(fid);
+    fclose(fNewId);
+    
