0a3a736aa2991e6c7fd96fdb6ec3c3093bb8cf02
michaelcrabb30 <michaelcrabb30@82006732-fb7e-4a22-9f28-86a212cce563>
2018-11-02 17:22:22 +0000

    Add line plots through 2D image
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@5846 82006732-fb7e-4a22-9f28-86a212cce563

--- dev/m_crabb/polarisation_hessian/line_plot_2d_img.m
+++ dev/m_crabb/polarisation_hessian/line_plot_2d_img.m
@@ -1,28 +1,37 @@
 function img_lp = line_plot_2d_img(img,x0,xf,n_points)
 % LINE_PLOT_2D_IMG : Plot straight line through 2D image
-% img_lp = line_plot_2d_img(img,x0,y0,x1,x1,n_points)
-% img is 2D image
-% x0 and xf are start and end Cartesian coordinates
-% n_points is number of points
-% img_lp is image along the 1D line
+% img_lp = line_plot_2d_img(img,x0,xf,n_points)
+% img - 2D image
+% x0/xf - start/end Cartesian coordinates (assumed lie on boundary): dim 2
+% n_points is number of (equispaced) points for line plot through
+% img_lp is image along 1D line: dim n_points
 
-m_ = (xf(2)-x0(2))/(xf(1)-x0(1)); c_ = xf(2)-m_*xf(1);
+if(xf(1)==x0(1)) %vertical line plot
+else
+    m_ = (xf(2)-x0(2))/(xf(1)-x0(1)); c_ = xf(2)-m_*xf(1);
+end
 
 elem_list = img.fwd_model.elems;
 node_list = img.fwd_model.nodes;
 
 %Specific points along straight line
 for i=1:n_points+1
-    xi = x0(1)+(i-1)*(xf(1)-x0(1))/n_points; %Regular in x
-    yi = m_*xi + c_;
-    xycoords(i,:)=[xi,yi];    
+    if(xf(1)==x0(1)) %vertical line plot just regular in y
+        xi = x0(1);
+        yi = x0(2) +(i-1)*(xf(2)-x0(2))/n_points;
+        xycoords(i,:)=[xi,yi];    
+    else
+        xi = x0(1)+(i-1)*(xf(1)-x0(1))/n_points; %Regular in x
+        yi = m_*xi + c_;
+        xycoords(i,:)=[xi,yi];          
+    end
 end
 
 %Find element for which each point belongs
 t_lp = tsearchn(node_list,elem_list,xycoords);
 
 %Plot the image
-img_lp(1)=1; img_lp(n_points+1)=1; %assume backgroun =1 
+img_lp(1)=1; img_lp(n_points+1)=1; %assume background =1 
 for i=2:n_points
     img_lp(i)=img.elem_data(t_lp(i));    
 end
--- dev/m_crabb/polarisation_hessian/test_line_plot_2d_img.m
+++ dev/m_crabb/polarisation_hessian/test_line_plot_2d_img.m
@@ -3,8 +3,8 @@ load test_phess_solve_data.mat
 %Chose a sep rad idx and plot
 sepidx=1; radidx=1;
 
-x0 = [-1/sqrt(2),-1/sqrt(2)];
-xf = [1/sqrt(2),1/sqrt(2)];
+x0 = [-1/sqrt(2);-1/sqrt(2)];
+xf = [1/sqrt(2);1/sqrt(2)];
 img_pt_disc_line = ...
     line_plot_2d_img(img_pt_disc{sepidx,radidx},x0,xf,500);        
 img_gn0_line = ...
