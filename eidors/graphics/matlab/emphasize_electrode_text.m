1402eb417894d65a78018d30895b15d5319fe9a6
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2018-11-24 20:10:22 +0000

    Emphasize electrode positions to view more easily   -- next step is to add options
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@5858 82006732-fb7e-4a22-9f28-86a212cce563

---
+++ eidors/graphics/matlab/emphasize_electrode_text.m
@@ -0,0 +1,24 @@
+function emphasize_electrode_text;
+% EMPHASIZE_ELECTRODE_TEXT: ephazise the electrode
+%   text on a "show_fem" plot
+% emphasize_electrode_text(hh)
+%
+% (C) 2005-2018 Andy Adler. License: GPL version 2 or version 3
+% $Id$
+
+hh = get(gca,'Children');
+
+bgcolour = [1,1,1];
+tmp = view;
+dirn = tmp(1:3,3); dirn = -dirn'/norm(dirn) 
+
+for hi = hh(:)'
+  if ~strcmp(get(hi,'Type'),'text'); 
+     continue;
+  end
+  set(hi, 'BackgroundColor', bgcolour);
+  set(hi, 'FontSize', 12);
+  pn = get(hi, 'Position') 
+  set(hi, 'Position', pn + dirn);
+end
+
