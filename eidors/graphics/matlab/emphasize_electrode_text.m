8942e37724c61b3aac12b01edb7c45097331f1c9
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2019-02-26 06:55:10 +0000

    Add ability to specity electrodes. Stil don't understand the "view" matrix
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@5916 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/graphics/matlab/emphasize_electrode_text.m
+++ eidors/graphics/matlab/emphasize_electrode_text.m
@@ -1,7 +1,8 @@
-function emphasize_electrode_text;
+function emphasize_electrode_text(elecs);
 % EMPHASIZE_ELECTRODE_TEXT: ephazise the electrode
 %   text on a "show_fem" plot
-% emphasize_electrode_text(hh)
+% emphasize_electrode_text(elecs)
+%   elecs => which elecs to emphasize (default, all)
 %
 % (C) 2005-2018 Andy Adler. License: GPL version 2 or version 3
 % $Id$
@@ -9,16 +10,23 @@ function emphasize_electrode_text;
 hh = get(gca,'Children');
 
 bgcolour = [1,1,1];
-tmp = view;
+tmp = inv(view);
 dirn = tmp(1:3,3); dirn = -dirn'/norm(dirn);
 
-for hi = hh(:)'
+if nargin==0;
+  elecs= 1:length(hh)'; % all of them
+end
+
+k=0;for hi = hh(:)'; k=k+1;
   if ~strcmp(get(hi,'Type'),'text'); 
      continue;
+  end
+  if ~any(str2num(get(hi,'String'))==elecs);
+      continue;
   end
   set(hi, 'BackgroundColor', bgcolour);
   set(hi, 'FontSize', 12);
   pn = get(hi, 'Position');
-  set(hi, 'Position', pn + dirn);
+  set(hi, 'Position', pn + dirn*10);
 end
 
