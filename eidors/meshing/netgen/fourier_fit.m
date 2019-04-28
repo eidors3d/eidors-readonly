eb699b80aa0ece45d4d719234a0b0e3b966a7763
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2019-04-28 13:27:42 +0000

    Add 'fit_spacing' feature to clean up code calling twice
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@5938 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/meshing/netgen/fourier_fit.m
+++ eidors/meshing/netgen/fourier_fit.m
@@ -1,4 +1,4 @@
-function C = fourier_fit(points,N,start);
+function [C,th] = fourier_fit(points,N,start);
 % FOURIER_FIT: use fourier series to interpolate onto a boundary
 %
 % [pp] = fourier_fit(points) fits a Fourier series
@@ -12,14 +12,28 @@ function C = fourier_fit(points,N,start);
 %    linear_frac - vector of length fractions (0 to 1) to calculate points
 %    start       - (optional) a seed for the first point
 %    xy          - cartesian coordinates of the points
+%
+%
+% xy = fourier_fit(points,'fit_spacing',spacing);
+% example
+%  xy = fourier_fit(point,'fit_spacing',0:.1:999);
+%  th = atan2(xy(:,2)-centroid(2),xy(:,1)-centroid(1));
+% 
 
 % (C) Andy Adler, 2010. Licenced under GPL v2 or v3
 % $Id$
 
 if ischar(points) && strcmp(points,'UNIT_TEST'); do_unit_test; return ; end
 
+if nargin<2; N= size(points,1); end
+if ischar(N) && strcmp(N,'fit_spacing')
+   pp = fourier_fit(points, size(points,1),points(1,:));
+   C = fourier_fit(pp, start);
+   return
+end
+
+
 if size(points,2)==2 % calling analysis function
-   if nargin<2; N= size(points,1); end
    C = fit_to_fourier(points,N);
 elseif size(points,2)==1 % calling synthesis function
    if nargin<3; start = []; end
@@ -79,9 +93,10 @@ function xy = fit_from_fourier(C,linear_frac,start);
    
 function do_unit_test
 
-    a = [
-   -0.8981   -0.7492   -0.2146    0.3162    0.7935    0.9615    0.6751    0.0565   -0.3635   -0.9745
-    0.1404    0.5146    0.3504    0.5069    0.2702   -0.2339   -0.8677   -0.6997   -0.8563   -0.4668 ]';
+a=[-0.8981  0.1404;-0.7492  0.5146;-0.2146  0.3504;
+    0.3162  0.5069; 0.7935  0.2702; 0.9615 -0.2339;
+    0.6751 -0.8677; 0.0565 -0.6997;-0.3635 -0.8563;
+   -0.9745 -0.4668];
 
    C= fourier_fit(a,10);
    xy = fourier_fit(C, linspace(.05,1.04,100));
@@ -94,3 +109,20 @@ function do_unit_test
    xy2= fourier_fit(C, [.3,.4,.5,.6]);
    plot(a(:,1),a(:,2),'r',xy(:,1),xy(:,2),'b',xy2(:,1),xy2(:,2),'m*');
    eidors_msg('VIEW GRAPH TO VERIFY',0);
+
+subplot(212);
+ n_elecs = 8; centroid = mean(a);
+ p = linspace(0,1,n_elecs+1)'; p(end) = [];
+ xy = fourier_fit(a,'fit_spacing',p);
+ plot(a(:,1),a(:,2),'r',xy(:,1),xy(:,2),'b*');
+
+  centroid = mean(a);
+ th = atan2(xy(:,2)-centroid(2), xy(:,1)-centroid(1));
+ hold on;
+ aa = [xy(:,1),centroid(1)+0*xy(:,1)]';
+ bb = [xy(:,2),centroid(1)+0*xy(:,2)]';
+ plot(aa,bb,'k-');
+  unit_test_cmp('fit th',diff(th(1:4)), [ -0.671324440246111;
+  -1.011260720375461; -0.791975282338955 ],1e-10);
+
+
