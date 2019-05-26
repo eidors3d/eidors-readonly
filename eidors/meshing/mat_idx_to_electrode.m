bf0c502e8a23caaac10f4509c3745d4b5dff41f6
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2019-05-26 13:47:16 +0000

    Documentation and code to work with images
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@5946 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/meshing/mat_idx_to_electrode.m
+++ eidors/meshing/mat_idx_to_electrode.m
@@ -1,24 +1,31 @@
-function fmdl = mat_idx_to_electrode(fmdl, mat_idxes)
+function [fmdl,rm_elems] = mat_idx_to_electrode(fmdl, mat_idxes)
 % MAT_IDX_TO_ELECTRODE: create electrodes from mat_idx values
 % fmdl = mat_idx_to_electrode(fmdl, mat_idxes)
 % fmdl: input and output fmdl
 % Options: fmdl.mat_idx_to_electrode.z_contact  (z_contact value to use ... or default)
 % mat_idxes = cell array of mat_idxes => convert electrode
 %    example mat_idxes = {1:2, 5, [12,14]}
+%
+% To work with an image object, use:
+%  [img.fwd_model,rm_elems] = mat_idx_to_electrode(img.fwd_model,{mat_idxes});
+%  img.elem_data(rm_elems) = [];
 
 % (C) 2019 Andy Adler. License: GPL v2 or v3.
 % $Id$
 
 if ischar(fmdl) && strcmp(fmdl,'UNIT_TEST'); do_unit_test; return; end
 
+rm_elems = [];
 for i = 1:length(mat_idxes)
-   fmdl = create_electrode_from_mat_idx(fmdl, mat_idxes{i});
+   [fmdl,rm_elemi] = create_electrode_from_mat_idx(fmdl, mat_idxes{i});
+   rm_elems = union(rm_elems,rm_elemi); 
 end
 
 fmdl = remove_unused_nodes(fmdl);
 
 % adds electrode to the end
-function fmdl = create_electrode_from_mat_idx(fmdl,nmat_idx);
+% femobj is removed elems
+function [fmdl,femobj] = create_electrode_from_mat_idx(fmdl,nmat_idx);
    zc = 1e-5;
    try zc = fmdl.mat_idx_to_electrode.z_contact;
    end
@@ -45,10 +52,33 @@ function fmdl = create_electrode_from_mat_idx(fmdl,nmat_idx);
 function do_unit_test
    extra={'ball_inside','ball_surface', [ ...
           'solid ball_inside  = sphere(-0.4,0,0.5;0.05);' ...
-          'solid ball_surface = sphere( 0.4,0,1.0;0.05);' ...
+          'solid ball_surface = sphere( 0.4,0,1.0;0.05) -maxh=.005;' ...
           ]};
    fmdl= ng_mk_cyl_models(1,[8,.5],[.1],extra); 
-   fmdl = mat_idx_to_electrode(fmdl, {2,3});
-   unit_test_cmp('cyl_models',num_elecs(fmdl),10);
+   fmd_= mat_idx_to_electrode(fmdl, {2,3});
+   unit_test_cmp('cyl_models 01',num_elecs(fmd_),10);
 
-   show_fem(fmdl); view(3,12);
+   fmd_= mat_idx_to_electrode(fmdl, {2:3});
+   unit_test_cmp('cyl_models 02',num_elecs(fmd_),9);
+
+   fmd_= mat_idx_to_electrode(fmdl, {2});
+   unit_test_cmp('cyl_models 03',num_elecs(fmd_),9);
+
+   img = mk_image(fmd_,1);
+   img.elem_data(fmd_.mat_idx{3})= 1.1;
+   img.calc_colours.ref_level = 1;
+   unit_test_cmp('cyl_models 04',num_elecs(img),9);
+
+   img = mk_image(fmdl,1);
+   img.elem_data(vertcat(fmdl.mat_idx{2:3}))= 1.1;
+   [img.fwd_model,rm_elems]= mat_idx_to_electrode( ...
+        img.fwd_model, {2});
+   img.elem_data(rm_elems) = [];
+   unit_test_cmp('cyl_models 05',num_elecs(img),9);
+   vol = get_elem_volume(img);
+   vvs = sum(vol(find(img.elem_data == 1.1)));
+   unit_test_cmp('cyl_models 05a',vvs,4/3*pi*.05^3,1e-5);
+  
+
+
+   show_fem(img); view(3,12);
