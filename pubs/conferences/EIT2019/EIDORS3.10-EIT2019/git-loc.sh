8e4d7337c52dd936e5c64d812550f6aa285ce227
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2019-06-16 13:30:51 +0000

    make sure perl outputs zeros for matlab
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@5975 82006732-fb7e-4a22-9f28-86a212cce563

--- pubs/conferences/EIT2019/EIDORS3.10-EIT2019/git-loc.sh
+++ pubs/conferences/EIT2019/EIDORS3.10-EIT2019/git-loc.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # Sequentially checkout versions 
 REPO=${1:-"$HOME/docs/eidors-git"}
+echo "Look at $REPO";
 OUTF=datafile.m
 LASTREV=$(cd $REPO && git svn log --oneline HEAD^^^^^..HEAD | head -1 | cut -f1 -d'|' | sed 's/r//')
+echo "LastRev is $LASTREV";
 echo 'loc = [ % all(/)  htdocs   dev   version  date' > $OUTF
 for ver in `seq 1 $LASTREV`; do 
    echo "$REPO: VER=$ver";
@@ -12,15 +14,15 @@ for ver in `seq 1 $LASTREV`; do
    find $REPO -type f -name \*.m -print0 | xargs -0 perl \
        -e  'BEGIN{my $lines = 0};' \
        -ne 'next if /^\s*%/; next if /^\s*$/; $lines++;' \
-       -e  'END{print "  $lines,";}' >> $OUTF ; # all (/)
-   find $REPO/htdocs -type f -name \*.m -print0 | xargs -0 perl \
+       -e  'END{printf"  %d,",$lines;}' >> $OUTF ; # all (/)
+   find $REPO/htdocs -path $REPO/htdocs/doc -prune -o -type f -name \*.m -print0 | xargs -0 perl \
        -e  'BEGIN{my $lines = 0};' \
        -ne 'next if /^\s*%/; next if /^\s*$/; $lines++;' \
-       -e  'END{print "  $lines,";}' >> $OUTF ; # htdocs
+       -e  'END{printf"  %d,",$lines;}' >> $OUTF ; # htdocs
    find $REPO/dev -type f -name \*.m -print0 | xargs -0 perl \
        -e  'BEGIN{my $lines = 0};' \
        -ne 'next if /^\s*%/; next if /^\s*$/; $lines++;' \
-       -e  'END{print "  $lines,";}' >> $OUTF ; # dev
+       -e  'END{printf"  %d,",$lines;}' >> $OUTF ; # dev
    echo "  $ver,    $DATE; " >> $OUTF;
 done
 echo "];" >> $OUTF
