4f04f5f0c6cb27bb3ef2cd5baa6a9d0469044eed
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2019-06-16 15:36:27 +0000

    figs and graph
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@5976 82006732-fb7e-4a22-9f28-86a212cce563

--- pubs/conferences/EIT2019/EIDORS3.10-EIT2019/graph_loc.m
+++ pubs/conferences/EIT2019/EIDORS3.10-EIT2019/graph_loc.m
@@ -1,23 +1,37 @@
 function graph_loc
    %% Graph the lines of code
-   datafile; dat = loc;
+   datafile; dat = loc; t=dat(:,5);
    clf
    hold on;
-   plot(dat(:,5),1e-3*dat(:,1),            '-','Color',[2,0,0]/3,'LineWidth',2);
-   plot(dat(:,5),1e-3*dat(:,1:3)*[1;-1;-1],'-','Color',[1,1,0]/3,'LineWidth',2);
-   plot(dat(:,5),1e-3*dat(:,2),            '-','Color',[0,2,0]/3,'LineWidth',2);
-   plot(dat(:,5),1e-3*dat(:,3),            '-','Color',[0,0,2]/3,'LineWidth',2);
+   hh(1)=plot(t,1e-3*dat(:,1),            '-','Color',[2,0,0]/3,'LineWidth',3);
+   hh(2)=plot(t,1e-3*dat(:,1:3)*[1;-1;-1],'-','Color',[1,1,0]/3,'LineWidth',3);
+   hh(3)=plot(t,1e-3*dat(:,2),            '-','Color',[0,2,0]/3,'LineWidth',3);
+   hh(4)=plot(t,1e-3*dat(:,3),            '-','Color',[0,0,2]/3,'LineWidth',3);
 
-   k=1;for yy=2004:2017;
+   k=1;for yy=2004:2019;
      tt(k) = (datenum(yy,1,1)-datenum(1970,1,1))*86400;
      tl{k} = num2str(yy); if rem(yy,2)==0; tl{k}=''; end
    k=k+1; end
    set(gca,'xtick',tt)
    set(gca,'xticklabel',tl);
    ylim([0, ylim*[0;1]]);
+   xlim([min(t),max(t)+0e7]);
+   releaselines
+%  uistack(hh,'top'); %%% Screws up colours
+   hold off;
+   set(gca,'Box','off')
+   set(gcf,'PaperPosition',[1,1,6,2.5]);
+   legend('Total','Eidors','Tutorials','Dev','location','northwest');
+   ylabel('Lines of Code (x1000)')
+   print -dpdf fig_loc.pdf
+   !LD_LIBRARY_PATH="" pdfcrop fig_loc.pdf fig_loc.pdf
+
+function releaselines
+
 release= [
-(datenum(2017,06,12)-datenum(1970,1,1))*86400; % eidors-v3.9 Currently a guess
-1431302400; %eidors-v3.8     2015-05-11
+(datenum(2019,06,30)-datenum(1970,1,1))*86400; % eidors-v3.9.1
+(datenum(2018,06,1)-datenum(1970,1,1))*86400; % eidors-v3.9.1
+(datenum(2017,06,21)-datenum(1970,1,1))*86400; % eidors-v3.9
 1431302400; %eidors-v3.8     2015-05-11
 1369785600; %eidors-v3.7.1   2013-05-29
 1366675200; %eidors_v3.7     2013-04-23
@@ -33,14 +47,11 @@ release= [
 1130803200; %EIDORS 3.0      2005-11-01
 %1129680000; %EIDORS 3.0RC1   2005-10-19
 ];
-   plot([1;1]*release(:)',[0,ylim*[0;1]], 'Color',[2,2,2]/4);
-   hold off;
-   set(gca,'Box','off')
-   set(gcf,'PaperPosition',[1,1,6,2.5]);
-   legend('Total','Eidors','Tutorials','Dev','location','northwest');
-   ylabel('Lines of Code (x1000)')
-   print -dpdf fig_loc.pdf
-   !LD_LIBRARY_PATH="" pdfcrop fig_loc.pdf fig_loc.pdf
+   hh=plot([1;1]*release(:)',[0,ylim*[0;1]], 'Color',[2,2,2]/4,'LineWidth',2);
+%  uistack(hh,'bottom'); % screws up legend
+   set(hh,'HandleVisibility','off'); % Don't show up in legend
+   hh= get(gca,'Children'); 
+   set(gca,'Children',[hh(end);hh(1:end-1)]);  % put grey at bottom
 
 function add_line(loc,clr)
    hold on
