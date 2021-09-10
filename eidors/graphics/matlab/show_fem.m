3d3625b7b2933f4bac3ede387c47a583d4c5a339
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2021-09-10 12:08:56 +0000

    Fix for tri meshes in 3D
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@6136 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/graphics/matlab/show_fem.m
+++ eidors/graphics/matlab/show_fem.m
@@ -653,9 +653,14 @@ function hh = paint_inho(data, nodes, elems, thresh, img)
         fc = 'interp';
     else
         colours = colours(ii,:);
-        colours = kron(colours,ones(4,1));
     end
-    idx = [1 2 3; 1 2 4; 1 3 4; 2 3 4]';
+    switch size(elems,2)
+        case 3
+            idx = [1 2 3];
+        case 4
+            idx = [1 2 3; 1 2 4; 1 3 4; 2 3 4]';
+            colours = kron(colours,ones(4,1));
+    end
     faces = reshape(elems(ii,idx)',3,[])';
         
     hh = patch('Faces',faces,'Vertices',nodes,'facecolor',fc,...
