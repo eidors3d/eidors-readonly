a8ad3c82c7158ca94082e9647a312892a0f2a94d
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2021-09-20 11:27:27 +0000

    Add copies of overloads/sparse.m in multiple private directories as a workaround for Matlab not using our overloaded version even though it should.
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@6142 82006732-fb7e-4a22-9f28-86a212cce563

---
+++ eidors/solvers/inverse/private/sparse.m
@@ -0,0 +1,18 @@
+function S = sparse(varargin)
+%SPARSE Create sparse matrix (EIDORS overload).
+%   S = SPARSE(X) converts a sparse or full matrix to sparse form by
+%   squeezing out any zero elements.
+%
+%  See also SPARFUN/SPARSE
+%
+% Sparse doesn't work for uint* inputs, so we need to preconvert to double
+
+% (C) 2011 Bartlomiej Grychtol. License: GPL v2 or v3.
+% $Id$
+
+for i= 1:nargin
+  if ~isa(varargin{i},'double')
+     varargin{i} = double(varargin{i});
+  end
+end
+S = builtin('sparse',varargin{:});
