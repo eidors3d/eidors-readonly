f6ce5ba584a6173ded56f5ddd7e2d8d6bb3c4fee
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2021-11-27 01:36:23 +0000

    refactored pieces
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@6204 82006732-fb7e-4a22-9f28-86a212cce563

--- dev/a_adler/remesh_solver.m
+++ dev/a_adler/remesh_solver.m
@@ -8,51 +8,64 @@ function data =nov26_remesh_solver(img)
 
 if ischar(img) && strcmp(img,'UNIT_TEST'); do_unit_test;return;end
 
-fmdl = img.fwd_model;
-E = calc_system_mat(img);       E=E.E;
-pp= fwd_model_parameters(fmdl); Q=pp.QQ;
-
-Nnode= num_nodes(img);
-base = [fmdl.remesh_solver.base_nodes(:);
-  Nnode+fmdl.remesh_solver.base_elecs(:)];
-extr = 1:size(E,1); extr(base) = [];
-base(base == fmdl.gnd_node) = [];
-A = E(base,base);
-B = E(base,extr);
-D = E(extr,extr);
-iA= Q(base,:);
-iD= Q(extr,:);
-
-vA = left_divide(A,iA);
-Ab = left_divide(A,B);
-bAb= B'*Ab;
-
-DmA= D - bAb;
-vD = left_divide(DmA,iD);
-Db = left_divide(DmA,B');
+[vA,Ab,bAb,B,base,extr] = base_block(img);
+[vD,Db]       = extr_block(img,extr,bAb,B);
 
 vI = vD - Db*vA;
 vO = vA - Ab*vI;
+v([base(:);extr(:)],:) = [vO; vI];
 
-v = zeros(size(Q));
-v(base,:) = vO;
-v(extr,:) = vI;
+data = pack_output(img,v);
 
-idx = find(any(pp.N2E));
-data.volt = v;
-data.time= NaN; % unknown
-data.name= 'solved by block solver';
 
-v_els= pp.N2E(:,idx) * v(idx,:);
-idx=0;
-stim = fmdl.stimulation;
-for i=1:length(stim)
-   meas_pat= stim(i).meas_pattern;
-   n_meas  = size(meas_pat,1);
-   vv( idx+(1:n_meas),: ) = meas_pat*v_els(:,i);
-   idx= idx+ n_meas;
-end
-data.meas= vv;
+function [vA,Ab,bAb,B,base,extr] = base_block(img);
+   fmdl = img.fwd_model;
+   pp= fwd_model_parameters(fmdl);
+   E = calc_system_mat(img);       E=E.E;
+
+   Nnode= num_nodes(img);
+   base = [fmdl.remesh_solver.base_nodes(:);
+     Nnode+fmdl.remesh_solver.base_elecs(:)];
+   extr = 1:size(E,1); extr(base) = [];
+   base(base == fmdl.gnd_node) = [];
+
+   A = E(base,base);
+   B = E(base,extr);
+   iA= pp.QQ(base,:);
+
+   vA = left_divide(A,iA);
+   Ab = left_divide(A,B);
+   bAb= B'*Ab;
+
+function [vD,Db] = extr_block(img,extr,bAb,B);
+   pp= fwd_model_parameters(img.fwd_model);
+   E = calc_system_mat(img);       E=E.E;
+
+   iD= pp.QQ(extr,:);
+   D = E(extr,extr);
+   DmA= D - bAb;
+   vD = left_divide(DmA,iD);
+   Db = left_divide(DmA,B');
+
+function data = pack_output(img,v)
+   fmdl = img.fwd_model;
+   pp = fwd_model_parameters(fmdl);
+
+   idx = find(any(pp.N2E));
+   data.volt = v;
+   data.time= NaN; % unknown
+   data.name= 'solved by block solver';
+
+   v_els= pp.N2E(:,idx) * v(idx,:);
+   idx=0;
+   stim = fmdl.stimulation;
+   for i=1:length(stim)
+      meas_pat= stim(i).meas_pattern;
+      n_meas  = size(meas_pat,1);
+      vv( idx+(1:n_meas),: ) = meas_pat*v_els(:,i);
+      idx= idx+ n_meas;
+   end
+   data.meas= vv;
 
 
 function do_unit_test
@@ -71,3 +84,4 @@ img.fwd_model.remesh_solver.base_elecs = 1:4;
 vn=remesh_solver(img);
 vf=fwd_solve(img);
 subplot(122);plot(vf.volt - vn.volt); box off;
+unit_test_cmp('a2C2',vf.volt,vn.volt,1e-12);
