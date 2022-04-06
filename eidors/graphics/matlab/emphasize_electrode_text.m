fff7a779379e993260063a91bd842a057faa2f0d
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2022-04-06 14:25:15 +0000

    Use viewmtx to fix direction matrix
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@6266 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/graphics/matlab/emphasize_electrode_text.m
+++ eidors/graphics/matlab/emphasize_electrode_text.m
@@ -17,7 +17,9 @@ bgcolour = [1,1,1];
 % Don't really understand the view matrix
 %tmp = inv(view);
  tmp = view;
-dirn = tmp(1:3,3); dirn =  dirn'/norm(dirn);
+dirn = [0,0,1,0]*tmp; % move into view's z dir
+dirn = dirn(1:3) 
+dirn = -dirn(:)'/norm(dirn);
 
 if nargin==0;
   elecs= 1:length(hh)'; % all of them
@@ -33,17 +35,20 @@ k=0;for hi = hh(:)'; k=k+1;
   set(hi, 'BackgroundColor', bgcolour);
   set(hi, 'FontSize', 12);
   pn = get(hi, 'Position');
-  set(hi, 'Position', pn + dirn*10);
+  set(hi, 'Position', pn + dirn*1 );
 end
 
 function do_unit_test
   fmdl = getfield(mk_common_model('n3r2',[16,2]),'fwd_model');
   subplot(221);
-  show_fem(fmdl,[0,1]); view(0,0);
+  show_fem(fmdl,[0,1]); view(0,0); drawnow
+tmp=view
   emphasize_electrode_text();
   subplot(222);
-  show_fem(fmdl,[0,1]); view(0,80);
+  show_fem(fmdl,[0,1]); view(0,80); drawnow
+tmp=view
   emphasize_electrode_text();
   subplot(223);
-  show_fem(fmdl,[0,1]); view(50,80);
+  show_fem(fmdl,[0,1]); view(50,80); drawnow
+tmp=view
   emphasize_electrode_text();
