4a8e4c1dc0e2a52bbbd2e92eb55520625159495b
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2022-04-07 07:41:51 +0000

    gmsh_stl2tet: Add support for struct input (with caching)
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@6276 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/models/gmsh_stl2tet.m
+++ eidors/models/gmsh_stl2tet.m
@@ -2,15 +2,23 @@ function mdl = gmsh_stl2tet(stlfile, maxh, extra)
 %GMSH_STL2TET creates a tetrahedral mesh from an stl file
 % mdl = gmsh_stl2tet(stlfile, maxh, extra) where:
 %        mdl - EIDORS fwd_model struct
-%    stlfile - path to stl file
+%    stlfile - either:
+%                 - a path to an stl file
+%               OR
+%                 - a struct with .vertices and .faces 
 %       maxh - maximum edge length (default: coarse mesh)
 %      extra - extra command line options to gmsh
 %
+% If stlfile is a struct, stl_write will be called first and an STL file
+% written in a temporary location. 
+%
+% CASHING: Calls are cashed iff stlfile is a struct.
+%
 % NOTE: Only one surface per file is allowed.
 %
-% See also CALL_GMSH
+% See also CALL_GMSH, STL_WRITE
 
-% (C) Bartlomiej Grychtol, 2012.
+% (C) Bartlomiej Grychtol, 2012-2021.
 % $Id$
 
 if nargin < 3
@@ -20,10 +28,27 @@ if nargin > 1 && ~isempty(maxh)
    extra = [' -clmax ' num2str(maxh), ' ', extra];
 end
 
-stem = strrep(stlfile,'.stl','');
+if isstruct(stlfile)
+    opt.cache_obj = {stlfile.vertices, stlfile.faces};
+    mdl = eidors_cache(@do_gmsh_stl2tet,{stlfile, extra}, opt);
+else
+    mdl = do_gmsh_stl2tet(stlfile, extra);
+end
+
+
+
+function mdl = do_gmsh_stl2tet(stlfile, extra)
+if isstruct(stlfile)
+    stem = tempname;
+    stl_write(stlfile, [stem, '.stl'])
+    stlname = [stem '.stl'];
+else
+    stem = strrep(stlfile,'.stl','');
+    stlname = stlfile;
+end
 %TODO: Some of this could be exposed as options (Algorithm, Optimize, ...)
 fid = fopen([stem '.geo'],'w');
-fprintf(fid,'Merge "%s";\n',stlfile);
+fprintf(fid,'Merge "%s";\n',stlname);
 fprintf(fid,'Surface Loop(1) = {1};\n');
 fprintf(fid,'Volume(2) = {1};\n');
 fprintf(fid,'Physical Volume(3) = {2};\n');
@@ -34,3 +59,9 @@ fclose(fid);
 call_gmsh([stem '.geo'], 3, extra);
 
 mdl = gmsh_mk_fwd_model([stem '.msh'],[],[],[]);
+
+delete([stem '.geo']);
+delete([stem '.msh']);
+if isstruct(stlfile)
+    delete(stlname);
+end
