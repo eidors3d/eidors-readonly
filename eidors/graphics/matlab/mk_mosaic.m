57ebe0a0eef6e3568bd5e0ca4bd578282cc20605
bgrychtol <bgrychtol@82006732-fb7e-4a22-9f28-86a212cce563>
2022-04-21 20:43:32 +0000

    Fix processing of multiple frames when vh is provided
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@6335 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/graphics/matlab/mk_mosaic.m
+++ eidors/graphics/matlab/mk_mosaic.m
@@ -21,6 +21,8 @@ function r_img = mk_mosaic(rimg, sep, vh, n_col)
 % License: GPL v2 or v3.
 % $Id$
 
+if nargin==1 && ischar(rimg) && strcmp(rimg, 'UNIT_TEST'), do_unit_test, return, end
+
 % jnk so that matab doesn't put larger dims in npy
 [npy,npx,jnk] = size(rimg);
 n_frames = size(rimg,3);
@@ -35,8 +37,9 @@ end
 if nargin < 4 
     n_col = 0;
 end
+vert_rows = 0;
 if nargin > 2 && ~isempty(vh)
-    img_cols = max( vh(:,1) );
+    img_cols = n_frames * max( vh(:,1) );
     img_rows = max( vh(:,2) );
 else
     % vertical slices must be kept together
@@ -62,15 +65,33 @@ imno= 1;
 for img_no = 1:n_frames
    for lev_no = 1:n_levels
       if ~isempty(vh) %won't work for multiple image inputs
-         i_col= vh( lev_no, 1) + img_no -1;
+         i_col= n_frames*(vh( lev_no, 1)-1) + img_no;
          i_row= vh( lev_no, 2);
       else
          i_col= rem( img_no-1, img_cols) + 1;
          i_row= (ceil( img_no / img_cols) -1) * n_levels + lev_no ;
       end
-% disp([imno, vert_rows, img_cols, img_rows, img_no, lev_no, i_col, i_row]);
+%       disp([imno, vert_rows, img_cols, img_rows, img_no, lev_no, i_col, i_row]);
       r_img(i_row*npy + idxy + sep*(i_row-1), ...
             i_col*npx + idxx + sep*(i_col-1)) = rimg(:,:,img_no,lev_no);
       imno= imno + 1; 
    end
 end
+
+function do_unit_test
+   img=calc_jacobian_bkgnd(mk_common_model('n3r2',[16,2]));
+   img.calc_colours.npoints= 16;
+   img.elem_data=toeplitz(1:size(img.fwd_model.elems,1),1);
+   img.elem_data = img.elem_data(:,[1,1]);
+   levels=[inf,inf,1,1,1;
+           0,inf,inf,2,1;
+           0,1,inf,  3,1];
+   rimg= calc_slices( img, levels(:,1:3) );
+   msk = mk_mosaic(rimg, 0, levels(:,4:5));
+   subplot(3,1,1)
+   imagesc(msk), axis equal tight
+   
+   msk = mk_mosaic(rimg);
+   subplot(3,1,2)
+   imagesc(msk), axis equal tight
+   
