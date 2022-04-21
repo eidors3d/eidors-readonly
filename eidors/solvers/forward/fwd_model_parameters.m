94dbd4f7ad2bc101fe30de127fa4eeb993a5550b
aadler <aadler@82006732-fb7e-4a22-9f28-86a212cce563>
2022-04-21 11:31:54 +0000

    Only calculate v2meas if .stimulation field is there
    git-svn-id: svn+ssh://svn.code.sf.net/p/eidors3d/code/trunk@6318 82006732-fb7e-4a22-9f28-86a212cce563

--- eidors/solvers/forward/fwd_model_parameters.m
+++ eidors/solvers/forward/fwd_model_parameters.m
@@ -86,9 +86,11 @@ copt.cache_obj = {fwd_model.nodes,fwd_model.elems,fwd_model.electrode};
 copt.fstr = 'calculate_N2E';
 [N2E,cem_electrodes] = eidors_cache(@calculate_N2E,{fwd_model, bdy, n_elec, n}, copt);
 
-stim = fwd_model.stimulation;
 if p>0
+  stim = fwd_model.stimulation;
   [pp.QQ, pp.VV, pp.n_meas] = calc_QQ_fast(N2E, stim, p);
+
+  pp.v2meas   = get_v2meas(pp.n_elec, pp.n_stim, stim);
 end
 
 % pack into a parameter return list
@@ -100,8 +102,6 @@ pp.n_dims   = d-1;
 pp.N2E      = N2E;
 pp.boundary = bdy;
 pp.normalize = mdl_normalize(fwd_model);
-
-pp.v2meas   = get_v2meas(pp.n_elec, pp.n_stim, stim);
 
 function v2meas = get_v2meas(n_elec,n_stim,stim)
     v2meas = sparse(n_elec*n_stim,0);
